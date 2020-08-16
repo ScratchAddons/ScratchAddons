5478ea7b53798a3c80982b221cfaa6c79edc4e48
World_Languages <mundofinky@gmail.com>
2020-08-16 20:01:25 -0300

    Huh why wasn't this previously uploaded?

---
+++ webpages/settings/index.js
@@ -0,0 +1,11 @@
+const vue = new Vue({
+    el: ".main",
+    data: {
+      addons: []
+    }
+});
+
+chrome.runtime.sendMessage("getSettingsInfo", ({manifests}) => {
+    vue.addons = manifests;
+    console.log(manifests);
+});
