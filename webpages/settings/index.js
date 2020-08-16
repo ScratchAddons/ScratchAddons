2fbe8a8e937c33eee2284712910612a78f80fd54
WorldLanguages <WorldLanguages@users.noreply.github.com>
2020-08-16 23:02:02 +0000

    style: format code

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -1,11 +1,11 @@
 const vue = new Vue({
-    el: ".main",
-    data: {
-      addons: []
-    }
+  el: ".main",
+  data: {
+    addons: [],
+  },
 });
 
-chrome.runtime.sendMessage("getSettingsInfo", ({manifests}) => {
-    vue.addons = manifests;
-    console.log(manifests);
+chrome.runtime.sendMessage("getSettingsInfo", ({ manifests }) => {
+  vue.addons = manifests;
+  console.log(manifests);
 });
