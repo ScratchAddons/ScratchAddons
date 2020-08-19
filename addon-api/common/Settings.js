3a89ed037623836eb9129c76cbba916b03e69fac
WorldLanguages <WorldLanguages@users.noreply.github.com>
2020-08-19 21:37:08 +0000

    style: format code

--- addon-api/common/Settings.js
+++ addon-api/common/Settings.js
@@ -5,9 +5,11 @@ export default class Settings extends EventTarget {
     scratchAddons.eventTargets.settings.push(this);
   }
   get(optionName) {
-    const settingsObj = scratchAddons.globalState.addonSettings[this._addonId] || {};
+    const settingsObj =
+      scratchAddons.globalState.addonSettings[this._addonId] || {};
     const value = settingsObj[optionName];
-    if (value === undefined) throw "ScratchAddons exception: invalid setting ID";
+    if (value === undefined)
+      throw "ScratchAddons exception: invalid setting ID";
     else return value;
   }
   _removeEventListeners() {
