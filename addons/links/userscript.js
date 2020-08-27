47481296222c4efac0c74285f82cfd30aaa3ebdb
JeffaloBob <JeffaloBob@users.noreply.github.com>
2020-08-27 06:55:00 +0000

    style: format code

--- addons/links/userscript.js
+++ addons/links/userscript.js
@@ -1,28 +1,27 @@
 export default async function ({ addon, global, console }) {
-    function linkify(inputText) {
-        var replacedText, replacePattern1, replacePattern2, replacePattern3;
+  function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-        //URLs starting with http://, https://, or ftp://
-        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-        //Change email addresses to mailto:: links.
-        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
-        if (inputText.includes('https://cdn.discordapp.com/emojis/')) {
-            return inputText
-        } else {
-            return replacedText;
-
-        }
+    if (inputText.includes("https://cdn.discordapp.com/emojis/")) {
+      return inputText;
+    } else {
+      return replacedText;
     }
+  }
 
-    document.querySelectorAll('div').forEach(i=>{
-        i.innerHTML=linkify(i.innerHTML)
-    })
+  document.querySelectorAll("div").forEach((i) => {
+    i.innerHTML = linkify(i.innerHTML);
+  });
 }
