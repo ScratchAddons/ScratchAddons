5214b405b52e0f9cb3ac3ee46e36f96e37a0a1b2
JeffaloBob <JeffaloBob@users.noreply.github.com>
2020-08-27 06:36:20 +0000

    style: format code

--- addons/links/userscript.js
+++ addons/links/userscript.js
@@ -1,7 +1,7 @@
-import "../../libraries/linkify.js"
-import "../../libraries/linkify-jquery.js"
+import "../../libraries/linkify.js";
+import "../../libraries/linkify-jquery.js";
 
 export default async function ({ addon, global, console }) {
-    $('p').linkify()
-    $('div').linkify()
+  $("p").linkify();
+  $("div").linkify();
 }
