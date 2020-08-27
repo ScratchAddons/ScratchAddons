5c75ebd7d1feca299dcec4e33a7128f9badc4707
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-08-27 08:52:52 +0200

    replace linkify with a worse linkify

--- addons/links/userscript.js
+++ addons/links/userscript.js
@@ -1,7 +1,28 @@
-import "../../libraries/linkify.js"
-import "../../libraries/linkify-jquery.js"
-
 export default async function ({ addon, global, console }) {
-    $('p').linkify()
-    $('div').linkify()
+    function linkify(inputText) {
+        var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+        //URLs starting with http://, https://, or ftp://
+        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+        //Change email addresses to mailto:: links.
+        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+        if (inputText.includes('https://cdn.discordapp.com/emojis/')) {
+            return inputText
+        } else {
+            return replacedText;
+
+        }
+    }
+
+    document.querySelectorAll('div').forEach(i=>{
+        i.innerHTML=linkify(i.innerHTML)
+    })
 }
