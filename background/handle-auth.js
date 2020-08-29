37409643fe67d0d7b0c6e4d4de10a99a4c78cf2c
World_Languages <mundofinky@gmail.com>
2020-08-29 13:03:53 -0300

    Add scratchLang, recheck cookies on csrftoken/lang change (#120)

--- background/handle-auth.js
+++ background/handle-auth.js
@@ -3,6 +3,11 @@
   scratchAddons.localState.ready.auth = true;
 })();
 
+chrome.cookies.onChanged.addListener(({ cookie, changeCause }) => {
+  if (cookie.name === "scratchsessionsid" || cookie.name === "scratchlanguage" || cookie.name === "scratchcsrftoken")
+    checkSession();
+});
+
 function checkSession() {
   return new Promise(async (resolve) => {
     const res = await fetch("https://scratch.mit.edu/session/", {
@@ -18,25 +23,38 @@ function checkSession() {
         userId: null,
         xToken: null,
         csrfToken: null,
+        scratchLang: null,
       };
       resolve();
     } else {
       let csrfToken = null;
+      let scratchLang = null;
       chrome.cookies.get(
         {
           url: "https://scratch.mit.edu/",
           name: "scratchcsrftoken",
         },
         (cookie) => {
-          if (cookie.value) csrfToken = cookie.value;
-          scratchAddons.globalState.auth = {
-            isLoggedIn: true,
-            username: json.user.username,
-            userId: json.user.id,
-            xToken: json.user.token,
-            csrfToken,
-          };
-          resolve();
+          if (cookie && cookie.value) csrfToken = cookie.value;
+          chrome.cookies.get(
+            {
+              url: "https://scratch.mit.edu/",
+              name: "scratchlanguage",
+            },
+            (cookie) => {
+              if (cookie && cookie.value) scratchLang = cookie.value;
+              else scratchLang = navigator.language;
+              scratchAddons.globalState.auth = {
+                isLoggedIn: true,
+                username: json.user.username,
+                userId: json.user.id,
+                xToken: json.user.token,
+                csrfToken,
+                scratchLang,
+              };
+              resolve();
+            }
+          );
         }
       );
     }
