9f5824893222b9868ba241b06c4cb775afd58cde
NoobTracker <NoobTracker@users.noreply.github.com>
2020-09-02 17:36:46 +0000

    style: format code

--- addons/third-party-button/userscript.js
+++ addons/third-party-button/userscript.js
@@ -2,26 +2,26 @@
   addon.settings.addEventListener("change", () => console.log("changed!"));
   const fork = document.createElement("a");
   const warp = document.createElement("a");
-  
+
   console.log(window.location.href);
   var projectid = window.location.pathname.split("/")[2];
   console.log(projectid);
-  
+
   fork.innerHTML = "<div style='margin:auto;color:white;'>forkphorus</div>";
   fork.href = "https://forkphorus.github.io/#" + projectid;
   fork.className = "button action-button";
   fork.style = "display:flex;";
-  
+
   warp.innerHTML = "<div style='margin:auto;color:white;'>TurboWarp</div>";
   warp.href = "https://turbowarp.github.io/#" + projectid;
   warp.className = "button action-button";
   warp.style = "display:flex;";
-  
+
   if (addon.settings.get("colors")) {
     fork.style = "display:flex;background-color:black;";
-    warp.style = "display:flex;background-color:red;";//if you know a better color, tell me.
+    warp.style = "display:flex;background-color:red;"; //if you know a better color, tell me.
   }
-  
+
   if (addon.settings.get("forkphorus")) {
     document.getElementsByClassName("action-buttons")[0].appendChild(fork);
   }
