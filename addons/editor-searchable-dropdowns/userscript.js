49aeac05ad900219d20766c234040030f911064e
Thomas Weber <weber_t@mailbox.org>
2020-09-02 16:27:31 -0500

    Just remembered that the addon is supposed to export a function

--- addons/editor-searchable-dropdowns/userscript.js
+++ addons/editor-searchable-dropdowns/userscript.js
@@ -1,63 +1,66 @@
-function callback(mutationList) {
-  for (const mutation of mutationList) {
-    if (mutation.type === "childList") {
-      const newNodes = mutation.addedNodes;
-      for (const node of newNodes) {
-        if (node.classList && node.classList.contains("blocklyDropdownMenu")) {
-          addSearch();
-          break;
+export default async function ({ addon, global, console }) {
+  function callback(mutationList) {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        const newNodes = mutation.addedNodes;
+        for (const node of newNodes) {
+          if (node.classList && node.classList.contains("blocklyDropdownMenu")) {
+            addSearch();
+            break;
+          }
         }
       }
     }
   }
-}
 
-function addSearch() {
-  const el = document.createElement("input");
-  el.type = "text";
-  el.addEventListener("input", search);
-  el.classList.add("u-dropdown-searchbar");
-  const container = getDropDownMenu();
-  container.insertBefore(el, container.firstChild);
-  el.focus();
+  function addSearch() {
+    const el = document.createElement("input");
+    el.type = "text";
+    el.addEventListener("input", search);
+    el.classList.add("u-dropdown-searchbar");
+    const container = getDropDownMenu();
+    container.insertBefore(el, container.firstChild);
+    el.focus();
 
-  for (const child of getItems()) {
-    child.hidden = false;
+    for (const child of getItems()) {
+      child.hidden = false;
+    }
   }
-}
 
-function search(e) {
-  const value = e.target.value.toLowerCase();
-  for (const child of getItems()) {
-    const text = child.textContent.toLowerCase();
-    const contains = text.includes(value);
-    child.hidden = !contains;
+  function search(e) {
+    const value = e.target.value.toLowerCase();
+    for (const child of getItems()) {
+      const text = child.textContent.toLowerCase();
+      const contains = text.includes(value);
+      child.hidden = !contains;
+    }
   }
-}
 
-let cachedDropDownContentElement = null;
-function getDropDownContentElement() {
-  if (cachedDropDownContentElement) {
+  let cachedDropDownContentElement = null;
+  function getDropDownContentElement() {
+    if (cachedDropDownContentElement) {
+      return cachedDropDownContentElement;
+    }
+    cachedDropDownContentElement = document.querySelector(".blocklyDropDownContent");
     return cachedDropDownContentElement;
   }
-  cachedDropDownContentElement = document.querySelector(".blocklyDropDownContent");
-  return cachedDropDownContentElement;
-}
 
-function getDropDownMenu() {
-  return getDropDownContentElement().querySelector(".blocklyDropdownMenu");
-}
-
-function getItems() {
-  const el = getDropDownMenu();
-  if (el) {
-    return Array.from(el.children).filter((child) => child.tagName !== "INPUT");
+  function getDropDownMenu() {
+    return getDropDownContentElement().querySelector(".blocklyDropdownMenu");
   }
-  return [];
-}
 
-const observer = new MutationObserver(callback);
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-});
+  function getItems() {
+    const el = getDropDownMenu();
+    if (el) {
+      return Array.from(el.children).filter((child) => child.tagName !== "INPUT");
+    }
+    return [];
+  }
+
+  const observer = new MutationObserver(callback);
+  // todo: don't put a MutationObserver on the entire body
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+}
