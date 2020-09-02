6f79130952bfdb4714286767b64b0a29685ce87c
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-09-02 09:24:16 -0400

    Create userscript.js

---
+++ addons/turbo-editor/userscript.js
@@ -0,0 +1,24 @@
+export default async function({ addon, global, console }) {
+    console.log("Turbowarp Scratch Mod (c) 2020 YOYITsM3M8 has loaded");
+    var current_url = document.location; // get url
+    var id = current_url.href.split('/')[4]; // get project id from url
+    function check() { // check if page loaded
+        const el = document.querySelector('.stage-wrapper_stage-wrapper_2bejr');
+        try {
+            if (el.classList[0] == "stage-wrapper_stage-wrapper_2bejr") {
+                el.removeChild(el.childNodes[0]);
+                el.removeChild(el.childNodes[0]);
+                var turboframe = document.createElement("iframe");
+                turboframe.src="https://turbowarp.org/fullscreen.html#"+id;
+                turboframe.width="482.22";
+                turboframe.height="406.22";
+                el.appendChild(turboframe);
+            } else {
+                setTimeout(check, 300); // try again in 300 milliseconds
+            }
+        }
+        catch(err) {setTimeout(check, 300);console.log(err);/* try again in 300 milliseconds */}
+    }
+    check()
+
+};
