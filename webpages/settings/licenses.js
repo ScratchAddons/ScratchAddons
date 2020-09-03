a2db96010ea56a805d98010898de3753d12f99af
apple502j <apple502j@users.noreply.github.com>
2020-09-03 06:30:51 +0000

    style: format code

--- webpages/settings/licenses.js
+++ webpages/settings/licenses.js
@@ -1,36 +1,40 @@
 const vue = new Vue({
   el: "body",
   data: {
-    libraries: []
+    libraries: [],
   },
-  methods: {}
+  methods: {},
 });
 
 chrome.runtime.sendMessage("getLibraryInfo", (libraryLicenses) => {
-
   const licenseNameToText = {};
-  const searchParams = (new URL(location.href)).searchParams;
-  const libraryParam = searchParams.get('libraries');
-  if (typeof libraryParam !== 'string') return;
-  const libraries = libraryParam.split(',');
+  const searchParams = new URL(location.href).searchParams;
+  const libraryParam = searchParams.get("libraries");
+  if (typeof libraryParam !== "string") return;
+  const libraries = libraryParam.split(",");
   console.log(libraryLicenses, libraries);
   for (const library of libraries) {
     const licenseName = libraryLicenses[library];
     if (!licenseName) continue;
     if (licenseNameToText.hasOwnProperty(licenseName)) {
-      vue.libraries = [...vue.libraries, {
-        name: library,
-        license: licenseNameToText[licenseName]
-      }];
+      vue.libraries = [
+        ...vue.libraries,
+        {
+          name: library,
+          license: licenseNameToText[licenseName],
+        },
+      ];
       continue;
     }
-    chrome.runtime.sendMessage({licenseName}, ({licenseText}) => {
+    chrome.runtime.sendMessage({ licenseName }, ({ licenseText }) => {
       licenseNameToText[licenseName] = licenseText;
-      vue.libraries = [...vue.libraries, {
-        name: library,
-        license: licenseText
-      }];
+      vue.libraries = [
+        ...vue.libraries,
+        {
+          name: library,
+          license: licenseText,
+        },
+      ];
     });
   }
-
 });
