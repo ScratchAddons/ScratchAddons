10aa94373a105cbbb2df334b8fa952ed07f5f0d0
NoobTracker <NoobTracker@users.noreply.github.com>
2020-09-03 11:34:58 +0000

    style: format code

--- addons/third-party-button/userscript.js
+++ addons/third-party-button/userscript.js
@@ -1,11 +1,11 @@
 ﻿export default async function ({ addon, global, console }) {
   addon.settings.addEventListener("change", () => console.log("changed!"));
   await addon.tab.waitForElement(".action-buttons");
-   
+
   var stageid = -1;
-  function replaceStage(e, url, w, h, id){
+  function replaceStage(e, url, w, h, id) {
     e.preventDefault(); //This could be moved into the if so you can click twice on the button to get to the site
-    if(id != stageid){
+    if (id != stageid) {
       stageid = id;
       //await addon.tab.waitForElement(".guiPlayer"); //That creates an error for some reason
       var iframe = document.createElement("iframe");
@@ -20,7 +20,7 @@
       el.replaceChild(iframe, el.childNodes[0]);
     }
   }
-   
+
   const fork = document.createElement("a");
   const warp = document.createElement("a");
 
@@ -32,13 +32,17 @@
   fork.href = "https://forkphorus.github.io/#" + projectid;
   fork.className = "button action-button";
   fork.style = "display:flex;";
-  fork.addEventListener("click", e => replaceStage(e, "https://forkphorus.github.io/embed.html?auto-start=false#", 482, 393, 0));
+  fork.addEventListener("click", (e) =>
+    replaceStage(e, "https://forkphorus.github.io/embed.html?auto-start=false#", 482, 393, 0)
+  );
 
   warp.innerHTML = "<div style='margin:auto;color:white;'>TurboWarp</div>";
   warp.href = "https://turbowarp.github.io/#" + projectid;
   warp.className = "button action-button";
   warp.style = "display:flex;";
-  warp.addEventListener("click", e => replaceStage(e, "https://turbowarp.org/embed.html?auto-start=false#", 482, 393, 1));
+  warp.addEventListener("click", (e) =>
+    replaceStage(e, "https://turbowarp.org/embed.html?auto-start=false#", 482, 393, 1)
+  );
 
   if (addon.settings.get("colors")) {
     fork.style = "display:flex;background-color:#1f313e;";
