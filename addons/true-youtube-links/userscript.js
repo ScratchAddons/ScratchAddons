ab0202959c0f7f63aeeb8f85e2368bb99942ad2f
NoobTracker <NoobTracker@users.noreply.github.com>
2020-09-04 12:32:55 +0000

    style: format code

--- addons/true-youtube-links/userscript.js
+++ addons/true-youtube-links/userscript.js
@@ -1,9 +1,9 @@
 ﻿export default async function ({ addon, global, console }) {
   addon.settings.addEventListener("change", () => console.log("changed!"));
-  while(true){
+  while (true) {
     await addon.tab.waitForElement("a:not(.trueYTLinksViewed)");
     var element = document.querySelector("a:not(.trueYTLinksViewed)");
-    if(element.href.indexOf("https://scratch.mit.edu/discuss/youtube/") == 0){
+    if (element.href.indexOf("https://scratch.mit.edu/discuss/youtube/") == 0) {
       element.href = element.href.replace("https://scratch.mit.edu/discuss/youtube/", "https://youtu.be/");
     }
     element.classList.add("trueYTLinksViewed");
