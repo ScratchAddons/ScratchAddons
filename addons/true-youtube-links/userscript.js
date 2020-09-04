4aa42c22502b4e1926a9038b8390bb53199d78be
NoobTracker <63962365+NoobTracker@users.noreply.github.com>
2020-09-04 14:32:16 +0200

    Replaces every /discuss/youtube/ link
    ... with a real youtu.be link.

---
+++ addons/true-youtube-links/userscript.js
@@ -0,0 +1,11 @@
+﻿export default async function ({ addon, global, console }) {
+  addon.settings.addEventListener("change", () => console.log("changed!"));
+  while(true){
+    await addon.tab.waitForElement("a:not(.trueYTLinksViewed)");
+    var element = document.querySelector("a:not(.trueYTLinksViewed)");
+    if(element.href.indexOf("https://scratch.mit.edu/discuss/youtube/") == 0){
+      element.href = element.href.replace("https://scratch.mit.edu/discuss/youtube/", "https://youtu.be/");
+    }
+    element.classList.add("trueYTLinksViewed");
+  }
+}
