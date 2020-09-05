77ec2f6d0fe1984ed91c6d24e5bc83f35a549ec3
NoobTracker <63962365+NoobTracker@users.noreply.github.com>
2020-09-05 18:33:53 +0200

    Add the visibility of line breaks
    ... and removes some annoying stuff.

---
+++ addons/comments-line-break/userscript.js
@@ -0,0 +1,12 @@
+﻿export default async function ({ addon, global, console }) {
+  addon.settings.addEventListener("change", () => console.log("changed!"));
+
+  while (true) {
+    await addon.tab.waitForElement(".comment-content:not(.markdownForCommentsViewed),.comment .content:not(.markdownForCommentsViewed)");
+    var element = document.querySelector(".comment-content:not(.markdownForCommentsViewed),.comment .content:not(.markdownForCommentsViewed)");
+    element.classList.add("markdownForCommentsViewed");
+    element.style="white-space:break-spaces;";
+
+    element.textContent = element.textContent.slice(22, element.textContent.length-12);
+  }
+}
