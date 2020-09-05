e268944c183fddf743bfd82ddf58f6ce93a40fb0
Hans5958 <email5958.1@gmail.com>
2020-09-05 19:34:52 +0700

    Fix Prettier issues

--- addons/more-links/userscript.js
+++ addons/more-links/userscript.js
@@ -2,9 +2,8 @@ import "/libraries/linkify.min.js";
 import "/libraries/linkify-element.min.js";
 
 export default async function ({ addon }) {
-
-  const pageType = document.location.pathname.substr(1).split("/")[0]
-  let comments
+  const pageType = document.location.pathname.substr(1).split("/")[0];
+  let comments;
 
   switch (pageType) {
     case "users":
@@ -23,12 +22,11 @@ export default async function ({ addon }) {
   }
 
   while (true) {
-    await addon.tab.waitForElement(".comment:not(.more-links-checked)")
-    comments = document.querySelectorAll(".comment:not(.more-links-checked)")
-    comments.forEach(comment => {
-      linkifyElement(comment)
-      comment.classList.add("more-links-checked")
-    })
+    await addon.tab.waitForElement(".comment:not(.more-links-checked)");
+    comments = document.querySelectorAll(".comment:not(.more-links-checked)");
+    comments.forEach((comment) => {
+      linkifyElement(comment);
+      comment.classList.add("more-links-checked");
+    });
   }
-
 }
