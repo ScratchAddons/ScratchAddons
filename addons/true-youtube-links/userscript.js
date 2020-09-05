b1d5cd065027e6477070b63ed052b71efd96948c
Hans5958 <email5958.1@gmail.com>
2020-09-05 19:35:29 +0700

    Fix Prettier issues

--- addons/true-youtube-links/userscript.js
+++ addons/true-youtube-links/userscript.js
@@ -1,13 +1,14 @@
 ﻿export default async function ({ addon, console }) {
-
   addon.settings.addEventListener("change", () => console.log("changed!"));
 
   while (true) {
     await addon.tab.waitForElement("a[href^=https://scratch.mit.edu/discuss/youtube/");
     var elements = document.querySelectorAll("a[href^=https://scratch.mit.edu/discuss/youtube/]");
-    elements.forEach(element => {
-      element.href = element.href.replace("https://scratch.mit.edu/discuss/youtube/", "https://www.youtube.com/watch?v=");
-    })
+    elements.forEach((element) => {
+      element.href = element.href.replace(
+        "https://scratch.mit.edu/discuss/youtube/",
+        "https://www.youtube.com/watch?v="
+      );
+    });
   }
-
 }
