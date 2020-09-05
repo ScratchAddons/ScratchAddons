e340a44b7466f932ee6f9dcbe6085709a6165bb0
NoobTracker <63962365+NoobTracker@users.noreply.github.com>
2020-09-05 18:46:46 +0200

    It gets messy ...
    but there were some things I had to fix.

--- addons/comments-line-break/userscript.js
+++ addons/comments-line-break/userscript.js
@@ -2,15 +2,10 @@
   addon.settings.addEventListener("change", () => console.log("changed!"));
 
   while (true) {
-    await addon.tab.waitForElement(
-      ".comment-content:not(.markdownForCommentsViewed),.comment .content:not(.markdownForCommentsViewed)"
-    );
-    var element = document.querySelector(
-      ".comment-content:not(.markdownForCommentsViewed),.comment .content:not(.markdownForCommentsViewed)"
-    );
-    element.classList.add("markdownForCommentsViewed");
-    element.style = "white-space:break-spaces;";
-
-    element.textContent = element.textContent.slice(22, element.textContent.length - 12);
+    await addon.tab.waitForElement(".comment-content:not(.commentsLineBreaksViewed),.comment .content:not(.commentsLineBreaksViewed)");
+    var element = document.querySelector(".comment-content:not(.commentsLineBreaksViewed),.comment .content:not(.commentsLineBreaksViewed)");
+    element.classList.add("commentsLineBreaksViewed");
+    element.style="white-space:break-spaces;";
+    element.textContent = element.textContent.slice(22, element.textContent.length-12);
   }
 }
