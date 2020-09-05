ca2e7910c29210a495697c0587bea314ae23628e
Hans5958 <email5958.1@gmail.com>
2020-09-05 20:29:47 +0700

    Add quotation marks

--- addons/true-youtube-links/userscript.js
+++ addons/true-youtube-links/userscript.js
@@ -2,8 +2,8 @@
   addon.settings.addEventListener("change", () => console.log("changed!"));
 
   while (true) {
-    await addon.tab.waitForElement("a[href^=https://scratch.mit.edu/discuss/youtube/");
-    var elements = document.querySelectorAll("a[href^=https://scratch.mit.edu/discuss/youtube/]");
+    await addon.tab.waitForElement('a[href^="https://scratch.mit.edu/discuss/youtube/"]');
+    var elements = document.querySelectorAll('a[href^="https://scratch.mit.edu/discuss/youtube/"]');
     elements.forEach((element) => {
       element.href = element.href.replace(
         "https://scratch.mit.edu/discuss/youtube/",
