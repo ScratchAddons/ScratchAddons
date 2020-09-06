f8421549c402dce042d975ef683f146c29e83692
NoobTracker <NoobTracker@users.noreply.github.com>
2020-09-06 16:57:15 +0000

    style: format code

--- addons/comments-line-break/userscript.js
+++ addons/comments-line-break/userscript.js
@@ -2,23 +2,26 @@
   addon.settings.addEventListener("change", () => console.log("changed!"));
 
   while (true) {
-    await addon.tab.waitForElement(".comment-content:not(.commentsLineBreaksViewed),.comment .content:not(.commentsLineBreaksViewed)");
-    var element = document.querySelector(".comment-content:not(.commentsLineBreaksViewed),.comment .content:not(.commentsLineBreaksViewed)");
-    element.style="white-space:break-spaces;";
+    await addon.tab.waitForElement(
+      ".comment-content:not(.commentsLineBreaksViewed),.comment .content:not(.commentsLineBreaksViewed)"
+    );
+    var element = document.querySelector(
+      ".comment-content:not(.commentsLineBreaksViewed),.comment .content:not(.commentsLineBreaksViewed)"
+    );
+    element.style = "white-space:break-spaces;";
 
-    if(document.querySelector(".comment .content:not(.commentsLineBreaksViewed)")){
+    if (document.querySelector(".comment .content:not(.commentsLineBreaksViewed)")) {
       /*element.textContent = element.textContent.slice(15, element.textContent.length);
       element.textContent = element.textContent.slice(0, element.textContent.indexOf('\n')) + 
                             element.textContent.slice(element.textContent.indexOf('\n') + 23, element.textContent.length);*/
     }
-
 
     //var result = '\n';
     //for(var i = 0;i < element.textContent.length;i++){
     //  result += element.textContent[i].charCodeAt(0) + ' ';
     //}
 
-    element.textContent += "\n\n" + element.innerHTML;//result;
+    element.textContent += "\n\n" + element.innerHTML; //result;
 
     element.classList.add("commentsLineBreaksViewed");
   }
