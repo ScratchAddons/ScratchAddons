a489bdad7b0c08c13aee3892dbf4a068ffa35d3d
WorldLanguages <WorldLanguages@users.noreply.github.com>
2020-09-17 22:32:00 +0000

    style: format code

--- background/transition.js
+++ background/transition.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener((details) => {
-  if(details.previousVersion && details.previousVersion !== "1.0.0") {
-    chrome.tabs.create({url: "https://scratchaddons.com/scratch-messaging-transition"});
+  if (details.previousVersion && details.previousVersion !== "1.0.0") {
+    chrome.tabs.create({ url: "https://scratchaddons.com/scratch-messaging-transition" });
   }
 });
