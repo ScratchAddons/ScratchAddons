0652d3c365a2956a4af9b30c8836403ec74149fe
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-09-26 15:48:26 +0000

    style: format code

--- addons/full-signature/expand.js
+++ addons/full-signature/expand.js
@@ -1,22 +1,24 @@
-$('.postsignature').attr("id", function(i){
-  return 'signature' + i;
+$(".postsignature").attr("id", function (i) {
+  return "signature" + i;
 });
 
-$('.postfootright').append('<button class="expand">Expand</button >');
+$(".postfootright").append('<button class="expand">Expand</button >');
 
-$('.expand').attr("id", function(i){
-  return 'expand' + i;
+$(".expand").attr("id", function (i) {
+  return "expand" + i;
 });
 
-$(".expand").click(function() {
-var id = this.id.replace('expand', '');
-var styles = '#signature'+id+"{max-height:fit-content!important;}"
-var newstyle = document.createTextNode(styles)
-var head, style;
-head = document.getElementsByTagName('head')[0];
-if (!head) { return; }
-style = document.createElement('style');
-style.type = 'text/css';
-style.appendChild(newstyle);
-head.appendChild(style);
-})
+$(".expand").click(function () {
+  var id = this.id.replace("expand", "");
+  var styles = "#signature" + id + "{max-height:fit-content!important;}";
+  var newstyle = document.createTextNode(styles);
+  var head, style;
+  head = document.getElementsByTagName("head")[0];
+  if (!head) {
+    return;
+  }
+  style = document.createElement("style");
+  style.type = "text/css";
+  style.appendChild(newstyle);
+  head.appendChild(style);
+});
