53a924baa0b7d07fbb39301724dfd6cdb62f9992
apple502j <33279053+apple502j@users.noreply.github.com>
2020-09-27 03:31:58 +0900

    Fix bugs on Chrome

--- background/handle-cookie.js
+++ background/handle-cookie.js
@@ -1,4 +1,9 @@
 // Remove Set-Cookie header that forces logout when they should not
+const specs = ['responseHeaders', 'blocking'];
+if (chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty("EXTRA_HEADERS")) {
+    specs.push('extraHeaders');
+}
+
 chrome.webRequest.onHeadersReceived.addListener(
     details => ({
         responseHeaders: details.responseHeaders.filter(
@@ -9,5 +14,5 @@ chrome.webRequest.onHeadersReceived.addListener(
         urls: ['https://scratch.mit.edu/site-api/comments/*'],
         types: ['xmlhttprequest', 'main_frame', 'image', 'sub_frame']
     },
-    ['responseHeaders', 'blocking']
+    specs
 );
