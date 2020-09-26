2a441e6acc02e10b0be0b8d4370c494104304248
apple502j <apple502j@users.noreply.github.com>
2020-09-26 18:33:00 +0000

    style: format code

--- background/handle-cookie.js
+++ background/handle-cookie.js
@@ -1,18 +1,16 @@
 // Remove Set-Cookie header that forces logout when they should not
-const specs = ['responseHeaders', 'blocking'];
+const specs = ["responseHeaders", "blocking"];
 if (chrome.webRequest.OnBeforeSendHeadersOptions.hasOwnProperty("EXTRA_HEADERS")) {
-    specs.push('extraHeaders');
+  specs.push("extraHeaders");
 }
 
 chrome.webRequest.onHeadersReceived.addListener(
-    details => ({
-        responseHeaders: details.responseHeaders.filter(
-            header => header.name.toLowerCase() !== 'set-cookie'
-        )        
-    }),
-    {
-        urls: ['https://scratch.mit.edu/site-api/comments/*'],
-        types: ['xmlhttprequest', 'main_frame', 'image', 'sub_frame']
-    },
-    specs
+  (details) => ({
+    responseHeaders: details.responseHeaders.filter((header) => header.name.toLowerCase() !== "set-cookie"),
+  }),
+  {
+    urls: ["https://scratch.mit.edu/site-api/comments/*"],
+    types: ["xmlhttprequest", "main_frame", "image", "sub_frame"],
+  },
+  specs
 );
