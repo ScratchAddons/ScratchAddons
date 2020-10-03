d144a8bafd67328980875a14f4a585d8406bc5bd
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-10-03 13:46:11 +0000

    style: format code

--- addons/full-signature/expand.js
+++ addons/full-signature/expand.js
@@ -1,39 +1,37 @@
 export default async function ({ addon, global, console }) {
-// Add id to each signature
-$(".postsignature").attr("id", function (i) {
-  return "signature" + i;
-});
+  // Add id to each signature
+  $(".postsignature").attr("id", function (i) {
+    return "signature" + i;
+  });
 
-// Add P element to each postsignature. The content is Expand Signature ▼
-$(".postsignature").prepend(
-  '<p data-expand="0" style="cursor:pointer; color: #4d97ff; font-weight: bold;" class="expand">Expand Signature ▽</p>'
-);
-  
-// They all start out closed, so it sets the value of each to 0. 
-$(".postsignature").data('expand','0');
+  // Add P element to each postsignature. The content is Expand Signature ▼
+  $(".postsignature").prepend(
+    '<p data-expand="0" style="cursor:pointer; color: #4d97ff; font-weight: bold;" class="expand">Expand Signature ▽</p>'
+  );
 
+  // They all start out closed, so it sets the value of each to 0.
+  $(".postsignature").data("expand", "0");
 
-// Add id to each expand element
-$(".expand").attr("id", function (i) {
-  return "expand" + i;
-});
+  // Add id to each expand element
+  $(".expand").attr("id", function (i) {
+    return "expand" + i;
+  });
 
-// When the expand element is clicked, it gets it's id, removes the expand bit from it.
-// Then, it adds a style element, that uses the id and adds it to the signature and the style. This expands it.
+  // When the expand element is clicked, it gets it's id, removes the expand bit from it.
+  // Then, it adds a style element, that uses the id and adds it to the signature and the style. This expands it.
 
-$(".expand").click(function () {
-  var id = this.id.replace("expand", "");
-  var fullid = this.id;
-    if ($('#signature'+id).data('expand') == '0') {
-    $('#signature'+id).attr("style","max-height:fit-content!important");
-    $('#'+fullid).text('Close Signature △');
-    $('#signature'+id).data("expand",'1');
-  } else {
+  $(".expand").click(function () {
+    var id = this.id.replace("expand", "");
+    var fullid = this.id;
+    if ($("#signature" + id).data("expand") == "0") {
+      $("#signature" + id).attr("style", "max-height:fit-content!important");
+      $("#" + fullid).text("Close Signature △");
+      $("#signature" + id).data("expand", "1");
+    } else {
       var newid = this.id.replace("expand", "");
-      $('#signature'+id).attr("style","");
-      $('#signature'+id).data("expand",'0');
-      $('#'+fullid).text('Expand Signature ▽');
-
-  }
-});
+      $("#signature" + id).attr("style", "");
+      $("#signature" + id).data("expand", "0");
+      $("#" + fullid).text("Expand Signature ▽");
+    }
+  });
 }
