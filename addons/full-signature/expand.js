983c827e2c82eb3cca6f776be53a6b2a18a4583c
Bill Shoener <nitrocipher@gmail.com>
2020-10-03 12:21:50 -0500

    Prettier formatted js file

--- addons/full-signature/expand.js
+++ addons/full-signature/expand.js
@@ -1,9 +1,13 @@
 // For every post with a signature line, add an id, and create expand button
-$(".box-content").each(function(index) {
-  $(this).find('.postmsg > .postsignature').attr('id', 'signature'+index);
-  if($(this).find('.postmsg > .postsignature').length !== 0){
-    $(this).find('.postfootright > ul').prepend(`<li class="expand" id="expand${index}"><a href="javascript:;">Expand Signature</a></li>`);
-    $(this).find('.postfootright > ul > .postreport').prepend('| ')
+$(".box-content").each(function (index) {
+  $(this)
+    .find(".postmsg > .postsignature")
+    .attr("id", "signature" + index);
+  if ($(this).find(".postmsg > .postsignature").length !== 0) {
+    $(this)
+      .find(".postfootright > ul")
+      .prepend(`<li class="expand" id="expand${index}"><a href="javascript:;">Expand Signature</a></li>`);
+    $(this).find(".postfootright > ul > .postreport").prepend("| ");
   }
 });
 
@@ -11,12 +15,12 @@ $(".box-content").each(function(index) {
 // Clicking again removes the expanded class
 $(".expand").click(function () {
   var id = this.id.replace("expand", "");
-  if($('#signature'+id).hasClass('expanded')){
-    $('#signature'+id).removeClass('expanded');
+  if ($("#signature" + id).hasClass("expanded")) {
+    $("#signature" + id).removeClass("expanded");
     $(this).html($(this).html().replace("Collapse", "Expand"));
     //console.log("Collapsed signature #"+id);
   } else {
-    $('#signature'+id).addClass('expanded');
+    $("#signature" + id).addClass("expanded");
     $(this).html($(this).html().replace("Expand", "Collapse"));
     //console.log("Expanded signature #"+id);
   }
