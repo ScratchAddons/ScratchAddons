4807b1fd4c8f2c6cca380ee487ad8f26e66ad561
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-03 15:45:15 -0400

    Update expand.js

--- addons/full-signature/expand.js
+++ addons/full-signature/expand.js
@@ -1,37 +1,47 @@
 export default async function ({ addon, global, console }) {
-  // Add id to each signature
-  $(".postsignature").attr("id", function (i) {
-    return "signature" + i;
-  });
+// Add id to each signature
+$(".postsignature").attr("id", function (i) {
+  return "signature" + i;
+});
 
-  // Add P element to each postsignature. The content is Expand Signature ▼
-  $(".postsignature").prepend(
-    '<p data-expand="0" style="cursor:pointer; color: #4d97ff; font-weight: bold;" class="expand">Expand Signature ▽</p>'
-  );
+// Add P element to each postsignature. The content is Expand Signature ▽<
+$(".box-content").each(function (index) {
+  $(this)
+    .find(".postmsg > .postsignature")
+    .attr("id", "signature" + index);
+  if ($(this).find(".postmsg > .postsignature").length !== 0) {
+    $(this)
+      .find(".postfootright > ul")
+      .prepend(`<p style="cursor:pointer; color: #4d97ff; font-weight: bold;" class="expand">Expand Signature ▽</p>`);
+    $(this).find(".postfootright > ul > .postreport").prepend("| ");
+  }
+});
 
-  // They all start out closed, so it sets the value of each to 0.
-  $(".postsignature").data("expand", "0");
 
-  // Add id to each expand element
-  $(".expand").attr("id", function (i) {
-    return "expand" + i;
-  });
+$(".postsignature").data('expand','0');
 
-  // When the expand element is clicked, it gets it's id, removes the expand bit from it.
-  // Then, it adds a style element, that uses the id and adds it to the signature and the style. This expands it.
 
-  $(".expand").click(function () {
-    var id = this.id.replace("expand", "");
-    var fullid = this.id;
-    if ($("#signature" + id).data("expand") == "0") {
-      $("#signature" + id).attr("style", "max-height:fit-content!important");
-      $("#" + fullid).text("Close Signature △");
-      $("#signature" + id).data("expand", "1");
-    } else {
+// Add id to each expand element
+$(".expand").attr("id", function (i) {
+  return "expand" + i;
+});
+
+// When the expand element is clicked, it gets its id, removes the expand bit from it.
+// Then, it adds a style element, that uses the id and adds it to the signature and the style. This expands it.
+
+$(".expand").click(function () {
+  var id = this.id.replace("expand", "");
+  var fullid = this.id;
+    if ($('#signature'+id).data('expand') == '0') { 
+    $('#signature'+id).attr("style","max-height:fit-content!important");
+    $('#'+fullid).text('Collapse Signature △');
+    $('#signature'+id).data("expand",'1');
+  } else {
       var newid = this.id.replace("expand", "");
-      $("#signature" + id).attr("style", "");
-      $("#signature" + id).data("expand", "0");
-      $("#" + fullid).text("Expand Signature ▽");
-    }
-  });
+      $('#signature'+id).attr("style","");
+      $('#signature'+id).data("expand",'0');
+      $('#'+fullid).text('Expand Signature ▽');
+
+  }
+});
 }
