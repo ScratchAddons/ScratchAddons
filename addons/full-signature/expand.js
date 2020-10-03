6128bafc2e487b66925d144465f3d696836d432d
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-03 08:27:29 -0400

    Huge Reworking for Expand Signature!

--- addons/full-signature/expand.js
+++ addons/full-signature/expand.js
@@ -1,3 +1,4 @@
+export default async function ({ addon, global, console }) {
 // Add id to each signature
 $(".postsignature").attr("id", function (i) {
   return "signature" + i;
@@ -5,7 +6,7 @@ $(".postsignature").attr("id", function (i) {
 
 // Add P element to each postsignature. The content is Expand Signature ▼
 $(".postsignature").prepend(
-  '<p style="cursor:pointer; color: #4d97ff; font-weight: bold;" class="expand">Expand Signature ▼</p>'
+  '<p data-expand="0" style="cursor:pointer; color: #4d97ff; font-weight: bold;" class="expand">Expand Signature ▽</p>'
 );
 
 // Add id to each expand element
@@ -15,17 +16,20 @@ $(".expand").attr("id", function (i) {
 
 // When the expand element is clicked, it gets it's id, removes the expand bit from it.
 // Then, it adds a style element, that uses the id and adds it to the signature and the style. This expands it.
-// Todo: Work on being able to close signature.
+
 $(".expand").click(function () {
   var id = this.id.replace("expand", "");
-  var styles = "#signature" + id + "{max-height:fit-content!important;}";
-  var newstyle = document.createTextNode(styles);
-  var head, style;
-  head = document.getElementsByTagName("head")[0];
-  if (!head) {
-    return;
+  var fullid = this.id;
+  if ($('#signature'+id).data('expand') == '0') {
+    $('#signature'+id).attr("style","max-height:fit-content!important");
+    $('#'+fullid).text('Close Signature △');
+    $('#signature'+id).data("expand",'1');
+  } else {
+      var newid = this.id.replace("expand", "");
+      $('#signature'+id).attr("style","");
+      $('#signature'+id).data("expand",'0');
+      $('#'+fullid).text('Expand Signature ▽');
+
   }
-  style = document.createElement("style");
-  style.appendChild(newstyle);
-  head.appendChild(style);
 });
+}
