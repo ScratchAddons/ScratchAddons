8a0f223d6fefa2f4aa8a863c084d24572abfbfc2
Geotale <72356786+Geotale@users.noreply.github.com>
2020-10-04 15:31:36 -0500

    Update userscript.js

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -1,14 +1,20 @@
 ﻿export default async function({addon, global, console}){
   //Select where the new tab will be appended, and create a new tab
   var nav = document.querySelectorAll(".tabs")[0],
-  tab = nav.appendChild(document.createElement("a"));
-
-  //Have it link to the profile, or link to a 404 page if it doesn't exist
+  tab = nav.appendChild(document.createElement("a")),
+  li = tab.appendChild(document.createElement("li")),
+  img = li.appendChild(document.createElement("img")),
+  span = li.appendChild(document.createElement("span"));
+  
+  //Link to the profile
   tab.href = "/users/" + document.getElementById("frc-q-1088").value;
 
-  //Create the actual tab stuff. I just reused an icon used for a blank studio. For some reason it looks weird without setting the width and height but it doesn't seem to change much in reality.
-  tab.innerHTML = '<li><img class="tab-icon" width="40" height="24" src="//cdn2.scratch.mit.edu/get_image/gallery/default_170x100.png"><span>Profile</span></li>';
+  //Set up the image. May be changed in the future to a different icon, preferably an SVG if found.
+  img.src = "//cdn2.scratch.mit.edu/get_image/gallery/default_170x100.png";
+  img.width = 40;
+  img.height = 24;
+  img.className = "tab-icon";
 
-  //Prevent the webpage from erasing itself because you just edited the innerHTML of an element
-  void 0
+  //Add the text below the image
+  span.innerText = "Profile";
 }
