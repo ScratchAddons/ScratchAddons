e0295c073d05b7e2a2c4151d4a0549e41c0c6233
Geotale <72356786+Geotale@users.noreply.github.com>
2020-10-04 14:15:38 -0500

    Delete userscript.js

--- addons/userscript.js
+++
@@ -1,7 +0,0 @@
-﻿export default async function({addon, global, console}){
-        var nav = document.querySelectorAll(".tabs")[0],
-        tab = nav.appendChild(document.createElement("a"));
-        tab.href = "/users/" + document.getElementById("frc-q-1088").value;
-        tab.innerHTML = '<li><img class="tab-icon profile" width="40" height="24" src="//cdn2.scratch.mit.edu/get_image/gallery/default_170x100.png"><span>Profile</span></li>';
-        void 0
-}
