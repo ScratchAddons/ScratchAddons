c39cc61799b4b3198a0ac11118a1899dea411342
Geotale <72356786+Geotale@users.noreply.github.com>
2020-10-04 14:30:29 -0500

    Update userscript.js

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -1,7 +1,14 @@
 ﻿export default async function({addon, global, console}){
-        var nav = document.querySelectorAll(".tabs")[0],
-        tab = nav.appendChild(document.createElement("a"));
-        tab.href = "/users/" + document.getElementById("frc-q-1088").value;
-        tab.innerHTML = '<li><img class="tab-icon profile" width="40" height="24" src="//cdn2.scratch.mit.edu/get_image/gallery/default_170x100.png"><span>Profile</span></li>';
-        void 0
+  //Select where the new tab will be appended, and create a new tab
+  var nav = document.querySelectorAll(".tabs")[0],
+  tab = nav.appendChild(document.createElement("a"));
+
+  //Have it link to the profile, or link to a 404 page if it doesn't exist
+  tab.href = "/users/" + document.getElementById("frc-q-1088").value;
+
+  //Create the actual tab stuff. I just reused an icon used for a blank studio. For some reason it looks weird without setting the width and height but it doesn't seem to change much in reality.
+  tab.innerHTML = '<li><img class="tab-icon" width="40" height="24" src="//cdn2.scratch.mit.edu/get_image/gallery/default_170x100.png"><span>Profile</span></li>';
+
+  //Prevent the webpage from erasing itself because you just edited the innerHTML of an element
+  void 0
 }
