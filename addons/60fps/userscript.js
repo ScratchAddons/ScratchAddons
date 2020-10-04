bae1f447f7bcd217fd8d08e5e7f80d1a9a73664f
JeffaloBob <JeffaloBob@users.noreply.github.com>
2020-10-04 07:22:31 +0000

    style: format code

--- addons/60fps/userscript.js
+++ addons/60fps/userscript.js
@@ -1,23 +1,23 @@
 export default async function ({ addon, global, console }) {
-    console.log("60fps");
+  console.log("60fps");
 
-    const vm = addon.tab.traps.onceValues.vm;
+  const vm = addon.tab.traps.onceValues.vm;
 
-    var button = await addon.tab.waitForElement("img.green-flag_green-flag_1kiAo")
-    
-    var mode = true;
-    button.addEventListener("click", (e) => {
-        if (e.ctrlKey) {
-            e.preventDefault();
-            mode = !mode;
-            vm.setCompatibilityMode(mode);
+  var button = await addon.tab.waitForElement("img.green-flag_green-flag_1kiAo");
 
-            if (mode) {
-                button.style.filter = "";
-            } else {
-                //60fps
-                button.style.filter = "hue-rotate(90deg)";
-            }
-        }
-    });
+  var mode = true;
+  button.addEventListener("click", (e) => {
+    if (e.ctrlKey) {
+      e.preventDefault();
+      mode = !mode;
+      vm.setCompatibilityMode(mode);
+
+      if (mode) {
+        button.style.filter = "";
+      } else {
+        //60fps
+        button.style.filter = "hue-rotate(90deg)";
+      }
+    }
+  });
 }
