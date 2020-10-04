07c5d596134d57818fdc72d0f818c08f70445db1
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-10-04 09:21:57 +0200

    make into await but i dont know how to do loop yet

--- addons/60fps/userscript.js
+++ addons/60fps/userscript.js
@@ -1,23 +1,23 @@
 export default async function ({ addon, global, console }) {
-  console.log("60fps");
+    console.log("60fps");
 
-  const vm = addon.tab.traps.onceValues.vm;
+    const vm = addon.tab.traps.onceValues.vm;
 
-  addon.tab.waitForElement("img.green-flag_green-flag_1kiAo").then((button) => {
+    var button = await addon.tab.waitForElement("img.green-flag_green-flag_1kiAo")
+    
     var mode = true;
     button.addEventListener("click", (e) => {
-      if (e.ctrlKey) {
-        e.preventDefault();
-        mode = !mode;
-        vm.setCompatibilityMode(mode);
+        if (e.ctrlKey) {
+            e.preventDefault();
+            mode = !mode;
+            vm.setCompatibilityMode(mode);
 
-        if (mode) {
-          button.style.filter = "";
-        } else {
-          //60fps
-          button.style.filter = "hue-rotate(90deg)";
+            if (mode) {
+                button.style.filter = "";
+            } else {
+                //60fps
+                button.style.filter = "hue-rotate(90deg)";
+            }
         }
-      }
     });
-  });
 }
