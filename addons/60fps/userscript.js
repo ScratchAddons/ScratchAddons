ee20b70d5a34520513c5373a010b7e22f6523aeb
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-10-04 15:12:33 +0200

    so guys we did it (add loop so it worsk)

--- addons/60fps/userscript.js
+++ addons/60fps/userscript.js
@@ -1,23 +1,31 @@
 export default async function ({ addon, global, console }) {
-  console.log("60fps");
+  console.log("60fps enabled");
 
   const vm = addon.tab.traps.onceValues.vm;
 
-  var button = await addon.tab.waitForElement("img.green-flag_green-flag_1kiAo");
+  while (true) {
+    var button = await addon.tab.waitForElement("img.green-flag_green-flag_1kiAo:not([fpschecked])");
+    button.setAttribute("fpschecked", "true");
+    var mode = true;
 
-  var mode = true;
-  button.addEventListener("click", (e) => {
-    if (e.altKey) {
-      e.preventDefault();
-      mode = !mode;
-      vm.setCompatibilityMode(mode);
+    //console.log('just added class')
 
-      if (mode) {
-        button.style.filter = "";
-      } else {
-        //60fps
-        button.style.filter = "hue-rotate(90deg)";
+    button.addEventListener("click", (e) => {
+      //console.log('click')
+      if (e.altKey) {
+        console.log('toggle 60fps')
+        mode = !mode;
+        vm.setCompatibilityMode(mode);
+
+        if (mode) {
+          button.style.filter = "";
+        } else {
+          //60fps
+          button.style.filter = "hue-rotate(90deg)";
+        }
       }
-    }
-  });
+    });
+  }
+
+
 }
