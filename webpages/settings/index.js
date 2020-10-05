4f438b78aa1a498eb94b00dde3cf729a5f7a050f
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-10-05 16:50:45 +0000

    style: format code

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -76,14 +76,16 @@ const vue = new Vue({
   },
   methods: {
     openReview() {
-     var browsercheck = typeof browser === "undefined"
-     if (browsercheck == 'true') {
-       window.open(`https://addons.mozilla.org/en-US/firefox/addon/scratch-messaging-extension/reviews/`);
-     } else {
-       window.open(`https://chrome.google.com/webstore/detail/scratch-addons/fbeffbjdlemaoicjdapfpikkikjoneco?hl=en-GB`);
-     }
+      var browsercheck = typeof browser === "undefined";
+      if (browsercheck == "true") {
+        window.open(`https://addons.mozilla.org/en-US/firefox/addon/scratch-messaging-extension/reviews/`);
+      } else {
+        window.open(
+          `https://chrome.google.com/webstore/detail/scratch-addons/fbeffbjdlemaoicjdapfpikkikjoneco?hl=en-GB`
+        );
+      }
     },
-     openCredits() {
+    openCredits() {
       window.open(`https://scratchaddons.com/contributors`);
     },
     openFeedback() {
