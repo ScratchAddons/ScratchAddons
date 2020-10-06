f4122094dd262cf03f8fb088f638ebae1c715855
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-10-06 19:38:00 +0100

    Update and rename editor.js to userscript.js

--- addons/forums-markdown/userscript.js
+++ addons/forums-markdown/userscript.js
@@ -1,13 +1,13 @@
 
 export default async function ({ addon, global, console }) {
 
-// Set up DOM variables for different pages
-  if (window.location.href.includes("/topic/create") {
-  var submitButton = document.querySelector("button.button")[0],
+// Set up DOM variables
+  var submitButton = document.querySelector("form#post > button.button")[0],
   textarea = document.querySelector("#id_body"),
-  form = document.querySelector("#post");
-  }
-  submitButton.type = "button"; // Stops the button from submitting the post request before we've converted all the markdown
+  form = document.querySelector("#post"),
+  preview = document.querySelector();
+  
+  submitButton.type = "button"; // Stops the form from submitting the post request before we've converted all the markdown
   
   submitButton.addEventListener("click", (event) => {
     textarea.innerText = translateMarkdownToHTML(textarea.innerText);
@@ -17,6 +17,7 @@ export default async function ({ addon, global, console }) {
   function markdownToHTML(markdown) {
     return markdown;
   }
+  
   function bbcodeToMarkdown(bbcode) {
     return bbcode;
   }
