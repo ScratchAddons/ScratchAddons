45d1f44ee75a599eabc2d52d772a41d55f1a43fd
WorldLanguages <mundofinky@gmail.com>
2020-10-07 13:33:00 -0300

    Prettier lint, fix addons.json, add "profile" icon

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -1,18 +1,17 @@
-﻿export default async function({addon, global, console}){
-  //Select where the new tab will be appended, and create a new tab
-  var nav = document.querySelectorAll(".tabs")[0],
-  tab = nav.appendChild(document.createElement("a")),
-  li = tab.appendChild(document.createElement("li")),
-  img = li.appendChild(document.createElement("img")),
-  span = li.appendChild(document.createElement("span"));
-  
-  //Link to the profile
-  tab.href = "/users/" + document.getElementById("frc-q-1088").value;
+export default async function ({ addon, global, console }) {
+  const nav = await addon.tab.waitForElement(".sub-nav.tabs");
 
-  //Set up the image. May be changed in the future to a different icon, preferably an SVG if found.
-  img.src = "//cdn2.scratch.mit.edu/get_image/gallery/default_170x100.png";
-  img.width = 40;
-  img.height = 24;
+  //Select where the new tab will be appended, and create a new tab
+  const tab = nav.appendChild(document.createElement("a")),
+    li = tab.appendChild(document.createElement("li")),
+    img = li.appendChild(document.createElement("img")),
+    span = li.appendChild(document.createElement("span"));
+
+  //Link to the profile
+  tab.href = "/users/" + document.querySelector('[name="q"]').value + "/";
+
+  //Set up the image.
+  img.src = addon.self.dir + "/user.svg";
   img.className = "tab-icon";
 
   //Add the text below the image
