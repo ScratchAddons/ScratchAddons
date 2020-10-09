4c97dc4b08f5afa745950e9d2021badf5f89fc0f
Hans5958 <email5958.1@gmail.com>
2020-10-09 14:51:04 +0700

    More links: Partially support relinking after viewing editor/fullscreen

--- addons/more-links/userscript.js
+++ addons/more-links/userscript.js
@@ -1,33 +1,52 @@
-export default async function ({ addon }) {
+export default async function ({ addon, console }) {
+  console.log("load");
   await addon.tab.loadScript("https://cdn.jsdelivr.net/npm/linkifyjs@2.1.9/dist/linkify.min.js");
   await addon.tab.loadScript("https://cdn.jsdelivr.net/npm/linkifyjs@2.1.9/dist/linkify-element.min.js");
 
   const pageType = document.location.pathname.substr(1).split("/")[0];
-  let comments;
+  console.log(pageType);
 
   switch (pageType) {
     case "users":
       document.querySelectorAll("#user-details .read-only").forEach((element) => linkifyElement(element));
       break;
+
     case "projects":
-      // Need to convert #[numbers] to solve conflict between tags and external Scratch player links.
-      await addon.tab.waitForElement(".project-description a");
-      document.querySelectorAll(".project-description a").forEach((element) => {
-        if (/\d+/.test(element.textContent)) element.outerHTML = element.textContent;
-      });
-      linkifyElement(document.querySelector(".project-description"));
+      (async () => {
+        while (true) {
+          console.log("wait projects");
+          await addon.tab.waitForElement(".project-description:not(.more-links-checked)");
+          console.log("go projects");
+          let element = document.querySelector(".project-description"); // How is this laggy?
+          // Need to convert #[numbers] to solve conflict between tags and external Scratch player links.
+          document.querySelectorAll(".project-description a").forEach((element) => {
+            if (/\d+/.test(element.textContent)) element.outerHTML = element.textContent;
+          });
+          linkifyElement(element);
+          console.log("linkified", element);
+          element.classList.add("more-links-checked");
+        }
+      })();
       break;
+
     case "studios":
       linkifyElement(document.querySelector("#description.read-only .overview"));
       break;
   }
 
-  while (true) {
-    await addon.tab.waitForElement(".comment:not(.more-links-checked)");
-    comments = document.querySelectorAll(".comment:not(.more-links-checked)");
-    comments.forEach((comment) => {
-      linkifyElement(comment);
-      comment.classList.add("more-links-checked");
-    });
-  }
+  console.log("comments?");
+
+  (async () => {
+    while (true) {
+      console.log("wait comments");
+      await addon.tab.waitForElement(".comment:not(.more-links-checked)");
+      console.log("go comments");
+      let comments = document.querySelectorAll(".comment:not(.more-links-checked)");
+      comments.forEach((comment) => {
+        linkifyElement(comment);
+        console.log("linkified", comment);
+        comment.classList.add("more-links-checked");
+      });
+    }
+  })();
 }
