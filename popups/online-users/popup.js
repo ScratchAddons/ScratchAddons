99bdccb6f0dc25b8758db36d44ea79bbe34b5aa1
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-10-10 18:02:54 +0100

    Update popup.js

---
+++ popups/online-users/popup.js
@@ -0,0 +1,32 @@
+var vue = new Vue({
+  el: "body",
+  data: {
+    loaded: false,
+    users: [],
+  },
+  computed: {
+    usersPfp: function () {
+      var list;
+      this.users.forEach(user => {
+        fetch(https://api.scratch.mit.edu/users/" + user)
+        .then(res => res.json)
+        .then(json => list.push(json.profile.images.32x32));
+      });
+      return list;
+    },
+    usersUrl: function () {
+      var list;
+      this.users.forEach(user => {
+        list.push("https://scratch.mit.edu/users/" + user);
+      });
+      return list;
+    },
+  },
+});
+
+fetchUsers();
+vue.loaded = true;
+
+function fetchUsers () {
+
+}
