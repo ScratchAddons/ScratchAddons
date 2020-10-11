233c6d1fb3eda600598fb3dad4d65118906fa35a
WorldLanguages <mundofinky@gmail.com>
2020-10-11 15:57:05 -0300

    Use addon.tab.waitForElement(selector, {markAsSeen: true})

--- addons/discuss-button/userscript.js
+++ addons/discuss-button/userscript.js
@@ -8,29 +8,15 @@ export default async function ({ addon, global, console }) {
   a.textContent = addon.settings.get("buttonName");
   link.appendChild(a);
 
-  function scratchWww(el) {
-    if (addon.settings.get("removeIdeasBtn")) el.getElementsByTagName("li")[3].remove();
-    el.insertBefore(link, el.getElementsByTagName("li")[3]);
-    el.classList.add("discuss-button-added");
-  }
-
   if (addon.tab.clientVersion === "scratch-www") {
-    const check = () => {
-      return new Promise(async (resolve) => {
-        const el = await addon.tab.waitForElement(
-          "div#navigation div.inner ul:not(.discuss-button-added):not(.production)"
-        );
-        scratchWww(el);
-        setTimeout(resolve, 1000);
-      });
-    };
-    check();
-    if (addon.tab.editorMode) {
-      // On a project
-      while (true) {
-        await check();
-      }
-    } else check();
+    while (true) {
+      const el = await addon.tab.waitForElement(
+        "div#navigation div.inner ul:not(.production)",
+        { markAsSeen: true}
+      );
+      if (addon.settings.get("removeIdeasBtn")) el.getElementsByTagName("li")[3].remove();
+      el.insertBefore(link, el.getElementsByTagName("li")[3]);
+    }
   } else {
     const el = await addon.tab.waitForElement("div#topnav ul.site-nav");
     if (addon.settings.get("removeIdeasBtn")) el.getElementsByTagName("li")[2].remove();
