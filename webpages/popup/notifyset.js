4aa4eda001e5368a9dc2a58e3263426a70377187
ahmetlii <ahmetlii@users.noreply.github.com>
2020-10-12 15:06:07 +0300

    callback from another js

---
+++ webpages/popup/notifyset.js
@@ -0,0 +1,6 @@
+if (localStorage.getItem("notifset") == true || null) {
+    <img src="./notificontrue.png" />
+  }
+  else if (localStorage.getItem("notifset") == false) {
+    <img src="./notificonfalse.png" />
+  }
