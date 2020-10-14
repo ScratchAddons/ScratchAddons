54459b268104b74e83535029272f62d0324926b6
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-14 10:02:55 -0400

    Ready for PR

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -89,7 +89,7 @@ const vue = new Vue({
       window.open(`https://scratchaddons.com/contributors`);
     },
     openFeedback() {
-      window.open(`https://scratchaddons.com/feedback?version=${chrome.runtime.getManifest().version_name}`);
+      window.open(`https://scratchaddons.com/feedback?version=${chrome.runtime.getManifest().version}`);
     },
     clearSearch() {
       this.searchInput = "";
