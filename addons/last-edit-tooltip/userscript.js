fd818984041f70a9d8c9eb3a1aa76287052c0449
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-10-14 19:19:11 -0400

    Fixed if user goes inside then outside editor

--- addons/last-edit-tooltip/userscript.js
+++ addons/last-edit-tooltip/userscript.js
@@ -2,7 +2,10 @@ export default async function ({ addon, global, console }) {
   fetch("https://api.scratch.mit.edu" + document.location.pathname).then(function(response) {
     return response.json()
   }).then(function(text) {
-    let dateMod = new Date(text.history.modified)
-    document.querySelector(".share-date").setAttribute("title", `Modified: ${dateMod.toLocaleString('en-us',{month:'short'})} ${dateMod.getDay()}, ${dateMod.getFullYear()}`)
+    while(true) {
+      const element = await addon.tab.waitForElement(".share-date", {markAsSeen:true});
+      let dateMod = new Date(text.history.modified)
+    element.setAttribute("title", `Modified: ${dateMod.toLocaleString('en-us',{month:'short'})} ${dateMod.getDay()}, ${dateMod.getFullYear()}`)
+    }
   });
 }
