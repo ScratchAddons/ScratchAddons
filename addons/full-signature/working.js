a3a80da9b6963acbbe760488bc3de14ab1293b9a
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-15 11:20:11 -0400

    Create working.js

---
+++ addons/full-signature/working.js
@@ -0,0 +1,20 @@
+export default async function ({ addon, global, console }) {
+  document.querySelector(".activity-stream").insertAdjacentHTML("beforeend", `
+  <button class="load-more-wibd">Load More</button>
+  `)
+  let dataLoaded = 6;
+  let loadMore = document.querySelector(".load-more-wibd")
+  loadMore.addEventListener("click", function () {
+    fetch(`https://scratch.mit.edu/messages/ajax/user-activity/?user=${document.querySelector(".header-text h2").innerHTML}&max=1000000`).then(function(response) {
+      return response.text()
+    }).then(function(text) {
+      let dummyEl = document.createElement("div")
+      dummyEl.innerHTML = text
+      let lastDataLoad = dataLoaded
+      for (; dataLoaded < lastDataLoad + 6; dataLoaded++) {
+        loadMore.insertAdjacentHTML("beforebegin", `<li>${dummyEl.querySelectorAll("ul li")[dataLoaded].innerHTML}</li>`)
+      }
+      dummyEl.remove()
+    });
+  })
+}
