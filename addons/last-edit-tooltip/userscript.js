df94ad32bcd408c5ccd7ba3258448e67c2045829
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-10-16 12:23:29 -0400

    Last Edit Tooltip Addon (#491)

---
+++ addons/last-edit-tooltip/userscript.js
@@ -0,0 +1,18 @@
+export default async function ({ addon, global, console }) {
+  fetch("https://api.scratch.mit.edu" + document.location.pathname)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(async function (text) {
+      while (true) {
+        const element = await addon.tab.waitForElement(".share-date", { markAsSeen: true });
+        let dateMod = new Date(text.history.modified);
+        element.setAttribute(
+          "title",
+          `Modified: ${dateMod.toLocaleString("en-us", {
+            month: "short",
+          })} ${dateMod.getDay()}, ${dateMod.getFullYear()}`
+        );
+      }
+    });
+}
