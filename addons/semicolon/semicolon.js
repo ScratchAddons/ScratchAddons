d38f7bde87082dca11ea6f3b14c0253bf5a9d6b6
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-10-16 16:49:34 +0000

    style: format code

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -1,4 +1,4 @@
-var semicolon = document.createElement('p'); 
-semicolon.textContent = ';';
+var semicolon = document.createElement("p");
+semicolon.textContent = ";";
 semicolon.classList.add("semicolon");
 document.body.appendChild(semicolon);
