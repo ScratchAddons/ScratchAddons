03db703e6a841ad6154e951c2dfd744498d1ca84
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-16 15:24:05 -0400

    Update semicolon.js

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -1,4 +1,6 @@
-var semicolon = document.createElement("p");
-semicolon.textContent = ";";
-semicolon.classList.add("semicolon");
-document.body.appendChild(semicolon);
+export default async function ({ addon, global, console }) {
+  var semicolon = document.createElement("p");
+  semicolon.textContent = ";";
+  semicolon.classList.add("semicolon");
+  document.body.appendChild(semicolon);
+}
