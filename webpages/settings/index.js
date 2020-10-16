8d291f67a40dab0e565f55756481f32605c04753
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-16 13:43:56 -0400

    Revert

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -68,19 +68,6 @@ const vue = new Vue({
           theme: true,
         },
       },
-      {
-        name: "Easter egg",
-        matchType: "tag",
-        matchName: "easteregg",
-        color: "yellow",
-        tabShow: {
-          all: false,
-          editor: false,
-          community: false,
-          theme: false,
-          easteregg: true,
-        },
-      },
     ],
   },
   computed: {
@@ -98,44 +85,6 @@ const vue = new Vue({
         );
       }
     },
-    easter() {
-      var allowedKeys = {
-        37: "left",
-        38: "up",
-        39: "right",
-        40: "down",
-        65: "a",
-        66: "b",
-      };
-
-      // the 'official' Konami Code sequence
-      var konamiCode = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"];
-
-      // a variable to remember the 'position' the user has reached so far.
-      var konamiCodePosition = 0;
-
-      // add keydown event listener
-      document.addEventListener("keydown", function (e) {
-        // get the value of the key code from the key map
-        var key = allowedKeys[e.keyCode];
-        // get the value of the required key from the konami code
-        var requiredKey = konamiCode[konamiCodePosition];
-
-        // compare the key with the required key
-        if (key == requiredKey) {
-          // move to the next key in the konami code sequence
-          konamiCodePosition++;
-
-          // if the last key is reached, activate cheats
-          if (konamiCodePosition == konamiCode.length) {
-            return true;
-          }
-        } else {
-          konamiCodePosition = 0;
-        }
-      });
-    },
-
     openCredits() {
       window.open(`https://scratchaddons.com/contributors`);
     },
@@ -219,7 +168,6 @@ chrome.runtime.sendMessage("getSettingsInfo", ({ manifests, addonsEnabled, addon
     manifest._tags.forums = manifest.tags.includes("forums");
     manifest._tags.forEditor = manifest.tags.includes("theme") && manifest.tags.includes("editor");
     manifest._tags.forWebsite = manifest.tags.includes("theme") && manifest.tags.includes("community");
-    manifest._tags.easteregg = manifest.tags.includes("easteregg");
   }
   // Sort: enabled first, then recommended disabled, then other disabled addons. All alphabetically.
   manifests.sort((a, b) => {
