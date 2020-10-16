6ed02d92772be317047f3b974ef3e9b142ba408c
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-10-16 17:24:59 +0000

    style: format code

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -98,45 +98,44 @@ const vue = new Vue({
         );
       }
     },
-  easter() {
+    easter() {
       var allowedKeys = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  65: 'a',
-  66: 'b'
-};
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down",
+        65: "a",
+        66: "b",
+      };
 
-// the 'official' Konami Code sequence
-var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+      // the 'official' Konami Code sequence
+      var konamiCode = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"];
 
-// a variable to remember the 'position' the user has reached so far.
-var konamiCodePosition = 0;
+      // a variable to remember the 'position' the user has reached so far.
+      var konamiCodePosition = 0;
 
-// add keydown event listener
-document.addEventListener('keydown', function(e) {
-  // get the value of the key code from the key map
-  var key = allowedKeys[e.keyCode];
-  // get the value of the required key from the konami code
-  var requiredKey = konamiCode[konamiCodePosition];
+      // add keydown event listener
+      document.addEventListener("keydown", function (e) {
+        // get the value of the key code from the key map
+        var key = allowedKeys[e.keyCode];
+        // get the value of the required key from the konami code
+        var requiredKey = konamiCode[konamiCodePosition];
 
-  // compare the key with the required key
-  if (key == requiredKey) {
+        // compare the key with the required key
+        if (key == requiredKey) {
+          // move to the next key in the konami code sequence
+          konamiCodePosition++;
 
-    // move to the next key in the konami code sequence
-    konamiCodePosition++;
+          // if the last key is reached, activate cheats
+          if (konamiCodePosition == konamiCode.length) {
+            return true;
+          }
+        } else {
+          konamiCodePosition = 0;
+        }
+      });
+    },
 
-    // if the last key is reached, activate cheats
-    if (konamiCodePosition == konamiCode.length) {
-      return true;
-    }
-  } else {
-    konamiCodePosition = 0;
-  }
-});
- },
-  
     openCredits() {
       window.open(`https://scratchaddons.com/contributors`);
     },
