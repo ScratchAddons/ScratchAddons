53b39ef144c0bcdf476be8109713c7b4a04b956e
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-10-17 18:04:43 +0100

    Format code

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -1,13 +1,12 @@
 export default async function ({ addon, global, console }) {
   const nav = await addon.tab.waitForElement(".sub-nav.tabs");
   // Get the search term and get the api end point to check the username
-  const userPage = `https://api.scratch.mit.edu/accounts/checkusername/${document
-    .querySelector('[name="q"]')
-    .value.trim()}/`;
+  const userPage = `https://api.scratch.mit.edu/accounts/checkusername/${document.querySelector('[name="q"]').value.trim()}/`;
   // Fetch the user page to see whether the user exists
   const response = await fetch(userPage);
   // Get the json of the response
   const json = await response.json();
+  
   // If the username exists make the link to the user page
   if (json.message === "username exists") {
     //Select where the new tab will be appended, and create a new tab
