41bd1744218e99158ec2c07022246329869924ca
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-19 10:48:53 -0400

    Update semicolon.js

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -1,6 +1,12 @@
 export default async function ({ addon, global, console }) {
-  var semicolon = document.createElement("p");
-  semicolon.textContent = ";";
-  semicolon.classList.add("semicolon");
-  document.body.appendChild(semicolon);
+  if (document.body.contains(document.querySelector('#signature'))) {
+    return
+  } else {
+      if (document.location.href.split("/")[5] == "editor") {
+        return
+      } else {
+          var semicolon = document.createElement("p");
+          semicolon.textContent = ";";
+          semicolon.classList.add("semicolon");
+          document.body.appendChild(semicolon);
 }
