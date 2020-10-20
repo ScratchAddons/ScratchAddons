1eeeec825b51779be8d3b49d096c94872178932c
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-20 15:49:09 -0400

    Update semicolon.js

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -1,14 +1,14 @@
 export default async function ({ addon, global, console }) {
-  if (document.body.contains(document.querySelector("#signature"))) {
-    console.log("Signature already existed");
-  } else {
-    if (document.location.href.split("/")[5] == "editor") {
-      console.log("In editor");
+    if (document.body.contains(document.querySelector("#signature"))) {
+      console.log("Signature already exists!");
     } else {
-      var semicolon = document.createElement("p");
-      semicolon.textContent = ";";
-      semicolon.classList.add("semicolon");
-      document.body.appendChild(semicolon);
+      if (addon.tab.editorMode === "editor") {
+        console.log("In editor!");
+      } else {
+        var semicolon = document.createElement("p");
+        semicolon.textContent = ";";
+        semicolon.classList.add("semicolon");
+        document.body.appendChild(semicolon);
+      }
     }
-  }
 }
