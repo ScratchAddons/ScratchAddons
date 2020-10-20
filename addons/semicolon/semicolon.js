48af5623d0df5f9a1581ac6ecadd131121ee0da4
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-10-20 19:49:35 +0000

    style: format code

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -1,14 +1,14 @@
 export default async function ({ addon, global, console }) {
-    if (document.body.contains(document.querySelector("#signature"))) {
-      console.log("Signature already exists!");
+  if (document.body.contains(document.querySelector("#signature"))) {
+    console.log("Signature already exists!");
+  } else {
+    if (addon.tab.editorMode === "editor") {
+      console.log("In editor!");
     } else {
-      if (addon.tab.editorMode === "editor") {
-        console.log("In editor!");
-      } else {
-        var semicolon = document.createElement("p");
-        semicolon.textContent = ";";
-        semicolon.classList.add("semicolon");
-        document.body.appendChild(semicolon);
-      }
+      var semicolon = document.createElement("p");
+      semicolon.textContent = ";";
+      semicolon.classList.add("semicolon");
+      document.body.appendChild(semicolon);
     }
+  }
 }
