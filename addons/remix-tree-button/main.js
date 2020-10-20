f4f0a2835716bbcbc1fdad30a8fb70574834c317
GDUcrash <gducrash@gmail.com>
2020-10-20 17:25:55 +0300

    i fixed all the stuff, w_l asked to fix
    poop

--- addons/remix-tree-button/main.js
+++ addons/remix-tree-button/main.js
@@ -4,40 +4,26 @@ export default async function ({ addon, global, console }) {
   const subactions = document.querySelector(".flex-row.subactions").querySelector(".action-buttons");
 
   const remixtree = document.createElement("button");
+
+  const remixtreeSpan = document.createElement("span");
+  remixtreeSpan.innerText = 'Remix Tree';
+  const remixtreeImg = document.createElement("img");
+  remixtreeImg.setAttribute('src', "https://scratch.mit.edu/svgs/project/remix-white.svg");
+  remixtreeImg.setAttribute('height', '15px');
+  remixtree.style.marginRight = '5px'; //no, i will not do this through userstyle, i simply won't.
+
   remixtree.className = "button action-button remixtree-button";
   remixtree.id = "scratchAddonsRemixTreeBtn";
-  remixtree.innerHTML = `
-    <img src="https://scratch.mit.edu/svgs/project/remix-white.svg" height="15px" style="margin-right: 5px;" />
-    <span>Remix Tree</span>
-    `;
-  remixtree.onclick = () => {
-    if (window.location.href.endsWith("/")) window.location.href += "remixtree";
-    else window.location.href += "/remixtree";
-  };
+  remixtree.appendChild(remixtreeImg);
+  remixtree.appendChild(remixtreeSpan);
+  remixtree.addEventListener('click', () => {
+    if (window.location.href.endsWith("/")) window.location.href.split('#')[0] += "remixtree"; // not the best way to get rid of the hash, but...
+    else window.location.href.split('#')[0] += "/remixtree"; 
+  });
   remixtree.style.display = "flex";
   remixtree.style.alignItems = "center";
   if (addon.settings.get("buttonColor")) {
-    remixtree.style.backgroundColor = hexTest(addon.settings.get("buttonColor"));
-  }
-
-  function hexTest(s) {
-    let t = "";
-    let hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
-    let len = [1, 3, 6];
-    let filter = s.split("").filter((f) => !hex.includes(f));
-    if (s.startsWith("#"))
-      filter = s
-        .split("")
-        .slice(1)
-        .filter((f) => !hex.includes(f.toLowerCase()));
-    console.log(s);
-    if (s.startsWith("#")) {
-      if (filter == "" && len.includes(s.length - 1)) t = s;
-    } else {
-      if (filter == "" && len.includes(s.length)) t = "#" + s;
-    }
-
-    return t;
+    remixtree.style.backgroundColor = addon.settings.get("buttonColor");
   }
 
   subactions.appendChild(remixtree);
