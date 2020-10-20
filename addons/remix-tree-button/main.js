a0eb520d50046668518735741a5a9011c1572c1a
GDUcrash <gducrash@gmail.com>
2020-10-20 17:29:38 +0300

    vjiojdewi forgot to run prettier
    big chungus reddit 69 moment

--- addons/remix-tree-button/main.js
+++ addons/remix-tree-button/main.js
@@ -6,19 +6,20 @@ export default async function ({ addon, global, console }) {
   const remixtree = document.createElement("button");
 
   const remixtreeSpan = document.createElement("span");
-  remixtreeSpan.innerText = 'Remix Tree';
+  remixtreeSpan.innerText = "Remix Tree";
   const remixtreeImg = document.createElement("img");
-  remixtreeImg.setAttribute('src', "https://scratch.mit.edu/svgs/project/remix-white.svg");
-  remixtreeImg.setAttribute('height', '15px');
-  remixtree.style.marginRight = '5px'; //no, i will not do this through userstyle, i simply won't.
+  remixtreeImg.setAttribute("src", "https://scratch.mit.edu/svgs/project/remix-white.svg");
+  remixtreeImg.setAttribute("height", "15px");
+  remixtree.style.marginRight = "5px"; //no, i will not do this through userstyle, i simply won't.
 
   remixtree.className = "button action-button remixtree-button";
   remixtree.id = "scratchAddonsRemixTreeBtn";
   remixtree.appendChild(remixtreeImg);
   remixtree.appendChild(remixtreeSpan);
-  remixtree.addEventListener('click', () => {
-    if (window.location.href.endsWith("/")) window.location.href.split('#')[0] += "remixtree"; // not the best way to get rid of the hash, but...
-    else window.location.href.split('#')[0] += "/remixtree"; 
+  remixtree.addEventListener("click", () => {
+    if (window.location.href.endsWith("/")) window.location.href.split("#")[0] += "remixtree";
+    // not the best way to get rid of the hash, but...
+    else window.location.href.split("#")[0] += "/remixtree";
   });
   remixtree.style.display = "flex";
   remixtree.style.alignItems = "center";
