28ca012c5fa7683d371a7b8c86e5723cb74e9b7b
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-10-21 18:34:33 -0400

    Now Using a newer API

--- addons/exact-count/studio.js
+++ addons/exact-count/studio.js
@@ -3,48 +3,31 @@ export default async function({
   global,
   console
 }) {
-  function countProjects(url, page, delta, callback) {
+  function countProjects(url, offset, delta, callback) {
     const request = new XMLHttpRequest();
-    request.open("GET", url + page + "/");
+    request.open("GET", url + offset);
     request.onreadystatechange = function() {
       if (request.readyState == 4) {
-        if (request.status == 200) {
-          countProjects(url, page + delta, delta, callback);
-        } else if (request.status == 404) {
-          if (page != 1) {
-            if (delta == 1) {
-              const correctReq = new XMLHttpRequest();
-              correctReq.open("GET", url + (page - 1) + "/");
-              correctReq.onreadystatechange = function() {
-                if (correctReq.readyState == 4) {
-                  if (correctReq.status == 200) {
-                    const parser = new DOMParser();
-                    const list = parser.parseFromString(
-                      "<ul id='projects'>" + correctReq.responseText + "</ul>", "text/html"
-                    );
-                    let count = 60 * (page - 2);
-                    count += list.querySelector("#projects").querySelectorAll("ul > li").length;
-                    callback(count);
-                  }
-                }
-              };
-              correctReq.send();
-            } else {
-              page -= delta;
-              delta /= 10;
-              countProjects(url, page + delta, delta, callback);
-            }
-          }
+        let pageLen = JSON.parse(request.response).length
+        if (pageLen == 20) {
+          countProjects(url, offset + delta, delta, callback);
+        } else if (pageLen > 0) {
+          let count = 20 * (offset-1) + pageLen;
+          callback(count);
+        } else {
+          offset -= delta;
+          delta /= 10;
+          countProjects(url, offset + delta, delta, callback);
         }
       }
-    };
+    }
     request.send();
   }
 
+
   if (document.querySelector("[data-count=projects]").innerText == "100+") {
-    const studio = (/[0-9]+/).exec(location.href)[0];
-    const apiUrlPrefix = "https://scratch.mit.edu/site-api/projects/in/" + studio + "/";
-    countProjects(apiUrlPrefix, 1, 100, function(count) {
+    const apiUrlPrefix = "https://api.scratch.mit.edu/studios/" + (/[0-9]+/).exec(location.href)[0] + "/projects/?limit=20&offset=";
+    countProjects(apiUrlPrefix, 0, 10000, function(count) {
       document.querySelector("[data-count=projects]").innerText = count;
     });
   }
