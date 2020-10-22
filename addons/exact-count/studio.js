2e55ad266e3bea0b4627be481c56c1c1e34da43d
WorldLanguages <WorldLanguages@users.noreply.github.com>
2020-10-22 23:42:32 +0000

    style: format code

--- addons/exact-count/studio.js
+++ addons/exact-count/studio.js
@@ -1,14 +1,10 @@
-export default async function({
-  addon,
-  global,
-  console
-}) {
+export default async function ({ addon, global, console }) {
   function countProjects(url, offset, delta, callback) {
     const request = new XMLHttpRequest();
     request.open("GET", url + offset);
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function () {
       if (request.readyState == 4) {
-        let pageLen = JSON.parse(request.response).length
+        let pageLen = JSON.parse(request.response).length;
         if (pageLen == 40) {
           countProjects(url, offset + delta, delta, callback);
         } else if (pageLen > 0) {
@@ -20,14 +16,14 @@ export default async function({
           countProjects(url, offset + delta, delta, callback);
         }
       }
-    }
+    };
     request.send();
   }
 
-
   if (document.querySelector("[data-count=projects]").innerText == "100+") {
-    const apiUrlPrefix = "https://api.scratch.mit.edu/studios/" + (/[0-9]+/).exec(location.pathname)[0] + "/projects/?limit=40&offset=";
-    countProjects(apiUrlPrefix, 0, 10000, function(count) {
+    const apiUrlPrefix =
+      "https://api.scratch.mit.edu/studios/" + /[0-9]+/.exec(location.pathname)[0] + "/projects/?limit=40&offset=";
+    countProjects(apiUrlPrefix, 0, 10000, function (count) {
       document.querySelector("[data-count=projects]").innerText = count;
     });
   }
