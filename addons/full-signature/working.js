4c8bf8a8b9f35334e406707e372250ad14f3c88a
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-10-23 10:36:03 -0400

    Update working.js

--- addons/full-signature/working.js
+++ addons/full-signature/working.js
@@ -1,5 +1,5 @@
 export default async function ({ addon, global, console }) {
-  let activityStream = document.querySelectorAll(".activity-stream li");
+ let activityStream = document.querySelectorAll(".activity-stream li");
   let parser = new DOMParser();
   let htmlparse = parser.parseFromString(
     `
@@ -10,29 +10,20 @@ export default async function ({ addon, global, console }) {
   document.querySelector(".activity-stream").appendChild(htmlparse.querySelector("button"));
   let dataLoaded = 6;
   let loadMore = document.querySelector(".load-more-wibd");
-  loadMore.addEventListener("click", function () {
-    fetch(
-      `https://scratch.mit.edu/messages/ajax/user-activity/?user=${
-        document.querySelector(".header-text h2").textContent
-      }&max=1000000`
-    )
-      .then(function (response) {
-        return response.text();
+  loadMore.addEventListener("click", function() {
+    fetch(`
+      https://scratch.mit.edu/messages/ajax/user-activity/?user=${
+      document.querySelector(".header-text h2").textContent
+      }&max=1000000`)
+      .then(response => response.text())
+      .then(response => {
+        var html = parser.parseFromString(response, 'text/html');
+        html.querySelectorAll("ul > li").forEach((li, index) => {
+          if (index > dataLoaded && index < dataLoaded + 6) {
+            activityStream[activityStream.length - 1].appendChild(li)
+          }
+        })
+        dataLoaded += 6
       })
-      .then(function (text) {
-        let dummyEl = document.createElement("div");
-        dummyEl.innerHTML = text;
-        let lastDataLoad = dataLoaded;
-        for (; dataLoaded < lastDataLoad + 6; dataLoaded++) {
-          htmlparse = parser.parseFromString(
-            `
-            <li>${dummyEl.querySelectorAll("ul li")[dataLoaded].innerHTML}</li>
-            `,
-            "text/html"
-          );
-          activityStream[activityStream.length - 1].appendChild(htmlparse.querySelector("li"));
-        }
-        dummyEl.remove();
-      });
-  });
+  })
 }
