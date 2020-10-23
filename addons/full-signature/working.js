7443c5c6ca755455700f15ef24658a1009721292
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-10-23 14:36:35 +0000

    style: format code

--- addons/full-signature/working.js
+++ addons/full-signature/working.js
@@ -1,5 +1,5 @@
 export default async function ({ addon, global, console }) {
- let activityStream = document.querySelectorAll(".activity-stream li");
+  let activityStream = document.querySelectorAll(".activity-stream li");
   let parser = new DOMParser();
   let htmlparse = parser.parseFromString(
     `
@@ -10,20 +10,20 @@ export default async function ({ addon, global, console }) {
   document.querySelector(".activity-stream").appendChild(htmlparse.querySelector("button"));
   let dataLoaded = 6;
   let loadMore = document.querySelector(".load-more-wibd");
-  loadMore.addEventListener("click", function() {
+  loadMore.addEventListener("click", function () {
     fetch(`
       https://scratch.mit.edu/messages/ajax/user-activity/?user=${
-      document.querySelector(".header-text h2").textContent
+        document.querySelector(".header-text h2").textContent
       }&max=1000000`)
-      .then(response => response.text())
-      .then(response => {
-        var html = parser.parseFromString(response, 'text/html');
+      .then((response) => response.text())
+      .then((response) => {
+        var html = parser.parseFromString(response, "text/html");
         html.querySelectorAll("ul > li").forEach((li, index) => {
           if (index > dataLoaded && index < dataLoaded + 6) {
-            activityStream[activityStream.length - 1].appendChild(li)
+            activityStream[activityStream.length - 1].appendChild(li);
           }
-        })
-        dataLoaded += 6
-      })
-  })
+        });
+        dataLoaded += 6;
+      });
+  });
 }
