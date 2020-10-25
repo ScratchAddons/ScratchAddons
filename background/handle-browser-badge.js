6e6c0fefa203d411e01ab086749939e92fa63f6a
WorldLanguages <mundofinky@gmail.com>
2020-10-25 15:40:02 -0300

    Add mute capability

--- background/handle-browser-badge.js
+++ background/handle-browser-badge.js
@@ -3,7 +3,7 @@ scratchAddons.localEvents.addEventListener("badgeUpdateNeeded", () => {
   // If you want your addon to use the badge's text
   // or color, please open an issue on GitHub.
   const hardcodedBadgeUser = "msg-count-badge";
-  if (scratchAddons.localState.addonsEnabled[hardcodedBadgeUser]) {
+  if (scratchAddons.localState.addonsEnabled[hardcodedBadgeUser] && !scratchAddons.muted) {
     let text = scratchAddons.localState.badges[hardcodedBadgeUser].text;
     if (text === null || text === 0) text = "";
     else if (typeof text === "number") text = String(text);
