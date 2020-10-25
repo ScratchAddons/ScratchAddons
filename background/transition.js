0f75e2289b87e2d4bb7bd4d79aff4ff2a371fb65
WorldLanguages <mundofinky@gmail.com>
2020-10-25 16:06:22 -0300

    Details before submitting v1.2.0

--- background/transition.js
+++ background/transition.js
@@ -2,5 +2,8 @@ chrome.runtime.onInstalled.addListener((details) => {
   if (details.previousVersion && details.previousVersion.startsWith("0")) {
     chrome.tabs.create({ url: "https://scratchaddons.com/scratch-messaging-transition" });
   }
+  else if(details.reason === "install") {
+    chrome.tabs.create({ url: "https://scratchaddons.com/welcome" });
+  }
 });
 chrome.runtime.setUninstallURL("https://scratchaddons.com/farewell");
