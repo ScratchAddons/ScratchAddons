efbc222b3b4dc81a8ec0e2f37cad658201fab60b
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-10-25 21:53:59 +0000

    style: format code

--- background/transition.js
+++ background/transition.js
@@ -1,8 +1,11 @@
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.previousVersion && details.previousVersion.startsWith("0")) {
     chrome.tabs.create({ url: "https://scratchaddons.com/scratch-messaging-transition" });
-  } else if (details.reason === "install" &&	chrome.runtime.getManifest().version_name.includes('-prerelease') === false) {
-      chrome.tabs.create({ url: "https://scratchaddons.com/welcome" });
+  } else if (
+    details.reason === "install" &&
+    chrome.runtime.getManifest().version_name.includes("-prerelease") === false
+  ) {
+    chrome.tabs.create({ url: "https://scratchaddons.com/welcome" });
   }
 });
 chrome.runtime.setUninstallURL("https://scratchaddons.com/farewell");
