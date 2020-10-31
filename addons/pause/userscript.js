ce25507f5c45d6cfe5749e84f159659bf114b1e0
JeffaloBob <JeffaloBob@users.noreply.github.com>
2020-10-31 10:44:26 +0000

    style: format code

--- addons/pause/userscript.js
+++ addons/pause/userscript.js
@@ -3,45 +3,45 @@ export default async function ({ addon, global, console }) {
 
   const vm = addon.tab.traps.onceValues.vm;
 
-  var playing = true
-  var threads = []
+  var playing = true;
+  var threads = [];
 
   while (true) {
     let bar = await addon.tab.waitForElement("[class^='controls_controls-container']", { markAsSeen: true });
 
-    var img = document.createElement('img')
-    img.className = 'pause-btn'
-    if (playing) img.src = addon.self.dir + "/pause.svg"
-    if (!playing) img.src = addon.self.dir + "/play.svg"
-    img.draggable = false
-    img.title = 'Pause'
+    var img = document.createElement("img");
+    img.className = "pause-btn";
+    if (playing) img.src = addon.self.dir + "/pause.svg";
+    if (!playing) img.src = addon.self.dir + "/play.svg";
+    img.draggable = false;
+    img.title = "Pause";
 
-    document.querySelector("[class^='green-flag']").insertAdjacentElement("afterend", img)
+    document.querySelector("[class^='green-flag']").insertAdjacentElement("afterend", img);
 
-    img.addEventListener('click', e => {
+    img.addEventListener("click", (e) => {
       if (!playing) {
-        if (vm.runtime.threads.length == 0) vm.runtime.threads = threads
-        vm.runtime.audioEngine.audioContext.resume()
-        vm.runtime.ioDevices.clock.resume()
-        img.src = addon.self.dir + "/pause.svg"
+        if (vm.runtime.threads.length == 0) vm.runtime.threads = threads;
+        vm.runtime.audioEngine.audioContext.resume();
+        vm.runtime.ioDevices.clock.resume();
+        img.src = addon.self.dir + "/pause.svg";
       } else {
-        threads = vm.runtime.threads
-        vm.runtime.threads = []
-        vm.runtime.audioEngine.audioContext.suspend()
-        vm.runtime.ioDevices.clock.pause()
-        img.src = addon.self.dir + "/play.svg"
+        threads = vm.runtime.threads;
+        vm.runtime.threads = [];
+        vm.runtime.audioEngine.audioContext.suspend();
+        vm.runtime.ioDevices.clock.pause();
+        img.src = addon.self.dir + "/play.svg";
       }
-      playing = !playing
-    })
+      playing = !playing;
+    });
 
-    document.querySelector("[class^='green-flag']").addEventListener('click', e => { //just incase someone presses greenflag after it has been pauseds
-      threads = []
-      vm.runtime.audioEngine.audioContext.resume()
-      vm.runtime.ioDevices.clock.resume()
-      img.src = addon.self.dir + "/pause.svg"
-      playing = true
-    })
-
+    document.querySelector("[class^='green-flag']").addEventListener("click", (e) => {
+      //just incase someone presses greenflag after it has been pauseds
+      threads = [];
+      vm.runtime.audioEngine.audioContext.resume();
+      vm.runtime.ioDevices.clock.resume();
+      img.src = addon.self.dir + "/pause.svg";
+      playing = true;
+    });
 
     //TODO: break points in scratch code
     //eg. when gf clicked, move 10 steps, set sa_Breakpoint = true or something like that, then when that scratch variable is set to true, pause the project
