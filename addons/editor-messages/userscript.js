52b04fd9c5134fd0be014c9d3407282d0e285f28
TheColaber <TheColaber@users.noreply.github.com>
2020-11-01 15:14:34 +0000

    style: format code

--- addons/editor-messages/userscript.js
+++ addons/editor-messages/userscript.js
@@ -1,14 +1,16 @@
 export default async function ({ addon, global, console }) {
   while (true) {
-    let nav = await addon.tab.waitForElement("[class^='menu-bar_account-info-group'] > [href^='/my']", {markAsSeen: true});
-    const messages = document.createElement("a")
-    messages.href = "/messages/"
-    messages.title = "Messages"
-    messages.classList.add("sa-editormessages")
-    let messageCount = document.createElement("span")
-    messageCount.classList.add("sa-editormessages-count")
-    messages.appendChild(messageCount)
-    document.querySelector("[class^='menu-bar_account-info-group']").insertBefore(messages, nav)
+    let nav = await addon.tab.waitForElement("[class^='menu-bar_account-info-group'] > [href^='/my']", {
+      markAsSeen: true,
+    });
+    const messages = document.createElement("a");
+    messages.href = "/messages/";
+    messages.title = "Messages";
+    messages.classList.add("sa-editormessages");
+    let messageCount = document.createElement("span");
+    messageCount.classList.add("sa-editormessages-count");
+    messages.appendChild(messageCount);
+    document.querySelector("[class^='menu-bar_account-info-group']").insertBefore(messages, nav);
     const setMessages = async () => {
       const msgCount = await addon.account.getMsgCount();
       messageCount.innerText = msgCount;
