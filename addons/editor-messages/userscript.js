5cd831de1edd4d243e3e9fffd33c3042e320c884
TheColaber <TheColaber@users.noreply.github.com>
2020-11-01 13:59:37 +0000

    style: format code

--- addons/editor-messages/userscript.js
+++ addons/editor-messages/userscript.js
@@ -1,22 +1,18 @@
-export default async function({
-  addon,
-  global,
-  console
-}) {
+export default async function ({ addon, global, console }) {
   while (true) {
     let nav = await addon.tab.waitForElement("[href^='/my']", {
-      markAsSeen: true
+      markAsSeen: true,
     });
-    const messages = document.createElement("a")
-    messages.href = "/messages/"
-    messages.title = "Messages"
-    messages.classList.add("sa-editormessages")
-    let messageCount = document.createElement("span")
-    messageCount.classList.add("sa-editormessages-count")
-    messages.appendChild(messageCount)
-    document.querySelector("[class^='menu-bar_account-info-group']").insertBefore(messages, nav)
+    const messages = document.createElement("a");
+    messages.href = "/messages/";
+    messages.title = "Messages";
+    messages.classList.add("sa-editormessages");
+    let messageCount = document.createElement("span");
+    messageCount.classList.add("sa-editormessages-count");
+    messages.appendChild(messageCount);
+    document.querySelector("[class^='menu-bar_account-info-group']").insertBefore(messages, nav);
     const setMessages = async () => {
-      const msgCount = await addon.account.getMsgCount()
+      const msgCount = await addon.account.getMsgCount();
       messageCount.innerText = msgCount;
       if (msgCount == 0) {
         messageCount.setAttribute("style", `display: none;`);
