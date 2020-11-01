db9c5122d9011efd4c73bca6fa3219fb9d962644
WorldLanguages <WorldLanguages@users.noreply.github.com>
2020-11-01 00:07:13 +0000

    style: format code

--- addons/animated-thumb/userscript.js
+++ addons/animated-thumb/userscript.js
@@ -1,25 +1,28 @@
-export default async function({ addon, global, console }) {
+export default async function ({ addon, global, console }) {
   while (true) {
     let nav = await addon.tab.waitForElement("[class^='menu-bar_main-menu']", {
-      markAsSeen: true
+      markAsSeen: true,
     });
     if (!document.querySelectorAll("[class^='author-info_username-line']").length > 0) {
-      let setthumb = document.createElement("div")
-      setthumb.classList.add("menu-bar_menu-bar-item_oLDa-")
-      let thumbinner = document.createElement("span")
-      thumbinner.setAttribute("class", "button_outlined-button_1bS__ menu-bar_menu-bar-button_3IDN0 community-button_community-button_2Lo_g")
-      thumbinner.setAttribute("role", "button")
-      setthumb.append(thumbinner)
-      let thumbcontent = document.createElement("div")
-      setthumb.classList.add("button_content_3jdgj")
-      thumbinner.append(thumbcontent)
-      let thumbspan = document.createElement("span")
-      thumbspan.innerText = "Set Thumbnail"
-      thumbcontent.append(thumbspan)
-      nav.append(setthumb)
-      setthumb.addEventListener("click", function(e) {
-        addon.tab.loadScript("https://worldlanguages.github.io/animatedThumbnailsBookmarklet/code.js")
-      })
+      let setthumb = document.createElement("div");
+      setthumb.classList.add("menu-bar_menu-bar-item_oLDa-");
+      let thumbinner = document.createElement("span");
+      thumbinner.setAttribute(
+        "class",
+        "button_outlined-button_1bS__ menu-bar_menu-bar-button_3IDN0 community-button_community-button_2Lo_g"
+      );
+      thumbinner.setAttribute("role", "button");
+      setthumb.append(thumbinner);
+      let thumbcontent = document.createElement("div");
+      setthumb.classList.add("button_content_3jdgj");
+      thumbinner.append(thumbcontent);
+      let thumbspan = document.createElement("span");
+      thumbspan.innerText = "Set Thumbnail";
+      thumbcontent.append(thumbspan);
+      nav.append(setthumb);
+      setthumb.addEventListener("click", function (e) {
+        addon.tab.loadScript("https://worldlanguages.github.io/animatedThumbnailsBookmarklet/code.js");
+      });
     }
   }
 }
