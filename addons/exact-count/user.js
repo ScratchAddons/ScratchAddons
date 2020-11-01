91a34352b2c6b461a96a41a2aca29b1a4a06f8a4
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-11-01 08:15:41 -0500

    Bug Fixed
    May be one of the ugliest code I've ever written.

--- addons/exact-count/user.js
+++ addons/exact-count/user.js
@@ -3,39 +3,38 @@ export default async function ({ addon, global, console }) {
   let user1 = url.substring(30, 100);
   let username = user1.substring(0, user1.indexOf("/"));
   let dalength = document.getElementsByClassName("box-head").length;
-
+  var boxNumber = 1
   getUserData({
     boxheadName: "Shared Projects",
-    url: "projects",
-    boxNumber: 1,
-  });
-  getUserData({
-    boxheadName: "Favorite Projects",
-    url: "favorites",
-    boxNumber: 2,
-  });
-  getUserData({
-    boxheadName: "Studios I'm Following",
-    url: "studios_following",
-    boxNumber: 3,
-  });
-  getUserData({
-    boxheadName: "Studios I Curate",
-    url: "studios",
-    boxNumber: 4,
-  });
-  getUserData({
-    boxheadName: "Following",
-    url: "following",
-    boxNumber: 5,
-  });
-  getUserData({
-    boxheadName: "Followers",
-    url: "followers",
-    boxNumber: 6,
+    url: "projects"
+  }, function () {
+    getUserData({
+      boxheadName: "Favorite Projects",
+      url: "favorites"
+    }, function () {
+      getUserData({
+        boxheadName: "Studios I'm Following",
+        url: "studios_following"
+      }, function () {
+        getUserData({
+          boxheadName: "Studios I Curate",
+          url: "studios"
+        }, function () {
+          getUserData({
+            boxheadName: "Following",
+            url: "following"
+          }, function () {
+            getUserData({
+              boxheadName: "Followers",
+              url: "followers"
+            });
+          });
+        });
+      });
+    });
   });
 
-  function getUserData(details) {
+  function getUserData(details, callback=null) {
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
@@ -43,7 +42,14 @@ export default async function ({ addon, global, console }) {
         let find = response.search("<h2>");
         let follownum = response.substring(find, find + 200).match(/\(([^)]+)\)/)[1];
         let a = `${details.boxheadName} (${follownum})`;
-        document.querySelectorAll(".box-head h4")[details.boxNumber - 1].innerText = a;
+        let current = document.querySelectorAll(".box-head h4")[boxNumber - 1]
+        if (current.innerText.startsWith(details.boxheadName)) {
+          current.innerText = a;
+          boxNumber++
+        }
+        if (callback) {
+          callback()
+        }
       }
     };
     xmlhttp.open("GET", `https://scratch.mit.edu/users/${username}/${details.url}/`, true);
