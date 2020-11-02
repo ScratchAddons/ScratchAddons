16fea74fadde10c2fcec2dab8f5e6665de5b1dc7
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-11-02 14:21:49 +0000

    style: format code

--- addons/full-signature/working.js
+++ addons/full-signature/working.js
@@ -1,8 +1,8 @@
 export default async function ({ addon, global, console }) {
   let activityStream = document.querySelectorAll(".activity-stream li");
-  let loadMore = document.createElement("button")
-  loadMore.classList.add("load-more-wibd")
-  loadMore.innerText = "Load More"
+  let loadMore = document.createElement("button");
+  loadMore.classList.add("load-more-wibd");
+  loadMore.innerText = "Load More";
   document.querySelector(".activity-stream").appendChild(loadMore);
   let dataLoaded = 6;
   loadMore.addEventListener("click", function () {
@@ -12,7 +12,7 @@ export default async function ({ addon, global, console }) {
       }&max=1000000`)
       .then((response) => response.text())
       .then((response) => {
-        var html = new DOMParser().parseFromString(response, "text/html"); 
+        var html = new DOMParser().parseFromString(response, "text/html");
         html.querySelectorAll("ul > li").forEach((li, index) => {
           if (index > dataLoaded && index < dataLoaded + 6) {
             activityStream[activityStream.length - 1].append(li);
