82175690ddbb5d706716ff9061d811bd8215fa4e
WorldLanguages <mundofinky@gmail.com>
2020-11-06 18:17:43 -0300

    Enable some old addons for all users on v1.3.0

--- background/get-addon-settings.js
+++ background/get-addon-settings.js
@@ -1,6 +1,19 @@
 chrome.storage.sync.get(["addonSettings", "addonsEnabled"], ({ addonSettings = {}, addonsEnabled = {} }) => {
   const func = () => {
     let madeAnyChanges = false;
+
+    // TODO: remove on v1.4.0
+    // Turns on some old addons for existing users, after the removal of the prototype
+    // handler. To detect if this is the first run on v1.3.0 update, we check if a
+    // new addon added on v1.3.0 has an undefined value for enabled. We also make
+    // sure this is not the first run ever by checking if addonsEnabled ~= {}
+    if (Object.keys(addonsEnabled) !== 0 && addonsEnabled["animated-thumb"] === undefined) {
+      addonsEnabled["60fps"] = true;
+      addonsEnabled["full-signature"] = true;
+      addonsEnabled["studio-tools"] = true;
+      madeAnyChanges = true;
+    }
+
     for (const { manifest, addonId } of scratchAddons.manifests) {
       const settings = addonSettings[addonId] || {};
       let madeChangesToAddon = false;
