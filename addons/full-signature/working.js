67d1aebe09d515b6c595568849c4c28b4d2a0a4b
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-11-06 15:10:59 -0500

    Update working.js

--- addons/full-signature/working.js
+++ addons/full-signature/working.js
@@ -1,24 +1,30 @@
 export default async function ({ addon, global, console }) {
   let activityStream = document.querySelectorAll(".activity-stream li");
-  let loadMore = document.createElement("button");
+  let container = document.querySelector(".activity-stream").appendChild(document.createElement("div"));
+  container.classList.add("load-more-wibd-container");
+  let loadMore = container.appendChild(document.createElement("button"));
   loadMore.classList.add("load-more-wibd");
   loadMore.innerText = "Load More";
-  document.querySelector(".activity-stream").appendChild(loadMore);
   let dataLoaded = 6;
-  loadMore.addEventListener("click", function () {
-    fetch(`
-      https://scratch.mit.edu/messages/ajax/user-activity/?user=${
-        document.querySelector(".header-text h2").textContent
-      }&max=1000000`)
-      .then((response) => response.text())
-      .then((response) => {
-        var html = new DOMParser().parseFromString(response, "text/html");
+  fetch(`
+    https://scratch.mit.edu/messages/ajax/user-activity/?user=${
+      window.location.pathname.substring(7).slice(0, -1)
+    }&max=1000000`)
+    .then((response) => response.text())
+    .then((response) => {
+      let html = new DOMParser().parseFromString(response, "text/html");
+      loadMore.addEventListener("click", function () {
+        let lastScroll = document.querySelector(".activity-stream").scrollTop
         html.querySelectorAll("ul > li").forEach((li, index) => {
           if (index > dataLoaded && index < dataLoaded + 6) {
             activityStream[activityStream.length - 1].append(li);
           }
         });
         dataLoaded += 6;
+        document.querySelector(".activity-stream").scrollTop = lastScroll;
+        if (dataLoaded >= html.querySelectorAll("ul > li").length) {
+          loadMore.remove()
+        }
       });
-  });
+    });
 }
