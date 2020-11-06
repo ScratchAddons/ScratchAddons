20e5fb9d7e53fc69e24467729527bcaf5d11c312
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-11-06 12:32:11 -0500

    Addon Fixes (#629)

--- addons/exact-count/user.js
+++ addons/exact-count/user.js
@@ -2,51 +2,54 @@ export default async function ({ addon, global, console }) {
   let url = window.location.href;
   let user1 = url.substring(30, 100);
   let username = user1.substring(0, user1.indexOf("/"));
-  let dalength = document.getElementsByClassName("box-head").length;
-
-  getUserData({
-    boxheadName: "Shared Projects",
-    url: "projects",
-    boxNumber: 1,
-  });
-  getUserData({
-    boxheadName: "Favorite Projects",
-    url: "favorites",
-    boxNumber: 2,
-  });
-  getUserData({
-    boxheadName: "Studios I'm Following",
-    url: "studios_following",
-    boxNumber: 3,
-  });
-  getUserData({
-    boxheadName: "Studios I Curate",
-    url: "studios",
-    boxNumber: 4,
-  });
-  getUserData({
-    boxheadName: "Following",
-    url: "following",
-    boxNumber: 5,
-  });
-  getUserData({
-    boxheadName: "Followers",
-    url: "followers",
-    boxNumber: 6,
-  });
+  getUserData([
+    {
+      boxheadName: "Shared Projects",
+      url: "projects",
+    },
+    {
+      boxheadName: "Favorite Projects",
+      url: "favorites",
+    },
+    {
+      boxheadName: "Studios I'm Following",
+      url: "studios_following",
+    },
+    {
+      boxheadName: "Studios I Curate",
+      url: "studios",
+    },
+    {
+      boxheadName: "Following",
+      url: "following",
+    },
+    {
+      boxheadName: "Followers",
+      url: "followers",
+    },
+  ]);
 
   function getUserData(details) {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-        let response = xmlhttp.responseText;
-        let find = response.search("<h2>");
-        let follownum = response.substring(find, find + 200).match(/\(([^)]+)\)/)[1];
-        let a = `${details.boxheadName} (${follownum})`;
-        document.querySelectorAll(".box-head h4")[details.boxNumber - 1].innerText = a;
-      }
-    };
-    xmlhttp.open("GET", `https://scratch.mit.edu/users/${username}/${details.url}/`, true);
-    xmlhttp.send();
+    for (let j = 0; j < details.length; j++) {
+      let xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+          let response = xmlhttp.responseText;
+          let find = response.search("<h2>");
+          let follownum = response.substring(find, find + 200).match(/\(([^)]+)\)/)[1];
+          let a = `${details[j].boxheadName} (${follownum})`;
+          let boxHeads = document.querySelectorAll(".box-head");
+          for (let i = 1; i < boxHeads.length - 1; i++) {
+            let viewAll = new URL(boxHeads[i].querySelector("a").href).pathname;
+            let link = viewAll.split("/users/" + username + "/")[1];
+            if (link == details[j].url + "/") {
+              boxHeads[i].querySelector("h4").innerText = a;
+            }
+          }
+        }
+      };
+      xmlhttp.open("GET", `https://scratch.mit.edu/users/${username}/${details[j].url}/`, true);
+      xmlhttp.send();
+    }
   }
 }
