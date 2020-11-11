d9ba23f2a09e3ec17468581e3c4dc6e3cf3283d0
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-11-11 10:53:20 -0500

    Fixed Bug

--- addons/last-edit-tooltip/userscript.js
+++ addons/last-edit-tooltip/userscript.js
@@ -11,7 +11,7 @@ export default async function ({ addon, global, console }) {
           "title",
           `Modified: ${dateMod.toLocaleString("en-us", {
             month: "short",
-          })} ${dateMod.getDay()}, ${dateMod.getFullYear()}`
+          })} ${dateMod.getDate()}, ${dateMod.getFullYear()}`
         );
       }
     });
