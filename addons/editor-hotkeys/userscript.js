62b66e78361c67dca4009d90e3ee4b0889a6cd01
TheColaber <TheColaber@users.noreply.github.com>
2020-11-11 14:16:03 +0000

    style: format code

--- addons/editor-hotkeys/userscript.js
+++ addons/editor-hotkeys/userscript.js
@@ -2,7 +2,7 @@ export default async function ({ addon, global, console }) {
   function convert(hotkey) {
     return {
       key: hotkey.slice(-1),
-      shiftKey: hotkey.includes("Shift")
+      shiftKey: hotkey.includes("Shift"),
     };
   }
   function click(query, pick = 0) {
@@ -12,26 +12,34 @@ export default async function ({ addon, global, console }) {
   }
   var allSettings = [];
   fetch(addon.self.dir + "/addon.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    for (var i = 0; i < data.settings.length; i++) {
-      allSettings[i] = data.settings[i].id;
-    }
-  });
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      for (var i = 0; i < data.settings.length; i++) {
+        allSettings[i] = data.settings[i].id;
+      }
+    });
   document.addEventListener("keydown", function (e) {
     for (var i = 0; i < allSettings.length; i++) {
-      let converted = convert(addon.settings.get(allSettings[i]))
-      if (e.shiftKey == converted.shiftKey && e.ctrlKey && (converted.key < 10 && converted.key >= 0 ? (e.shiftKey ? e.code == "Digit" + converted.key : e.key == converted.key) : e.key == converted.key)) {
-        e.preventDefault()
+      let converted = convert(addon.settings.get(allSettings[i]));
+      if (
+        e.shiftKey == converted.shiftKey &&
+        e.ctrlKey &&
+        (converted.key < 10 && converted.key >= 0
+          ? e.shiftKey
+            ? e.code == "Digit" + converted.key
+            : e.key == converted.key
+          : e.key == converted.key)
+      ) {
+        e.preventDefault();
         if (i == 0) {
-          click('[class^="green-flag_green-flag"]')
+          click('[class^="green-flag_green-flag"]');
         } else if (i == 1) {
-          click('[class^="stop-all_stop-all"]')
+          click('[class^="stop-all_stop-all"]');
         } else if (i == 2) {
           if (!click('[class^="community-button_community-button"]')) {
-            click('.see-inside-button')
+            click(".see-inside-button");
           }
         }
       }
