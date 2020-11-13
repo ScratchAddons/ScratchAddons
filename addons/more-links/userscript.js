398a119284ea879b22342ca79a6f09e18e716aac
Hans5958 <email5958.1@gmail.com>
2020-11-13 20:06:02 +0700

    More links: Implement new features, final changes

--- addons/more-links/userscript.js
+++ addons/more-links/userscript.js
@@ -1,10 +1,8 @@
 export default async function ({ addon, console }) {
-  console.log("load");
   await addon.tab.loadScript("https://cdn.jsdelivr.net/npm/linkifyjs@2.1.9/dist/linkify.min.js");
   await addon.tab.loadScript("https://cdn.jsdelivr.net/npm/linkifyjs@2.1.9/dist/linkify-element.min.js");
 
   const pageType = document.location.pathname.substr(1).split("/")[0];
-  console.log(pageType);
 
   switch (pageType) {
     case "users":
@@ -14,17 +12,13 @@ export default async function ({ addon, console }) {
     case "projects":
       (async () => {
         while (true) {
-          console.log("wait projects");
-          await addon.tab.waitForElement(".project-description:not(.more-links-checked)");
-          console.log("go projects");
-          let element = document.querySelector(".project-description"); // How is this laggy?
+          let element = await addon.tab.waitForElement(".project-description", { markAsSeen: true });
           // Need to convert #[numbers] to solve conflict between tags and external Scratch player links.
           document.querySelectorAll(".project-description a").forEach((element) => {
             if (/\d+/.test(element.textContent)) element.outerHTML = element.textContent;
           });
+          element.normalize();
           linkifyElement(element);
-          console.log("linkified", element);
-          element.classList.add("more-links-checked");
         }
       })();
       break;
@@ -34,19 +28,10 @@ export default async function ({ addon, console }) {
       break;
   }
 
-  console.log("comments?");
-
   (async () => {
     while (true) {
-      console.log("wait comments");
-      await addon.tab.waitForElement(".comment:not(.more-links-checked)");
-      console.log("go comments");
-      let comments = document.querySelectorAll(".comment:not(.more-links-checked)");
-      comments.forEach((comment) => {
-        linkifyElement(comment);
-        console.log("linkified", comment);
-        comment.classList.add("more-links-checked");
-      });
+      let comment = await addon.tab.waitForElement(".comment", { markAsSeen: true });
+      linkifyElement(comment);
     }
   })();
 }
