be9f44b0d149b2b44dd92b599d4c35f66fcb10cb
TheColaber <TheColaber@users.noreply.github.com>
2020-11-15 20:04:17 +0000

    style: format code

--- addons/feature-unshared/userscript.js
+++ addons/feature-unshared/userscript.js
@@ -1,15 +1,15 @@
 export default async function ({ addon, global, console }) {
   while (true) {
     let popup = await addon.tab.waitForElement("#featured-project-modal", { markAsSeen: true });
-    let page = 1
+    let page = 1;
     while (popup.querySelector("li")) {
-      popup.querySelector("li").remove()
+      popup.querySelector("li").remove();
     }
-    popup.querySelector("[data-control='load-more']").remove()
-    let loadMore = popup.querySelector(".modal-body").appendChild(document.createElement("div"))
-    loadMore.setAttribute("class", "button small grey")
-    loadMore.id = "sa-loadMore-projects"
-    loadMore.setAttribute("style", "margin:auto;width:100%;text-align:center;")
+    popup.querySelector("[data-control='load-more']").remove();
+    let loadMore = popup.querySelector(".modal-body").appendChild(document.createElement("div"));
+    loadMore.setAttribute("class", "button small grey");
+    loadMore.id = "sa-loadMore-projects";
+    loadMore.setAttribute("style", "margin:auto;width:100%;text-align:center;");
     let text = loadMore.appendChild(document.createElement("span"));
     text.innerText = "Load More";
     appendOptions(page);
@@ -24,10 +24,10 @@ export default async function ({ addon, global, console }) {
         })
         .then((data) => {
           for (var i = 0; i < data.length; i++) {
-            popup.querySelector("ul").appendChild(createOption(data[i].pk, data[i].fields.title))
+            popup.querySelector("ul").appendChild(createOption(data[i].pk, data[i].fields.title));
           }
           if (data.length < 40) {
-            loadMore.remove()
+            loadMore.remove();
           }
         });
     }
@@ -35,15 +35,15 @@ export default async function ({ addon, global, console }) {
   function createOption(projectID, projectTitle) {
     let option = document.createElement("li");
     let container = option.appendChild(document.createElement("div"));
-    container.setAttribute("class", "project thumb")
-    container.setAttribute("data-id", projectID)
+    container.setAttribute("class", "project thumb");
+    container.setAttribute("data-id", projectID);
     let img = container.appendChild(document.createElement("img"));
-    img.src = `//uploads.scratch.mit.edu/projects/thumbnails/${projectID}.png`
+    img.src = `//uploads.scratch.mit.edu/projects/thumbnails/${projectID}.png`;
     let title = container.appendChild(document.createElement("span"));
-    title.classList.add("title")
+    title.classList.add("title");
     let link = title.appendChild(document.createElement("a"));
     link.href = `/projects/${projectID}`;
-    link.innerText = projectTitle
+    link.innerText = projectTitle;
     return option;
   }
 }
