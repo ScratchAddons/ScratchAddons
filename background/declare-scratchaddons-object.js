c74030a8683bdb8e0f66364b5c5388b42d8d1bba
apple502j <33279053+apple502j@users.noreply.github.com>
2020-11-16 00:48:33 +0900

    Remove Lord's Prayer

--- background/declare-scratchaddons-object.js
+++ background/declare-scratchaddons-object.js
@@ -22,16 +22,6 @@ scratchAddons.manifests = [];
 // Other files may add their own global methods here so that addon-api files can access them
 scratchAddons.methods = {};
 
-/*
-Our Father in ECMA,
-hallowed be your name.
-Your proposal come.
-Your will be done, on global scope as it is in async function.
-Give us this syntax and our top level await.
-And forgive us our deadlocks, as we also have forgiven our sibling execution.
-And do not bring us to the time of polyfill, but rescue us from the asynchronous loading.
-For the specification and the documentation and the benefits are yours forever. Amen.
-*/
 scratchAddons.l10n = new BackgroundLocalizationProvider();
 // Can't load translations here.
 
