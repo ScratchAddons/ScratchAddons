19c616849ac38a1d3d6a832d87335cfa216e6e70
Thomas Weber <weber_t@mailbox.org>
2020-11-15 21:54:52 -0600

    Run prettier

--- addons/block-switching/userscript.js
+++ addons/block-switching/userscript.js
@@ -2,224 +2,224 @@ export default async function ({ addon, global, console }) {
   const blockSwitches = {
     motion_turnright: [
       {
-        opcode: "motion_turnleft"
-      }
+        opcode: "motion_turnleft",
+      },
     ],
     motion_turnleft: [
       {
-        opcode: "motion_turnright"
-      }
+        opcode: "motion_turnright",
+      },
     ],
     operator_equals: [
       {
-        opcode: "operator_gt"
+        opcode: "operator_gt",
       },
       {
-        opcode: "operator_lt"
-      }
+        opcode: "operator_lt",
+      },
     ],
     operator_gt: [
       {
-        opcode: "operator_equals"
+        opcode: "operator_equals",
       },
       {
-        opcode: "operator_lt"
-      }
+        opcode: "operator_lt",
+      },
     ],
     operator_lt: [
       {
-        opcode: "operator_equals"
+        opcode: "operator_equals",
       },
       {
-        opcode: "operator_gt"
-      }
+        opcode: "operator_gt",
+      },
     ],
     operator_add: [
       {
-        opcode: "operator_subtract"
+        opcode: "operator_subtract",
       },
       {
-        opcode: "operator_multiply"
+        opcode: "operator_multiply",
       },
       {
-        opcode: "operator_divide"
+        opcode: "operator_divide",
       },
       {
-        opcode: "operator_mod"
-      }
+        opcode: "operator_mod",
+      },
     ],
     operator_subtract: [
       {
-        opcode: "operator_add"
+        opcode: "operator_add",
       },
       {
-        opcode: "operator_multiply"
+        opcode: "operator_multiply",
       },
       {
-        opcode: "operator_divide"
+        opcode: "operator_divide",
       },
       {
-        opcode: "operator_mod"
-      }
+        opcode: "operator_mod",
+      },
     ],
     operator_multiply: [
       {
-        opcode: "operator_add"
+        opcode: "operator_add",
       },
       {
-        opcode: "operator_subtract"
+        opcode: "operator_subtract",
       },
       {
-        opcode: "operator_divide"
+        opcode: "operator_divide",
       },
       {
-        opcode: "operator_mod"
-      }
+        opcode: "operator_mod",
+      },
     ],
     operator_divide: [
       {
-        opcode: "operator_add"
+        opcode: "operator_add",
       },
       {
-        opcode: "operator_subtract"
+        opcode: "operator_subtract",
       },
       {
-        opcode: "operator_multiply"
+        opcode: "operator_multiply",
       },
       {
-        opcode: "operator_mod"
-      }
+        opcode: "operator_mod",
+      },
     ],
     operator_mod: [
       {
-        opcode: "operator_add"
+        opcode: "operator_add",
       },
       {
-        opcode: "operator_subtract"
+        opcode: "operator_subtract",
       },
       {
-        opcode: "operator_multiply"
+        opcode: "operator_multiply",
       },
       {
-        opcode: "operator_divide"
-      }
+        opcode: "operator_divide",
+      },
     ],
     motion_setx: [
       {
         opcode: "motion_changexby",
-        remap: {X: 'DX'}
+        remap: { X: "DX" },
       },
       {
         opcode: "motion_sety",
-        remap: {X: 'Y'}
+        remap: { X: "Y" },
       },
       {
         opcode: "motion_changeyby",
-        remap: {X: 'DY'}
-      }
+        remap: { X: "DY" },
+      },
     ],
     motion_changexby: [
       {
         opcode: "motion_setx",
-        remap: {DX: 'X'}
+        remap: { DX: "X" },
       },
       {
         opcode: "motion_sety",
-        remap: {DX: 'Y'}
+        remap: { DX: "Y" },
       },
       {
         opcode: "motion_changeyby",
-        remap: {DX: 'DY'}
-      }
+        remap: { DX: "DY" },
+      },
     ],
     motion_sety: [
       {
         opcode: "motion_setx",
-        remap: {Y: 'X'}
+        remap: { Y: "X" },
       },
       {
         opcode: "motion_changexby",
-        remap: {Y: 'DX'}
+        remap: { Y: "DX" },
       },
       {
         opcode: "motion_changeyby",
-        remap: {Y: 'DY'}
-      }
+        remap: { Y: "DY" },
+      },
     ],
     motion_changeyby: [
       {
         opcode: "motion_setx",
-        remap: {DY: 'X'}
+        remap: { DY: "X" },
       },
       {
         opcode: "motion_changexby",
-        remap: {DY: 'DX'}
+        remap: { DY: "DX" },
       },
       {
         opcode: "motion_sety",
-        remap: {DY: 'Y'}
-      }
+        remap: { DY: "Y" },
+      },
     ],
     data_setvariableto: [
       {
-        opcode: "data_changevariableby"
-      }
+        opcode: "data_changevariableby",
+      },
     ],
     data_changevariableby: [
       {
-        opcode: "data_setvariableto"
-      }
+        opcode: "data_setvariableto",
+      },
     ],
     data_showvariable: [
       {
-        opcode: "data_hidevariable"
-      }
+        opcode: "data_hidevariable",
+      },
     ],
     data_hidevariable: [
       {
-        opcode: "data_showvariable"
-      }
+        opcode: "data_showvariable",
+      },
     ],
     motion_xposition: [
       {
-        opcode: "motion_yposition"
-      }
+        opcode: "motion_yposition",
+      },
     ],
     motion_yposition: [
       {
-        opcode: "motion_xposition"
-      }
+        opcode: "motion_xposition",
+      },
     ],
     looks_show: [
       {
-        opcode: "looks_hide"
-      }
+        opcode: "looks_hide",
+      },
     ],
     looks_hide: [
       {
-        opcode: "looks_show"
-      }
+        opcode: "looks_show",
+      },
     ],
     control_if: [
       {
-        opcode: "control_if_else"
-      }
+        opcode: "control_if_else",
+      },
     ],
     control_if_else: [
       {
-        opcode: "control_if"
-      }
+        opcode: "control_if",
+      },
     ],
     sensing_mousex: [
       {
-        opcode: "sensing_mousey"
-      }
+        opcode: "sensing_mousey",
+      },
     ],
     sensing_mousey: [
       {
-        opcode: "sensing_mousex"
-      }
-    ]
+        opcode: "sensing_mousex",
+      },
+    ],
   };
 
   const blockToDom = (block) => {
@@ -236,7 +236,7 @@ export default async function ({ addon, global, console }) {
     // Make a copy of the block with the proper type set.
     // It doesn't seem to be possible to change a Block's type after it's created, so we'll just make a new block instead.
     const xml = blockToDom(block);
-    xml.setAttribute('type', opcodeData.opcode);
+    xml.setAttribute("type", opcodeData.opcode);
 
     const id = block.id;
     const parent = block.getParent();
@@ -249,18 +249,20 @@ export default async function ({ addon, global, console }) {
       parentConnection = parentConnections.find((c) => c.targetConnection && c.targetConnection.sourceBlock_ === block);
       // There's two types of connections from child -> parent. We need to figure out which one is used.
       const blockConnections = block.getConnections_();
-      const blockToParentConnection = blockConnections.find((c) => c.targetConnection && c.targetConnection.sourceBlock_ === parent);
+      const blockToParentConnection = blockConnections.find(
+        (c) => c.targetConnection && c.targetConnection.sourceBlock_ === parent
+      );
       blockConnectionType = blockToParentConnection.type;
     }
 
     // Apply input remappings.
     if (opcodeData.remap) {
       for (const child of xml.children) {
-        if (child.tagName === 'VALUE') {
-          const oldName = child.getAttribute('name');
+        if (child.tagName === "VALUE") {
+          const oldName = child.getAttribute("name");
           const newName = opcodeData.remap[oldName];
           if (newName) {
-            child.setAttribute('name', newName);
+            child.setAttribute("name", newName);
           }
         }
       }
@@ -291,7 +293,7 @@ export default async function ({ addon, global, console }) {
       options.push({
         enabled: true,
         text: opcodeData.opcode, // TODO: display human readable name; translate
-        callback: menuCallbackFactory(this, opcodeData)
+        callback: menuCallbackFactory(this, opcodeData),
       });
     }
   };
@@ -309,10 +311,10 @@ export default async function ({ addon, global, console }) {
 
     block._blockswitchingNativeContextMenu = block.customContextMenu;
     block.customContextMenu = customContextMenuHandler;
-  }
+  };
 
   const changeListener = (change) => {
-    if (change.type !== 'create') {
+    if (change.type !== "create") {
       return;
     }
 
