bfc16ca802552656fe2b7090511e06841c19c302
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-11-17 08:45:33 -0500

    Update userscript.js

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -1,19 +1,26 @@
 export default async function ({ addon, global, console }) {
   const nav = await addon.tab.waitForElement(".sub-nav.tabs");
-
-  //Select where the new tab will be appended, and create a new tab
+  //Create elements for tab
   const tab = nav.appendChild(document.createElement("a")),
     li = tab.appendChild(document.createElement("li")),
     img = li.appendChild(document.createElement("img")),
-    span = li.appendChild(document.createElement("span"));
+    span = li.appendChild(document.createElement("span")),
+    user = document.querySelector('[name="q"]').value.trim(),
+    url = "/users/" + user + "/";
 
-  //Link to the profile
-  tab.href = "/users/" + document.querySelector('[name="q"]').value.trim() + "/";
-
-  //Set up the image.
+  //Set up elements
   img.src = addon.self.dir + "/user.svg";
   img.className = "tab-icon";
-
-  //Add the text below the image
   span.innerText = "Profile";
+
+  //Check if user exsits
+  fetch(url)
+    .then((response) => {
+      if (response.ok) tab.href = url;
+      else {
+        img.style.filter = "grayscale(100%) brightness(100%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(1)";
+        span.style.color = "red";
+        li.title = user + " does not exist!"
+      }
+    });
 }
