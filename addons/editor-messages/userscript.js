a260d96777762b701be6a8cb34994ca4130fb1b0
WorldLanguages <WorldLanguages@users.noreply.github.com>
2020-11-20 17:27:30 +0000

    style: format code

--- addons/editor-messages/userscript.js
+++ addons/editor-messages/userscript.js
@@ -25,7 +25,7 @@ export default async function ({ addon, global, console }) {
         setInterval(setMessages, 5000);
         addon.tab.removeEventListener("urlChange", thisFunction);
       }
-    })
+    });
   }
 
   while (true) {
