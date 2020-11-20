2cd8d09420acbb50d5aad85e78282763ce4f4082
TheColaber <TheColaber@users.noreply.github.com>
2020-11-20 23:17:04 +0000

    style: format code

--- addons/exact-count/forum.js
+++ addons/exact-count/forum.js
@@ -2,11 +2,11 @@ export default async function ({ addon, global, console }) {
   while (true) {
     let userbox = await addon.tab.waitForElement(".postleft dl", { markAsSeen: true });
     if (userbox.childNodes[6] ? userbox.childNodes[6].nodeValue.includes("+") : false) {
-      fetch('https://scratchdb.lefty.one/v2/forum/user/info/' + userbox.querySelector("a").innerText)
+      fetch("https://scratchdb.lefty.one/v2/forum/user/info/" + userbox.querySelector("a").innerText)
         .then((response) => response.json())
         .then((data) => {
           userbox.childNodes[6].nodeValue = `${data.counts.total.count} posts`;
-        })
+        });
     }
   }
 }
