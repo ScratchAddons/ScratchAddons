a0415d9b9c7ed9b3479e1c89850e87c02a359ec1
pufferfish101007 <pufferfish101007@users.noreply.github.com>
2020-11-21 15:04:42 +0000

    style: format code

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -6,7 +6,11 @@ export default async function ({ addon, global, console, msg }) {
     img = li.appendChild(document.createElement("img")),
     span = li.appendChild(document.createElement("span")),
     user = document.querySelector('[name="q"]').value.trim(),
-    valid = user.length <= 20 && user.length >= 3 && !user.includes(" ") && user.match(/([\x30-\x39]|[\x41-\x5a]|[\x61-\x7a]|\x2d|\x5f)+/g)[0] == user;
+    valid =
+      user.length <= 20 &&
+      user.length >= 3 &&
+      !user.includes(" ") &&
+      user.match(/([\x30-\x39]|[\x41-\x5a]|[\x61-\x7a]|\x2d|\x5f)+/g)[0] == user;
   //Set up elements
   img.src = addon.self.dir + "/user.svg";
   img.className = "tab-icon";
@@ -16,6 +20,6 @@ export default async function ({ addon, global, console, msg }) {
   if (!valid) {
     img.style.filter = "grayscale(100%) brightness(100%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(1)";
     span.style.color = "red";
-    li.title = msg("invalid-username", {username: user});
+    li.title = msg("invalid-username", { username: user });
   }
 }
