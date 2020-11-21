468a3efd3bfa89bea4a5303815fd67d4d1a7da89
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-11-21 10:20:16 +0000

    Update userscript.js

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -1,16 +1,11 @@
 export default async function ({ addon, global, console }) {
   const nav = await addon.tab.waitForElement(".sub-nav.tabs");
   // Get the search term and get the api end point to check the username
-  const userPage = `https://api.scratch.mit.edu/accounts/checkusername/${document
+  const searchTerm = document
     .querySelector('[name="q"]')
-    .value.trim()}/`;
-  // Fetch the user page to see whether the user exists
-  const response = await fetch(userPage);
-  // Get the json of the response
-  const json = await response.json();
-
-  // If the username exists make the link to the user page
-  if (json.message === "username exists") {
+    .value.trim();
+  // If the username has the potential to be a valid username then create the link to the user page
+  if (!/[^a-z1-9_-]+/i.test(searchTerm) && 1 < searchTerm.length && searchTerm.length <= 20) {
     //Select where the new tab will be appended, and create a new tab
     const tab = nav.appendChild(document.createElement("a")),
       li = tab.appendChild(document.createElement("li")),
@@ -18,8 +13,7 @@ export default async function ({ addon, global, console }) {
       span = li.appendChild(document.createElement("span"));
 
     // Link to the profile
-    tab.href = `https://scratch.mit.edu/users/${document.querySelector('[name="q"]').value.trim()}/`;
-    // Set up the image
+    tab.href = `https://scratch.mit.edu/users/${searchTerm}/`;
     img.src = addon.self.dir + "/user.svg";
     img.className = "tab-icon";
 
