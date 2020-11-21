5563d1d53d92d753f14b3df7ef554074a6dd59ce
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-11-20 19:07:29 -0500

    Update forum.js

--- addons/exact-count/forum.js
+++ addons/exact-count/forum.js
@@ -1,7 +1,7 @@
 export default async function ({ addon, global, console }) {
   while (true) {
     let userbox = await addon.tab.waitForElement(".postleft dl", { markAsSeen: true });
-    if (userbox.childNodes[6] ? userbox.childNodes[6].nodeValue.includes("+") : false) {
+    if (userbox.childNodes[6] && userbox.childNodes[6].nodeValue.includes("+")) {
       fetch("https://scratchdb.lefty.one/v2/forum/user/info/" + userbox.querySelector("a").innerText)
         .then((response) => response.json())
         .then((data) => {
