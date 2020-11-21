fd05574addc7d95f26428fff1f30e80ebaf9d83e
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-11-21 18:57:15 -0500

    Fix WL things

--- addons/confirm-actions/userscript.js
+++ addons/confirm-actions/userscript.js
@@ -1,26 +1,21 @@
 export default async function ({ addon, console }) {
-  if (addon.settings.get("projectsharing"))
-    actionAlert("[class*='share-button_share-button']", "Are you sure you want to share?");
-  if (addon.settings.get("followinguser"))
-    actionAlert(
-      ".follow-button",
-      `Are you sure you want to follow/unfollow @${
-        document.querySelector(".header-text h2") ? document.querySelector(".header-text h2").innerText : ""
-      }?`
-    );
+  if (addon.settings.get("projectsharing")) actionAlert("[class*='share-button_share-button'], .banner-button", "Are you sure you want to share?");
+  if (addon.settings.get("followinguser")) actionAlert(".follow-button", "Are you sure you want to follow/unfollow this user?");
   if (addon.settings.get("joiningstudio")) actionAlert("a.accept", "Are you sure you would like to join this studio?");
   async function actionAlert(queryButton, res) {
-    let button = await addon.tab.waitForElement(queryButton);
-    let canAction = false;
-    button.addEventListener("click", function (e) {
-      if (!canAction) {
-        e.cancelBubble = true;
-        e.preventDefault();
-        if (confirm("Scratch Addons:\n" + res)) {
-          canAction = true;
-          button.click();
-        }
-      } else canAction = false;
-    });
+    while (true) {
+      let button = await addon.tab.waitForElement(queryButton, { markAsSeen: true });
+      let canAction = false;
+      button.addEventListener("click", function (e) {
+        if (!canAction) {
+          e.cancelBubble = true;
+          e.preventDefault();
+          if (confirm("Scratch Addons:\n" + res)) {
+            canAction = true;
+            button.click();
+          }
+        } else canAction = false;
+      });
+    }
   }
 }
