c6fc9805f92f6baf12c4f8ac9d7464629dac4d16
pufferfish101007 <pufferfish101007@users.noreply.github.com>
2020-11-21 10:20:42 +0000

    style: format code

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -1,9 +1,7 @@
 export default async function ({ addon, global, console }) {
   const nav = await addon.tab.waitForElement(".sub-nav.tabs");
   // Get the search term and get the api end point to check the username
-  const searchTerm = document
-    .querySelector('[name="q"]')
-    .value.trim();
+  const searchTerm = document.querySelector('[name="q"]').value.trim();
   // If the username has the potential to be a valid username then create the link to the user page
   if (!/[^a-z1-9_-]+/i.test(searchTerm) && 1 < searchTerm.length && searchTerm.length <= 20) {
     //Select where the new tab will be appended, and create a new tab
