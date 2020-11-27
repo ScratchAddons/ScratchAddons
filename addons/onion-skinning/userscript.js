8e8133329df14cf9cc2500391739e6248c67e50d
Thomas Weber <weber_t@mailbox.org>
2020-11-27 13:13:21 -0600

    better

--- addons/onion-skinning/userscript.js
+++ addons/onion-skinning/userscript.js
@@ -1,14 +1,11 @@
 export default async function ({ addon, global, console }) {
-  if (!addon.tab.redux.state) return console.warn("Redux is not available!");
-
   let project;
   let onionLayer;
-  let paintLayer;
   let paperCanvas;
 
   const foundPaper = (_project) => {
     project = _project;
-    console.log('project', _project);
+    console.log("project", _project);
 
     const originalExportSVG = project.exportSVG;
     project.exportSVG = function (...args) {
@@ -17,61 +14,70 @@ export default async function ({ addon, global, console }) {
       this.addLayer(onionLayer);
       return ret;
     };
-
-    // setTimeout(() => {
-    // //   const layer = project.activeLayer.clone();
-    // //   layer.removeChildren();
-    // //   console.log('layer', layer);
-
-    // //   const costume = addon.tab.redux.state.scratchGui.vm.editingTarget.sprite.costumes[0];
-
-    // //   project.importSVG(cat, {
-    // //     expandShapes: true,
-    // //     onLoad: function(item) {
-    // //       item.remove();
-    // //       console.log('item', item);
-    // //       layer.addChild(item);
-    // //     }
-    // //   });
-    // }, 1000);
   };
 
   const foundPaperCanvas = (_paperCanvas) => {
     paperCanvas = _paperCanvas;
-    console.log('paperCanvas', paperCanvas);
+    console.log("paperCanvas", paperCanvas);
 
-    paintLayer = project.activeLayer;
-
-    onionLayer = project.activeLayer.clone();
-    console.log('onionLayer', onionLayer);
+    const paintLayer = project.activeLayer;
+    onionLayer = new project.activeLayer.constructor();
+    console.log("onionLayer", onionLayer);
     onionLayer.opacity = 0.2;
-    onionLayer.data = {};
     onionLayer.locked = true;
-    onionLayer.removeChildren();
+    paintLayer.activate();
+
+    updateOnionLayer();    
+  };
+
+  const updateOnionLayer = () => {
+    const costumeList = Array.from(document.querySelector("[class^='selector_list-area']").children);
+    let selectedCostume = -1;
+    for (let i = 0; i < costumeList.length; i++) {
+      const item = costumeList[i].firstChild;
+      if (item && item.className.includes("is-selected")) {
+        selectedCostume = i;
+        break;
+      }
+    }
+
+    if (selectedCostume === -1) {
+      // Should never happen.
+      throw new Error("Couldn't find selected costume");
+    }
+
+    if (selectedCostume === 0) {
+      // Can"t show an onion skin if there is no previous skin.
+      return;
+    }
+
+    const onionIndex = selectedCostume - 1;
+
+    const vm = addon.tab.traps.onceValues.vm;
+    const costume = vm.editingTarget.sprite.costumes[onionIndex];
+    const asset = vm.getCostume(onionIndex);
+
+    if (typeof paperCanvas.initializeSvg !== "function" || typeof paperCanvas.importSvg !== "function") {
+      throw new Error("Assumptions invalid.");
+    }
 
     const originalInitializeSvg = paperCanvas.initializeSvg;
     paperCanvas.initializeSvg = function (...args) {
+      const paintLayer = project.activeLayer;
       onionLayer.activate();
       originalInitializeSvg.call(this, ...args);
       paintLayer.activate();
       paperCanvas.initializeSvg = originalInitializeSvg;
     };
 
-    updateOnionLayer();    
-  };
-
-  const updateOnionLayer = () => {
-    const vm = addon.tab.redux.state.scratchGui.vm;
-    const costume = vm.editingTarget.sprite.costumes[0];
-    const asset = vm.getCostume(0);
-
     paperCanvas.importSvg(asset, costume.rotationCenterX, costume.rotationCenterY);
   };
 
   // https://github.com/LLK/paper.js/blob/16d5ff0267e3a0ef647c25e58182a27300afad20/src/item/Project.js#L64-L65
-  Object.defineProperty(Object.prototype, '_view', {
+  Object.defineProperty(Object.prototype, "_view", {
     set(value) {
-      Object.defineProperty(this, '_view', {
+      // TODO: this can and will break things
+      Object.defineProperty(this, "_view", {
         value: value,
         writable: true
       });
@@ -79,9 +85,11 @@ export default async function ({ addon, global, console }) {
     }
   });
 
-  Object.defineProperty(Object.prototype, 'queuedImport', {
+  // https://github.com/LLK/scratch-paint/blob/develop/src/containers/paper-canvas.jsx
+  Object.defineProperty(Object.prototype, "queuedImport", {
     set(value) {
-      Object.defineProperty(this, 'queuedImport', {
+      // TODO: this can and will break things
+      Object.defineProperty(this, "queuedImport", {
         value: value,
         writable: true
       });
