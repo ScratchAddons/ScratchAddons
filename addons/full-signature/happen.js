aa8a7456832fcc787d201e03340c18ac0dcff7ae
TheColaber <TheColaber@users.noreply.github.com>
2020-12-02 20:32:13 +0000

    style: format code

--- addons/full-signature/happen.js
+++ addons/full-signature/happen.js
@@ -5,30 +5,34 @@ export default async function ({ addon, global, console, msg }) {
     let container = document.querySelector(".activity-ul").appendChild(document.createElement("div"));
     container.classList.add("load-more-wh-container");
     let loadMore = container.appendChild(document.createElement("button"));
-    loadMore.className = "load-more-wh button"
+    loadMore.className = "load-more-wh button";
     loadMore.innerText = msg("load-more");
     let dataLoaded = 5;
-    let fetched = []
-    let displayedFetch = []
-    loadMore.addEventListener(
-      "click",
-      async function () {
-        dataLoaded+=5
-        console.log(Math.floor(dataLoaded/40)*40);
-        if (dataLoaded > fetched.length) {
-          await addon.fetch(`
-          https://api.scratch.mit.edu/users/${addon.auth.username}/following/users/activity?limit=40&offset=${Math.floor(dataLoaded/40)*40}`)
-            .then((response) => response.json())
-            .then((rows) => {
-              rows.filter(item => fetched.find(item2 => item2.id === item.id) === undefined).forEach(item => fetched.push(item));
-            });
-        }
-        displayedFetch = fetched.slice(0, dataLoaded)
-        console.log(displayedFetch);
-        await addon.tab.redux.dispatch({type: "SET_ROWS", rowType: "activity", rows:displayedFetch})
-        document.querySelector(".activity-ul").appendChild(container);
-        if (dataLoaded > fetched.length) container.remove()
+    let fetched = [];
+    let displayedFetch = [];
+    loadMore.addEventListener("click", async function () {
+      dataLoaded += 5;
+      console.log(Math.floor(dataLoaded / 40) * 40);
+      if (dataLoaded > fetched.length) {
+        await addon
+          .fetch(
+            `
+          https://api.scratch.mit.edu/users/${addon.auth.username}/following/users/activity?limit=40&offset=${
+              Math.floor(dataLoaded / 40) * 40
+            }`
+          )
+          .then((response) => response.json())
+          .then((rows) => {
+            rows
+              .filter((item) => fetched.find((item2) => item2.id === item.id) === undefined)
+              .forEach((item) => fetched.push(item));
+          });
       }
-    );
+      displayedFetch = fetched.slice(0, dataLoaded);
+      console.log(displayedFetch);
+      await addon.tab.redux.dispatch({ type: "SET_ROWS", rowType: "activity", rows: displayedFetch });
+      document.querySelector(".activity-ul").appendChild(container);
+      if (dataLoaded > fetched.length) container.remove();
+    });
   }
 }
