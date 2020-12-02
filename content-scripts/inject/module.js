cce62789261a83e39f84ab749de95c3545f40300
WorldLanguages <mundofinky@gmail.com>
2020-12-02 14:36:51 -0300

    Make sure body exists before trying to add a class to it

--- content-scripts/inject/module.js
+++ content-scripts/inject/module.js
@@ -40,10 +40,11 @@ scratchAddons.methods.getMsgCount = () => {
 function bodyIsEditorClassCheck() {
   const pathname = location.pathname.toLowerCase();
   const split = pathname.split("/").filter(Boolean);
+  if (!split[0] || split[0] !== "projects") return;
   if (split.includes("editor") || split.includes("fullscreen")) document.body.classList.add("sa-body-editor");
   else document.body.classList.remove("sa-body-editor");
 }
-bodyIsEditorClassCheck();
+document.addEventListener("DOMContentLoaded", bodyIsEditorClassCheck);
 
 const originalReplaceState = history.replaceState;
 history.replaceState = function () {
