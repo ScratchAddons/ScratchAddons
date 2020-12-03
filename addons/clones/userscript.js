de1111b89a6f936ef10de584f321b88cdc135d9d
WorldLanguages <mundofinky@gmail.com>
2020-12-03 12:52:42 -0300

    Fix mouse-pos & clones overflowing small stage

--- addons/clones/userscript.js
+++ addons/clones/userscript.js
@@ -3,57 +3,72 @@ export default async function ({ addon, global, console, msg }) {
 
   const vm = addon.tab.traps.onceValues.vm;
 
+  vm.runtime.__cloneCounter = vm.runtime._cloneCounter;
+
+  Object.defineProperty(vm.runtime, "_cloneCounter", {
+    get: function () {
+      return this.__cloneCounter;
+    },
+    set: function (v) {
+      doCloneChecks(v);
+      return (this.__cloneCounter = v);
+    },
+  });
+
+  hideInSmallStageMode({ addon });
+
+  var countContainerContainer = document.createElement("div");
+  var countContainer = document.createElement("div");
+  var count = document.createElement("span");
+  var icon = document.createElement("img");
+
+  countContainerContainer.className = "clone-container-container";
+  countContainer.className = "clone-container";
+  icon.className = "clone-icon";
+
+  countContainerContainer.appendChild(icon);
+  countContainerContainer.appendChild(countContainer);
+  countContainer.appendChild(count);
+
+  function doCloneChecks(v) {
+    if (v <= 0) {
+      countContainerContainer.style.display = "none";
+    } else {
+      countContainerContainer.style.display = "";
+    }
+  
+    if (v == 300) {
+      count.style.color = "#ff6680";
+      icon.src = addon.self.dir + "/300cats.svg";
+    } else {
+      count.style.color = "";
+      icon.src = addon.self.dir + "/cat.svg";
+    }
+    count.innerText = msg("clones", { cloneCount: v });
+  }
+
   while (true) {
     let bar = await addon.tab.waitForElement("[class^='controls_controls-container']", { markAsSeen: true });
 
     if (addon.tab.editorMode === "editor") {
-      var countContainerContainer = document.createElement("div");
-      var countContainer = document.createElement("div");
-      var count = document.createElement("span");
-      var icon = document.createElement("img");
-
-      countContainerContainer.className = "clone-container-container";
-      countContainer.className = "clone-container";
-      icon.className = "clone-icon";
-
       doCloneChecks(vm.runtime._cloneCounter);
 
       icon.src = addon.self.dir + "/cat.svg";
 
-      countContainerContainer.appendChild(icon);
-      countContainerContainer.appendChild(countContainer);
-      countContainer.appendChild(count);
-
       bar.appendChild(countContainerContainer);
-
-      vm.runtime.__cloneCounter = vm.runtime._cloneCounter;
-
-      Object.defineProperty(vm.runtime, "_cloneCounter", {
-        get: function () {
-          return this.__cloneCounter;
-        },
-        set: function (v) {
-          doCloneChecks(v);
-          return (this.__cloneCounter = v);
-        },
-      });
     }
+  }
+}
 
-    function doCloneChecks(v) {
-      if (v <= 0) {
-        countContainerContainer.style.display = "none";
-      } else {
-        countContainerContainer.style.display = "";
-      }
+async function hideInSmallStageMode({ addon }) {
+  while(true) {
+    await addon.tab.waitForElement(".stage-header_stage-size-toggle-group_17LtK", { markAsSeen: true });
 
-      if (v == 300) {
-        count.style.color = "#ff6680";
-        icon.src = addon.self.dir + "/300cats.svg";
-      } else {
-        count.style.color = "";
-        icon.src = addon.self.dir + "/cat.svg";
-      }
-      count.innerText = msg("clones", { cloneCount: v });
-    }
+    document.querySelector(".stage-header_stage-button-first_y_CLF").addEventListener("click", () => {
+      document.querySelector(".clone-container-container").style.display = "none";
+    });
+    document.querySelector(".stage-header_stage-button-last_eeKZ4").addEventListener("click", () => {
+      document.querySelector(".clone-container-container").style.display = "";
+    });
   }
 }
--- addons/mouse-pos/userscript.js
+++ addons/mouse-pos/userscript.js
@@ -3,6 +3,38 @@ export default async function ({ addon, global, console }) {
 
   const vm = addon.tab.traps.onceValues.vm;
 
+  vm.runtime.ioDevices.mouse.__scratchX = vm.runtime.ioDevices.mouse._scratchX;
+  vm.runtime.ioDevices.mouse.__scratchY = vm.runtime.ioDevices.mouse._scratchY;
+
+  var x = vm.runtime.ioDevices.mouse.__scratchX ? vm.runtime.ioDevices.mouse.__scratchX : 0;
+  var y = vm.runtime.ioDevices.mouse.__scratchY ? vm.runtime.ioDevices.mouse.__scratchY : 0;
+
+  const showUpdatedValue = () => pos.setAttribute("data-content", `${x}, ${y}`);
+
+  Object.defineProperty(vm.runtime.ioDevices.mouse, "_scratchX", {
+    get: function () {
+      return this.__scratchX;
+    },
+    set: function (setx) {
+      x = setx;
+      showUpdatedValue();
+      return (this.__scratchX = setx);
+    },
+  });
+
+  Object.defineProperty(vm.runtime.ioDevices.mouse, "_scratchY", {
+    get: function () {
+      return this.__scratchY;
+    },
+    set: function (sety) {
+      y = sety;
+      showUpdatedValue();
+      return (this.__scratchY = sety);
+    },
+  });
+
+  hideInSmallStageMode({ addon });
+
   while (true) {
     let bar = await addon.tab.waitForElement(".controls_controls-container_2xinB", { markAsSeen: true });
 
@@ -20,34 +52,20 @@ export default async function ({ addon, global, console }) {
 
       bar.appendChild(posContainerContainer);
 
-      vm.runtime.ioDevices.mouse.__scratchX = vm.runtime.ioDevices.mouse._scratchX;
-      vm.runtime.ioDevices.mouse.__scratchY = vm.runtime.ioDevices.mouse._scratchY;
-
-      var x = vm.runtime.ioDevices.mouse.__scratchX ? vm.runtime.ioDevices.mouse.__scratchX : 0;
-      var y = vm.runtime.ioDevices.mouse.__scratchY ? vm.runtime.ioDevices.mouse.__scratchY : 0;
-
-      pos.setAttribute("data-content", `${x}, ${y}`);
-
-      Object.defineProperty(vm.runtime.ioDevices.mouse, "_scratchX", {
-        get: function () {
-          return this.__scratchX;
-        },
-        set: function (setx) {
-          x = setx;
-          return (this.__scratchX = setx);
-        },
-      });
-
-      Object.defineProperty(vm.runtime.ioDevices.mouse, "_scratchY", {
-        get: function () {
-          return this.__scratchY;
-        },
-        set: function (sety) {
-          y = sety;
-          pos.setAttribute("data-content", `${x}, ${y}`);
-          return (this.__scratchY = sety);
-        },
-      });
+      showUpdatedValue();
     }
+  }
+}
+
+async function hideInSmallStageMode({ addon }) {
+  while(true) {
+    await addon.tab.waitForElement(".stage-header_stage-size-toggle-group_17LtK", { markAsSeen: true });
+
+    document.querySelector(".stage-header_stage-button-first_y_CLF").addEventListener("click", () => {
+      document.querySelector(".pos-container-container").style.display = "none";
+    });
+    document.querySelector(".stage-header_stage-button-last_eeKZ4").addEventListener("click", () => {
+      document.querySelector(".pos-container-container").style.display = "";
+    });
   }
 }
