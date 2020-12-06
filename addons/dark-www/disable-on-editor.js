f23263b1184a6d88b0e9794409e12660aeca8e58
apple502j <apple502j@users.noreply.github.com>
2020-12-06 06:09:05 +0000

    style: format code

--- addons/dark-www/disable-on-editor.js
+++ addons/dark-www/disable-on-editor.js
@@ -1,27 +1,27 @@
 export default async ({ addon }) => {
   // When user clicks "See Project Page"
-  addon.tab.addEventListener("urlChange", e => {
+  addon.tab.addEventListener("urlChange", (e) => {
     if (!e.detail) return;
     const regex = /(?:editor|fullscreen)\/?/gi;
     const oldMatched = regex.test(e.detail.oldUrl || "");
     const newMatched = regex.test(e.detail.newUrl || "");
-    const darkWWWStyles = document.querySelectorAll("style[data-addon-id=\"dark-www\"]");
+    const darkWWWStyles = document.querySelectorAll('style[data-addon-id="dark-www"]');
     if (darkWWWStyles) {
       if (!oldMatched && newMatched) {
         // Media query is always false, effectively disabling the addon
-        darkWWWStyles.forEach(elem => elem.media = "(color) and (not color)");
+        darkWWWStyles.forEach((elem) => (elem.media = "(color) and (not color)"));
       } else if (!newMatched && oldMatched) {
-        darkWWWStyles.forEach(elem => elem.media = "");
+        darkWWWStyles.forEach((elem) => (elem.media = ""));
       }
     }
   });
-  
+
   // Initial page load
   const editorMode = addon.tab.editorMode;
   if (editorMode && editorMode !== "projectpage") {
-    const darkWWWStyles = document.querySelectorAll("style[data-addon-id=\"dark-www\"]");
+    const darkWWWStyles = document.querySelectorAll('style[data-addon-id="dark-www"]');
     if (darkWWWStyles) {
-      darkWWWStyles.forEach(elem => elem.media = "(color) and (not color)");
+      darkWWWStyles.forEach((elem) => (elem.media = "(color) and (not color)"));
     }
   }
-}
+};
