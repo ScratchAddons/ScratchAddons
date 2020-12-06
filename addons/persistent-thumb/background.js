2d1a3a81f1258cb4ad44b933b10860559a37a684
apple502j <apple502j@users.noreply.github.com>
2020-12-06 14:05:21 +0000

    style: format code

--- addons/persistent-thumb/background.js
+++ addons/persistent-thumb/background.js
@@ -4,16 +4,16 @@ export default async ({ addon, console }) => {
     if (!/\/editor/g.test(details.originUrl)) return;
     if (!details.requestHeaders) return;
     // If X-ScratchAddons-Allow-Persistent is set, ignore.
-    if (details.requestHeaders.find(header => header.name === "X-ScratchAddons-Allow-Persistent")) return;
+    if (details.requestHeaders.find((header) => header.name === "X-ScratchAddons-Allow-Persistent")) return;
     console.log("Blocked thumbnail from being saved.");
-    return ({ cancel: true });
+    return { cancel: true };
   };
-  
+
   chrome.webRequest.onBeforeSendHeaders.addListener(
     onThumbnailSet,
     {
       urls: ["https://scratch.mit.edu/internalapi/project/thumbnail/*/set/"],
-      types: ["xmlhttprequest"]
+      types: ["xmlhttprequest"],
     },
     ["blocking", "requestHeaders"]
   );
