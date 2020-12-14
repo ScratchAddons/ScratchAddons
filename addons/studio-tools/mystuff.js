d8d398bfd1fad2fac9a07f8c38cd4f48a47e5ee9
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-12-14 13:01:11 -0500

    resolve #1029

---
+++ addons/studio-tools/mystuff.js
@@ -0,0 +1,17 @@
+export default async function ({ addon, global, console, msg }) {
+  while (true) {
+    let item = await addon.tab.waitForElement(".media-stats", { markAsSeen: true });
+    if (window.location.hash == "#galleries" && item.childElementCount == 2) {
+      let leaveButton = item.appendChild(document.createElement("a"));
+      leaveButton.className = "unshare";
+      leaveButton.innerText = "Leave";
+      leaveButton.setAttribute("data-id", item.parentElement.querySelector(".title a").href.match(/[0-9]+/g))
+      leaveButton.addEventListener("click", async function (e) {
+        if (confirm(msg("leave-confirm"))) {
+          await addon.fetch(`https://scratch.mit.edu/site-api/users/curators-in/${leaveButton.getAttribute("data-id")}/remove/?usernames=${Scratch.INIT_DATA.LOGGED_IN_USER.model.username}`, { method: "PUT" });
+          window.location.reload()
+        }
+      })
+    }
+  }
+}
