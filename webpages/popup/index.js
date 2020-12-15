c50c1118349e8d85364740fab60a85cd192cf036
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-12-14 22:27:54 -0500

    Add vue to js

--- webpages/popup/index.js
+++ webpages/popup/index.js
@@ -1,12 +1,7 @@
 //theme switching
-
 const lightThemeLink = document.createElement("link");
 lightThemeLink.setAttribute("rel", "stylesheet");
 lightThemeLink.setAttribute("href", "light.css");
-
-document.getElementById("title-text").textContent = chrome.i18n.getMessage("extensionName");
-document.getElementById("settings-icon").title = chrome.i18n.getMessage("settings");
-
 chrome.storage.sync.get(["globalTheme"], function (r) {
   let rr = false; //true = light, false = dark
   if (r.globalTheme) rr = r.globalTheme;
@@ -14,81 +9,53 @@ chrome.storage.sync.get(["globalTheme"], function (r) {
     document.head.appendChild(lightThemeLink);
   }
 });
-document.getElementById("settings").onclick = () => {
-  chrome.runtime.openOptionsPage();
-  setTimeout(() => window.close(), 100);
-};
 
-chrome.runtime.sendMessage("getSettingsInfo", (res) => {
-  let popups = [];
-  let keys = Object.keys(res.addonsEnabled);
-  keys.forEach((addon, i) => {
-    if (res.addonsEnabled[addon]) {
-      let manifest = res.manifests.find((o) => o.addonId == addon).manifest;
-      if (manifest.popup) {
-        manifest.popup.addonId = addon;
-        popups.push(manifest.popup);
+const vue = new Vue({
+  el: "body",
+  data: {
+    popups: [],
+    currentPopup: null,
+  },
+  computed: {
+    version() {
+      return chrome.runtime.getManifest().version;
+    },
+  },
+  methods: {
+    msg(message, ...params) {
+      return chrome.i18n.getMessage(message, ...params);
+    },
+    closePopup() {
+      setTimeout(() => window.close(), 100);
+    },
+    openSettingsPage() {
+      chrome.runtime.openOptionsPage();
+      closePopup()
+    },
+    openChangelog() {
+      window.open("https://scratchaddons.com/changelog?versionname=" + chrome.runtime.getManifest().version_name);
+      closePopup()
+    },
+    setPopup(popup) {
+      if (vue.currentPopup !== popup) {
+        vue.currentPopup = popup;
+        document.getElementById("iframe").src = `../../popups/${popup.addonId}/popup.html`;
+        if (document.querySelector(".popup-name.sel")) document.querySelector(".popup-name.sel").classList.remove("sel");
+        document.querySelector(`.popup-name[data-id="${popup.addonId}"]`).classList.add("sel");
       }
-    }
-  });
-
-  let order = ["scratch-messaging", "cloud-games"];
-  order.forEach((addon, i) => {
-    popups = popups.sort((a, b) => order.indexOf(a.addonId) - order.indexOf(b.addonId));
-  });
-
-  let currentPopup = popups[0];
-  for (const popup of popups) {
-    const el = document.createElement("div");
-    el.classList.add("popup-name");
-    el.setAttribute("data-id", popup.addonId);
-    if (popup.icon) {
-      const icon = document.createElement("img");
-      icon.classList.add("popup-icon");
-      icon.setAttribute("src", popup.icon);
-      el.appendChild(icon);
-    }
-    const a = document.createElement("a");
-    a.classList.add("popup-title");
-    a.textContent = chrome.i18n.getMessage(popup.name);
-    el.appendChild(a);
-    if (popup.fullscreen) {
-      a.textContent += "\u00a0";
-      const popoutA = document.createElement("a");
-      popoutA.className = "popout";
-      popoutA.href = `../../popups/${popup.addonId}/popup.html`;
-      popoutA.target = "_blank";
-      popoutA.onclick = () => setTimeout(() => window.close(), 100);
-      const img = document.createElement("img");
-      img.src = "../../images/icons/popout.svg";
-      img.className = "popout-img";
-      img.title = chrome.i18n.getMessage("openInNewTab");
-      popoutA.appendChild(img);
-      el.appendChild(popoutA);
-    }
-
-    el.onclick = () => {
-      if (currentPopup !== popup) setPopup(popup);
-    };
-    document.getElementById("popup-chooser").appendChild(el);
-  }
-
-  setPopup(currentPopup);
-
-  function setPopup(popup) {
-    currentPopup = popup;
-    document.getElementById("iframe").src = `../../popups/${popup.addonId}/popup.html`;
-    if (document.querySelector(".popup-name.sel")) document.querySelector(".popup-name.sel").classList.remove("sel");
-    document.querySelector(`.popup-name[data-id="${popup.addonId}"]`).classList.add("sel");
-  }
+    },
+  },
 });
 
-var version = document.getElementById("version");
-version.innerText = "v" + chrome.runtime.getManifest().version;
-version.title = chrome.i18n.getMessage("changelog");
-version.onclick = () => {
-  window.open("https://scratchaddons.com/changelog?versionname=" + chrome.runtime.getManifest().version_name);
-  setTimeout(() => window.close(), 100);
-};
+chrome.runtime.sendMessage("getSettingsInfo", res => {
+  let order = ["scratch-messaging", "cloud-games"]
+  let keys = Object.keys(res.addonsEnabled).filter(k => res.addonsEnabled[k] && res.manifests.find(o => o.addonId == k).manifest.popup);
+  keys.forEach((addon, i) => {
+    res.manifests.find(o => o.addonId == addon).manifest.popup.addonId = addon;
+    vue.popups.push(res.manifests.find(o => o.addonId == addon).manifest.popup);
+    vue.popups = vue.popups.sort((a, b) => order.indexOf(a.addonId) - order.indexOf(b.addonId))
+  });
+  setTimeout(() => vue.setPopup(vue.popups[0]), 0);
+});
 
 chrome.runtime.sendMessage("checkPermissions");
