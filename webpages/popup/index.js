d96ee3ce214e03b83947b78aa3bf19eecb09dfba
TheColaber <TheColaber@users.noreply.github.com>
2020-12-15 03:28:34 +0000

    style: format code

--- webpages/popup/index.js
+++ webpages/popup/index.js
@@ -30,30 +30,33 @@ const vue = new Vue({
     },
     openSettingsPage() {
       chrome.runtime.openOptionsPage();
-      closePopup()
+      closePopup();
     },
     openChangelog() {
       window.open("https://scratchaddons.com/changelog?versionname=" + chrome.runtime.getManifest().version_name);
-      closePopup()
+      closePopup();
     },
     setPopup(popup) {
       if (vue.currentPopup !== popup) {
         vue.currentPopup = popup;
         document.getElementById("iframe").src = `../../popups/${popup.addonId}/popup.html`;
-        if (document.querySelector(".popup-name.sel")) document.querySelector(".popup-name.sel").classList.remove("sel");
+        if (document.querySelector(".popup-name.sel"))
+          document.querySelector(".popup-name.sel").classList.remove("sel");
         document.querySelector(`.popup-name[data-id="${popup.addonId}"]`).classList.add("sel");
       }
     },
   },
 });
 
-chrome.runtime.sendMessage("getSettingsInfo", res => {
-  let order = ["scratch-messaging", "cloud-games"]
-  let keys = Object.keys(res.addonsEnabled).filter(k => res.addonsEnabled[k] && res.manifests.find(o => o.addonId == k).manifest.popup);
+chrome.runtime.sendMessage("getSettingsInfo", (res) => {
+  let order = ["scratch-messaging", "cloud-games"];
+  let keys = Object.keys(res.addonsEnabled).filter(
+    (k) => res.addonsEnabled[k] && res.manifests.find((o) => o.addonId == k).manifest.popup
+  );
   keys.forEach((addon, i) => {
-    res.manifests.find(o => o.addonId == addon).manifest.popup.addonId = addon;
-    vue.popups.push(res.manifests.find(o => o.addonId == addon).manifest.popup);
-    vue.popups = vue.popups.sort((a, b) => order.indexOf(a.addonId) - order.indexOf(b.addonId))
+    res.manifests.find((o) => o.addonId == addon).manifest.popup.addonId = addon;
+    vue.popups.push(res.manifests.find((o) => o.addonId == addon).manifest.popup);
+    vue.popups = vue.popups.sort((a, b) => order.indexOf(a.addonId) - order.indexOf(b.addonId));
   });
   setTimeout(() => vue.setPopup(vue.popups[0]), 0);
 });
