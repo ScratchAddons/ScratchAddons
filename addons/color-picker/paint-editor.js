ec0fbefabd2844d41140f4808d4003fbb6cf0d93
apple502j <33279053+apple502j@users.noreply.github.com>
2020-12-16 19:56:55 +0900

    Add default value for hex input

--- addons/color-picker/paint-editor.js
+++ addons/color-picker/paint-editor.js
@@ -2,10 +2,35 @@ import {normalizeHex, getHexRegex} from "../../libraries/normalize-color.js";
 
 export default async ({addon, console, msg}) => {
   let elem;
+  let prevEventHandler;
+  await addon.tab.loadScript(addon.self.lib + "/tinycolor-min.js");
   while (true) {
     elem = await addon.tab.waitForElement("div[class*=\"color-picker_swatch-row\"]", {markAsSeen: true});
     addon.tab.redux.initialize();
+    if (addon.tab.redux && typeof prevEventHandler === "function") {
+      addon.tab.redux.removeEventListener("statechanged", prevEventHandler);
+      prevEventHandler = null;
+    }
     if (addon.tab.editorMode !== "editor") continue;
+    const getColor = () => {
+      let fillOrStroke;
+      const state = addon.tab.redux.state;
+      if (state.scratchPaint.modals.fillColor) {
+        fillOrStroke = "fill";
+      } else if (state.scratchPaint.modals.strokeColor) {
+        fillOrStroke = "stroke";
+      } else {
+        fillOrStroke = "ihadastroke";
+        return;
+      }
+      const colorType = state.scratchPaint.fillMode.colorIndex;
+      const primaryOrSecondary = ["primary", "secondary"][colorType];
+      const color = state.scratchPaint.color[`${fillOrStroke}Color`][primaryOrSecondary];
+      if (color === null || color === "scratch-paint/style-path/mixed") return;
+      // This value can be arbitrary - it can be HEX, RGB, etc.
+      // Use tinycolor to convert them.
+      return tinycolor(color).toHexString();
+    };
     const setColor = hex => {
       hex = normalizeHex(hex);
       if (!addon.tab.redux.state || !addon.tab.redux.state.scratchPaint) return;
@@ -26,18 +51,21 @@ export default async ({addon, console, msg}) => {
       addon.tab.redux.addEventListener("statechanged", onEyeDropperOpened);
       elem.children[1].children[0].click();
     };
+    const defaultColor = getColor();
     const saColorPicker = Object.assign(document.createElement("div"), {
       className: "sa-color-picker sa-color-picker-paint"
     });
     const saColorPickerColor = Object.assign(document.createElement("input"), {
       className: "sa-color-picker-color sa-color-picker-paint-color",
-      type: "color"
+      type: "color",
+      value: defaultColor || "#000000"
     });
     const saColorPickerText = Object.assign(document.createElement("input"), {
       className: "sa-color-picker-text sa-color-picker-paint-text",
       type: "text",
       pattern: "^#?([0-9a-fA-F]{3}){1,2}$",
-      placeholder: msg("hex")
+      placeholder: msg("hex"),
+      value: defaultColor || ""
     });
     saColorPickerColor.addEventListener("change", () => {
       setColor(saColorPickerText.value = saColorPickerColor.value);
@@ -46,7 +74,18 @@ export default async ({addon, console, msg}) => {
       const val = saColorPickerText.value;
       if (!getHexRegex().test(val)) return;
       setColor(saColorPickerColor.value = normalizeHex(val));
-    })
+    });
+    prevEventHandler = e => {
+      const type = e.detail.action.type;
+      if (type === "scratch-paint/color-index/CHANGE_COLOR_INDEX") {
+        setTimeout(() => {
+          const color = getColor();
+          saColorPickerColor.value = color || "#000000";
+          saColorPickerText.value = color || "";
+        }, 100);
+      }
+    }
+    addon.tab.redux.addEventListener("statechanged", prevEventHandler);
     saColorPicker.appendChild(saColorPickerColor);
     saColorPicker.appendChild(saColorPickerText);
     elem.parentElement.insertBefore(saColorPicker, elem);
