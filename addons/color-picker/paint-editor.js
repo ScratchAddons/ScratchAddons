1495ead8b270eda8b1f707f7a090151859fad18f
apple502j <33279053+apple502j@users.noreply.github.com>
2020-12-16 18:42:08 +0900

    Don't mess with view bounds

--- addons/color-picker/paint-editor.js
+++ addons/color-picker/paint-editor.js
@@ -4,26 +4,22 @@ export default async ({addon, console, msg}) => {
   let elem;
   while (true) {
     elem = await addon.tab.waitForElement("div[class*=\"color-picker_swatch-row\"]", {markAsSeen: true});
+    addon.tab.redux.initialize();
     if (addon.tab.editorMode !== "editor") continue;
     const setColor = hex => {
       hex = normalizeHex(hex);
       if (!addon.tab.redux.state || !addon.tab.redux.state.scratchPaint) return;
-      addon.tab.redux.initialize();
       // The only way to reliably set color is to invoke eye dropper via click()
       // then faking that the eye dropper reported the value.
       const onEyeDropperOpened = e => {
         if (e.detail.action.type !== "scratch-paint/eye-dropper/ACTIVATE_COLOR_PICKER") return;
         addon.tab.redux.removeEventListener("statechanged", onEyeDropperOpened);
         setTimeout(() => {
+          const previousTool = addon.tab.redux.state.scratchPaint.color.eyeDropper.previousTool;
+          if (previousTool) previousTool.activate();
           addon.tab.redux.state.scratchPaint.color.eyeDropper.callback(hex);
-          // Hack to get a clone of view bounds object w/o messing with original one
-          const oldViewBounds = addon.tab.redux.state.scratchPaint.viewBounds;
-          const newViewBounds = Object.assign({}, oldViewBounds);
-          Object.setPrototypeOf(newViewBounds, oldViewBounds.constructor.prototype);
-          // This resets eyedropper
           addon.tab.redux.dispatch({
-            type: "scratch-paint/view/UPDATE_VIEW_BOUNDS",
-            viewBounds: newViewBounds
+            type: "scratch-paint/eye-dropper/DEACTIVATE_COLOR_PICKER"
           });
         }, 50);
       };
