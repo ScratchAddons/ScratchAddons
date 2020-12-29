3765138f45154918e5398089a7baf9fceda8db87
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-12-29 17:25:57 -0500

    upload files

--- addons/60fps/userscript.js
+++ addons/60fps/userscript.js
@@ -1,39 +1,44 @@
 export default async function ({ addon, global, console }) {
-  console.log("60fps enabled");
-
-  const vm = addon.tab.traps.onceValues.vm;
-
-  let altPressesCount = 0;
-  let altPressedRecently = false;
-  window.addEventListener("keydown", (event) => {
-    if (event.key === "Alt") {
-      altPressesCount++;
-      const pressCount = altPressesCount;
-      altPressedRecently = true;
-      setTimeout(() => {
-        if (pressCount === altPressesCount) altPressedRecently = false;
-      }, 2500);
-    }
-  });
-
+  let gloabal_fps = 30;
   while (true) {
+    const vm = addon.tab.traps.onceValues.vm;
     let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", { markAsSeen: true });
-    let mode = true;
-
+    let mode = false;
+    const setFPS = fps => {
+      gloabal_fps = fps;
+      clearInterval(vm.runtime._steppingInterval);
+      vm.runtime._steppingInterval = null;
+      vm.runtime.start();
+    }
     button.addEventListener("click", (e) => {
       if (e.altKey) {
         mode = !mode;
-        vm.setCompatibilityMode(mode);
-        button.style.filter = mode ? "" : "hue-rotate(90deg)";
+        if (mode) setFPS(addon.settings.get("framerate"));
+        else setFPS(30);
+        button.style.filter = mode ? "hue-rotate(90deg)" : "";
       }
     });
-    button.addEventListener("contextmenu", (e) => {
-      if (altPressedRecently) {
-        e.preventDefault();
-        mode = !mode;
-        vm.setCompatibilityMode(mode);
-        button.style.filter = mode ? "" : "hue-rotate(90deg)";
+    const checkFPS = (fps) => {
+      let valid = 30;
+      // if framerate is a number and is not negitive or 0
+      if (Number(fps) == Number(fps) && fps > 0) {
+        if (vm.runtime.compatibilityMode) valid = (1000 / fps);
+      }
+      return valid;
+    }
+    addon.settings.addEventListener("change", function() {
+      if (vm.runtime._steppingInterval) {
+        setFPS(addon.settings.get("framerate"));
       }
     });
+    vm.runtime.start = function() {
+      if (this._steppingInterval) return;
+      let interval = checkFPS(gloabal_fps);
+      this.currentStepTime = interval;
+      this._steppingInterval = setInterval(() => {
+        this._step();
+      }, interval);
+      this.emit("RUNTIME_STARTED");
+    }
   }
 }
