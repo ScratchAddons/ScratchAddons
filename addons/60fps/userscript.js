6d08995637d10b995c4c733cbc395058034c2e7f
TheColaber <TheColaber@users.noreply.github.com>
2020-12-29 22:26:30 +0000

    style: format code

--- addons/60fps/userscript.js
+++ addons/60fps/userscript.js
@@ -4,12 +4,12 @@ export default async function ({ addon, global, console }) {
     const vm = addon.tab.traps.onceValues.vm;
     let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", { markAsSeen: true });
     let mode = false;
-    const setFPS = fps => {
+    const setFPS = (fps) => {
       gloabal_fps = fps;
       clearInterval(vm.runtime._steppingInterval);
       vm.runtime._steppingInterval = null;
       vm.runtime.start();
-    }
+    };
     button.addEventListener("click", (e) => {
       if (e.altKey) {
         mode = !mode;
@@ -22,16 +22,16 @@ export default async function ({ addon, global, console }) {
       let valid = 30;
       // if framerate is a number and is not negitive or 0
       if (Number(fps) == Number(fps) && fps > 0) {
-        if (vm.runtime.compatibilityMode) valid = (1000 / fps);
+        if (vm.runtime.compatibilityMode) valid = 1000 / fps;
       }
       return valid;
-    }
-    addon.settings.addEventListener("change", function() {
+    };
+    addon.settings.addEventListener("change", function () {
       if (vm.runtime._steppingInterval) {
         setFPS(addon.settings.get("framerate"));
       }
     });
-    vm.runtime.start = function() {
+    vm.runtime.start = function () {
       if (this._steppingInterval) return;
       let interval = checkFPS(gloabal_fps);
       this.currentStepTime = interval;
@@ -39,6 +39,6 @@ export default async function ({ addon, global, console }) {
         this._step();
       }, interval);
       this.emit("RUNTIME_STARTED");
-    }
+    };
   }
 }
