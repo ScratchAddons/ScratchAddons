809119b49259e9aedebaf8b8a3fae02a76923a54
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-12-28 23:09:31 -0500

    upload

--- addons/project-info/userscript.js
+++ addons/project-info/userscript.js
@@ -1,30 +1,44 @@
 export default async function ({ addon, console, msg }) {
-  let projectInfo;
-  while (true) {
-    const buttons = await addon.tab.waitForElement(".preview .project-buttons", { markAsSeen: true });
-    const container = document.createElement("div");
-    container.className = "sa-project-info";
-    buttons.insertBefore(container, buttons.firstChild);
-    if (!projectInfo) {
-      const projectId = location.pathname.split("/")[2];
-      const projectData = await (await fetch("https://projects.scratch.mit.edu/" + projectId)).json();
-      if ("targets" in projectData) {
-        let scriptCount = 0;
-        for (let target of projectData.targets) {
-          scriptCount += Object.values(target.blocks).filter((block) => block.topLevel).length;
-        }
-        projectInfo = {
-          spriteCount: projectData.targets.length - 1, // see scratch-www/src/lib/project-info.js, line 10
-          scriptCount: scriptCount,
-        };
-      } else if ("info" in projectData) {
-        projectInfo = projectData.info;
-      } else {
-        projectInfo = { spriteCount: "?", scriptCount: "?" };
-      }
-    }
-    container.appendChild(document.createTextNode(msg("sprite", { num: projectInfo.spriteCount })));
-    container.appendChild(document.createElement("br"));
-    container.appendChild(document.createTextNode(msg("script", { num: projectInfo.scriptCount })));
+  const vm = addon.tab.traps.onceValues.vm;
+  
+  const getBlockCount = async () => {
+    let scriptCount = 0;
+    let sprites = new Set(vm.runtime.targets.map(i => i.sprite.blocks._blocks));
+    sprites.forEach((sprite, i) => {
+      scriptCount += Object.values(sprite).filter(o => !o.shadow).length;
+    });
+    return {
+      scriptCount,
+      spriteCount: sprites.size - 1
+    };
   }
+  vm.runtime.on("PROJECT_LOADED", async () => {
+    (async () => {
+      if (addon.settings.get("editorCount")) {
+        while (true) {
+          const topBar = await addon.tab.waitForElement("[class^='menu-bar_main-menu']", { markAsSeen: true });
+          let display = topBar.appendChild(document.createElement("span"));
+          display.innerText = msg("blocks", { num: (await getBlockCount()).scriptCount });
+          let debounce;
+          vm.on('PROJECT_CHANGED', async () => {
+            clearInterval(debounce)
+            debounce = setTimeout(async () => {
+              display.innerText = msg("blocks", { num: (await getBlockCount()).scriptCount });
+            }, 1000);
+          });
+        }
+      }
+    })();
+
+    while (true) {
+      const buttons = await addon.tab.waitForElement(".preview .project-buttons", { markAsSeen: true });
+      const container = document.createElement("div");
+      container.className = "sa-project-info";
+      buttons.insertBefore(container, buttons.firstChild);
+      let projectInfo = await getBlockCount();
+      container.appendChild(document.createTextNode(msg("sprite", { num: projectInfo.spriteCount })));
+      container.appendChild(document.createElement("br"));
+      container.appendChild(document.createTextNode(msg("script", { num: projectInfo.scriptCount })));
+    }
+  });
 }
