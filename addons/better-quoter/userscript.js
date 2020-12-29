53ec4650fc2a98ae9b0c7597cf3988301c99b923
TheColaber <TheColaber@users.noreply.github.com>
2020-12-29 17:53:09 +0000

    style: format code

--- addons/better-quoter/userscript.js
+++ addons/better-quoter/userscript.js
@@ -3,9 +3,12 @@ export default async function ({ addon, global, console }) {
   while (true) {
     let quoteButton = await addon.tab.waitForElement(".postquote a", { markAsSeen: true });
     quoteButton.setAttribute("onclick", "return false");
-    quoteButton.addEventListener("mouseup", e => {
-      let blockpost = e.path.find(e => e.classList.contains("blockpost"));
-      if (window.getSelection().toString().length) textarea.value += `[quote=${blockpost.querySelector(".black.username").innerText}]${window.getSelection().toString()}[/quote]`;
+    quoteButton.addEventListener("mouseup", (e) => {
+      let blockpost = e.path.find((e) => e.classList.contains("blockpost"));
+      if (window.getSelection().toString().length)
+        textarea.value += `[quote=${
+          blockpost.querySelector(".black.username").innerText
+        }]${window.getSelection().toString()}[/quote]`;
       else copy_paste(blockpost.id);
     });
   }
