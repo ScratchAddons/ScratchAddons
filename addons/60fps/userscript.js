22765c62a1c339368b03e5752d516ff91cbab92f
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-12-31 14:45:50 -0500

    cb support

--- addons/60fps/userscript.js
+++ addons/60fps/userscript.js
@@ -1,23 +1,39 @@
 export default async function ({ addon, global, console }) {
   let gloabal_fps = 30;
+  const vm = addon.tab.traps.onceValues.vm;
+  let altPressesCount = 0;
+  let altPressedRecently = false;
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "Alt") {
+      altPressesCount++;
+      const pressCount = altPressesCount;
+      altPressedRecently = true;
+      setTimeout(() => {
+        if (pressCount === altPressesCount) altPressedRecently = false;
+      }, 2500);
+    }
+  });
   while (true) {
-    const vm = addon.tab.traps.onceValues.vm;
     let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", { markAsSeen: true });
     let mode = false;
+    const flagListener = (e) => {
+      if (altPressedRecently) {
+        e.preventDefault();
+        mode = !mode;
+        if (mode) setFPS(addon.settings.get("framerate"));
+        else setFPS(30);
+        button.style.filter = mode ? "hue-rotate(90deg)" : "";
+      }
+    }
+    button.addEventListener("click", (e) => flagListener(e));
+    button.addEventListener("contextmenu", (e) => flagListener(e));
+
     const setFPS = (fps) => {
       gloabal_fps = fps;
       clearInterval(vm.runtime._steppingInterval);
       vm.runtime._steppingInterval = null;
       vm.runtime.start();
     };
-    button.addEventListener("click", (e) => {
-      if (e.altKey) {
-        mode = !mode;
-        if (mode) setFPS(addon.settings.get("framerate"));
-        else setFPS(30);
-        button.style.filter = mode ? "hue-rotate(90deg)" : "";
-      }
-    });
     const checkFPS = (fps) => {
       let valid = 30;
       // if framerate is a number and is not negitive or 0
