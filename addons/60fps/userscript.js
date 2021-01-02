b117433e0b7ee359c2f7c69954f59818b65c5761
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-01-02 10:11:16 -0500

    Update userscript.js

--- addons/60fps/userscript.js
+++ addons/60fps/userscript.js
@@ -34,14 +34,6 @@ export default async function ({ addon, global, console }) {
       vm.runtime._steppingInterval = null;
       vm.runtime.start();
     };
-    const checkFPS = (fps) => {
-      let valid = 30;
-      // if framerate is a number and is not negitive or 0
-      if (Number(fps) == Number(fps) && fps > 0) {
-        if (vm.runtime.compatibilityMode) valid = 1000 / fps;
-      }
-      return valid;
-    };
     addon.settings.addEventListener("change", function () {
       if (vm.runtime._steppingInterval) {
         setFPS(addon.settings.get("framerate"));
@@ -49,7 +41,7 @@ export default async function ({ addon, global, console }) {
     });
     vm.runtime.start = function () {
       if (this._steppingInterval) return;
-      let interval = checkFPS(gloabal_fps);
+      let interval = 1000 / gloabal_fps;
       this.currentStepTime = interval;
       this._steppingInterval = setInterval(() => {
         this._step();
