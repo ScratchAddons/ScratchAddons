6bd41d4888a1d6936fcd90327c094d0e8e85b917
mxmou <mxmou@users.noreply.github.com>
2021-01-02 16:21:54 +0000

    style: format code

--- addons/dark-www/experimental_forums_preview.js
+++ addons/dark-www/experimental_forums_preview.js
@@ -5,20 +5,20 @@ function createStyle(url) {
   return style;
 }
 
-export default async function({ addon, console }) {
+export default async function ({ addon, console }) {
   const preview = await addon.tab.waitForElement(".markItUpPreviewFrame");
-  const observer = new MutationObserver(function(records, observer) {
+  const observer = new MutationObserver(function (records, observer) {
     for (let record of records) {
       if (record.type == "childList") {
         for (let node of record.addedNodes) {
           if (node.tagName == "LINK" && node.href.endsWith("djangobb_forum/css/pygments.css")) {
             preview.contentDocument.head.appendChild(createStyle(addon.self.dir + "/experimental_scratchr2.css"));
             preview.contentDocument.head.appendChild(createStyle(addon.self.dir + "/experimental_forums.css"));
-			preview.contentDocument.head.appendChild(createStyle(addon.self.dir + "/pygments.css"));
+            preview.contentDocument.head.appendChild(createStyle(addon.self.dir + "/pygments.css"));
           }
         }
       }
     }
   });
-  observer.observe(preview.contentDocument, {subtree: true, childList: true});
+  observer.observe(preview.contentDocument, { subtree: true, childList: true });
 }
