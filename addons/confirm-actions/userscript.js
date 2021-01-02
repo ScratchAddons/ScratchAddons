3f1c395def88bd6b9e97edc2463dc44e992c1c45
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-01-02 13:54:31 -0500

    Update userscript.js

--- addons/confirm-actions/userscript.js
+++ addons/confirm-actions/userscript.js
@@ -1,7 +1,7 @@
 export default async function ({ addon, console, msg }) {
   if (addon.settings.get("projectsharing"))
     actionAlert("[class*='share-button_share-button'], .banner-button", msg("share"));
-  if (addon.settings.get("followinguser")) actionAlert(".follow-button", msg("follow"));
+  if (addon.settings.get("followinguser")) actionAlert("div.follow-button", msg("follow"));
   if (addon.settings.get("joiningstudio")) actionAlert("a.accept", msg("joinstudio"));
   async function actionAlert(queryButton, res) {
     while (true) {
