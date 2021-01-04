9bf2c336f133f9e9270d4c0866d1298b6ba799f0
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-04 11:28:12 -0500

    Update semicolon.js

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -1,12 +1,9 @@
 export default async function ({ addon, global, console }) {
-  if (addon.tab.editorMode === "editor") {
-    console.log("In editor!");
-  } else {
     var semicolon = document.createElement("p");
     semicolon.textContent = ";";
     semicolon.classList.add("semicolon");
     document.body.appendChild(semicolon);
-  }
+  
   addon.tab.addEventListener("urlChange", () => {
     if (addon.tab.editorMode === "editor" || window.location.href.includes("fullscreen")) {
       document.getElementsByClassName("semicolon")[0].style.display = "none";
