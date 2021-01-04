64d2a513000259dbee3fdca3fe1f8339f2b938f6
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-04 08:17:36 -0500

    Update semicolon.js

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -7,4 +7,11 @@ export default async function ({ addon, global, console }) {
     semicolon.classList.add("semicolon");
     document.body.appendChild(semicolon);
   }
+  addon.tab.addEventListener("urlChange", () => {
+    if (addon.tab.editorMode === "editor" || window.location.href.includes('fullscreen') {
+      document.getElementsByClassName("semicolon")[0].style.display = "none";
+    } else {
+      document.getElementsByClassName("semicolon")[0].style.display = "block";
+    }
+  })
 }
