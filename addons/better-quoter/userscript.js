64e89dba758769fee119c7374c822c0e63229111
apple502j <apple502j@users.noreply.github.com>
2021-01-04 16:25:45 +0000

    style: format code

--- addons/better-quoter/userscript.js
+++ addons/better-quoter/userscript.js
@@ -14,9 +14,7 @@ export default async function ({ addon, global, console }) {
         selection.focusNode &&
         blockpost.contains(selection.focusNode)
       )
-        textarea.value += `[quote=${
-          blockpost.querySelector(".black.username").innerText
-        }]${selectionStr}[/quote]`;
+        textarea.value += `[quote=${blockpost.querySelector(".black.username").innerText}]${selectionStr}[/quote]`;
       else copy_paste(blockpost.id);
       textarea.scrollIntoView(false);
       textarea.focus();
