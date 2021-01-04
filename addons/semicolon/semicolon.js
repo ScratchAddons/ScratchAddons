93d94cdc426eba30328008fb362c47be7f4e8f82
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-01-04 13:12:14 +0000

    style: format code

--- addons/semicolon/semicolon.js
+++ addons/semicolon/semicolon.js
@@ -1,11 +1,10 @@
 export default async function ({ addon, global, console }) {
-    if (addon.tab.editorMode === "editor") {
-      console.log("In editor!");
-    } else {
-      var semicolon = document.createElement("p");
-      semicolon.textContent = ";";
-      semicolon.classList.add("semicolon");
-      document.body.appendChild(semicolon);
-    }
-  
+  if (addon.tab.editorMode === "editor") {
+    console.log("In editor!");
+  } else {
+    var semicolon = document.createElement("p");
+    semicolon.textContent = ";";
+    semicolon.classList.add("semicolon");
+    document.body.appendChild(semicolon);
+  }
 }
