00242537f18af5b0fb6a7d891e8b1726676b8f77
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-01-05 16:05:03 -0500

    cool init commit

---
+++ addons/mute-project/userscript.js
@@ -0,0 +1,16 @@
+export default async function ({ addon, global, console }) {
+  const vm = addon.tab.traps.onceValues.vm;
+  while (true) {
+    let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", { markAsSeen: true });
+    let mode = false;
+    button.addEventListener("click", (e) => {
+      if (e.ctrlKey) {
+        e.cancelBubble = true;
+        e.preventDefault();
+        mode = !mode;
+        if (mode) vm.editingTarget.blocks.runtime.audioEngine.audioContext.suspend()
+        else vm.editingTarget.blocks.runtime.audioEngine.audioContext.resume()
+      }
+    });
+  }
+}
