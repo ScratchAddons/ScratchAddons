b3cb041a5750144cebf1c5acac00b1d289a02d51
pufferfish101007 <pufferfish101007@users.noreply.github.com>
2021-01-07 18:14:19 +0000

    style: format code

--- addons/better-quoter/userscript.js
+++ addons/better-quoter/userscript.js
@@ -142,7 +142,9 @@ export default async function ({ addon, global, console }) {
         selection.focusNode &&
         blockpost.contains(selection.focusNode)
       )
-        textarea.value += `[quote=${blockpost.querySelector(".black.username").innerText}]${getSelectionBBCode()}[/quote]`;
+        textarea.value += `[quote=${
+          blockpost.querySelector(".black.username").innerText
+        }]${getSelectionBBCode()}[/quote]`;
       else copy_paste(blockpost.id);
       textarea.scrollIntoView(false);
       textarea.focus();
