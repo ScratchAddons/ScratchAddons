17b03b32440c9f363ecb2f0a51e59a058f15aa9c
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-01-12 18:41:29 -0500

    fix cat block issue

--- addons/editor-stepping/userscript.js
+++ addons/editor-stepping/userscript.js
@@ -16,7 +16,7 @@ export default async function ({ addon, global, console }) {
   function addInterval() {
     const interval = setInterval(() => {
       Array.prototype.forEach.call(document.querySelectorAll("path[style*='outline' i]"), (e) =>
-        e.removeAttribute("style")
+        e.style.outline = ""
       );
       virtualMachine.runtime.threads.forEach((thread) => {
         thread.stack.forEach((e) => {
