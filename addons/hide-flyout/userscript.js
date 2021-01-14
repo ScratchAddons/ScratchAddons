935494a8998f7479c7e1b7cb9e3ab84a19a8a536
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-01-14 16:33:26 -0500

    new update
    33% done

--- addons/hide-flyout/userscript.js
+++ addons/hide-flyout/userscript.js
@@ -1,27 +1,34 @@
 export default async function ({ addon, global, console }) {
   while (true) {
     let flyOut = await addon.tab.waitForElement(".blocklyFlyout", { markAsSeen: true });
-    let state = true;
-    function mouseMove(e) {
-      let sidePos = document.querySelector(".blocklyToolboxDiv").getBoundingClientRect().x;
-      let scrollBar = document.querySelector(".blocklyFlyoutScrollbar");
-      if (e.clientX - sidePos < 310) {
-        if (!state) {
-          flyOut.classList.remove("sa-flyoutClose");
-          flyOut.style.animation = "openFlyout 1s 1";
-          scrollBar.classList.remove("sa-flyoutClose");
-          scrollBar.style.animation = "openScrollbar 1s 1";
-        }
-      } else {
-        flyOut.classList.add("sa-flyoutClose");
-        flyOut.style.animation = "closeFlyout 1s 1";
-        scrollBar.classList.add("sa-flyoutClose");
-        scrollBar.style.animation = "closeScrollbar 1s 1";
+    let scrollBar = document.querySelector(".blocklyFlyoutScrollbar");
 
-      }
-      state = e.clientX - sidePos < 310;
+    // Placeholder Div
+    let placeHolderDiv = document.body.appendChild(document.createElement("div"));
+    placeHolderDiv.className = "sa-flyout-placeHolder";
+    placeHolderDiv.style.height = `${flyOut.getBoundingClientRect().height}px`;
+    placeHolderDiv.style.width = `${flyOut.getBoundingClientRect().width}px`;
+    placeHolderDiv.style.left = `${flyOut.getBoundingClientRect().left}px`;
+    placeHolderDiv.style.top = `${flyOut.getBoundingClientRect().top}px`;
+
+    function onmouseenter() {
+      console.log("enter");
+      flyOut.classList.remove("sa-flyoutClose");
+      flyOut.style.animation = "openFlyout 0.5s 1";
+      scrollBar.classList.remove("sa-flyoutClose");
+      scrollBar.style.animation = "openScrollbar 0.5s 1";
     }
-    document.removeEventListener("mousemove", mouseMove);
-    document.addEventListener("mousemove", mouseMove)
+    function onmouseleave(e) {
+      console.log("leave");
+      // If we go behind the flyout, let's return
+      if (e && e.clientX <= flyOut.getBoundingClientRect().left) return;
+      flyOut.classList.add("sa-flyoutClose");
+      flyOut.style.animation = "closeFlyout 0.5s 1";
+      scrollBar.classList.add("sa-flyoutClose");
+      scrollBar.style.animation = "closeScrollbar 0.5s 1";
+    }
+    onmouseleave(); // close flyout on load
+    placeHolderDiv.onmouseenter = onmouseenter;
+    flyOut.onmouseleave = onmouseleave; // notice this is to flyout
   }
 }
