401e0eff728d14245eec42d94237033841db697a
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-01-14 17:57:12 -0500

    Finally Done!

--- addons/hide-flyout/userscript.js
+++ addons/hide-flyout/userscript.js
@@ -10,28 +10,37 @@ export default async function ({ addon, global, console }) {
     placeHolderDiv.style.width = `${flyOut.getBoundingClientRect().width - 5}px`;
     placeHolderDiv.style.left = `${flyOut.getBoundingClientRect().left}px`;
     placeHolderDiv.style.top = `${flyOut.getBoundingClientRect().top}px`;
+    let flyoutLock = false;
+    let lockDisplay = document.body.appendChild(document.createElement("img"));
+    lockDisplay.src = addon.self.dir + "/unlock.svg";
+    lockDisplay.style.top = `${flyOut.getBoundingClientRect().top}px`;
+    lockDisplay.style.left = `${flyOut.getBoundingClientRect().right - 32}px`;
+    lockDisplay.className = "sa-lock-image"
+    lockDisplay.onclick = function () {
+      flyoutLock = !flyoutLock;
+      lockDisplay.src = addon.self.dir + `/${flyoutLock ? "" : "un"}lock.svg`;
+    }
 
     function onmouseenter() {
-      console.log("enter");
       flyOut.classList.remove("sa-flyoutClose");
-      flyOut.style.animation = "openFlyout 0.5s 1";
+      flyOut.style.animation = `openFlyout ${addon.settings.get("speed")}s 1`;
       scrollBar.classList.remove("sa-flyoutClose");
-      scrollBar.style.animation = "openScrollbar 0.5s 1";
-      placeHolderDiv.onmouseenter = "";
-      scrollBar.onmouseleave = onmouseleave; // notice this is to flyout
+      scrollBar.style.animation = `openScrollbar ${addon.settings.get("speed")}s 1`;
+      lockDisplay.classList.remove("sa-flyoutClose");
+      lockDisplay.style.animation = `openLock ${addon.settings.get("speed")}s 1`;
     }
     function onmouseleave(e) {
-      // If we go behind the flyout, let's return
-      if (e && (e.clientX <= scrollBar.getBoundingClientRect().left)) return;
-      console.log("leave");
+      // If we go behind the flyout or the user has locked it, let's return
+      if ((e && e.clientX <= scrollBar.getBoundingClientRect().left) || flyoutLock) return;
       flyOut.classList.add("sa-flyoutClose");
-      flyOut.style.animation = "closeFlyout 0.5s 1";
+      flyOut.style.animation = `closeFlyout ${addon.settings.get("speed")}s 1`;
       scrollBar.classList.add("sa-flyoutClose");
-      scrollBar.style.animation = "closeScrollbar 0.5s 1";
-      placeHolderDiv.onmouseenter = onmouseenter;
-      scrollBar.onmouseleave = "";
+      scrollBar.style.animation = `closeScrollbar ${addon.settings.get("speed")}s 1`;
+      lockDisplay.classList.add("sa-flyoutClose");
+      lockDisplay.style.animation = `closeLock ${addon.settings.get("speed")}s 1`;
     }
     onmouseleave(); // close flyout on load
     placeHolderDiv.onmouseenter = onmouseenter;
+    scrollBar.onmouseleave = onmouseleave;
   }
 }
