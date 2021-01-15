711d5a0c43f862efd949227b620fc12c9067b3de
TheColaber <TheColaber@users.noreply.github.com>
2021-01-15 04:55:40 +0000

    style: format code

--- addons/hide-flyout/userscript.js
+++ addons/hide-flyout/userscript.js
@@ -22,7 +22,7 @@ export default async function ({ addon, global, console }) {
         flyoutLock = !flyoutLock;
         lockDisplay.src = addon.self.dir + `/${flyoutLock ? "" : "un"}lock.svg`;
       };
-      document.body.appendChild(lockDisplay)
+      document.body.appendChild(lockDisplay);
     }
 
     function getSpeedValue() {
@@ -45,7 +45,11 @@ export default async function ({ addon, global, console }) {
     }
     function onmouseleave(e) {
       // If we go behind the flyout or the user has locked it, let's return
-      if ((addon.settings.get("catagoryclick") && (e && e.clientX <= scrollBar.getBoundingClientRect().left)) || flyoutLock) return;
+      if (
+        (addon.settings.get("catagoryclick") && e && e.clientX <= scrollBar.getBoundingClientRect().left) ||
+        flyoutLock
+      )
+        return;
       flyOut.classList.add("sa-flyoutClose");
       flyOut.style.animation = `closeFlyout ${getSpeedValue()}s 1`;
       scrollBar.classList.add("sa-flyoutClose");
@@ -68,7 +72,7 @@ export default async function ({ addon, global, console }) {
             toggle = true;
           }
           selectedCat = catagory;
-        }
+        };
       }
     } else {
       placeHolderDiv.onmouseenter = onmouseenter;
