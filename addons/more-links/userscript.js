a0d7746bc254b5dd55b568d81a9947be7d6ec2c6
apple502j <33279053+apple502j@users.noreply.github.com>
2021-01-19 04:45:30 +0900

    Replace linkifyjs with a regex (#1345)
    * Allow userscripts to load a web worker
    
    * Replace linkifyjs with a regex
    
    * style: format code
    
    Co-authored-by: apple502j <apple502j@users.noreply.github.com>

--- addons/more-links/userscript.js
+++ addons/more-links/userscript.js
@@ -1,12 +1,11 @@
-export default async function ({ addon, console }) {
-  await addon.tab.loadScript(addon.self.lib + "/linkify.min.js");
-  await addon.tab.loadScript(addon.self.lib + "/linkify-element.min.js");
+import { linkifyTextNode, linkifyTag } from "../../libraries/fast-linkify.js";
 
+export default async function ({ addon, console }) {
   const pageType = document.location.pathname.substr(1).split("/")[0];
 
   switch (pageType) {
     case "users":
-      document.querySelectorAll("#user-details .read-only").forEach((element) => linkifyElement(element));
+      document.querySelectorAll("p.overview").forEach((element) => linkifyTextNode(element));
       break;
 
     case "projects":
@@ -21,20 +20,29 @@ export default async function ({ addon, console }) {
             }
           });
           element.normalize();
-          linkifyElement(element);
+          linkifyTextNode(element);
         }
       })();
       break;
 
     case "studios":
-      linkifyElement(document.querySelector("#description.read-only .overview"));
+      linkifyTag(document.querySelector("#description.read-only .overview"));
       break;
   }
 
   (async () => {
-    while (true) {
-      let comment = await addon.tab.waitForElement(".comment", { markAsSeen: true });
-      linkifyElement(comment);
+    if (addon.tab.clientVersion === "scratchr2") {
+      while (true) {
+        let comment = await addon.tab.waitForElement(".comment .content", { markAsSeen: true });
+        // scratchr2 comment is a simple linkifyTextNode.
+        linkifyTextNode(comment);
+      }
+    } else {
+      while (true) {
+        let comment = await addon.tab.waitForElement("span.comment-content", { markAsSeen: true });
+        // scratch-www comment is <span>-based.
+        linkifyTag(comment, HTMLSpanElement);
+      }
     }
   })();
 }
