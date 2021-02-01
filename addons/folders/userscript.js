0a3af923ee78c29b1c623ab7befc1c627da49f27
Thomas Weber <weber_t@mailbox.org>
2021-02-01 17:27:45 -0600

    More WIP, more broken

--- addons/folders/userscript.js
+++ addons/folders/userscript.js
@@ -1,113 +1,142 @@
-export default async function ({addon, global, console}) {
-  const REACT_INTERNAL_PREFIX = '__reactInternalInstance$';
-  let hasInjectedReactInternals = false;
-
+export default async function ({ addon, global, console, msg }) {
+  const REACT_INTERNAL_PREFIX = "__reactInternalInstance$";
   const ID_FOLDER_PREFIX = "sa_folder_folder_";
   const ID_BACK = "sa_folder_back";
 
-  const getFolderFromName = name => {
-    const idx = name.indexOf('/');
+  let reactInternalKey;
+
+  const getFolderFromName = (name) => {
+    const idx = name.indexOf("/");
     if (idx === -1) {
       return null;
     }
     return name.substr(0, idx);
   };
 
-  let SortableHOCInstance;
+  const getSortableHOCFromElement = (el) => {
+    const nearestSpriteSelector = el.closest("[class*='sprite-selector_sprite-selector']");
+    if (nearestSpriteSelector) {
+      return nearestSpriteSelector[reactInternalKey].child.sibling.child.stateNode;
+    }
+    const nearestAssetPanelWrapper = el.closest('[class*="asset-panel_wrapper"]');
+    if (nearestAssetPanelWrapper) {
+      return nearestAssetPanelWrapper[reactInternalKey].child.child.stateNode;
+    }
+    throw new Error('cannot find SortableHOC');
+  };
 
-  while (true) {
-    const spriteSelector = await addon.tab.waitForElement("[class*='sprite-selector_sprite-selector']", {
-      markAsSeen: true
+  const setFolder = (component, folder) => {
+    const sortableHOCInstance = getSortableHOCFromElement(component.ref);
+    sortableHOCInstance.setState({
+      folder,
     });
+  };
 
-    const reactInternalKey = Object.keys(spriteSelector).find(i => i.startsWith(REACT_INTERNAL_PREFIX));
-    SortableHOCInstance = spriteSelector[reactInternalKey].child.sibling.child.stateNode;
-
-    if (!hasInjectedReactInternals) {
-      hasInjectedReactInternals = true;
-
-      const setFolder = (folder) => {
-        // TODO: calculate everything here to avoid two setState
-        SortableHOCInstance.setState({
-          folder
-        });
-      };
-
-      const spriteSelectorItem = await addon.tab.waitForElement("[class*='sprite-selector_sprite-wrapper']");
-      const SpriteSelectorItemInstance = spriteSelectorItem[reactInternalKey].child.child.child.stateNode;
-
-      const SortableHOC = SortableHOCInstance.constructor;
-      const SpriteSelectorItem = SpriteSelectorItemInstance.constructor;
-
-      SortableHOC.prototype.componentDidUpdate = function (prevProps, prevState) {
-        const folder = this.state ? this.state.folder : null;
-        if (!prevState || prevState.folder !== folder || prevProps.items !== this.props.items) {
-          const items = [];
-          if (folder) {
-            items.push({
-              name: 'Back',
-              id: ID_BACK
-            });
-            for (const item of this.props.items) {
-              const itemFolder = getFolderFromName(item.name);
-              if (itemFolder === folder) {
-                items.push(item);
+  const patchSortableHOC = (SortableHOC) => {
+    SortableHOC.prototype.componentDidUpdate = function (prevProps, prevState) {
+      console.log(this.props.items);
+      const folder = this.state ? this.state.folder : null;
+      if (!prevState || prevState.folder !== folder || prevProps.items !== this.props.items) {
+        const items = [];
+  
+        if (folder) {
+          const backItem = {
+            name: msg("back"),
+            id: ID_BACK,
+            costume: {
+              asset: {
+                encodeDataURI() {
+                  return addon.self.dir + '/back.svg';
+                }
               }
             }
-          } else {
-            const alreadyIncludedFolders = [];
-            for (const item of this.props.items) {
-              const itemFolder = getFolderFromName(item.name);
-              if (itemFolder) {
-                if (alreadyIncludedFolders.includes(itemFolder)) {
-                  continue;
-                }
-                // TODO: highlight if active
-                alreadyIncludedFolders.push(itemFolder);
-                items.push({
-                  name: itemFolder,
-                  id: `${ID_FOLDER_PREFIX}${itemFolder}`
-                });
-              } else {
-                items.push(item);
-              }
+          };
+          items.push(backItem);
+  
+          for (const item of this.props.items) {
+            const itemFolder = getFolderFromName(item.name);
+            if (itemFolder === folder) {
+              items.push(item);
             }
           }
-          this.setState({
-            items
-          });
+        } else {
+          const folderItems = {};
+          for (const item of this.props.items) {
+            const itemFolder = getFolderFromName(item.name);
+            if (itemFolder) {
+              if (!folderItems[itemFolder]) {
+                const itemProp = {
+                  name: `[F] ${itemFolder}`, // TODO
+                  id: `${ID_FOLDER_PREFIX}${itemFolder}`,
+                  costume: item.costume,
+                };
+                folderItems[itemFolder] = itemProp;
+                items.push(itemProp);
+              }
+              // if (item.id === this.props.selectedId) {
+              //   this.props.selectedId = folderItems[itemFolder].id;
+              // }
+            } else {
+              items.push(item);
+            }
+          }
         }
-      };
+        this.setState({
+          items,
+        });
+      }
+    };
+  
+    const originalSortableHOCRender = SortableHOC.prototype.render;
+    SortableHOC.prototype.render = function () {
+      if (!this.state) {
+        return originalSortableHOCRender.call(this);
+      }
+  
+      const originalItems = this.props.items;
+      this.props.items = this.state.items;
+      const result = originalSortableHOCRender.call(this);
+      this.props.items = originalItems;
+      return result;
+    };
+  };
 
-      const originalSortableHOCRender = SortableHOC.prototype.render;
-      SortableHOC.prototype.render = function () {
-        if (!this.state) {
-          return originalSortableHOCRender.call(this);
-        }
-
-        const originalItems = this.props.items;
-        this.props.items = this.state.items;
-        const result = originalSortableHOCRender.call(this);
-        this.props.items = originalItems;
-        return result;
-      };
-
-      // TODO: pretend that folder is selected if something inside it is
-      const originalSpriteSelectorItemHandleClick = SpriteSelectorItem.prototype.handleClick;
-      SpriteSelectorItem.prototype.handleClick = function (e) {
+  const patchSpriteSelectorItem = (SpriteSelectorItem) => {
+    const originalSpriteSelectorItemHandleClick = SpriteSelectorItem.prototype.handleClick;
+    SpriteSelectorItem.prototype.handleClick = function (e) {
+      if (typeof this.props.id === 'string') {
         if (this.props.id === ID_BACK) {
           e.preventDefault();
-          setFolder(null);
+          setFolder(this, null);
           return;
         }
         if (this.props.id.startsWith(ID_FOLDER_PREFIX)) {
           e.preventDefault();
           const folderName = this.props.id.substr(ID_FOLDER_PREFIX.length);
-          setFolder(folderName);
+          setFolder(this, folderName);
           return;
         }
-        originalSpriteSelectorItemHandleClick.call(this, e);
-      };
-    }
+      }
+      originalSpriteSelectorItemHandleClick.call(this, e);
+    };
+  };
+
+  // Sprite list
+  {
+    const spriteSelectorItemElement = await addon.tab.waitForElement("[class*='sprite-selector_sprite-wrapper']");  
+    reactInternalKey = Object.keys(spriteSelectorItemElement).find((i) => i.startsWith(REACT_INTERNAL_PREFIX));
+    const sortableHOCInstance = getSortableHOCFromElement(spriteSelectorItemElement);
+    const spriteSelectorItemInstance = spriteSelectorItemElement[reactInternalKey].child.child.child.stateNode;
+    patchSortableHOC(sortableHOCInstance.constructor);
+    patchSpriteSelectorItem(spriteSelectorItemInstance.constructor);
+    sortableHOCInstance.forceUpdate();
+  }
+
+  // Costume and sound list
+  {
+    const selectorListItem = await addon.tab.waitForElement("[class*='selector_list-item']");
+    const sortableHOCInstance = getSortableHOCFromElement(selectorListItem);
+    patchSortableHOC(sortableHOCInstance.constructor);
+    sortableHOCInstance.forceUpdate();
   }
 }
