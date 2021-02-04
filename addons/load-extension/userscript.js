4c2babf1b72404d09447cb0fc394dee21e9295e0
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-02-04 11:51:52 -0500

    add more extensions

--- addons/load-extension/userscript.js
+++ addons/load-extension/userscript.js
@@ -1,6 +1,7 @@
 export default async function ({ addon, global, console }) {
+  // TODO: Add removing extensions.
   for (let ext of Object.keys(addon.settings.getAll)) {
-    if (addon.settings.getAll[ext]) {
+    if (addon.settings.getAll[ext] && !addon.tab.traps.vm.extensionManager.isExtensionLoaded(ext)) {
       addon.tab.traps.vm.extensionManager.loadExtensionIdSync(ext);
     }
   }
