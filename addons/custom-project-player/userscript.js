63b32c7036aaae2263fd42c896a83b07f6cd370e
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-02-10 20:37:28 -0600

    Update userscript.js

--- addons/custom-project-player/userscript.js
+++ addons/custom-project-player/userscript.js
@@ -1,15 +1,17 @@
-
 export default async function ({ addon, global, console }) {
-  console.log("Hello, " + addon.auth.username);
-  
-  
- 
 var id = window.location.href;
 var pid= id.match(/\d/g);
 pid = pid.join("");
-console.log(pid);
-
-
-let player = await addon.tab.waitForElement(".guiPlayer");
-document.getElementsByClassName("guiPlayer")[0].innerHTML = '<iframe src="https://forkphorus.github.io/embed.html?id=' + pid + '&auto-start=true&light-content=false" width="480" height="392" allowfullscreen="true" allowtransparency="true" style="border:none;"></iframe>';
+ let player = await addon.tab.waitForElement(".guiPlayer");
+  var FPP = document.createElement('iframe');
+   FPP.src = 'https://forkphorus.github.io/embed.html?id=' + pid + '&auto-start=true&light-content=false';
+   FPP.width = 480;
+   FPP.height = 392;
+   FPP.className = "newPlayer"
+   FPP.allowfullscreen = "true";
+   var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
+   target.remove();
+    document.querySelector(".guiPlayer").appendChild(FPP);
+    let alerts = await addon.tab.waitForElement(".project-info-alert");
+	document.getElementsByClassName("project-info-alert")[0].remove();
 }
