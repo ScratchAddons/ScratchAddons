04b49b2fda96e38ab097fd5fcb98ea4103261ef9
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-02-11 13:41:19 -0600

    changed some things around to prevent crashes

---
+++ custom-project-player/userscript.js
@@ -0,0 +1,26 @@
+export default async function ({ addon, global, console }) {
+  var id = window.location.href;
+  var pid = id.match(/\d/g);
+  pid = pid.join("");
+
+  while (1) {
+	  
+    let alerts = await addon.tab.waitForElement(".project-info-alert"); // this is the thing that says that the project uses your username. 
+    document.getElementsByClassName("project-info-alert")[0].remove();
+		  
+    let player = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
+    var FPP = document.createElement("iframe");
+    FPP.src = "https://forkphorus.github.io/embed.html?id=" + pid + "&auto-start=true&light-content=false";
+    FPP.width = 480;
+    FPP.height = 392;
+    FPP.className = "newPlayer";
+    FPP.allowfullscreen = "true";
+
+    var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
+    target.remove();
+
+    document.querySelector(".guiPlayer").appendChild(FPP);
+
+
+  }
+}
