dd06c16b7a3fca8df65e3654983669bf9e653d68
WorldLanguages <mundofinky@gmail.com>
2021-02-12 16:42:44 -0300

    Prettier lint

--- addons/copy-message-link/profile.js
+++ addons/copy-message-link/profile.js
@@ -1,27 +1,25 @@
-export default async function ({
-  addon,
-  global,
-  console,
-  msg
-}) {
-  while(true) {
+export default async function ({ addon, global, console, msg }) {
+  while (true) {
     const comment = await addon.tab.waitForElement("div.comment", {
-      markAsSeen: true
+      markAsSeen: true,
     });
-    const newElem = document.createElement('span');
-    newElem.className = 'actions report';
-    newElem.textContent = msg('copyLink');
+    const newElem = document.createElement("span");
+    newElem.className = "actions report";
+    newElem.textContent = msg("copyLink");
     newElem.onclick = () => {
       // For profiles, respect correct username casing in URL
-      let url = location.pathname.split("/")[1] === "users" ? `${location.origin}/users/${Scratch.INIT_DATA.PROFILE.model.id}/` : `${location.origin}${location.pathname}`;
+      let url =
+        location.pathname.split("/")[1] === "users"
+          ? `${location.origin}/users/${Scratch.INIT_DATA.PROFILE.model.id}/`
+          : `${location.origin}${location.pathname}`;
       navigator.clipboard.writeText(`${url}#${comment.id}`);
-      newElem.textContent = msg('copied');
-      newElem.style.fontWeight = 'bold';
+      newElem.textContent = msg("copied");
+      newElem.style.fontWeight = "bold";
       setTimeout(() => {
-        newElem.textContent = msg('copyLink');
-        newElem.style.fontWeight = '';
+        newElem.textContent = msg("copyLink");
+        newElem.style.fontWeight = "";
       }, 5000);
-    }
+    };
     comment.querySelector("div.actions-wrap").appendChild(newElem);
   }
 }
