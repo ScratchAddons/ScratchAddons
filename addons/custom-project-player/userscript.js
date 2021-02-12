052e2605a1d614f0b67689b2ab4ef22ddeccfa52
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-02-11 20:35:21 -0600

    Update userscript.js

--- addons/custom-project-player/userscript.js
+++ addons/custom-project-player/userscript.js
@@ -2,18 +2,33 @@ export default async function ({ addon, global, console }) {
   var id = window.location.href;
   var pid = id.match(/\d/g);
   pid = pid.join("");
-  while (1) {
-    let player = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
-    var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
-    target.style.display = "none";
+  addon.tab.addEventListener("urlChange", (e) => {
+	console.log(event.detail.newUrl);
+	console.log(event.detail.newUrl.includes("editor"));
+	  if (!event.detail.newUrl.includes("editor")){
+		  
+ target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
+    target.style.display = "none"; 
     var FPP = document.createElement("iframe");
     FPP.src = "https://forkphorus.github.io/embed.html?id=" + pid + "&auto-start=true&light-content=false";
     FPP.width = 480;
     FPP.height = 392;
     FPP.className = "newPlayer";
     FPP.allowfullscreen = "true";
-    document.querySelector(".guiPlayer").appendChild(FPP);
-    let alerts = await addon.tab.waitForElement(".project-info-alert");
+   document.querySelector(".guiPlayer").appendChild(FPP);
+	  }
+});
+let player = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
+ var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
+    target.style.display = "none"; 
+    var FPP = document.createElement("iframe");
+    FPP.src = "https://forkphorus.github.io/embed.html?id=" + pid + "&auto-start=true&light-content=false";
+    FPP.width = 480;
+    FPP.height = 392;
+    FPP.className = "newPlayer";
+    FPP.allowfullscreen = "true";
+   document.querySelector(".guiPlayer").appendChild(FPP);
+	let alerts = await addon.tab.waitForElement(".project-info-alert");
     document.getElementsByClassName("project-info-alert")[0].remove();
-  }
 }
+
