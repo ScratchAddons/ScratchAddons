df836c2874ecb32a90da6d0e03975e05a42846fc
SJRNoodles <SJRNoodles@users.noreply.github.com>
2021-02-12 00:16:00 +0000

    style: format code

--- addons/custom-project-player/userscript.js
+++ addons/custom-project-player/userscript.js
@@ -3,17 +3,17 @@ export default async function ({ addon, global, console }) {
   var pid = id.match(/\d/g);
   pid = pid.join("");
   while (1) {
- let player = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
- var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
-    target.style.display = "none"; 
+    let player = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
+    var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
+    target.style.display = "none";
     var FPP = document.createElement("iframe");
     FPP.src = "https://forkphorus.github.io/embed.html?id=" + pid + "&auto-start=true&light-content=false";
     FPP.width = 480;
     FPP.height = 392;
     FPP.className = "newPlayer";
     FPP.allowfullscreen = "true";
-   document.querySelector(".guiPlayer").appendChild(FPP);
-	let alerts = await addon.tab.waitForElement(".project-info-alert");
+    document.querySelector(".guiPlayer").appendChild(FPP);
+    let alerts = await addon.tab.waitForElement(".project-info-alert");
     document.getElementsByClassName("project-info-alert")[0].remove();
   }
 }
