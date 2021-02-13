a0e6ddf3d32c2d8a4e36d234424ffcb0061388dc
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-02-13 13:29:20 -0600

    Add files via upload

--- addons/custom-project-player/userscript.js
+++ addons/custom-project-player/userscript.js
@@ -3,11 +3,13 @@ export default async function ({ addon, global, console }) {
   var pid = id.match(/\d/g);
   pid = pid.join("");
   addon.tab.addEventListener("urlChange", (e) => {
+    console.log(event.detail.newUrl);
+    console.log(event.detail.newUrl.includes("editor"));
     if (!event.detail.newUrl.includes("editor")) {
       target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
       target.style.display = "none";
       var FPP = document.createElement("iframe");
-      FPP.src = "https://forkphorus.github.io/embed.html?id=" + pid + "&auto-start=true&light-content=false";
+      FPP.src = "https://https://turbowarp.org/" + pid + "/embed";
       FPP.width = 480;
       FPP.height = 392;
       FPP.className = "newPlayer";
@@ -15,18 +17,19 @@ export default async function ({ addon, global, console }) {
       document.querySelector(".guiPlayer").appendChild(FPP);
     }
   });
-  if (!id.includes("editor")) {
-    let player = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
-    var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
-    target.style.display = "none";
-    var FPP = document.createElement("iframe");
-    FPP.src = "https://forkphorus.github.io/embed.html?id=" + pid + "&auto-start=true&light-content=false";
-    FPP.width = 480;
-    FPP.height = 392;
-    FPP.className = "newPlayer";
-    FPP.allowfullscreen = "true";
-    document.querySelector(".guiPlayer").appendChild(FPP);
-    let alerts = await addon.tab.waitForElement(".project-info-alert");
-    document.getElementsByClassName("project-info-alert")[0].remove();
+  if(!id.includes("editor")){
+	  console.log(id.includes("editor"));
+  let player = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
+  var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
+  target.style.display = "none";
+  var FPP = document.createElement("iframe");
+  FPP.src = "https://turbowarp.org/" + pid + "/embed";
+  FPP.width = 480;
+  FPP.height = 392;
+  FPP.className = "newPlayer";
+  FPP.allowfullscreen = "true";
+  document.querySelector(".guiPlayer").appendChild(FPP);
+  let alerts = await addon.tab.waitForElement(".project-info-alert");
+  document.getElementsByClassName("project-info-alert")[0].remove();
   }
 }
