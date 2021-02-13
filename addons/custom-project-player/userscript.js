557f196b56b697b61f0144bc0dcead404c63fe0f
SJRNoodles <SJRNoodles@users.noreply.github.com>
2021-02-13 19:34:32 +0000

    style: format code

--- addons/custom-project-player/userscript.js
+++ addons/custom-project-player/userscript.js
@@ -49,24 +49,24 @@ export default async function ({ addon, global, console }) {
       FPP.height = 392;
       FPP.className = "newPlayer";
       FPP.allowfullscreen = "true";
-	  FPP.style = "border:none;";
+      FPP.style = "border:none;";
       document.querySelector(".guiPlayer").appendChild(FPP);
     }
   });
-  if(!id.includes("editor")){
-	  console.log(id.includes("editor"));
-  await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
-  var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
-  target.style.display = "none";
-  var FPP = document.createElement("iframe");
-  FPP.src = "https://turbowarp.org/" + pid + "/embed";
-  FPP.width = 480;
-  FPP.height = 392;
-  FPP.className = "newPlayer";
-  FPP.allowfullscreen = "true";
-  FPP.style = "border:none;";
-  document.querySelector(".guiPlayer").appendChild(FPP);
-  let alerts = await addon.tab.waitForElement(".project-info-alert");
-  document.getElementsByClassName("project-info-alert")[0].remove();
+  if (!id.includes("editor")) {
+    console.log(id.includes("editor"));
+    await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
+    var target = document.getElementsByClassName("stage-wrapper_stage-wrapper_2bejr")[0];
+    target.style.display = "none";
+    var FPP = document.createElement("iframe");
+    FPP.src = "https://turbowarp.org/" + pid + "/embed";
+    FPP.width = 480;
+    FPP.height = 392;
+    FPP.className = "newPlayer";
+    FPP.allowfullscreen = "true";
+    FPP.style = "border:none;";
+    document.querySelector(".guiPlayer").appendChild(FPP);
+    let alerts = await addon.tab.waitForElement(".project-info-alert");
+    document.getElementsByClassName("project-info-alert")[0].remove();
   }
 }
