403129446bbe7a2d921c94eb2ffe32f17a7f6083
Thomas Weber <weber_t@mailbox.org>
2021-02-22 18:03:22 -0600

    Move some things around, fix more bugs

--- addons/variable-manager/userscript.js
+++ addons/variable-manager/userscript.js
@@ -1,12 +1,13 @@
 export default async function ({ addon, global, console, msg }) {
   const vm = addon.tab.traps.vm;
-  window.vm = vm; // for debugging if i forget to commehnt this out plz yel at me thanks
 
   let contentArea;
+  let localVariables = [];
+  let globalVariables = [];
+  let preventUpdate = false;
 
   const manager = document.createElement("div");
   manager.classList.add(addon.tab.scratchClass("asset-panel_wrapper"), "sa-var-manager");
-  manager.id = "var-manager";
 
   const localVars = document.createElement("div");
   const localHeading = document.createElement("span");
@@ -41,6 +42,122 @@ export default async function ({ addon, global, console, msg }) {
   varTab.appendChild(varTabIcon);
   varTab.appendChild(varTabText);
 
+  class WrappedVariable {
+    constructor (scratchVariable, target) {
+      this.scratchVariable = scratchVariable;
+      this.target = target;
+      this.buildDOM();
+    }
+
+    updateValue () {
+      // TODO do not update if no change
+      // TODO check visibility
+      if (this.scratchVariable.type == "list") {
+        this.input.value = this.scratchVariable.value.join("\n");
+      } else {
+        this.input.value = this.scratchVariable.value;
+      }
+    }
+
+    resizeInputIfList () {
+      if (this.scratchVariable.type === "list") {
+        this.input.style.height = "auto";
+        const height = Math.min(1000, this.input.scrollHeight);
+        if (height > 0) {
+          this.input.style.height = height + "px";
+        }
+      }
+    }
+
+    buildDOM () {
+      const row = document.createElement("tr");
+      this.row = row;
+      const label = document.createElement("td");
+      label.innerText = this.scratchVariable.name;
+
+      const value = document.createElement("td");
+      value.className = "sa-var-manager-value";
+
+      let input;
+      if (this.scratchVariable.type === 'list') {
+        input = document.createElement("textarea");
+      } else {
+        input = document.createElement("input");
+      }
+      this.input = input;
+
+      this.updateValue();
+      if (this.scratchVariable.type === 'list') {
+        this.input.addEventListener("input", () => this.resizeInputIfList(), false);
+      }
+
+      input.addEventListener("keydown", (e) => {
+        if (e.key === "Enter" && !e.shiftKey) {
+          e.preventDefault();
+          if (this.scratchVariable.type == "list") {
+            vm.setVariableValue(this.target.id, this.scratchVariable.id, input.value.split("\n"));
+          } else {
+            vm.setVariableValue(this.target.id, this.scratchVariable.id, input.value);
+          }
+          input.blur();
+        }
+      });
+
+      input.addEventListener("focus", (e) => {
+        preventUpdate = true;
+        manager.classList.add("freeze");
+      });
+
+      input.addEventListener("blur", (e) => {
+        preventUpdate = false;
+        manager.classList.remove("freeze");
+      });
+
+      value.appendChild(input);
+      row.appendChild(label);
+      row.appendChild(value);
+    }
+  }
+
+  function fullReload() {
+    if (addon.tab.redux.state.scratchGui.editorTab.activeTabIndex !== 3 || preventUpdate) return;
+
+    const editingTarget = vm.runtime.getEditingTarget();
+    const stage = vm.runtime.getTargetForStage();
+    localVariables = Object.values(editingTarget.variables).map(i => new WrappedVariable(i, editingTarget));
+    globalVariables = Object.values(stage.variables).map(i => new WrappedVariable(i, stage));
+
+    localHeading.style.display = localVariables.length === 0 ? 'none' : '';
+    globalHeading.style.display = globalVariables.length === 0 ? 'none' : '';
+
+    while (localList.firstChild) {
+      localList.removeChild(localList.firstChild);
+    }
+    while (globalList.firstChild) {
+      globalList.removeChild(globalList.firstChild);
+    }
+
+    for (const variable of localVariables) {
+      localList.appendChild(variable.row);
+      variable.resizeInputIfList();
+    }
+    for (const variable of globalVariables) {
+      globalList.appendChild(variable.row);
+      variable.resizeInputIfList();
+    }
+  }
+
+  function quickReload() {
+    if (addon.tab.redux.state.scratchGui.editorTab.activeTabIndex !== 3 || preventUpdate) return;
+
+    for (const variable of localVariables) {
+      variable.updateValue();
+    }
+    for (const variable of globalVariables) {
+      variable.updateValue();
+    }
+  }
+
   varTab.addEventListener("click", (e) => {
     addon.tab.redux.dispatch({ type: "scratch-gui/navigation/ACTIVATE_TAB", activeTabIndex: 3 });
   });
@@ -74,122 +191,6 @@ export default async function ({ addon, global, console, msg }) {
     quickReload();
     return ret;
   };
-
-  let preventUpdate = false;
-
-  class WrappedVariable {
-    constructor (scratchVariable, target) {
-      this.scratchVariable = scratchVariable;
-      this.target = target;
-      this.buildDOM();
-    }
-
-    updateValue () {
-      // TODO do not update if no change
-      // TODO check visibility
-      if (this.scratchVariable.type == "list") {
-        this.input.value = this.scratchVariable.value.join("\n");
-      } else {
-        this.input.value = this.scratchVariable.value;
-      }
-    }
-
-    buildDOM () {
-      const row = document.createElement("tr");
-      this.row = row;
-      const label = document.createElement("td");
-      label.innerText = this.scratchVariable.name;
-
-      const value = document.createElement("td");
-      value.className = "sa-var-manager-value";
-
-      function inputResize() {
-        input.style.height = "auto";
-        input.style.height = input.scrollHeight + "px";
-      }
-
-      let input;
-      if (this.scratchVariable.type === 'list') {
-        input = document.createElement("textarea");
-      } else {
-        input = document.createElement("input");
-      }
-      this.input = input;
-
-      this.updateValue();
-      if (this.scratchVariable.type === 'list') {
-        this.input.setAttribute("style", "height:" + this.input.scrollHeight + "px;overflow-y:hidden;");
-        this.input.addEventListener("input", inputResize, false);
-      }
-
-      input.addEventListener("keydown", (e) => {
-        if (e.key === "Enter" && !e.shiftKey) {
-          e.preventDefault();
-          if (this.scratchVariable.type == "list") {
-            vm.setVariableValue(this.target.id, this.scratchVariable.id, input.value.split("\n"));
-          } else {
-            vm.setVariableValue(this.target.id, this.scratchVariable.id, input.value);
-          }
-          input.blur();
-        }
-      });
-
-      input.addEventListener("focus", (e) => {
-        preventUpdate = true;
-        manager.classList.add("freeze");
-      });
-
-      input.addEventListener("blur", (e) => {
-        preventUpdate = false;
-        manager.classList.remove("freeze");
-      });
-
-      value.appendChild(input);
-      row.appendChild(label);
-      row.appendChild(value);
-    }
-  }
-
-  let localVariables = [];
-  let globalVariables = [];
-
-  function fullReload() {
-    if (addon.tab.redux.state.scratchGui.editorTab.activeTabIndex !== 3 || preventUpdate) return;
-    console.log("full list reload");
-
-    const editingTarget = vm.runtime.getEditingTarget();
-    const stage = vm.runtime.getTargetForStage();
-    localVariables = Object.values(editingTarget.variables).map(i => new WrappedVariable(i, editingTarget));
-    globalVariables = Object.values(stage.variables).map(i => new WrappedVariable(i, stage));
-
-    localHeading.style.display = localVariables.length === 0 ? 'none' : '';
-    globalHeading.style.display = globalVariables.length === 0 ? 'none' : '';
-
-    while (localList.firstChild) {
-      localList.removeChild(localList.firstChild);
-    }
-    while (globalList.firstChild) {
-      globalList.removeChild(globalList.firstChild);
-    }
-
-    for (const variable of localVariables) {
-      localList.appendChild(variable.row);
-    }
-    for (const variable of globalVariables) {
-      globalList.appendChild(variable.row);
-    }
-  }
-
-  function quickReload() {
-    if (addon.tab.redux.state.scratchGui.editorTab.activeTabIndex !== 3 || preventUpdate) return;
-
-    for (const variable of localVariables) {
-      variable.updateValue();
-    }
-    for (const variable of globalVariables) {
-      variable.updateValue();
-    }
-  }
 
   while (true) {
     const tabs = await addon.tab.waitForElement("[class^='react-tabs_react-tabs__tab-list']", {
