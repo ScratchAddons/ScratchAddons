5eac141f87992f6348423257249139b074cf8c65
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-28 17:13:41 +0000

    Remove try catch

--- addons/block-palette-icons/userscript.js
+++ addons/block-palette-icons/userscript.js
@@ -19,20 +19,16 @@ export default async function ({ addon, global, console }) {
       "number_icon",
       "block_icon",
     ];
-    try {
-      //For each .scratchCategoryItemBubble add an icon
-      document.querySelectorAll(".scratchCategoryItemBubble").forEach((item, i) => {
-        //Make the padding a little bigger to fit the icons.
-        item.style.padding = "11px";
-        //Position it relative so that absolute positioning will be relative to the bubble.
-        item.style.position = "relative";
-        let k = document.createElement("img");
-        k.src = `./icons/${icons[i]}.svg`;
-		k.setAttribute("style", "top: 15%; color: white; left: 15%; position: absolute; font-size: 17px");
-		item.appendChild(k);
-      });
-    } catch (e) {
-      console.error("Debug for block pallete icons: Error: " + e.stack);
-    }
+    //For each .scratchCategoryItemBubble add an icon
+    document.querySelectorAll(".scratchCategoryItemBubble").forEach((item, i) => {
+      //Make the padding a little bigger to fit the icons.
+      item.style.padding = "11px";
+      //Position it relative so that absolute positioning will be relative to the bubble.
+      item.style.position = "relative";
+      let k = document.createElement("img");
+      k.src = `./icons/${icons[i]}.svg`;
+      k.setAttribute("style", "top: 15%; color: white; left: 15%; position: absolute; font-size: 17px");
+      item.appendChild(k);
+    });
   }
 }
