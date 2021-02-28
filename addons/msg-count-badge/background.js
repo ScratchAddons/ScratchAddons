d17c37da136ec632f93d9e1b6d5c16ff915cadab
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-02-28 09:41:30 -0500

    last thing

--- addons/msg-count-badge/background.js
+++ addons/msg-count-badge/background.js
@@ -2,7 +2,6 @@ export default async function ({ addon, global, console, setTimeout, setInterval
   const setBadge = async () => {
     const msgCount = await addon.account.getMsgCount();
     addon.badge.text = msgCount;
-    addon.self.sendMessage({ hewwo: "????" });
   };
   setBadge();
   setInterval(setBadge, 2500);
