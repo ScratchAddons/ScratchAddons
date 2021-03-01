aca5dd7c8a565091dfb7341dbf8f7a2b0de6a53c
Explosion-Scratch <explosion.scratch@gmail.com>
2021-03-01 19:39:08 +0000

    Styles in style.css

--- addons/message-filters/userscript.js
+++ addons/message-filters/userscript.js
@@ -25,7 +25,6 @@ export default async function({addon,global,console}) {
       inp_container.classList.add("input_container");
       let inp = document.createElement("input");
       inp.setAttribute("type", "checkbox");
-      inp.setAttribute("style", "margin-right: 3px;  margin-right: .5vmax;")
       inp.checked = true;
       inp.id = String.fromCharCode(97 + i);
       inp.setAttribute("data-for", filter[keys[i]])
@@ -47,9 +46,6 @@ export default async function({addon,global,console}) {
       label.setAttribute("for", String.fromCharCode(97 + i));
       label.appendChild(document.createTextNode(keys[i].replace(keys[i][0], keys[i][0].toUpperCase())));
       inp_container.appendChild(label);
-      inp_container.setAttribute("style", "padding: 5px; width: 49%; display: inline-block; text-align: left; width: 20vmax; display: inline-block; padding: 10px; border-radius: 5px; transition-duration: .3s; transition-property: background-color; cursor: pointer; overflow: hidden; height: 50%; ")
-      inp_container.onmouseover = () => inp_container.style.background = "#05f3";
-      inp_container.onmouseout = () => inp_container.style.background = "transparent";
       checkboxes.appendChild(inp_container);
   }
   setInterval(() => {
@@ -72,6 +68,5 @@ export default async function({addon,global,console}) {
       }
       localStorage.setItem("message_preferences", JSON.stringify(active));
   }, 50)
-  checkboxes.setAttribute("style", "box-sizing: border-box; padding: 2vw; width: 100%; background: #05f2; display: block; margin: 0 auto; border-radius: 5px; border: 1px solid #05f5; width: 100%; margin-top: 10px;")
   document.querySelector(".messages-social-title").appendChild(checkboxes);
 }
