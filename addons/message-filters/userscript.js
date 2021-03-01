bbaf31daa64e3276921a6bbceef07f900f8c3d48
Explosion-Scratch <Explosion-Scratch@users.noreply.github.com>
2021-03-01 19:39:51 +0000

    style: format code

--- addons/message-filters/userscript.js
+++ addons/message-filters/userscript.js
@@ -1,18 +1,18 @@
-export default async function({addon,global,console}) {
+export default async function ({ addon, global, console }) {
   let list = await addon.tab.waitForElement(".messages-social-list", {
-      markAsSeen: true,
+    markAsSeen: true,
   });
   document.querySelector(".select").remove();
   let filter = {
-      "studio activity": "mod-studio-activity",
-      comments: "mod-comment-message",
-      "favorites": "mod-love-favorite",
-      loves: "mod-love-project",
-      remixes: "mod-remix-project",
-      follows: "mod-follow-user",
-      "studio invites": "mod-curator-invite",
-      "forum activity": "mod-forum-activity",
-  }
+    "studio activity": "mod-studio-activity",
+    comments: "mod-comment-message",
+    favorites: "mod-love-favorite",
+    loves: "mod-love-project",
+    remixes: "mod-remix-project",
+    follows: "mod-follow-user",
+    "studio invites": "mod-curator-invite",
+    "forum activity": "mod-forum-activity",
+  };
   let active = Object.keys(filter).map((i) => filter[i]);
   let checkboxes = document.createElement("div");
   checkboxes.classList.add("checkboxes");
@@ -21,52 +21,52 @@ export default async function({addon,global,console}) {
   checkboxes.appendChild(heading);
   let keys = Object.keys(filter);
   for (let i = 0; i < keys.length; i++) {
-      let inp_container = document.createElement("div");
-      inp_container.classList.add("input_container");
-      let inp = document.createElement("input");
-      inp.setAttribute("type", "checkbox");
-      inp.checked = true;
-      inp.id = String.fromCharCode(97 + i);
-      inp.setAttribute("data-for", filter[keys[i]])
-      setInterval(() => {
-          if (inp.checked) {
-              if (!active.includes(inp.getAttribute("data-for"))) active.push(inp.getAttribute("data-for"));
-          } else {
-              if (active.includes(inp.getAttribute("data-for"))) {
-                  let index = active.indexOf(inp.getAttribute("data-for"));
-                  if (index > -1) {
-                      active.splice(index, 1);
-                  }
-              }
+    let inp_container = document.createElement("div");
+    inp_container.classList.add("input_container");
+    let inp = document.createElement("input");
+    inp.setAttribute("type", "checkbox");
+    inp.checked = true;
+    inp.id = String.fromCharCode(97 + i);
+    inp.setAttribute("data-for", filter[keys[i]]);
+    setInterval(() => {
+      if (inp.checked) {
+        if (!active.includes(inp.getAttribute("data-for"))) active.push(inp.getAttribute("data-for"));
+      } else {
+        if (active.includes(inp.getAttribute("data-for"))) {
+          let index = active.indexOf(inp.getAttribute("data-for"));
+          if (index > -1) {
+            active.splice(index, 1);
           }
-      }, 50)
-      inp_container.appendChild(inp);
-      let label = document.createElement("label");
-      label.classList.add("input_label");
-      label.setAttribute("for", String.fromCharCode(97 + i));
-      label.appendChild(document.createTextNode(keys[i].replace(keys[i][0], keys[i][0].toUpperCase())));
-      inp_container.appendChild(label);
-      checkboxes.appendChild(inp_container);
+        }
+      }
+    }, 50);
+    inp_container.appendChild(inp);
+    let label = document.createElement("label");
+    label.classList.add("input_label");
+    label.setAttribute("for", String.fromCharCode(97 + i));
+    label.appendChild(document.createTextNode(keys[i].replace(keys[i][0], keys[i][0].toUpperCase())));
+    inp_container.appendChild(label);
+    checkboxes.appendChild(inp_container);
   }
   setInterval(() => {
-      let messages = document.querySelectorAll(".social-message");
-      let count = 0;
-      for (let i = 0; i < messages.length; i++) {
-          let message = messages[i];
-          let classes = message.classList;
-          for (let j = 0; j < classes.length; j++) {
-              if (active.includes(classes[j]) || active.length === 0) {
-                  count++;
-                  message.style.display = "list-item";
-              } else {
-                  message.style.display = "none";
-              }
-          }
+    let messages = document.querySelectorAll(".social-message");
+    let count = 0;
+    for (let i = 0; i < messages.length; i++) {
+      let message = messages[i];
+      let classes = message.classList;
+      for (let j = 0; j < classes.length; j++) {
+        if (active.includes(classes[j]) || active.length === 0) {
+          count++;
+          message.style.display = "list-item";
+        } else {
+          message.style.display = "none";
+        }
       }
-      if (count < 40 && active.length > 0) {
-          document.querySelector(".messages-social-loadmore").click();
-      }
-      localStorage.setItem("message_preferences", JSON.stringify(active));
-  }, 50)
+    }
+    if (count < 40 && active.length > 0) {
+      document.querySelector(".messages-social-loadmore").click();
+    }
+    localStorage.setItem("message_preferences", JSON.stringify(active));
+  }, 50);
   document.querySelector(".messages-social-title").appendChild(checkboxes);
 }
