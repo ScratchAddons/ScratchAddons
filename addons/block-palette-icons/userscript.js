72ad43a1036eff3e746dee3c22432159c50db83a
--Explosion-- <explosion.scratch@gmail.com>
2021-03-02 09:38:03 -0600

    Block palette icons addon (#1689)
    * Add my addon
    
    * Yep. I miss-spelled palette
    
    * No innerHTML?
    
    * lol I forgot to actually ADD the element I created and stuff
    
    * Missing comma lol
    
    * Forgot to include iconify in libraries
    
    * Icons in icon folder
    
    * Move to the end of addons.json
    
    * Remove iconify lib
    
    * Remove reccomended
    
    * Remove functions
    
    * Remove call to iconify lib
    
    * Remove function
    
    * Remove add function
    
    * Remove try catch
    
    * Fix icons not appearing
    
    * WOW. Nice and centered.
    
    * Better iconsgit stage .!
    
    * Different icons and support for data category tweaks
    
    * Better addon name and description
    
    * Set element.style instead of using style attribute
    
    * Prettier lint
    
    * Allow addon name and description to be localized
    
    * Fix see inside error
    
    Co-authored-by: GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
    Co-authored-by: WorldLanguages <mundofinky@gmail.com>

---
+++ addons/block-palette-icons/userscript.js
@@ -0,0 +1,44 @@
+//When the page loads add the icons.
+export default async function ({ addon, global, console }) {
+  while (true) {
+    const tabs = await addon.tab.waitForElement(".scratchCategoryMenu", {
+      markAsSeen: true,
+    });
+
+    /*
+     * An array of iconify icons for the categories.
+     */
+    let icons = [
+      "motion_icon",
+      "looks_icon",
+      "sound_icon",
+      "events_icon",
+      "control_icon",
+      "sensing_icon",
+      "operators_icon",
+      "variables_icon",
+      "block_icon",
+    ];
+    if (document.querySelector(".scratchCategoryId-lists")) icons.splice(8, 0, "list_icon");
+    //For each .scratchCategoryItemBubble add an icon
+    document.querySelectorAll(".scratchCategoryItemBubble").forEach((item, i) => {
+      //Make the padding a little bigger to fit the icons.
+      item.style.padding = "11px";
+      //Position it relative so that absolute positioning will be relative to the bubble.
+      item.style.position = "relative";
+      let k = document.createElement("img");
+      k.src = `${addon.self.dir}/icons/${icons[i]}.svg`;
+      Object.assign(k.style, {
+        filter: "brightness(50000%)",
+        top: "50%",
+        color: "white",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        position: "absolute",
+        width: "17px",
+        height: "17px",
+      });
+      item.appendChild(k);
+    });
+  }
+}
