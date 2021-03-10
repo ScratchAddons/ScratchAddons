16932d4bab388f811fb021b9fc10a5f0a1e2e815
Thomas Weber <weber_t@mailbox.org>
2021-03-10 12:52:07 -0600

    Do not break when entering/leaving editor

--- addons/mute-project/userscript.js
+++ addons/mute-project/userscript.js
@@ -1,19 +1,19 @@
 export default async function ({ addon, global, console }) {
   const vm = addon.tab.traps.vm;
+  let muted = false;
+  let icon = document.createElement("img");
+  icon.src = "/static/assets/e21225ab4b675bc61eed30cfb510c288.svg";
+  icon.style.display = "none";
   while (true) {
     let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", { markAsSeen: true });
     let container = button.parentElement;
-    let icon = document.createElement("img");
     container.appendChild(icon);
-    icon.src = "/static/assets/e21225ab4b675bc61eed30cfb510c288.svg";
-    icon.style.display = "none";
-    let mode = false;
     button.addEventListener("click", (e) => {
       if (e.ctrlKey) {
         e.cancelBubble = true;
         e.preventDefault();
-        mode = !mode;
-        if (mode) {
+        muted = !muted;
+        if (muted) {
           vm.editingTarget.blocks.runtime.audioEngine.audioContext.suspend();
           icon.style.display = "block";
         } else {
