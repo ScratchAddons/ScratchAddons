d11fcb9c571536b950f8bbd01864bbb5745247dd
lisa_wolfgang <lisawolfgang.onscratch@gmail.com>
2021-03-09 22:39:27 -0600

    Some adjustments

--- addons/custom-block-size/userscript.js
+++ addons/custom-block-size/userscript.js
@@ -5,10 +5,8 @@ export default async function ({ addon, global, console }) {
     const workspace = Blockly.getMainWorkspace()
     const BlockSvg = Blockly.getMainWorkspace().newBlock().constructor
 
-    var { GRID_UNIT } = BlockSvg
-
-    //The actual sizing is increased from the setting because this addon was originally intended for smaller blocks only
-    GRID_UNIT *= addon.settings.get("paddingSize") * 1.75 / 100
+    const { GRID_UNIT } = BlockSvg
+    var multiplier = 1
 
     function forceUpdateBlocks (workspace) {
       workspace.getAllBlocks()
@@ -21,77 +19,86 @@ export default async function ({ addon, global, console }) {
     }
 
     function applyChanges () {
-      BlockSvg.MIN_BLOCK_X = 13 * GRID_UNIT
-      BlockSvg.MIN_BLOCK_X_OUTPUT = 0
-      BlockSvg.MIN_BLOCK_X_SHADOW_OUTPUT = 5 * GRID_UNIT
-      BlockSvg.MIN_BLOCK_Y = 7 * GRID_UNIT
-      BlockSvg.EXTRA_STATEMENT_ROW_Y = 5 * GRID_UNIT
-      BlockSvg.MIN_BLOCK_X_WITH_STATEMENT = 20 * GRID_UNIT
-      BlockSvg.MIN_BLOCK_Y_SINGLE_FIELD_OUTPUT = 0
-      BlockSvg.MIN_BLOCK_Y_REPORTER = 7 * GRID_UNIT
-      BlockSvg.MIN_STATEMENT_INPUT_HEIGHT = 3 * GRID_UNIT
-      BlockSvg.NOTCH_WIDTH = 6 * GRID_UNIT
-      BlockSvg.ICON_SEPARATOR_HEIGHT = 6 * GRID_UNIT
+      multiplier = addon.settings.get("paddingSize") / 100
+      BlockSvg.MIN_BLOCK_X = 16 * GRID_UNIT * multiplier
+      BlockSvg.MIN_BLOCK_X_OUTPUT = 12 * GRID_UNIT * multiplier
+      BlockSvg.MIN_BLOCK_X_SHADOW_OUTPUT = 10 * GRID_UNIT * multiplier
+      BlockSvg.MIN_BLOCK_Y = 12 * GRID_UNIT * multiplier
+      BlockSvg.EXTRA_STATEMENT_ROW_Y = 8 * GRID_UNIT * multiplier
+      BlockSvg.MIN_BLOCK_X_WITH_STATEMENT = 40 * GRID_UNIT * multiplier
+      BlockSvg.MIN_BLOCK_Y_SINGLE_FIELD_OUTPUT = 8 * GRID_UNIT * multiplier
+      BlockSvg.MIN_BLOCK_Y_REPORTER = 10 * GRID_UNIT * multiplier
+      BlockSvg.MIN_STATEMENT_INPUT_HEIGHT = 6 * GRID_UNIT * multiplier
+      BlockSvg.NOTCH_WIDTH = 8 * GRID_UNIT * multiplier
+      BlockSvg.NOTCH_HEIGHT = 2 * GRID_UNIT * multiplier
+      BlockSvg.NOTCH_START_PADDING = 3 * GRID_UNIT
+      BlockSvg.CORNER_RADIUS = 1 * GRID_UNIT //* multiplier
+      BlockSvg.ICON_SEPARATOR_HEIGHT = 10 * GRID_UNIT * multiplier
       BlockSvg.NOTCH_PATH_LEFT = (
         'c 2,0 3,1 4,2 ' +
-        'l 2,2 ' +
+        'l 4,4 ' +
         'c 1,1 2,2 4,2 ' +
-        'h 8 ' +
-        'c 2,0 3,-1 4,-2 ' +
-        'l 2,-2 ' +
+        'h 12' +
+        ' c 2,0 3,-1 4,-2 ' +
+        'l 4,-4 ' +
         'c 1,-1 2,-2 4,-2'
       )
       BlockSvg.NOTCH_PATH_RIGHT = (
         'c -2,0 -3,1 -4,2 ' +
-        'l -2,2 ' +
+        'l -4,4 ' +
         'c -1,1 -2,2 -4,2 ' +
-        'h -8 ' +
-        'c -2,0 -3,-1 -4,-2 ' +
-        'l -2,-2 ' +
+        'h -12' +
+        ' c -2,0 -3,-1 -4,-2 ' +
+        'l -4,-4 ' +
         'c -1,-1 -2,-2 -4,-2'
       )
       BlockSvg.INPUT_SHAPE_HEXAGONAL =
-        'M ' + 2.5 * GRID_UNIT + ',-1 ' +
+        'M ' + 4 * GRID_UNIT + ',0 ' +
         ' h ' + 4 * GRID_UNIT +
-        ' l ' + 2.5 * GRID_UNIT + ',' + 2.5 * GRID_UNIT +
-        ' l ' + -2.5 * GRID_UNIT + ',' + 2.5 * GRID_UNIT +
+        ' l ' + 4 * GRID_UNIT + ',' + 4 * GRID_UNIT +
+        ' l ' + -4 * GRID_UNIT + ',' + 4 * GRID_UNIT +
         ' h ' + -4 * GRID_UNIT +
-        ' l ' + -2.5 * GRID_UNIT + ',' + -2.5 * GRID_UNIT +
-        ' l ' + 2.5 * GRID_UNIT + ',' + -2.5 * GRID_UNIT +
+        ' l ' + -4 * GRID_UNIT + ',' + -4 * GRID_UNIT +
+        ' l ' + 4 * GRID_UNIT + ',' + -4 * GRID_UNIT +
         ' z'
-      BlockSvg.INPUT_SHAPE_HEXAGONAL_WIDTH = 9 * GRID_UNIT
+      BlockSvg.INPUT_SHAPE_HEXAGONAL_WIDTH = 12 * GRID_UNIT * multiplier
       BlockSvg.INPUT_SHAPE_ROUND =
         'M ' + (4 * GRID_UNIT) + ',0' +
         ' h ' + (4 * GRID_UNIT) +
         ' a ' + (4 * GRID_UNIT) + ' ' +
         (4 * GRID_UNIT) + ' 0 0 1 0 ' +
+        (8 * GRID_UNIT)
         ' h ' + (-4 * GRID_UNIT) +
         ' a ' + (4 * GRID_UNIT) + ' ' +
         (4 * GRID_UNIT) + ' 0 0 1 0 -' +
-        ' z'
-      BlockSvg.INPUT_SHAPE_ROUND_WIDTH = 0
-      BlockSvg.INPUT_SHAPE_HEIGHT = 4 * GRID_UNIT
-      BlockSvg.FIELD_HEIGHT = 5 * GRID_UNIT // NOTE: Determines string input heights
-      BlockSvg.FIELD_WIDTH = 0
-      BlockSvg.EDITABLE_FIELD_PADDING = 0 // TODO
-      BlockSvg.BOX_FIELD_PADDING = 2 * GRID_UNIT
-      BlockSvg.DROPDOWN_ARROW_PADDING = 1 * GRID_UNIT
-      BlockSvg.FIELD_WIDTH_MIN_EDIT = 0
-      BlockSvg.INPUT_AND_FIELD_MIN_X = 9 * GRID_UNIT
-      BlockSvg.INLINE_PADDING_Y = 1 * GRID_UNIT // For when reporters are inside reporters
-      BlockSvg.BOX_FIELD_PADDING = 2 * GRID_UNIT
-      BlockSvg.BOX_FIELD_PADDING = 2 * GRID_UNIT
-      BlockSvg.SHAPE_IN_SHAPE_PADDING[1][0] = 4 * GRID_UNIT
-      BlockSvg.SHAPE_IN_SHAPE_PADDING[1][2] = 3 * GRID_UNIT
-      BlockSvg.SHAPE_IN_SHAPE_PADDING[1][3] = 3 * GRID_UNIT
+        (8 * GRID_UNIT) + ' z'
+      BlockSvg.INPUT_SHAPE_ROUND_WIDTH = 12 * GRID_UNIT * multiplier
+      BlockSvg.INPUT_SHAPE_HEIGHT = 8 * GRID_UNIT * multiplier
+      BlockSvg.FIELD_HEIGHT = 8 * GRID_UNIT * multiplier // NOTE: Determines string input heights
+      BlockSvg.FIELD_WIDTH = 6 * GRID_UNIT * multiplier
+      BlockSvg.FIELD_DEFAULT_CORNER_RADIUS = 4 * GRID_UNIT * multiplier
+      BlockSvg.EDITABLE_FIELD_PADDING = 1.5 * GRID_UNIT * multiplier
+      BlockSvg.BOX_FIELD_PADDING = 2 * GRID_UNIT * multiplier
+      BlockSvg.DROPDOWN_ARROW_PADDING = 2 * GRID_UNIT * multiplier
+      BlockSvg.FIELD_WIDTH_MIN_EDIT = 8 * GRID_UNIT * multiplier
+      BlockSvg.INPUT_AND_FIELD_MIN_X = 12 * GRID_UNIT * multiplier
+      BlockSvg.INLINE_PADDING_Y = 1 * GRID_UNIT * multiplier // For when reporters are inside reporters
+      BlockSvg.SHAPE_IN_SHAPE_PADDING[1][0] = 5 * GRID_UNIT * multiplier
+      BlockSvg.SHAPE_IN_SHAPE_PADDING[1][2] = 5 * GRID_UNIT * multiplier
+      BlockSvg.SHAPE_IN_SHAPE_PADDING[1][3] = 5 * GRID_UNIT * multiplier
     }
 
-    applyChanges()
-    updateAllBlocks()
+    function applyAndUpdate() {
+      applyChanges()
+      updateAllBlocks()
+    }
     
     addon.settings.addEventListener("change", function() {
       applyChanges()
       updateAllBlocks()
     });
+
+    applyAndUpdate()
+
   })(window.Blockly)
 }
