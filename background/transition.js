0b90bf7de3806c4af6a6cb9ea5ac383c92fd2a03
apple502j <apple502j@users.noreply.github.com>
2021-03-10 14:40:58 +0000

    style: format code

--- background/transition.js
+++ background/transition.js
@@ -8,10 +8,7 @@ chrome.runtime.onInstalled.addListener(async (details) => {
     chrome.runtime.getManifest().version_name.includes("-prerelease") === false
   ) {
     chrome.tabs.create({ url: "https://scratchaddons.com/welcome" });
-  } else if (
-    details.previousVersion &&
-    /^1\.1[01]\.0/g.test(details.previousVersion)
-  ) {
+  } else if (details.previousVersion && /^1\.1[01]\.0/g.test(details.previousVersion)) {
     scratchAddons.localEvents.addEventListener("ready", () => {
       const loadExtension = scratchAddons.globalState.addonSettings["load-extensions"];
       if (!loadExtension || !loadExtension.videoSensing) return;
