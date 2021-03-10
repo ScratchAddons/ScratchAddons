016954eeae0d4bc862df309b93e591bc9d6552d1
GarboMuffin <33787854+GarboMuffin@users.noreply.github.com>
2021-03-10 14:05:01 -0600

    Improve mute addon (#1803)
    * Do not break when entering/leaving editor
    
    * Just mute the audio engine, don't completely stop it

--- addons/mute-project/userscript.js
+++ addons/mute-project/userscript.js
@@ -1,23 +1,23 @@
 export default async function ({ addon, global, console }) {
   const vm = addon.tab.traps.vm;
+  let muted = false;
+  let icon = document.createElement("img");
+  icon.src = "/static/assets/e21225ab4b675bc61eed30cfb510c288.svg";
+  icon.style.display = "none";
   while (true) {
     let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", { markAsSeen: true });
     let container = button.parentElement;
-    let icon = document.createElement("img");
     container.appendChild(icon);
-    icon.src = "/static/assets/e21225ab4b675bc61eed30cfb510c288.svg";
-    icon.style.display = "none";
-    let mode = false;
     button.addEventListener("click", (e) => {
       if (e.ctrlKey) {
         e.cancelBubble = true;
         e.preventDefault();
-        mode = !mode;
-        if (mode) {
-          vm.editingTarget.blocks.runtime.audioEngine.audioContext.suspend();
+        muted = !muted;
+        if (muted) {
+          vm.runtime.audioEngine.inputNode.gain.value = 0;
           icon.style.display = "block";
         } else {
-          vm.editingTarget.blocks.runtime.audioEngine.audioContext.resume();
+          vm.runtime.audioEngine.inputNode.gain.value = 1;
           icon.style.display = "none";
         }
       }
