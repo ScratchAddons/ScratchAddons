5f1040676f104117c56fd1ad254f2af679d5a013
RedGuy12 <paul@reid-family.org>
2021-03-11 13:27:53 -0600

    refactor, bugs

--- background/get-addon-storage.js
+++ background/get-addon-storage.js
@@ -1,4 +1,19 @@
-updateAddonStorage();
+var storage = new Promise((resolve) => {
+  chrome.storage.sync.get((result) => {
+    resolve(result);
+  });
+});
+storage = await storage;
+var key;
+var addonStorage = {};
+for (let i = 0; i < Object.keys(storage).length; i++) {
+  key = Object.keys(storage)[i].split("/");
+  if (key.length == 2) {
+    addonStorage[key[0]] ?? (addonStorage[key[0]] = {}); // ?? returns the preceeding value if it is not null and the following value if it is. so in this case if addonStorage[key[0]] is null, it will execute addonStorage[key[0]] = {} and return null, otherwise the value of addonStorage[key[0]]
+    addonStorage[key[0]][key[1]] = Object.values(storage)[i];
+  }
+}
+scratchAddons.globalState.addonStorage = addonStorage;
 
 chrome.runtime.onMessageExternal.addListener((request) => {
   console.log(request);
@@ -7,26 +22,10 @@ chrome.runtime.onMessageExternal.addListener((request) => {
       name: request.addonStorageID,
       value: request.addonStorageValue,
     });
-    updateAddonStorage();
-  }
-});
-
-async function updateAddonStorage() {
-  var storage = new Promise((resolve) => {
-    chrome.storage.sync.get((result) => {
-      resolve(result);
-    });
-  });
-  storage = await storage;
-  var key;
-  var addonStorage = {};
-  for (let i = 0; i < Object.keys(storage).length; i++) {
-    key = Object.keys(storage)[i].split("/");
+    var key = request.addonStorageID.split("/")
     if (key.length == 2) {
-      addonStorage[key[0]] = addonStorage[key[0]] ?? {};
-      // ?? returns the preceeding value if it is not null and the following value if it is. so in this case if addonStorage[key[0]] is null, it will retun an empty object, otherwise the value of addonStorage[key[0]]
-      addonStorage[key[0]][key[1]] = Object.values(storage)[i];
+      scratchAddons.globalState.addonStorage[key[0]] ?? (scratchAddons.globalState.addonStorage[key[0]] = {});
+      scratchAddons.globalState.addonStorage[key[0]][key[1]] = request.addonStorageValue;
     }
   }
-  scratchAddons.globalState.addonStorage = addonStorage;
-}
+});
