4c5b6011ac5b6825daebeebbd612ffba7a8d67e8
lisa_wolfgang <lisawolfgang.onscratch@gmail.com>
2021-03-12 20:29:24 -0600

    Fixed dropdowns and added higher padding cap

--- addons/custom-block-shape/userscript.js
+++ addons/custom-block-shape/userscript.js
@@ -158,20 +158,7 @@ export default async function ({ addon, global, console }) {
       var originalDropdownObject = BlocklyInstance.FieldDropdown.prototype.positionArrow;
       BlocklyInstance.FieldDropdown.prototype.positionArrow = function (x) {
         this.arrowY_ = 11 * multiplier;
-        var addedWidth = 0;
-        if (this.sourceBlock_.RTL) {
-          this.arrowX_ = this.arrowSize_ - BlockSvg.DROPDOWN_ARROW_PADDING;
-          addedWidth = this.arrowSize_ + BlockSvg.DROPDOWN_ARROW_PADDING;
-        } else {
-          this.arrowX_ = x + BlockSvg.DROPDOWN_ARROW_PADDING / 2;
-          addedWidth = this.arrowSize_ + BlockSvg.DROPDOWN_ARROW_PADDING;
-        }
-        if (this.box_) {
-          // Bump positioning to the right for a box-type drop-down.
-          this.arrowX_ += BlockSvg.BOX_FIELD_PADDING;
-        }
-        this.arrow_.setAttribute("transform", "translate(" + this.arrowX_ + "," + this.arrowY_ + ")");
-        return originalDropdownObject.call(this);
+        return originalDropdownObject.call(this, x);
       };
 
       //Corner setting
