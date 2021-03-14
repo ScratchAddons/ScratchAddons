59425b3b7419b2a0ad7b84fa7f2a54735aef948f
RedGuy12 <RedGuy12@users.noreply.github.com>
2021-03-14 15:41:31 +0000

    style: format code

--- addon-api/common/Storage.js
+++ addon-api/common/Storage.js
@@ -1,13 +1,13 @@
-import Storage from "../common/Storage.js"
+import Storage from "../common/Storage.js";
 /**
  * Manages storage.
  * @extends Storage
  */
 export default class usStorage extends Storage {
-  constructor (addonObject) {
-    super()
-    this._addonId = addonObject.self.id
-    this._extentionId = scratchAddons.eventTargets.self[0].lib.split("/")[2]
+  constructor(addonObject) {
+    super();
+    this._addonId = addonObject.self.id;
+    this._extentionId = scratchAddons.eventTargets.self[0].lib.split("/")[2];
   }
   /**
    * Gets a stored string.
@@ -19,12 +19,12 @@ export default class usStorage extends Storage {
    */
   get(storedID, mode) {
     if (typeof storedID !== "string") {
-      throw new Error("Scratch Addons exception: stored ID must be a string")
+      throw new Error("Scratch Addons exception: stored ID must be a string");
     }
     if (!["sync", "local", "cookie"].includes(mode)) {
-      throw new Error("Scratch Addons exception: mode must be one of: sync, local, or cookie")
+      throw new Error("Scratch Addons exception: mode must be one of: sync, local, or cookie");
     }
-    return scratchAddons.globalState.addonStorage[mode][this._addonId]?.[storedID]
+    return scratchAddons.globalState.addonStorage[mode][this._addonId]?.[storedID];
     // ?. returns null if the previous value is null, otherwise it will continue evaluating as if it wasn't there. if the next charecter is not a [, then it adds a dot.
     //like this: object?.key would be treated as object.key if object was not null. object?.["key"] would be object["key"] if object was not null.)
   }
@@ -41,24 +41,24 @@ export default class usStorage extends Storage {
    */
   async set(storedID, value, mode) {
     if (typeof storedID !== "string") {
-      throw new Error("Scratch Addons exception: stored ID must be a string")
+      throw new Error("Scratch Addons exception: stored ID must be a string");
     }
     if (storedID.length == 0) {
-      throw new Error("Scratch Addons exception: stored ID is empty")
+      throw new Error("Scratch Addons exception: stored ID is empty");
     }
     if (storedID.indexOf("/") > -1) {
-      throw new Error("Scratch Addons exception: stored ID can not have /s")
+      throw new Error("Scratch Addons exception: stored ID can not have /s");
     }
     if (!["sync", "local", "cookie"].includes(mode)) {
-      throw new Error("Scratch Addons exception: mode must be one of: sync, local, or cookie")
+      throw new Error("Scratch Addons exception: mode must be one of: sync, local, or cookie");
     }
     if (chrome.storage) {
-      import {setStorage} from '../../background/get-addon-storage.js'
+      import { setStorage } from "../../background/get-addon-storage.js";
       return await setStorage({
         addonStorageID: this._addonId + "/" + storedID,
         addonStorageValue: value,
         addonStorageMode: mode,
-      })
+      });
     } else {
       return await new Promise((resolve) => {
         chrome.runtime.sendMessage(
@@ -69,14 +69,14 @@ export default class usStorage extends Storage {
             addonStorageMode: mode,
           },
           resolve
-        )
-      })
+        );
+      });
     }
   }
   /**
    * @private
    */
   get _eventTargetKey() {
-    return "storage"
+    return "storage";
   }
 }
--- background/get-addon-storage.js
+++ background/get-addon-storage.js
@@ -1,10 +1,10 @@
-const promisify = (callbackFn) => (...args) => new Promise((resolve) => callbackFn(...args, resolve))
+const promisify = (callbackFn) => (...args) => new Promise((resolve) => callbackFn(...args, resolve));
 const storageGet = async (mode) => {
-  return await promisify(chrome.storage[mode].get.bind(chrome.storage[mode]))(null)
-}
+  return await promisify(chrome.storage[mode].get.bind(chrome.storage[mode]))(null);
+};
 const storageSet = async (id, value, mode) => {
-  return await promisify(chrome.storage[mode].set.bind(chrome.storage[mode]))(Object.fromEntries([[id, value]]))
-}
+  return await promisify(chrome.storage[mode].set.bind(chrome.storage[mode]))(Object.fromEntries([[id, value]]));
+};
 const cookieGet = async () => {
   return Object.fromEntries(
     (
@@ -12,8 +12,8 @@ const cookieGet = async () => {
         url: "https://scratch.mit.edu",
       })
     ).map((c) => [c.name, c.value])
-  )
-}
+  );
+};
 const cookieSet = async (name, value) => {
   return await promisify(chrome.cookies.set.bind(chrome.cookies))({
     url: "https://scratch.mit.edu",
@@ -21,52 +21,52 @@ const cookieSet = async (name, value) => {
     secure: false,
     expirationDate: 2147483647,
     value: value,
-  })
-}
+  });
+};
 // Initialize scratchAddons.globalState.addonStorage from chrome.storage
 // get from chrome.storage.sync
-scratchAddons.globalState.addonStorage.sync = init(await storageGet("sync"))
+scratchAddons.globalState.addonStorage.sync = init(await storageGet("sync"));
 
 // get from chrome.storage.local
-scratchAddons.globalState.addonStorage.local = init(await storageGet("local"))
+scratchAddons.globalState.addonStorage.local = init(await storageGet("local"));
 
 // get from cookies
-scratchAddons.globalState.addonStorage.cookie = init(await cookieGet())
+scratchAddons.globalState.addonStorage.cookie = init(await cookieGet());
 
 // Setting values
 chrome.runtime.onMessageExternal.addListener(async (request, _, sendResponse) => {
-  sendResponse(await setStorage(request, sendResponse))
-})
+  sendResponse(await setStorage(request, sendResponse));
+});
 
 export async function setStorage(request) {
   // the stuff that matters: set the value
   // it needs to be here because the event handler does not have access to chrome.storage in userscripts
-  var id = request.addonStorageID
-  var mode = request.addonStorageMode
-  var value = request.addonStorageValue
-  var key = id.split("/") // seperate key into stored ID and addon ID
-  var storage = scratchAddons.globalState.addonStorage[mode]
-  storage[key[0]] ?? (storage[key[0]] = {}) // just in case the addon has not had any other stored values before
-  storage[key[0]][key[1]] = value // set in scratchAddons.globalStagte.addonStorage
-  scratchAddons.globalState.addonStorage[mode] = storage
-  await(mode == "cookie" ? cookieSet(id, value) : storageSet(id, value, mode)) // set it in chrome.storage/document.cookie
+  var id = request.addonStorageID;
+  var mode = request.addonStorageMode;
+  var value = request.addonStorageValue;
+  var key = id.split("/"); // seperate key into stored ID and addon ID
+  var storage = scratchAddons.globalState.addonStorage[mode];
+  storage[key[0]] ?? (storage[key[0]] = {}); // just in case the addon has not had any other stored values before
+  storage[key[0]][key[1]] = value; // set in scratchAddons.globalStagte.addonStorage
+  scratchAddons.globalState.addonStorage[mode] = storage;
+  await (mode == "cookie" ? cookieSet(id, value) : storageSet(id, value, mode)); // set it in chrome.storage/document.cookie
   return {
     name: id,
     value: value,
     mode: mode,
-  }
+  };
 }
 
 function init(storage) {
   // turn storage from an object to an array of arrays
-  var key
-  var addonStorage = {}
+  var key;
+  var addonStorage = {};
   for (let i = 0; i < Object.keys(storage).length; i++) {
-    key = Object.keys(storage)[i].split("/")
+    key = Object.keys(storage)[i].split("/");
     if (key.length == 2) {
-      addonStorage[key[0]] ?? (addonStorage[key[0]] = {}) // ?? returns the preceeding value if it is not null and the following value if it is. so in this case if addonStorage[key[0]] is null, it will execute addonStorage[key[0]] = {} and return null, otherwise the value of addonStorage[key[0]]
-      addonStorage[key[0]][key[1]] = Object.values(storage)[i]
+      addonStorage[key[0]] ?? (addonStorage[key[0]] = {}); // ?? returns the preceeding value if it is not null and the following value if it is. so in this case if addonStorage[key[0]] is null, it will execute addonStorage[key[0]] = {} and return null, otherwise the value of addonStorage[key[0]]
+      addonStorage[key[0]][key[1]] = Object.values(storage)[i];
     }
   }
-  return addonStorage
+  return addonStorage;
 }
