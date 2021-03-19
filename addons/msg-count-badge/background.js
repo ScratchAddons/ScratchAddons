5387b151e1a16f87ec0d844cd20a0fbe94f24fea
wgyt <github@wgyt.tk>
2021-03-18 20:14:17 -0400

    Update background.js

--- addons/msg-count-badge/background.js
+++ addons/msg-count-badge/background.js
@@ -2,6 +2,7 @@ export default async function ({ addon, global, console, setTimeout, setInterval
   const setBadge = async () => {
     const msgCount = await addon.account.getMsgCount();
     addon.badge.text = msgCount;
+    addon.badge.color = '#CC621E'
   };
   setBadge();
   setInterval(setBadge, 2500);
