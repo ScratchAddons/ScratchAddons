9c00f4f6dd0d73d892344c886a6524ab0759fa9a
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-03-22 18:25:22 +0000

    style: format code

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,13 @@
 import { message, danger, warn } from "danger";
 const modified = danger.git.modified_files;
-if (!danger.github.pr.title.includes('Translation update:')) {
-const modifiedMD = danger.git.modified_files.join("- ");
-message("Changed Files in this PR: \n - " + modifiedMD);
-const modifiedLibFiles = modified.filter((p) => p.includes("libraries/"));
+if (!danger.github.pr.title.includes("Translation update:")) {
+  const modifiedMD = danger.git.modified_files.join("- ");
+  message("Changed Files in this PR: \n - " + modifiedMD);
+  const modifiedLibFiles = modified.filter((p) => p.includes("libraries/"));
 
-if (modifiedLibFiles) {
-  if (!modifiedLibFiles.includes("CREDITS.md") || !modifiedLibFiles.includes("license-info.json")) {
-    warn("There are library changes, but you didn't change CREDITS.md or license-info.json. Do they need changing?");
+  if (modifiedLibFiles) {
+    if (!modifiedLibFiles.includes("CREDITS.md") || !modifiedLibFiles.includes("license-info.json")) {
+      warn("There are library changes, but you didn't change CREDITS.md or license-info.json. Do they need changing?");
+    }
   }
-}
 }
