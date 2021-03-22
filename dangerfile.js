6a846de1a20006aa21b2f4dc68a44a80d2ece890
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-03-22 14:24:48 -0400

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,6 @@
 import { message, danger, warn } from "danger";
 const modified = danger.git.modified_files;
-
+if (!danger.github.pr.title.includes('Translation update:')) {
 const modifiedMD = danger.git.modified_files.join("- ");
 message("Changed Files in this PR: \n - " + modifiedMD);
 const modifiedLibFiles = modified.filter((p) => p.includes("libraries/"));
@@ -9,4 +9,5 @@ if (modifiedLibFiles) {
   if (!modifiedLibFiles.includes("CREDITS.md") || !modifiedLibFiles.includes("license-info.json")) {
     warn("There are library changes, but you didn't change CREDITS.md or license-info.json. Do they need changing?");
   }
+}
 }
