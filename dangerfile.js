52a4c07c18639c8cddc2411690c6e5433b9bba58
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-03-22 22:01:55 +0000

    style: format code

--- dangerfile.js
+++ dangerfile.js
@@ -1,24 +1,26 @@
 import { message, danger, warn } from "danger";
-const prettier = require('prettier')
-const fs = require('fs')
+const prettier = require("prettier");
+const fs = require("fs");
 var path = require("path");
 
 const modified = danger.git.modified_files;
 let badFiles = 0;
 for (var file of modified) {
-resolved = path.resolve(file)
- let readFile = fs.readFileSync(resolved,{encoding:'utf8', flag:'r'})
-let info = prettier.getFileInfo.sync(resolved)
-console.log(resolved)
-if (info.ignored) return
-   const extname = path.extname(file);
-   const filePath = path.basename(file, extname);
-   if (extname == '.svg') return
-  let check = prettier.check(readFile, {filepath: filePath + extname})
-  if (!check) badFiles++
+  resolved = path.resolve(file);
+  let readFile = fs.readFileSync(resolved, { encoding: "utf8", flag: "r" });
+  let info = prettier.getFileInfo.sync(resolved);
+  console.log(resolved);
+  if (info.ignored) return;
+  const extname = path.extname(file);
+  const filePath = path.basename(file, extname);
+  if (extname == ".svg") return;
+  let check = prettier.check(readFile, { filepath: filePath + extname });
+  if (!check) badFiles++;
 }
 if (badFiles > 0) {
-  warn(`${badFiles} need to be formatted with Prettier. To enable it, go to https://github.com/<yourusername>/ScratchAddons/actions and enable it.`)
+  warn(
+    `${badFiles} need to be formatted with Prettier. To enable it, go to https://github.com/<yourusername>/ScratchAddons/actions and enable it.`
+  );
 }
 if (!danger.github.pr.title.includes("Translation update:")) {
   const modifiedMD = danger.git.modified_files.join("- ");
