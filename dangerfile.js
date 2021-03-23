930735ecd0e655b6459b60ec2ee6a1c9117a9e2a
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-03-23 09:24:36 -0400

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -2,8 +2,9 @@ import { message, danger, warn, markdown } from "danger";
 const prettier = require("prettier");
 const fs = require("fs");
 var path = require("path");
-message(danger.github.utils.fileLinks(["/libraries"]));
-//message("Thanks for PRing! Someone will be along to check it shortly!");
+if (danger.github.pr.author_association == "FIRST_TIME_CONTRIBUTOR") {
+  message("Since this is your first contribution, here are some links while you wait for a review.\n[Our contributing guidelines](https://github.com/ScratchAddons/ScratchAddons/blob/master/CONTRIBUTING.md)")
+}
 const modified = danger.git.modified_files;
 let badFiles = [];
 for (var file of modified) {
@@ -16,11 +17,11 @@ for (var file of modified) {
   const filePath = path.basename(file, extname);
   if (extname == ".svg") return;
   let check = prettier.check(readFile, { filepath: filePath + extname });
-  if (!check) badFiles.push("`" + file + "`");
+  if (!check) badFiles.push(file);
 }
 if (badFiles.length > 0) {
   warn(
-    `${badFiles} need to be formatted with Prettier. To enable it, go to https://github.com/<yourusername>/ScratchAddons/actions and enable it.`
+    `${danger.github.utils.fileLinks(badfiles)} need to be formatted with Prettier. To enable it, go to https://github.com/<yourusername>/ScratchAddons/actions and enable it.`
   );
 }
 if (!danger.github.pr.title.includes("Translation update:")) {
