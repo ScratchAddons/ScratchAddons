dc6c16df5548e01d36cb42802cb3b11ead36fed8
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-03-30 14:54:22 -0600

    Update userscript.js

--- addons/custom-zoom/userscript.js
+++ addons/custom-zoom/userscript.js
@@ -1,55 +1,59 @@
 export default async function ({ addon, global, console }) {
-  function setZoom(e) {
-    //Function to set the custom zoom parameters
-    if (!window.Blockly) {
-      //Fallback to prevent errors, if Blockly is not defined a temporary object is created
-      ///Use var instead of let to allow the variable to be used outside of this code block
-      var Blockly = {
-        getMainWorkspace: function() {
-          return {
-            options: {
-              zoomOptions: {}
-            }
-          }; //Return fake options object
-        },
-      };
-    };
-    if (e.detail.newUrl.includes("/projects/") {
-      //Set the zoom parameters
-      Blockly.getMainWorkspace().options.zoomOptions.maxScale = addon.settings.get("maxZoom") / 100;
-      Blockly.getMainWorkspace().options.zoomOptions.minScale = addon.settings.get("minZoom") / 100;
-      Blockly.getMainWorkspace().options.zoomOptions.startScale = addon.settings.get("startZoom") / 100;
-      Blockly.getMainWorkspace().options.zoomOptions.scaleSpeed = 1.2 * (addon.settings.get("zoomSpeed") / 100);
+    function setZoom(e) {
+        //Function to set the custom zoom parameters
+        if (!window.Blockly) {
+            //Fallback to prevent errors, if Blockly is not defined a temporary object is created
+            ///Use var instead of let to allow the variable to be used outside of this code block
+            var Blockly = {
+                getMainWorkspace: function () {
+                    return {
+                        options: {
+                            zoomOptions: {},
+                        },
+                    }; //Return fake options object
+                },
+            };
+        }
+        if (e.detail.newUrl.includes("/editor")) {
+            //Set the zoom parameters
+            Blockly.getMainWorkspace().options.zoomOptions.maxScale = addon.settings.get("maxZoom") / 100;
+            Blockly.getMainWorkspace().options.zoomOptions.minScale = addon.settings.get("minZoom") / 100;
+            Blockly.getMainWorkspace().options.zoomOptions.startScale = addon.settings.get("startZoom") / 100;
+            Blockly.getMainWorkspace().options.zoomOptions.scaleSpeed = 1.2 * (addon.settings.get("zoomSpeed") / 100);
+        }
+        if (!defaultTranslate) {
+            if (!Blockly.getMainWorkspace().zoomControls_) return;
+            defaultTranslate = Blockly.getMainWorkspace().zoomControls_.attributes.transform.value;
+        }
+        if (!defaultRect) {
+            if (!Blockly.getMainWorkspace().zoomControls_) return;
+            defaultRect = Blockly.getMainWorkspace().zoomControls_.svgGroup_.getBoundingClientRect();
+        }
+        try {
+            document.removeEventListener("mousemove", onMouseMove);
+        } catch {}
+        if (e.detail.newUrl.includes("/editor")) document.addEventListener("mousemove", onMouseMove);
     }
-    if (!defaultTranslate) {
-      if (!Blockly.getMainWorkspace().zoomControls_) return
-      defaultTranslate = Blockly.getMainWorkspace().zoomControls_.attributes.transform.value
+    function hideShow(inArea = false, speed = "default") {
+        let speeds = {
+            none: "0",
+            short: "0.25",
+            default: "0.5",
+            long: "1",
+        };
+        if (!window.Blockly) return;
+        let controls = Blockly.getMainWorkspace().zoomControls_.svgGroup_;
+        controls.style.transition = `${speeds[speed]}s ease-in-out`;
+        if (inArea) {
+            controls.attributes.transform.value = "translate(600,475)";
+        } else {
+            controls.attributes.transform.value = defaultTranslate || "";
+        }
     }
-    if (!defaultRect) {
-      if (!Blockly.getMainWorkspace().zoomControls_) return
-      defaultRect = Blockly.getMainWorkspace().zoomControls_.svgGroup_.getBoundingClientRect()
+    function onMouseMove(e) {
+        hideShow(e.x >= defaultRext.left && e.x <= defaultRext.right && e.y >= defaultRext.top && e.y <= defaultRext.bottom, addon.settings.get("speed"));
     }
-  }
-  function hideShow(inArea = false, speed = "default") {
-    let speeds = {
-      none: "0",
-      short: "0.25",
-      default: "0.5",
-      long: "1"
-    };
-    if (!window.Blockly) return
-    let controls = Blockly.getMainWorkspace().zoomControls_.svgGroup_;
-    controls.style.transition = `${speeds[speed]}s ease-in-out`;
-    if (inArea) {
-      controls.attributes.transform.value = "translate(600,475)"
-    } else {
-      controls.attributes.transform.value = defaultTranslate || ""
-    }
-  }
-  function onMouseMove(e) {
-    
-  }
-  let defaultTranslate, defaultRect;
-  setZoom({ detail: { newUrl: window.location.href } });
-  addon.tab.addEventListener("urlChange", setZoom);
+    let defaultTranslate, defaultRect;
+    setZoom({ detail: { newUrl: window.location.href } });
+    addon.tab.addEventListener("urlChange", setZoom);
 }
