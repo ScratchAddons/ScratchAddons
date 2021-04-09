725af32eff54da3a52642e3f1afc7f7549b01f63
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2021-04-09 15:07:45 +0100

    Use prototype, + instead of parseInt

--- addons/initialise-sprite-position/userscript.js
+++ addons/initialise-sprite-position/userscript.js
@@ -1,20 +1,20 @@
 export default async function ({ addon }) {
   let vm = addon.tab.traps.vm;
-  let oldAddSprite = vm.addSprite;
+  let oldAddSprite = vm.constructor.prototype.addSprite;
   
-  vm.addSprite = function (input) {
+  vm.constructor.prototype.addSprite = function (input) {
     let spriteObj;
     spriteObj = JSON.parse(input);
     let isEmpty = spriteObj.costumes[0].baseLayerMD5 === "cd21514d0531fdffb22204e0ec5ed84a.svg";
       
     if (isEmpty || !addon.settings.get("library")) {
       if (spriteObj.scratchX) {
-        spriteObj.scratchX = parseInt(addon.settings.get("x"));
-        spriteObj.scratchY = parseInt(addon.settings.get("y"));
+        spriteObj.scratchX = +addon.settings.get("x");
+        spriteObj.scratchY = +addon.settings.get("y");
       }
       if (spriteObj.x) {
-        spriteObj.x = parseInt(addon.settings.get("x"));
-        spriteObj.y = parseInt(addon.settings.get("y"));
+        spriteObj.x = +addon.settings.get("x");
+        spriteObj.y = +addon.settings.get("y");
       }
     }
     oldAddSprite.call(this, JSON.stringify(spriteObj));
