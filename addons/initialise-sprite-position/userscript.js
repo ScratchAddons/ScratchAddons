32dff0dd26c39e915895a6a33edcde9a09e2d09b
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2021-04-09 13:56:25 +0100

    New addon: initialise sprite position

---
+++ addons/initialise-sprite-position/userscript.js
@@ -0,0 +1,22 @@
+export default async function ({ addon }) {
+  let vm = addon.tab.traps.vm;
+  let oldAddSprite = vm.addSprite;
+  
+  vm.addSprite = function (input) {
+    let spriteObj;
+    spriteObj = JSON.parse(input);
+    let isEmpty = spriteObj.costumes[0].baseLayerMD5 === "cd21514d0531fdffb22204e0ec5ed84a.svg";
+      
+    if (isEmpty || !Boolean(addon.settings.get("library"))) {
+      if (spriteObj.scratchX) {
+        spriteObj.scratchX = parseInt(addon.settings.get("x"));
+        spriteObj.scratchY = parseInt(addon.settings.get("y"));
+      }
+      if (spriteObj.x) {
+        spriteObj.x = parseInt(addon.settings.get("x"));
+        spriteObj.y = parseInt(addon.settings.get("y"));
+      }
+    }
+    oldAddSprite.call(this, JSON.stringify(spriteObj));
+  };
+}
