891e87387ff8f41c2b1dc021b04788a864100231
8bitjake <8bitjake@users.noreply.github.com>
2021-04-10 17:19:37 +0000

    style: format code

--- addons/msg-title/background.js
+++ addons/msg-title/background.js
@@ -1,7 +1,7 @@
 export default async function ({ addon, msg, console }) {
   var oldTitle = document.title;
   setInterval(async () => {
-    let count = await addon.account.getMsgCount()
-    document.title = count === 0 ? oldTitle : `(${count}) ${oldTitle}`
-  }, 1000)
-};
+    let count = await addon.account.getMsgCount();
+    document.title = count === 0 ? oldTitle : `(${count}) ${oldTitle}`;
+  }, 1000);
+}
