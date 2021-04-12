85d7bd90e27df407649eda8011e08eed50110926
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-04-11 21:57:22 -0400

    Fix Stuff

--- addons/shape-opacity/userscript.js
+++ addons/shape-opacity/userscript.js
@@ -2,41 +2,34 @@ export default async function ({ addon, global, console, msg }) {
   let appended = false;
   const clss = addon.tab.scratchClass;
 
+  const inputGroup = document.createElement("div");
   const labelContainer = document.createElement("label");
-  labelContainer.className = clss("label_input-group");
+  inputGroup.append(labelContainer);
 
   const opacityText = document.createElement("span");
-  opacityText.className = clss("label_input-label");
   opacityText.innerText = msg("opacity");
 
   const opacityInput = document.createElement("input");
   opacityInput.min = 0;
   opacityInput.max = 100;
   opacityInput.type = "number";
+  opacityInput.value = 100;
 
   labelContainer.append(opacityText, opacityInput);
 
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", ({ detail }) => {
     const { action } = detail;
-    if (action.type === "scratch-paint/select/CHANGE_SELECTED_ITEMS") {
-      if (action.selectedItems.length === 0) {
-        labelContainer.style.display = "none";
-      } else {
+    console.log(action);
+    switch (action.type) {
+      case "scratch-paint/select/CHANGE_SELECTED_ITEMS":
+        opacityInput.disabled = true;
+        if (action.selectedItems.length === 0) return;
         // Scratch returns undefined for bitmapMode when in bitmap mode...
         if (action.bitmapMode !== false) return;
-        labelContainer.style.display = "";
-        if (!appended) {
-          appended = true;
-          const appendRow = document.querySelector("[class*=paint-editor_mod-mode-tools]");
-          appendRow.append(labelContainer);
 
-          // There appears to be 2 classes with the same name but with different discriminators.
-          // scratchClass gives us the wrong one, so in order to get the right one, we must use an element already appended.
-          opacityInput.className = document.querySelector(
-            "[class*=paint-editor_mod-labeled-icon-height] input"
-          ).className;
-        }
+        opacityInput.disabled = false;
+
         if (action.selectedItems.length === 1) {
           opacityInput.value = action.selectedItems[0].opacity * 100;
         } else {
@@ -47,7 +40,7 @@ export default async function ({ addon, global, console, msg }) {
           if (values.size === 1) {
             opacityInput.value = values.values().next().value * 100;
           } else {
-            opacityInput.value = 0;
+            opacityInput.value = 100; // Default value
           }
         }
         opacityInput.oninput = () => {
@@ -55,7 +48,21 @@ export default async function ({ addon, global, console, msg }) {
             selected.opacity = opacityInput.value / 100;
           }
         };
-      }
+        break;
     }
   });
+
+  while (true) {
+    const appendRow = await addon.tab.waitForElement("[class*=paint-editor_mod-labeled-icon-height]", {
+      markAsSeen: true,
+    });
+    appendRow.append(inputGroup);
+
+    // scratchClass is bad.
+    const row = document.querySelector("[class*=paint-editor_mod-labeled-icon-height]");
+    opacityInput.className = row.querySelector("[class^=input_input-form]").className;
+    opacityText.className = row.querySelector("[class^=label_input-label]").className;
+    inputGroup.className = row.querySelector("[class*=input-group_input-group]").className;
+    labelContainer.className = row.querySelector("[class*=label_input-group]").className;
+  }
 }
