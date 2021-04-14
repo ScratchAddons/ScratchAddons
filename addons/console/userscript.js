6164eeb77ca857d333ebf818171438d4c5117776
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-04-14 10:51:04 -0400

    goToBlock when a log is clicked

--- addons/console/userscript.js
+++ addons/console/userscript.js
@@ -17,11 +17,79 @@ export default async function ({ addon, global, console, msg }) {
 
   let injected;
   let debug;
+  const goToBlock = (blockId) => {
+    const offsetX = 32,
+      offsetY = 32;
+    const block = workspace.getBlockById(blockId);
+    if (!block) return;
+
+    // Copied from devtools. If it's code get's improved for this function, bring those changes here too.
+    let root = block.getRootBlock();
+
+    let base = block;
+    while (base.getOutputShape() && base.getSurroundParent()) {
+      base = base.getSurroundParent();
+    }
+
+    let ePos = base.getRelativeToSurfaceXY(), // Align with the top of the block
+      rPos = root.getRelativeToSurfaceXY(), // Align with the left of the block 'stack'
+      eSiz = block.getHeightWidth(),
+      scale = workspace.scale,
+      // x = (ePos.x + (workspace.RTL ? -1 : 1) * eSiz.width / 2) * scale,
+      x = rPos.x * scale,
+      y = ePos.y * scale,
+      xx = block.width + x, // Turns out they have their x & y stored locally, and they are the actual size rather than scaled or including children...
+      yy = block.height + y,
+      // xx = eSiz.width * scale + x,
+      // yy = eSiz.height * scale + y,
+
+      s = workspace.getMetrics();
+    if (
+      x < s.viewLeft + offsetX - 4 ||
+      xx > s.viewLeft + s.viewWidth ||
+      y < s.viewTop + offsetY - 4 ||
+      yy > s.viewTop + s.viewHeight
+    ) {
+      // sx = s.contentLeft + s.viewWidth / 2 - x,
+      let sx = x - s.contentLeft - offsetX,
+        // sy = s.contentTop - y + Math.max(Math.min(32, 32 * scale), (s.viewHeight - yh) / 2);
+        sy = y - s.contentTop - offsetY;
+
+      // workspace.hideChaff(),
+      workspace.scrollbar.set(sx, sy);
+
+      // Flashing
+      const myFlash = { block: null, timerID: null, colour: null };
+      if (myFlash.timerID > 0) {
+        clearTimeout(myFlash.timerID);
+        myFlash.block.setColour(myFlash.colour);
+      }
+
+      let count = 4;
+      let flashOn = true;
+      myFlash.colour = block.getColour();
+      myFlash.block = block;
+
+      function _flash() {
+        myFlash.block.svgPath_.style.fill = flashOn ? "#ffff80" : myFlash.colour;
+        flashOn = !flashOn;
+        count--;
+        if (count > 0) {
+          myFlash.timerID = setTimeout(_flash, 200);
+        } else {
+          myFlash.timerID = 0;
+        }
+      }
+
+      _flash();
+    }
+  };
   const addItem = (thread) => {
     let logs = document.querySelector(".debug > .logs");
     let div = document.createElement("div");
     div.innerText = thread.stackFrames[0].params.text;
     div.classList = `log ${addon.tab.scratchClass("sprite-info_sprite-info")}`;
+    div.onclick = () => goToBlock(thread.topBlock);
     logs.appendChild(div);
     console.log(thread.stackFrames[0].params.text);
 
