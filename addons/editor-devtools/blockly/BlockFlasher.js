ScratchAddons/ScratchAddons

commit b7c63009b771da62f0f491fc8593c3097730a4bb

Fix "custom Colors addon makes devtools highlighting not work" (#1563)
The Blockly method just edited the `fill` attribute, which is overridden by the stylesheet. This way edits the `style` attribute, which overrides everything

addons/editor-devtools/blockly/BlockFlasher.js
@@ -22,7 +22,7 @@ export default class BlockFlasher {
      * @private
      */
     function _flash() {
-      myFlash.block.setColour(flashOn ? "#ffff80" : myFlash.colour);
+      myFlash.block.svgPath_.style.fill = flashOn ? "#ffff80" : myFlash.colour;
       flashOn = !flashOn;
       count--;
       if (count > 0) {

commit f64c872cf92ce45b9f73f38cff5748eb38dc807a

Fix for block flashing bug introduced by #1563 (Fix "custom Colors addon makes devtools highlighting not work")

addons/editor-devtools/blockly/BlockFlasher.js
@@ -9,7 +9,7 @@ export default class BlockFlasher {
   static flash(block) {
     if (myFlash.timerID > 0) {
       clearTimeout(myFlash.timerID);
-      myFlash.block.setColour(myFlash.colour);
+      myFlash.block.svgPath_.style.fill = myFlash.colour;
     }
 
     let count = 4;