1b07a05601fa0d3796a03c664f00e9807808a542
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-04-14 14:25:40 +0000

    style: format code

--- addons/console/userscript.js
+++ addons/console/userscript.js
@@ -1,6 +1,6 @@
 export default async function ({ addon, global, console, msg }) {
   const img = document.createElement("img");
-  img.className = `debug-btn ${addon.tab.scratchClass('button_outlined-button')}`;
+  img.className = `debug-btn ${addon.tab.scratchClass("button_outlined-button")}`;
   img.src = addon.self.dir + "/debug.svg";
   img.draggable = false;
   img.title = msg("debug");
