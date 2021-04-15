d7f865a3c32691676064eb95feee211cbc128ba6
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-04-15 09:22:36 -0400

    use new api

--- addons/console/userscript.js
+++ addons/console/userscript.js
@@ -8,12 +8,10 @@ export default async function ({ addon, global, console, msg }) {
   img.addEventListener("click", () => showConsole());
 
   const vm = addon.tab.traps.vm;
-  let blockColorOverrides = {};
-  blockColorOverrides["log %s"] = {
-    color: "#43cfca",
-    secondaryColor: "#3aa8a4",
-    tertiaryColor: "#3aa8a4",
-  };
+  addon.tab.addBlock("log %s", ["content"], ({ content }, target) => {
+    console.log(content, target);
+    addItem(content);
+  });
 
   let injected;
   let debug;
@@ -84,35 +82,31 @@ export default async function ({ addon, global, console, msg }) {
       _flash();
     }
   };
-  const addItem = (thread) => {
-    for (let frame of thread.stackFrames) {
-      if (!frame.isLoop) {
-        let logs = document.querySelector(".debug > .logs");
-        let div = document.createElement("div");
-        div.innerText = frame.params.text;
-        div.classList = `log ${addon.tab.scratchClass("sprite-info_sprite-info")}`;
-        logs.appendChild(div);
+  const addItem = (content) => {
+    let logs = document.querySelector(".debug > .logs");
+    let div = document.createElement("div");
+    div.innerText = content;
+    div.classList = `log ${addon.tab.scratchClass("sprite-info_sprite-info")}`;
+    logs.appendChild(div);
 
-        const blockInputs = thread.blockContainer._cache.inputs[thread.topBlock];
-        const inputBlock = workspace.getBlockById(Object.values(blockInputs)[0].block);
-        if (inputBlock.type === "data_variable") {
-          let varBlock;
-          for (const variable of workspace.getAllVariables()) {
-            const block = workspace.getVariableUsesById(variable.id_).find((vari) => vari.id === inputBlock.id);
-            if (block) varBlock = variable;
-          }
-          const varSpan = document.createElement("span");
-          varSpan.innerText = varBlock.name;
-          varSpan.className = "console-variable";
-          div.prepend(varSpan);
-        }
-        let link = document.createElement("a");
-        link.innerText = "Go to";
-
-        link.addEventListener("click", () => goToBlock(thread.topBlock));
-        div.appendChild(link);
+    const blockInputs = thread.blockContainer._cache.inputs[thread.topBlock];
+    const inputBlock = workspace.getBlockById(Object.values(blockInputs)[0].block);
+    if (inputBlock.type === "data_variable") {
+      let varBlock;
+      for (const variable of workspace.getAllVariables()) {
+        const block = workspace.getVariableUsesById(variable.id_).find((vari) => vari.id === inputBlock.id);
+        if (block) varBlock = variable;
       }
+      const varSpan = document.createElement("span");
+      varSpan.innerText = varBlock.name;
+      varSpan.className = "console-variable";
+      div.prepend(varSpan);
     }
+    let link = document.createElement("a");
+    link.innerText = "Go to";
+
+    link.addEventListener("click", () => goToBlock(thread.topBlock));
+    div.appendChild(link);
   };
   const addConsole = () => {
     document.querySelector("body").insertAdjacentHTML(
@@ -168,118 +162,118 @@ export default async function ({ addon, global, console, msg }) {
     let debug = document.querySelector(".debug");
     debug.style.display = "block";
   };
-  const injectWorkspace = () => {
-    if (injected) {
-      return;
-    }
-    injected = true;
-
-    workspace = Blockly.getMainWorkspace();
-    if (!workspace) throw new Error("expected workspace");
-
-    let BlockSvg = Object.values(Blockly.getMainWorkspace().getFlyout().checkboxes_)[0].block.constructor;
-    let oldUpdateColor = BlockSvg.prototype.updateColour;
-    BlockSvg.prototype.updateColour = function (...a) {
-      if (this.procCode_) {
-        let p = this.procCode_;
-        if (blockColorOverrides[p.trim().toLowerCase()]) {
-          let c = blockColorOverrides[p.trim().toLowerCase()];
-          this.colour_ = c.color;
-          this.colourSecondary_ = c.secondaryColor;
-          this.colourTertiary_ = c.tertiaryColor;
-          let updateChildColors = function updateChildColors() {
-            this.childBlocks_.forEach(
-              ((e) => {
-                e.setColour(e.getColour(), e.getColourSecondary(), this.getColourTertiary());
-              }).bind(this)
-            );
-          }.bind(this);
-          updateChildColors();
-          const oldPush = this.childBlocks_.constructor.prototype.push.bind(this.childBlocks_);
-          this.childBlocks_.push = function (...a) {
-            updateChildColors();
-            return oldPush(...a);
-          };
-        }
-      }
-      return oldUpdateColor.call(this, ...a);
-    };
-
-    const flyout = workspace.getFlyout();
-    if (!flyout) throw new Error("expected flyout");
-
-    if (!vm) throw new Error("expected vm");
-
-    // Each time a new workspace is made, these callbacks are reset, so re-register whenever a flyout is shown.
-    // https://github.com/LLK/scratch-blocks/blob/61f02e4cac0f963abd93013842fe536ef24a0e98/core/flyout_base.js#L469
-    const originalShow = flyout.constructor.prototype.show;
-    flyout.constructor.prototype.show = function (xml) {
-      this.workspace_.registerToolboxCategoryCallback("console", function (e) {
-        return [
-          ...new DOMParser()
-            .parseFromString(
-              `<top>
-  <block type="procedures_call" gap="16"><mutation generateshadows="true" proccode="log %s" argumentids="[&quot;E|XlmQQ}1C:3vH-VY2Q_&quot;]" argumentnames="[&quot;text&quot;]" argumentdefaults="[&quot;&quot;]" warp="false"></mutation></block></top>`,
-              "text/xml"
-            )
-            .querySelectorAll("block"),
-        ];
-      });
-      const oldStepToProcedure = vm.runtime.sequencer.stepToProcedure;
-      vm.runtime.sequencer.stepToProcedure = function (thread, proccode) {
-        if (proccode.trim().toLowerCase() === "log %s") {
-          for (var blockId of thread.stack) {
-            const block = workspace.getBlockById(blockId);
-            if (block?.procCode_ === "log %s") {
-              console.log(block);
-            }
-          }
-        }
-        return oldStepToProcedure.call(this, thread, proccode.text);
-      };
-
-      originalShow.call(this, xml);
-    };
-
-    // We use Scratch's extension category mechanism to replace the data category with our own.
-    // https://github.com/LLK/scratch-gui/blob/ddd2fa06f2afa140a46ec03be91796ded861e65c/src/containers/blocks.jsx#L344
-    // https://github.com/LLK/scratch-gui/blob/2ceab00370ad7bd8ecdf5c490e70fd02152b3e2a/src/lib/make-toolbox-xml.js#L763
-    // https://github.com/LLK/scratch-vm/blob/a0c11d6d8664a4f2d55632e70630d09ec6e9ae28/src/engine/runtime.js#L1381
-    const originalGetBlocksXML = vm.runtime.getBlocksXML;
-    vm.runtime.getBlocksXML = function (target) {
-      const result = originalGetBlocksXML.call(this, target);
-      result.push({
-        id: "console",
-        xml: `
-            <category
-              name="Console"
-              id="console"
-              colour="#43cfca"
-              secondaryColour="#3aa8a4"
-              custom="console">
-            </category>`,
-      });
-      return result;
-    };
-
-    // If editingTarget has not been set yet, we have injected before the editor has loaded and emitWorkspaceUpdate will be called later.
-    // Otherwise, it's possible that the editor has already loaded and updated its toolbox, so force a workspace update.
-    // Workspace updates are slow, so don't do them unless necessary.
-    if (vm.editingTarget) {
-      vm.emitWorkspaceUpdate();
-    }
-  };
+  // const injectWorkspace = () => {
+  //   if (injected) {
+  //     return;
+  //   }
+  //   injected = true;
+  //
+  //   workspace = Blockly.getMainWorkspace();
+  //   if (!workspace) throw new Error("expected workspace");
+  //
+  //   let BlockSvg = Object.values(Blockly.getMainWorkspace().getFlyout().checkboxes_)[0].block.constructor;
+  //   let oldUpdateColor = BlockSvg.prototype.updateColour;
+  //   BlockSvg.prototype.updateColour = function (...a) {
+  //     if (this.procCode_) {
+  //       let p = this.procCode_;
+  //       if (blockColorOverrides[p.trim().toLowerCase()]) {
+  //         let c = blockColorOverrides[p.trim().toLowerCase()];
+  //         this.colour_ = c.color;
+  //         this.colourSecondary_ = c.secondaryColor;
+  //         this.colourTertiary_ = c.tertiaryColor;
+  //         let updateChildColors = function updateChildColors() {
+  //           this.childBlocks_.forEach(
+  //             ((e) => {
+  //               e.setColour(e.getColour(), e.getColourSecondary(), this.getColourTertiary());
+  //             }).bind(this)
+  //           );
+  //         }.bind(this);
+  //         updateChildColors();
+  //         const oldPush = this.childBlocks_.constructor.prototype.push.bind(this.childBlocks_);
+  //         this.childBlocks_.push = function (...a) {
+  //           updateChildColors();
+  //           return oldPush(...a);
+  //         };
+  //       }
+  //     }
+  //     return oldUpdateColor.call(this, ...a);
+  //   };
+  //
+  //   const flyout = workspace.getFlyout();
+  //   if (!flyout) throw new Error("expected flyout");
+  //
+  //   if (!vm) throw new Error("expected vm");
+  //
+  //   // Each time a new workspace is made, these callbacks are reset, so re-register whenever a flyout is shown.
+  //   // https://github.com/LLK/scratch-blocks/blob/61f02e4cac0f963abd93013842fe536ef24a0e98/core/flyout_base.js#L469
+  //   const originalShow = flyout.constructor.prototype.show;
+  //   flyout.constructor.prototype.show = function (xml) {
+  //     this.workspace_.registerToolboxCategoryCallback("console", function (e) {
+  //       return [
+  //         ...new DOMParser()
+  //           .parseFromString(
+  //             `<top>
+  // <block type="procedures_call" gap="16"><mutation generateshadows="true" proccode="log %s" argumentids="[&quot;E|XlmQQ}1C:3vH-VY2Q_&quot;]" argumentnames="[&quot;text&quot;]" argumentdefaults="[&quot;&quot;]" warp="false"></mutation></block></top>`,
+  //             "text/xml"
+  //           )
+  //           .querySelectorAll("block"),
+  //       ];
+  //     });
+  //     const oldStepToProcedure = vm.runtime.sequencer.stepToProcedure;
+  //     vm.runtime.sequencer.stepToProcedure = function (thread, proccode) {
+  //       if (proccode.trim().toLowerCase() === "log %s") {
+  //         for (var blockId of thread.stack) {
+  //           const block = workspace.getBlockById(blockId);
+  //           if (block?.procCode_ === "log %s") {
+  //             console.log(block);
+  //           }
+  //         }
+  //       }
+  //       return oldStepToProcedure.call(this, thread, proccode.text);
+  //     };
+  //
+  //     originalShow.call(this, xml);
+  //   };
+  //
+  //   // We use Scratch's extension category mechanism to replace the data category with our own.
+  //   // https://github.com/LLK/scratch-gui/blob/ddd2fa06f2afa140a46ec03be91796ded861e65c/src/containers/blocks.jsx#L344
+  //   // https://github.com/LLK/scratch-gui/blob/2ceab00370ad7bd8ecdf5c490e70fd02152b3e2a/src/lib/make-toolbox-xml.js#L763
+  //   // https://github.com/LLK/scratch-vm/blob/a0c11d6d8664a4f2d55632e70630d09ec6e9ae28/src/engine/runtime.js#L1381
+  //   const originalGetBlocksXML = vm.runtime.getBlocksXML;
+  //   vm.runtime.getBlocksXML = function (target) {
+  //     const result = originalGetBlocksXML.call(this, target);
+  //     result.push({
+  //       id: "console",
+  //       xml: `
+  //           <category
+  //             name="Console"
+  //             id="console"
+  //             colour="#43cfca"
+  //             secondaryColour="#3aa8a4"
+  //             custom="console">
+  //           </category>`,
+  //     });
+  //     return result;
+  //   };
+  //
+  //   // If editingTarget has not been set yet, we have injected before the editor has loaded and emitWorkspaceUpdate will be called later.
+  //   // Otherwise, it's possible that the editor has already loaded and updated its toolbox, so force a workspace update.
+  //   // Workspace updates are slow, so don't do them unless necessary.
+  //   if (vm.editingTarget) {
+  //     vm.emitWorkspaceUpdate();
+  //   }
+  // };
 
   while (true) {
-    injectWorkspace();
+    //injectWorkspace();
     const button = await addon.tab.waitForElement("[class^='stage-header_stage-size-row']", { markAsSeen: true });
     button.insertAdjacentElement("afterBegin", img);
 
-    while (true) {
-      vm.runtime.targets.forEach(
-        (e) => (e.blocks._cache.procedureParamNames["log %s"] = [["text"], ["E|XlmQQ}1C:3vH-VY2Q_"], [""]])
-      );
-      await new Promise((cb) => requestAnimationFrame((_) => cb()));
-    }
+    // while (true) {
+    //   vm.runtime.targets.forEach(
+    //     (e) => (e.blocks._cache.procedureParamNames["log %s"] = [["text"], ["E|XlmQQ}1C:3vH-VY2Q_"], [""]])
+    //   );
+    //   await new Promise((cb) => requestAnimationFrame((_) => cb()));
+    // }
   }
 }
