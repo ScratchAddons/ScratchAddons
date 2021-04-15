72da072e5bd66a8325b52a3eb55fb61ee9ca8f79
CubeyTheCube <72284516+CubeyTheCube@users.noreply.github.com>
2021-04-15 13:39:13 -0400

    Use waitForElement

--- addons/studio-invite-button/userscript.js
+++ addons/studio-invite-button/userscript.js
@@ -18,45 +18,47 @@ export default async function ({ addon, global, console }) {
       }
     );
   }
+  addon.tab.waitForElement('#description').then((description) => {
   // checks if the user is a manager and the URL is correct
-  if (document.getElementById("description").classList.contains("editable") && urlRegex.test(window.location.href)) {
-    Array.from(document.getElementsByClassName("comment")).forEach((comment) => {
-      const inviteButton = document.createElement("span"); // create the button
-      inviteButton.innerText = "Invite";
-      inviteButton.classList.add("actions");
-      inviteButton.style.visibility = "hidden";
-      inviteButton.style.color = "rgb(157, 157, 157)";
-
-      comment.querySelector(".actions-wrap").appendChild(inviteButton);
-      comment.addEventListener("mouseover", () => {
-        inviteButton.style.visibility = "visible"; // should only be visible when the mouse is hovering over the comment
-      });
-      comment.addEventListener("mouseout", () => {
+    if (description.classList.contains("editable") && urlRegex.test(window.location.href)) {
+      Array.from(document.getElementsByClassName("comment")).forEach((comment) => {
+        const inviteButton = document.createElement("span"); // create the button
+        inviteButton.innerText = "Invite";
+        inviteButton.classList.add("actions");
         inviteButton.style.visibility = "hidden";
-      });
-      const listener = inviteButton.addEventListener("click", () => {
-        inviteToStudio(comment.querySelector(".name").textContent.trim())
-          .then((resp) => resp.text())
-          .then((resp) => {
-            try {
-              const parsed = JSON.parse(resp);
-              // handle the different possible responses
-              if (parsed.status === "success") {
-                inviteButton.innerHTML = "Invited!";
-              } else if (parsed.message.endsWith("is already a curator of this studio")) {
-                inviteButton.innerHTML = "This user is already a curator";
-              } else {
-                throw Error(0);
+        inviteButton.style.color = "rgb(157, 157, 157)";
+
+        comment.querySelector(".actions-wrap").appendChild(inviteButton);
+        comment.addEventListener("mouseover", () => {
+          inviteButton.style.visibility = "visible"; // should only be visible when the mouse is hovering over the comment
+        });
+        comment.addEventListener("mouseout", () => {
+          inviteButton.style.visibility = "hidden";
+        });
+        const listener = inviteButton.addEventListener("click", () => {
+          inviteToStudio(comment.querySelector(".name").textContent.trim())
+            .then((resp) => resp.text())
+            .then((resp) => {
+              try {
+                const parsed = JSON.parse(resp);
+                // handle the different possible responses
+                if (parsed.status === "success") {
+                  inviteButton.innerText = "Invited!";
+                } else if (parsed.message.endsWith("is already a curator of this studio")) {
+                  inviteButton.innerText = "This user is already a curator";
+                } else {
+                  throw Error(0);
+                }
+              } catch (err) {
+                inviteButton.innerText = "Whoops, something went wrong";
               }
-            } catch (err) {
-              inviteButton.innerHTML = "Whoops, something went wrong";
-            }
-            comment.querySelector(".reply").click();
-            inviteButton.style.fontWeight = "bold";
-            inviteButton.removeEventListener("click", listener);
-            inviteButton.classList.remove("actions"); // after it's clicked it can't be clicked again
-          });
+              comment.querySelector(".reply").click();
+              inviteButton.style.fontWeight = "bold";
+              inviteButton.removeEventListener("click", listener);
+              inviteButton.classList.remove("actions"); // after it's clicked it can't be clicked again
+            });
+        });
       });
-    });
-  }
+    }
+  });
 }
