ScratchAddons/ScratchAddons

commit b7c63009b771da62f0f491fc8593c3097730a4bb

Fix "custom Colors addon makes devtools highlighting not work" (#1563)
The Blockly method just edited the `fill` attribute, which is overridden by the stylesheet. This way edits the `style` attribute, which overrides everything

addons/editor-devtools/blockly/BlockFlasher.js
@@ -22,7 +22,7 @@ export default class BlockFlasher {
      * @private
      */
     function _flash() {
-      myFlash.block.setColour(flashOn ? "#ffff80" : myFlash.colour);
+      myFlash.block.svgPath_.style.fill = flashOn ? "#ffff80" : myFlash.colour;
       flashOn = !flashOn;
       count--;
       if (count > 0) {

commit 05504cdcf564b8f308feaf2d3b0653580df642f7

Enable / Disable features in settings of Dev Tools (#2146)
* Bugfix - Event should have no default background color

* Enhancement - Enable / Disable features in settings

* Enhancement - Enable / Disable features in settings

* Enhancement - Enable / Disable features in settings

* Enhancement - Add Make space feature when right click on a block stack

* Enhancement - Add Make space feature when right click on a block stack

* Enhancement - Add Make space feature when right click on a block stack
- Also remove date & version from help

* Enhancement - Add Make space feature when right click on a block stack
- Also remove date & version from help

* Help - Correct Case

* Fixup - Remove as yet unused settings, and incorrect usage of undefined keyword

* Fix for block flashing bug introduced by #1563 (Fix "custom Colors addon makes devtools highlighting not work")

* Enhance manifest wording

Co-authored-by: WorldLanguages <mundofinky@gmail.com>

addons/editor-devtools/DevTools.js
@@ -369,22 +369,25 @@ export default class DevTools {
   /**
    * A much nicer way of laying out the blocks into columns
    */
-  doCleanUp(e) {
+  doCleanUp(e, dataId) {
     let workspace = this.utils.getWorkspace();
     if (e) {
       e.cancelBubble = true;
       e.preventDefault();
       this.hidePopups(workspace);
-      setTimeout(() => this.doCleanUp(), 0);
+      setTimeout(() => this.doCleanUp(undefined, dataId), 0);
       return;
     }
+
+    let makeSpaceForBlock = dataId && workspace.getBlockById(dataId);
+    makeSpaceForBlock = makeSpaceForBlock && makeSpaceForBlock.getRootBlock();
 
     UndoGroup.startUndoGroup(workspace);
 
     let result = this.getOrderedTopBlockColumns(true);
     let columns = result.cols;
     let orphanCount = result.orphans.blocks.length;
-    if (orphanCount > 0) {
+    if (orphanCount > 0 && !dataId) {
       let message = this.msg("orphaned", {
         count: orphanCount,
       });
@@ -406,15 +409,17 @@ export default class DevTools {
       let maxWidth = 0;
 
       for (const block of column.blocks) {
+        let extraWidth = block === makeSpaceForBlock ? 380 : 0;
+        let extraHeight = block === makeSpaceForBlock ? 480 : 72;
         let xy = block.getRelativeToSurfaceXY();
         if (cursorX - xy.x !== 0 || cursorY - xy.y !== 0) {
           block.moveBy(cursorX - xy.x, cursorY - xy.y);
         }
         let heightWidth = block.getHeightWidth();
-        cursorY += heightWidth.height + 72;
+        cursorY += heightWidth.height + extraHeight;
 
         let maxWidthWithComments = maxWidths[block.id] || 0;
-        maxWidth = Math.max(maxWidth, Math.max(heightWidth.width, maxWidthWithComments));
+        maxWidth = Math.max(maxWidth, Math.max(heightWidth.width + extraWidth, maxWidthWithComments));
       }
 
       cursorX += maxWidth + 96;
@@ -446,7 +451,7 @@ export default class DevTools {
         }
       }
 
-      if (unusedLocals.length > 0) {
+      if (unusedLocals.length > 0 && !dataId) {
         const unusedCount = unusedLocals.length;
         let message = this.msg("unused-var", {
           count: unusedCount,
@@ -1229,12 +1234,15 @@ export default class DevTools {
   }
 
   /**
-   * Initiates a drag event for all block stacks expect those in the set of ids.
+   * Initiates a drag event for all block stacks except those in the set of ids.
    * But why? - Because we know all the ids of the existing stacks before we paste / duplicate - so we can find the
    * new stack by excluding all the known ones.
    * @param ids Set of previously known ids
    */
   beginDragOfNewBlocksNotInIDs(ids) {
+    if (!this.addon.settings.get("enablePasteBlocksAtMouse")) {
+      return;
+    }
     let wksp = this.utils.getWorkspace();
     let topBlocks = wksp.getTopBlocks();
     for (const block of topBlocks) {
@@ -1297,6 +1305,9 @@ export default class DevTools {
       if (document.activeElement.tagName === "INPUT") {
         return;
       }
+      // todo: if (!this.addon.settings.get("enableCtrlLeftRightNav")) {
+      //         return;
+      //       }
       if (this.isScriptEditor()) {
         this.utils.navigationHistory.goBack();
       } else if (this.isCostumeEditor()) {
@@ -1312,6 +1323,9 @@ export default class DevTools {
       if (document.activeElement.tagName === "INPUT") {
         return;
       }
+      // todo: if (!this.addon.settings.get("enableCtrlLeftRightNav")) {
+      //         return;
+      //       }
       if (this.isScriptEditor()) {
         this.utils.navigationHistory.goForward();
       } else if (this.isCostumeEditor()) {
@@ -1396,27 +1410,32 @@ export default class DevTools {
               return;
             }
             if (isBackground) {
+              let cleanupPlus = this.addon.settings.get("enableCleanUpPlus");
+
               let nodes = blocklyContextMenu.children;
               const realBlockly = await this.addon.tab.traps.getBlockly();
-              for (const node of nodes) {
-                if (node.textContent === realBlockly.Msg.CLEAN_UP) {
-                  node.remove();
-                  break;
+              if (cleanupPlus) {
+                for (const node of nodes) {
+                  if (node.textContent === realBlockly.Msg.CLEAN_UP) {
+                    node.remove();
+                    break;
+                  }
                 }
               }
-              blocklyContextMenu.insertAdjacentHTML(
-                "beforeend",
-                `
-                            <div id="s3devCleanUp" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
-                                <div class="goog-menuitem-content" style="user-select: none;">${this.m(
-                                  "clean-plus"
-                                )}</div>
-                            </div>
-                            <div id="s3devPaste" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none;">
-                                <div class="goog-menuitem-content" style="user-select: none;">${this.m("paste")}</div>
-                            </div>
-                            `
-              );
+              let html = cleanupPlus
+                ? `
+                  <div id="s3devCleanUp" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
+                      <div class="goog-menuitem-content" style="user-select: none;">${this.m("clean-plus")}</div>
+                  </div>
+              `
+                : "";
+
+              html += `
+                  <div id="s3devPaste" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none;">
+                      <div class="goog-menuitem-content" style="user-select: none;">${this.m("paste")}</div>
+                  </div>
+              `;
+              blocklyContextMenu.insertAdjacentHTML("beforeend", html);
             } else {
               let wksp = this.utils.getWorkspace();
               let block = wksp.getBlockById(dataId);
@@ -1455,22 +1474,19 @@ export default class DevTools {
                 blocklyContextMenu.insertAdjacentHTML(
                   "beforeend",
                   `
-                                    <div id="s3devCopy" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
-                                        <div class="goog-menuitem-content" style="user-select: none;">${this.m(
-                                          "copy-all"
-                                        )}</div>
-                                    </div>
-                                    <div id="s3devCopyBlock" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none;">
-                                        <div class="goog-menuitem-content" style="user-select: none;">${this.m(
-                                          "copy-block"
-                                        )}</div>
-                                    </div>
-                                    <div id="s3devCutBlock" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none;">
-                                        <div class="goog-menuitem-content" style="user-select: none;">${this.m(
-                                          "cut-block"
-                                        )}</div>
-                                    </div>
-                                `
+                    <div id="s3devMakeSpace" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
+                        <div class="goog-menuitem-content" style="user-select: none;">${this.m("make-space")}</div>
+                    </div>
+                    <div id="s3devCopy" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
+                        <div class="goog-menuitem-content" style="user-select: none;">${this.m("copy-all")}</div>
+                    </div>
+                    <div id="s3devCopyBlock" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none;">
+                        <div class="goog-menuitem-content" style="user-select: none;">${this.m("copy-block")}</div>
+                    </div>
+                    <div id="s3devCutBlock" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none;">
+                        <div class="goog-menuitem-content" style="user-select: none;">${this.m("cut-block")}</div>
+                    </div>
+                  `
                 );
               }
 
@@ -1479,12 +1495,12 @@ export default class DevTools {
                 blocklyContextMenu.insertAdjacentHTML(
                   "beforeend",
                   `
-                                <div id="s3devReplaceAllVars" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
-                                    <div class="goog-menuitem-content" style="user-select: none;">${this.m("swap", {
-                                      var: block.getCategory() === "data" ? this.m("variables") : this.m("lists"),
-                                    })}</div>
-                                </div>
-                                `
+                        <div id="s3devReplaceAllVars" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
+                            <div class="goog-menuitem-content" style="user-select: none;">${this.m("swap", {
+                              var: block.getCategory() === "data" ? this.m("variables") : this.m("lists"),
+                            })}</div>
+                        </div>
+                  `
                 );
                 this.selVarID = block.getVars()[0];
               }
@@ -1499,6 +1515,10 @@ export default class DevTools {
             let copyDiv = blocklyContextMenu.querySelector("div#s3devCleanUp");
             if (copyDiv) {
               copyDiv.addEventListener("click", (...e) => this.doCleanUp(...e));
+            }
+            copyDiv = blocklyContextMenu.querySelector("div#s3devMakeSpace");
+            if (copyDiv) {
+              copyDiv.addEventListener("click", (e) => this.doCleanUp(e, dataId));
             }
             copyDiv = blocklyContextMenu.querySelector("div#s3devCopy");
             if (copyDiv) {
@@ -1624,6 +1644,10 @@ export default class DevTools {
       return;
     }
 
+    // todo: if (!this.addon.settings.get("enableBlockInjector")) {
+    //         return;
+    //       }
+
     e.cancelBubble = true;
     e.preventDefault();
 
@@ -1680,6 +1704,10 @@ export default class DevTools {
         this.blockCursor = null; // Clear the cursor if using the mouse
         this.middleClickWorkspace(e);
       }
+      return;
+    }
+
+    if (!this.addon.settings.get("enableMiddleClickFinder")) {
       return;
     }
 

addons/editor-devtools/blockly/BlockFlasher.js
@@ -9,7 +9,7 @@ export default class BlockFlasher {
   static flash(block) {
     if (myFlash.timerID > 0) {
       clearTimeout(myFlash.timerID);
-      myFlash.block.setColour(myFlash.colour);
+      myFlash.block.svgPath_.style.fill = myFlash.colour;
     }
 
     let count = 4;

addons/editor-devtools/userscript.css
@@ -154,7 +154,6 @@ div.s3devDDOut.vis ul.s3devDD {
 }
 
 .event {
-  background-color: rgb(255, 213, 0);
   color: rgb(255, 191, 0);
 }
 .event:hover,

addons/editor-devtools/userscript.js
@@ -7,10 +7,6 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
     return;
   }
 
-  // 0-indexed 6 = July
-  const releaseDate = new Date(2021, 2, 28);
-  const releaseDateLocalized = new Intl.DateTimeFormat(msg.locale).format(releaseDate);
-
   const helpHTML = `
 <div id="s3devHelpPop" class="${addon.tab.scratchClass("modal_modal-overlay")}">
 <div class="${addon.tab.scratchClass("modal_modal-content")}">
@@ -27,16 +23,15 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
   </div>
 </div>
 <div id="s3devHelpContent">
-<p>${m("version", {
-    version: "1.12.0",
-    date: releaseDateLocalized,
+<p>${m("help-title")} ${m("help-by", {
     ndash: "&ndash;",
     url: '<a target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/griffpatch">Griffpatch</a>',
   })}</p>
 <hr />
 <h2><strong>${m("code-tab-features")}</strong></h2>
-<p><strong>${m("interactive-find-bar")}</strong> - ${m("interactive-find-bar-desc")}</p>
+<p><strong>${m("interactive-find-bar")}</strong> &ndash; ${m("interactive-find-bar-desc")}</p>
 <p><strong>${m("improved-tidy-up")}</strong> &ndash; ${m("improved-tidy-up-desc")}</p>
+<p><strong>${m("help-make-space")} ${m("help-new")}</strong> &ndash; ${m("help-make-space-desc")}</p>
 <p><strong>${m("copy-to-clipboard")}</strong> &ndash; ${m("copy-to-clipboard-desc")}</p>
 <p><strong>${m("paste-from-clipboard")}</strong> &ndash; ${m("paste-from-clipboard-desc")}</p>
 <p><strong>${m("swap-variable")}</strong> &ndash; ${m("swap-variable-desc")}</p>