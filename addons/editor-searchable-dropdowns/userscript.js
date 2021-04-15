61f7940a26f2e984ae7a696421cf2825f9f39064
WorldLanguages <mundofinky@gmail.com>
2021-04-15 20:00:55 -0300

    Readd dynamicDisable to editor dropdowns

--- addons/editor-searchable-dropdowns/userscript.js
+++ addons/editor-searchable-dropdowns/userscript.js
@@ -14,6 +14,7 @@ export default async function ({ addon, global, console }) {
     blocklyDropDownContent.style.width = getComputedStyle(blocklyDropDownContent).width;
 
     const searchBar = document.createElement("input");
+    addon.tab.displayNoneWhileDisabled(searchBar);
 
     searchBar.type = "text";
     searchBar.addEventListener("input", handleInputEvent);
