af7cdad2ac12ce8801d373136b387deee9fbbebd
jeffalo <40470736+jeffalo@users.noreply.github.com>
2021-04-18 19:35:21 +0200

    seperate reactions and update manifest

--- addons/my-ocular/discuss.js
+++ addons/my-ocular/discuss.js
@@ -3,62 +3,10 @@ export default async function ({ addon, global, console, msg }) {
 
   posts.forEach(async (i) => {
     let username = i.querySelector(".username").innerText;
-    let postID = i.id.split("p")[1];
 
     let left = i.querySelector(".postleft").children[0];
-    let footer = i.querySelector(".postfootright").children[0];
-
-    let viewOnOcularContainer = document.createElement("li");
-    let viewOnOcular = document.createElement("a");
-    viewOnOcular.innerText = "🔍";
-    viewOnOcular.title = msg("view-on-ocular");
-    viewOnOcular.href = `https://ocular.jeffalo.net/post/${postID}`;
-    viewOnOcularContainer.appendChild(document.createTextNode("| "));
-    viewOnOcularContainer.appendChild(viewOnOcular);
-    viewOnOcularContainer.appendChild(document.createTextNode(" |"));
-    footer.insertAdjacentElement("afterbegin", viewOnOcularContainer);
 
     const { userStatus, color } = await fetchStatus(username);
-
-    if (addon.auth.isLoggedIn) {
-      let reactionList = document.createElement("li"); // it's a list item, because its inside the postfootright list. so it's basically a nested list
-      async function makeReactionList() {
-        const reactions = await fetchReactions(postID);
-
-        reactionList.innerHTML = "";
-        reactions.forEach((reaction) => {
-          let reactionButton = document.createElement("span");
-          reactionButton.innerText = `${reaction.emoji} ${reaction.reactions.length}`;
-
-          reactionButton.className = "my-ocular-reaction-button";
-          if (reaction.reactions.find((r) => r.user == addon.auth.username)) {
-            reactionButton.classList.add("selected");
-          }
-
-          reactionButton.addEventListener("click", (e) => {
-            e.preventDefault();
-            let ocular = window.open(
-              `https://ocular.jeffalo.net/react/${postID}?emoji=${reaction.emoji}`,
-              "ocular",
-              "width=300,height=300"
-            );
-            let timer = setInterval(checkClosed, 500);
-
-            function checkClosed() {
-              if (ocular.closed) {
-                clearInterval(timer);
-                makeReactionList();
-              }
-            }
-          });
-
-          reactionList.appendChild(reactionButton);
-        });
-      }
-      footer.insertAdjacentElement("afterbegin", reactionList);
-
-      makeReactionList();
-    }
 
     if (userStatus) {
       let br = document.createElement("br");
@@ -86,14 +34,6 @@ export default async function ({ addon, global, console, msg }) {
         userStatus: data.status,
         color: data.color,
       });
-    });
-  }
-
-  async function fetchReactions(id) {
-    return new Promise(async (resolve, reject) => {
-      let response = await fetch(`https://my-ocular.jeffalo.net/api/reactions/${id}`);
-      let data = await response.json();
-      resolve(data);
     });
   }
 }
