e210178a862ece5b445ad252228044cc0cccfa9f
jeffalo <40470736+jeffalo@users.noreply.github.com>
2021-04-22 21:23:43 +0200

    reaction buttons and rebrand my-ocular addon (#2163)
    * reaction buttons
    
    * style: format code
    
    * fix the bug
    
    * add view on ocular button
    
    * style: format code
    
    * padding
    
    * don't use computedStyle
    
    * style: format code
    
    * seperate reactions and update manifest
    
    * style: format code
    
    * 🔍 ocular
    
    Co-authored-by: jeffalo <jeffalo@users.noreply.github.com>

--- addons/my-ocular/discuss.js
+++ addons/my-ocular/discuss.js
@@ -1,33 +1,39 @@
 export default async function ({ addon, global, console, msg }) {
-  var lefts = document.querySelectorAll("div.postleft > dl");
-  lefts.forEach(async (i) => {
-    var username = i.children[0].innerText;
+  let posts = document.querySelectorAll(".blockpost");
 
-    var response = await fetch(`https://my-ocular.jeffalo.net/api/user/${username}`);
-    var data = await response.json();
-    var userStatus = data.status;
-    var color = data.color;
+  posts.forEach(async (i) => {
+    let username = i.querySelector(".username").innerText;
+
+    let left = i.querySelector(".postleft").children[0];
+
+    const { userStatus, color } = await fetchStatus(username);
 
     if (userStatus) {
-      var br = document.createElement("br");
-      var status = document.createElement("i");
+      let br = document.createElement("br");
+      let status = document.createElement("i");
       status.title = msg("status-hover");
       status.innerText = userStatus;
 
-      var dot = document.createElement("span");
+      let dot = document.createElement("span");
       dot.title = msg("status-hover");
-      dot.style.height = "10px";
-      dot.style.width = "10px";
-      dot.style.marginLeft = "5px";
-      dot.style.backgroundColor = "#bbb"; //default incase bad
-      dot.style.borderRadius = "50%";
-      dot.style.display = "inline-block";
+      dot.className = "my-ocular-dot";
 
       dot.style.backgroundColor = color;
 
-      i.appendChild(br);
-      i.appendChild(status);
-      i.appendChild(dot);
+      left.appendChild(br);
+      left.appendChild(status);
+      if (color) left.appendChild(dot);
     }
   });
+
+  async function fetchStatus(username) {
+    return new Promise(async (resolve, reject) => {
+      let response = await fetch(`https://my-ocular.jeffalo.net/api/user/${username}`);
+      let data = await response.json();
+      resolve({
+        userStatus: data.status,
+        color: data.color,
+      });
+    });
+  }
 }
