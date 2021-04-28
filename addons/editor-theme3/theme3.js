ebcf493fea5b2ace192dd4f245bfcbe47ce42c3b
apple502j <33279053+apple502j@users.noreply.github.com>
2021-04-29 06:10:04 +0900

    Lint 20210429 (#2319)
    * Fix lint error
    
    * Compress SVG files

--- addons/editor-theme3/theme3.js
+++ addons/editor-theme3/theme3.js
@@ -3,7 +3,7 @@ import { textColor, multiply } from "../../libraries/text_color.js";
 function updateSettings(addon, newStyle) {
   var stylesheet = "";
   const textMode = addon.settings.get("text");
-  if (textMode == "black") {
+  if (textMode === "black") {
     stylesheet += `
       .blocklyText {
         fill: #575e75;
@@ -72,7 +72,7 @@ function updateSettings(addon, newStyle) {
 
   for (var prop of Object.keys(categories)) {
     var settingName = categories[prop].var ? categories[prop].var : prop;
-    if (textMode == "white" || textMode == "black") {
+    if (textMode === "white" || textMode === "black") {
       stylesheet += `g[data-category="${prop}"] > path.blocklyBlockBackground {
         fill: var(--editorTheme3-${settingName}Color);
       }
@@ -246,7 +246,7 @@ function updateSettings(addon, newStyle) {
         stylesheet += `g[data-category] /* specificity */ > path.blocklyBlockBackground[fill="#0FBD8C"] {
           fill: ${background};
           stroke: var(--editorTheme3-${prop}Color);
-        } 
+        }
         path.blocklyBlockBackground[fill="#0FBD8C"] ~ .blocklyText {
           fill: var(--editorTheme3-${prop}Color);
         }
