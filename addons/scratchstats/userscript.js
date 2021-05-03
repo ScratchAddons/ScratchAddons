c322792594d6e0ad776560f11ee9165ca4db6f8b
apple502j <33279053+apple502j@users.noreply.github.com>
2021-05-04 04:09:29 +0900

    Calculate stepSize (#2364)
    * Calculate stepSize
    
    * style: format code
    
    * I should probably add no-console rule
    
    Co-authored-by: apple502j <apple502j@users.noreply.github.com>

--- addons/scratchstats/userscript.js
+++ addons/scratchstats/userscript.js
@@ -85,6 +85,9 @@ export default async function ({ addon, msg, console }) {
           canvasContainer.style.height = "400px";
           const canvas = document.createElement("canvas");
           canvasContainer.appendChild(canvas);
+          const stepAvg = historyData.reduce((acc, cur) => acc + cur.value / historyData.length, 0);
+          const stepLog = Math.log10(stepAvg);
+          const stepSize = Math.pow(10, Math.max(Math.round(stepLog) - 1, 1));
           new Chart(canvas, {
             type: "scatter",
             data: {
@@ -113,6 +116,13 @@ export default async function ({ addon, msg, console }) {
                   {
                     ticks: {
                       callback: (x) => new Date(x).toDateString(),
+                    },
+                  },
+                ],
+                yAxes: [
+                  {
+                    ticks: {
+                      stepSize,
                     },
                   },
                 ],
