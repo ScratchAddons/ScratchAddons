8c1156f619eded1c6df2f07dacecc4e4a13c3a7a
Thomas Weber <weber_t@mailbox.org>
2021-05-05 11:26:18 -0500

    also fix language changes while im here

--- addons/mediarecorder/userscript.js
+++ addons/mediarecorder/userscript.js
@@ -13,7 +13,7 @@ export default async ({ addon, console, msg }) => {
   while (true) {
     const elem = await addon.tab.waitForElement('div[class*="menu-bar_file-group"] > div:last-child:not(.sa-record)', {
       markAsSeen: true,
-      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED"],
+      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
     });
     const getOptions = () => {
       const recordOption = Object.assign(document.createElement("div"), {
--- addons/mute-project/userscript.js
+++ addons/mute-project/userscript.js
@@ -21,7 +21,7 @@ export default async function ({ addon, global, console }) {
   while (true) {
     let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", {
       markAsSeen: true,
-      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED"],
+      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
     });
     let container = button.parentElement;
     container.appendChild(icon);
