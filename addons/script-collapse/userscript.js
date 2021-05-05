ad237584922107555354f5760e9ef200d22ec337
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-05-05 09:29:41 -0600

    Update for addon api change
    Replaced old method of context menu item creation.

--- addons/script-collapse/userscript.js
+++ addons/script-collapse/userscript.js
@@ -10,53 +10,31 @@ export default async function ({ addon, console, safeMsg: m }) {
     id: null,
     eventList: [],
   };
-
-  function click(e) {
-    if (e.button === 2) {
-      let block = e.target.closest("[data-id]");
-      let isBackground = !block && e.target.closest("svg.blocklySvg");
-      if (block && !isBackground) {
-        let id = block.dataset.id;
-        setTimeout(() => {
-          if (!document.querySelector("div.blocklyWidgetDiv")) return;
-
-          let menu = document.querySelector("div.blocklyWidgetDiv").querySelector("div.blocklyContextMenu");
-
-          if (!menu) return;
-
-          block = Blockly.getMainWorkspace().getBlockById(id);
-
-          let isCollapsed = block.collapsed_;
-
-          if (block.startHat_ || block.type == "procedures_call" || block.outputShape_) return; //Collapsing for these are broke...
-
-          menu.insertAdjacentHTML(
-            "beforeend",
-            `<div id="sa-script-collapse" class="goog-menuitem s3dev-mi" role="menuitem" style="user-select: none; border-top: 1px solid hsla(0, 0%, 0%, 0.15);">
-  <div class="goog-menuitem-content" style="user-select: none;">${isCollapsed ? m("expand") : m("collapse")}</div>
-</div>`
-          );
-
-          let menuItem = menu.querySelector("#sa-script-collapse");
-
-          menuItem.addEventListener("click", () => {
-            isCollapsed = !isCollapsed;
-            block.setCollapsed(isCollapsed);
-            menu.remove();
-            if (isCollapsed) cache.push(block.id);
-            else {
-              if (cache.includes(block.id)) {
-                //Just to be sure...
-                cache.splice(cache.indexOf(block.id), 1);
-              }
+  
+  addon.tab.createBlockContextMenu(
+    (items, block) => {
+      if (block.startHat_ || block.type == "procedures_call" || block.outputShape_) return; //Collapsing for these are broke...
+      items.push({
+        enabled: true,
+        seperator: true,
+        text: block.collapsed_ ? m("expand") : m("collapse"),
+        callback: () => {
+          block.setCollapsed(!block.collapsed_);
+          if (block.collapsed_) cache.push(block.id);
+          else {
+            if (cache.includes(block.id)) {
+              //Just to be sure...
+              cache.splice(cache.indexOf(block.id), 1);
             }
-          });
-        }, 0);
-      }
+          }
+        }
+      });
+    }, {
+      workspace: false,
+      blocks: true,
+      flyout: false
     }
-  }
-
-  document.addEventListener("mousedown", click, true);
+  );
 
   function workspaceChange(e) {
     if (eventGroup.id !== e.group) {
