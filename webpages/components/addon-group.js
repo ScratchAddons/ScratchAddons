29901386283f97f06a6f56e3f6572caa36589d27
GrahamSH <grahamshllk@gmail.com>
2021-05-06 17:47:19 +0000

    File-ize

---
+++ webpages/components/addon-group.js
@@ -0,0 +1,27 @@
+export default {
+    props: ["group"],
+    template: await (await fetch('addon-group.html')).text(),
+    data() {
+      return {};
+    },
+    computed: {
+      shouldShow() {
+        if (this.group.id === "new" && this.$root.searchInput !== "") return false;
+        return this.shownCount > 0;
+      },
+      shownCount() {
+        // Recompute after root loaded
+        void this.$root.loaded;
+  
+        return this.$children.filter((addon) => addon.shouldShow).length;
+      },
+      manifestsById() {
+        return this.$root.manifestsById;
+      },
+    },
+    methods: {
+      toggle() {
+        this.group.expanded = !this.group.expanded;
+      },
+    },
+  }
