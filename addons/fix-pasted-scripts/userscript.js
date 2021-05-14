195b64ee5b0ec3b818dfb16735b2c0014b6ccb3d
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-05-14 11:02:52 -0500

    Made CTRL+click actually functional, some bug fixes

--- addons/fix-pasted-scripts/userscript.js
+++ addons/fix-pasted-scripts/userscript.js
@@ -9,14 +9,28 @@ export default async function ({ addon, global, console }) {
   var wasJustDragged = false;
   var duplicateBehavior = false;
 
+  // Necessary for the CTRL + click option to work
+  var ctrlKeyPressed = false;
+  document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey) {
+      ctrlKeyPressed = true;
+    }
+  });
+  document.addEventListener('keyup', function(e) {
+    if (!e.ctrlKey) {
+      ctrlKeyPressed = false;
+    }
+  });
+
   const newBlocklyListen = function (e) {
     // Checks if the Blockly event is a script being clicked
-    if (e.element === "stackclick" && enabled) {
+    if (!addon.self.disabled && e.element === "stackclick" && enabled) {
       // Completely disable all script clicking if the addon setting is enabled
-      if (runMode == 'fullDisable' || (runMode == 'ctrl' && !e.ctrlKey)) {
+      if (runMode == 'fullDisable' || (runMode == 'ctrl' && !ctrlKeyPressed)) {
         return;
-        // Checks if the script was duplicated/pasted -- if so, disable the response (running the script) for this event
-      } else if (!(wasJustDragged && duplicateBehavior)) {
+      // Checks if the script was duplicated/pasted -- if so, disable the response (running the script) for this event
+      // If CTRL + click mode is on, and the user is holding CTRL, the script running is intentional, and the check can be bypassed
+      } else if (!(wasJustDragged && duplicateBehavior) || (runMode == 'ctrl' && ctrlKeyPressed)) {
         originalBlocklyListen.call(this, e);
       }
       wasJustDragged = false;
@@ -28,6 +42,9 @@ export default async function ({ addon, global, console }) {
         duplicateBehavior = false;
       } else if (e.element === "selected" && e.type === "ui") {
         duplicateBehavior = true;
+      } else if (e.type === "delete") {
+        wasJustDragged = false;
+        duplicateBehavior = false;
       }
       originalBlocklyListen.call(this, e);
     }
