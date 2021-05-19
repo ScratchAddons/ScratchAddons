f26b80759dde917981a6fbeac9aad983f8a63e4e
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-05-19 14:44:40 -0600

    Update userscript.js

--- addons/custom-fonts/userscript.js
+++ addons/custom-fonts/userscript.js
@@ -7,7 +7,8 @@ export default async function ({ addon, console }) {
     ["headfnt", "header"],
     ["mainfnt", "main"],
     ["editfnt", "editor"],
-  ];
+    ["footerfnt", "footer"]
+  ]
 
   let enabled = true;
 
@@ -26,7 +27,7 @@ export default async function ({ addon, console }) {
     enabled = true;
     styles = styles.map(function (style) {
       return document.head.appendChild(style);
-    });
+    })
   }
 
   addon.self.addEventListener("disabled", disable);
@@ -35,32 +36,33 @@ export default async function ({ addon, console }) {
 
   let templates = {
     nav: '#navigation, [class*="menu-bar_menu-bar"], #topnav { %1 }',
-    blocks: ".blocklyText, .blocklyHtmlInput, .scratchCommentBody, .scratchCommentText, .scratchblocks text  { %1 }",
-    header: ".box-header { %1 }",
-    main: ".box-content, .project-title, .comment, .button, .preview-row { %1 }",
-    editor:
-      '.scratchCategoryMenu, [role="tablist"], [class*="loader"], [class*="sprite-selector_sprite-selector"] *, [class*="target-pane_stage-selector-wrapper"] *, .pos-container > span, [class*="asset-panel_wrapper"] *:not(svg *) { %1 }',
+    blocks: '.blocklyText, .blocklyHtmlInput, .scratchCommentBody, .scratchCommentText, .scratchblocks text  { %1 }',
+    header: '.box-header, .box-head, form *, .tabs-index * { %1 }',
+    main: '.box-content, .project-title, .comment, .button, .preview-row { %1 }',
+    editor: '.scratchCategoryMenu, [role="tablist"], [class*="loader"], [class*="sprite-selector_sprite-selector"] *, [class*="target-pane_stage-selector-wrapper"] *, .pos-container > span, [class*="asset-panel_wrapper"] *:not(svg *) { %1 }',
+    footer: '#footer * { %1 }'
   };
 
-  let defSpacing = getWidth();
+  let defSpacing =  getWidth();
 
-  load(true);
+   load(true);
 
   addon.settings.addEventListener("change", () => load());
 
   async function load(init = false) {
+
     let fonts = options.map(([id, nme]) => addon.settings.get(id).trim());
 
     let needsLoad = fonts.filter((fnt) => !document.fonts.check(`12px ${fnt}`) && fnt.toLowerCase() !== "helvetica");
 
-    if (needsLoad.length > 0) {
+    if (needsLoad.length >0) {
       await new Promise(function (resolve) {
         WebFont.load({
           google: {
-            families: needsLoad,
+            families: needsLoad
           },
           active: resolve,
-          fontinactive: resolve, //Font failed, but we don't want to throw an error.
+          fontinactive: resolve //Font failed, but we don't want to throw an error.
         });
       });
     }
@@ -68,20 +70,12 @@ export default async function ({ addon, console }) {
     for (let [id, nme] of options) {
       let s = nme === "blocks" ? await calcSpacing(addon.settings.get(id).trim()) : 0;
       if (init) {
-        addStyle(
-          createStyle(
-            styleFromTemplate(templates[nme], `font-family: ${addon.settings.get(id)}; letter-spacing: ${s}px;`),
-            { id: `sa-custom-${nme}-font` }
-          )
-        );
+        addStyle(createStyle(styleFromTemplate(templates[nme],`font-family: ${addon.settings.get(id)}; letter-spacing: ${s}px;`), { id: `sa-custom-${nme}-font` }));
       } else {
-        updateStyle(
-          `sa-custom-${nme}-font`,
-          styleFromTemplate(templates[nme], `font-family: ${addon.settings.get(id)}; letter-spacing: ${s}px;`)
-        );
+        updateStyle(`sa-custom-${nme}-font`, styleFromTemplate(templates[nme],`font-family: ${addon.settings.get(id)}; letter-spacing: ${s}px;`))
       }
     }
-  }
+  };
 
   function styleFromTemplate(template, ...args) {
     let matches = template.match(/\%([0-9]+)/g);
@@ -107,14 +101,14 @@ export default async function ({ addon, console }) {
   }
 
   function updateStyle(id, text) {
-    return (styles.filter((style) => style.id === id)[0].innerHTML = text);
+    return styles.filter(style => style.id === id)[0].innerHTML = text;
   }
 
   function getWidth() {
     let chars = "mmmmmmmmmmlli";
     let p = document.createElement("p");
     p.classList.add("blocklyText");
-    p.style.opacity = "0";
+    p.style.opacity = "0"
     p.innerHTML = chars;
     p.style.letterSpacing = 0;
     p.style.position = "absolute";
@@ -126,15 +120,15 @@ export default async function ({ addon, console }) {
 
   async function calcSpacing(font) {
     let styl = createStyle(styleFromTemplate(templates.blocks, `font-family: ${font} !important`), {
-      id: "sa-custom-font-test",
+      id: "sa-custom-font-test"
     });
     document.head.appendChild(styl);
-    await new Promise((resolve) => {
+    await new Promise(resolve => {
       styl.addEventListener("load", resolve);
-    });
+    })
     let spacing = getWidth();
     let diff = spacing - defSpacing;
-    console.log(document.fonts.check(`12px ${font}`));
+    console.log(document.fonts.check(`12px ${font}`))
     //styl.remove();
     if (diff > 0) {
       return -diff * 1.25;
