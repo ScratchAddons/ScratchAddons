4a082cba0d43ab1faba652b41f420808203cd1ed
WorldLanguages <mundofinky@gmail.com>
2021-05-20 18:16:45 -0300

    Many changes, see comment below

--- addons/fix-pasted-scripts/userscript.js
+++ addons/fix-pasted-scripts/userscript.js
@@ -1,10 +1,14 @@
 // This addon works by modifying the VM and Blockly to not react to clicking scripts.
 
 export default async function ({ addon, global, console }) {
-  const vm = await addon.tab.traps.vm;
-  const originalBlocklyListen = await vm.editingTarget.blocks.constructor.prototype.blocklyListen;
+  const vm = addon.tab.traps.vm;
+  await new Promise((resolve, reject) => {
+    if (vm.editingTarget) return resolve();
+    vm.runtime.once("PROJECT_LOADED", resolve);
+  });
+  const originalBlocklyListen = vm.editingTarget.blocks.constructor.prototype.blocklyListen;
   const BlocklyInstance = await addon.tab.traps.getBlockly();
-  const originalObject = await BlocklyInstance.BlockSvg.prototype.onMouseDown_;
+  const originalObject = BlocklyInstance.BlockSvg.prototype.onMouseDown_;
 
   // Necessary for the CTRL + click option to work
   var ctrlKeyPressed = false;
@@ -21,7 +25,7 @@ export default async function ({ addon, global, console }) {
 
   // Fixes the duplicate/pasting bug, no matter the setting (@GarboMuffin's implementation)
   BlocklyInstance.BlockSvg.prototype.onMouseDown_ = function (e) {
-    if (this.workspace && this.workspace.isDragging()) {
+    if (!addon.self.disabled && this.workspace && this.workspace.isDragging()) {
       return;
     } else {
       return originalObject.call(this, e);
@@ -31,11 +35,7 @@ export default async function ({ addon, global, console }) {
   // Limits all script running, based on setting
   const newBlocklyListen = function (e) {
     var runMode = addon.settings.get("runMode");
-    if (
-      !addon.self.disabled &&
-      e.element === "stackclick" &&
-      (runMode == "fullDisable" || (runMode == "ctrl" && !ctrlKeyPressed))
-    ) {
+    if (!addon.self.disabled && e.element === "stackclick" && runMode == "ctrl" && !ctrlKeyPressed) {
       return;
     } else {
       originalBlocklyListen.call(this, e);
