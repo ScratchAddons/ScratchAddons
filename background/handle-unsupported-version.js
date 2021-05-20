32289125ba4131c1011f1972d8aa60f5a410e7ec
WorldLanguages <mundofinky@gmail.com>
2021-05-20 18:45:59 -0300

    Do not open on startup & replace tab if triggered from settings

--- background/handle-unsupported-version.js
+++ background/handle-unsupported-version.js
@@ -1,18 +1,19 @@
-function handleUnsupported() {
-  const checkIfUnsupported = () => {
-    const getVersion = () => {
-      let userAgent = /(Firefox|Chrome)\/([0-9.]+)/.exec(navigator.userAgent);
-      if (!userAgent) return { browser: null, version: null };
-      return { browser: userAgent[1], version: userAgent[2].split(".")[0] };
-    };
-
-    let { browser, version } = getVersion();
-    return (browser === "Chrome" && version < 81) || (browser === "Firefox" && version < 74);
+const checkIfUnsupported = () => {
+  const getVersion = () => {
+    let userAgent = /(Firefox|Chrome)\/([0-9.]+)/.exec(navigator.userAgent);
+    if (!userAgent) return { browser: null, version: null };
+    return { browser: userAgent[1], version: userAgent[2].split(".")[0] };
   };
-  if (checkIfUnsupported()) chrome.tabs.create({ url: "https://scratchaddons.com/unsupported" });
-}
-handleUnsupported();
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request === "checkIfUnsupported") handleUnsupported();
-});
+  let { browser, version } = getVersion();
+  return (browser === "Chrome" && version < 81) || (browser === "Firefox" && version < 74);
+};
+
+if (checkIfUnsupported()) {
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request === "checkIfUnsupported") {
+      if (sender.tab) chrome.tabs.update(sender.tab.id, { url: "https://scratchaddons.com/unsupported" });
+      else chrome.tabs.create({ url: "https://scratchaddons.com/unsupported" });
+    }
+  });
+}
