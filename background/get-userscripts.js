0efec5d17fb43163d0c612cbd6f01413b828bc62
World_Languages <mundofinky@gmail.com>
2021-05-23 16:56:39 -0300

    Fix edge case TypeError in get-userscripts.js (#2575)
    * Fix edge case TypeError in get-userscripts.js
    
    * else* if

--- background/get-userscripts.js
+++ background/get-userscripts.js
@@ -239,7 +239,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       if (cacheEntry.loading) {
         scratchAddons.localEvents.addEventListener("csInfoCacheUpdated", function thisFunction() {
           cacheEntry = getCacheEntry();
-          if (!cacheEntry.loading) {
+          if (!cacheEntry) {
+            scratchAddons.localEvents.removeEventListener("csInfoCacheUpdated", thisFunction);
+          } else if (!cacheEntry.loading) {
             sendResponse(cacheEntry.info);
             csInfoCache.delete(identity);
             scratchAddons.localEvents.removeEventListener("csInfoCacheUpdated", thisFunction);
