3286d1d83d5146ed7b487cf82e380b8efef6d8e3
WorldLanguages <mundofinky@gmail.com>
2021-05-25 15:40:07 -0300

    load-vue-components: support <style> element

--- libraries/common/load-vue-components.js
+++ libraries/common/load-vue-components.js
@@ -1,3 +1,5 @@
+const styles = {};
+
 /**
  * Loads Vue components.
  * @param {string[]} filenames - filenames of the components, without extensions.
@@ -11,7 +13,11 @@ export default (filenames) =>
       const jsPromise = import(jsUrl);
       return fetch(htmlUrl)
         .then((resp) => resp.text())
-        .then((text) => new DOMParser().parseFromString(text, "text/html").querySelector("template").innerHTML)
+        .then((text) => {
+          const dom = new DOMParser().parseFromString(text, "text/html");
+          styles[filename] = dom.querySelector("style");
+          return dom.querySelector("template").innerHTML;
+        })
         .then((template) => jsPromise.then((esm) => esm.default({ template })));
     })
-  );
+  ).then(() => filenames.forEach((filename) => styles[filename] && document.head.appendChild(styles[filename])));
