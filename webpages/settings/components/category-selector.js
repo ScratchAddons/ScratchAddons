c1f8191b5bf61d580ed5b79a4e6ef130287e7868
World_Languages <mundofinky@gmail.com>
2021-05-25 12:44:27 -0300

    Split settings page components into files, enhance loading speed (#2567)
    * Move components to their own files
    
    * Format code
    
    * Move tags.js to new "data" folder, move addonGroups there
    
    * Move categories to data folder
    
    * Preload JS imports and components on head
    
    * Reduce layout shift
    
    * Format code
    
    * Show parts of UI when addon list loading
    
    * Fix regression
    
    * Don't load <addon-body> for search pseudogroup immediatelly
    
    * Format code
    
    * Add loaded property to category component
    
    * Format code
    
    * Full screen autofocus search bar in Firefox / slow Chrome
    
    * Remove autofocus
    
    Avoid "Autofocus processing was blocked because a document already has a focused element."
    
    * Remove unnecessary classname added in this PR
    
    * Remove poop.js (whoops)
    
    * Fix grantedOptionalPermissions is undefined in addon-body.js
    
    * Format code
    
    * Remove id from component template
    
    * Remove "more settings" next to search bar in iframe
    
    * Remove unneeded flex: 0 0 to avoid layout shift
    
    * Use v-cloak and add CSS so that it works before Vue loads
    
    * Format code
    
    Co-authored-by: WorldLanguages <WorldLanguages@users.noreply.github.com>

---
+++ webpages/settings/components/category-selector.js
@@ -0,0 +1,27 @@
+export default async function ({ template }) {
+  const CategorySelector = Vue.extend({
+    props: ["category"],
+    template,
+    data() {
+      return {};
+    },
+    computed: {
+      selectedCategory() {
+        return this.$root.selectedCategory;
+      },
+      shouldShow() {
+        const categoriesWithParent = this.$root.categories
+          .filter((category) => category.parent === this.category.parent)
+          .map((category) => category.id);
+        return !this.category.parent || [this.category.parent, ...categoriesWithParent].includes(this.selectedCategory);
+      },
+    },
+    methods: {
+      onClick(event) {
+        event.stopPropagation();
+        this.$root.selectedCategory = this.category.id;
+      },
+    },
+  });
+  Vue.component("category-selector", CategorySelector);
+}
