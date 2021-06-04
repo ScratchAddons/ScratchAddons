b7e8e8b7cf7204c217be285c500ceb1e6d19d3ef
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-04 16:44:34 +0000

    Format code

--- addons/redirect-mobile-forums/userscript.js
+++ addons/redirect-mobile-forums/userscript.js
@@ -1,3 +1,3 @@
-export default async function({ addon, global, console, msg }) {
-  window.location.replace(window.location.href.replace("m/", ""))
+export default async function ({ addon, global, console, msg }) {
+  window.location.replace(window.location.href.replace("m/", ""));
 }
