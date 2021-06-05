d3aba434de2de2fb43d3dc1ab112b3bbafaf1dca
William (Wgyt) Horning <williamhorning@wgyt.tk>
2021-06-05 07:11:17 -0400

    add todo in popup, and finished rpc

--- addons/onestate/addon.js
+++ addons/onestate/addon.js
@@ -12,9 +12,15 @@ export default async function ({ addon, global, console }) {
         rpcon = addon.settings.get("rpc")
     });
     document.defaultView.addEventListener("beforeunload", function (event) {
-        global.setOneState(false, "")
+        global.setOneState(false, "", "")
     })
-    global.setOneState = async (online, rpc) => {
+    global.setOneState = async (online, rpc, rpcurl) => {
+        var data = {
+            online: online,
+            richpresense: rpc,
+            richpresenseurl: rpcurl,
+            retroid: token
+        }
         return await fetch(`https://state.onedot.cf/api/v1/user/${user}`, {
             method: 'POST',
             mode: 'cors',
@@ -25,11 +31,7 @@ export default async function ({ addon, global, console }) {
             },
             redirect: 'follow',
             referrerPolicy: 'no-referrer',
-            body: JSON.stringify({
-                online: online,
-                richpresense: rpc,
-                retroid: token
-            })
+            body: JSON.stringify(data)
         });
     }
     global.getOneState = async (user) => {
@@ -37,8 +39,28 @@ export default async function ({ addon, global, console }) {
         return await data
     }
     if (rpcon) {
-        global.setOneState(true, ""/* todo: check url and make a string saying playing ... or something */)
+        var path = document.defaultView.location.pathname
+        var projectx = /\/projects\/.*/g
+        var studiosx = /\/studios\/.*/g
+        var discussx = /\/discuss\/.*/g
+        if (projectx.test(path)) {
+            if (path.includes('editor')) {
+                global.setOneState(true, `Editing ${document.title}`, path);
+            } else {
+                global.setOneState(true, `Playing ${document.title}`, path);
+            }
+        } else if (studiosx.test(path)) {
+            global.setOneState(true, `Looking at a ${document.title}`, path);
+        } else if (discussx.test(path)) {
+            if (path.includes('topic')) {
+                global.setOneState(true, `Looking at ${document.title}`, path);
+            } else if (path === '/discuss' || path === '/discuss/') {
+                global.setOneState(true, `Looking at discussion forums`, path);
+            } else {
+                global.setOneState(true, `Looking at ${document.title}`, path);
+            }
+        }
     } else {
-        global.setOneState(true, "")
+        global.setOneState(true, "", "")
     }
 }
