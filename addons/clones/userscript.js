666a2d475643ed3c91ca295640985723af29b044
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-06-06 14:35:18 -0400

    Fix clone count not showing when going out of editor (#2694)

--- addons/clones/userscript.js
+++ addons/clones/userscript.js
@@ -18,8 +18,6 @@ export default async function ({ addon, global, console, msg }) {
 
   let countContainerContainer = document.createElement("div");
 
-  addon.tab.displayNoneWhileDisabled(countContainerContainer, { display: "flex" });
-
   let countContainer = document.createElement("div");
   let count = document.createElement("span");
   let icon = document.createElement("span");
@@ -42,20 +40,14 @@ export default async function ({ addon, global, console, msg }) {
   function doCloneChecks() {
     const v = vm.runtime._cloneCounter;
     // performance
-    if (v === lastChecked) return false;
+    if (v === lastChecked) return;
     countContainerContainer.dataset.count = lastChecked = v;
     count.dataset.str = cache[v] || "";
-    return true;
+
+    if (v == 0) countContainerContainer.style.display = "none";
+    else addon.tab.displayNoneWhileDisabled(countContainerContainer, { display: "flex" });
   }
 
-  const check = () => {
-    if (doCloneChecks() && addon.tab.editorMode === "editor") {
-      if (!countContainerContainer.isConnected) {
-        const elem = document.querySelector("[class^='controls_controls-container']");
-        elem.appendChild(countContainerContainer);
-      }
-    }
-  };
   vm.runtime.on("targetWasRemoved", (t) => {
     // Fix bug with inaccurate clone counter
     if (t.isOriginal) vm.runtime.changeCloneCounter(1);
@@ -63,7 +55,18 @@ export default async function ({ addon, global, console, msg }) {
   const oldStep = vm.runtime.constructor.prototype._step;
   vm.runtime.constructor.prototype._step = function (...args) {
     const ret = oldStep.call(this, ...args);
-    check();
+    doCloneChecks();
     return ret;
   };
+
+  while (true) {
+    let bar = await addon.tab.waitForElement('[class*="controls_controls-container"]', {
+      markAsSeen: true,
+      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
+    });
+
+    if (addon.tab.editorMode === "editor") {
+      bar.appendChild(countContainerContainer);
+    }
+  }
 }
