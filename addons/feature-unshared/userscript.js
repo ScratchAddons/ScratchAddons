86c7e70e5094aff694bb6d1b97414892d78dc67a
apple502j <apple502j@users.noreply.github.com>
2021-06-11 15:12:52 +0000

    Format code

--- addons/feature-unshared/userscript.js
+++ addons/feature-unshared/userscript.js
@@ -1,11 +1,16 @@
 export default async function ({ addon, global, console }) {
-  const mainJS = await addon.tab.waitForElement("script[src$=\"js/apps/userprofile/main.js\"]");
-  mainJS.addEventListener("load", () => {
-    const init = Scratch.UserProfile.Router.prototype.initialize;
-    Scratch.UserProfile.Router.prototype.initialize = function (...args) {
-      const v = init.call(this, ...args);
-      this.profileModel.related.shared.options.collectionType = this.profileModel.related.shared._meta.collectionType = "all/#";
-      return v;
-    }
-  }, { once: true });
+  const mainJS = await addon.tab.waitForElement('script[src$="js/apps/userprofile/main.js"]');
+  mainJS.addEventListener(
+    "load",
+    () => {
+      const init = Scratch.UserProfile.Router.prototype.initialize;
+      Scratch.UserProfile.Router.prototype.initialize = function (...args) {
+        const v = init.call(this, ...args);
+        this.profileModel.related.shared.options.collectionType =
+          this.profileModel.related.shared._meta.collectionType = "all/#";
+        return v;
+      };
+    },
+    { once: true }
+  );
 }
