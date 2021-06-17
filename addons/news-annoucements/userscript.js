3c5842bbb02f0eef049345f1365a01229d8205f2
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-06-17 07:43:22 -0400

    Update userscript.js
    remove addon stuff, mxmou said it is fine

--- addons/news-annoucements/userscript.js
+++ addons/news-annoucements/userscript.js
@@ -1,3 +1,1 @@
-export default async function ({ addon, global, console }) {
-  window.location.replace("https://scratch.mit.edu/discuss/5/");
-}
+window.location.replace("https://scratch.mit.edu/discuss/5/");
