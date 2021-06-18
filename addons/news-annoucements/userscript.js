600fc5ab4136b8668110a5e14eceab53a34493ad
Shluffy <Shluffy@users.noreply.github.com>
2021-06-18 20:37:05 +0000

    Format code

--- addons/news-annoucements/userscript.js
+++ addons/news-annoucements/userscript.js
@@ -1,3 +1,3 @@
-export default function() {
-  window.location.replace("https://scratch.mit.edu/discuss/5")
+export default function () {
+  window.location.replace("https://scratch.mit.edu/discuss/5");
 }
