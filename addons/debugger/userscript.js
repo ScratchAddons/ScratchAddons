0d973f5af40f6730a6b149aaab22b5e65432c69e
Thomas Weber <weber_t@mailbox.org>
2021-06-29 23:16:49 -0500

    Display name does not have to match actual name

--- addons/debugger/userscript.js
+++ addons/debugger/userscript.js
@@ -27,27 +27,32 @@ export default async function ({ addon, global, console, msg }) {
   };
   addon.tab.addBlock("sa-pause", {
     args: [],
+    displayName: msg('block-sa-pause'),
     callback: pause,
     hidden: true
   });
   addon.tab.addBlock("\u200B\u200Bbreakpoint\u200B\u200B", {
     args: [],
+    displayName: msg('block-breakpoint'),
     callback: pause
   });
   addon.tab.addBlock("\u200B\u200Blog\u200B\u200B %s", {
     args: ["content"],
+    displayName: msg('block-log'),
     callback: ({ content }, thread) => {
       addItem(content, thread, "log");
     }
   });
   addon.tab.addBlock("\u200B\u200Bwarn\u200B\u200B %s", {
     args: ["content"],
+    displayName: msg('block-warn'),
     callback: ({ content }, thread) => {
       addItem(content, thread, "warn");
     }
   });
   addon.tab.addBlock("\u200B\u200Berror\u200B\u200B %s", {
     args: ["content"],
+    displayName: msg('block-error'),
     callback: ({ content }, thread) => {
       addItem(content, thread, "error");
     }
