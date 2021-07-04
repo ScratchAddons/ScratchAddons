f2e22ce52d404ba1b8f0f8215ee6a63a9bf31f24
WorldLanguages <mundofinky@gmail.com>
2021-07-04 15:30:50 -0300

    Cleaner light mode code

--- webpages/popup-loader.js
+++ webpages/popup-loader.js
@@ -101,10 +101,8 @@ const lightThemeLink = document.createElement("link");
 lightThemeLink.setAttribute("rel", "stylesheet");
 lightThemeLink.setAttribute("href", "light.css");
 
-chrome.storage.sync.get(["globalTheme"], function (r) {
-  let rr = false; //true = light, false = dark
-  if (r.globalTheme) rr = r.globalTheme;
-  if (rr) {
+chrome.storage.sync.get(["globalTheme"], function ({ globalTheme = false }) {
+  if (globalTheme === true) {
     scratchAddons.isLightMode = true;
     document.head.appendChild(lightThemeLink);
   }
