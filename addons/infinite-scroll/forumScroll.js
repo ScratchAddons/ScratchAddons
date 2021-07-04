2b965c54d04c596323257c4973e949e54f5e2baf
Hans5958 <email5958.1@gmail.com>
2021-07-05 05:02:26 +0700

    Add improvements on infinite scrolling (#2879)
    * Add improvements on infinite scrolling
    
    - Start updating if page already loads on the bottom part
    - Add page separator (forum category only)
    - Start using global stylesheet
    
    * Remove test code, adjustments for other themes
    
    * Add credits
    
    * Format code
    
    * Trigger Prettier on Actions
    
    * Format code
    
    * Do changes based on review
    
    * Do changes based on review 2
    
    * Format code
    
    * Fix styles
    
    * Whoops
    
    * Use manifest match instead of checking the pathname
    
    * Format code
    
    Co-authored-by: Hans5958 <Hans5958@users.noreply.github.com>

--- addons/infinite-scroll/forumScroll.js
+++ addons/infinite-scroll/forumScroll.js
@@ -1,52 +1,68 @@
-export default async function ({ addon, global, console }) {
-  if (window.location.pathname.split("/").length === 4) {
-    for (let show of document.getElementsByClassName("pagination")) show.style.display = "none";
-    let page = 1;
-    let lock = false;
-    window.addEventListener("scroll", () => {
-      if (
-        window.scrollY + window.innerHeight >=
-        document.getElementById("djangobbindex").offsetHeight - document.getElementById("footer").offsetHeight
-      ) {
-        if (!lock) {
-          lock = true;
-          page++;
-          let nextPage;
-          if (location.search) {
-            let search = location.search.replace(/(&|\?)page=[0-9]+/, "");
-            nextPage = `https://scratch.mit.edu${location.pathname}${search}&page=${page}`;
-          } else {
-            nextPage = `https://scratch.mit.edu${location.pathname}?page=${page}`;
-          }
-          window
-            .fetch(nextPage)
-            .catch((err) => {
-              console.log("Unable to fetch the page!");
-            })
-            .then((res) => res.text())
-            .then((data) => {
-              let parser = new DOMParser();
-              let doc = parser.parseFromString(data, "text/html");
-              let table, posts;
-              let vf = document.getElementById("vf");
-              if (vf) {
-                table = vf.getElementsByTagName("tbody")[0];
-                posts = doc.getElementById("vf").getElementsByTagName("tr");
-              } else {
-                table = document.getElementById("djangobbindex");
-                posts = doc.getElementById("djangobbindex").getElementsByClassName("blockpost");
-              }
-              for (let i = 1; i < posts.length; i++) {
-                if (vf) {
-                  table.appendChild(posts[i]);
-                } else {
-                  table.insertBefore(posts[i], table.querySelector(".postlinksb"));
-                }
-              }
-              lock = false;
-            });
-        }
-      }
-    });
+export default async function ({ addon, global, console, msg }) {
+  // This line below will always return the element since it will
+  // only run on the category page based on the match.
+  let vf = document.getElementById("vf");
+
+  let pageSeparator, pageSeparatorTd;
+  if (vf) {
+    pageSeparator = document.createElement("tr");
+    pageSeparatorTd = document.createElement("th");
+    pageSeparatorTd.className = "tcl";
+    pageSeparatorTd.scope = "col";
+    pageSeparatorTd.colSpan = 4;
+    pageSeparator.appendChild(pageSeparatorTd);
   }
+
+  let page = 1;
+  let lock = false;
+
+  const update = () => {
+    if (
+      window.scrollY + window.innerHeight >=
+      document.getElementById("djangobbindex").offsetHeight - document.getElementById("footer").offsetHeight
+    ) {
+      if (!lock) {
+        lock = true;
+        page++;
+        let nextPage;
+        if (location.search) {
+          let search = location.search.replace(/(&|\?)page=[0-9]+/, "");
+          nextPage = `https://scratch.mit.edu${location.pathname}${search}&page=${page}`;
+        } else {
+          nextPage = `https://scratch.mit.edu${location.pathname}?page=${page}`;
+        }
+        window
+          .fetch(nextPage)
+          .catch((err) => {
+            console.log("Unable to fetch the page!");
+          })
+          .then((res) => res.text())
+          .then((data) => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(data, "text/html");
+            let table, posts;
+            if (vf) {
+              table = vf.getElementsByTagName("tbody")[0];
+              posts = doc.getElementById("vf").getElementsByTagName("tr");
+              pageSeparatorTd.textContent = msg("page-num", { page });
+              table.appendChild(pageSeparator.cloneNode(true));
+            } else {
+              table = document.getElementById("djangobbindex");
+              posts = doc.getElementById("djangobbindex").getElementsByClassName("blockpost");
+            }
+            for (let i = 1; i < posts.length; i++) {
+              if (vf) {
+                table.appendChild(posts[i]);
+              } else {
+                table.insertBefore(posts[i], table.querySelector(".linksb"));
+              }
+            }
+            lock = false;
+          });
+      }
+    }
+  };
+
+  window.addEventListener("scroll", () => update(), { passive: true });
+  update();
 }
