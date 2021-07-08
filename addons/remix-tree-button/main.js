babdefbaea10d7361604c962f49315333ff55e05
Maximouse <51849865+mxmou@users.noreply.github.com>
2021-07-08 19:18:49 +0200

    Add dynamic enable/disable to remix-tree-button

--- addons/remix-tree-button/main.js
+++ addons/remix-tree-button/main.js
@@ -15,6 +15,8 @@ export default async function ({ addon, global, console, msg }) {
 
           const remixtreeSpan = document.createElement("span");
           remixtreeSpan.innerText = msg("remix-tree");
+          
+          addon.tab.displayNoneWhileDisabled(remixtree);
           remixtree.className = "button action-button remixtree-button";
           remixtree.id = "scratchAddonsRemixTreeBtn";
           remixtree.appendChild(remixtreeSpan);
