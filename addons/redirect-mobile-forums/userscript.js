b02bbd5b2fc8ce168ab64f2709ba64103383bf55
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-08 20:47:53 +0100

    Redirect addon

--- addons/redirect-mobile-forums/userscript.js
+++ addons/redirect-mobile-forums/userscript.js
@@ -1,3 +1,16 @@
 export default async function ({ addon, global, console, msg }) {
-  window.location.replace(window.location.href.replace("m/", ""));
+  const redirects = [
+    { from: new RegExp("^/discuss/m/(.*)$"), to: "/discuss/$1", id: "mobileForums" },
+    { from: new RegExp('^/news'), to: "/discuss/5/", id: "news"}
+  ]
+
+  let decodedURL = decodeURI(location.pathname);
+
+  let rule = redirects.find(redirect => addon.settings.get(redirect.id) && redirect.from.test(decodedURL))
+
+  if (!rule) return;
+
+  let url = decodedURL.replace(rule.from, rule.to)
+
+  window.location.replace(url)
 }
