0cdeb81ac1ff3fe74c81dc3de2029ee84f0684d6
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-07-08 18:11:39 -0400

    Fix reenabled event not launching when dynamicDisable is true (#2598)
    * Fix reenabled event not launching when dynamicDisable is true
    
    * Format code
    
    * Force dynamicEnable and dynamicDisable to be booleans in message
    
    Co-authored-by: TheColaber <TheColaber@users.noreply.github.com>
    Co-authored-by: WorldLanguages <mundofinky@gmail.com>

--- background/get-userscripts.js
+++ background/get-userscripts.js
@@ -36,6 +36,8 @@ scratchAddons.localEvents.addEventListener("addonDynamicEnable", ({ detail }) =>
                       addonId,
                       injectAsStyleElt: !!manifest.injectAsStyleElt,
                       index: scratchAddons.manifests.findIndex((addon) => addon.addonId === addonId),
+                      dynamicEnable: Boolean(manifest.dynamicEnable),
+                      dynamicDisable: Boolean(manifest.dynamicDisable),
                     },
                   },
                   { frameId: 0 }
