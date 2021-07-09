68a454fec6f3aadcbfc24f20fe68a2d53ea827af
Maximouse <51849865+mxmou@users.noreply.github.com>
2021-07-09 09:10:09 +0200

    Remove dynamic enable/disable from youtube-fullscreen

--- addons/youtube-fullscreen/userscript.js
+++ addons/youtube-fullscreen/userscript.js
@@ -1,12 +1,5 @@
 export default async function ({ addon, console }) {
   const player = await addon.tab.waitForElement(".youtube-player");
-
-  function setFullscreenEnabled(enabled) {
-    if (enabled) player.setAttribute("allowfullscreen", true);
-    else player.removeAttribute("allowfullscreen");
-    player.src += ""; // reload video
-  }
-  setFullscreenEnabled(true);
-  addon.self.addEventListener("disabled", () => setFullscreenEnabled(false));
-  addon.self.addEventListener("reenabled", () => setFullscreenEnabled(true));
+  player.setAttribute("allowfullscreen", true);
+  player.src += ""; // reload video
 }
