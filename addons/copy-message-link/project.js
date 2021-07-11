c0b88f12e546b8b916a6f6cd5d1d0415ca5f7459
apple502j <33279053+apple502j@users.noreply.github.com>
2021-07-12 02:06:42 +0900

    Fix crash when copy-message-link loads sooner than the website (#2979)
    * Implement elementCondition API
    
    * Fix crash when copy-message-link loads sooner than the website

--- addons/copy-message-link/project.js
+++ addons/copy-message-link/project.js
@@ -23,6 +23,10 @@ export default async function ({ addon, global, console, msg }) {
         newElem.style.fontWeight = "";
       }, 5000);
     };
-    comment.querySelector("div.action-list").prepend(newElem);
+    const actionList = await addon.tab.waitForElement("div.action-list", {
+      markAsSeen: true,
+      elementCondition: (e) => comment.contains(e),
+    });
+    actionList.prepend(newElem);
   }
 }
