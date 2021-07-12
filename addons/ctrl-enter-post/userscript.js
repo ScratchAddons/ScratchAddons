6658288c439af4529149774c0470e7dd0ec889e7
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-12 16:21:28 +0100

    Initial commit

---
+++ addons/ctrl-enter-post/userscript.js
@@ -0,0 +1,12 @@
+export default async function({ addon }) {
+    let type = location.pathname.split('/')[2]
+
+    let textarea = await addon.tab.waitForElement(type == 'settings' ? '#id_signature' : '#id_body')
+    let postButton = await addon.tab.waitForElement(type == 'topic' ? '.button.grey:nth-child(1)': 'button')
+
+    textarea.addEventListener('keyup', (e) => {
+        if ((e.ctrlKey || e.metaKey) && e.code == "Enter" ) {
+            postButton.click()
+        }
+    })
+}
