11d297f335f673533298f12d49c59c994ee5aef4
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-12 16:46:34 +0100

    scratch-www comments

---
+++ addons/ctrl-enter-post/comments.js
@@ -0,0 +1,22 @@
+export default async function({ addon, global, console, msg }) {
+    let isScratchR2 = addon.tab.clientVersion == 'scratchr2'
+    
+    let textboxSelector = isScratchR2 ? '[name=\'content\']' : '[name=\'compose-comment\']'
+    // TODO: fix on user profiles as it fails
+
+    while (true) {
+        let textbox = await addon.tab.waitForElement(textboxSelector, { markAsSeen: true })
+        var button;
+        if (isScratchR2) {
+            button = textbox.parentNode.querySelector('.control-group:not(.tooltip) div[data-control=\'post\']')
+        } else {
+            button = textbox.parentNode.parentNode.parentNode.querySelector('.compose-bottom-row .compose-post')
+        }
+
+        textbox.addEventListener('keyup', (e) => {
+            if ((e.ctrlKey || e.metaKey) && e.code == "Enter" ) {
+                button.click()
+            }
+        })
+    }
+}
