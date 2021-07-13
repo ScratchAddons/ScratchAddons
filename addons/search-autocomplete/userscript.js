62311be8923ac4eff0b5627a5f10438caf828621
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-13 15:02:28 +0000

    Format code

--- addons/search-autocomplete/userscript.js
+++ addons/search-autocomplete/userscript.js
@@ -1,8 +1,8 @@
 export default async function ({ addon, msg, global, console }) {
-    var searchBar;
-    if (addon.tab.clientVersion == 'scratchr2') {
-        searchBar = document.querySelector('#search-input')
-    } else searchBar = await addon.tab.waitForElement('#frc-q-1088')
+  var searchBar;
+  if (addon.tab.clientVersion == "scratchr2") {
+    searchBar = document.querySelector("#search-input");
+  } else searchBar = await addon.tab.waitForElement("#frc-q-1088");
 
-    searchBar.autocomplete = 'off'
+  searchBar.autocomplete = "off";
 }
