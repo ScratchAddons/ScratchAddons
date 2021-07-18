e2421ff30b2ec99d8171b07356648e80a59dc36b
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-18 22:07:00 +0100

    Remove unnecessary console.log

--- addons/live-comments/userscript.js
+++ addons/live-comments/userscript.js
@@ -1,122 +1,79 @@
 export default async function ({ addon, global, console, msg }) {
-    const { redux } = addon.tab;
-    let [ , type, id ] = location.pathname.split('/')
-    
-    if (type == 'studios') {
-        await redux.waitForState(state => redux.state.studio.infoStatus == "FETCHED", {
-            actions: ['SET_INFO']
-        })
-    } else {
-        await redux.waitForState((state) => state.preview.status.project === "FETCHED", {
-            actions: ['SET_PROJECT_INFO']
-        })
-    }
-    
-    const URL = `https://api.scratch.mit.edu/${type == 'projects' ? `users/${redux.state.preview.projectInfo.author.username}/projects` : `studios`}/${id}/comments`
+  const { redux } = addon.tab;
+  let [ , type, id ] = location.pathname.split('/')
+  
+  if (type == 'studios') {
+      await redux.waitForState(state => redux.state.studio.infoStatus == "FETCHED", {
+          actions: ['SET_INFO']
+      })
+  } else {
+      await redux.waitForState((state) => state.preview.status.project === "FETCHED", {
+          actions: ['SET_PROJECT_INFO']
+      })
+  }
+  
+  const URL = `https://api.scratch.mit.edu/${type == 'projects' ? `users/${redux.state.preview.projectInfo.author.username}/projects` : `studios`}/${id}/comments`
 
-    let getAllComments = () => [...redux.state.comments.comments]
-    let getAllReplies = () => {
-        let replies = []
+  let getAllComments = () => [...redux.state.comments.comments]
+  let getAllReplies = () => {
+      let replies = []
 
-        for (let re of Object.values(redux.state.comments.replies)) {
-            replies.push(...re)
-        }
-
-        return replies
-    }
-
-
-    setInterval(async () => {
-        var loopOffset = 40
-        var offset = -loopOffset
-        while (true) {
-            offset += loopOffset
-            let res = await fetch(URL + `?offset=${offset}&limit=${loopOffset}`)
-            let json = await res.json()
-            if (json.length == 0) return
-            let loadedComments = getAllComments().map(e => e.id)
-
-            for (let j in json) {
-                let comment = json[j]
-
-                if (loadedComments.indexOf(comment.id) !== -1) {
-                    break
-                }
-
-                redux.dispatch({ type: "ADD_NEW_COMMENT", comment })
-            }
-
-            if (json.length < loopOffset) break
-        }
-
-        
-        let comments = [ ...redux.state.comments.comments ]
-
-        for (let k in comments) {
-            let comment = comments[k]
-            offset = -loopOffset
-            while (true) {
-                offset += loopOffset
-                let res = await fetch(URL + `/${comment.id}/replies?offset=${offset}&limit=${loopOffset}`)
-                let json = (await res.json()).reverse()
-
-                if (json.length == 0) break
-                for (let reply of json) {
-                    let loadedComments = getAllReplies().map(c => c.id)
-
-                    if (loadedComments.indexOf(reply.id) !== -1) break;
-                    console.log(`NEW REPLY: `, reply, "MY PARENT IS", reply.parent_id)
-                    
-                    redux.dispatch({ type: "ADD_NEW_COMMENT", comment: reply, topLevelCommentId: comment.id })
-                }
-
-        if (loadedComments.indexOf(comment.id) !== -1) {
-          break;
-        }
-
-        redux.dispatch({ type: "ADD_NEW_COMMENT", comment });
+      for (let re of Object.values(redux.state.comments.replies)) {
+          replies.push(...re)
       }
 
-      if (json.length < loopOffset) break;
-    }
-
-    offset = -loopOffset;
-    let comments = getAllComments();
-
-    for (let comment of comments) {
-      while (true) {
-        offset += loopOffset;
-        let res = await fetch(URL + `/${comment.id}/replies?offset=${offset}&limit=${loopOffset}`);
-        let json = await res.json();
-
-<<<<<<< HEAD
-    
-}
-=======
-        if (json.length == 0) break;
-        for (let reply of json) {
-          let loadedComments = getAllComments().map((r) => r.id.toString());
-
-          if (loadedComments.indexOf(reply.id.toString()) !== -1) break;
-          console.log(`NEW REPLY: `, reply, "MY PARENT IS", reply.parent_id);
-
-          redux.dispatch({ type: "ADD_NEW_COMMENT", comment: reply, topLevelCommentId: reply.parent_id });
-        }
-
-        if (json.length < loopOffset) break;
-      }
-    }
-  }, 1000);
-
-  function getAllComments() {
-    let loadedComments = [...redux.state.comments.comments];
-    for (let replies of Object.values(redux.state.comments.replies)) {
-      loadedComments.push(...replies);
-    }
-
-    return loadedComments;
+      return replies
   }
 
-  function getTopLevelCommentId(rep) {}
+
+  setInterval(async () => {
+      var loopOffset = 40
+      var offset = -loopOffset
+      while (true) {
+          offset += loopOffset
+          let res = await fetch(URL + `?offset=${offset}&limit=${loopOffset}`)
+          let json = await res.json()
+          if (json.length == 0) return
+          let loadedComments = getAllComments().map(e => e.id)
+
+          for (let j in json) {
+              let comment = json[j]
+
+              if (loadedComments.indexOf(comment.id) !== -1) {
+                  break
+              }
+
+              redux.dispatch({ type: "ADD_NEW_COMMENT", comment })
+          }
+
+          if (json.length < loopOffset) break
+      }
+
+      
+      let comments = [ ...redux.state.comments.comments ]
+
+      for (let k in comments) {
+          let comment = comments[k]
+          offset = -loopOffset
+          while (true) {
+              offset += loopOffset
+              let res = await fetch(URL + `/${comment.id}/replies?offset=${offset}&limit=${loopOffset}`)
+              let json = (await res.json()).reverse()
+
+              if (json.length == 0) break
+              for (let reply of json) {
+                  let loadedComments = getAllReplies().map(c => c.id)
+
+                  if (loadedComments.indexOf(reply.id) !== -1) break;
+                  
+                  redux.dispatch({ type: "ADD_NEW_COMMENT", comment: reply, topLevelCommentId: comment.id })
+              }
+
+              if (json.length < loopOffset) break
+          }
+      }
+
+  }, 1000)
+
+  
 }
->>>>>>> cbb55354930ca54d5caed84b3c7681dd536708c9
