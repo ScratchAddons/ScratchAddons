2ac2480cb0caf214b4549c98d98e53514c079407
Maximouse <51849865+mxmou@users.noreply.github.com>
2021-07-19 17:04:02 +0200

    Implement full screen

---
+++ webpages/popup/fullscreen.js
@@ -0,0 +1,14 @@
+import loadVueComponent from "../../libraries/common/load-vue-components.js";
+import loadPopup from "../popup-loader.js";
+
+const addonId = location.search.substring(1);
+let components = await loadVueComponent([{
+  url: `popups/${addonId}/popup`,
+  params: await loadPopup(addonId),
+}]);
+
+const vue = new Vue({
+  el: "body",
+  components,
+  data: {addonId},
+});
