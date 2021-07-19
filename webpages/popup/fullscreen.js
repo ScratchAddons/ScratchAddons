fd3c990833d74d116e6640ab2b1796bc4209d53d
mxmou <mxmou@users.noreply.github.com>
2021-07-19 15:05:17 +0000

    Format code

--- webpages/popup/fullscreen.js
+++ webpages/popup/fullscreen.js
@@ -2,13 +2,15 @@ import loadVueComponent from "../../libraries/common/load-vue-components.js";
 import loadPopup from "../popup-loader.js";
 
 const addonId = location.search.substring(1);
-let components = await loadVueComponent([{
-  url: `popups/${addonId}/popup`,
-  params: await loadPopup(addonId),
-}]);
+let components = await loadVueComponent([
+  {
+    url: `popups/${addonId}/popup`,
+    params: await loadPopup(addonId),
+  },
+]);
 
 const vue = new Vue({
   el: "body",
   components,
-  data: {addonId},
+  data: { addonId },
 });
