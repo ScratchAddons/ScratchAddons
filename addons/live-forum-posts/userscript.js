7391b54e09f3b57c2cf6013e0419cbc67d7c8602
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-20 18:30:05 +0100

    Soft posting

--- addons/live-forum-posts/userscript.js
+++ addons/live-forum-posts/userscript.js
@@ -1,4 +1,5 @@
 export default async function ({ addon, global, console, msg }) {
+  var isFetching = false
   let sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
   let postContainer = document.querySelector("#djangobbindex");
@@ -6,11 +7,43 @@ export default async function ({ addon, global, console, msg }) {
   if (postEls.length === 20) return; // Return if no posts are avaliable to be loaded
   let posts = postEls.map((el) => ({ id: el.id.substr(1), el }));
 
-  while (true) {
-    await sleep(addon.settings.get("waitTime") * 1000);
-    let res = await fetch(location.href);
+  if (addon.settings.get('softPosting')) {
+    let submitButton = document.querySelector("button[name='AddPostForm']")
+    let markItUpEditor = document.getElementById('id_body')
+    submitButton.addEventListener('click', (e) => {
+      e.preventDefault()
+      let data = new FormData()
 
-    let html = await res.text();
+      data.append('csrfmiddlewaretoken', addon.auth.csrfToken)
+      data.append('body', markItUpEditor.value)
+      data.append('AddPostForm', '')
+
+      fetch(location.href, {
+        method: 'POST',
+        body: data,
+        credentials: 'include'
+      }).then(async res => {
+        if (res.url.split('#')[0] !== location.href.split('#')[0]) {
+          // We are now on the next page, go to the next page
+          console.log(res.url, location.href, res.url == location.href)
+        } else {
+          // TODO: handle 60 second waits
+          markItUpEditor.value = ''
+          await getNewPosts(await res.text())
+        }
+      })
+    })
+  }
+
+  function addPost(post) {
+    posts[posts.length - 1].el.insertAdjacentHTML("afterend", post.outerHTML);
+    scratchblocks.renderMatching(`#${post.id} pre.blocks`, { style: "scratch2" });
+    posts.push({ id: post.id.substr(1), el: document.getElementById(post.id) });
+  }
+
+  async function getNewPosts(prefetched) {
+    isFetching = true
+    let html = prefetched ? prefetched : await fetch(location.href).then(r => r.text());
 
     let parser = new DOMParser();
 
@@ -39,15 +72,19 @@ export default async function ({ addon, global, console, msg }) {
         continue;
       }
 
-      posts[posts.length - 1].el.insertAdjacentHTML("afterend", post.outerHTML);
-      scratchblocks.renderMatching(`#${post.id} pre.blocks`, { style: "scratch2" });
-      posts.push({ id: post.id.substr(1), el: document.getElementById(post.id) });
+      addPost(post)
     }
     if (gotPosts.length == 20) {
       // Update pagination divs
       let paginated = doc.querySelector(".pagination");
 
-      document.querySelectorAll(".paginated").innerHTML = paginated;
+      document.querySelectorAll(".paginated").forEach(e => (e.innerHTML = paginated))
     }
+
+    isFetching = false
+  }
+  while (true) {
+    await sleep(addon.settings.get("waitTime") * 1000);
+    await getNewPosts()
   }
 }
