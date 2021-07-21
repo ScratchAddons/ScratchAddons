01d6857eb092dfcb17795ade8ef14209dbcce9bc
mxmou <mxmou@users.noreply.github.com>
2021-07-21 17:29:41 +0000

    Format code

--- addons/editor-dark-mode/paper.js
+++ addons/editor-dark-mode/paper.js
@@ -11,17 +11,14 @@ export default async function ({ addon, console }) {
       artboardBackground = addon.settings.get("accent");
       workspaceBackground = alphaBlend(
         addon.settings.get("accent"),
-        multiply(addon.settings.get("primary"), { a: 0.1 }),
+        multiply(addon.settings.get("primary"), { a: 0.1 })
       );
-      checkerboardColor = alphaBlend(
-        addon.settings.get("accent"),
-        multiply(addon.settings.get("primary"), { a: 0.2 }),
-      );
+      checkerboardColor = alphaBlend(addon.settings.get("accent"), multiply(addon.settings.get("primary"), { a: 0.2 }));
       blueOutlineColor = textColor(
         addon.settings.get("primary"),
-        multiply(addon.settings.get("primary"), {r: 0.66, g: 0.76, b: 0.8}),
-        brighten(addon.settings.get("primary"), {r: 0.75, g: 0.75, b: 0.75}),
-        60,
+        multiply(addon.settings.get("primary"), { r: 0.66, g: 0.76, b: 0.8 }),
+        brighten(addon.settings.get("primary"), { r: 0.75, g: 0.75, b: 0.75 }),
+        60
       );
     } else {
       artboardBackground = "#ffffff";
@@ -41,12 +38,12 @@ export default async function ({ addon, console }) {
         layer._children[1].strokeColor = blueOutlineColor;
       }
     }
-  }
+  };
   addon.settings.addEventListener("change", updateColors);
   addon.self.addEventListener("disabled", updateColors);
   addon.self.addEventListener("reenabled", updateColors);
   while (true) {
-    await addon.tab.waitForElement("[class^=paper-canvas_paper-canvas_]", {markAsSeen: true});
+    await addon.tab.waitForElement("[class^=paper-canvas_paper-canvas_]", { markAsSeen: true });
     updateColors();
   }
 }
