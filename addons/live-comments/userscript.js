cd9bb210581de30b93a193d2a14dbe92aefc374a
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-21 08:10:42 +0100

    Update userscript.js

--- addons/live-comments/userscript.js
+++ addons/live-comments/userscript.js
@@ -2,29 +2,30 @@ export default async function ({ addon, global, console, msg }) {
   let wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
   const { redux } = addon.tab;
-  let [, type, id] = location.pathname.split("/");
-
-  if (type == "studios") {
-    await redux.waitForState((state) => redux.state.studio.infoStatus == "FETCHED", {
-      actions: ["SET_INFO"],
-    });
+  let [ , type, id ] = location.pathname.split('/')
+  
+  if (type == 'studios') {
+      await redux.waitForState(state => redux.state.studio.infoStatus == "FETCHED", {
+          actions: ['SET_INFO']
+      })
   } else {
-    await redux.waitForState((state) => state.preview.status.project === "FETCHED", {
-      actions: ["SET_PROJECT_INFO"],
-    });
+      await redux.waitForState((state) => state.preview.status.project === "FETCHED", {
+          actions: ['SET_PROJECT_INFO']
+      })
   }
+  
+  const URL = `https://api.scratch.mit.edu/${type == 'projects' ? `users/${redux.state.preview.projectInfo.author.username}/projects` : `studios`}/${id}/comments`
 
-  const URL = `https://api.scratch.mit.edu/${
-    type == "projects" ? `users/${redux.state.preview.projectInfo.author.username}/projects` : `studios`
-  }/${id}/comments`;
-
-  let getAllComments = () => [...redux.state.comments.comments];
+  let getAllComments = () => [...redux.state.comments.comments]
   let getAllReplies = () => {
-    let replies = [];
+      let replies = []
 
-    for (let re of Object.values(redux.state.comments.replies)) {
-      replies.push(...re);
-    }
+      for (let re of Object.values(redux.state.comments.replies)) {
+          replies.push(...re)
+      }
+
+      return replies
+  }
 
   var newest = Date.now()
 
@@ -62,10 +63,9 @@ export default async function ({ addon, global, console, msg }) {
           if (json.length < loopOffset) break
       }
 
-      if (json.length < loopOffset) break;
-    }
+      
+      let comments = [ ...redux.state.comments.comments ]
 
-<<<<<<< HEAD
       for (let k in comments) {
           let comment = comments[k]
           offset = -loopOffset
@@ -90,33 +90,12 @@ export default async function ({ addon, global, console, msg }) {
                   
                   redux.dispatch({ type: "ADD_NEW_COMMENT", comment: reply, topLevelCommentId: comment.id })
               }
-=======
-    let comments = [...redux.state.comments.comments];
 
-    for (let k in comments) {
-      let comment = comments[k];
-      offset = -loopOffset;
-      while (true) {
-        offset += loopOffset;
-        let res = await fetch(URL + `/${comment.id}/replies?offset=${offset}&limit=${loopOffset}`);
-        let json = (await res.json()).reverse();
-
-        if (json.length == 0) break;
-        for (let reply of json) {
-          let loadedComments = getAllReplies().map((c) => c.id);
->>>>>>> a62fcb7b1a146d6d130497cf24f389e864836567
-
-          if (loadedComments.indexOf(reply.id) !== -1) break;
-
-<<<<<<< HEAD
-  }
-=======
-          redux.dispatch({ type: "ADD_NEW_COMMENT", comment: reply, topLevelCommentId: comment.id });
-        }
->>>>>>> a62fcb7b1a146d6d130497cf24f389e864836567
-
-        if (json.length < loopOffset) break;
+              if (json.length < loopOffset) break
+          }
       }
-    }
-  }, 1000);
+
+  }
+
+  
 }
