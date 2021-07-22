3d4e574920466d606d1ecd04ad2b251bbba89050
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-22 15:37:22 +0000

    Format code

--- addons/redirect-mobile-forums/background.js
+++ addons/redirect-mobile-forums/background.js
@@ -1,14 +1,24 @@
 export default async function ({ addon, global, console, msg }) {
   // dynamicEnable/disable is pretty hacky in background
   let main = ({ url }) => {
-    let newURL = url.replace(/\/discuss\/m\/(.*)/g, "/discuss/$1")
+    let newURL = url.replace(/\/discuss\/m\/(.*)/g, "/discuss/$1");
     return {
-      redirectUrl: newURL
-    }
-  }
+      redirectUrl: newURL,
+    };
+  };
 
-  chrome.webRequest.onBeforeRequest.addListener(main, {urls: ["https://scratch.mit.edu/discuss/m/*"], types: ["main_frame"]}, ["blocking"])
+  chrome.webRequest.onBeforeRequest.addListener(
+    main,
+    { urls: ["https://scratch.mit.edu/discuss/m/*"], types: ["main_frame"] },
+    ["blocking"]
+  );
 
-  addon.self.addEventListener("disabled", () => chrome.webRequest.onBeforeRequest.removeListener(main))
-  addon.self.addEventListener("reenabled", () => chrome.webRequest.onBeforeRequest.addListener(main, {urls: ["https://scratch.mit.edu/discuss/m/*"], types: ["main_frame"]}, ["blocking"]))
+  addon.self.addEventListener("disabled", () => chrome.webRequest.onBeforeRequest.removeListener(main));
+  addon.self.addEventListener("reenabled", () =>
+    chrome.webRequest.onBeforeRequest.addListener(
+      main,
+      { urls: ["https://scratch.mit.edu/discuss/m/*"], types: ["main_frame"] },
+      ["blocking"]
+    )
+  );
 }
