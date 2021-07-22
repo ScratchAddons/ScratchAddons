0827064a9b543d81bcea89aa14e1c629e69fd59f
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-22 16:35:17 +0100

    Support dynamicEnable/dynamicDisable (addListener is kinda hacky)

--- addons/redirect-mobile-forums/background.js
+++ addons/redirect-mobile-forums/background.js
@@ -1,13 +1,14 @@
 export default async function ({ addon, global, console, msg }) {
-  chrome.webRequest.onBeforeRequest.addListener((details) => {
-    let url = details.url.replace(/\/discuss\/m\/(.*)/g, "/discuss/$1")
+  // dynamicEnable/disable is pretty hacky in background
+  let main = ({ url }) => {
+    let newURL = url.replace(/\/discuss\/m\/(.*)/g, "/discuss/$1")
     return {
-      redirectUrl: url
+      redirectUrl: newURL
     }
-  }, {
-    urls: [
-      "https://scratch.mit.edu/discuss/m/*"
-    ],
-    types: ["main_frame"]
-  }, ["blocking"])
+  }
+
+  chrome.webRequest.onBeforeRequest.addListener(main, {urls: ["https://scratch.mit.edu/discuss/m/*"], types: ["main_frame"]}, ["blocking"])
+
+  addon.self.addEventListener("disabled", () => chrome.webRequest.onBeforeRequest.removeListener(main))
+  addon.self.addEventListener("reenabled", () => chrome.webRequest.onBeforeRequest.addListener(main, {urls: ["https://scratch.mit.edu/discuss/m/*"], types: ["main_frame"]}, ["blocking"]))
 }
