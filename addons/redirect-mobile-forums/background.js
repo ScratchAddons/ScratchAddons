c256a465e3b1afa2b76968e4318acd794cb7af3b
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-22 15:09:16 +0000

    Format code

--- addons/redirect-mobile-forums/background.js
+++ addons/redirect-mobile-forums/background.js
@@ -1,13 +1,15 @@
 export default async function ({ addon, global, console, msg }) {
-  chrome.webRequest.onBeforeRequest.addListener((details) => {
-    let url = details.url.replace(/\/discuss\/m\/(.*)/g, "/discuss/$1")
-    return {
-      redirectUrl: url
-    }
-  }, {
-    urls: [
-      "https://scratch.mit.edu/discuss/m/*"
-    ],
-    types: ["main_frame"]
-  }, ["blocking"])
+  chrome.webRequest.onBeforeRequest.addListener(
+    (details) => {
+      let url = details.url.replace(/\/discuss\/m\/(.*)/g, "/discuss/$1");
+      return {
+        redirectUrl: url,
+      };
+    },
+    {
+      urls: ["https://scratch.mit.edu/discuss/m/*"],
+      types: ["main_frame"],
+    },
+    ["blocking"]
+  );
 }
