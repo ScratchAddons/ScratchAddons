42a3911da8d9174abb52e8bfd6d9984d98c0af69
mxmou <mxmou@users.noreply.github.com>
2021-07-26 11:58:42 +0000

    Format code

--- addons/editor-dark-mode/bubbles.js
+++ addons/editor-dark-mode/bubbles.js
@@ -155,7 +155,7 @@ export default async function ({ addon, console }) {
       renderer = newRenderer;
       overrideCreateTextSkin();
       return oldAttachRenderer(newRenderer);
-    }
+    };
   }
   addon.settings.addEventListener("change", () => {
     if (!usingModifiedCreateTextSkin && renderer !== undefined) {
