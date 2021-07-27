6b8bd8d5f0b26a68f4dfe01a17578558f12ede63
GarboMuffin <33787854+GarboMuffin@users.noreply.github.com>
2021-07-27 17:38:04 -0400

    editor-stage-left: fix a couple bugs (#3116)
    * editor-stage-left: update areas immediately if enabled late
    
    * editor-stage-left: dispatch global resize event
    
    * editor-stage-left: updateAreas -> resize
    
    * Format code
    
    Co-authored-by: GarboMuffin <GarboMuffin@users.noreply.github.com>

--- addons/editor-stage-left/fix-share-the-love.js
+++ addons/editor-stage-left/fix-share-the-love.js
@@ -1,11 +1,11 @@
 export default async function ({ addon, global, console }) {
   const ScratchBlocks = await addon.tab.traps.getBlockly();
-  const updateAreas = () => {
+  const resize = () => {
     const workspace = Blockly.getMainWorkspace();
-    if (workspace) workspace.recordCachedAreas();
+    if (workspace) window.dispatchEvent(new Event("resize"));
   };
-  addon.self.addEventListener("disabled", updateAreas);
-  addon.self.addEventListener("reenabled", updateAreas);
+  addon.self.addEventListener("disabled", resize);
+  addon.self.addEventListener("reenabled", resize);
   const originalGetClientRect = ScratchBlocks.Toolbox.prototype.getClientRect;
   ScratchBlocks.Toolbox.prototype.getClientRect = function () {
     // we are trying to undo the effect of BIG_NUM in https://github.com/LLK/scratch-blocks/blob/ab26fa2960643fa38fbc7b91ca2956be66055070/core/flyout_vertical.js#L739
@@ -15,4 +15,5 @@ export default async function ({ addon, global, console }) {
     rect.width -= 1000000000;
     return rect;
   };
+  if (addon.self.enabledLate) resize();
 }
