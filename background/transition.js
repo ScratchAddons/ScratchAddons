2a28e89876eb453e080a84e2647cf574a79f4396
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-28 19:48:35 +0000

    Format code

--- background/transition.js
+++ background/transition.js
@@ -1,21 +1,24 @@
 function getExtensionLocale() {
-  let locale = chrome.i18n.getUILanguage().toLowerCase()
-  if (locale.includes("-")) locale = locale.split("-")[0]
-  if (locale.startsWith('pt')) locale = 'pt-br'
-  return locale
+  let locale = chrome.i18n.getUILanguage().toLowerCase();
+  if (locale.includes("-")) locale = locale.split("-")[0];
+  if (locale.startsWith("pt")) locale = "pt-br";
+  return locale;
 }
 chrome.runtime.onInstalled.addListener(async (details) => {
-  const locale = getExtensionLocale()
+  const locale = getExtensionLocale();
   const currentVersion = chrome.runtime.getManifest().version;
   const [major, minor, _] = currentVersion.split(".");
   if (details.previousVersion && details.previousVersion.startsWith("0")) {
-    const url = locale == 'en' ? "https://scratchaddons.com/scratch-messaging-transition" : `https://scratchaddons.com/${locale}/scratch-messaging-transition`
+    const url =
+      locale == "en"
+        ? "https://scratchaddons.com/scratch-messaging-transition"
+        : `https://scratchaddons.com/${locale}/scratch-messaging-transition`;
     chrome.tabs.create({ url });
   } else if (
     details.reason === "install" &&
     chrome.runtime.getManifest().version_name.includes("-prerelease") === false
   ) {
-    const url = locale == 'en' ? "https://scratchaddons.com/welcome" : `https://scratchaddons.com/${locale}/welcome`
+    const url = locale == "en" ? "https://scratchaddons.com/welcome" : `https://scratchaddons.com/${locale}/welcome`;
     chrome.tabs.create({ url });
   }
 
@@ -26,7 +29,8 @@ chrome.runtime.onInstalled.addListener(async (details) => {
   }
 });
 if (chrome.runtime.getManifest().version_name.includes("-prerelease") === false) {
-  let locale = getExtensionLocale()
-  const uninstallURL = locale == 'en' ? "https://scratchaddons.com/farewell" : `https://scratchaddons.com/${locale}/farewell`
+  let locale = getExtensionLocale();
+  const uninstallURL =
+    locale == "en" ? "https://scratchaddons.com/farewell" : `https://scratchaddons.com/${locale}/farewell`;
   chrome.runtime.setUninstallURL(uninstallURL);
 }
