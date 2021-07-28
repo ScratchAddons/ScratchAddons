a4fb9e01e1edd60ff1fc76725c0a41fd70d65489
William Horning <w.horning27@gmail.com>
2021-07-28 16:45:09 -0400

    Better console

--- content-scripts/inject/run-userscript.js
+++ content-scripts/inject/run-userscript.js
@@ -6,18 +6,53 @@ export default async function runAddonUserscripts({ addonId, scripts, enabledLat
   for (const scriptInfo of scripts) {
     const { url: scriptPath, runAtComplete } = scriptInfo;
     const scriptUrl = `${new URL(import.meta.url).origin}/addons/${addonId}/${scriptPath}`;
-    console.log(
-      `%cDebug addons/${addonId}/${scriptPath}: ${scriptUrl}, runAtComplete: ${runAtComplete}`,
-      "color:red; font-weight: bold; font-size: 1.2em;"
-    );
     const loadUserscript = async () => {
       await scratchAddons.l10n.loadByAddonId(addonId);
       const module = await import(scriptUrl);
-      const log = _realConsole.log.bind(console, `%c[${addonId}]`, "color:darkorange; font-weight: bold;");
-      const warn = _realConsole.warn.bind(console, `%c[${addonId}]`, "color:darkorange font-weight: bold;");
       const msg = (key, placeholders) =>
         scratchAddons.l10n.get(key.startsWith("/") ? key.slice(1) : `${addonId}/${key}`, placeholders);
       msg.locale = scratchAddons.l10n.locale;
+      const betterConsole = {
+        _createOutput(...args) {
+          var argLength = args.length
+          var logContent = args.slice(0, argLength)
+          var logCategory = args[argLength - 1]
+          if (logCategory && argLength !== 1) return [`%cSA%c${logCategory}%c`, this._style.leftPrefix, this._style.rightPrefix, this._style.text, ...logContent]
+          else return [`%cSA%c`, this._style.singlePrefix, this._style.text, ...logContent]
+        },
+        _style: {
+          leftPrefix: "background:  #ff7b26; color: white; border-radius: 0.5rem 0 0 0.5rem; padding: 0 0.5rem",
+          rightPrefix: "background: #222; color: white; border-radius: 0 0.5rem 0.5rem 0; padding: 0 0.5rem",
+          singlePrefix: "background:  #ff7b26; color: white; border-radius: 0.5rem; padding: 0 0.5rem",
+          text: ""
+        },
+        log(...args) {
+          _realConsole.log(...this._createOutput(...args))
+        },
+        debug(...args) {
+          _realConsole.debug(...this._createOutput(...args))
+        },
+        error(...args) {
+          _realConsole.error(...this._createOutput(...args))
+        },
+        info(...args) {
+          _realConsole.info(...this._createOutput(...args))
+        },
+        warn(...args) {
+          _realConsole.warn(...this._createOutput(...args))
+        },
+        table(...args) {
+          _realConsole.table(...args)
+        }
+      }
+      const localConsole = {
+        log: (log)=>{return betterConsole.log(log,addonId)},
+        debug: (log)=>{return betterConsole.debug(log,addonId)},
+        error: (log)=>{return betterConsole.error(log,addonId)},
+        info: (log)=>{return betterConsole.info(log,addonId)},
+        warn: (log)=>{return betterConsole.warn(log,addonId)},
+        table: (log)=>{return betterConsole.table(log,addonId)}
+      }
       module.default({
         addon: addonObj,
         global: globalObj,
