04715b983b3f2c9a61154e1d15149377c060d1e2
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-29 13:03:44 +0100

    CSS instead of JS for remix and view count

--- addons/hide-project-stats/userscript.js
+++ addons/hide-project-stats/userscript.js
@@ -7,8 +7,6 @@ export default async function ({ addon, global, console, msg }) {
 
     addon.settings.get("loves") && (document.querySelector(".project-loves").innerText = "");
     addon.settings.get("favorites") && (document.querySelector(".project-favorites").innerText = "");
-    addon.settings.get("remixes") && document.querySelector(".project-remixes").remove();
-    addon.settings.get("views") && document.querySelector(".project-views").remove();
 
     // Loves & favorites mutate innerText; we use redux
 
