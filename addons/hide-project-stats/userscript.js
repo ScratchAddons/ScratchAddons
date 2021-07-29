6c2968e3ed0038b031ccdc8ef3262f59a7999ebf
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-29 11:00:45 +0100

    fixes

--- addons/hide-project-stats/userscript.js
+++ addons/hide-project-stats/userscript.js
@@ -1,17 +1,25 @@
 export default async function ({ addon, global, console, msg }) {
-    await addon.tab.waitForElement('.stats', {
-        reduxCondition: (state) => state.scratchGui.mode.isPlayerOnly
-    })
+    while (1) {
+        await addon.tab.waitForElement('.stats', {
+            reduxCondition: (state) => state.scratchGui.mode.isPlayerOnly,
+            markAsSeen: true
+        })
+    
+        addon.settings.get("loves") && (document.querySelector(".project-loves").innerText = '')
+        addon.settings.get("favorites") && (document.querySelector(".project-favorites").innerText = '')
+        addon.settings.get("remixes") && (document.querySelector(".project-remixes").remove())
+        addon.settings.get("views") && (document.querySelector(".project-views").remove())
+    
+        // Loves & favorites mutate innerText; we use redux
 
-    addon.settings.get("loves") && (document.querySelector(".project-loves").innerText = '')
-    addon.settings.get("favorites") && (document.querySelector(".project-favorites").innerText = '')
-    addon.settings.get("remixes") && (document.querySelector(".project-remixes").remove())
-    addon.settings.get("views") && (document.querySelector(".project-views").remove())
+        let listener = e => {
+            if (!state.scratchGui.mode.isPlayerOnly) return addon.tab.redux.removeEventListener('statechanged', listener)
+            let projectLoves = document.querySelector(".project-loves")
+            let projectFavorites = document.querySelector(".project-favorites")
+            projectLoves && (projectLoves.innerText = '')
+            projectFavorites && (projectFavorites.innerText = '')
+        }
 
-    // Loves & favorites mutate innerText; we use redux
-
-    addon.tab.redux.addEventListener('statechanged', e => {
-        document.querySelector(".project-loves").innerText = ''
-        document.querySelector(".project-favorites").innerText = ''
-    })
+        addon.tab.redux.addEventListener('statechanged', listener)
+    }
 }
