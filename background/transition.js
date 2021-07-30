792b41bb93988f965c73e1cf790916a26551d20b
WorldLanguages <mundofinky@gmail.com>
2021-07-30 18:09:35 -0300

    Add UTM tags to transition.js

--- background/transition.js
+++ background/transition.js
@@ -1,15 +1,16 @@
+const utm = `utm_source=extension&utm_medium=tabscreate&utm_campaign=v${chrome.runtime.getManifest().version}`;
 chrome.runtime.onInstalled.addListener(async (details) => {
   const uiLanguage = chrome.i18n.getUILanguage();
   const localeSlash = uiLanguage.startsWith("en-") ? "" : `${uiLanguage.split("-")[0]}/`;
   const currentVersion = chrome.runtime.getManifest().version;
   const [major, minor, _] = currentVersion.split(".");
   if (details.previousVersion && details.previousVersion.startsWith("0")) {
-    chrome.tabs.create({ url: `https://scratchaddons.com/${localeSlash}scratch-messaging-transition/` });
+    chrome.tabs.create({ url: `https://scratchaddons.com/${localeSlash}scratch-messaging-transition/?${utm}` });
   } else if (
     details.reason === "install" &&
     chrome.runtime.getManifest().version_name.includes("-prerelease") === false
   ) {
-    chrome.tabs.create({ url: `https://scratchaddons.com/${localeSlash}welcome/` });
+    chrome.tabs.create({ url: `https://scratchaddons.com/${localeSlash}welcome/?${utm}` });
   }
 
   if (details.reason === "install") {
@@ -19,5 +20,5 @@ chrome.runtime.onInstalled.addListener(async (details) => {
   }
 });
 if (chrome.runtime.getManifest().version_name.includes("-prerelease") === false) {
-  chrome.runtime.setUninstallURL(`https://scratchaddons.com/${localeSlash}farewell/`);
+  chrome.runtime.setUninstallURL(`https://scratchaddons.com/${localeSlash}farewell/${utm}`);
 }
