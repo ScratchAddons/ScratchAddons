9029a519fc9e02fcc149aa13d951976ad8188f82
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-07-29 20:17:55 -0500

    Initial commit

--- addons/msg-count-badge/background.js
+++ addons/msg-count-badge/background.js
@@ -1,12 +1,34 @@
 export default async function ({ addon, global, console, setTimeout, setInterval, clearTimeout, clearInterval }) {
+
   const setBadge = async () => {
+
     const msgCount = await addon.account.getMsgCount();
-    addon.badge.text = msgCount;
+
+    if (addon.settings.get("showOffline") && isNaN(parseInt(msgCount))) {
+
+      // Offline badge color
+      addon.badge.color = addon.settings.get("offlineColor");
+
+      // Offline badge indicator
+      addon.badge.text = addon.settings.get("offlineText");
+
+    } else {
+
+      // Regular badge color
+      addon.badge.color = addon.settings.get("countColor");
+
+      // Regular message count
+      addon.badge.text = msgCount;
+
+    };
   };
+  
   setBadge();
-  addon.badge.color = addon.settings.get("color");
+
   addon.settings.addEventListener("change", () => {
-    addon.badge.color = addon.settings.get("color");
+    setBadge();
   });
+
   setInterval(setBadge, 2500);
-}
+
+};
