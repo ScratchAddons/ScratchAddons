fb3902de24bb09bca520a2d3176714879540ff6f
CST1229 <68464103+CST1229@users.noreply.github.com>
2021-07-31 16:07:02 +0200

    Copy Code button in forums

---
+++ addons/forum-copy-code/userscript.js
@@ -0,0 +1,21 @@
+export default async function ({ addon, global, console, msg }) {
+	while (true) {
+		const codeBlock = await addon.tab.waitForElement("div.code", {
+			markAsSeen: true
+		}); //For every code block
+		
+		const copyCode = document.createElement("div"); //Div used to store the text
+		copyCode.className = "sa-copyCodeDiv"; //Class
+		addon.tab.displayNoneWhileDisabled(copyCode, { display: "block" }); //Dynamic disable
+		
+		const copyCodeButton = document.createElement("span"); //The actual button
+		copyCodeButton.className = "sa-copyCodeButton"; //Class
+		copyCodeButton.textContent = msg("copy-code"); //The text
+		copyCodeButton.setAttribute("onclick", 
+		"console.log('copied'); const codeBlockText = this.parentNode.nextSibling.children[0].textContent; var textArea = document.createElement('textarea'); document.body.appendChild(textArea); textArea.textContent = codeBlockText; textArea.select(); document.execCommand('copy'); textArea.remove();"
+		); //Code to copy the code
+		
+		copyCode.appendChild(copyCodeButton); //Add the copyCodeButton link to the copyCodeDiv
+		codeBlock.parentNode.insertBefore(copyCode, codeBlock); //And finally, add the copyCodeDiv next to the code block
+	}
+}
