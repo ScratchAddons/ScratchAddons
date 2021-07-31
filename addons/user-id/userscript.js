c482be7682b404ebbf1b028ee30fa4d83f65ff22
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-07-31 15:10:08 +0300

    Update addons/user-id/userscript.js
    Co-authored-by: FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>

--- addons/user-id/userscript.js
+++ addons/user-id/userscript.js
@@ -1,18 +1,11 @@
-export default async function ({ addon, global, console }) {
-// define the user's username header
-let usernameheader = document.getElementsByClassName("header-text")[0].firstElementChild
-
-let userid = document.createElement("font");
-userid.style.color = "lightgrey"
-userid.style.size = 2;
-
-// fetch the user's id by making a get request to api.scratch.mit.edu
-// location.pathname returns /users/USERNAME/
-fetch('https://api.scratch.mit.edu' + location.pathname)
-  .then(response => response.json())
-  // Add the ID of the response to the username header
-  .then(data => userid.textContent = "#" + data.id);
-
-// add the id next to the header
-usernameheader.appendChild(userid);
+export default function ({ addon }) {
+	let usernameHeader = document.querySelector("header-text").firstElementChild;
+	const userId = Object.assign(document.createElement("font"), {
+		innerText: `#${Scratch.INIT_DATA.PROFILE.model.userId}`,
+		className: "sa-user-id"
+	});
+	userId.style.color = "lightgrey";
+	userId.style.size = 2;
+	
+	usernameHeader.appendChild(userId)
 }
