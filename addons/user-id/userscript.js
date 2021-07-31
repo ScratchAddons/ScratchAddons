732360fb549939b05ac115df00e1dc5953978d56
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-07-31 11:49:41 +0300

    Update userscript.js

--- addons/user-id/userscript.js
+++ addons/user-id/userscript.js
@@ -2,10 +2,17 @@ export default async function ({ addon, global, console }) {
 // define the user's username header
 let usernameheader = document.getElementsByClassName("header-text")[0].firstElementChild
 
+let userid = document.createElement("font");
+userid.style.color = "lightgrey"
+userid.style.size = 2;
+
 // fetch the user's id by making a get request to api.scratch.mit.edu
 // location.pathname returns /users/USERNAME/
 fetch('https://api.scratch.mit.edu' + location.pathname)
   .then(response => response.json())
   // Add the ID of the response to the username header
-  .then(data => usernameheader.insertAdjacentHTML('beforeend', `<font size="3" color="lightgrey">#` + data.id + `</font>`));
+  .then(data => userid.textContent = "#" + data.id);
+
+// add the id next to the header
+usernameheader.appendChild(userid);
 }
