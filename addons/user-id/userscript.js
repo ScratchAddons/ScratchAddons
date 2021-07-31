40e5e7a7944757dcd909c5752071756a9afa15b3
CodeGuy92 <CodeGuy92@users.noreply.github.com>
2021-07-31 12:14:34 +0000

    Format code

--- addons/user-id/userscript.js
+++ addons/user-id/userscript.js
@@ -1,11 +1,11 @@
 export default function ({ addon }) {
-	let usernameHeader = document.querySelector("header-text").firstElementChild;
-	const userId = Object.assign(document.createElement("span"), {
-		innerText: `#${Scratch.INIT_DATA.PROFILE.model.userId}`,
-		className: "sa-user-id"
-	});
-	userId.style.color = "lightgrey";
-	userId.style.size = 2;
-	
-	usernameHeader.appendChild(userId)
+  let usernameHeader = document.querySelector("header-text").firstElementChild;
+  const userId = Object.assign(document.createElement("span"), {
+    innerText: `#${Scratch.INIT_DATA.PROFILE.model.userId}`,
+    className: "sa-user-id",
+  });
+  userId.style.color = "lightgrey";
+  userId.style.size = 2;
+
+  usernameHeader.appendChild(userId);
 }
