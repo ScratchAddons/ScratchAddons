b4e851a675784a9c03cb6b7153fab1f2df1096f3
WorldLanguages <mundofinky@gmail.com>
2021-08-02 18:00:06 -0300

    Replace project *after* saving to cloud to avoid corrupt stage-less projects

--- addons/default-project/userscript.js
+++ addons/default-project/userscript.js
@@ -1,34 +1,32 @@
 export default async function ({ addon, global, console, msg }) {
   let pendingReplacement = false;
-  const replace = async () => {
-    // Current loadingState is LOADING_VM_NEW_DEFAULT
-
-    if (pendingReplacement) {
-      // Never happens AFAIK
-      console.log("Pending replacement");
-      return;
-    }
-    pendingReplacement = true;
-
-    const isLoggedIn = await addon.auth.fetchIsLoggedIn();
-    if (isLoggedIn) {
-      await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "SHOWING_WITH_ID");
-    } else {
-      // If the user is logged out, SHOWING_WITHOUT_ID will be the last state.
-      await addon.tab.redux.waitForState(
-        (state) => state.scratchGui.projectState.loadingState === "SHOWING_WITHOUT_ID"
-      );
-    }
-
-    const projectId = addon.settings.get("projectId");
-    if (projectId !== 510186917) addon.tab.traps.vm.downloadProjectId(projectId);
-    pendingReplacement = false;
-  };
-
-  if (addon.tab.redux.state.scratchGui.projectState.loadingState === "LOADING_VM_NEW_DEFAULT") replace();
-
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", async (e) => {
-    if (e.detail.action.type === "scratch-gui/project-state/DONE_FETCHING_DEFAULT") replace();
+    if (e.detail.action.type === "scratch-gui/project-state/DONE_LOADING_VM_WITHOUT_ID") {
+      // Current loadingState is SHOWING_WITHOUT_ID
+
+      if (pendingReplacement) {
+        // Never happens AFAIK
+        console.log("Pending replacement");
+        return;
+      }
+      pendingReplacement = true;
+
+      let expired = false; // So that nothing goes catastrophically wrong
+      setTimeout(() => (expired = true), 10000);
+
+      const isLoggedIn = await addon.auth.fetchIsLoggedIn();
+      if (isLoggedIn) {
+        await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "CREATING_NEW");
+        await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "SHOWING_WITH_ID");
+        await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "AUTO_UPDATING");
+        await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "SHOWING_WITH_ID");
+        // By this point, vanilla new project was saved to cloud
+      }
+
+      const projectId = addon.settings.get("projectId");
+      if (projectId !== 510186917 && !expired) addon.tab.traps.vm.downloadProjectId(projectId);
+      pendingReplacement = false;
+    }
   });
 }
