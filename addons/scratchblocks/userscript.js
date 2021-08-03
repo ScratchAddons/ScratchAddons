868090b9d8d8a2879eef0b59b0e433c1fa4cee59
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-08-03 14:15:39 +0100

    l10n

--- addons/scratchblocks/userscript.js
+++ addons/scratchblocks/userscript.js
@@ -1,4 +1,5 @@
 export default async function ({ addon, global, console, msg }) {
+  window.scratchblocks3Enabled = true
   // thanks cubey
   function scale(svg, factor) {
     svg.setAttribute("width", svg.getAttribute("width") * factor);
@@ -6,14 +7,46 @@ export default async function ({ addon, global, console, msg }) {
   }
   // document.querySelectorAll("pre.blocks").forEach((e) => (e.innerHTML = "Loading"));
   await addon.tab.loadScript(addon.self.lib + "/thirdparty/cs/scratchblocks-v3.5.2-min.js"); // load new scratchblocks
+  await addon.tab.loadScript(addon.self.lib + "/thirdparty/cs/translations-all-v3.5.2.js") // load translations
 
   document.querySelectorAll("pre.blocks").forEach((el) => {
     el.innerHTML = ""; // clear html
     el.innerText = el.getAttribute("data-original");
   });
 
+  const forumId = /\d+/.exec(document.querySelector(".linkst li:nth-child(2) a").href)[0]
+  const forumIdToLang = {
+    13: "de",
+    14: "es",
+    15: "fr",
+    16: ["zh-cn", "zh-tw"],
+    17: "pl",
+    18: "ja",
+    19: "nl",
+    20: "pt",
+    21: "it",
+    22: "he",
+    23: "ko",
+    24: "nb",
+    25: "tr",
+    26: "el",
+    27: "ru",
+    33: "ca",
+    36: "id",
+    59: "fa",
+  };
+  let lang = ["en"];
+  if (forumIdToLang[forumId]) {
+    if (Array.isArray(forumIdToLang[forumId])) {
+      lang = lang.concat(forumIdToLang[forumId]);
+    } else {
+      lang = ["en", forumIdToLang[forumId]];
+    }
+  }
+
+
   scratchblocks.renderMatching("pre.blocks", {
-    languages: ["en"],
+    languages: lang,
     style: "scratch3",
   });
 
