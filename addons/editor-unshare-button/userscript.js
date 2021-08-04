b9651a390946837f5f9f5950330cbcb22d9665aa
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-08-04 15:35:06 +0000

    Format code

--- addons/editor-unshare-button/userscript.js
+++ addons/editor-unshare-button/userscript.js
@@ -46,7 +46,7 @@ export default async function ({ addon, msg, global, console }) {
             status: "FETCHED",
           });
 
-          button.classList.remove('sa-unshare-button')
+          button.classList.remove("sa-unshare-button");
           redux.dispatch({
             type: "UPDATE_PROJECT_INFO",
             info: {
