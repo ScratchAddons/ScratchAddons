4c345a5ea4229144eb36a1d16fd34c0d545a7ad6
Thomas Weber <weber_t@mailbox.org>
2021-08-04 17:44:25 -0500

    Add some options

--- addons/paint-by-default/userscript.js
+++ addons/paint-by-default/userscript.js
@@ -1,4 +1,33 @@
 export default async function ({ addon, console }) {
+  const getButtonToClick = (mainButton) => {
+    let index;
+    const indexes = {
+      upload: 0,
+      surprise: 1,
+      paint: 2,
+      record: 2,
+      library: 3
+    };
+    const assetPanelWrapper = mainButton.closest("[class*=asset-panel_wrapper_]");
+    if (assetPanelWrapper) {
+      if (assetPanelWrapper.querySelector("[class*=sound-editor_editor-container_]")) {
+        index = indexes[addon.settings.get("sound")];
+      } else {
+        index = indexes[addon.settings.get("costume")];
+      }
+    } else {
+      index = indexes[addon.settings.get("sprite")];
+    }
+    if (typeof index !== 'number') {
+      // should never happen
+      return;
+    }
+    const moreButtonsElement = mainButton.parentElement.querySelector('[class*="action-menu_more-buttons_"]');
+    const moreButtons = moreButtonsElement.children;
+    // Search from end of array for compatibility with better-img-uploads
+    const buttonToClick = moreButtons[moreButtons.length - (4 - index)];
+    return buttonToClick;
+  };
   document.body.addEventListener(
     "click",
     (e) => {
@@ -6,19 +35,16 @@ export default async function ({ addon, console }) {
         return;
       }
       const mainButton = e.target.closest('[class*="action-menu_main-button_"]');
-      if (mainButton) {
-        const assetPanelWrapper = mainButton.closest("[class*=asset-panel_wrapper_]");
-        if (assetPanelWrapper && assetPanelWrapper.querySelector("[class*=sound-editor_editor-container_]")) {
-          return;
-        }
-        e.stopPropagation();
-        const moreButtonsElement = mainButton.parentElement.querySelector('[class*="action-menu_more-buttons_"]');
-        const moreButtons = moreButtonsElement.children;
-        // use "second from last" to find the paint button for compatibility with HD image uploads
-        const buttonToClick = moreButtons[moreButtons.length - 2];
-        const elementToClick = buttonToClick.querySelector("button");
-        elementToClick.click();
+      if (!mainButton) {
+        return;
       }
+      const buttonToClick = getButtonToClick(mainButton);
+      if (!buttonToClick) {
+        return;
+      }
+      e.stopPropagation();
+      const elementToClick = buttonToClick.querySelector("button");
+      elementToClick.click();
     },
     {
       bubble: true,
