de4fab1f099ea6da29bfff28ada63b13204bb373
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-08-08 23:46:46 +0100

    Add some dynamicEnable/disable to some addons (#3192)
    * studio-followers: dynamicEnable/disable
    
    * semicolon: dynamicEnable/disable
    
    * studio-tools: dynamicEnable/disable
    
    * Format code
    
    * i should move my strange debugging
    
    * Revert back to addon.auth.fetchUsername
    
    * true-youtube-links: dynamicEnable/disable
    
    * Format code
    
    * remove unneeded comment
    
    * no dynamicEnable/disable for both youtube addons
    
    * studio-tools: hide "leave studio" button on disable
    
    * Use displayNoneWhileDisabled properly in studio-followers
    
    Co-authored-by: FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
    Co-authored-by: WorldLanguages <mundofinky@gmail.com>

--- addons/studio-tools/migrated.js
+++ addons/studio-tools/migrated.js
@@ -30,6 +30,7 @@ export default async ({ addon, console, msg }) => {
 
     const adderSec = document.createElement("div");
     adderSec.className = "studio-adder-section";
+    addon.tab.displayNoneWhileDisabled(adderSec);
 
     const adderHeader = document.createElement("h3");
     const adderHeaderSpan = document.createElement("span");
@@ -184,6 +185,7 @@ export default async ({ addon, console, msg }) => {
       const studioInfo = document.querySelector(".studio-info");
       const followButton = document.querySelector(".studio-follow-button");
       studioInfo.insertBefore(leaveSection, followButton.parentNode.nextSibling);
+      addon.tab.displayNoneWhileDisabled(leaveSection);
     }
   };
   render();
