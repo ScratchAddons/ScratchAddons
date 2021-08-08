b6123090fa4787b8f1429dbfb9849e0e7086e91f
WorldLanguages <mundofinky@gmail.com>
2021-08-08 13:54:20 -0300

    Keep old setting ID unchanged

--- addons/msg-count-badge/background.js
+++ addons/msg-count-badge/background.js
@@ -10,7 +10,7 @@ export default async function ({ addon, global, console, setTimeout, setInterval
       addon.badge.text = addon.settings.get("offlineText");
     } else {
       // Regular badge color
-      addon.badge.color = addon.settings.get("countColor");
+      addon.badge.color = addon.settings.get("color");
 
       // Regular message count
       addon.badge.text = msgCount;
