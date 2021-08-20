8cc9b1cf8c6eb07fb53cb2923f63d4baa16e6ea5
World_Languages <mundofinky@gmail.com>
2021-08-20 12:42:49 -0300

    v1.19 update notification (#3333)
    * v1.19 update notification
    
    * Update version number for debugger feedback

--- content-scripts/cs.js
+++ content-scripts/cs.js
@@ -505,7 +505,7 @@ const showBanner = () => {
   });
   const notifInnerText1 = Object.assign(document.createElement("span"), {
     style: NOTIF_TEXT_STYLE,
-    innerHTML: escapeHTML(chrome.i18n.getMessage("extensionUpdateInfo1_v1_18", DOLLARS)).replace(
+    innerHTML: escapeHTML(chrome.i18n.getMessage("extensionUpdateInfo1_v1_19", DOLLARS)).replace(
       /\$(\d+)/g,
       (_, i) =>
         [
@@ -524,7 +524,7 @@ const showBanner = () => {
   });
   const notifInnerText2 = Object.assign(document.createElement("span"), {
     style: NOTIF_TEXT_STYLE,
-    textContent: chrome.i18n.getMessage("extensionUpdateInfo2_v1_18"),
+    textContent: chrome.i18n.getMessage("extensionUpdateInfo2_v1_19"),
   });
   const notifFooter = Object.assign(document.createElement("span"), {
     style: NOTIF_TEXT_STYLE,
