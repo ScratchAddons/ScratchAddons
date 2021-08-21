5db490320729a2a14014989be5a5ed42b87110e1
GarboMuffin <GarboMuffin@users.noreply.github.com>
2021-08-21 19:39:30 +0000

    Format code

--- addons/disable-paste-offset/userscript.js
+++ addons/disable-paste-offset/userscript.js
@@ -1,8 +1,11 @@
 export default async function ({ addon, console }) {
   addon.tab.redux.initialize();
-  addon.tab.redux.addEventListener("statechanged", ({detail}) => {
+  addon.tab.redux.addEventListener("statechanged", ({ detail }) => {
     if (addon.self.disabled) return;
-    if (detail.action.type === "scratch-paint/clipboard/SET" || detail.action.type === "scratch-paint/clipboard/INCREMENT_PASTE_OFFSET") {
+    if (
+      detail.action.type === "scratch-paint/clipboard/SET" ||
+      detail.action.type === "scratch-paint/clipboard/INCREMENT_PASTE_OFFSET"
+    ) {
       addon.tab.redux.state.scratchPaint.clipboard.pasteOffset = 0;
     }
   });
