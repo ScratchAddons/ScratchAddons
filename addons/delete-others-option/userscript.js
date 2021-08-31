818db96d6f9345e8030c3642c62c8f90b3077fd0
Joeclinton1 <Joeclinton1@users.noreply.github.com>
2021-08-31 15:53:29 +0000

    Format code

--- addons/delete-others-option/userscript.js
+++ addons/delete-others-option/userscript.js
@@ -1,63 +1,63 @@
 import { insertAfter, removeClassContainingText, getAncestorWithClass, getAncestorWithId } from "./utils.js";
 
 export default async ({ addon, console }) => {
-    function deleteOtherCostumes(contextMenu) {
-        //prompt user with confirmation dialog
-        if (!confirm("Are you sure you want to delete all other costumes?")) {
-            return;
-        }
-
-        // this works for both costumes and backdrops since backdrops use costumes under the hood.
-        const currentCostumeSelector = contextMenu.parentNode.parentNode;
-        const costumeSelectorsContainer = currentCostumeSelector.parentNode;
-        const costumeSelectors = Array.from(costumeSelectorsContainer.children);
-        for (var i = costumeSelectors.length - 1; i >= 0; i--) {
-            const costumeSelector = costumeSelectors[i];
-            if (costumeSelector !== currentCostumeSelector) {
-                contextMenu = costumeSelector.querySelector(".react-contextmenu");
-                const restoreCostumeFun = addon.tab.traps.vm.deleteCostume(i);
-                addon.tab.redux.dispatch({
-                    type: "scratch-gui/restore-deletion/RESTORE_UPDATE",
-                    state: { restoreFun: restoreCostumeFun, deletedItem: "Costume" },
-                });
-            }
-        }
+  function deleteOtherCostumes(contextMenu) {
+    //prompt user with confirmation dialog
+    if (!confirm("Are you sure you want to delete all other costumes?")) {
+      return;
     }
 
-    function addDeleteOthersOption(contextMenu) {
-        const existingDeleteOthers = contextMenu.querySelector(".delete-others");
-        if (existingDeleteOthers) {
-            return;
-        }
+    // this works for both costumes and backdrops since backdrops use costumes under the hood.
+    const currentCostumeSelector = contextMenu.parentNode.parentNode;
+    const costumeSelectorsContainer = currentCostumeSelector.parentNode;
+    const costumeSelectors = Array.from(costumeSelectorsContainer.children);
+    for (var i = costumeSelectors.length - 1; i >= 0; i--) {
+      const costumeSelector = costumeSelectors[i];
+      if (costumeSelector !== currentCostumeSelector) {
+        contextMenu = costumeSelector.querySelector(".react-contextmenu");
+        const restoreCostumeFun = addon.tab.traps.vm.deleteCostume(i);
+        addon.tab.redux.dispatch({
+          type: "scratch-gui/restore-deletion/RESTORE_UPDATE",
+          state: { restoreFun: restoreCostumeFun, deletedItem: "Costume" },
+        });
+      }
+    }
+  }
 
-        const deleteNode = contextMenu.querySelectorAll(".react-contextmenu-item")[2];
-        if (deleteNode) {
-            const deleteOthersNode = deleteNode.cloneNode();
-            deleteOthersNode.textContent = "delete others";
-            deleteOthersNode.classList.add("delete-others");
-            removeClassContainingText(deleteOthersNode.classList, "border");
-            deleteOthersNode.addEventListener("click", (e) => {
-                deleteOtherCostumes(contextMenu);
-            });
-
-            insertAfter(deleteOthersNode, deleteNode);
-        }
+  function addDeleteOthersOption(contextMenu) {
+    const existingDeleteOthers = contextMenu.querySelector(".delete-others");
+    if (existingDeleteOthers) {
+      return;
     }
 
-    document.addEventListener("contextmenu", (e) => {
-        // check user right clicked on a context menu inside react tab 3
-        const contextMenuWrapper = getAncestorWithClass(e.target, "react-contextmenu-wrapper");
-        const tab = getAncestorWithId(e.target, "react-tabs-3");
-        if (!contextMenuWrapper || !tab) {
-            return;
-        }
+    const deleteNode = contextMenu.querySelectorAll(".react-contextmenu-item")[2];
+    if (deleteNode) {
+      const deleteOthersNode = deleteNode.cloneNode();
+      deleteOthersNode.textContent = "delete others";
+      deleteOthersNode.classList.add("delete-others");
+      removeClassContainingText(deleteOthersNode.classList, "border");
+      deleteOthersNode.addEventListener("click", (e) => {
+        deleteOtherCostumes(contextMenu);
+      });
 
-        //Check more than one costumeSelector exists
-        const costumeSelectors = contextMenuWrapper.parentNode.parentNode.children;
-        if (costumeSelectors.length < 2) {
-            return;
-        }
+      insertAfter(deleteOthersNode, deleteNode);
+    }
+  }
 
-        addDeleteOthersOption(contextMenuWrapper.querySelector(".react-contextmenu"));
-    });
+  document.addEventListener("contextmenu", (e) => {
+    // check user right clicked on a context menu inside react tab 3
+    const contextMenuWrapper = getAncestorWithClass(e.target, "react-contextmenu-wrapper");
+    const tab = getAncestorWithId(e.target, "react-tabs-3");
+    if (!contextMenuWrapper || !tab) {
+      return;
+    }
+
+    //Check more than one costumeSelector exists
+    const costumeSelectors = contextMenuWrapper.parentNode.parentNode.children;
+    if (costumeSelectors.length < 2) {
+      return;
+    }
+
+    addDeleteOthersOption(contextMenuWrapper.querySelector(".react-contextmenu"));
+  });
 };
