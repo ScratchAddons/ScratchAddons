14d55e67d8fb2d64f4bef569285926de8cef9b26
Joeclinton1 <Joeclinton1@users.noreply.github.com>
2021-08-31 15:07:51 +0000

    Format code

--- addons/delete-others-option/utils.js
+++ addons/delete-others-option/utils.js
@@ -1,37 +1,40 @@
-
 export function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 export function queryByText(node, text) {
-    const childNodes = Array.from(node.childNodes)
-    return childNodes.find((childNode) => childNode.innerText === text);
+  const childNodes = Array.from(node.childNodes);
+  return childNodes.find((childNode) => childNode.innerText === text);
 }
 
 export function removeClassContainingText(classList, text) {
-    const classArray = Array.from(classList);
-    const classContaingText = classArray.find((nodeClass) => nodeClass.includes(text))
-    if (classContaingText) {
-        classList.remove(classContaingText)
-    }
+  const classArray = Array.from(classList);
+  const classContaingText = classArray.find((nodeClass) => nodeClass.includes(text));
+  if (classContaingText) {
+    classList.remove(classContaingText);
+  }
 }
 
 export function getAncestorWithClass(node, className) {
-    while (true) {
-        node = node.parentNode;
-        if (!node) { return false; }
-        if (node.classList.contains(className)) {
-            return node;
-        }
+  while (true) {
+    node = node.parentNode;
+    if (!node) {
+      return false;
     }
+    if (node.classList.contains(className)) {
+      return node;
+    }
+  }
 }
 
 export function getAncestorWithId(node, id) {
-    while (true) {
-        node = node.parentNode;
-        if (!node) { return false; }
-        if (node.id === id) {
-            return node;
-        }
+  while (true) {
+    node = node.parentNode;
+    if (!node) {
+      return false;
     }
+    if (node.id === id) {
+      return node;
+    }
+  }
 }
