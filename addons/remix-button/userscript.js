f3ffead7ec327137bae8c485ca7831c054bdfe0e
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-09-01 23:46:33 +0100

    Remix your own projects (#3331)
    * Remix your own projects
    
    * Update addons/remix-button/userscript.js
    
    Co-authored-by: GrahamSH <grahamshllk@gmail.com>
    
    * Use beforeRemixButton shared space
    
    * Change tags, dynamic disable and enable, tweak addon name and desc
    
    * Do not log error in console if projectInfo is empty object
    
    * Format code
    
    * Slightly change description
    
    Co-authored-by: FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
    Co-authored-by: GrahamSH <grahamshllk@gmail.com>
    Co-authored-by: WorldLanguages <mundofinky@gmail.com>
    Co-authored-by: WorldLanguages <WorldLanguages@users.noreply.github.com>

---
+++ addons/remix-button/userscript.js
@@ -0,0 +1,41 @@
+export default async function ({ addon, msg, global, console }) {
+  const isLoggedIn = await addon.auth.fetchIsLoggedIn();
+  if (!isLoggedIn) return;
+  const username = await addon.auth.fetchUsername();
+  const { redux } = addon.tab;
+
+  await redux.waitForState(
+    (state) => state.preview.status.project === "FETCHED" && state.preview.projectInfo.author?.username === username
+  );
+  while (true) {
+    const seeInsideButton = await addon.tab.waitForElement(".project-buttons .see-inside-button", {
+      markAsSeen: true,
+      reduxCondition: (state) => (state.scratchGui ? state.scratchGui.mode.isPlayerOnly : true),
+    });
+
+    const button = document.createElement("button");
+    button.className = "button remix-button sa-remix-button";
+
+    const remixButtonAlt = addon.tab.scratchMessage("project.remixButton.altText");
+    button.setAttribute("title", remixButtonAlt);
+    button.setAttribute("alt", remixButtonAlt);
+    button.addEventListener("click", (e) => {
+      redux.dispatch({
+        type: "scratch-gui/project-state/START_REMIXING",
+      });
+      redux.dispatch({
+        type: "RESET_COMMENTS",
+      });
+
+      button.classList.add("remixing", "disabled");
+    });
+
+    const inner = document.createElement("span");
+    inner.innerText = addon.tab.scratchMessage("project.remixButton");
+
+    button.append(inner);
+
+    addon.tab.appendToSharedSpace({ space: "beforeRemixButton", element: button, order: 9 });
+    addon.tab.displayNoneWhileDisabled(button);
+  }
+}
