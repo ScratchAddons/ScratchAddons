3d2eb696158a462297518c7450cb317ce067b3f4
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-09-01 10:48:29 +0100

    Update forum.js

--- addons/exact-count/forum.js
+++ addons/exact-count/forum.js
@@ -2,7 +2,7 @@ export default async function ({ addon, global, console }) {
   while (true) {
     let userbox = await addon.tab.waitForElement(".postleft dl", { markAsSeen: true });
     if (userbox.childNodes[6] && userbox.childNodes[6].nodeValue.includes("+")) {
-      const postCountReal = Number(/(\d\d?\d?\d?)\+? posts?/.exec(userbox.childNodes[6].nodeValue)[1]);
+      const postCountReal = Number(/(\d+)\+? posts?/.exec(userbox.childNodes[6].nodeValue)[1]);
       fetch("https://scratchdb.lefty.one/v3/forum/user/info/" + userbox.querySelector("a").innerText)
         .then((response) => response.json())
         .then((data) => {
