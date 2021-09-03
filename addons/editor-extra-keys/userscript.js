ffb57430fc26f2ace53a523ae5b3c8f8ad864db5
Tacodude7729 <Tacodude7729@users.noreply.github.com>
2021-09-03 23:11:22 +0000

    Format code

--- addons/editor-extra-keys/userscript.js
+++ addons/editor-extra-keys/userscript.js
@@ -2,50 +2,49 @@ export default async function ({ addon, global, cons, msg }) {
   const Blockly = await addon.tab.traps.getBlockly();
 
   function appendKeys(keys) {
-    keys.push(...[
-      ['`', '`'],
-      ['-', '-'],
-      ['=', '='],
-      ['[', '['],
-      [']', ']'],
-      ['\\', '\\'],
-      [';', ';'],
-      ['\'', '\''],
-      [',', ','],
-      ['.', '.'],
-      ['/', '/']
-    ])
-    keys.splice(5, 0, [msg("enter-key"), 'enter']);
+    keys.push(
+      ...[
+        ["`", "`"],
+        ["-", "-"],
+        ["=", "="],
+        ["[", "["],
+        ["]", "]"],
+        ["\\", "\\"],
+        [";", ";"],
+        ["'", "'"],
+        [",", ","],
+        [".", "."],
+        ["/", "/"],
+      ]
+    );
+    keys.splice(5, 0, [msg("enter-key"), "enter"]);
   }
 
-  Blockly.Blocks['sensing_keyoptions'] = {
+  Blockly.Blocks["sensing_keyoptions"] = {
     jsonInitOriginal: undefined,
-    initOriginal: Blockly.Blocks['sensing_keyoptions'].init,
+    initOriginal: Blockly.Blocks["sensing_keyoptions"].init,
     init: function () {
-      if (this.jsonInitOriginal === undefined)
-        this.jsonInitOriginal = this.jsonInit;
+      if (this.jsonInitOriginal === undefined) this.jsonInitOriginal = this.jsonInit;
       this.jsonInit = function (obj) {
         appendKeys(obj.args0[0].options);
         this.jsonInitOriginal(obj);
-      }
+      };
       this.initOriginal();
-    }
-  }
+    },
+  };
 
-
-  Blockly.Blocks['event_whenkeypressed'] = {
+  Blockly.Blocks["event_whenkeypressed"] = {
     jsonInitOriginal: undefined,
-    initOriginal: Blockly.Blocks['event_whenkeypressed'].init,
+    initOriginal: Blockly.Blocks["event_whenkeypressed"].init,
     init: function () {
-      if (this.jsonInitOriginal === undefined)
-        this.jsonInitOriginal = this.jsonInit;
+      if (this.jsonInitOriginal === undefined) this.jsonInitOriginal = this.jsonInit;
       this.jsonInit = function (obj) {
         appendKeys(obj.args0[0].options);
         this.jsonInitOriginal(obj);
-      }
+      };
       this.initOriginal();
-    }
-  }
+    },
+  };
 
   addon.tab.traps.vm.emitWorkspaceUpdate();
 }
