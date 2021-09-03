fceaed641555b76ad0ca8718a6eca9b0fb699702
Tacodude <27910867+Tacodude7729@users.noreply.github.com>
2021-09-04 09:10:39 +1000

    Append keys instead of overwriting them.

--- addons/editor-extra-keys/userscript.js
+++ addons/editor-extra-keys/userscript.js
@@ -1,149 +1,51 @@
 export default async function ({ addon, global, cons, msg }) {
   const Blockly = await addon.tab.traps.getBlockly();
 
-  Blockly.Blocks["sensing_keyoptions"] = {
-    init: function () {
-      this.jsonInit({
-        message0: "%1",
-        args0: [
-          {
-            type: "field_dropdown",
-            name: "KEY_OPTION",
-            options: [
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_SPACE, "space"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_UP, "up arrow"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_DOWN, "down arrow"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_RIGHT, "right arrow"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_LEFT, "left arrow"],
-              [msg("enter-key"), "enter"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_ANY, "any"],
-              ["a", "a"],
-              ["b", "b"],
-              ["c", "c"],
-              ["d", "d"],
-              ["e", "e"],
-              ["f", "f"],
-              ["g", "g"],
-              ["h", "h"],
-              ["i", "i"],
-              ["j", "j"],
-              ["k", "k"],
-              ["l", "l"],
-              ["m", "m"],
-              ["n", "n"],
-              ["o", "o"],
-              ["p", "p"],
-              ["q", "q"],
-              ["r", "r"],
-              ["s", "s"],
-              ["t", "t"],
-              ["u", "u"],
-              ["v", "v"],
-              ["w", "w"],
-              ["x", "x"],
-              ["y", "y"],
-              ["z", "z"],
-              ["0", "0"],
-              ["1", "1"],
-              ["2", "2"],
-              ["3", "3"],
-              ["4", "4"],
-              ["5", "5"],
-              ["6", "6"],
-              ["7", "7"],
-              ["8", "8"],
-              ["9", "9"],
-              ["`", "`"],
-              ["-", "-"],
-              ["=", "="],
-              ["[", "["],
-              ["]", "]"],
-              ["\\", "\\"],
-              [";", ";"],
-              ["'", "'"],
-              [",", ","],
-              [".", "."],
-              ["/", "/"],
-            ],
-          },
-        ],
-        extensions: ["colours_sensing", "output_string"],
-      });
-    },
-  };
+  function appendKeys(keys) {
+    keys.push(...[
+      ['`', '`'],
+      ['-', '-'],
+      ['=', '='],
+      ['[', '['],
+      [']', ']'],
+      ['\\', '\\'],
+      [';', ';'],
+      ['\'', '\''],
+      [',', ','],
+      ['.', '.'],
+      ['/', '/']
+    ])
+    keys.splice(5, 0, [msg("enter-key"), 'enter']);
+  }
 
-  Blockly.Blocks["event_whenkeypressed"] = {
+  Blockly.Blocks['sensing_keyoptions'] = {
+    jsonInitOriginal: undefined,
+    initOriginal: Blockly.Blocks['sensing_keyoptions'].init,
     init: function () {
-      this.jsonInit({
-        id: "event_whenkeypressed",
-        message0: Blockly.Msg.EVENT_WHENKEYPRESSED,
-        args0: [
-          {
-            type: "field_dropdown",
-            name: "KEY_OPTION",
-            options: [
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_SPACE, "space"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_UP, "up arrow"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_DOWN, "down arrow"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_RIGHT, "right arrow"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_LEFT, "left arrow"],
-              [msg("enter-key"), "enter"],
-              [Blockly.Msg.EVENT_WHENKEYPRESSED_ANY, "any"],
-              ["a", "a"],
-              ["b", "b"],
-              ["c", "c"],
-              ["d", "d"],
-              ["e", "e"],
-              ["f", "f"],
-              ["g", "g"],
-              ["h", "h"],
-              ["i", "i"],
-              ["j", "j"],
-              ["k", "k"],
-              ["l", "l"],
-              ["m", "m"],
-              ["n", "n"],
-              ["o", "o"],
-              ["p", "p"],
-              ["q", "q"],
-              ["r", "r"],
-              ["s", "s"],
-              ["t", "t"],
-              ["u", "u"],
-              ["v", "v"],
-              ["w", "w"],
-              ["x", "x"],
-              ["y", "y"],
-              ["z", "z"],
-              ["0", "0"],
-              ["1", "1"],
-              ["2", "2"],
-              ["3", "3"],
-              ["4", "4"],
-              ["5", "5"],
-              ["6", "6"],
-              ["7", "7"],
-              ["8", "8"],
-              ["9", "9"],
-              ["`", "`"],
-              ["-", "-"],
-              ["=", "="],
-              ["[", "["],
-              ["]", "]"],
-              ["\\", "\\"],
-              [";", ";"],
-              ["'", "'"],
-              [",", ","],
-              [".", "."],
-              ["/", "/"],
-            ],
-          },
-        ],
-        category: Blockly.Categories.event,
-        extensions: ["colours_event", "shape_hat"],
-      });
-    },
-  };
+      if (this.jsonInitOriginal === undefined)
+        this.jsonInitOriginal = this.jsonInit;
+      this.jsonInit = function (obj) {
+        appendKeys(obj.args0[0].options);
+        this.jsonInitOriginal(obj);
+      }
+      this.initOriginal();
+    }
+  }
+
+
+  Blockly.Blocks['event_whenkeypressed'] = {
+    jsonInitOriginal: undefined,
+    initOriginal: Blockly.Blocks['event_whenkeypressed'].init,
+    init: function () {
+      if (this.jsonInitOriginal === undefined)
+        this.jsonInitOriginal = this.jsonInit;
+      this.jsonInit = function (obj) {
+        appendKeys(obj.args0[0].options);
+        this.jsonInitOriginal(obj);
+      }
+      this.initOriginal();
+    }
+  }
 
   addon.tab.traps.vm.emitWorkspaceUpdate();
 }
