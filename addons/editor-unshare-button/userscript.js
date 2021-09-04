197d7a47abd1e71ae7c069fde0c78bd259564086
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-09-04 16:50:23 +0000

    Format code

--- addons/editor-unshare-button/userscript.js
+++ addons/editor-unshare-button/userscript.js
@@ -8,52 +8,49 @@ export default async function ({ addon, msg, global, console }) {
 
     button.classList.add("sa-unshare-button");
     button.querySelector("span").innerText = msg("unshare-button");
-    button.addEventListener(
-      "click",
-      (e) => {
-        if (!confirm(msg("unshare-msg"))) return;
+    button.addEventListener("click", (e) => {
+      if (!confirm(msg("unshare-msg"))) return;
+      redux.dispatch({
+        type: "SET_COMMENT_FETCH_STATUS",
+        infoType: "project",
+        status: "FETCHING",
+      });
+      const project = redux.state.preview.projectInfo;
+      const author = project.author;
+      fetch(`/site-api/projects/all/${redux.state.preview.projectInfo.id}/`, {
+        headers: {
+          "x-csrftoken": addon.auth.csrfToken,
+          "x-requested-with": "XMLHttpRequest",
+        },
+        method: "PUT",
+        credentials: "include",
+        body: JSON.stringify({
+          isPublished: false,
+        }),
+      }).then((res) => {
+        if (res.status !== 200) {
+          return redux.dispatch({
+            type: "SET_COMMENT_FETCH_STATUS",
+            infoType: "project",
+            status: "FETCH_ERROR",
+          });
+          // We do not set error
+        }
+
         redux.dispatch({
           type: "SET_COMMENT_FETCH_STATUS",
           infoType: "project",
-          status: "FETCHING",
+          status: "FETCHED",
         });
-        const project = redux.state.preview.projectInfo;
-        const author = project.author;
-        fetch(`/site-api/projects/all/${redux.state.preview.projectInfo.id}/`, {
-          headers: {
-            "x-csrftoken": addon.auth.csrfToken,
-            "x-requested-with": "XMLHttpRequest",
+
+        button.classList.remove("sa-unshare-button");
+        redux.dispatch({
+          type: "UPDATE_PROJECT_INFO",
+          info: {
+            is_published: false,
           },
-          method: "PUT",
-          credentials: "include",
-          body: JSON.stringify({
-            isPublished: false,
-          }),
-        }).then((res) => {
-          if (res.status !== 200) {
-            return redux.dispatch({
-              type: "SET_COMMENT_FETCH_STATUS",
-              infoType: "project",
-              status: "FETCH_ERROR",
-            });
-            // We do not set error
-          }
-
-          redux.dispatch({
-            type: "SET_COMMENT_FETCH_STATUS",
-            infoType: "project",
-            status: "FETCHED",
-          });
-
-          button.classList.remove("sa-unshare-button");
-          redux.dispatch({
-            type: "UPDATE_PROJECT_INFO",
-            info: {
-              is_published: false,
-            },
-          });
         });
-      }
-    );
+      });
+    });
   }
 }
