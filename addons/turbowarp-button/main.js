9b24b844c842e90b87d454659fe98fa764c5db66
TheColaber <TheColaber@users.noreply.github.com>
2021-09-05 22:02:12 +0000

    Format code

--- addons/turbowarp-button/main.js
+++ addons/turbowarp-button/main.js
@@ -4,7 +4,7 @@ export default async function ({ addon, global, console, msg }) {
     if (document.querySelector("#scratchAddonsTurboBtn")) return;
     //console.log(addon.tab.editorMode);
     //(addon.tab.editorMode == "projectpage" || addon.tab.editorMode == "fullscreen")  //?
-    
+
     if (addon.tab.editorMode == "projectpage") {
       addon.tab.waitForElement(".stage-header_stage-size-row_14N65").then(() => {
         const subactions = document.querySelector(".stage-header_stage-size-row_14N65");
@@ -21,15 +21,14 @@ export default async function ({ addon, global, console, msg }) {
         turbo.appendChild(turboSpan);
 
         var tSettings = "";
-//console.log(addon.settings.get("HQPen"));
+        //console.log(addon.settings.get("HQPen"));
 
         if (addon.settings.get("HQPen") == true) {
           tSettings = "?" + "hqpen";
         }
         turbo.addEventListener("click", () => {
-          window.location.href = `https://turbowarp.org/${
-            window.location.href.split("projects")[1].split("/")[1]
-          }` + tSettings;
+          window.location.href =
+            `https://turbowarp.org/${window.location.href.split("projects")[1].split("/")[1]}` + tSettings;
         });
         if (addon.settings.get("buttonColor")) {
           turbo.style.backgroundColor = addon.settings.get("buttonColor");
