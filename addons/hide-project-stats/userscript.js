d864b33b08a4374259025c4815a2977b3f5e8c25
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-09-08 17:17:34 -0500

    New approach

--- addons/hide-project-stats/userscript.js
+++ addons/hide-project-stats/userscript.js
@@ -1,53 +1,52 @@
 export default async function ({ addon, global, console, msg, safeMsg: m }) {
-  let loves = { state: false };
-  let favorites = { state: false };
+  let loves = { name: "love" };
+  let favorites = { name: "favorite" };
 
-  function toggleLabels(manualState) {
-    console.log("State toggled");
-    toggleOnButton(loves, "love", manualState);
-    toggleOnButton(favorites, "favorite", manualState);
+  function initializeLabels(button) {
+    button.labelExists = false;
+    button.buttonElement = document.getElementsByClassName(`project-${button.name}s`)[0];
+    button.userLiked = (document.getElementsByClassName(`${button.name}d`).length != 0);
+    button.labelElement = document.createElement("span");
+    button.labelElement.id = `sa-${button.name}-label`;
+    button.buttonElement.after(button.labelElement);
   }
 
-  function toggleOnButton(button, name, manualState) {
-    if (typeof manualState == "boolean") {
-      button.state = manualState;
-    }
-    if (button.element == undefined) {
-      button.element = document.getElementsByClassName(`project-${name}s`)[0];
-    }
-    if (addon.settings.get(`${name}s`) != button.state || (addon.self.disabled && button.state)) {
-      if (addon.settings.get(`${name}s`) && !addon.self.disabled) {
-        // Setting was turned on
-        button.count = button.element.textContent;
-        button.element.innerText = m(name);
-        button.state = true;
+  function toggleLabels() {
+    toggleOnButton(loves);
+    toggleOnButton(favorites);
+  }
+
+  function toggleOnButton(button) {
+    if (addon.settings.get(`${button.name}s`) && !addon.self.disabled) {
+      // Setting was turned on
+      button.labelExists = true;
+      if (button.userLiked) {
+        button.labelElement.innerText = m(`${button.name}-enabled`);
       } else {
-        // Setting was turned off
-        button.element.innerText = button.count;
-        button.state = false;
+        button.labelElement.innerText = m(`${button.name}-disabled`);
       }
-      // Prevents flash of count before addon loads
-      button.element.style = "visibility: visible;";
+    } else {
+      // Setting was turned off
+      button.labelExists = false;
+      button.labelElement.innerText = "";
     }
   }
 
+  initializeLabels(loves);
+  initializeLabels(favorites);
   toggleLabels();
 
-  addon.settings.addEventListener("change", () => {
-    toggleLabels();
-  });
-  addon.self.addEventListener("disabled", () => {
-    toggleLabels();
-  });
-  addon.self.addEventListener("reenabled", () => {
-    toggleLabels();
-  });
+  addon.settings.addEventListener("change", () => { toggleLabels(); });
+  addon.self.addEventListener("disabled", () => { toggleLabels(); });
+  addon.self.addEventListener("reenabled", () => { toggleLabels(); });
   addon.tab.redux.addEventListener("statechanged", (data) => {
     if (data.detail.action.type === "SET_LOVED") {
-      toggleLabels(false);
+      loves.userLiked = !loves.userLiked;
+      toggleOnButton(loves);
     }
     if (data.detail.action.type === "SET_FAVED") {
-      toggleLabels(false);
+      favorites.userLiked = !favorites.userLiked;
+      toggleOnButton(favorites);
     }
   });
 }
