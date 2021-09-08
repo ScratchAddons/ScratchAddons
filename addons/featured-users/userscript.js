ff0b8ff65253ad0fe0cd5972aaed9fc83b99e831
LankyBox01 <LankyBox01@users.noreply.github.com>
2021-09-08 19:29:27 +0000

    Format code

--- addons/featured-users/userscript.js
+++ addons/featured-users/userscript.js
@@ -25,32 +25,32 @@ slicktrack.className = "slick-track";
 slicktrack.style.opacity = 1;
 slicktrack.style.transform = "translate3d(0px, 0px, 0px)";
 slicktrack.style.width = "2925px";
-slicklist.append(slicktrack)
+slicklist.append(slicktrack);
 
 // Fetch lanky's database & create elements for featured users
 var raw;
-await fetch('https://www.scratch-featured-users.tk/json')
-    .then(response => response.json())
-    .then(data => raw = data)
+await fetch("https://www.scratch-featured-users.tk/json")
+  .then((response) => response.json())
+  .then((data) => (raw = data));
 
 // Loop through the database & append elements
-for (var key in raw){
-let usertop = document.createElement("div");
-usertop.className = "thumbnail gallery slick-slide slick-active";
-usertop.style.paddingLeft = "7px";
-usertop.style.paddingRight = "7px";
-let usersecond = document.createElement("a");
-usersecond.className = "thumbnail-image";
-usersecond.href = "https://scratch.mit.edu/users/" + raw[key].username + "/";
-let userprofilepic = document.createElement("img");
-userprofilepic.style.width = "100px"
-userprofilepic.src = "https://cdn2.scratch.mit.edu/get_image/user/" + raw[key].id + "_60x60.png";
-usersecond.append(userprofilepic)
-let userprofilelink = document.createElement("a");
-userprofilelink.innerText = raw[key].username;
-usersecond.append(userprofilelink)
-usertop.append(usersecond)
-slicktrack.append(usertop)
+for (var key in raw) {
+  let usertop = document.createElement("div");
+  usertop.className = "thumbnail gallery slick-slide slick-active";
+  usertop.style.paddingLeft = "7px";
+  usertop.style.paddingRight = "7px";
+  let usersecond = document.createElement("a");
+  usersecond.className = "thumbnail-image";
+  usersecond.href = "https://scratch.mit.edu/users/" + raw[key].username + "/";
+  let userprofilepic = document.createElement("img");
+  userprofilepic.style.width = "100px";
+  userprofilepic.src = "https://cdn2.scratch.mit.edu/get_image/user/" + raw[key].id + "_60x60.png";
+  usersecond.append(userprofilepic);
+  let userprofilelink = document.createElement("a");
+  userprofilelink.innerText = raw[key].username;
+  usersecond.append(userprofilelink);
+  usertop.append(usersecond);
+  slicktrack.append(usertop);
 }
 
 // Append everything to the splash section list
