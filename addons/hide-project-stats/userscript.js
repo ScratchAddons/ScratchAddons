19d4eb3f3cbf6592c5db54549effe46e24c320c6
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-09-11 14:29:22 -0500

    Better-documented code

--- addons/hide-project-stats/userscript.js
+++ addons/hide-project-stats/userscript.js
@@ -2,9 +2,10 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
   let loves = { name: "love" };
   let favorites = { name: "favorite" };
   let username = await addon.auth.fetchUsername();
+  let visitingOwnProject = username !== null && document.querySelector(".button.action-button.report-button") === null;
 
-  let visitingOwnProject; // Set by while(true) loop
-
+  // Adds the elements that contain the "Love" and "Favorite" labels
+  // to the love and favorite buttons.
   function initializeLabels(button) {
     button.buttonElement = document.querySelector(`.project-${button.name}s`);
     button.labelElement = document.createElement("span");
@@ -12,12 +13,14 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
     button.buttonElement.after(button.labelElement);
   }
 
+  // Calculates whether or not various project stats should show.
+  // Love and favorite counts are handled separately since they get custom
+  // labels.
   function refreshLabels() {
     refreshButton(loves);
     refreshButton(favorites);
     // If the user has the "show stats on my own projects" setting enabled,
     // show the remix and view count if the user owns the project.
-    // Love and favorite counts are handled separately since they get custom labels.
     document.querySelectorAll(".project-remixes, .project-views").forEach((element) => {
       if (!addon.self.disabled && addon.settings.get("showOwnStats") && visitingOwnProject) {
         element.classList.add("stat-display");
@@ -27,22 +30,31 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
     });
   }
 
-  // Handles the buttons -- we can't simply hide them or the user wouldn't be able to love or favorite.
-  // This just controls an appended label -- a lot of the work is actually done by CSS.
+  // Handles the buttons -- we can't simply hide them or the user wouldn't be
+  // able to love or favorite.
+  // This just controls an appended label and whether or not the count should
+  // be displayed.
   async function refreshButton(button) {
+    // Checks if the button's count should be replaced with a custom label
     if (
       !addon.self.disabled &&
       addon.settings.get(`${button.name}s`) &&
       !(addon.settings.get("showOwnStats") && visitingOwnProject)
     ) {
-      // Setting was turned on
+      // Show a custom label
       if (document.querySelector(`.${button.name}d`) !== null) {
-        // Checks for class that enables button
+        // The user loved/favorited the project
         button.labelElement.innerText = m(`${button.name}-enabled`);
       } else {
+        // The user has not loved/favorited the project
         button.labelElement.innerText = m(`${button.name}-disabled`);
       }
+
+      // Do not show the love/favorite count
       button.buttonElement.classList.remove("stat-display");
+
+      // Checks if the user is signed in or not, and hides the love/favorite
+      // buttons accordingly
       username = await addon.auth.fetchUsername();
       if (username === null) {
         button.buttonElement.classList.add("hidden");
@@ -52,12 +64,21 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
         button.labelElement.classList.remove("hidden");
       }
     } else {
-      // Setting was turned off
+      // Do not show a custom label
       button.labelElement.innerText = "";
+      // Show the love/favorite count
       button.buttonElement.classList.add("stat-display");
     }
   }
 
+  // Acually run the addon sequence.
+  await addon.tab.waitForElement(".project-loves", { markAsSeen: true });
+  initializeLabels(loves);
+  initializeLabels(favorites);
+  refreshLabels();
+
+  // Re-calculate visibility of various elements when settings change
+  // or the user loves/favorites the project
   addon.settings.addEventListener("change", () => {
     refreshLabels();
   });
@@ -76,12 +97,10 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
     }
   });
 
+  // Since the user can sign in during the same session, the login status always
+  // needs to be updated.
   while (true) {
     await addon.tab.waitForElement(".project-loves", { markAsSeen: true });
     visitingOwnProject = username !== null && document.querySelector(".button.action-button.report-button") === null;
-
-    initializeLabels(loves);
-    initializeLabels(favorites);
-    refreshLabels();
   }
 }
