8e0f091a4b3a2df6dd9b8cba6daf077a7a62a298
apple502j <33279053+apple502j@users.noreply.github.com>
2021-09-15 18:39:09 +0900

    studio-tools: show Leave button after transfer

--- addons/studio-tools/migrated.js
+++ addons/studio-tools/migrated.js
@@ -191,7 +191,7 @@ export default async ({ addon, console, msg }) => {
   render();
   addon.tab.addEventListener("urlChange", render);
   redux.addEventListener("statechanged", (e) => {
-    if (e.detail.action.type === "SET_ROLES") {
+    if (e.detail.action.type === "SET_ROLES" || e.detail.action.type === "SET_INFO") {
       checkPermissions();
       render();
     }
