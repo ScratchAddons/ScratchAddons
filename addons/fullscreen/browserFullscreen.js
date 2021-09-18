d63a9d304dd445dde217218d7d00228c619bea36
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-09-17 22:17:51 -0500

    Added fullscreen option, cleaned up

--- addons/fullscreen/browserFullscreen.js
+++
@@ -1,1 +0,0 @@
-export default async function ({ addon, global, console }) {}
---
+++ addons/fullscreen/userscript.js
@@ -0,0 +1,45 @@
+// Used for the automatic browser fullscreen setting
+export default async function ({ addon, global, console }) {
+  function updateBrowserFullscreen() {
+    if (addon.settings.get("browserFullscreen")) {
+      if (document.fullscreenElement === null && addon.tab.editorMode === "fullscreen") {
+        document.documentElement.requestFullscreen();
+      } else if (document.fullscreenElement !== null && addon.tab.editorMode !== "fullscreen") {
+        document.exitFullscreen();
+      }
+    }
+  }
+
+  function updateScratchFullscreen() {
+    if (addon.settings.get("browserFullscreen")) {
+      let fullscreen;
+      if (addon.settings.get("editorFullscreen")) {
+        fullscreen = window.innerHeight == window.screen.height;
+        console.log(window.innerHeight)
+        console.log(window.screen.height)
+      }
+      if ((fullscreen || document.fullscreenElement !== null) && addon.tab.editorMode !== "fullscreen") {
+        addon.tab.redux.dispatch({
+          type: "scratch-gui/mode/SET_FULL_SCREEN",
+          isFullScreen: true
+        });
+      } else if ((!fullscreen || document.fullscreenElement === null) && addon.tab.editorMode === "fullscreen") {
+        addon.tab.redux.dispatch({
+          type: "scratch-gui/mode/SET_FULL_SCREEN",
+          isFullScreen: false
+        });
+      }
+    }
+  }
+
+  addon.tab.addEventListener("urlChange", updateBrowserFullscreen);
+  window.addEventListener("resize", updateScratchFullscreen);
+  addon.settings.addEventListener("change", function() {
+    updateBrowserFullscreen();
+    updateScratchFullscreen();
+  });
+  addon.self.addEventListener("reenabled", function() {
+    updateBrowserFullscreen();
+    updateScratchFullscreen();
+  })
+}
