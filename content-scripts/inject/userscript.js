8ba247b5447bf9f22643125af30e23e610efca3f
RedGuy12 <paul@reid-family.org>
2021-09-19 16:03:40 -0500

    async

--- content-scripts/inject/userscript.js
+++ content-scripts/inject/userscript.js
@@ -1,6 +1,6 @@
 import runAddonUserscripts from "./run-userscript.js";
 const addons = (await fetch("../addons/addons.json").then(r => r.json())).filter(addon => !addon.startsWith("//"))
-addons.forEach(addon => {
+addons.forEach(async(addon) => {
 	const manifest = (await fetch("../addons/" + addon + "addon.json").then(r => r.json()))
 	runAddonUserscripts(addon,manifest.scripts,false)
 })
