2a9725dd88fe90fe36b13562f2fd332920f0a383
RedGuy12 <RedGuy12@users.noreply.github.com>
2021-09-19 21:00:16 +0000

    Format code

--- content-scripts/inject/userscript.js
+++ content-scripts/inject/userscript.js
@@ -1,7 +1,6 @@
 import runAddonUserscripts from "./run-userscript.js";
-const addons = (await fetch("../addons/addons.json").then(r => r.json())).filter(addon => !addon.startsWith("//"))
-addons.forEach(addon => {
-	const manifest = (await fetch("../addons/" + addon + "addon.json").then(r => r.json()))
-	runAddonUserscripts(addon,manifest.scripts,false)
-})
-
+const addons = (await fetch("../addons/addons.json").then((r) => r.json())).filter((addon) => !addon.startsWith("//"));
+addons.forEach((addon) => {
+  const manifest = await fetch("../addons/" + addon + "addon.json").then((r) => r.json());
+  runAddonUserscripts(addon, manifest.scripts, false);
+});
