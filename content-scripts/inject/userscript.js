f2ec37fcc23979818200f26d31c0f6800a5bcefc
RedGuy12 <paul@reid-family.org>
2021-09-19 16:24:02 -0500

    /

--- content-scripts/inject/userscript.js
+++ content-scripts/inject/userscript.js
@@ -3,7 +3,7 @@ const addons = (await fetch(new URL("../../addons/addons.json", import.meta.url)
   (addon) => !addon.startsWith("//")
 );
 addons.forEach(async (addonId) => {
-  const manifest = await fetch(new URL("../../addons/" + addonId + "addon.json", import.meta.url).href).then((r) =>
+  const manifest = await fetch(new URL("../../addons/" + addonId + "/addon.json", import.meta.url).href).then((r) =>
     r.json()
   );
   runAddonUserscripts({ addonId, scripts: manifest.scripts });
