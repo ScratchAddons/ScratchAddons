d1f6c136016b9cb3aac903eefc12f7de1c6b0953
RedGuy12 <paul@reid-family.org>
2021-09-19 16:21:56 -0500

    forgot a dir

--- content-scripts/inject/userscript.js
+++ content-scripts/inject/userscript.js
@@ -1,9 +1,9 @@
 import runAddonUserscripts from "./run-userscript.js";
-const addons = (await fetch(new URL( "../addons/addons.json",import.meta.url,).href).then((r) => r.json())).filter(
+const addons = (await fetch(new URL( "../../addons/addons.json",import.meta.url,).href).then((r) => r.json())).filter(
   (addon) => !addon.startsWith("//")
 );
 addons.forEach(async (addonId) => {
-  const manifest = await fetch(new URL("../addons/" + addonId + "addon.json",import.meta.url).href).then((r) =>
+  const manifest = await fetch(new URL("../../addons/" + addonId + "addon.json",import.meta.url).href).then((r) =>
     r.json()
   );
   runAddonUserscripts({ addonId, scripts: manifest.scripts });
