afde45611a5edd2c1ecda13519897aa8901ddc0e
RedGuy12 <paul@reid-family.org>
2021-09-19 19:18:06 -0500

    this was bad code to begin with

--- content-scripts/inject/run-userscript.js
+++ content-scripts/inject/run-userscript.js
@@ -6,7 +6,7 @@ export default async function runAddonUserscripts({ addonId, scripts, enabledLat
   const globalObj = Object.create(null);
   for (const scriptInfo of scripts) {
     const { url: scriptPath, runAtComplete } = scriptInfo;
-    const scriptUrl = `${new URL(import.meta.url).origin}/addons/${addonId}/${scriptPath}`;
+    const scriptUrl = new URL("../../" + `addons/${addonId}/${scriptPath}`, import.meta.url).href
     const loadUserscript = async () => {
       await scratchAddons.l10n.loadByAddonId(addonId);
       const module = await import(scriptUrl);
