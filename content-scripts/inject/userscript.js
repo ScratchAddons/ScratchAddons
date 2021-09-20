aca7b83f08b4cf3b10480cf8f18ac1bd4c3c2d2d
RedGuy12 <paul@reid-family.org>
2021-09-19 19:58:26 -0500

    forgot to fix the url (again) :joy:

--- content-scripts/inject/userscript.js
+++ content-scripts/inject/userscript.js
@@ -1,6 +1,6 @@
 import runAddonUserscripts from "./run-userscript.js";
 import Localization from "./l10n.js";
-import globalStateProxy from "./imports/global-state.js";
+import globalStateProxy from "../../background/imports/global-state.js";
 
 (async function () {
   scratchAddons.globalState = globalStateProxy;
