b999f3cc228f577ca43b9f787569f550cad03f7a
Thomas Weber <weber_t@mailbox.org>
2021-09-21 18:07:09 -0500

    ctrl+click to duplicate only that block

--- addons/block-duplicate/userscript.js
+++ addons/block-duplicate/userscript.js
@@ -4,8 +4,23 @@ export default async function ({ addon, global, console }) {
   // https://github.com/LLK/scratch-blocks/blob/e86f115457006d1cde83baa23eaaf1ee16d315f5/core/gesture.js#L454
   ScratchBlocks.Gesture.prototype.startDraggingBlock_ = function (...args) {
     if (this.mostRecentEvent_.shiftKey && !addon.self.disabled) {
+      // Scratch will reset the group on its own when the drag ends
+      ScratchBlocks.Events.setGroup(true);
       this.shouldDuplicateOnDrag_ = true;
     }
     return originalStartDraggingBlock.call(this, ...args);
+  };
+  // https://github.com/LLK/scratch-blocks/blob/e86f115457006d1cde83baa23eaaf1ee16d315f5/core/gesture.js#L983
+  const originalDuplicateOnDrag = ScratchBlocks.Gesture.prototype.duplicateOnDrag_;
+  ScratchBlocks.Gesture.prototype.duplicateOnDrag_ = function (...args) {
+    const ret = originalDuplicateOnDrag.call(this, ...args);
+    if ((this.mostRecentEvent_.ctrlKey || this.mostRecentEvent_.metaKey) && !addon.self.disabled) {
+      const block = this.targetBlock_;
+      const nextBlock = block.getNextBlock();
+      if (nextBlock) {
+        nextBlock.dispose();
+      }
+    }
+    return ret;
   };
 }
