3c7164dd9d8a19844be01f20138589cf915abddb
GrahamSH <grahamshllk@gmail.com>
2021-09-22 00:14:48 +0000

    SPEED

--- background/load-addon-manifests.js
+++ background/load-addon-manifests.js
@@ -7,98 +7,109 @@
   const useDefault = scratchAddons.l10n.locale.startsWith("en");
   scratchAddons.manifests.push(
     ...(await Promise.all(
-      folderNames.filter((folderName)=> {return !folderName.startsWith("//")}).map(async (folderName) => {
-        const manifest = await (await fetch(chrome.runtime.getURL(`addons/${folderName}/addon.json`))).json();
-        if (!useDefault) {
-          manifest._english = {};
-          for (const prop of ["name", "description"]) {
-            if (manifest[prop]) {
-              manifest._english[prop] = manifest[prop];
-              manifest[prop] = scratchAddons.l10n.get(`${folderName}/@${prop}`, {}, manifest[prop]);
-            }
-          }
-          if (manifest.info) {
-            for (const info of manifest.info || []) {
-              info.text = scratchAddons.l10n.get(`${folderName}/@info-${info.id}`, {}, info.text);
-            }
-          }
-          if (manifest.popup) {
-            manifest.popup.name = scratchAddons.l10n.get(`${folderName}/@popup-name`, {}, manifest.popup.name);
-          }
-        }
-        for (const propName of ["userscripts", "userstyles"]) {
-          for (const injectable of manifest[propName] || []) {
-            const { matches } = injectable;
-            if (typeof matches === "string" && matches.startsWith("^")) {
-              injectable._scratchDomainImplied = !matches.startsWith("^https:");
-              injectable.matches = new RegExp(matches, "u");
-            } else if (Array.isArray(matches)) {
-              for (let i = matches.length; i--; ) {
-                const match = matches[i];
-                if (typeof match === "string" && match.startsWith("^")) {
-                  matches[i] = new RegExp(match, "u");
-                  matches[i]._scratchDomainImplied = !match.startsWith("^https:");
+      folderNames
+        .filter((folderName) => {
+          return !folderName.startsWith("//");
+        })
+        .map(async (folderName) => {
+          return fetch(chrome.runtime.getURL(`addons/${folderName}/addon.json`))
+            .then((res) => res.json())
+            .then((manifest) => {
+              if (!useDefault) {
+                manifest._english = {};
+                for (const prop of ["name", "description"]) {
+                  if (manifest[prop]) {
+                    manifest._english[prop] = manifest[prop];
+                    manifest[prop] = scratchAddons.l10n.get(`${folderName}/@${prop}`, {}, manifest[prop]);
+                  }
+                }
+                if (manifest.info) {
+                  for (const info of manifest.info || []) {
+                    info.text = scratchAddons.l10n.get(`${folderName}/@info-${info.id}`, {}, info.text);
+                  }
+                }
+                if (manifest.popup) {
+                  manifest.popup.name = scratchAddons.l10n.get(`${folderName}/@popup-name`, {}, manifest.popup.name);
                 }
               }
-            }
-          }
-        }
-
-        for (const preset of manifest.presets || []) {
-          for (const prop of ["name", "description"]) {
-            if (preset[prop] && !useDefault) {
-              preset[prop] = scratchAddons.l10n.get(`${folderName}/@preset-${prop}-${preset.id}`, {}, preset[prop]);
-            }
-          }
-        }
-        for (const option of manifest.settings || []) {
-          if (!useDefault) {
-            option.name = scratchAddons.l10n.get(
-              `${folderName}/@settings-name-${option.id}`,
-              {
-                commentIcon: "@comment.svg",
-                forumIcon: "@forum.svg",
-                heartIcon: "@heart.svg",
-                starIcon: "@star.svg",
-                followIcon: "@follow.svg",
-                studioAddIcon: "@studio-add.svg",
-                studioIcon: "@studio.svg",
-                remixIcon: "@remix.svg",
-                adminusersIcon: "@adminusers.svg",
-                usersIcon: "@users.svg",
-              },
-              option.name
-            );
-          }
-          switch (option.type) {
-            case "string":
-              if (!useDefault) {
-                option.default = scratchAddons.l10n.get(
-                  `${folderName}/@settings-default-${option.id}`,
-                  {},
-                  option.default
-                );
+              for (const propName of ["userscripts", "userstyles"]) {
+                for (const injectable of manifest[propName] || []) {
+                  const { matches } = injectable;
+                  if (typeof matches === "string" && matches.startsWith("^")) {
+                    injectable._scratchDomainImplied = !matches.startsWith("^https:");
+                    injectable.matches = new RegExp(matches, "u");
+                  } else if (Array.isArray(matches)) {
+                    for (let i = matches.length; i--; ) {
+                      const match = matches[i];
+                      if (typeof match === "string" && match.startsWith("^")) {
+                        matches[i] = new RegExp(match, "u");
+                        matches[i]._scratchDomainImplied = !match.startsWith("^https:");
+                      }
+                    }
+                  }
+                }
               }
-              break;
-            case "select":
-              option.potentialValues = option.potentialValues.map((value) => {
-                if (value && value.id) {
-                  if (!useDefault) {
-                    value.name = scratchAddons.l10n.get(
-                      `${folderName}/@settings-select-${option.id}-${value.id}`,
+
+              for (const preset of manifest.presets || []) {
+                for (const prop of ["name", "description"]) {
+                  if (preset[prop] && !useDefault) {
+                    preset[prop] = scratchAddons.l10n.get(
+                      `${folderName}/@preset-${prop}-${preset.id}`,
                       {},
-                      value.name
+                      preset[prop]
                     );
                   }
-                  return value;
                 }
-                return { name: value, id: value };
-              });
-              break;
-          }
-        }
-        return { addonId: folderName, manifest };
-      })
+              }
+              for (const option of manifest.settings || []) {
+                if (!useDefault) {
+                  option.name = scratchAddons.l10n.get(
+                    `${folderName}/@settings-name-${option.id}`,
+                    {
+                      commentIcon: "@comment.svg",
+                      forumIcon: "@forum.svg",
+                      heartIcon: "@heart.svg",
+                      starIcon: "@star.svg",
+                      followIcon: "@follow.svg",
+                      studioAddIcon: "@studio-add.svg",
+                      studioIcon: "@studio.svg",
+                      remixIcon: "@remix.svg",
+                      adminusersIcon: "@adminusers.svg",
+                      usersIcon: "@users.svg",
+                    },
+                    option.name
+                  );
+                }
+                switch (option.type) {
+                  case "string":
+                    if (!useDefault) {
+                      option.default = scratchAddons.l10n.get(
+                        `${folderName}/@settings-default-${option.id}`,
+                        {},
+                        option.default
+                      );
+                    }
+                    break;
+                  case "select":
+                    option.potentialValues = option.potentialValues.map((value) => {
+                      if (value && value.id) {
+                        if (!useDefault) {
+                          value.name = scratchAddons.l10n.get(
+                            `${folderName}/@settings-select-${option.id}-${value.id}`,
+                            {},
+                            value.name
+                          );
+                        }
+                        return value;
+                      }
+                      return { name: value, id: value };
+                    });
+                    break;
+                }
+              }
+              return { addonId: folderName, manifest };
+            });
+        })
     ))
   );
   scratchAddons.localState.ready.manifests = true;
