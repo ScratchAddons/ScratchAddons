81d723216274323aaf69aac1fc632d93d4ecce83
RedGuy12 <paul@reid-family.org>
2021-09-23 15:43:30 -0500

    adopt

--- userscript/script.user.js
+++ userscript/script.user.js
@@ -42,7 +42,7 @@ if (/^\/(scratch\-addons\-extension|sa\-ext)\/settings\/?$/i.test(location.pathn
       if (!document.head) document.documentElement.append(document.createElement("head"));
       updateAttrs(document.head, dom.head);
       document.head.innerHTML = "";
-      [...dom.head.children].forEach((element) => document.importNode(element));
+      [...dom.head.children].forEach((element) => document.adoptNode(element));
 
       if (!document.body) document.documentElement.append(document.createElement("body"));
       updateAttrs(document.body, dom.body);
