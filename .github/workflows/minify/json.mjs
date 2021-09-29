1868d6fe4283f694039194f15503c7098ccd8132
RedGuy12 <paul@reid-family.org>
2021-09-29 12:59:47 -0500

    bro what
    why was that there
    
    and how did it work before

--- .github/workflows/minify/json.mjs
+++ .github/workflows/minify/json.mjs
@@ -5,7 +5,7 @@ import getInDir from "./getInDir.mjs";
 getInDir({ path: "./", ext: ".json" }).forEach(async (filePath) => {
   console.log(`Minifying ${filePath}`);
   const source = await readFile(filePath, "utf8").catch(console.error);
-  let parsed = JSON.parse(source, terserConfig);
+  let parsed = JSON.parse(source);
 
   // Strip translator helps
   if (filePath.includes("/_locales/")) {
@@ -27,8 +27,9 @@ getInDir({ path: "./", ext: ".json" }).forEach(async (filePath) => {
 });
 
 getInDir({ path: "./", ext: ".map" }).forEach(async (filePath) => {
+  console.log(`Minifying ${filePath}`);
   const source = await readFile(filePath, "utf8").catch(console.error);
-  const parsed = JSON.parse(source, terserConfig);
+  const parsed = JSON.parse(source);
   const minfied = JSON.stringify(parsed);
   writeFile(filePath, minfied);
 });
