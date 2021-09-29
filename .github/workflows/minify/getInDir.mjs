6477fb248f65c184121bcc6b6687fa4682defb18
RedGuy12 <paul@reid-family.org>
2021-09-29 12:48:41 -0500

    ignore node_modules

--- .github/workflows/minify/getInDir.mjs
+++ .github/workflows/minify/getInDir.mjs
@@ -4,7 +4,7 @@ import { join, resolve } from "path";
 export default function getAllFiles({ path, arrayOfFiles = [], ext }) {
   let files = readdirSync(path);
   files.forEach((file) => {
-    if (statSync(`${path}/${file}`).isDirectory() && file !== ".git") {
+    if (statSync(`${path}/${file}`).isDirectory() && ![".git", "node_modules"].includes(file)) {
       arrayOfFiles = getAllFiles({ path: `${path}/${file}`, arrayOfFiles, ext });
     } else if (file.endsWith(ext) && file !== ".eslintrc.json") {
       arrayOfFiles.push(join(resolve(), `${path}/${file}`));
