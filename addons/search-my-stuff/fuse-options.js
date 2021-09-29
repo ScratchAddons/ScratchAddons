ab3c7e1fa31cd7cef6481b26c143d300d634483a
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-09-28 23:16:49 -0500

    Basic functionality

---
+++ addons/search-my-stuff/fuse-options.js
@@ -0,0 +1,12 @@
+export default {
+  includeScore: true,
+  threshold: 0.35,
+  ignoreLocation: true,
+  useExtendedSearch: true,
+  keys: [
+    {
+      name: "name",
+      weight: 1,
+    }
+  ]
+};
--- addons/search-my-stuff/userscript.js
+++ addons/search-my-stuff/userscript.js
@@ -7,6 +7,10 @@
  *
  * This addon has many similarities to `forum-search`.
  */
+
+import Fuse from "../../libraries/thirdparty/fuse.esm.min.js"
+import fuseOptions from "./fuse-options.js";
+
 export default async function ({ addon, global, console, msg }) {
   // Create the search bar
   let search = document.createElement("form");
@@ -37,18 +41,66 @@ export default async function ({ addon, global, console, msg }) {
   let navIndex = document.querySelector(".inner");
   navIndex.appendChild(search);
 
+  // Index page
+  let projects = [];
+  let fuse;
+  await addon.tab.waitForElement("ul.media-list");
+  let resultsContainer = document.querySelector("ul.media-list");
+  indexPage();
+
   // When the user makes a keystroke in the search bar
   searchBar.addEventListener("input", (e) => {
-    triggerNewSearch(searchBar.value, searchDropdown.value);
+    triggerNewSearch();
   });
 
   // When a new filter is selected from the dropdown
   searchDropdown.addEventListener("change", (e) => {
-    triggerNewSearch(searchBar.value, searchDropdown.value);
+    if (!fuse) indexPage();
+    triggerNewSearch();
   });
 
-  // Searches the My Stuff page for matching project titles,
-  // then reorders based on "sort by"
+  // Indexes the project tile elements and their project names,
+  // then updates the search algorithm
+  async function indexPage() {
+    while (true) {
+      const project = await addon.tab.waitForElement(".media-list > li", {
+        markAsSeen: true,
+      });
+      projects.push({
+        name: project.querySelector(".media-info-item.title > a").innerText,
+        element: project
+      });
+      fuse = new Fuse(projects, fuseOptions);
+      // Refresh search if user clicks "load more" after searching
+      if (searchBar.value != "") {
+        triggerNewSearch();
+      }
+    }
+  }
+
+  // Reorders based on "sort by"
   // (relevance algorithm is the same as SA settings page)
-  function triggerNewSearch() {}
+  function triggerNewSearch() {
+    let query = searchBar.value;
+    let filter = searchDropdown.value;
+    // Blank query should restore the original order
+    if (query == "") {
+      projects.forEach(project => {
+        project.element.remove();
+        resultsContainer.appendChild(project.element);
+      });
+      return
+    }
+    const fuseSearch = fuse.search(query).sort((a, b) => {
+      // Sort very good matches at the top no matter what
+      if ((a.score < 0.1) ^ (b.score < 0.1)) return a.score < 0.1 ? -1 : 1;
+    });
+    projects.forEach(project => {
+      project.element.remove();
+    });
+    fuseSearch.forEach(result => {
+      resultsContainer.appendChild(result.item.element);
+    });
+  }
+
 }
