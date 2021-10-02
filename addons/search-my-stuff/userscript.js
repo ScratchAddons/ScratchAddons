882036e911b7ca707c7c7d9526d4f1637decae1a
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-10-01 23:22:07 -0500

    Support for old "sort by" (removed custom dropdown)

--- addons/search-my-stuff/userscript.js
+++ addons/search-my-stuff/userscript.js
@@ -9,7 +9,6 @@
  *
  * TO-DO:
  * - "No results" notice
- * - Filters
  * - `/` and `enter` hotkeys
  * - Better handling for "Load more"
  */
@@ -18,14 +17,11 @@ import Fuse from "../../libraries/thirdparty/fuse.esm.min.js";
 import fuseOptions from "./fuse-options.js";
 
 export default async function ({ addon, global, console, msg }) {
-  // Inject our custom components
-  let search, searchBar, searchDropdown, oldDropdown, resultsContainer;
+  // Inject our search bar
+  let search, searchBar, searchDropdown, resultsContainer, fuse;
+  let projects = [];
   initialize();
   inject();
-
-  // Index page
-  let projects = [];
-  let fuse;
   indexPage();
 
   // When the user hits enter in the search bar
@@ -37,14 +33,10 @@ export default async function ({ addon, global, console, msg }) {
   // When the user makes a keystroke in the search bar
   searchBar.addEventListener("input", triggerNewSearch);
 
-  // When a new filter is selected from the dropdown
-  searchDropdown.addEventListener("change", () => {
-    if (!fuse) indexPage();
-    triggerNewSearch();
-  });
-
   // When the addon is disabled
-  addon.self.addEventListener("disabled", destroy);
+  addon.self.addEventListener("disabled", () => {
+    search.remove();
+  });
 
   // When the addon is re-enabled
   addon.self.addEventListener("reenabled", inject);
@@ -54,6 +46,7 @@ export default async function ({ addon, global, console, msg }) {
     projects = [];
     resultsContainer = undefined;
     await addon.tab.waitForElement(".media-list > li");
+    searchDropdown = document.querySelector(".dropdown.button.grey.small");
     inject();
   });
 
@@ -65,44 +58,41 @@ export default async function ({ addon, global, console, msg }) {
     searchBar.id = "search-my-stuff-input";
     searchBar.setAttribute("type", "text");
     search.appendChild(searchBar);
-
-    // Create the new "sort by"
-    searchDropdown = document.createElement("select");
-    searchDropdown.id = "search-my-stuff-dropdown";
-    let types = ["relevance", "newest", "oldest"];
-    for (let type of types) {
-      let dropdownOption = document.createElement("option");
-      dropdownOption.value = type;
-      dropdownOption.appendChild(document.createTextNode(msg(type)));
-      searchDropdown.appendChild(dropdownOption);
-    }
-    search.appendChild(searchDropdown);
+    // Keep track of the dropdown
+    await addon.tab.waitForElement(".dropdown.button.grey.small");
+    searchDropdown = document.querySelector(".dropdown.button.grey.small");
   }
 
   /**
-   * Removes the old "sort by" and adds our new search elements.
+   * Adds our new search bar.
+   * 
+   * Since the My Stuff page reloads several elements when switching tabs or
+   * filters, some things need to be reinjected or have their references
+   * updated.
    */
   async function inject() {
-    // Keep track of the old dropdown for removal and readding later
-    await addon.tab.waitForElement(".dropdown.button.grey.small");
-    oldDropdown = document.querySelector(".dropdown.button.grey.small");
-    oldDropdown.remove();
-    // Determine if we're on the My Studios tab
+    // Determine which tab we're on and switch the placeholder text
     if (window.location.href.includes("galleries")) {
       searchBar.setAttribute("placeholder", msg("studio-placeholder"));
+    } else if (window.location.href.includes("trash")) {
+      searchBar.setAttribute("placeholder", msg("trash-placeholder"));
     } else {
       searchBar.setAttribute("placeholder", msg("project-placeholder"));
     }
     // Add new search elements
-    let navIndex = document.querySelector(".inner");
-    navIndex.appendChild(search);
+    await searchDropdown;
+    searchDropdown.before(search);
     // Update the element that stores the search results
     resultsContainer = await addon.tab.waitForElement(".media-list > .media-list");
+    // Update the dropdown listener
+    document.querySelector(".dropdown-menu.radio-style > [data-control=\"sort\"]").addEventListener("click", () => {
+      projects = [];
+    });
   }
 
   /**
    * Indexes the project tile elements and their project names,
-   * then updates the search algorithm
+   * then updates the search algorithm.
    */
   async function indexPage() {
     while (true) {
@@ -114,7 +104,7 @@ export default async function ({ addon, global, console, msg }) {
         element: project,
       });
       fuse = new Fuse(projects, fuseOptions);
-      // Refresh search if user clicks "load more" or switches tabs after searching
+      // Refresh search results if user clicks "load more" or switches tabs with a query still in the search bar
       if (searchBar.value != "") {
         triggerNewSearch();
       }
@@ -122,12 +112,10 @@ export default async function ({ addon, global, console, msg }) {
   }
 
   /**
-   * Reorders based on "sort by"
-   * (relevance algorithm is the same as SA settings page)
+   * Reorders based on fuzzy search algorithm.
    */
   async function triggerNewSearch() {
     let query = searchBar.value;
-    let filter = searchDropdown.value;
     await resultsContainer;
     // Blank query should restore the original order
     if (query == "") {
@@ -147,14 +135,5 @@ export default async function ({ addon, global, console, msg }) {
         resultsContainer.appendChild(result.item.element);
       });
     }
-  }
-
-  /**
-   * Removes our custom components and restores the original dropdown.
-   */
-  async function destroy() {
-    search.remove();
-    let navIndex = document.querySelector(".inner");
-    navIndex.appendChild(oldDropdown);
   }
 }
