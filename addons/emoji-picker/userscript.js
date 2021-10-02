b079dc30950f33947bedcbf87068ff71baa1786b
CST1229 <68464103+CST1229@users.noreply.github.com>
2021-10-02 15:31:06 +0200

    Fix error

--- addons/emoji-picker/userscript.js
+++ addons/emoji-picker/userscript.js
@@ -359,7 +359,7 @@ export default async function ({ addon, global, console, msg }) {
   const setEmojiPickerPos = function () {
     emojiPicker.style.top = emojiPickerOffset + "px";
     //scratchr2 makes the body and root <html>'s height value the size of the screen somehow so I have to do this
-    realDocumentBody = addon.tab.clientVersion === "scratchr2" ? document.querySelector("#pagewrapper") : document.body;
+    let realDocumentBody = addon.tab.clientVersion === "scratchr2" ? document.querySelector("#pagewrapper") : document.body;
     if (emojiPicker.getBoundingClientRect().bottom > realDocumentBody.getBoundingClientRect().bottom - 48) {
       //Emoji picker may be partially hidden, move up
       emojiPicker.style.top = -emojiPicker.getBoundingClientRect().height + "px";
