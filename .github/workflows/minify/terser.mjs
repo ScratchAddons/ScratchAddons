f31cba009751fb7226c8ed031a97d342f0b923c7
RedGuy12 <paul@reid-family.org>
2021-10-03 10:18:54 -0500

    disable terser on .user.js

--- .github/workflows/minify/terser.mjs
+++ .github/workflows/minify/terser.mjs
@@ -8,6 +8,7 @@ import getInDir from "./getInDir.mjs";
 const terserConfig = JSON.parse(readFileSync(resolve(process.cwd(), "./.terserrc")));
 
 getInDir({ path: "./", ext: ".js" }).forEach(async (filePath) => {
+  if (filePath.endsWith(".user.js")) return
   console.log(`Minifying ${filePath}`);
   const source = await readFile(filePath, "utf8").catch(console.error);
   const minfied = (await minifyJs(source, terserConfig).catch(console.error)).code;
