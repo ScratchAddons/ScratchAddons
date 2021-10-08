2ea10a9f4ccd07212d2d6d9f8df60ee55b57cfbd
GarboMuffin <33787854+GarboMuffin@users.noreply.github.com>
2021-10-08 16:33:09 -0500

    block-duplicate: fix obscured shadow IDs (#3619)
    * block-duplicate: fix obscured shadow IDs
    
    * fix typo

--- addons/block-duplicate/userscript.js
+++ addons/block-duplicate/userscript.js
@@ -5,32 +5,43 @@ export default async function ({ addon, global, console }) {
   ScratchBlocks.Gesture.prototype.startDraggingBlock_ = function (...args) {
     if (
       !this.flyout_ &&
+      !this.shouldDuplicateOnDrag_ &&
       this.targetBlock_.type !== "procedures_definition" &&
       this.mostRecentEvent_.altKey &&
       !addon.self.disabled
     ) {
-      // Scratch will reset the group on its own when the drag ends
+      // Scratch will reset these when the drag ends
       if (!ScratchBlocks.Events.getGroup()) {
         ScratchBlocks.Events.setGroup(true);
       }
-      this.shouldDuplicateOnDrag_ = true;
-      this.sa_shouldIsolateDuplicatedBlock = this.mostRecentEvent_.ctrlKey || this.mostRecentEvent_.metaKey;
-    } else {
-      this.sa_shouldIsolateDuplicatedBlock = false;
-    }
-    return originalStartDraggingBlock.call(this, ...args);
-  };
-  // https://github.com/LLK/scratch-blocks/blob/e86f115457006d1cde83baa23eaaf1ee16d315f5/core/gesture.js#L983
-  const originalDuplicateOnDrag = ScratchBlocks.Gesture.prototype.duplicateOnDrag_;
-  ScratchBlocks.Gesture.prototype.duplicateOnDrag_ = function (...args) {
-    const ret = originalDuplicateOnDrag.call(this, ...args);
-    if (this.sa_shouldIsolateDuplicatedBlock) {
-      const block = this.targetBlock_;
-      const nextBlock = block.getNextBlock();
-      if (nextBlock) {
-        nextBlock.dispose();
+      this.startWorkspace_.setResizesEnabled(false);
+      // Based on https://github.com/LLK/scratch-blocks/blob/feda366947432b9d82a4f212f43ff6d4ab6f252f/core/scratch_blocks_utils.js#L171
+      // Setting this.shouldDuplicateOnDrag_ = true does NOT work because it doesn't call changeObscuredShadowIds
+      ScratchBlocks.Events.disable();
+      let newBlock;
+      try {
+        const xmlBlock = ScratchBlocks.Xml.blockToDom(this.targetBlock_);
+        newBlock = ScratchBlocks.Xml.domToBlock(xmlBlock, this.startWorkspace_);
+        ScratchBlocks.scratchBlocksUtils.changeObscuredShadowIds(newBlock);
+        const xy = this.targetBlock_.getRelativeToSurfaceXY();
+        newBlock.moveBy(xy.x, xy.y);
+        if (this.mostRecentEvent_.ctrlKey || this.mostRecentEvent_.metaKey) {
+          const nextBlock = newBlock.getNextBlock();
+          if (nextBlock) {
+            nextBlock.dispose();
+          }
+        }
+      } catch (e) {
+        console.error(e);
+      }
+      ScratchBlocks.Events.enable();
+      if (newBlock) {
+        this.targetBlock_ = newBlock;
+        if (ScratchBlocks.Events.isEnabled()) {
+          ScratchBlocks.Events.fire(new ScratchBlocks.Events.BlockCreate(newBlock));
+        }
       }
     }
-    return ret;
+    return originalStartDraggingBlock.call(this, ...args);
   };
 }
