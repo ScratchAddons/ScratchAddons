ceaa3246450ae17dd39b96d9efd8d04cd13ae3be
Tacodude <27910867+Tacodude7729@users.noreply.github.com>
2021-10-14 19:06:47 +1100

    Add FPS addon.

---
+++ addons/fps/userscript.js
@@ -0,0 +1,36 @@
+export default async function ({ addon, global, cons, msg }) {
+    await new Promise((resolve) => {
+        if (addon.tab.traps.vm.editingTarget) return resolve();
+        addon.tab.traps.vm.runtime.once("PROJECT_LOADED", resolve);
+    });
+
+    const renderer = addon.tab.traps.vm.runtime.renderer;
+
+    let fpsCounterElement = document.createElement("span");
+    fpsCounterElement.className = "fps-counter";
+
+    const renderTimes = [];
+    var fps = "?";
+    var lastFps = 0;
+
+    renderer.ogDraw = renderer.draw;
+    renderer.draw = function () {
+        const now = Date.now();
+        while (renderTimes.length > 0 && renderTimes[0] <= now - 2000)
+            renderTimes.shift();
+        renderTimes.push(now);
+        fps = Math.floor(renderTimes.length / 2);
+        if (fps !== lastFps)
+            fpsCounterElement.innerText = msg("fpsCounter", { fps: lastFps = fps });
+        addon.tab.displayNoneWhileDisabled(fpsCounterElement, { display: "flex" });
+        renderer.ogDraw();
+    }
+
+    while (true) {
+        await addon.tab.waitForElement('[class*="controls_controls-container"]', {
+            markAsSeen: true,
+            reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
+        });
+        addon.tab.appendToSharedSpace({ space: "afterStopButton", element: fpsCounterElement, order: 2 });
+    }
+}
