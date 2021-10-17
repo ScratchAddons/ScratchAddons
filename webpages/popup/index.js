c0a75798ee54247cc65db7f9179898fbc29e69b6
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-10-16 21:43:59 -0400

    Make changelog link in the popup into a `href` link

--- webpages/popup/index.js
+++ webpages/popup/index.js
@@ -46,13 +46,6 @@ const vue = new Vue({
       chrome.runtime.openOptionsPage();
       this.closePopup();
     },
-    openChangelog() {
-      const uiLanguage = chrome.i18n.getUILanguage();
-      const localeSlash = uiLanguage.startsWith("en") ? "" : `${uiLanguage.split("-")[0]}/`;
-      const utm = `utm_source=extension&utm_medium=popup&utm_campaign=v${chrome.runtime.getManifest().version}`;
-      window.open(`https://scratchaddons.com/${localeSlash}changelog/?${utm}`);
-      this.closePopup();
-    },
     setPopup(popup) {
       if (this.currentPopup !== popup) {
         this.currentPopup = popup;
@@ -62,6 +55,14 @@ const vue = new Vue({
     },
     iframeSrc(addonId) {
       return vue.popups.find((addon) => addon._addonId === addonId).html;
+    },
+  },
+  computed: {
+    changelogLink() {
+      const uiLanguage = chrome.i18n.getUILanguage();
+      const localeSlash = uiLanguage.startsWith("en") ? "" : `${uiLanguage.split("-")[0]}/`;
+      const utm = `utm_source=extension&utm_medium=popup&utm_campaign=v${chrome.runtime.getManifest().version}`;
+      return `https://scratchaddons.com/${localeSlash}changelog/?${utm}`;
     },
   },
 });
