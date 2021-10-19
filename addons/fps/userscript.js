ef6f9700da24d4b25d3f0181556c2c4e7b66ee7b
Tacodude7729 <Tacodude7729@users.noreply.github.com>
2021-10-19 02:55:42 +0000

    Format code

--- addons/fps/userscript.js
+++ addons/fps/userscript.js
@@ -1,39 +1,37 @@
 export default async function ({ addon, global, cons, msg }) {
-    await new Promise((resolve) => {
-        if (addon.tab.traps.vm.editingTarget) return resolve();
-        addon.tab.traps.vm.runtime.once("PROJECT_LOADED", resolve);
+  await new Promise((resolve) => {
+    if (addon.tab.traps.vm.editingTarget) return resolve();
+    addon.tab.traps.vm.runtime.once("PROJECT_LOADED", resolve);
+  });
+
+  const renderer = addon.tab.traps.vm.runtime.renderer;
+
+  let fpsCounterElement = document.createElement("span");
+  fpsCounterElement.className = "fps-counter";
+
+  const renderTimes = [];
+  var fps = "?";
+  var lastFps = 0;
+  var firstTime = -1;
+
+  renderer.ogDraw = renderer.draw;
+  renderer.draw = function () {
+    const now = Date.now();
+    while (renderTimes.length > 0 && renderTimes[0] <= now - 2000) renderTimes.shift();
+    renderTimes.push(now);
+    fps = Math.floor(renderTimes.length / 2);
+    if (firstTime === -1) firstTime = now;
+    if (now - firstTime <= 2500) fps = "?";
+    if (fps !== lastFps) fpsCounterElement.innerText = msg("fpsCounter", { fps: (lastFps = fps) });
+    addon.tab.displayNoneWhileDisabled(fpsCounterElement, { display: "flex" });
+    renderer.ogDraw();
+  };
+
+  while (true) {
+    await addon.tab.waitForElement('[class*="controls_controls-container"]', {
+      markAsSeen: true,
+      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
     });
-
-    const renderer = addon.tab.traps.vm.runtime.renderer;
-
-    let fpsCounterElement = document.createElement("span");
-    fpsCounterElement.className = "fps-counter";
-
-    const renderTimes = [];
-    var fps = "?";
-    var lastFps = 0;
-    var firstTime = -1;
-
-    renderer.ogDraw = renderer.draw;
-    renderer.draw = function () {
-        const now = Date.now();
-        while (renderTimes.length > 0 && renderTimes[0] <= now - 2000)
-            renderTimes.shift();
-        renderTimes.push(now);
-        fps = Math.floor(renderTimes.length / 2);
-        if (firstTime === -1) firstTime = now;
-        if (now - firstTime <= 2500) fps = "?";
-        if (fps !== lastFps)
-            fpsCounterElement.innerText = msg("fpsCounter", { fps: lastFps = fps });
-        addon.tab.displayNoneWhileDisabled(fpsCounterElement, { display: "flex" });
-        renderer.ogDraw();
-    }
-
-    while (true) {
-        await addon.tab.waitForElement('[class*="controls_controls-container"]', {
-            markAsSeen: true,
-            reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
-        });
-        addon.tab.appendToSharedSpace({ space: "afterStopButton", element: fpsCounterElement, order: 3 });
-    }
+    addon.tab.appendToSharedSpace({ space: "afterStopButton", element: fpsCounterElement, order: 3 });
+  }
 }
