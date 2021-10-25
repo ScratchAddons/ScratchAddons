79680b9398ddd43e44064fd6d8bb6c14abea16ca
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-10-24 23:04:04 -0500

    Custom block definition peek + reduce transparency option

--- addons/editor-comment-previews/userscript.js
+++ addons/editor-comment-previews/userscript.js
@@ -47,13 +47,11 @@ export default async function ({ addon, global, console }) {
   const workspaceElement = await addon.tab.waitForElement('[class*="blocks_blocks"]');
   commentObserver.observe(workspaceElement, { childList: true, subtree: true });
 
-  // Listeners for regular blocks
+  // Listeners for blocks
   const isBlockElement = (element) => {
     if (
-      (element.matches(".blocklyBlockCanvas g.blocklyDraggable") ||
-        element.parentElement.matches(".blocklyBlockCanvas g.blocklyDraggable")) &&
-      (element.querySelector(".blocklyIconGroup") !== null ||
-        element.parentElement.querySelector(".blocklyIconGroup") !== null)
+      element.matches(".blocklyBlockCanvas g.blocklyDraggable") ||
+      element.parentElement.matches(".blocklyBlockCanvas g.blocklyDraggable")
     ) {
       if (element.matches(".blocklyBlockCanvas g.blocklyDraggable")) {
         // We are directly hovering over one of the block's elements
@@ -68,22 +66,48 @@ export default async function ({ addon, global, console }) {
   };
 
   document.addEventListener("mousemove", (e) => {
-    let element = e.target.parentElement;
-    const elementType = isBlockElement(element);
-    if (elementType !== false) {
-      if (elementType === "component") {
-        element = element.parentElement;
-      }
-      const commentId = vm.editingTarget.blocks._blocks[element.getAttribute("data-id")].comment;
-      const commentObject = vm.editingTarget.comments[commentId];
-      if (!addon.self.disabled && addon.settings.get("hover-view-block") && commentObject.text !== "") {
-        previewElement.style.left = e.pageX + 8 + "px";
-        previewElement.style.top = e.pageY + 8 + "px";
-        previewElement.innerText = commentObject.text;
-        if (previewElement.innerText !== "") previewElement.classList.remove("sa-comment-preview-hidden");
+    if (
+      !addon.self.disabled &&
+      (addon.settings.get("hover-view-block") || addon.settings.get("hover-view-procedure"))
+    ) {
+      let element = e.target.parentElement;
+      const elementType = isBlockElement(element);
+      if (elementType !== false) {
+        if (elementType === "component") {
+          element = element.parentElement;
+        }
+        const blocks = vm.editingTarget.blocks._blocks;
+        const block = blocks[element.getAttribute("data-id")];
+        if (block) {
+          const comments = vm.editingTarget.comments;
+          let commentObject = comments[block.comment];
+          if (addon.settings.get("hover-view-block") && commentObject && commentObject.text !== "") {
+            setPreview(e, commentObject, previewElement);
+          } else if (addon.settings.get("hover-view-procedure") && block.opcode === "procedures_call") {
+            const blockName = block.mutation.proccode;
+            const prototypeBlock =
+              blocks[
+                Object.entries(blocks).find((i) => {
+                  return i[1].opcode === "procedures_prototype" && i[1].mutation.proccode === blockName;
+                })[0]
+              ];
+            const definitionBlock = blocks[prototypeBlock.parent];
+            commentObject = comments[definitionBlock.comment];
+            if (commentObject.text !== "") {
+              setPreview(e, commentObject, previewElement);
+            }
+          }
+        }
       }
     }
   });
+
+  function setPreview(event, comment, element) {
+    element.style.left = event.pageX + 8 + "px";
+    element.style.top = event.pageY + 8 + "px";
+    element.innerText = comment.text;
+    if (element.innerText !== "") element.classList.remove("sa-comment-preview-hidden");
+  }
 
   document.addEventListener("mouseout", (e) => {
     const element = e.target.parentElement;
@@ -92,10 +116,20 @@ export default async function ({ addon, global, console }) {
     }
   });
 
-  document.addEventListener("mousedown", (e) => {
-    const element = e.target.parentElement;
-    if (isBlockElement(element)) {
-      previewElement.classList.add("sa-comment-preview-hidden");
-    }
+  document.addEventListener("click", (e) => {
+    previewElement.classList.add("sa-comment-preview-hidden");
   });
+
+  function setTransparency() {
+    console.log("transparency set");
+    if (addon.settings.get("reduce-transparency")) {
+      previewElement.classList.add("sa-reduce-transparency");
+    } else {
+      previewElement.classList.remove("sa-reduce-transparency");
+    }
+  }
+
+  setTransparency();
+
+  addon.settings.addEventListener("change", setTransparency);
 }
