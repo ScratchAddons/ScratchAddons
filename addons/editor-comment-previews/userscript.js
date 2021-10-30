5b6c9ecce9731cd5d86c2518c64b12aaa8820f9a
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-10-30 16:23:50 -0500

    Refactor comment handling, add "follow mouse" option
    - Comments no longer use a MutationObserver -- they now rely on the same document event listeners as blocks.
    - The user now has the option to toggle the preview mouse-following behavior independently of the delay setting.

--- addons/editor-comment-previews/userscript.js
+++ addons/editor-comment-previews/userscript.js
@@ -1,6 +1,5 @@
 /**
  * TO-DO:
- * - Refactor collapsed comment previews
  * - Clean up everything
  * - Fix block-to-block bug
  */
@@ -30,50 +29,19 @@ export default async function ({ addon, global, console }) {
   previewElement.classList.add("sa-comment-preview-hidden");
   document.querySelector("body").appendChild(previewElement);
 
-  // Listeners for collapsed comments
-  const commentObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.target.matches("g.blocklyBubbleCanvas")) {
-        if (mutation.removedNodes.length === 0) {
-          const hoverArea = mutation.addedNodes[0];
-          hoverArea.addEventListener("mousemove", (e) => {
-            if (
-              !addon.self.disabled &&
-              addon.settings.get("hover-view") &&
-              hoverArea.querySelector("text.scratchCommentText").getAttribute("display") === null &&
-              hoverArea.querySelector("text.scratchCommentText").innerText !== ""
-            ) {
-              previewElement.style.left = e.pageX + 8 + "px";
-              previewElement.style.top = e.pageY + 8 + "px";
-              previewElement.innerText = hoverArea.querySelector("textarea").value;
-              if (previewElement.innerText !== "") previewElement.classList.remove("sa-comment-preview-hidden");
-            }
-          });
-          hoverArea.addEventListener("mouseleave", (e) => {
-            previewElement.classList.add("sa-comment-preview-hidden");
-          });
-          hoverArea.querySelector("image").addEventListener("mouseup", (e) => {
-            previewElement.classList.add("sa-comment-preview-hidden");
-          });
-        } else {
-          previewElement.classList.add("sa-comment-preview-hidden");
-        }
-      }
-    });
-  });
-  const workspaceElement = await addon.tab.waitForElement('[class*="blocks_blocks"]');
-  commentObserver.observe(workspaceElement, { childList: true, subtree: true });
-
   // Listeners for blocks
   let mouseOver = false;
-  let mouseOverBlock = null;
+
   const isBlockElement = (element) => {
     if (
       element &&
       (element.matches(".blocklyBlockCanvas g.blocklyDraggable") ||
-        element.parentElement.matches(".blocklyBlockCanvas g.blocklyDraggable"))
+        element.parentElement.matches(".blocklyBlockCanvas g.blocklyDraggable") ||
+        element.matches("g.blocklyBubbleCanvas *"))
     ) {
-      if (element.matches(".blocklyBlockCanvas g.blocklyDraggable")) {
+      if (element.matches("g.blocklyBubbleCanvas *")) {
+        return "comment";
+      } else if (element.matches(".blocklyBlockCanvas g.blocklyDraggable")) {
         // We are directly hovering over one of the block's elements
         return "block";
       } else {
@@ -90,42 +58,55 @@ export default async function ({ addon, global, console }) {
       let element = e.target.parentElement;
       const elementType = isBlockElement(element);
       if (mouseOver === false) {
-        if (addon.settings.get("hover-view-block") || addon.settings.get("hover-view-procedure")) {
+        if (
+          addon.settings.get("hover-view") ||
+          addon.settings.get("hover-view-block") ||
+          addon.settings.get("hover-view-procedure")
+        ) {
           if (elementType !== false) {
-            if (elementType === "component") {
-              element = element.parentElement;
-            }
-            let blocks;
-            if (element.matches(".blocklyFlyout *")) {
-              blocks = flyout.workspace_.blockDB_;
-            } else {
-              blocks = vm.editingTarget.blocks._blocks;
-            }
-            let block = blocks[element.getAttribute("data-id")];
-            if (block) {
-              const comments = vm.editingTarget.comments;
-              let commentObject = comments[block.comment];
-              if (addon.settings.get("hover-view-block") && commentObject && commentObject.text !== "") {
-                mouseOverBlock = block.id;
-                setPreview(e, commentObject, previewElement);
-              } else if (
-                addon.settings.get("hover-view-procedure") &&
-                (block.opcode === "procedures_call" || block.procCode_)
+            if (elementType === "comment" && addon.settings.get("hover-view")) {
+              const hoverArea = e.target.parentElement;
+              if (
+                hoverArea.querySelector("text.scratchCommentText") &&
+                hoverArea.querySelector("text.scratchCommentText").getAttribute("display") === null &&
+                hoverArea.querySelector("text.scratchCommentText").innerText !== ""
               ) {
-                let blockName;
-                if (block.mutation) blockName = block.mutation.proccode;
-                else blockName = block.procCode_;
-                const prototypeBlock =
-                  vm.editingTarget.blocks._blocks[
-                    Object.entries(vm.editingTarget.blocks._blocks).find((i) => {
-                      return i[1].opcode === "procedures_prototype" && i[1].mutation.proccode === blockName;
-                    })[0]
-                  ];
-                const definitionBlock = vm.editingTarget.blocks._blocks[prototypeBlock.parent];
-                commentObject = comments[definitionBlock.comment];
-                if (commentObject.text !== "") {
-                  mouseOverBlock = block.id;
-                  setPreview(e, commentObject, previewElement);
+                setPreview(e, hoverArea.querySelector("textarea").value, previewElement);
+              }
+            } else {
+              if (elementType === "component") {
+                element = element.parentElement;
+              }
+              let blocks;
+              if (element.matches(".blocklyFlyout *")) {
+                blocks = flyout.workspace_.blockDB_;
+              } else {
+                blocks = vm.editingTarget.blocks._blocks;
+              }
+              let block = blocks[element.getAttribute("data-id")];
+              if (block) {
+                const comments = vm.editingTarget.comments;
+                let commentObject = comments[block.comment];
+                if (addon.settings.get("hover-view-block") && commentObject && commentObject.text !== "") {
+                  setPreview(e, commentObject.text, previewElement);
+                } else if (
+                  addon.settings.get("hover-view-procedure") &&
+                  (block.opcode === "procedures_call" || block.procCode_)
+                ) {
+                  let blockName;
+                  if (block.mutation) blockName = block.mutation.proccode;
+                  else blockName = block.procCode_;
+                  const prototypeBlock =
+                    vm.editingTarget.blocks._blocks[
+                      Object.entries(vm.editingTarget.blocks._blocks).find((i) => {
+                        return i[1].opcode === "procedures_prototype" && i[1].mutation.proccode === blockName;
+                      })[0]
+                    ];
+                  const definitionBlock = vm.editingTarget.blocks._blocks[prototypeBlock.parent];
+                  commentObject = comments[definitionBlock.comment];
+                  if (commentObject.text !== "") {
+                    setPreview(e, commentObject.text, previewElement);
+                  }
                 }
               }
             }
@@ -134,7 +115,6 @@ export default async function ({ addon, global, console }) {
       } else if (elementType === false) {
         previewElement.classList.add("sa-comment-preview-hidden");
         mouseOver = false;
-        mouseOverBlock = null;
       }
     }
   });
@@ -142,7 +122,7 @@ export default async function ({ addon, global, console }) {
   document.addEventListener("mousemove", (e) => {
     if (
       !addon.self.disabled &&
-      (addon.settings.get("delay") === "none" ||
+      (addon.settings.get("follow-mouse") === true ||
         (previewElement.classList.contains("sa-comment-preview-hidden") &&
           window.getComputedStyle(previewElement).opacity < 0.001))
     ) {
@@ -152,13 +132,13 @@ export default async function ({ addon, global, console }) {
     if (e.target === previewElement) previewElement.classList.add("sa-comment-preview-hidden");
   });
 
-  function setPreview(e, comment, element) {
+  function setPreview(e, text, element) {
     let timeout;
     if (addon.settings.get("delay") === "long") timeout = 500;
     else if (addon.settings.get("delay") === "short") timeout = 300;
     else timeout = 0;
     mouseOver = true;
-    element.innerText = comment.text;
+    element.innerText = text;
     setTimeout(() => {
       if (element.innerText !== "" && mouseOver) {
         element.classList.remove("sa-comment-preview-hidden");
@@ -169,7 +149,6 @@ export default async function ({ addon, global, console }) {
   document.addEventListener("click", (e) => {
     previewElement.classList.add("sa-comment-preview-hidden");
     mouseOver = false;
-    mouseOverBlock = null;
   });
 
   function setAppearance() {
