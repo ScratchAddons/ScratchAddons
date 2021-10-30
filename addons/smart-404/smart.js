07bbdf6c96bca74fd3354823fbd7cafcee38ca9c
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-30 21:15:40 +1300

    First functional version

--- addons/smart-404/smart.js
+++ addons/smart-404/smart.js
@@ -1,26 +1,53 @@
 export default async function ({ addon, global, console }) {
-    console.log(console);
-    if (addon.tab.clientVersion == "scratchr2") {
-        let box = document.getElementsByClassName("box-content")[0];
-        runBox(box);
-    } else {
-        let box = document.getElementsByClassName("flex-row inner")[0];
-        runBox(box);
+  if (addon.tab.clientVersion == "scratchr2") {
+    let box = document.getElementsByClassName("box-content")[0];
+    runBox(box);
+  } else {
+    let box = document.getElementsByClassName("flex-row inner")[0];
+    runBox(box);
+  }
+
+  function runBox(box) {
+    // this code will just error out if there is not a 404 and should not cause any problems
+    let smartbox = document.createElement("div");
+    smartbox.className = "sa-smart-error";
+    let p = document.createElement("p");
+    p.innerText = "We think that you might be trying to find this: ";
+    p.style = "float: left;"
+    smartbox.append(p);
+    let link = document.createElement("a");
+    const showTip = calculateTip();
+    link.href = showTip;
+    link.innerText = " " + showTip;
+    link.style = "float: right;"
+    smartbox.append(link);
+    box.prepend(smartbox);
+  }
+  function calculateTip() {
+    let tip = "https://scratch.mit.edu";
+    const url = location.pathname;
+    if (url.endsWith("%29")) {
+      tip = "https://scratch.mit.edu" + url.substr(0, url.length - 3);
     }
-    
-    
-    function runBox(box) {
-        let smartbox = document.createElement("div");
-        smartbox.className = "sa-smart-error";
-        let h1 = document.createElement("h1");
-        h1.innerText = location.pathname;
-        smartbox.prepend(h1);
-        box.prepend(smartbox);
-    }
-    function calculateTip() {
-        let tip = "https://scratch.mit.edu";
-        if (tip.endsWith("%")) {
-            
+    if (url.endsWith(")")) {
+        tip = "https://scratch.mit.edu" + url.substr(0, url.length - 1);
+      }
+    if (url.endsWith("//")) {
+        const spliturl = url.toString().split("");
+        const reversedarray = spliturl.reverse();
+        const reversedURL = reversedarray.join('');
+        for (let index = 0; index < reversedURL.length; index++) {
+            if (reversedURL[index] == "/") {
+                continue;
+            } else {
+                tip = "https://scratch.mit.edu" + url.substr(0, url.length - index);
+                break;
+            }
         }
     }
+    if (location.pathname == "/projects" || location.pathname == "/projects/") {
+        tip = "https://scratch.mit.edu/explore";
+    }
+    return tip;
+  }
 }
