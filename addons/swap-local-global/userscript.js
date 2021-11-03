06fb284d4dabf67c5d5ff62edf65bfa46ceaf5e3
GarboMuffin <GarboMuffin@users.noreply.github.com>
2021-11-03 22:25:55 +0000

    Format code

--- addons/swap-local-global/userscript.js
+++ addons/swap-local-global/userscript.js
@@ -5,28 +5,32 @@ export default async function ({ addon, msg, console }) {
   const getTargetName = (target) => {
     if (target.isStage) {
       // Stage always has an internal name of "Stage", but we want a translatable name
-      return ScratchBlocks.ScratchMsgs.translate('SENSING_OF_STAGE', 'Stage');
+      return ScratchBlocks.ScratchMsgs.translate("SENSING_OF_STAGE", "Stage");
     }
     return target.getName();
   };
 
-  const getTargetsThatUseVariable = (id) => vm.runtime.targets
-    .filter((target) => target.isOriginal)
-    .filter((target) => Object.values(target.blocks._blocks).find((block) => (
-      (block.fields.LIST && block.fields.LIST.id === id) ||
-      (block.fields.VARIABLE && block.fields.VARIABLE.id === id)
-    )));
+  const getTargetsThatUseVariable = (id) =>
+    vm.runtime.targets
+      .filter((target) => target.isOriginal)
+      .filter((target) =>
+        Object.values(target.blocks._blocks).find(
+          (block) =>
+            (block.fields.LIST && block.fields.LIST.id === id) ||
+            (block.fields.VARIABLE && block.fields.VARIABLE.id === id)
+        )
+      );
 
   // https://github.com/LLK/scratch-vm/blob/7c6f1e44fb0a9b0d0279225cd4c62fbe59b6af54/src/engine/blocks.js#L388-L394
-  const getTargetsWithLocalVariableNamed = (name, type) => vm.runtime.targets
-    .filter((target) => target.isOriginal && target.lookupVariableByNameAndType(name, type, true));
+  const getTargetsWithLocalVariableNamed = (name, type) =>
+    vm.runtime.targets.filter((target) => target.isOriginal && target.lookupVariableByNameAndType(name, type, true));
 
   const getVmVariable = (id) => vm.editingTarget.lookupVariableById(id);
   const isStageSelected = () => vm.editingTarget.isStage;
 
   const deleteVariableWithoutDeletingBlocks = (workspace, variable) => {
     // variable can be an ID or an actual Blockly variable object
-    if (typeof variable === 'string') {
+    if (typeof variable === "string") {
       variable = workspace.getVariableById(variable);
     }
     workspace.variableMap_.deleteVariable(variable);
@@ -90,25 +94,25 @@ export default async function ({ addon, msg, console }) {
         if (oldMonitorState.visible) {
           vm.runtime.monitorBlocks.changeBlock({
             id,
-            element: 'checkbox',
-            value: true
+            element: "checkbox",
+            value: true,
           });
         }
         const isLocal = !vm.runtime.getTargetForStage().variables[id];
         let newMonitorState = oldMonitorState;
         if (isLocal) {
           const target = vm.editingTarget;
-          newMonitorState = newMonitorState.set('targetId', target.id)
-          newMonitorState = newMonitorState.set('spriteName', target.getName());
+          newMonitorState = newMonitorState.set("targetId", target.id);
+          newMonitorState = newMonitorState.set("spriteName", target.getName());
         } else {
-          newMonitorState = newMonitorState.set('targetId', null);
-          newMonitorState = newMonitorState.set('spriteName', null);
+          newMonitorState = newMonitorState.set("targetId", null);
+          newMonitorState = newMonitorState.set("spriteName", null);
         }
         if (newVmVariable.name !== oldVmVariable.name) {
           const monitorBlocks = vm.runtime.monitorBlocks;
           const block = monitorBlocks.getBlock(id);
           if (block) {
-            newMonitorState = newMonitorState.set('params', monitorBlocks._getBlockParams(block));
+            newMonitorState = newMonitorState.set("params", monitorBlocks._getBlockParams(block));
           }
         }
         vm.runtime.requestAddMonitor(newMonitorState);
@@ -121,7 +125,7 @@ export default async function ({ addon, msg, console }) {
   };
 
   const convertVariable = (oldBlocklyVariable, newLocal, newCloud) => {
-    const CLOUD_PREFIX = '☁ ';
+    const CLOUD_PREFIX = "☁ ";
 
     const name = oldBlocklyVariable.name;
     const id = oldBlocklyVariable.getId();
@@ -134,7 +138,7 @@ export default async function ({ addon, msg, console }) {
 
     // Cloud variables must always be global
     if (newCloud && newLocal) {
-      alert(msg('cant-convert-cloud'));
+      alert(msg("cant-convert-cloud"));
       return;
     }
 
@@ -143,7 +147,7 @@ export default async function ({ addon, msg, console }) {
       if (newLocal) {
         // Stage cannot have local variables
         if (isStageSelected()) {
-          alert(msg('cant-convert-stage'));
+          alert(msg("cant-convert-stage"));
           return;
         }
         // Variables used by unfocused sprites cannot be made local
@@ -151,13 +155,17 @@ export default async function ({ addon, msg, console }) {
         const targets = getTargetsThatUseVariable(id);
         if (!targets.every((i) => i === editingTarget)) {
           if (targets.length > 1) {
-            alert(msg('cant-convert-to-local', {
-              sprites: targets.map(getTargetName).join(', ')
-            }))
+            alert(
+              msg("cant-convert-to-local", {
+                sprites: targets.map(getTargetName).join(", "),
+              })
+            );
           } else {
-            alert(msg('cant-convert-used-elsewhere', {
-              sprite: getTargetName(targets[0])
-            }));
+            alert(
+              msg("cant-convert-used-elsewhere", {
+                sprite: getTargetName(targets[0]),
+              })
+            );
           }
           return;
         }
@@ -165,9 +173,11 @@ export default async function ({ addon, msg, console }) {
         // Global variables must not conflict with any local variables
         const targets = getTargetsWithLocalVariableNamed(name, type).filter((target) => target !== editingTarget);
         if (targets.length > 0) {
-          alert(msg('cant-convert-conflict', {
-            sprites: targets.map(getTargetName).join(', ')
-          }));
+          alert(
+            msg("cant-convert-conflict", {
+              sprites: targets.map(getTargetName).join(", "),
+            })
+          );
           return;
         }
       }
@@ -178,7 +188,7 @@ export default async function ({ addon, msg, console }) {
       if (newCloud) {
         newName = CLOUD_PREFIX + name;
       } else if (name.startsWith(CLOUD_PREFIX)) {
-        newName = name.replace(CLOUD_PREFIX, '');
+        newName = name.replace(CLOUD_PREFIX, "");
       }
     }
 
@@ -200,8 +210,8 @@ export default async function ({ addon, msg, console }) {
     const createEvent = stack[stack.length - 1];
     const deleteEvent = stack[stack.length - 2];
     if (
-      (createEvent instanceof ScratchBlocks.Events.VarCreate) &&
-      (deleteEvent instanceof ScratchBlocks.Events.VarDelete)
+      createEvent instanceof ScratchBlocks.Events.VarCreate &&
+      deleteEvent instanceof ScratchBlocks.Events.VarDelete
     ) {
       createEvent.run = customUndoVarCreate;
       deleteEvent.run = customUndoVarDelete;
@@ -221,56 +231,64 @@ export default async function ({ addon, msg, console }) {
     }
 
     const headerTitle = promptBody.parentElement.querySelector('[class^="modal_header-item_2zQTd"]');
-    if (variable.type === '') {
-      headerTitle.textContent = msg('edit-variable-header');
+    if (variable.type === "") {
+      headerTitle.textContent = msg("edit-variable-header");
     } else {
-      headerTitle.textContent = msg('edit-list-header');
+      headerTitle.textContent = msg("edit-list-header");
     }
 
-    const root = document.createElement('div');
+    const root = document.createElement("div");
 
     const createLabeledInput = (text, value) => {
-      const outer = document.createElement('label');
-      const input = document.createElement('input');
-      if (value === 'checkbox') {
-        input.type = 'checkbox';
+      const outer = document.createElement("label");
+      const input = document.createElement("input");
+      if (value === "checkbox") {
+        input.type = "checkbox";
       } else {
-        input.name = 'variableScopeOption';
-        input.type = 'radio';
+        input.name = "variableScopeOption";
+        input.type = "radio";
         input.value = value;
       }
       outer.appendChild(input);
-      const label = document.createElement('span');
+      const label = document.createElement("span");
       label.textContent = text;
       outer.appendChild(label);
       return {
         outer,
         label,
-        input
+        input,
       };
     };
-    const promptDisabledClass = addon.tab.scratchClass('prompt_disabled-label');
+    const promptDisabledClass = addon.tab.scratchClass("prompt_disabled-label");
 
-    const noLocalsInStageSection = document.createElement('div');
-    noLocalsInStageSection.className = addon.tab.scratchClass('prompt_info-message', 'prompt_cloud-option', {
-      others: 'sa-swap-local-global-stage'
+    const noLocalsInStageSection = document.createElement("div");
+    noLocalsInStageSection.className = addon.tab.scratchClass("prompt_info-message", "prompt_cloud-option", {
+      others: "sa-swap-local-global-stage",
     });
-    noLocalsInStageSection.appendChild(Object.assign(document.createElement('span'), {
-      textContent: addon.tab.scratchMessage('gui.gui.variablePromptAllSpritesMessage')
-    }));
+    noLocalsInStageSection.appendChild(
+      Object.assign(document.createElement("span"), {
+        textContent: addon.tab.scratchMessage("gui.gui.variablePromptAllSpritesMessage"),
+      })
+    );
 
-    const scopeSection = document.createElement('div');
-    scopeSection.className = addon.tab.scratchClass('prompt_options-row', 'prompt_cloud-option');
-    const forAllSprites = createLabeledInput(addon.tab.scratchMessage('gui.gui.variableScopeOptionAllSprites'), 'global');
-    const forThisSpriteOnly = createLabeledInput(addon.tab.scratchMessage('gui.gui.variableScopeOptionSpriteOnly'), 'local');
+    const scopeSection = document.createElement("div");
+    scopeSection.className = addon.tab.scratchClass("prompt_options-row", "prompt_cloud-option");
+    const forAllSprites = createLabeledInput(
+      addon.tab.scratchMessage("gui.gui.variableScopeOptionAllSprites"),
+      "global"
+    );
+    const forThisSpriteOnly = createLabeledInput(
+      addon.tab.scratchMessage("gui.gui.variableScopeOptionSpriteOnly"),
+      "local"
+    );
     forAllSprites.input.checked = !variable.isLocal;
     forThisSpriteOnly.input.checked = variable.isLocal;
     scopeSection.appendChild(forAllSprites.outer);
     scopeSection.appendChild(forThisSpriteOnly.outer);
 
-    const cloudSection = document.createElement('div');
-    cloudSection.className = addon.tab.scratchClass('prompt_cloud-option');
-    const cloudCheckbox = createLabeledInput(addon.tab.scratchMessage('gui.gui.cloudVariableOption'), 'checkbox');
+    const cloudSection = document.createElement("div");
+    cloudSection.className = addon.tab.scratchClass("prompt_cloud-option");
+    const cloudCheckbox = createLabeledInput(addon.tab.scratchMessage("gui.gui.cloudVariableOption"), "checkbox");
     cloudCheckbox.input.checked = variable.isCloud;
     cloudSection.appendChild(cloudCheckbox.outer);
     const updateDisabledInputs = () => {
@@ -281,7 +299,7 @@ export default async function ({ addon, msg, console }) {
         forAllSprites.input.click();
       }
     };
-    cloudCheckbox.input.addEventListener('change', updateDisabledInputs);
+    cloudCheckbox.input.addEventListener("change", updateDisabledInputs);
     updateDisabledInputs();
 
     let isAnythingConfigurable = false;
@@ -291,21 +309,23 @@ export default async function ({ addon, msg, console }) {
       isAnythingConfigurable = true;
       root.appendChild(scopeSection);
     }
-    if (variable.type === '') {
+    if (variable.type === "") {
       isAnythingConfigurable = true;
       root.appendChild(cloudSection);
     }
     if (isAnythingConfigurable) {
-      root.prepend(Object.assign(document.createElement('div'), {
-        textContent: msg('edit'),
-        className: 'sa-swap-local-global-hint'
-      }));
+      root.prepend(
+        Object.assign(document.createElement("div"), {
+          textContent: msg("edit"),
+          className: "sa-swap-local-global-hint",
+        })
+      );
     }
     promptBody.insertBefore(root, promptBody.lastChild);
 
     return {
       isLocal: () => forThisSpriteOnly.input.checked,
-      isCloud: () => cloudCheckbox.input.checked
+      isCloud: () => cloudCheckbox.input.checked,
     };
   };
 
@@ -324,28 +344,31 @@ export default async function ({ addon, msg, console }) {
     return ret;
   };
 
-  addon.tab.createBlockContextMenu((items, block) => {
-    if (!addon.self.disabled && (block.getCategory() === "data" || block.getCategory() === "data-lists")) {
-      const variable = block.workspace.getVariableById(block.getVars()[0]);
-      if (variable) {
-        if (items.length > 0) {
-          if (items[0].text === ScratchBlocks.ScratchMsgs.translate('RENAME_VARIABLE')) {
-            items[0].text = msg('edit-variable-option');
-          } else if (items[0].text === ScratchBlocks.ScratchMsgs.translate('RENAME_LIST')) {
-            items[0].text = msg('edit-list-option');
+  addon.tab.createBlockContextMenu(
+    (items, block) => {
+      if (!addon.self.disabled && (block.getCategory() === "data" || block.getCategory() === "data-lists")) {
+        const variable = block.workspace.getVariableById(block.getVars()[0]);
+        if (variable) {
+          if (items.length > 0) {
+            if (items[0].text === ScratchBlocks.ScratchMsgs.translate("RENAME_VARIABLE")) {
+              items[0].text = msg("edit-variable-option");
+            } else if (items[0].text === ScratchBlocks.ScratchMsgs.translate("RENAME_LIST")) {
+              items[0].text = msg("edit-list-option");
+            }
           }
+          items.push({
+            enabled: true,
+            separator: true,
+            text: msg(`to-${variable.isLocal ? "global" : "local"}`),
+            callback: () => convertVariable(variable, !variable.isLocal, variable.isCloud),
+          });
         }
-        items.push({
-          enabled: true,
-          separator: true,
-          text: msg(`to-${variable.isLocal ? 'global' : 'local'}`),
-          callback: () => convertVariable(variable, !variable.isLocal, variable.isCloud)
-        });
       }
+      return items;
+    },
+    {
+      flyout: true,
+      blocks: true,
     }
-    return items;
-  }, {
-    flyout: true,
-    blocks: true
-  });
+  );
 }
