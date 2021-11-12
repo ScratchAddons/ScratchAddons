9a3311b7af0220b5fd85c9d20e5be96f6a0dc535
Thomas Weber <weber_t@mailbox.org>
2021-11-12 15:51:45 -0600

    style

--- addons/editor-comment-previews/userscript.js
+++ addons/editor-comment-previews/userscript.js
@@ -2,20 +2,20 @@ export default async function ({ addon, global, console }) {
   const vm = addon.tab.traps.vm;
 
   const updateStyles = () => {
-    previewInner.classList.toggle('sa-comment-preview-delay', addon.settings.get('delay') !== 'none');
-    previewInner.classList.toggle('sa-comment-preview-reduce-transparency', addon.settings.get('reduce-transparency'));
-    previewInner.classList.toggle('sa-comment-preview-fade', !addon.settings.get('reduce-animation'));
+    previewInner.classList.toggle("sa-comment-preview-delay", addon.settings.get("delay") !== "none");
+    previewInner.classList.toggle("sa-comment-preview-reduce-transparency", addon.settings.get("reduce-transparency"));
+    previewInner.classList.toggle("sa-comment-preview-fade", !addon.settings.get("reduce-animation"));
   };
 
   const afterDelay = (cb) => {
-    if (!previewInner.classList.contains('sa-comment-preview-hidden')) {
+    if (!previewInner.classList.contains("sa-comment-preview-hidden")) {
       // If not hidden, updating immediately is preferred
       cb();
       return;
     }
     const delay = addon.settings.get("delay");
-    if (delay === 'long') return setTimeout(cb, 500);
-    if (delay === 'short') return setTimeout(cb, 300);
+    if (delay === "long") return setTimeout(cb, 500);
+    if (delay === "short") return setTimeout(cb, 300);
     cb();
   };
 
@@ -25,42 +25,38 @@ export default async function ({ addon, global, console }) {
   let mouseY = 0;
   let doNotShowUntilMoveMouse = false;
 
-  const previewOuter = document.createElement('div');
-  previewOuter.classList.add('sa-comment-preview-outer');
-
+  const previewOuter = document.createElement("div");
+  previewOuter.classList.add("sa-comment-preview-outer");
   const previewInner = document.createElement("div");
   previewInner.classList.add("sa-comment-preview-inner");
   previewInner.classList.add("sa-comment-preview-hidden");
   updateStyles();
   addon.settings.addEventListener("change", updateStyles);
-
   previewOuter.appendChild(previewInner);
   document.body.appendChild(previewOuter);
 
   const getBlock = (id) => vm.editingTarget.blocks.getBlock(id) || vm.runtime.flyoutBlocks.getBlock(id);
   const getComment = (block) => block && block.comment && vm.editingTarget.comments[block.comment];
   const getProcedureDefinitionBlock = (procCode) => {
-    const procedurePrototype = Object.values(vm.editingTarget.blocks._blocks).find((i) => (
-      i.opcode === "procedures_prototype" && i.mutation.proccode === procCode
-    ));
+    const procedurePrototype = Object.values(vm.editingTarget.blocks._blocks).find(
+      (i) => i.opcode === "procedures_prototype" && i.mutation.proccode === procCode
+    );
     if (procedurePrototype) {
       // Usually `parent` will exist but sometimes it doesn't
       if (procedurePrototype.parent) {
         return getBlock(procedurePrototype.parent);
       }
       const id = procedurePrototype.id;
-      return Object.values(vm.editingTarget.blocks._blocks).find((i) => (
-        i.opcode === 'procedures_definition' &&
-        i.inputs.custom_block &&
-        i.inputs.custom_block.block === id
-      ));
+      return Object.values(vm.editingTarget.blocks._blocks).find(
+        (i) => i.opcode === "procedures_definition" && i.inputs.custom_block && i.inputs.custom_block.block === id
+      );
     }
     return null;
   };
 
   const setText = (text) => {
     previewInner.innerText = text;
-    previewInner.classList.remove('sa-comment-preview-hidden');
+    previewInner.classList.remove("sa-comment-preview-hidden");
     updateMousePosition();
   };
 
@@ -71,7 +67,7 @@ export default async function ({ addon, global, console }) {
   const hidePreview = () => {
     if (hoveredElement) {
       hoveredElement = null;
-      previewInner.classList.add('sa-comment-preview-hidden');
+      previewInner.classList.add("sa-comment-preview-hidden");
     }
   };
 
@@ -79,13 +75,12 @@ export default async function ({ addon, global, console }) {
     if (addon.self.disabled) {
       return;
     }
-
     clearTimeout(showTimeout);
     if (doNotShowUntilMoveMouse) {
       return;
     }
 
-    const el = e.target.closest('.blocklyBubbleCanvas g, .blocklyBlockCanvas .blocklyDraggable[data-id]');
+    const el = e.target.closest(".blocklyBubbleCanvas g, .blocklyBlockCanvas .blocklyDraggable[data-id]");
     if (el === hoveredElement) {
       // Nothing to do.
       return;
@@ -96,19 +91,19 @@ export default async function ({ addon, global, console }) {
     }
 
     let text = null;
-    if (addon.settings.get("hover-view") && e.target.closest('.blocklyBubbleCanvas g')) {
-      const collapsedText = el.querySelector('text.scratchCommentText');
-      if (collapsedText.getAttribute('display') !== 'none') {
-        const textarea = el.querySelector('textarea');
+    if (addon.settings.get("hover-view") && e.target.closest(".blocklyBubbleCanvas g")) {
+      const collapsedText = el.querySelector("text.scratchCommentText");
+      if (collapsedText.getAttribute("display") !== "none") {
+        const textarea = el.querySelector("textarea");
         text = textarea.value;
       }
-    } else if (e.target.closest('.blocklyBlockCanvas .blocklyDraggable[data-id]')) {
+    } else if (e.target.closest(".blocklyBlockCanvas .blocklyDraggable[data-id]")) {
       const id = el.dataset.id;
       const block = getBlock(id);
       const comment = getComment(block);
       if (addon.settings.get("hover-view-block") && comment) {
         text = comment.text;
-      } else if (block && block.opcode === 'procedures_call' && addon.settings.get("hover-view-procedure")) {
+      } else if (block && block.opcode === "procedures_call" && addon.settings.get("hover-view-procedure")) {
         const procCode = block.mutation.proccode;
         const procedureDefinitionBlock = getProcedureDefinitionBlock(procCode);
         const procedureComment = getComment(procedureDefinitionBlock);
@@ -118,7 +113,7 @@ export default async function ({ addon, global, console }) {
       }
     }
 
-    if (text !== null && text.trim() !== '') {
+    if (text !== null && text.trim() !== "") {
       showTimeout = afterDelay(() => {
         hoveredElement = el;
         setText(text);
@@ -132,15 +127,19 @@ export default async function ({ addon, global, console }) {
     mouseX = e.clientX;
     mouseY = e.clientY;
     doNotShowUntilMoveMouse = false;
-    if (addon.settings.get("follow-mouse") && !previewInner.classList.contains('sa-comment-preview-hidden')) {
+    if (addon.settings.get("follow-mouse") && !previewInner.classList.contains("sa-comment-preview-hidden")) {
       updateMousePosition();
     }
   });
 
-  document.addEventListener("mousedown", () => {
-    hidePreview();
-    doNotShowUntilMoveMouse = true;
-  }, {
-    capture: true
-  });
+  document.addEventListener(
+    "mousedown",
+    () => {
+      hidePreview();
+      doNotShowUntilMoveMouse = true;
+    },
+    {
+      capture: true,
+    }
+  );
 }
