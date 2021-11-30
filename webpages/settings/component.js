4fdd344ea948e9fe2a6dd2c60f33686a59a68e31
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-11-29 22:44:52 -0500

    fuse from master

--- webpages/settings/component.js
+++ webpages/settings/component.js
@@ -129,6 +129,17 @@ export default {
       }
 
       if (!fuse) return [];
+      const addonListObjs = Object.values(
+        this.addonListObjs.reduce((acc, cur) => {
+          if (
+            !acc[cur.manifest._addonId] ||
+            (acc[cur.manifest._addonId] && cur.group.id !== "featuredNew" && cur.group.id !== "new")
+          ) {
+            acc[cur.manifest._addonId] = cur;
+          }
+          return acc;
+        }, Object.create(null))
+      );
       const fuseSearch = fuse.search(this.searchInput).sort((a, b) => {
         // Sort very good matches at the top no matter what
         if ((a.score < 0.1) ^ (b.score < 0.1)) return a.score < 0.1 ? -1 : 1;
@@ -136,10 +147,10 @@ export default {
         else return b.item._enabled - a.item._enabled;
       });
       const results = fuseSearch.map((result) =>
-        this.addonListObjs.find((obj) => obj.manifest._addonId === result.item._addonId)
+        addonListObjs.find((obj) => obj.manifest._addonId === result.item._addonId)
       );
-      for (const obj of this.addonListObjs) obj.matchesSearch = results.includes(obj);
-      return this.addonListObjs.sort((b, a) => results.indexOf(b) - results.indexOf(a));
+      for (const obj of addonListObjs) obj.matchesSearch = results.includes(obj);
+      return addonListObjs.sort((b, a) => results.indexOf(b) - results.indexOf(a));
     },
     addonAmt() {
       return `${Math.floor(this.manifests.length / 5) * 5}+`;
