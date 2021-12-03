2551dc6cae6d1652e934de3e5f0bac9b0a62a56b
Thomas Weber <weber_t@mailbox.org>
2021-12-03 16:48:51 -0600

    Rasterize vector onion layers
    Improves performance

--- addons/onion-skinning/userscript.js
+++ addons/onion-skinning/userscript.js
@@ -320,7 +320,29 @@ export default async function ({ addon, global, console, msg }) {
             root.translate(paperCenter.subtract(root.bounds.width, root.bounds.height));
           }
 
-          layer.addChild(root);
+          const MAX_SIZE = 4096;
+          const bounds = root.strokeBounds;
+          let resolution = 5;
+          if (bounds.width * resolution > MAX_SIZE) {
+            resolution = MAX_SIZE / bounds.width;
+          }
+          if (bounds.height * resolution > MAX_SIZE) {
+            resolution = MAX_SIZE / bounds.height;
+          }
+
+          const raster = root.rasterize(
+            // resolution
+            72 * resolution,
+            // insert
+            false,
+            // bound rect
+            bounds
+          );
+          raster.parent = layer;
+          raster.guide = true;
+          raster.locked = true;
+          raster.position = root.position;
+
           resolve();
         },
       });
