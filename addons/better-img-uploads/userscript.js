ecb1f44afc19eff5b1772d464507d11df701447a
CST1229 <CST1229@users.noreply.github.com>
2021-12-11 18:37:02 +0000

    Format code

--- addons/better-img-uploads/userscript.js
+++ addons/better-img-uploads/userscript.js
@@ -115,7 +115,7 @@ export default async function ({ addon, console, msg }) {
       });
 
       let dim = { width: i.width, height: i.height };
-	  const originalDim = JSON.parse(JSON.stringify(dim));
+      const originalDim = JSON.parse(JSON.stringify(dim));
 
       if (mode === "fit") {
         //Make sure the image fits completely in the stage
@@ -177,7 +177,9 @@ export default async function ({ addon, console, msg }) {
       processed.push(
         new File( //Create the svg file
           [
-            `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewbox="0,0,${dim.width},${dim.height}" width="${dim.width}" height="${dim.height}">
+            `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewbox="0,0,${
+              dim.width
+            },${dim.height}" width="${dim.width}" height="${dim.height}">
         <g>
           <g
               data-paper-data='{"isPaintingLayer":true}'
