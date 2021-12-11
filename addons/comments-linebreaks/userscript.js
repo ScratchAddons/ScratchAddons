4f3fe9a1a6cd630be7773c7021ce4a5cb6e674f5
Samq64 <Samq64@users.noreply.github.com>
2021-12-11 14:06:48 +0000

    Format code

--- addons/comments-linebreaks/userscript.js
+++ addons/comments-linebreaks/userscript.js
@@ -5,7 +5,7 @@ export default async function ({ addon, global, console }) {
     const comment = await addon.tab.waitForElement(".comment .content", { markAsSeen: true });
     comment.style.whiteSpace = "break-spaces";
     if (addon.settings.get("scrollbars")) {
-      comment.style.maxHeight = (addon.settings.get("height")*20)+"px";
+      comment.style.maxHeight = addon.settings.get("height") * 20 + "px";
       comment.style.overflow = "auto";
     }
     formatProfileComments(comment);
