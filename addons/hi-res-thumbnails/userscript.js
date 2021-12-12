4c3381185160b3587ce60ba3585b355cff7a8cf5
(quasar) nebula <qznebula@protonmail.com>
2021-12-12 00:23:12 -0400

    hi res thumbnails: downscaling & special cases

--- addons/hi-res-thumbnails/userscript.js
+++ addons/hi-res-thumbnails/userscript.js
@@ -20,7 +20,7 @@ export default async function ({ addon, console }) {
       continue main;
     }
 
-    [width, height] = fixLowRes(width, height, image);
+    [width, height] = resizeToScreenRes(width, height, image);
     [width, height] = scaleDimensions(width, height);
 
     if (cdn2) {
@@ -36,24 +36,45 @@ export default async function ({ addon, console }) {
     }
   }
 
-  function fixLowRes(width, height, image) {
+  function resizeToScreenRes(width, height, image) {
     // Note this returns a value in CSS pixel units, i.e. it doesn't take into
     // account browser scaling or high-DPI screens. That's good, because we
     // want to leave the actual resolution-increase factor completely up to
     // the addon user/settings!
     const rect = image.getBoundingClientRect();
 
-    // Use greater of both scales to choose the dimension which needs the most
-    // scaling and to maintain the ratio between the rendered and loaded
-    // dimensions.
-    const scale = Math.max(rect.width / width, rect.height / height);
+    // It's possible that the Scratch site is deliberately loading an image at
+    // a greater resolution than it displays at - because with dynamic layout,
+    // if the screen (window) resizes, the image may later be displayed larger.
+    //
+    // If there's an arbitrarily determined "maximum" size for this image, we
+    // should use that as the target dimensions instead. (Unless it's
+    // incidentally smaller, in which case, fall back to the greater [rendered]
+    // dimensions.)
 
-    if (scale > 1) {
-      width *= scale;
-      height *= scale;
+    let widthMax, heightMax;
+
+    if (image.classList.contains('studio-project-image')) {
+      widthMax = 347;
+      heightMax = 260;
     }
 
-    return [width, height];
+    // Use greater of scales across both axes to choose the dimension which
+    // needs the most scaling and to maintain the ratio between the rendered and
+    // loaded dimensions.
+    let scale = Math.max(rect.width / width, rect.height / height);
+
+    // Calculate scale for "maximum" dimensions, and use it instead - but only
+    // if it's greater than the actual scale. Theoretically, max possible
+    // always be >= actual, but if Scratch CSS changes and this addon is
+    // outdated, it's better to prefer the actual displayed dimensions as the
+    // basis we apply the resolution multiplier to later.
+    if (widthMax) {
+      const scaleMax = Math.max(widthMax / width, heightMax / height);
+      scale = Math.max(scale, scaleMax);
+    }
+
+    return [width * scale, height * scale];
   }
 
   function scaleDimensions(width, height) {
