8279374849986b58e9260c44fd7fe12583b197bb
easrng <easrng@users.noreply.github.com>
2021-12-23 01:04:51 +0000

    Format code

--- addons/expose-vm/userscript.js
+++ addons/expose-vm/userscript.js
@@ -1,5 +1,9 @@
 export default async function ({ addon }) {
   window.vm = addon.tab.traps.vm;
-  addon.self.addEventListener("disabled", () => {delete window.vm});
-  addon.self.addEventListener("reenabled", () => {window.vm = addon.tab.traps.vm});
+  addon.self.addEventListener("disabled", () => {
+    delete window.vm;
+  });
+  addon.self.addEventListener("reenabled", () => {
+    window.vm = addon.tab.traps.vm;
+  });
 }
