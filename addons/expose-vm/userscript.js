91541ac78dff570a2b97b01598036e0a4c2d64a5
easrng <easrng@gmail.com>
2021-12-23 01:04:16 +0000

    expose vm addon

---
+++ addons/expose-vm/userscript.js
@@ -0,0 +1,5 @@
+export default async function ({ addon }) {
+  window.vm = addon.tab.traps.vm;
+  addon.self.addEventListener("disabled", () => {delete window.vm});
+  addon.self.addEventListener("reenabled", () => {window.vm = addon.tab.traps.vm});
+}
