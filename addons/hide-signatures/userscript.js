c3b5721c47d96e898f0299974624e57a38f4c9e5
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-12-29 14:25:14 +0000

    Format code

--- addons/hide-signatures/userscript.js
+++ addons/hide-signatures/userscript.js
@@ -1,38 +1,42 @@
+export default async function ({ addon, global, console, msg }) {
+  // get signatures
+  const posts = document.querySelectorAll(".blockpost");
 
-export default async function ({ addon, global, console, msg }) {    
-    // get signatures
-    const posts = document.querySelectorAll(".blockpost")
-    
-    for (const post of posts) {
-        const actionsWrapper = post.querySelector(".postfootright")
-        const signature = post.querySelector(".postsignature")
+  for (const post of posts) {
+    const actionsWrapper = post.querySelector(".postfootright");
+    const signature = post.querySelector(".postsignature");
 
-        const hideBtnWrapper = document.createElement("li")
-        const hideBtn = document.createElement("a")
+    const hideBtnWrapper = document.createElement("li");
+    const hideBtn = document.createElement("a");
 
-        hideBtn.href = "#hide"
-        hideBtn.innerText = msg("hide")
-        
-        let show = !addon.settings.get("hide")
-        signature.style.display = show ? "block" : "none"
-        hideBtn.innerText = msg(show ? "hide" : "show")
+    hideBtn.href = "#hide";
+    hideBtn.innerText = msg("hide");
 
-        hideBtn.addEventListener("click", e => {
-            e.preventDefault()
-            e.stopPropagation()
-            show = !show
-            signature.style.display = show ? "block" : "none"
-            hideBtn.innerText = msg(show ? "hide" : "show")
-        })
+    let show = !addon.settings.get("hide");
+    signature.style.display = show ? "block" : "none";
+    hideBtn.innerText = msg(show ? "hide" : "show");
 
-        addon.self.addEventListener("disabled", () => {
-            show = true;
-            signature.style.display = "block"
-        })
+    hideBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      show = !show;
+      signature.style.display = show ? "block" : "none";
+      hideBtn.innerText = msg(show ? "hide" : "show");
+    });
 
-        hideBtnWrapper.appendChild(hideBtn)
+    addon.self.addEventListener("disabled", () => {
+      show = true;
+      signature.style.display = "block";
+    });
 
-        addon.tab.appendToSharedSpace({ space: "forumsBeforePostReport", element: hideBtnWrapper, scope: actionsWrapper, order: 9 })
-        addon.tab.displayNoneWhileDisabled(hideBtnWrapper)
-    }
+    hideBtnWrapper.appendChild(hideBtn);
+
+    addon.tab.appendToSharedSpace({
+      space: "forumsBeforePostReport",
+      element: hideBtnWrapper,
+      scope: actionsWrapper,
+      order: 9,
+    });
+    addon.tab.displayNoneWhileDisabled(hideBtnWrapper);
+  }
 }
