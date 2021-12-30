fbf25004facaa105ddd4453da23111f1623d21d7
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2021-12-30 14:03:58 -0800

    Run prettier
    I noticed that most people don't actually use it, but I decided to use it, since there's a config file already.

--- addons/faster-site-messages-update/userscript.js
+++ addons/faster-site-messages-update/userscript.js
@@ -1,30 +1,31 @@
 export default async function ({ addon, console, setTimeout, setInterval, clearTimeout, clearInterval }) {
-    if (addon.tab.clientVersion === null) return;
-    if (window.location.href.includes("/projects") && window.location.href.includes("/editor")) return;
-    let msgCount;
-    // console.log("Hi from the addon!")
-    const setBadge = () => {
-      if (msgCount === null && addon.settings.get("showOffline")) {
-          // Do nothing
+  if (addon.tab.clientVersion === null) return;
+  if (window.location.href.includes("/projects") && window.location.href.includes("/editor")) return;
+  let msgCount;
+  // console.log("Hi from the addon!")
+  const setBadge = () => {
+    if (msgCount === null && addon.settings.get("showOffline")) {
+      // Do nothing
+    } else {
+      var targetElement;
+      if (addon.tab.clientVersion === "scratch-www") {
+        targetElement = document.querySelector(
+          "#navigation > div > ul > li.link.right.messages > a > span.message-count.show"
+        );
       } else {
-        var targetElement;
-        if (addon.tab.clientVersion === "scratch-www") {
-          targetElement=document.querySelector("#navigation > div > ul > li.link.right.messages > a > span.message-count.show");
-        }
-        else {
-          targetElement=document.querySelector("#topnav > div > div > ul.account-nav.logged-in > li.messages > a > span");
-        }
-        // console.log(document)
-        targetElement.innerText=msgCount;
-
+        targetElement = document.querySelector(
+          "#topnav > div > div > ul.account-nav.logged-in > li.messages > a > span"
+        );
       }
-    };
-    const getMsgCountAndSetBadge = async () => {
-      msgCount = await addon.account.getMsgCount();
-      setBadge();
-    };
-  
-    getMsgCountAndSetBadge();
-    window.setInterval(getMsgCountAndSetBadge, 2500);
-  }
-  
+      // console.log(document)
+      targetElement.innerText = msgCount;
+    }
+  };
+  const getMsgCountAndSetBadge = async () => {
+    msgCount = await addon.account.getMsgCount();
+    setBadge();
+  };
+
+  getMsgCountAndSetBadge();
+  window.setInterval(getMsgCountAndSetBadge, 2500);
+}
