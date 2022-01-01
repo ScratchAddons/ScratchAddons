0ab05fccbc8a0a14c2de15fcf2c751c90658e841
GarboMuffin <GarboMuffin@users.noreply.github.com>
2022-01-01 04:33:38 +0000

    Format code

--- addons/search-sprites/userscript.js
+++ addons/search-sprites/userscript.js
@@ -3,19 +3,19 @@ export default async function ({ addon, global, console, msg }) {
   let spriteSelectorContainer;
 
   const container = document.createElement("div");
-  container.className = 'sa-search-sprites-container';
+  container.className = "sa-search-sprites-container";
   addon.tab.displayNoneWhileDisabled(container, {
-    display: 'flex'
+    display: "flex",
   });
 
   const searchBox = document.createElement("input");
-  searchBox.className = 'sa-search-sprites-box';
-  searchBox.placeholder = msg('placeholder');
-  searchBox.autocomplete = 'off';
+  searchBox.className = "sa-search-sprites-box";
+  searchBox.placeholder = msg("placeholder");
+  searchBox.autocomplete = "off";
   // search might make more sense, but browsers treat them special in ways that this addon does not handle,
   // so just leave it as a text input. Also note that Scratch uses type=text for its own search inputs in
   // the libraries, so this fits right in.
-  searchBox.type = 'text';
+  searchBox.type = "text";
 
   const search = (query) => {
     if (!spritesContainer) return;
@@ -24,14 +24,11 @@ export default async function ({ addon, global, console, msg }) {
     const containsQuery = (str) => str.toLowerCase().includes(query);
 
     for (const sprite of spritesContainer.children) {
-      const visible = (
+      const visible =
         !query ||
         containsQuery(sprite.children[0].children[1].innerText) ||
-        (
-          containsQuery(sprite.children[0].children[2].children[0].innerText) &&
-          sprite.children[0].classList.contains("sa-folders-folder")
-        )
-      );
+        (containsQuery(sprite.children[0].children[2].children[0].innerText) &&
+          sprite.children[0].classList.contains("sa-folders-folder"));
       sprite.style.display = visible ? "" : "none";
     }
   };
@@ -41,14 +38,14 @@ export default async function ({ addon, global, console, msg }) {
   });
 
   const reset = () => {
-    search('');
-    searchBox.value = '';
+    search("");
+    searchBox.value = "";
   };
 
   const resetButton = document.createElement("button");
   resetButton.className = "sa-search-sprites-reset";
   resetButton.addEventListener("click", reset);
-  resetButton.textContent = '×';
+  resetButton.textContent = "×";
   addon.self.addEventListener("disabled", reset);
 
   container.appendChild(searchBox);
