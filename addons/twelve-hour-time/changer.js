9056bf8999cdfe60a80136245a8b0a31c2c008b7
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-09 13:24:50 +1300

    basic function

--- addons/twelve-hour-time/changer.js
+++ addons/twelve-hour-time/changer.js
@@ -1,3 +1,23 @@
 export default async function ({ addon, global, console }) {
-  console.log(`HELLO WORLD!`);
+  const boxHeads = document.querySelectorAll(".box-head");
+  const formatter = new Intl.DateTimeFormat('default',
+  {
+      hour12: true,
+      timeStyle: "medium",
+      dateStyle: "long"
+  });
+  boxHeads.forEach((el) => {
+    if(!el.getElementsByTagName("a")[0]) return;
+    const text = el.getElementsByTagName("a")[0].innerText;
+    const dateParse = text.replace(/[.]/g, '');
+    let dateCheck;
+    if (dateParse.startsWith("Yesterday")) {
+      // I need help here
+    } else {
+      dateCheck = new Date(dateParse);
+    }
+    console.warn(text, dateParse)
+    el.getElementsByTagName("a")[0].innerText = formatter.format(dateCheck)
+    console.log(formatter.format(dateCheck))
+  })
 }
