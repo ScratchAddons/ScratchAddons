2394645470dcc68b2ff33d3a74ee3d283e95e084
dhuls <dillhound@outlook.com>
2022-02-24 08:48:59 -0600

    add cmd key support (#4320)
    can't test since I don't have a Mac, see forums.js in ctrl-enter-post

--- addons/mute-project/userscript.js
+++ addons/mute-project/userscript.js
@@ -6,7 +6,7 @@ export default async function ({ addon, global, console }) {
   icon.loading = "lazy";
   icon.style.display = "none";
   const toggleMute = (e) => {
-    if (e.ctrlKey) {
+    if (e.ctrlKey || e.metaKey) {
       e.cancelBubble = true;
       e.preventDefault();
       muted = !muted;
