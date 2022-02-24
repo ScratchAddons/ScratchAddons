6804b48aedbdefb3dce03b203c9c59481cc333d7
TheColaber <72760579+TheColaber@users.noreply.github.com>
2022-02-24 17:14:33 -0500

    Fix bugs

--- addons/sounds-duration/userscript.js
+++ addons/sounds-duration/userscript.js
@@ -1,38 +1,43 @@
 export default async function ({ addon, msg, global, console }) {
   while (true) {
-    let container = await addon.tab.waitForElement("[class^=sound-editor_editor-container]", { markAsSeen: true });
-    let el = container.querySelector("[class^=sound-editor_row]").appendChild(document.createElement("div"));
-    let state = container[addon.tab.traps.getInternalKey(container)].return.return.return.stateNode;
+    const container = await addon.tab.waitForElement("[class^=sound-editor_editor-container]", { markAsSeen: true });
+    const el = container.querySelector("[class^=sound-editor_row]").appendChild(document.createElement("div"));
+    const state = container[addon.tab.traps.getInternalKey(container)].return.return.return.stateNode;
+
+    const norm = (num) => (Math.round(num * 100) / 100).toFixed(2);
 
     el.textContent =
-      (0).toFixed(2) +
-      "/" +
-      (state.audioBufferPlayer.buffer.length / state.audioBufferPlayer.buffer.sampleRate).toFixed(2);
+      norm(0) + "/" + norm(state.audioBufferPlayer.buffer.length / state.audioBufferPlayer.buffer.sampleRate);
 
-    let _handleUpdatePlayhead = state.handleUpdatePlayhead;
+    const _handleUpdatePlayhead = state.handleUpdatePlayhead;
     state.handleUpdatePlayhead = function (playhead) {
       _handleUpdatePlayhead.call(this, playhead);
       const timeSinceStart = (Date.now() - this.startTime) / 1000;
       const trimStartTime = this.buffer.duration * this.trimStart;
       const trimmedDuration = this.buffer.duration * this.trimEnd - trimStartTime;
-      el.textContent = timeSinceStart.toFixed(2) + "/" + trimmedDuration.toFixed(2);
+      el.textContent = norm(timeSinceStart) + "/" + norm(trimmedDuration);
     };
 
-    let _componentWillReceiveProps = state.componentWillReceiveProps;
+    const _handleStoppedPlaying = state.handleStoppedPlaying;
+    state.handleStoppedPlaying = function () {
+      _handleStoppedPlaying.call(this);
+      el.textContent =
+        norm(0) + "/" + norm(state.audioBufferPlayer.buffer.length / state.audioBufferPlayer.buffer.sampleRate);
+    };
+
+    const _componentWillReceiveProps = state.componentWillReceiveProps;
     state.componentWillReceiveProps = function (newProps) {
       _componentWillReceiveProps.call(this, newProps);
       el.textContent =
-        (0).toFixed(2) +
-        "/" +
-        (this.audioBufferPlayer.buffer.length / this.audioBufferPlayer.buffer.sampleRate).toFixed(2);
+        norm(0) + "/" + norm(state.audioBufferPlayer.buffer.length / state.audioBufferPlayer.buffer.sampleRate);
     };
 
-    let _handleUpdateTrim = state.handleUpdateTrim;
+    const _handleUpdateTrim = state.handleUpdateTrim;
     state.handleUpdateTrim = function (trimStart, trimEnd) {
       _handleUpdateTrim.call(this, trimStart, trimEnd);
       const trimStartTime = state.audioBufferPlayer.buffer.duration * trimStart;
       const trimmedDuration = state.audioBufferPlayer.buffer.duration * trimEnd - trimStartTime;
-      el.textContent = (0).toFixed(2) + "/" + trimmedDuration.toFixed(2);
+      el.textContent = norm(0) + "/" + norm(trimmedDuration);
     };
   }
 }
