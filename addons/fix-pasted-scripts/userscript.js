b5bf7269ae9dd2bf1de61922880c15b15afb683d
GarboMuffin <GarboMuffin@users.noreply.github.com>
2022-02-26 03:46:48 +0000

    Format code

--- addons/fix-pasted-scripts/userscript.js
+++ addons/fix-pasted-scripts/userscript.js
@@ -2,7 +2,7 @@ export default async function ({ addon, global, console }) {
   const ScratchBlocks = await addon.tab.traps.getBlockly();
   const originalGetGesture = ScratchBlocks.WorkspaceSvg.prototype.getGesture;
   ScratchBlocks.WorkspaceSvg.prototype.getGesture = function (e) {
-    if (!addon.self.disabled && e.type === 'mousedown' && this.isDragging()) {
+    if (!addon.self.disabled && e.type === "mousedown" && this.isDragging()) {
       return null;
     }
     return originalGetGesture.call(this, e);
