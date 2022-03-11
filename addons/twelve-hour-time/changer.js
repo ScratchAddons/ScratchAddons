2a253990649d4518af782f59483ffce96d26eb37
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-03-11 22:16:44 +1300

    Add option for relative dates
    Not sure if they're called relative dates.

--- addons/twelve-hour-time/changer.js
+++ addons/twelve-hour-time/changer.js
@@ -6,46 +6,58 @@ export default async function ({ addon, global, console }) {
     const dateCheck = handleFormatting(text);
     el.getElementsByTagName("a")[0].innerText = dateCheck;
   });
-}
 
-// this part was all written by @towerofnix on github
-// thank you so much!!!!!!!!!!!!
+  // this part was mostly written by @towerofnix on github!
 
-function handleFormatting(text) {
-  const formatter = new Intl.DateTimeFormat("default", {
-    hour12: true,
-    timeStyle: "medium",
-    dateStyle: "long",
-  });
+  function handleFormatting(text) {
+    const formatter = new Intl.DateTimeFormat("default", {
+      hour12: true,
+      timeStyle: "medium",
+      dateStyle: "long",
+    });
 
-  const timeParts = text.replace(/\./g, "").match(/^(.*?)\s+([0-9][0-9]):([0-9][0-9]):([0-9][0-9])$/);
-  if (!timeParts) {
-    console.warn(`Failed to parse forum timestamp: ${text}`);
-    return;
+    const timeParts = text.replace(/\./g, "").match(/^(.*?)\s+([0-9][0-9]):([0-9][0-9]):([0-9][0-9])$/);
+    if (!timeParts) {
+      console.warn(`Failed to parse forum timestamp: ${text}`);
+      return;
+    }
+
+    const [day, hours, minutes, seconds] = timeParts.slice(1);
+
+    let date;
+
+    let isRelative = false;
+
+    switch (day) {
+      case "Today":
+        isRelative = true;
+        date = new Date();
+        break;
+      case "Yesterday":
+        isRelative = true;
+        date = new Date();
+        date.setDate(date.getDate() - 1);
+        break;
+      default:
+        date = new Date(day);
+    }
+
+    date.setHours(hours, minutes, seconds, 0);
+
+    if (isNaN(date.getTime())) {
+      console.warn(`Failed to calculate date/time for forum timestamp: ${text} - regex timeParts:`, timeParts);
+      return;
+    }
+
+    let formattedDate = formatter.format(date);
+    if (isRelative && addon.settings.get("relative-dates") === true) {
+      const resetFormatter = new Intl.DateTimeFormat("default", {
+        hour12: true,
+        dateStyle: "long",
+      });
+      formattedDate = formattedDate.replace(resetFormatter.format(date), day);
+    }
+
+    return formattedDate;
   }
-
-  const [day, hours, minutes, seconds] = timeParts.slice(1);
-
-  let date;
-
-  switch (day) {
-    case "Today":
-      date = new Date();
-      break;
-    case "Yesterday":
-      date = new Date();
-      date.setDate(date.getDate() - 1);
-      break;
-    default:
-      date = new Date(day);
-  }
-
-  date.setHours(hours, minutes, seconds, 0);
-
-  if (isNaN(date.getTime())) {
-    console.warn(`Failed to calculate date/time for forum timestamp: ${text} - regex timeParts:`, timeParts);
-    return;
-  }
-
-  return formatter.format(date);
 }
