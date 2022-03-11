d6dbdb64369323e485d64ba9db8368d2565e0793
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-03-11 19:58:54 +1300

    add working version

--- addons/twelve-hour-time/changer.js
+++ addons/twelve-hour-time/changer.js
@@ -1,23 +1,48 @@
 export default async function ({ addon, global, console }) {
   const boxHeads = document.querySelectorAll(".box-head");
-  const formatter = new Intl.DateTimeFormat('default',
-  {
-      hour12: true,
-      timeStyle: "medium",
-      dateStyle: "long"
-  });
   boxHeads.forEach((el) => {
-    if(!el.getElementsByTagName("a")[0]) return;
+    if (!el.getElementsByTagName("a")[0]) return;
     const text = el.getElementsByTagName("a")[0].innerText;
-    const dateParse = text.replace(/[.]/g, '');
-    let dateCheck;
-    if (dateParse.startsWith("Yesterday")) {
-      // I need help here
-    } else {
-      dateCheck = new Date(dateParse);
-    }
-    console.warn(text, dateParse)
-    el.getElementsByTagName("a")[0].innerText = formatter.format(dateCheck)
-    console.log(formatter.format(dateCheck))
-  })
+    const dateCheck = handleFormatting(text)
+    el.getElementsByTagName("a")[0].innerText = dateCheck;
+  });
+}
+
+function handleFormatting(text) {
+  const formatter = new Intl.DateTimeFormat("default", {
+    hour12: true,
+    timeStyle: "medium",
+    dateStyle: "long",
+  });
+
+  const timeParts = text.replace(/\./g, "").match(/^(.*?)\s+([0-9][0-9]):([0-9][0-9]):([0-9][0-9])$/);
+  if (!timeParts) {
+    console.warn(`Failed to parse forum timestamp: ${text}`);
+    return;
+  }
+
+  const [day, hours, minutes, seconds] = timeParts.slice(1);
+
+  let date;
+
+  switch (day) {
+    case "Today":
+      date = new Date();
+      break;
+    case "Yesterday":
+      date = new Date();
+      date.setDate(date.getDate() - 1);
+      break;
+    default:
+      date = new Date(day);
+  }
+
+  date.setHours(hours, minutes, seconds, 0);
+
+  if (isNaN(date.getTime())) {
+    console.warn(`Failed to calculate date/time for forum timestamp: ${text} - regex timeParts:`, timeParts);
+    return;
+  }
+
+  return formatter.format(date);
 }
