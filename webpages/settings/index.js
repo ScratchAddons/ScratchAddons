c775d0109f14055b736466c59977972e50578a23
World_Languages <mundofinky@gmail.com>
2022-03-12 02:23:35 -0300

    Link to scratchaddons.com/credits instead of /contributors (#4393)

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -171,7 +171,7 @@ let fuse;
           const versionName = chrome.runtime.getManifest().version_name;
           const utm = `utm_source=extension&utm_medium=settingspage&utm_campaign=v${version}`;
           return {
-            contributors: `https://scratchaddons.com/${localeSlash}contributors?${utm}`,
+            contributors: `https://scratchaddons.com/${localeSlash}credits?${utm}`,
             feedback: `https://scratchaddons.com/${localeSlash}feedback/?ext_version=${versionName}&${utm}`,
             changelog: `https://scratchaddons.com/${localeSlash}changelog?${utm}`,
           };
