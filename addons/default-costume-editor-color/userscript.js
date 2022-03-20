bc3a0ff1a6e0141412e79b834799d5f551138645
Thomas Weber <muffin@muffin.ink>
2022-03-19 19:43:12 -0500

    Fix a bunch of persistence bugs

--- addons/default-costume-editor-color/userscript.js
+++ addons/default-costume-editor-color/userscript.js
@@ -8,37 +8,62 @@ export default async function ({ addon, global, console, msg }) {
     return;
   }
 
-  // Removes transparency (which Scratch does not support) and normalizes colors to match how Scratch represents them.
-  const parseColor = (color) => color.substring(0, 7).toUpperCase();
-
   // Special value Scratch uses as color when objects with different colors are selected
   // https://github.com/LLK/scratch-paint/blob/6733e20b56f52d139f9885952a57c7da012a542f/src/helper/style-path.js#L10
   const MIXED = "scratch-paint/style-path/mixed";
 
-  const SCRATCH_DEFAULT_FILL = parseColor("#9966FF");
-  const SCRATCH_DEFAULT_STROKE = parseColor("#000000");
+  const hexComponent = (str) => (+str).toString(16).toUpperCase().padStart(2, '0');
 
-  // color could be a hex color string, MIXED, or null
-  const isValidColor = (color) => typeof color === "string" && color.startsWith("#");
+  const parseColor = (color) => {
+    // Scratch colors must be in hex format and do not support transparency
+    if (typeof color !== 'string' || color === MIXED) {
+      return null;
+    }
+    if (color.startsWith('#')) {
+      return color.substring(0, 7).toUpperCase();
+    }
+    // paper can return rgba() colors but we can't set transparent colors anyways
+    const rgbMatch = color.match(/^rgb\((\d+)\s*,(\d+)\s*,(\d+)\)$/);
+    if (rgbMatch) {
+      const [_, r, g, b] = rgbMatch;
+      return `#${hexComponent(r)}${hexComponent(g)}${hexComponent(b)}`;
+    }
+    console.warn('Could not normalize color', color);
+    return null;
+  };
 
-  // returns hex color string, MIXED, or null
-  const getCurrentFillColor = () => addon.tab.redux.state.scratchPaint.color.fillColor.primary;
-  const getCurrentStrokeColor = () => addon.tab.redux.state.scratchPaint.color.strokeColor.primary;
+  const normalizeColor = (color) => parseColor(color) || '#000000';
 
-  const getCurrentStrokeWidth = () => addon.tab.redux.state.scratchPaint.color.strokeWidth;
+  const SCRATCH_DEFAULT_FILL = normalizeColor("#9966FF");
+  const SCRATCH_DEFAULT_STROKE = normalizeColor("#000000");
+
+  /**
+   * @returns {string|MIXED|null}
+   */
+  const getFillColor = (state=addon.tab.redux.state) => state.scratchPaint.color.fillColor.primary;
+
+  /**
+   * @returns {string|MIXED|null}
+   */
+  const getStrokeColor = (state=addon.tab.redux.state) => state.scratchPaint.color.strokeColor.primary;
+
+  /**
+   * @returns {number}
+   */
+  const getStrokeWidth = (state=addon.tab.redux.state) => state.scratchPaint.color.strokeWidth;
 
   let defaultFillColor;
   let defaultStrokeColor;
   let defaultStrokeWidth;
   const setDefaultColorsToSettings = () => {
-    defaultFillColor = parseColor(addon.settings.get("fill"));
-    defaultStrokeColor = parseColor(addon.settings.get("stroke"));
+    defaultFillColor = normalizeColor(addon.settings.get("fill"));
+    defaultStrokeColor = normalizeColor(addon.settings.get("stroke"));
     defaultStrokeWidth = addon.settings.get("strokeSize");
   };
   setDefaultColorsToSettings();
 
   const applyFillColor = () => {
-    if (defaultFillColor !== getCurrentFillColor()) {
+    if (defaultFillColor !== getFillColor()) {
       addon.tab.redux.dispatch({
         type: "scratch-paint/fill-style/CHANGE_FILL_COLOR",
         color: defaultFillColor,
@@ -46,7 +71,7 @@ export default async function ({ addon, global, console, msg }) {
     }
   };
   const applyStrokeColor = () => {
-    if (defaultStrokeColor !== getCurrentStrokeColor()) {
+    if (defaultStrokeColor !== getStrokeColor()) {
       addon.tab.redux.dispatch({
         type: "scratch-paint/stroke-style/CHANGE_STROKE_COLOR",
         color: defaultStrokeColor,
@@ -54,7 +79,7 @@ export default async function ({ addon, global, console, msg }) {
     }
   };
   const applyStrokeWidth = () => {
-    if (getCurrentStrokeWidth() !== defaultStrokeWidth) {
+    if (getStrokeWidth() !== defaultStrokeWidth) {
       addon.tab.redux.dispatch({
         type: "scratch-paint/stroke-width/CHANGE_STROKE_WIDTH",
         strokeWidth: defaultStrokeWidth,
@@ -83,16 +108,28 @@ export default async function ({ addon, global, console, msg }) {
     const action = detail.action;
 
     if (!activatingTool && addon.settings.get("persistence")) {
-      if (action.type === "scratch-paint/fill-style/CHANGE_FILL_COLOR") {
-        if (isValidColor(action.color)) {
-          defaultFillColor = action.color;
+      const oldFillColor = getFillColor(detail.prev);
+      const newFillColor = getFillColor(detail.next);
+      if (oldFillColor !== newFillColor) {
+        const parsed = parseColor(newFillColor);
+        if (parsed) {
+          defaultFillColor = parsed;
         }
-      } else if (action.type === "scratch-paint/stroke-style/CHANGE_STROKE_COLOR") {
-        if (isValidColor(action.color)) {
-          defaultStrokeColor = action.color;
+      }
+
+      const oldStrokeColor = getStrokeColor(detail.prev);
+      const newStrokeColor = getStrokeColor(detail.next);
+      if (oldStrokeColor !== newStrokeColor) {
+        const parsed = parseColor(newStrokeColor);
+        if (parsed) {
+          defaultStrokeColor = parsed;
         }
-      } else if (action.type === "scratch-paint/stroke-width/CHANGE_STROKE_WIDTH") {
-        defaultStrokeWidth = action.strokeWidth;
+      }
+
+      const oldStrokeWidth = getStrokeWidth(detail.prev);
+      const newStrokeWidth = getStrokeWidth(detail.next);
+      if (oldStrokeWidth !== newStrokeWidth) {
+        defaultStrokeWidth = newStrokeWidth;
       }
     }
 
@@ -102,22 +139,23 @@ export default async function ({ addon, global, console, msg }) {
       // We have to do this weird redux trick because we can't modify these constants:
       // https://github.com/LLK/scratch-paint/blob/6733e20b56f52d139f9885952a57c7da012a542f/src/reducers/fill-style.js#L7
       // https://github.com/LLK/scratch-paint/blob/6733e20b56f52d139f9885952a57c7da012a542f/src/reducers/stroke-style.js#L7
-      activatingTool = true;
-      const initialFillColor = getCurrentFillColor();
-      const initialStrokeColor = getCurrentStrokeColor();
+      const initialFillColor = getFillColor();
+      const initialStrokeColor = getStrokeColor();
       const shouldCheckIfFillChanges = !initialFillColor || initialFillColor === MIXED;
       const shouldCheckIfStrokeChanges = !initialStrokeColor || initialStrokeColor === MIXED;
       if (shouldCheckIfFillChanges || shouldCheckIfStrokeChanges) {
+        activatingTool = true;
         queueMicrotask(() => {
           activatingTool = false;
           if (shouldCheckIfFillChanges) {
-            const finalFillColor = getCurrentFillColor();
+            const finalFillColor = getFillColor();
             if (finalFillColor === SCRATCH_DEFAULT_FILL) {
               applyFillColor();
             }
           }
+
           if (shouldCheckIfStrokeChanges) {
-            const finalStrokeColor = getCurrentStrokeColor();
+            const finalStrokeColor = getStrokeColor();
             if (
               finalStrokeColor === SCRATCH_DEFAULT_STROKE ||
               (initialStrokeColor === MIXED && finalStrokeColor !== MIXED)
