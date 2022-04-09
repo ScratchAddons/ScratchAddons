a50d43da298d44b12844dbea7a68db3d3d018166
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-04-09 09:21:53 -0500

    Update userscript.js

--- addons/turbowarp-redirect/userscript.js
+++ addons/turbowarp-redirect/userscript.js
@@ -2,5 +2,5 @@ export default async function ({ addon, console, msg }) {
   await addon.tab.waitForElement("img.not-available-image", {
     markAsSeen: true,
   });
-  window.location.assign("//turbowarp.org/" + window.location.pathname.split("/")[2]) + "#unshared";
+  window.location.assign(addon.settings.get("redirectUrl") + window.location.pathname.split("/")[2]) + "#unshared";
 }
