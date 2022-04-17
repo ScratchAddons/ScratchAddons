f911cf7d0f5fbda21467b223f9839edc9c348727
BarelySmooth <83728060+BarelySmooth@users.noreply.github.com>
2022-04-17 11:49:35 +0400

    lots of fixes

--- addons/remaining-replies/userscript.js
+++ addons/remaining-replies/userscript.js
@@ -1,18 +1,9 @@
 export default async function ({ addon, msg }) {
-  // function addRemainingReplyCount(comment) {
-
-  // }
-
-  while (true) {
-    // wait for input box
-    const comment = await addon.tab.waitForElement("div.comment", {
-      markAsSeen: true,
-    });
-
+  function addRemainingReplyCount(comment) {
     // skip the main comment input at the top of the page
-    if (comment.classList.contains("compose-row")) continue;
+    if (comment.classList.contains("compose-row")) return;
     // comments are off on the page
-    if (!comment.querySelector(".comment-reply span")) continue;
+    if (!comment.querySelector(".comment-reply span")) return;
 
     let parentComment = comment.parentElement.classList.contains("replies")
       ? comment.parentElement.parentElement.children[0]
@@ -32,6 +23,56 @@ export default async function ({ addon, msg }) {
     )[0];
 
     const remainingReplies = 25 - parentCommentData?.reply_count;
-    comment.querySelector(".comment-reply span").innerText += ` (${remainingReplies} left)`;
+    comment.querySelector(".comment-reply span").innerText = `reply (${remainingReplies} left)`;
   }
+
+  async function waitForComment() {
+    while (true) {
+      // wait for input box
+      const comment = await addon.tab.waitForElement("div.comment", {
+        markAsSeen: true,
+      });
+
+      addRemainingReplyCount(comment);
+    }
+  }
+
+  // async function refreshRemainingReplyCount() {
+  //   document.querySelectorAll(".comment").forEach((comment) => {
+  //     addRemainingReplyCount(comment);
+  //   });
+  // }
+
+  // async function waitForPostButton() {
+  //   while (true) {
+  //     // wait for post button
+  //     const postButton = await addon.tab.waitForElement("button.compose-post", { markAsSeen: true });
+
+  //     postButton.addEventListener("click", async () => {
+  //       console.log("post button clicked");
+  //       const newComment = await addon.tab.waitForElement(".comment-reply", {
+  //         markAsSeen: true,
+  //       });
+
+  //       // alert("whoa there, you created a new comment!");
+  //     });
+  //   }
+  // }
+
+  waitForComment();
+  // waitForPostButton();
+
+  addon.tab.redux.initialize();
+  addon.tab.redux.addEventListener("statechanged", (action, prev, next) => {
+    if (action.detail.action.type === "ADD_NEW_COMMENT") {
+      console.log("parent id", action.detail.action.comment.parent_id);
+      const comment = document.getElementById(`comments-${action.detail.action.comment.parent_id}`);
+      addRemainingReplyCount(comment);
+
+      // parent comment
+      addon.tab.redux.state.comments.replies[action.detail.action.comment.parent_id].forEach((reply) => {
+        addRemainingReplyCount(document.getElementById(`comments-${reply.id}`));
+      });
+    }
+  });
 }
