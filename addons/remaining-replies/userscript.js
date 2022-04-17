d1ea5d93c18fbddf0c20f60b149414fed6f2c0f7
BarelySmooth <83728060+BarelySmooth@users.noreply.github.com>
2022-04-17 11:50:41 +0400

    clean up

--- addons/remaining-replies/userscript.js
+++ addons/remaining-replies/userscript.js
@@ -9,13 +9,6 @@ export default async function ({ addon, msg }) {
       ? comment.parentElement.parentElement.children[0]
       : comment;
 
-    // if (comment.parentElement.classList.contains("replies")) {
-    //   // the current comment is a reply to another comment
-    //   parentComment = comment.parentElement.parentElement.children[0];
-    // } else {
-    //   parentComment = comment;
-    // }
-
     const parentCommentID = parentComment.getAttribute("id");
     const parentCommentData = addon.tab.redux.state.comments.comments.filter(
       // substring in order to remove the "comments-" prefix
@@ -37,30 +30,7 @@ export default async function ({ addon, msg }) {
     }
   }
 
-  // async function refreshRemainingReplyCount() {
-  //   document.querySelectorAll(".comment").forEach((comment) => {
-  //     addRemainingReplyCount(comment);
-  //   });
-  // }
-
-  // async function waitForPostButton() {
-  //   while (true) {
-  //     // wait for post button
-  //     const postButton = await addon.tab.waitForElement("button.compose-post", { markAsSeen: true });
-
-  //     postButton.addEventListener("click", async () => {
-  //       console.log("post button clicked");
-  //       const newComment = await addon.tab.waitForElement(".comment-reply", {
-  //         markAsSeen: true,
-  //       });
-
-  //       // alert("whoa there, you created a new comment!");
-  //     });
-  //   }
-  // }
-
   waitForComment();
-  // waitForPostButton();
 
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", (action, prev, next) => {
@@ -69,7 +39,6 @@ export default async function ({ addon, msg }) {
       const comment = document.getElementById(`comments-${action.detail.action.comment.parent_id}`);
       addRemainingReplyCount(comment);
 
-      // parent comment
       addon.tab.redux.state.comments.replies[action.detail.action.comment.parent_id].forEach((reply) => {
         addRemainingReplyCount(document.getElementById(`comments-${reply.id}`));
       });
