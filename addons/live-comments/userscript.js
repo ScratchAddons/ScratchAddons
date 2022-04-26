43a5075c40f42fce1d455f8216365a13fd6a73c5
Weredime <100447465+Weredime@users.noreply.github.com>
2022-04-26 21:00:55 +0100

    refactor: just refactor the whole thing cos why not

--- addons/live-comments/userscript.js
+++ addons/live-comments/userscript.js
@@ -1,98 +1,103 @@
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export default async function ({ addon, global, console, msg }) {
-  let wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    const { redux } = addon.tab;
+    const [, type, id] = location.pathname.split("/");
 
-  const { redux } = addon.tab;
-  let [, type, id] = location.pathname.split("/");
-
-  if (type == "studios") {
-    await redux.waitForState((state) => redux.state.studio.infoStatus == "FETCHED", {
-      actions: ["SET_INFO"],
-    });
-  } else {
-    await redux.waitForState((state) => state.preview.status.project === "FETCHED", {
-      actions: ["SET_PROJECT_INFO"],
-    });
-  }
-
-  const URL = `https://api.scratch.mit.edu/${
-    type == "projects" ? `users/${redux.state.preview.projectInfo.author.username}/projects` : `studios`
-  }/${id}/comments`;
-
-  let getAllComments = () => [...redux.state.comments.comments];
-  let getAllReplies = () => {
-    let replies = [];
-
-    for (let re of Object.values(redux.state.comments.replies)) {
-      replies.push(...re);
-    }
-
-    return replies;
-  };
-
-  var newest = Date.now();
-
-  while (true) {
-    await wait(10000);
-    var loopOffset = 40;
-    var offset = -loopOffset;
-    while (true) {
-      offset += loopOffset;
-      let res = await fetch(URL + `?offset=${offset}&limit=${loopOffset}`);
-      let data = await res.json();
-
-      let json = data.map((e) => {
-        let time = new Date(e.datetime_created).getTime();
-        if (time > newest) {
-          return e;
-        } else return undefined;
-      });
-
-      if (json.length == 0) return;
-      let loadedComments = getAllComments().map((e) => e.id);
-
-      for (let j in json) {
-        let comment = json[j];
-        if (!comment) continue;
-
-        if (loadedComments.indexOf(comment.id) !== -1) {
-          break;
-        }
-
-        redux.dispatch({ type: "ADD_NEW_COMMENT", comment });
-      }
-
-      if (json.length < loopOffset) break;
-    }
-
-    let comments = [...redux.state.comments.comments];
-
-    for (let k in comments) {
-      let comment = comments[k];
-      offset = -loopOffset;
-      while (true) {
-        offset += loopOffset;
-        let res = await fetch(URL + `/${comment.id}/replies?offset=${offset}&limit=${loopOffset}`);
-        let data = await res.json();
-
-        let json = data.map((e) => {
-          let time = new Date(e.datetime_created).getTime();
-          if (time > newest) {
-            return e;
-          } else return undefined;
+    if (type === "studios") {
+        await redux.waitForState(({ studio }) => studio.infoStatus === "FETCHED", { 
+            actions: ["SET_INFO"]
         });
+    } else {
+        await redux.waitForState(({ preview }) => preview.status.project === "FETCHED", {
+            actions: ["SET_PROJECT_INFO"]
+        });
+    }
 
-        if (json.length == 0) break;
-        for (let reply of json) {
-          if (!reply) continue;
-          let loadedComments = getAllReplies().map((c) => c.id);
+    const typePath = type === "projects" ? `users/${redux.state.preview.projectInfo.author.username}` : "studios";
+    const commentsURL = `https://api.scratch.mit.edu/${typePath}/${id}/comments`;
 
-          if (loadedComments.indexOf(reply.id) !== -1) continue;
+    const getAllComments = () => [...redux.state.comments.comments];
+    const getAllReplies = () => {
+        const replies = [];
 
-          redux.dispatch({ type: "ADD_NEW_COMMENT", comment: reply, topLevelCommentId: comment.id });
+        for (const reply of Object.values(redux.state.comments.replies)) {
+            replies.push(...reply);
         }
 
-        if (json.length < loopOffset) break;
-      }
+        return replies
+    };
+
+    let newest = Date.now();
+
+    while (true) {
+        const loopOffset = 40;
+        let offset = -loopOffset;
+        while (true) {
+            offset += loopOffset;
+
+            const res = await fetch(`${commentsURL}?offset=${offset}&limit=${loopOffset}`);
+            const data = await res.json();
+
+            const comments = data.map(comment => {
+                const commentCreatedTime = new Date(comment.datetime_created).getTime();
+
+                if (commentCreatedTime > newest) {
+                    return comment;
+                } else {
+                    return undefined;
+                }
+            });
+
+            if (comments.length === 0) break;
+
+            const loadedComments = getAllComments().map(comment => comment.id);
+
+            for (const comment of comments) {
+                if (!comment) continue;
+
+                if (loadedComments.indexOf(comment.id) !== -1) {
+                    break;
+                }
+
+                redux.dispatch({ type: "ADD_NEW_COMMENT", comment });
+            }
+
+            if (comments.length < loopOffset) break;
+        }
+
+        const comments = getAllComments();
+
+        for (const comment of comments) {
+            offset = -loopOffset;
+
+            while (true) {
+                offset += loopOffset;
+                const res = await fetch(`${commentsURL}/${comment.id}/replies?offset=${offset}&limit=${loopOffset}`);
+                const unprocReplies = await res.json();
+
+                const replies = unprocReplies.map(reply => {
+                    const replyCreatedTime = new Date(reply.datetime_created).getTime();
+
+                    if (replyCreatedTime > newest) {
+                        return reply;
+                    } else {
+                        return undefined;
+                    }
+                });
+
+                if (replies.length === 0) break;
+
+                const loadedReplies = getAllReplies().map(reply => reply.id);
+
+                for (const reply of replies) {
+                    if (!reply) continue;
+
+                    if (loadedReplies.indexOf(reply.id) !== -1) continue;
+
+                    redux.dispatch({ type: "ADD_NEW_COMMENT", comment: reply, topLevelCommentId: comment.id });
+                }
+            }
+        }
     }
-  }
 }
