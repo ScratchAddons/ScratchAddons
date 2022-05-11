ec0033d377e06ac44e1b18b02b9d397f185eef22
retron <retronbv@gmail.com>
2022-05-11 06:32:56 -0400

    push  before putting into custom navbar

--- addons/compact-nav/www.js
+++ addons/compact-nav/www.js
@@ -16,8 +16,6 @@ export default async function ({ addon, console }) {
       profileName.appendChild(profileNameChild);
       dropdown.insertBefore(profileName, dropdown.firstChild);
       dropdown.childNodes[1].classList.add("divider");
-      accountnav.firstChild.childNodes[1].setAttribute("style","display:none;");
-      accountnav.firstChild.firstChild.setAttribute("style","margin-right: 0px !important;");
     }
     function disable(){
       profileNameChild.removeChild(profileNameChildChild);
