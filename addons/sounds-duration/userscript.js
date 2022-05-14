5134d6f57870d2e23ab2dc15eeead08492b5234b
TheColaber <thecolaber@gmail.com>
2022-05-14 17:41:48 -0400

    Sound Duration Addon (#4324)
    * Sound Duration Addon
    
    * Fix bugs
    
    * Fix bug
    
    * Position and styling
    
    * Change description, add featured tag, versionAdded
    
    * Add reduxCondition optimization to waitForElement
    
    * Add comment link to scratch-gui/sound-editor.jsx
    
    Co-authored-by: Maximouse <51849865+mxmou@users.noreply.github.com>
    Co-authored-by: World_Languages <mundofinky@gmail.com>

---
+++ addons/sounds-duration/userscript.js
@@ -0,0 +1,54 @@
+export default async function ({ addon, msg, global, console }) {
+  while (true) {
+    const container = await addon.tab.waitForElement("[class^=sound-editor_editor-container]", {
+      markAsSeen: true,
+      reduxCondition: (state) => state.scratchGui.editorTab.activeTabIndex === 2 && !state.scratchGui.mode.isPlayerOnly,
+    });
+    const el = container.querySelector("[class^=sound-editor_row]").appendChild(
+      Object.assign(document.createElement("div"), {
+        className: "sa-sound-duration",
+      })
+    );
+    const state = container[addon.tab.traps.getInternalKey(container)].return.return.return.stateNode;
+
+    const norm = (num) => (Math.round(num * 100) / 100).toFixed(2);
+    const separator = " / ";
+
+    el.textContent =
+      norm(0) + separator + norm(state.audioBufferPlayer.buffer.length / state.audioBufferPlayer.buffer.sampleRate);
+
+    // https://github.com/LLK/scratch-gui/blob/develop/src/containers/sound-editor.jsx
+
+    const _handleUpdatePlayhead = state.handleUpdatePlayhead;
+    state.handleUpdatePlayhead = function (playhead) {
+      _handleUpdatePlayhead.call(this, playhead);
+      const timeSinceStart = (Date.now() - this.startTime) / 1000;
+      const trimStartTime = state.audioBufferPlayer.buffer.duration * state.audioBufferPlayer.trimStart;
+      const trimmedDuration = state.audioBufferPlayer.buffer.duration * state.audioBufferPlayer.trimEnd - trimStartTime;
+      el.textContent = norm(timeSinceStart) + separator + norm(trimmedDuration);
+    };
+
+    const _handleStoppedPlaying = state.handleStoppedPlaying;
+    state.handleStoppedPlaying = function () {
+      _handleStoppedPlaying.call(this);
+      const trimStartTime = state.audioBufferPlayer.buffer.duration * state.audioBufferPlayer.trimStart;
+      const trimmedDuration = state.audioBufferPlayer.buffer.duration * state.audioBufferPlayer.trimEnd - trimStartTime;
+      el.textContent = norm(0) + separator + norm(trimmedDuration);
+    };
+
+    const _componentWillReceiveProps = state.componentWillReceiveProps;
+    state.componentWillReceiveProps = function (newProps) {
+      _componentWillReceiveProps.call(this, newProps);
+      el.textContent =
+        norm(0) + separator + norm(state.audioBufferPlayer.buffer.length / state.audioBufferPlayer.buffer.sampleRate);
+    };
+
+    const _handleUpdateTrim = state.handleUpdateTrim;
+    state.handleUpdateTrim = function (trimStart, trimEnd) {
+      _handleUpdateTrim.call(this, trimStart, trimEnd);
+      const trimStartTime = state.audioBufferPlayer.buffer.duration * trimStart;
+      const trimmedDuration = state.audioBufferPlayer.buffer.duration * trimEnd - trimStartTime;
+      el.textContent = norm(0) + separator + norm(trimmedDuration);
+    };
+  }
+}
