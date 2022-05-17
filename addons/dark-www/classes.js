b99c685b82763332d300841dc4f6c0110c7e5dd5
Maximouse <51849865+mxmou@users.noreply.github.com>
2022-05-17 11:52:26 +0200

    2021 Annual Report

--- addons/dark-www/classes.js
+++ addons/dark-www/classes.js
@@ -1,8 +1,15 @@
 export default async function ({ addon, console }) {
-  addon.tab.waitForElement(".annual-report-content .covid-response-section").then(() => {
-    document.querySelector(".page").classList.add("sa-annual-report-2019");
+  addon.tab.waitForElement(".annual-report-content > .covid-response-section").then(() => {
+    document.body.classList.add("sa-annual-report-2019");
   });
-  addon.tab.waitForElement(".annual-report-content .looking-forward").then(() => {
-    document.querySelector(".page").classList.add("sa-annual-report-2020");
+  addon.tab.waitForElement(".annual-report-content > .directors-message").then(() => {
+    document.body.classList.add("sa-annual-report-2020");
   });
+  addon.tab.waitForElement(".annual-report-content > .reach-section.t").then(() => {
+    document.body.classList.add("sa-annual-report-2021");
+  });
+  while (true) {
+    const tweet = await addon.tab.waitForElement(".twitter-tweet iframe[src*='theme=light']", { markAsSeen: true });
+    setInterval(() => {tweet.src = tweet.src.replace("theme=light", "theme=dark");}, 0);
+  }
 }
