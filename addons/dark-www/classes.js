eb29850652f40713aecd79af231d1334f6fcc4c0
Maximouse <51849865+mxmou@users.noreply.github.com>
2022-05-18 11:10:34 +0200

    Improve Twitter embed code

--- addons/dark-www/classes.js
+++ addons/dark-www/classes.js
@@ -1,3 +1,5 @@
+import { textColor } from "../../libraries/common/cs/text-color.esm.js";
+
 export default async function ({ addon, console }) {
   addon.tab.waitForElement(".annual-report-content > .covid-response-section").then(() => {
     document.body.classList.add("sa-annual-report-2019");
@@ -8,10 +10,30 @@ export default async function ({ addon, console }) {
   addon.tab.waitForElement(".annual-report-content > .reach-section.t").then(() => {
     document.body.classList.add("sa-annual-report-2021");
   });
+  let tweets = [];
+  function updateTweetTheme(iframe) {
+    if (!addon.self.disabled && textColor(addon.settings.get("box"), false, true)) {
+      iframe.src = iframe.src.replace("theme=light", "theme=dark");
+    } else {
+      iframe.src = iframe.src.replace("theme=dark", "theme=light");
+    }
+  }
+  function updateAllTweets() {
+    for (let tweet of tweets) updateTweetTheme(tweet);
+  }
+  addon.self.addEventListener("disabled", updateAllTweets);
+  addon.self.addEventListener("reenabled", updateAllTweets);
+  addon.settings.addEventListener("change", updateAllTweets);
   while (true) {
     const tweet = await addon.tab.waitForElement(".twitter-tweet iframe[src*='theme=light']", { markAsSeen: true });
-    setInterval(() => {
-      tweet.src = tweet.src.replace("theme=light", "theme=dark");
-    }, 0);
+    tweets.push(tweet);
+    if (textColor(addon.settings.get("box"), false, true)) {
+      updateTweetTheme(tweet);
+      const loadListener = () => {
+        updateTweetTheme(tweet);
+        tweet.removeEventListener("load", loadListener);
+      };
+      tweet.addEventListener("load", loadListener);
+    }
   }
 }
