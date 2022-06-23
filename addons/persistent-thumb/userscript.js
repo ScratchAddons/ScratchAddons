b4840dcde0c606f412b81e41fa5faa28ac6df982
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-06-22 21:25:47 -0400

    new addon: persistent-thumb

---
+++ addons/persistent-thumb/userscript.js
@@ -0,0 +1,16 @@
+export default async function ({ addon, global, console }) {
+  const el = await addon.tab.waitForElement("div.stage_green-flag-overlay-wrapper_2hUi_.box_box_2jjDp", {
+    markAsSeen: true,
+  });
+  
+  const projectId = window.location.pathname.split("/")[2];
+  const thumb = `https://cdn2.scratch.mit.edu/get_image/project/${projectId}_480x360.png`
+  setThumb();
+  
+  addon.self.addEventListener("disabled", () => el.style.backgroundImage = "none");
+  addon.self.addEventListener("reenabled", () => setThumb());
+  
+  function setThumb() {
+    el.style.backgroundImage = `url(${thumb})`
+  }
+}
