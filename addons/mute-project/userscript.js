657752bbd4e36f0c4dc76812394ab4dbeb7aabfc
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-06-29 20:11:16 -0400

    mute-project: add slider option

--- addons/mute-project/userscript.js
+++ addons/mute-project/userscript.js
@@ -1,28 +1,54 @@
 export default async function ({ addon, global, console }) {
   const vm = addon.tab.traps.vm;
-  let muted = false;
+  let vol = 1;
   let icon = document.createElement("img");
-  icon.src = "/static/assets/e21225ab4b675bc61eed30cfb510c288.svg";
+  let mute = "/static/assets/e21225ab4b675bc61eed30cfb510c288.svg";
+  let quiet = "/static/assets/3547fa1f2678a483a19f46852f36b426.svg";
+  let loud = "/static/assets/b2c44c738c9cbc1a99cd6edfd0c2b85b.svg";
+  icon.src = loud;
   icon.loading = "lazy";
-  icon.style.display = "none";
+  let slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = 0;
+  slider.max = 1;
+  slider.step = 0.01;
+  slider.style.width = "100px"
   const toggleMute = (e) => {
     if (!addon.self.disabled && (e.ctrlKey || e.metaKey)) {
       e.cancelBubble = true;
       e.preventDefault();
-      muted = !muted;
-      if (muted) {
-        vm.runtime.audioEngine.inputNode.gain.value = 0;
-        icon.style.display = "block";
-      } else {
-        vm.runtime.audioEngine.inputNode.gain.value = 1;
-        icon.style.display = "none";
-      }
+      setVol((vol == 0) ? 1 : 0)
     }
   };
+  
+  function setVol(v) {
+    vol = v
+    vm.runtime.audioEngine.inputNode.gain.value = vol;
+    slider.value = vol;
+    if (vol == 0) {
+      icon.src = mute;
+      icon.style.display = "block";
+      return
+    }
+    if (vol < 0.5) {
+      icon.src = quiet;
+    } else {
+      icon.src = loud;
+    }
+    icon.style.display = addon.settings.get("show-slider") ? "block" : "none";
+  }
+  
   addon.self.addEventListener("disabled", () => {
-    muted = false;
     vm.runtime.audioEngine.inputNode.gain.value = 1;
     icon.style.display = "none";
+    slider.style.display = "none";
+  });
+  
+  addon.self.addEventListener("reenabled", () => {
+    if (addon.settings.get("show-slider")) {
+      slider.style.display = "block";
+    }
+    setVol(1)
   });
 
   while (true) {
@@ -33,5 +59,19 @@ export default async function ({ addon, global, console }) {
     addon.tab.appendToSharedSpace({ space: "afterStopButton", element: icon, order: 0 });
     button.addEventListener("click", toggleMute);
     button.addEventListener("contextmenu", toggleMute);
+    addon.tab.appendToSharedSpace({ space: "afterStopButton", element: slider, order: 0 });
+    setVol(1)
+    slider.addEventListener("change", function(e) {
+      setVol(this.value);
+    });
+    addon.settings.addEventListener("change", function(){
+      if (addon.settings.get("show-slider")) {
+        slider.style.display = "block";
+      } else {
+        slider.style.display = "none";
+        vol = 1;
+      }
+      setVol(vol)
+     });
   }
 }
