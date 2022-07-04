b70516e5bf50377df0f53773c6af5d0ba8b83f8e
Norbiros <norbiros@protonmail.com>
2022-07-04 18:45:27 +0200

    Bug fixes and lot more!

--- addons/my-ocular/discuss.js
+++ addons/my-ocular/discuss.js
@@ -1,40 +1,41 @@
 export default async function ({ addon, global, console, msg }) {
+  let statusSetting = addon.settings.get("show-status");
   let posts = document.querySelectorAll(".blockpost");
   let cache = Object.create(null);
-  let statusSetting = addon.settings.get("show-status");
 
+  // Update title, dot and status on settings change
   addon.settings.addEventListener("change", async function () {
     posts.forEach(async (i) => {
       let username = i.querySelector(".username").innerText;
       let statusElement = i.querySelector(".my-ocular-status");
-      const { userStatus, color } = await fetchStatus(username);
-      i.querySelector(".my-ocular-dot").style.backgroundColor =
-        statusSetting == "ocular" ? (color ? color : "#bbb") : "";
-      if (userStatus) {
-        statusElement.innerText = userStatus;
-      } else {
-        statusElement.innerText = "";
-      }
-      updateTitle(statusElement);
+      statusSetting = addon.settings.get("show-status");
+      
+      const { userStatus, color, statusType } = await fetchStatus(username);
+      i.querySelector(".my-ocular-dot").style.backgroundColor = statusType == "ocular" ? (color ? color : "#bbb") : "";
+      statusElement.innerText = userStatus ? userStatus : ""; // If status is not set, show "" instead of undefined
+      updateTitle(statusElement, statusType);
     });
   });
 
   posts.forEach(async (i) => {
     let username = i.querySelector(".username").innerText;
-
     let left = i.querySelector(".postleft").children[0];
 
-    const { userStatus, color } = await (cache[username] || (cache[username] = fetchStatus(username)));
+    const { userStatus, color, statusType } = await (cache[username] || (cache[username] = fetchStatus(username)));
 
     let br = document.createElement("br");
     addon.tab.displayNoneWhileDisabled(br);
+
+    // Create status element
     let status = document.createElement("i");
     status.classList.add("my-ocular-status");
     addon.tab.displayNoneWhileDisabled(status);
-    updateTitle(status);
+    updateTitle(status, statusType);
+
+    // Create ocular dot
     let dot = document.createElement("span");
     addon.tab.displayNoneWhileDisabled(dot, { display: "inline-block" });
-    updateTitle(dot);
+    updateTitle(dot, statusType);
     dot.className = "my-ocular-dot";
 
     left.appendChild(br);
@@ -43,22 +44,28 @@ export default async function ({ addon, global, console, msg }) {
 
     if (userStatus) {
       status.innerText = userStatus;
-      dot.style.backgroundColor = statusSetting == "ocular" ? (color ? color : "#bbb") : "";
+      dot.style.backgroundColor = statusType == "ocular" ? (color ? color : "#bbb") : "";
     }
   });
 
   async function fetchStatus(username) {
-    let showstatus = addon.settings.get("show-status");
-    let response = await fetchSpecificStatus(username, showstatus);
-    if (await response.status == null) {
-      let statusType = showstatus == "aviate" ? "ocular" : (showstatus == "ocular" ? "aviate" : "");
-      response = await fetchSpecificStatus(username, statusType);
-      if (response) statusSetting = statusType
+    let statusType; // Why? Different posts can have different statuses
+    let response = await fetchSpecificStatus(username, addon.settings.get("show-status"));
+    // If status is not set, try to get another status
+    if (!response.status) {
+      let otherStatus = addon.settings.get("show-status") == "aviate" ? "ocular" : (addon.settings.get("show-status") == "ocular" ? "aviate" : "");
+      response = await fetchSpecificStatus(username, otherStatus);
+      if (response.status) {
+        statusSetting = statusType;
+        statusType = otherStatus;
+      }
+    } else if (response != false) {
+      statusType = addon.settings.get("show-status");
     }
-    const data = (await response) ? await response : '"status": "","color": ""';
     return {
-      userStatus: data.status?.replace(/\n/g, " "),
-      color: data.color,
+      userStatus: response.status?.replace(/\n/g, " "),
+      color: response.color,
+      statusType: statusType,
     };
   }
 
@@ -68,11 +75,11 @@ export default async function ({ addon, global, console, msg }) {
     } else if (type == "aviate") {
       return (await fetch(`https://aviateapp.eu.org/api/${username}`)).json();
     }
-    return;
+    return false;
   }
 
-  function updateTitle(el) {
-    if (statusSetting == "ocular") el.title = msg("status-hover");
-    else if (statusSetting == "aviate") el.title = msg("aviate-status-hover");
+  function updateTitle(el, statusType) {
+    if (statusType == "ocular") el.title = msg("status-hover");
+    else if (statusType == "aviate") el.title = msg("aviate-status-hover");
   }
 }
--- addons/my-ocular/profile.js
+++ addons/my-ocular/profile.js
@@ -1,17 +1,21 @@
 export default async function ({ addon, global, console, msg }) {
   let statusSetting = addon.settings.get("show-status");
+  
+  // Update title, dot and status on settings change
   addon.settings.addEventListener("change", async function () {
     let statusElement = document.querySelector("#my-ocular-status");
-    let status = (await fetchStatus(username)).status;
-    if (statusElement && status) {
-      statusElement.innerText = status;
+    statusSetting = addon.settings.get("show-status"); // Update statusSetting variable
+    
+    let status = await fetchStatus(username);
+    if (status?.status) {
+      statusElement.innerText = status.status;
+      dot.style.backgroundColor = statusSetting == "ocular" ? (status.color ? status.color : "#bbb") : "";
+      updateTitle(statusElement);
       locationElem.classList.add("group");
     } else {
       statusElement.innerText = "";
       locationElem.classList.remove("group");
     }
-    dot.style.backgroundColor = addon.settings.get("show-status") == "ocular" ? (color ? color : "#bbb") : "";
-    updateTitle(statusElement);
   });
 
   let username = document.querySelector("#profile-data > div.box-head > div > h2").innerText;
@@ -22,25 +26,23 @@ export default async function ({ addon, global, console, msg }) {
   updateTitle(statusSpan);
   statusSpan.id = "my-ocular-status";
 
-
+  // Create ocular dot
   let dot = document.createElement("span");
   addon.tab.displayNoneWhileDisabled(dot, { display: "inline-block" });
   dot.title = msg("status-hover");
   dot.className = "my-ocular-dot";
 
-
   let locationElem = document.createElement("span"); // Create a new location element
   locationElem.innerText = container.innerText; // Set it to the old innertext
 
   container.innerText = ""; // Clear the old location
-
   container.appendChild(locationElem); // give it the location
   container.appendChild(statusSpan);
   container.appendChild(dot);
-
+  
   if (typeof data.status !== "string") return;
   var statusText = data.status.replace(/\n/g, " "); // clear out newlines
-  if (data !== false && statusText) {
+  if (statusText) {
     locationElem.classList.add("group");
     statusSpan.innerText = data.status;
     dot.style.backgroundColor = data.color;
@@ -48,24 +50,24 @@ export default async function ({ addon, global, console, msg }) {
 
 
   async function fetchStatus(username) {
-    let showstatus = addon.settings.get("show-status");
-    let response = await fetchSpecificStatus(username, showstatus);
-    if (await response.status == null) {
-      let statusType = showstatus == "aviate" ? "ocular" : (showstatus == "ocular" ? "aviate" : "");
+    let response = await fetchSpecificStatus(username, addon.settings.get("show-status"));
+    // If status is not set, try to get another status
+    if (!response.status) {
+      let statusType = addon.settings.get("show-status") == "aviate" ? "ocular" : (addon.settings.get("show-status") == "ocular" ? "aviate" : "");
       response = await fetchSpecificStatus(username, statusType);
-      if (response) statusSetting = statusType
+      if (response.status) statusSetting = statusType
     }
-    const data = (await response) ? await response : '"status": "","color": ""';
-    return data ? data : false;
+    response = response.status ? response : "'status': '', 'color': 'red'";
+    return response;
   }
 
   async function fetchSpecificStatus(username, type) {
     if (type == "ocular") {
-      return (await fetch(`https://my-ocular.jeffalo.net/api/user/${username}`)).json();
+      return await (await fetch(`https://my-ocular.jeffalo.net/api/user/${username}`)).json();
     } else if (type == "aviate") {
-      return (await fetch(`https://aviateapp.eu.org/api/${username}`)).json();
+      return await (await fetch(`https://aviateapp.eu.org/api/${username}`)).json();
     }
-    return;
+    return "";
   }
 
   function updateTitle(el) {
