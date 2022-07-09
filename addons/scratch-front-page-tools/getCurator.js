3ce19296a07be5ccede9ac57814fad759f4756c1
Platformer444 <Platformer444@users.noreply.github.com>
2022-07-09 10:05:50 +0000

    Format code

--- addons/scratch-front-page-tools/getCurator.js
+++ addons/scratch-front-page-tools/getCurator.js
@@ -1,8 +1,8 @@
-let curatorText = document.querySelectorAll('.box-header>h4')[4].textContent;
-let curator = '';
+let curatorText = document.querySelectorAll(".box-header>h4")[4].textContent;
+let curator = "";
 let continueloop = false;
 for (var i = 0; i < curatorText.length; i++) {
-  if (curatorText[i-2] === 'y' || continueloop) {
+  if (curatorText[i - 2] === "y" || continueloop) {
     curator = curator + curatorText[i];
     continueloop = true;
   }
--- addons/scratch-front-page-tools/getSDS.js
+++ addons/scratch-front-page-tools/getSDS.js
@@ -1,8 +1,8 @@
-let sdsText = document.querySelectorAll('.box-header>h4')[5].textContent;
-let sds = '';
+let sdsText = document.querySelectorAll(".box-header>h4")[5].textContent;
+let sds = "";
 let continueloop = false;
 for (var i = 0; i < sdsText.length; i++) {
-  if (sdsText[i-2] === '-' || continueloop) {
+  if (sdsText[i - 2] === "-" || continueloop) {
     sds = sds + sdsText[i];
     continueloop = true;
   }
