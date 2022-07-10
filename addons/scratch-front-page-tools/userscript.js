afb23b76522aed919a3e3a590e5807f26904b91b
Platformer444 <78246772+Platformer444@users.noreply.github.com>
2022-07-10 15:26:46 +0530

    Update userscript.js

--- addons/scratch-front-page-tools/userscript.js
+++ addons/scratch-front-page-tools/userscript.js
@@ -1,14 +1,10 @@
-import curator from "./getCurator.js";
-import sds from "./getSDS.js";
-
 export default async function ({ addon, global, console }) {
-  Load();
   DeleteRows();
   AddRows();
+  Load();
 
   function Load() {
-    let rowDataRaw =
-      "{" +
+    let rowDataRaw = '{'+
       '"0" : "1",' +
       '"1" : "1",' +
       '"2" : "1",' +
@@ -18,70 +14,117 @@ export default async function ({ addon, global, console }) {
       '"6" : "1",' +
       '"7" : "1",' +
       '"8" : "1"' +
-      "}";
-
-    if (!localStorage.getItem("rowData")) {
-      localStorage.setItem("rowData", rowDataRaw);
+    '}';
+    
+    if (!localStorage.getItem('rowData')) {
+      localStorage.setItem('rowData', rowDataRaw);
     }
 
-    let rowData = JSON.parse(localStorage.getItem("rowData"));
-    let rows = document.querySelectorAll(".box");
+    let rowData = JSON.parse(localStorage.getItem('rowData'));
+    let rows = document.querySelectorAll('.box');
 
-    for (var i = 0; i < rows.length; i++) {
-      if (rowData[i] === "1") {
-        rows[i].style.display = "inline-block";
+    for (let i = 0; i < rows.length; i++) {
+      if (rowData[i] === '1') {
+        rows[i].style.display = 'inline-block';
       } else {
-        rows[i].style.display = "none";
+        rows[i].style.display = 'none';
       }
     }
   }
 
   function DeleteRows() {
-    const allBoxHeaders = document.querySelectorAll(".box>.box-header>p").length;
+    const allBoxHeaders = document.querySelectorAll('.box>.box-header>p').length;
 
-    for (var i = 0; i < allBoxHeaders; i++) {
-      let delButton = document.createElement("button");
-      delButton.className = "sa-del-button";
-      document.querySelectorAll(".box>.box-header>p")[Number(i)].appendChild(delButton);
+    for (let i = 0; i < allBoxHeaders; i++) {
+      let available = document.querySelectorAll('.box-header>p>.sa-del-button')[Number(i)];
+      console.log(available);
+      if (available === undefined) {
+        let delButton = document.createElement('button');
+        delButton.className = 'sa-del-button';
+        document.querySelectorAll('.box>.box-header>p')[Number(i)].appendChild(delButton);
 
-      const delImg = document.createElement("img");
-      delImg.src = "https://scratch.mit.edu/static/assets/a5787bb7364d8131ed49a8f53037d7f4.svg";
-      delButton.appendChild(delImg);
+        const delImg = document.createElement('img');
+        delImg.src = 'https://scratch.mit.edu/static/assets/a5787bb7364d8131ed49a8f53037d7f4.svg';
+        delButton.appendChild(delImg);
+      }
     }
 
-    var delBTN = document.querySelectorAll(".sa-del-button");
-    delBTN.forEach((el) =>
-      el.addEventListener("click", () => {
-        let parent = el.parentNode;
-        let boxHeader = parent.parentNode;
-        let box = boxHeader.parentNode;
-        let boxHeaders = document.querySelectorAll(".box-header");
+    let delBTN = document.querySelectorAll('.sa-del-button')
+    delBTN.forEach(el => el.addEventListener('click', () => {
+      let parent = el.parentNode;
+      let boxHeader = parent.parentNode;
+      let box = boxHeader.parentNode;
+      let boxHeaders = document.querySelectorAll('.box-header');
 
-        for (var i = 0; i < boxHeaders.length; i++) {
-          if (boxHeaders[i].textContent === boxHeader.textContent) {
-            let num = i;
-            box.style.display = "none";
-            let rowData = JSON.parse(localStorage.getItem("rowData"));
-            rowData[num] = "0";
-            let rowDataRaw = JSON.stringify(rowData);
-            localStorage.setItem("rowData", rowDataRaw);
-            break;
-          }
+      for (let i = 0; i < boxHeaders.length; i++) {
+        if (boxHeaders[i].textContent === boxHeader.textContent) {
+          let num = i;
+          box.style.display = 'none';
+          let rowData = JSON.parse(localStorage.getItem('rowData'));
+          rowData[num] = '0';
+          let rowDataRaw = JSON.stringify(rowData);
+          localStorage.setItem('rowData', rowDataRaw);
+          break;
         }
-      })
-    );
+      }
+    }));
   }
 
   function AddRows() {
-    var splash = document.querySelectorAll(".splash")[0];
-    var firstSibling = document.querySelectorAll(".splash>.mod-splash")[0];
-
-    let addBTN = document.createElement("button");
-    addBTN.className = "button sa-add-button";
+    let splash = document.querySelectorAll('.splash')[0];
+    let firstSibling = document.querySelectorAll('.splash>.mod-splash')[0];
+    let addBTN = document.createElement('button');
+    addBTN.className = 'button sa-add-button';
     splash.insertBefore(addBTN, firstSibling);
 
-    let addSpan = document.createElement("span");
-    addSpan.textContent = "+";
+    let addSpan = document.createElement('span');
+    addSpan.textContent = '+';
     addBTN.appendChild(addSpan);
+
+    let optionDiv = document.createElement('div');
+    optionDiv.className = 'sa-option-div';
+    optionDiv.style.display = 'none';
+    splash.insertBefore(optionDiv, firstSibling);
+
+    let form = document.createElement('form');
+    optionDiv.appendChild(form);
+
+    let rows = document.querySelectorAll('.box-header>h4');
+    for (let i = 0; i < rows.length; i++) {
+      let option = document.createElement('input');
+      option.type = 'checkbox';
+      option.name = 'option' + String(i);
+      form.appendChild(option);
+
+      let label = document.createElement('label');
+      label.textContent = rows[i].textContent;
+      label.for = 'option' + String(i);
+      form.appendChild(label);
+
+      form.appendChild(document.createElement('br'));
+    }
+
+    addBTN.addEventListener('click', () => {
+      let breakLine = document.querySelectorAll('.splash>br');
+      if (optionDiv.style.display === 'none') {
+        optionDiv.style.display = 'block';
+      } else if (optionDiv.style.display === 'block') {
+        optionDiv.style.display = 'none';
+
+        let rowData = JSON.parse(localStorage.getItem('rowData'));
+        let options = document.querySelectorAll('.sa-option-div>form>input');
+        for (let i = 0; i < options.length; i++) {
+          let show = options[i].checked;
+          if (show) {
+            rowData[i] = '1';
+          }
+          options[i].checked = 'true';
+        }
+        let rowDataRaw = JSON.stringify(rowData);
+        localStorage.setItem('rowData', rowDataRaw);
+        Load();
+        DeleteRows();
+      }
+    });
   }
 }
