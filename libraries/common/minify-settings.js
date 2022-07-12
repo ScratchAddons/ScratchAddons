1f2383b54fbe541d0558b11826f9ec3c386985a2
WorldLanguages <mundofinky@gmail.com>
2022-07-12 16:42:00 -0300

    Add comments to minify-settings.js

--- libraries/common/minify-settings.js
+++ libraries/common/minify-settings.js
@@ -14,6 +14,7 @@ export default (settings, manifests) => {
     }, {});
   for (const [addonId, setting] of Object.entries(newSettings)) {
     if (manifestObj && !manifestObj[addonId]) {
+      // Delete settings from addons that no longer exist
       delete newSettings[addonId];
     } else {
       for (const settingsKey of Object.keys(setting)) {
@@ -22,10 +23,14 @@ export default (settings, manifests) => {
           !settingsKey.startsWith("_") &&
           !manifestObj[addonId].settings?.some((s) => settingsKey === s.id)
         ) {
+          // Delete settings that no longer exist (exception: those with setting id starting with underscore)
           delete setting[settingsKey];
         }
       }
       if (Object.keys(setting).length === 0) {
+        // Delete empty settings objects, even if "manifests" arg is null
+        // One of the deletions above might have caused an object to be empty
+        // Importing settings from a JSON file also causes most objects to be empty
         delete newSettings[addonId];
       }
     }
