0fa55d8cd692f93f4dbb7c7ba3a38eb63abb52ae
BroJac5246 <104146239+BroJac5246@users.noreply.github.com>
2022-07-12 22:08:04 -0500

    Add support for Dynamic Disable and Reenable

--- addons/remove-search-bar-autocomplete/userscript.js
+++ addons/remove-search-bar-autocomplete/userscript.js
@@ -1,8 +1,15 @@
 export default async function ({ addon, global, console }) {
   //Detects whether the current page is 2.0- or 3.0- styled. The search bars have different IDs depending on this, so we need to figure out which ID to use.
-  if (addon.tab.clientVersion == "scratch-www") {
-    document.getElementById("frc-q-1088").autocomplete = "off";
-  } else {
-    document.getElementById("search-input").autocomplete = "off";
+  function toggle(state) {
+    if (addon.tab.clientVersion == "scratch-www") {
+      document.getElementById("frc-q-1088").autocomplete=state
+    } else {
+      document.getElementById("search-input").autocomplete=state
+    }
   }
+
+  toggle("off");
+	
+  addon.self.addEventListener("disabled", () => toggle("on"));
+  addon.self.addEventListener("reenabled", () => toggle("off"));
 }
