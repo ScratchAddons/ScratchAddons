28e101a88c9710616772a5ba53c5d81b68f4b8e3
BroJac5246 <BroJac5246@users.noreply.github.com>
2022-07-13 03:08:34 +0000

    Format code

--- addons/remove-search-bar-autocomplete/userscript.js
+++ addons/remove-search-bar-autocomplete/userscript.js
@@ -2,14 +2,14 @@ export default async function ({ addon, global, console }) {
   //Detects whether the current page is 2.0- or 3.0- styled. The search bars have different IDs depending on this, so we need to figure out which ID to use.
   function toggle(state) {
     if (addon.tab.clientVersion == "scratch-www") {
-      document.getElementById("frc-q-1088").autocomplete=state
+      document.getElementById("frc-q-1088").autocomplete = state;
     } else {
-      document.getElementById("search-input").autocomplete=state
+      document.getElementById("search-input").autocomplete = state;
     }
   }
 
   toggle("off");
-	
+
   addon.self.addEventListener("disabled", () => toggle("on"));
   addon.self.addEventListener("reenabled", () => toggle("off"));
 }
