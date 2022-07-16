7e848ea1df0d53549ef201a13ecdae99499cd784
Weredime <100447465+Weredime@users.noreply.github.com>
2022-07-16 20:02:59 +0100

    better-emojis: refactor

--- addons/better-emojis/userscript.js
+++ addons/better-emojis/userscript.js
@@ -1,95 +1,53 @@
-export default async function ({ addon, global, console }) {
-  const updateEmojis = () => {
-    if (addon.tab.clientVersion === "scratch-www") {
-      if (addon.tab.redux?.state?.scratchGui && !addon.tab.redux.state.scratchGui.mode.isPlayerOnly) {
-        return;
-      }
-      const emojis = document.getElementsByClassName("emoji");
-      for (const emoji of emojis) {
-        checkEmojiNew(emoji);
-      }
-    } else if (addon.tab.clientVersion === "scratchr2") {
-      const emojis = document.getElementsByClassName("easter-egg");
-      for (const emoji of emojis) {
-        checkEmoji(emoji);
-      }
+export default async function ({ addon, global, console, msg }) {
+  const EMOJIS = [
+    "aww-cat",
+    "cat",
+    "cool-cat",
+    "fav-it-cat",
+    "gobo",
+    "huh-cat",
+    "lol-cat",
+    "love-it-cat",
+    "meow",
+    "pizza-cat",
+    "rainbow-cat",
+    "tongue-out-cat",
+    "upside-down-cat",
+    "waffle",
+    "wink-cat",
+  ];
+  const WWW_COMMENTS_REGEX = /^https?:\/\/scratch.mit.edu\/images\/emoji\/(.*).png$/;
+  const R2_COMMENTS_REGEX = /^https:\/\/cdn.scratch.mit.edu\/scratchr2\/static\/(.*)\/images\/easter_eggs\/(.*).png$/;
+  const COMMENTS_REGEX = addon.tab.clientVersion === "scratch-www" ? WWW_COMMENTS_REGEX : R2_COMMENTS_REGEX;
+
+  addon.self.addEventListener("disabled", () => {
+    const emojis = document.querySelectorAll("img.easter-egg, img.emoji");
+    for (const emoji of emojis) {
+      emoji.src =
+        addon.tab.clientVersion === "scratch-www"
+          ? `https://scratch.mit.edu/images/emoji/${emoji.dataset.emoji}.png`
+          : `https://cdn.scratch.mit.edu/scratchr2/static/images/easter_eggs/${emoji.dataset.emoji}.png`;
     }
-  };
-
-  updateEmojis();
-  const obs = new MutationObserver(updateEmojis);
-  obs.observe(document.body, {
-    childList: true,
-    subtree: true,
   });
+  addon.self.addEventListener("reenabled", () => {
+    const emojis = document.querySelectorAll("img.easter-egg[data-emoji], img.emoji[data-emoji]");
+    for (const emoji of emojis) {
+      emoji.src = addon.self.dir + "/images/" + emoji.dataset.emoji + ".svg";
+    }
+  });
+  while (true) {
+    const emoji = await addon.tab.waitForElement(".emoji, .easter-egg", {
+      markAsSeen: true,
+      reduxCondition: (state) => state?.scratchGui.mode.isPlayerOnly,
+    });
 
-  function checkEmoji(el) {
-    if (el.src.includes("images/easter_eggs/cat.png")) el.src = addon.self.dir + "/images/cat.svg";
-    if (el.src.includes("images/easter_eggs/aww-cat.png")) el.src = addon.self.dir + "/images/aww-cat.svg";
-    if (el.src.includes("images/easter_eggs/cool-cat.png")) el.src = addon.self.dir + "/images/cool-cat.svg";
-    if (el.src.includes("images/easter_eggs/tongue-out-cat.png"))
-      el.src = addon.self.dir + "/images/tongue-out-cat.svg";
-    if (el.src.includes("images/easter_eggs/wink-cat.png")) el.src = addon.self.dir + "/images/wink-cat.svg";
-    if (el.src.includes("images/easter_eggs/lol-cat.png")) el.src = addon.self.dir + "/images/lol-cat.svg";
-    if (el.src.includes("images/easter_eggs/upside-down-cat.png"))
-      el.src = addon.self.dir + "/images/upside-down-cat.svg";
-    if (el.src.includes("images/easter_eggs/huh-cat.png")) el.src = addon.self.dir + "/images/cute-cat.svg";
-    if (el.src.includes("images/easter_eggs/love-it-cat.png")) el.src = addon.self.dir + "/images/love-it-cat.svg";
-    if (el.src.includes("images/easter_eggs/fav-it-cat.png")) el.src = addon.self.dir + "/images/fav-it-cat.svg";
-    if (el.src.includes("images/easter_eggs/rainbow-cat.png")) el.src = addon.self.dir + "/images/rainbow-cat.svg";
-    if (el.src.includes("images/easter_eggs/pizza-cat.png")) el.src = addon.self.dir + "/images/pizza-cat.svg";
-    if (el.src.includes("images/easter_eggs/meow.png")) el.src = addon.self.dir + "/images/meow.svg";
-    if (el.src.includes("images/easter_eggs/gobo.png")) el.src = addon.self.dir + "/images/gobo.svg";
-    if (el.src.includes("images/easter_eggs/waffle.png")) el.src = addon.self.dir + "/images/waffle.svg";
-  }
-
-  function checkEmojiNew(el) {
-    switch (el.src) {
-      case "https://scratch.mit.edu/images/emoji/cat.png":
-        el.src = addon.self.dir + "/images/cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/aww-cat.png":
-        el.src = addon.self.dir + "/images/aww-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/cool-cat.png":
-        el.src = addon.self.dir + "/images/cool-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/tongue-out-cat.png":
-        el.src = addon.self.dir + "/images/tongue-out-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/wink-cat.png":
-        el.src = addon.self.dir + "/images/wink-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/lol-cat.png":
-        el.src = addon.self.dir + "/images/lol-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/upside-down-cat.png":
-        el.src = addon.self.dir + "/images/upside-down-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/huh-cat.png":
-        el.src = addon.self.dir + "/images/cute-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/love-it-cat.png":
-        el.src = addon.self.dir + "/images/love-it-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/fav-it-cat.png":
-        el.src = addon.self.dir + "/images/fav-it-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/rainbow-cat.png":
-        el.src = addon.self.dir + "/images/rainbow-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/pizza-cat.png":
-        el.src = addon.self.dir + "/images/pizza-cat.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/meow.png":
-        el.src = addon.self.dir + "/images/meow.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/gobo.png":
-        el.src = addon.self.dir + "/images/gobo.svg";
-        break;
-      case "https://scratch.mit.edu/images/emoji/waffle.png":
-        el.src = addon.self.dir + "/images/waffle.svg";
-        break;
+    const match = emoji.src.match(COMMENTS_REGEX);
+    if (match !== null) {
+      const emojiId = match[addon.tab.clientVersion === "scratch-www" ? 1 : 2];
+      if (EMOJIS.includes(emojiId)) {
+        if (!addon.self.disabled) emoji.src = addon.self.dir + "/images/" + emojiId + ".svg";
+        emoji.dataset.emoji = emojiId;
+      }
     }
   }
 }
