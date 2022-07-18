75bdec8849bd7fd877740563230a45604b38d621
Norbiros <Norbiros@users.noreply.github.com>
2022-07-18 08:28:50 +0000

    Format code

--- addons/hide-delete-button/userscript.js
+++ addons/hide-delete-button/userscript.js
@@ -11,12 +11,21 @@ export default async function ({ addon, console, msg }) {
       }
 
       let type = null;
-      
-      if ( addon.settings.get("sprites") && e.target.closest("div[class*='sprite-selector_sprite-wrapper_'] div[class*='delete-button_delete-button_']") ) {
+
+      if (
+        addon.settings.get("sprites") &&
+        e.target.closest("div[class*='sprite-selector_sprite-wrapper_'] div[class*='delete-button_delete-button_']")
+      ) {
         type = "sprite";
-      } else if (addon.settings.get("sounds") && e.target.closest("[data-tabs] > :nth-child(5) div[class*='delete-button_delete-button_']")) {
+      } else if (
+        addon.settings.get("sounds") &&
+        e.target.closest("[data-tabs] > :nth-child(5) div[class*='delete-button_delete-button_']")
+      ) {
         type = "sound";
-      } else if (addon.settings.get("costumes") && e.target.closest("[data-tabs] > :nth-child(3) div[class*='delete-button_delete-button_']")) {
+      } else if (
+        addon.settings.get("costumes") &&
+        e.target.closest("[data-tabs] > :nth-child(3) div[class*='delete-button_delete-button_']")
+      ) {
         type = "costume";
       }
 
@@ -24,7 +33,7 @@ export default async function ({ addon, console, msg }) {
         e.preventDefault();
         e.stopPropagation();
         addon.tab
-          .confirm(msg("confirm-title", {object: msg(type)}), msg("confirm-message", {object: msg(type)}), {
+          .confirm(msg("confirm-title", { object: msg(type) }), msg("confirm-message", { object: msg(type) }), {
             okButtonLabel: msg("yes"),
             cancelButtonLabel: msg("no"),
             useEditorClasses: true,
