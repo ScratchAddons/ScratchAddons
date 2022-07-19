101042274e7a70ff5f593861b54d26be10fa9e94
GarboMuffin <muffin@muffin.ink>
2022-07-19 18:30:26 -0500

    hide-stage: Fix renderer crashes (#4861)
    * hide-stage: Fix crashes related to renderer breaking on 0x0 stage
    
    * Move sa-stage-hidden class to document root
    
    * Hide stage color pickers when stage is hidden
    
    * Format code
    
    * Fix comment
    
    * Fix another comment
    
    Co-authored-by: GarboMuffin <GarboMuffin@users.noreply.github.com>

--- addons/hide-stage/userscript.js
+++ addons/hide-stage/userscript.js
@@ -1,34 +1,76 @@
 export default async function ({ addon, console, msg }) {
+  // Wait until the project is loaded so that the renderer will definitely exist.
+  const vm = addon.tab.traps.vm;
+  await new Promise((resolve) => {
+    if (vm.editingTarget) return resolve();
+    vm.runtime.once("PROJECT_LOADED", resolve);
+  });
+
   let stageHidden = false;
-  let bodyWrapper;
   let smallStageButton;
   let largeStageButton;
   let hideStageButton;
 
   function hideStage() {
     stageHidden = true;
-    if (!bodyWrapper) return;
-    bodyWrapper.classList.add("sa-stage-hidden");
+    document.documentElement.classList.add("sa-stage-hidden");
     hideStageButton.classList.remove(addon.tab.scratchClass("stage-header_stage-button-toggled-off"));
     window.dispatchEvent(new Event("resize")); // resizes the code area and paint editor canvas
   }
 
   function unhideStage(e) {
     stageHidden = false;
-    if (!bodyWrapper) return;
-    bodyWrapper.classList.remove("sa-stage-hidden");
+    document.documentElement.classList.remove("sa-stage-hidden");
     hideStageButton.classList.add(addon.tab.scratchClass("stage-header_stage-button-toggled-off"));
     window.dispatchEvent(new Event("resize")); // resizes the code area and paint editor canvas
   }
 
   addon.self.addEventListener("disabled", () => unhideStage());
 
+  // Some places in Scratch's renderer use canvas.clientWidth and canvas.clientHeight.
+  // When the stage is hidden these will return 0 which can cause infinite loops or crashes.
+  const renderer = vm.renderer;
+
+  // Used by "touching mouse pointer" block and hovered sprite detection.
+  const originalClientSpaceToScratchBounds = renderer.clientSpaceToScratchBounds;
+  renderer.clientSpaceToScratchBounds = function patchedClientSpaceToScratchBounds(...args) {
+    const result = originalClientSpaceToScratchBounds.apply(this, args);
+    if (stageHidden) {
+      // We'll just say that the mouse is very far offscreen so that it shouldn't be touching anything
+      const BIG_NUMBER = 1000000;
+      // left, right, bottom, top
+      result.initFromBounds(BIG_NUMBER, BIG_NUMBER, BIG_NUMBER, BIG_NUMBER);
+    }
+    return result;
+  };
+
+  // Used by code editor eye droppers.
+  // We already hide the button with CSS but just in case it somehow gets called anyways, we'll still fix
+  // this method.
+  const originalExtractColor = renderer.extractColor;
+  renderer.extractColor = function patchedExtractColor(...args) {
+    if (stageHidden) {
+      // Pretend the stage is transparent. The user has no way to hover over the stage anyways.
+      return {
+        data: new Uint8ClampedArray(4),
+        width: 1,
+        height: 1,
+        color: {
+          r: 0,
+          g: 0,
+          b: 0,
+          a: 0,
+        },
+      };
+    }
+    return originalExtractColor.apply(this, args);
+  };
+
   while (true) {
     const stageControls = await addon.tab.waitForElement("[class*='stage-header_stage-size-toggle-group_']", {
       markAsSeen: true,
       reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
     });
-    bodyWrapper = document.querySelector("[class*='gui_body-wrapper_']");
     smallStageButton = stageControls.firstChild;
     smallStageButton.classList.add("sa-stage-button-middle");
     largeStageButton = stageControls.lastChild;
