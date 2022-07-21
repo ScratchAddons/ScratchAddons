52381fa8f20f7f82f6d3c21955592f0f174f13b6
Weredime <100447465+Weredime@users.noreply.github.com>
2022-07-21 21:53:13 +0100

    Remove diff

--- addons/exact-count/forum.js
+++ addons/exact-count/forum.js
@@ -18,7 +18,6 @@ export default async function ({ addon, global, console }) {
     const userbox = await addon.tab.waitForElement(".postleft dl", { markAsSeen: true });
     if (userbox.childNodes[6] && userbox.childNodes[6].nodeValue.includes("+")) {
       const postCountReal = Number(/(\d+)\+? posts?/.exec(userbox.childNodes[6].nodeValue)[1]);
-
       fetch("https://scratchdb.lefty.one/v3/forum/user/info/" + userbox.querySelector("a").innerText)
         .then((response) => response.json())
         .then((data) => {
