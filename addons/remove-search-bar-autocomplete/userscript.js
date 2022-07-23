7ebe8f89b9489573f3677b74f96b71efc71309dc
BroJac5246 <104146239+BroJac5246@users.noreply.github.com>
2022-07-22 20:42:29 -0600

    Add addon.tab.waitForElement
    Also updated a comment. Thanks to Weredime for this.

--- addons/remove-search-bar-autocomplete/userscript.js
+++ addons/remove-search-bar-autocomplete/userscript.js
@@ -1,15 +1,24 @@
 export default async function ({ addon, global, console }) {
-  //Detects whether the current page is 2.0- or 3.0- styled. The search bars have different IDs depending on this, so we need to figure out which ID to use.
-  function toggle(state) {
-    if (addon.tab.clientVersion == "scratch-www") {
-      document.getElementById("frc-q-1088").autocomplete = state;
-    } else {
-      document.getElementById("search-input").autocomplete = state;
+  // Detects whether the current page is using scratch-www or scratchr2.
+  // The search bars have different selectors depending on this, so we need to figure out which selector to use.
+  const SEARCH_BAR_SELECTOR = addon.tab.clientVersion === "scratch-www" ? "#frc-q-1088" : "#search-input";
+  let searchBar = null;
+  addon.self.addEventListener("disabled", () => {
+    if (searchBar) searchBar.autocomplete = "on";
+  });
+  addon.self.addEventListener("reenabled", () => {
+    if (searchBar) searchBar.autocomplete = "off";
+  });
+  while (true) {
+    // Wait for the current search bar to load.
+    searchBar = await addon.tab.waitForElement(SEARCH_BAR_SELECTOR, {
+      markAsSeen: true,
+      reduxCondition: (state) => (state.scratchGui ? state.scratchGui.mode.isPlayerOnly : true),
+    });
+    // Check if this addon is disabled; because the end-user
+    // could have disabled the addon while they were in the editor.
+    if (!addon.self.disabled) {
+      searchBar.autocomplete = "off";
     }
   }
-
-  toggle("off");
-
-  addon.self.addEventListener("disabled", () => toggle("on"));
-  addon.self.addEventListener("reenabled", () => toggle("off"));
 }
