91eda66ed4dbdb8ed923c810d0e809af0019a374
TheColaber <72760579+TheColaber@users.noreply.github.com>
2022-07-24 08:53:34 -0400

    Add transition code
    Read comment for what we are doing in this transition

--- background/get-addon-settings.js
+++ background/get-addon-settings.js
@@ -51,88 +51,16 @@ chrome.storage.sync.get(["addonSettings", "addonsEnabled"], ({ addonSettings = {
       // TODO: we should be using Object.create(null) instead of {}
       const settings = addonSettings[addonId] || {};
       let madeChangesToAddon = false;
-      if (addonId === "project-info" && settings.editorCount) {
-        // Transition v1.22 to v1.23
-        // project-info was split into 2 addons
-        madeChangesToAddon = madeAnyChanges = true;
-        delete settings.editorCount;
-        addonsEnabled["block-count"] = true;
-      }
-      if (addonId === "editor-dark-mode") {
-        // Transition v1.22 to v1.23
-        // TurboWarp Dark preset changes:
-        updatePresetIfMatching(
-          manifest.presets.find((p) => p.id === "tw-dark"),
-          settings,
-          {
-            page: "#111111",
-            primary: "#ff4d4d",
-            highlightText: "#ff4d4d",
-            menuBar: "#333333",
-            activeTab: "#1e1e1e",
-            tab: "#2e2e2e",
-            selector: "#1e1e1e",
-            selector2: "#2e2e2e",
-            selectorSelection: "#111111",
-            accent: "#111111",
-            input: "#1e1e1e",
-            workspace: "#1e1e1e",
-            categoryMenu: "#111111",
-            palette: "#111111",
-            fullscreen: "#111111",
-            stageHeader: "#111111",
-            border: "#ffffff0d",
-          },
-          1
-        );
-        // Experimental Dark changes:
-        updatePresetIfMatching(
-          manifest.presets.find((p) => p.id === "experimentalDark"),
-          settings,
-          {
-            page: "#001533",
-            primary: "#4d97ff",
-            highlightText: "#4d97ff",
-            menuBar: "#4d97ff",
-            activeTab: "#282828",
-            tab: "#192f4d",
-            selector: "#030b16",
-            selector2: "#192f4d",
-            selectorSelection: "#282828",
-            accent: "#282828",
-            input: "#282828",
-            workspace: "#282828",
-            categoryMenu: "#282828",
-            palette: "#333333",
-            fullscreen: "#282828",
-            stageHeader: "#333333",
-            border: "#444444",
-          },
-          2
-        );
+
+      // Transition v1.27.0 to v1.28.0
+      if (addonId === "find-bar" && addonsEnabled["find-bar"] === true) {
+        // Because find-bar was previously with devtools, if the user had devtools disabled, then we should also disable find-bar.
+        if (addonsEnabled["editor-devtools"] === false) {
+          madeChangesToAddon = madeAnyChanges = true;
+          addonsEnabled["find-bar"] = false;
+        }
       }
       if (manifest.settings) {
-        if (
-          addonId === "discuss-button" &&
-          addonsEnabled["discuss-button"] === true &&
-          (settings.buttonName || settings.removeIdeasBtn)
-        ) {
-          // Transition v1.23.0 modes to v1.24.0 settings
-          madeChangesToAddon = true;
-          madeAnyChanges = true;
-
-          let option = manifest.settings.find((option) => option.id === "items");
-          settings.items = [...option.default];
-          settings.items.splice(2, 0, {
-            name: settings.buttonName,
-            url: "/discuss",
-          });
-          if (settings.removeIdeasBtn) settings.items.splice(3, 1);
-
-          delete settings.removeIdeasBtn;
-          delete settings.buttonName;
-        }
-
         for (const option of manifest.settings) {
           if (settings[option.id] === undefined) {
             madeChangesToAddon = true;
