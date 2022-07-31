d42a55ee9a1832762ff89d9002e9eaa59d757415
apple502j <33279053+apple502j@users.noreply.github.com>
2022-07-31 23:26:21 +0900

    Dynamic settings update

--- addons/clones/userscript.js
+++ addons/clones/userscript.js
@@ -1,7 +1,7 @@
 export default async function ({ addon, global, console, msg }) {
   const vm = addon.tab.traps.vm;
 
-  const showIconOnly = addon.settings.get("showicononly");
+  let showIconOnly = addon.settings.get("showicononly");
 
   if (addon.tab.redux.state && addon.tab.redux.state.scratchGui.stageSize.stageSize === "small") {
     document.body.classList.add("sa-clones-small");
@@ -39,10 +39,10 @@ export default async function ({ addon, global, console, msg }) {
     .fill()
     .map((_, i) => msg("clones", { cloneCount: i }));
 
-  function doCloneChecks() {
+  function doCloneChecks(force) {
     const v = vm.runtime._cloneCounter;
     // performance
-    if (v === lastChecked) return;
+    if (v === lastChecked && !force) return;
     countContainerContainer.dataset.count = lastChecked = v;
     if (showIconOnly) {
       count.dataset.str = v;
@@ -53,6 +53,11 @@ export default async function ({ addon, global, console, msg }) {
     if (v === 0) countContainerContainer.style.display = "none";
     else addon.tab.displayNoneWhileDisabled(countContainerContainer, { display: "flex" });
   }
+
+  addon.settings.addEventListener("change", () => {
+    showIconOnly = addon.settings.get("showicononly");
+    doCloneChecks(true);
+  });
 
   vm.runtime.on("targetWasRemoved", (t) => {
     // Fix bug with inaccurate clone counter
