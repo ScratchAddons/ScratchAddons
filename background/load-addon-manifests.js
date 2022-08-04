004446dd8eb30003fb54d8fe1e4a2056bbf066ed
Weredime <100447465+Weredime@users.noreply.github.com>
2022-08-04 10:16:28 +0100

    Don't crash if an addon.json is invalid

--- background/load-addon-manifests.js
+++ background/load-addon-manifests.js
@@ -42,7 +42,13 @@ const localizeSettings = (addonId, setting, tableId) => {
   const useDefault = scratchAddons.l10n.locale.startsWith("en");
   for (const addonId of addonIds) {
     if (addonId.startsWith("//")) continue;
-    const manifest = await (await fetch(`/addons/${addonId}/addon.json`)).json();
+    let manifest;
+    try {
+      manifest = await (await fetch(`/addons/${addonId}/addon.json`)).json();
+    } catch (ex) {
+      console.error(`Failed to load addon manifest for ${addonId}, skipping to next addon:`, ex);
+      continue;
+    }
     let potentiallyNeedsMissingDynamicWarning =
       manifest.updateUserstylesOnSettingsChange && !(manifest.dynamicEnable && manifest.dynamicDisable);
     if (!useDefault) {
