d798490f3fe5a3e4955e8949c48d4f45432ae917
apple502j <33279053+apple502j@users.noreply.github.com>
2022-08-04 16:56:12 +0900

    Add support for updateUserstylesOnSettingsChange

--- addons/discuss-button/r2.js
+++ addons/discuss-button/r2.js
@@ -1,16 +1,45 @@
-export default async function ({ addon, console }) {
-  if (!addon.settings.get("compact-nav")) return;
-  if (addon.tab.clientVersion == "scratch-www") return;
-  if (await addon.auth.fetchIsLoggedIn()) {
-    const username = await addon.auth.fetchUsername();
-    const container = document.querySelector(".dropdown");
-    const dropdown = container.querySelector(".dropdown-menu .user-nav");
-    const profileSpans = dropdown.childNodes[0].childNodes[0];
-    const span = profileSpans.appendChild(document.createElement("span"));
-    span.className = "sa-profile-name";
-    span.textContent = username;
+export default async function ({ addon }) {
+  if (addon.tab.clientVersion === "scratch-www") return;
+  let span;
 
-    // Remove username next to icon.
-    container.firstChild.removeChild(container.firstChild.childNodes[1]);
-  }
+  const add = async () => {
+    if (!span && (await addon.auth.fetchIsLoggedIn())) {
+      const username = await addon.auth.fetchUsername();
+      const container = document.querySelector(".dropdown");
+      const dropdown = container.querySelector(".dropdown-menu .user-nav");
+      const profileSpans = dropdown.childNodes[0].childNodes[0];
+      span = profileSpans.appendChild(document.createElement("span"));
+      span.className = "sa-profile-name";
+      span.textContent = username;
+
+      // Remove username next to icon.
+      container.firstChild.childNodes[1].textContent = "";
+    }
+  };
+  const remove = async () => {
+    const container = document.querySelector(".dropdown");
+    if (container) {
+      const username = await addon.auth.fetchUsername();
+      container.firstChild.childNodes[1].textContent = username;
+    }
+    span?.remove();
+    span = null;
+  };
+
+  if (addon.settings.get("compact-nav")) add();
+  addon.settings.addEventListener("change", () => {
+    if (addon.settings.get("compact-nav")) {
+      add();
+    } else {
+      remove();
+    }
+  });
+  addon.self.addEventListener("disabled", () => remove());
+  addon.self.addEventListener("reenabled", () => {
+    if (addon.settings.get("compact-nav")) {
+      add();
+    } else {
+      remove();
+    }
+  });
 }
