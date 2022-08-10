3a912cbdb6110d6a280957b73bb729a2d02bf90b
CST1229 <68464103+CST1229@users.noreply.github.com>
2022-08-10 19:03:15 +0200

    get the replacement glow filter id from userscript
    beecause it can change

--- addons/zebra-striping/userscript.js
+++ addons/zebra-striping/userscript.js
@@ -2,6 +2,7 @@ export default async function ({ addon, msg, global, console }) {
   const vm = addon.tab.traps.vm;
   const scratchBlocks = await addon.tab.traps.getBlockly();
   const _render = scratchBlocks.BlockSvg.prototype.render;
+  
   scratchBlocks.BlockSvg.prototype.render = function (opt_bubble) {
     if (!this.isInFlyout && !this.isShadow()) {
       let block = this;
@@ -42,5 +43,23 @@ export default async function ({ addon, msg, global, console }) {
 
   if (vm.editingTarget) {
     vm.emitWorkspaceUpdate();
+  }
+  
+  // The replacement glow filter's ID is randomly generated and changes
+  // when the workspace is reloaded (which includes loading the page and
+  // seeing the project page then seeing inside).
+  // As we need to stack the filter with the striping filter in the
+  // userstyle, we need to use the usersciript to get the filter's ID
+  // and set a CSS variable on the document's root.
+  while (true) {
+  const replacementGlowEl = await addon.tab.waitForElement(
+	'filter[id*="blocklyReplacementGlowFilter"]', {
+		markAsSeen: true
+	}
+  );
+	document.documentElement.style.setProperty(
+	"--zebraStriping-replacementGlow",
+	`url(#${replacementGlowEl.id})`
+	);
   }
 }
