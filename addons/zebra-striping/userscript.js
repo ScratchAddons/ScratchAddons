568ffa77ea3dc214109dfa3f3e726d91deac10a3
TheColaber <72760579+TheColaber@users.noreply.github.com>
2022-08-09 20:19:49 -0400

    Whow, an easier way to rewrite, thats crazy

--- addons/zebra-striping/userscript.js
+++ addons/zebra-striping/userscript.js
@@ -3,29 +3,25 @@ export default async function ({ addon, msg, global, console }) {
   const scratchBlocks = await addon.tab.traps.getBlockly();
   const _render = scratchBlocks.BlockSvg.prototype.render;
   scratchBlocks.BlockSvg.prototype.render = function (opt_bubble) {
-    console.log(this.isShadow_);
     if (!this.isInFlyout && !this.isShadow_) {
       let block = this;
-      while (block.getSurroundParent() && block.getSurroundParent().type === block.type) {
+      while (block.getSurroundParent() && block.getSurroundParent().category_ === block.category_) {
         block = block.getSurroundParent();
       }
 
-      function zebra(block, z) {
-        if (block.zebra && !z) {
-          block.svgPath_.classList.remove("sa-zebra-stripe");
-        }
-        block.zebra = z;
-        if (z) {
-          block.svgPath_.classList.add("sa-zebra-stripe");
-        }
-        for (const b of block.childBlocks_) {
-          const parent = b.getSurroundParent();
-          if (parent && !b.isShadow_ && parent.type === b.type) {
-            zebra(b, !parent.zebra);
+      for (const b of block.getDescendants()) {
+        const parent = b.getSurroundParent();
+        if ((!b.isShadow_ && !parent) || (parent && parent.category_ === b.category_)) {
+          const zebra = parent ? !parent.zebra : false;
+          if (!zebra && b.zebra) {
+            b.svgPath_.classList.remove("sa-zebra-stripe");
           }
+          if (zebra && !b.zebra) {
+            b.svgPath_.classList.add("sa-zebra-stripe");
+          }
+          b.zebra = zebra;
         }
       }
-      zebra(block, false);
     }
     _render.call(this, opt_bubble);
   };
