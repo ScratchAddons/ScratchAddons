7f55931bd214615ee73859ccfe1d96d61cdb0857
TheColaber <72760579+TheColaber@users.noreply.github.com>
2022-08-12 18:30:40 -0400

    Create a new tab to put error in the dev's face

--- background/load-addon-manifests.js
+++ background/load-addon-manifests.js
@@ -47,6 +47,9 @@ const localizeSettings = (addonId, setting, tableId) => {
       manifest = await (await fetch(`/addons/${addonId}/addon.json`)).json();
     } catch (ex) {
       console.error(`Failed to load addon manifest for ${addonId}, skipping to next addon:`, ex);
+      chrome.tabs.create({
+        url: `data:text/plain,Scratch Addons crashed: invalid addon.json for addon with id ${addonId}. Click the "Errors" button on the extension tile for more details.`,
+      });
       continue;
     }
     let potentiallyNeedsMissingDynamicWarning =
