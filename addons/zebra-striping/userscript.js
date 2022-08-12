ScratchAddons/ScratchAddons

commit 0070f8f893231e6b8536147e5ba5d0fb7e1c9f8d

Remove worst case O(n^2) time complexity
This fixes a performance regression when editing projects with deeply
nested scripts. The problem with the old logic is that when you edit
something deep into a script, that block and every parent will each each
re-stripe everything from the top down, which is unnecessary and has
O(n^2) time complexity (although it isn't noticeable issues until blocks
are nested >100 levels deep).
The fix is to re-stripe everything exactly once, which is now the
responsibility of the "top" block.
As a side-effect, I think the code is more straightforward and no longer
mutates block objects (except the render trap)

addons/zebra-striping/userscript.js
@@ -2,39 +2,77 @@ export default async function ({ addon, msg, global, console }) {
   const vm = addon.tab.traps.vm;
   const scratchBlocks = await addon.tab.traps.getBlockly();
 
+  /**
+   * Returns the direct children of a block and the siblings of those children.
+   * For the script:
+   * repeat ((1) + (2)) {
+   *   move (10) steps
+   *   repeat (5) {
+   *     ...
+   *   }
+   * }
+   * say (Hello)
+   * Calling this function on the top repeat block would return:
+   *  - the addition block (but not its inputs)
+   *  - the move steps block (but not its inputs)
+   *  - the second repeat block (but not its inputs or children)
+   * @returns {unknown[]}
+   */
+  const getImmediateDescendants = (block) => {
+    const blocks = [];
+    // getChildren() will also return the next block which we don't want to include
+    const blockNext = block.getNextBlock();
+    for (let childBlock of block.getChildren()) {
+      while (childBlock && childBlock !== blockNext) {
+        blocks.push(childBlock);
+        childBlock = childBlock.getNextBlock();
+      }
+    }
+    return blocks;
+  };
+
   const originalRender = scratchBlocks.BlockSvg.prototype.render;
   scratchBlocks.BlockSvg.prototype.render = function (opt_bubble) {
     if (!this.isInFlyout && !this.isShadow()) {
-      let top = this;
-      while (top.getSurroundParent() && top.getSurroundParent().getCategory() === top.getCategory()) {
-        top = top.getSurroundParent();
-      }
+      const surroundingParent = this.getSurroundParent();
+      const category = this.getCategory();
+      const isTop = !surroundingParent || (surroundingParent.getCategory() !== category);
 
-      for (const block of top.getDescendants()) {
-        const parent = block.getSurroundParent();
+      // The "top block" is responsible for determining which of its children should be striped.
+      // All modifications that can change whether a block should be striped should bubble up to the
+      // top block.
+      if (isTop) {
+        let zebra = false;
+        let blocks = [this];
+        let nextBlocks = [];
 
-        const zebra = parent ? (block.isShadow() ? parent.zebra : !parent.zebra) : false;
-        if (block.isShadow() || !parent || (parent && parent.getCategory() === block.getCategory())) {
-          const els = [block.svgPath_];
-          for (const input of block.inputList) {
-            if (input.outlinePath) {
-              els.push(input.outlinePath);
-            }
-            for (const field of input.fieldRow) {
-              if (field.fieldGroup_) {
-                els.push(field.fieldGroup_);
+        while (blocks.length) {
+          for (const block of blocks) {
+            for (const child of getImmediateDescendants(block)) {
+              if (!child.isShadow() && child.getCategory() === category) {
+                nextBlocks.push(child);
               }
             }
-          }
-          for (const el of els) {
-            if (!zebra && block.zebra) {
-              el.classList.remove("sa-zebra-stripe");
+
+            const stripedElements = [block.svgPath_];
+            for (const input of block.inputList) {
+              if (input.outlinePath) {
+                stripedElements.push(input.outlinePath);
+              }
+              for (const field of input.fieldRow) {
+                if (field.fieldGroup_) {
+                  stripedElements.push(field.fieldGroup_);
+                }
+              }
             }
-            if (zebra && !block.zebra) {
-              el.classList.add("sa-zebra-stripe");
+            for (const el of stripedElements) {
+              el.classList.toggle("sa-zebra-stripe", zebra);
             }
           }
-          block.zebra = zebra;
+
+          zebra = !zebra;
+          blocks = nextBlocks;
+          nextBlocks = [];
         }
       }
     }

commit d4e7834d5a46332b82fec7ad65e8020815485ea4

Unbreak shadow striping
Broken by 0070f8f893231e6b8536147e5ba5d0fb7e1c9f8d

addons/zebra-striping/userscript.js
@@ -47,9 +47,14 @@ export default async function ({ addon, msg, global, console }) {
         let nextBlocks = [];
 
         while (blocks.length) {
-          for (const block of blocks) {
+          // Use old-style loop because we add items as we iterate
+          for (let i = 0; i < blocks.length; i++) {
+            const block = blocks[i];
+
             for (const child of getImmediateDescendants(block)) {
-              if (!child.isShadow() && child.getCategory() === category) {
+              if (child.isShadow()) {
+                blocks.push(child);
+              } else if (child.getCategory() === category) {
                 nextBlocks.push(child);
               }
             }