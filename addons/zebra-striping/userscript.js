ScratchAddons/ScratchAddons

commit 0070f8f893231e6b8536147e5ba5d0fb7e1c9f8d

Remove worst case O(n^2) time complexity
This fixes a performance regression when editing projects with deeply
nested scripts. The problem with the old logic is that when you edit
something deep into a script, that block and every parent will each each
re-stripe everything from the top down, which is unnecessary and has
O(n^2) time complexity (although it isn't noticeable issues until blocks
are nested >100 levels deep).
The fix is to re-stripe everything exactly once, which is now the
responsibility of the "top" block.
As a side-effect, I think the code is more straightforward and no longer
mutates block objects (except the render trap)

addons/zebra-striping/userscript.js
@@ -2,39 +2,77 @@ export default async function ({ addon, msg, global, console }) {
   const vm = addon.tab.traps.vm;
   const scratchBlocks = await addon.tab.traps.getBlockly();
 
+  /**
+   * Returns the direct children of a block and the siblings of those children.
+   * For the script:
+   * repeat ((1) + (2)) {
+   *   move (10) steps
+   *   repeat (5) {
+   *     ...
+   *   }
+   * }
+   * say (Hello)
+   * Calling this function on the top repeat block would return:
+   *  - the addition block (but not its inputs)
+   *  - the move steps block (but not its inputs)
+   *  - the second repeat block (but not its inputs or children)
+   * @returns {unknown[]}
+   */
+  const getImmediateDescendants = (block) => {
+    const blocks = [];
+    // getChildren() will also return the next block which we don't want to include
+    const blockNext = block.getNextBlock();
+    for (let childBlock of block.getChildren()) {
+      while (childBlock && childBlock !== blockNext) {
+        blocks.push(childBlock);
+        childBlock = childBlock.getNextBlock();
+      }
+    }
+    return blocks;
+  };
+
   const originalRender = scratchBlocks.BlockSvg.prototype.render;
   scratchBlocks.BlockSvg.prototype.render = function (opt_bubble) {
     if (!this.isInFlyout && !this.isShadow()) {
-      let top = this;
-      while (top.getSurroundParent() && top.getSurroundParent().getCategory() === top.getCategory()) {
-        top = top.getSurroundParent();
-      }
+      const surroundingParent = this.getSurroundParent();
+      const category = this.getCategory();
+      const isTop = !surroundingParent || (surroundingParent.getCategory() !== category);
 
-      for (const block of top.getDescendants()) {
-        const parent = block.getSurroundParent();
+      // The "top block" is responsible for determining which of its children should be striped.
+      // All modifications that can change whether a block should be striped should bubble up to the
+      // top block.
+      if (isTop) {
+        let zebra = false;
+        let blocks = [this];
+        let nextBlocks = [];
 
-        const zebra = parent ? (block.isShadow() ? parent.zebra : !parent.zebra) : false;
-        if (block.isShadow() || !parent || (parent && parent.getCategory() === block.getCategory())) {
-          const els = [block.svgPath_];
-          for (const input of block.inputList) {
-            if (input.outlinePath) {
-              els.push(input.outlinePath);
-            }
-            for (const field of input.fieldRow) {
-              if (field.fieldGroup_) {
-                els.push(field.fieldGroup_);
+        while (blocks.length) {
+          for (const block of blocks) {
+            for (const child of getImmediateDescendants(block)) {
+              if (!child.isShadow() && child.getCategory() === category) {
+                nextBlocks.push(child);
               }
             }
-          }
-          for (const el of els) {
-            if (!zebra && block.zebra) {
-              el.classList.remove("sa-zebra-stripe");
+
+            const stripedElements = [block.svgPath_];
+            for (const input of block.inputList) {
+              if (input.outlinePath) {
+                stripedElements.push(input.outlinePath);
+              }
+              for (const field of input.fieldRow) {
+                if (field.fieldGroup_) {
+                  stripedElements.push(field.fieldGroup_);
+                }
+              }
             }
-            if (zebra && !block.zebra) {
-              el.classList.add("sa-zebra-stripe");
+            for (const el of stripedElements) {
+              el.classList.toggle("sa-zebra-stripe", zebra);
             }
           }
-          block.zebra = zebra;
+
+          zebra = !zebra;
+          blocks = nextBlocks;
+          nextBlocks = [];
         }
       }
     }

commit 3e261205c5ddae1a67987ece033021225007943e

New addon: Alternating nested block colors (#4881)
* Da zebra srtriping addon

Also remove the hungarian _locales folder that somehow got here for some reason

* Format code

* stripeStyling, intensity option

* Format code

* add selective striping; misc changes

* Format code

* rename addon

* use more blockly + runatcomplete
also bugs probably

* Format code

* Throw away the VM and use only Blockly for calculating striping

* What if we compare colors instead of opcode extensions

:thinking:

* Update comment

* Update comment again

* Format code

* block.getSurroundParent() exists

thanks me for reading the old striping addon's code so that i could find out about this function

also, replace some other _ variable gets with function calls

* Format code

* Stripe by polluting blockly

* Whow, an easier way to rewrite, thats crazy

* Make sure to stipe the inputs

* use functions instead of _-suffixed variables

* get the replacement glow filter id from userscript

beecause it can change

* Format code

* Update addon.json

* code style nitpicks

* Revert "Update addon.json"

This reverts commit f4d061a090eb749f32344d1c6c334b849f1d8d10.

vm is not defined if you run too early

* Remove worst case O(n^2) time complexity

This fixes a performance regression when editing projects with deeply
nested scripts. The problem with the old logic is that when you edit
something deep into a script, that block and every parent will each each
re-stripe everything from the top down, which is unnecessary and has
O(n^2) time complexity (although it isn't noticeable issues until blocks
are nested >100 levels deep).
The fix is to re-stripe everything exactly once, which is now the
responsibility of the "top" block.
As a side-effect, I think the code is more straightforward and no longer
mutates block objects (except the render trap)

* Reduce duplication in CSS

* Improve editor-stepping and debugger compatibility

* Add redux events to blocklyReplacementGlowFilter finder

Minor performance improvement

* Unbreak shadow striping

Broken by 0070f8f893231e6b8536147e5ba5d0fb7e1c9f8d

addons/zebra-striping/userscript.js
@@ -1,0 +1,65 @@
+export default async function ({ addon, msg, global, console }) {
+  const vm = addon.tab.traps.vm;
+  const ScratchBlocks = await addon.tab.traps.getBlockly();
+
+  const originalRender = ScratchBlocks.BlockSvg.prototype.render;
+  ScratchBlocks.BlockSvg.prototype.render = function (opt_bubble) {
+    // Any changes that affect block striping should bubble to the top block of the script.
+    // The top block of the script is responsible for striping all of its children.
+    // This way stripes are computed exactly once.
+    if (!this.isInFlyout && !this.isShadow() && this.getParent() === null) {
+      const stripeState = new Map();
+      // Conveniently getDescendants() returns blocks in an order such that each block's
+      // parent will always come before that block (except the first block which has no
+      // parent).
+      for (const block of this.getDescendants()) {
+        const parent = block.getSurroundParent();
+
+        let isStriped = false;
+        if (parent) {
+          if (block.isShadow()) {
+            isStriped = !!stripeState.get(parent);
+          } else if (parent.getCategory() === block.getCategory()) {
+            isStriped = !stripeState.get(parent);
+          }
+        }
+        stripeState.set(block, isStriped);
+
+        const elements = [block.svgPath_];
+        for (const input of block.inputList) {
+          if (input.outlinePath) {
+            elements.push(input.outlinePath);
+          }
+          for (const field of input.fieldRow) {
+            if (field.fieldGroup_) {
+              elements.push(field.fieldGroup_);
+            }
+          }
+        }
+        for (const el of elements) {
+          el.classList.toggle("sa-zebra-stripe", isStriped);
+        }
+      }
+    }
+    return originalRender.call(this, opt_bubble);
+  };
+
+  if (vm.editingTarget) {
+    vm.emitWorkspaceUpdate();
+  }
+
+  // The replacement glow filter's ID is randomly generated and changes
+  // when the workspace is reloaded (which includes loading the page and
+  // seeing the project page then seeing inside).
+  // As we need to stack the filter with the striping filter in the
+  // userstyle, we need to use the usersciript to get the filter's ID
+  // and set a CSS variable on the document's root.
+  while (true) {
+    const replacementGlowEl = await addon.tab.waitForElement('filter[id*="blocklyReplacementGlowFilter"]', {
+      markAsSeen: true,
+      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
+      reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
+    });
+    document.documentElement.style.setProperty("--zebraStriping-replacementGlow", `url(#${replacementGlowEl.id})`);
+  }
+}

addons/zebra-striping/userstyle.css
@@ -1,0 +1,26 @@
+:root {
+  --zebraStriping-percent: calc(var(--zebraStriping-shadeNumber) * var(--zebraStriping-intensity) / 100);
+  --zebraStriping-bright: calc(1 + var(--zebraStriping-percent));
+  --zebraStriping-saturate: calc(1 - var(--zebraStriping-percent));
+  --zebraStriping-filter: brightness(var(--zebraStriping-bright)) saturate(var(--zebraStriping-saturate));
+  /* "Temporary" value, gets replaced by userscript */
+  --zebraStriping-replacementGlow: url(#blocklyReplacementGlowFilter);
+}
+
+.sa-zebra-stripe {
+  filter: var(--zebraStriping-filter);
+}
+
+.sa-zebra-stripe[filter*="#blocklyReplacementGlowFilter"] {
+  filter: var(--zebraStriping-filter) var(--zebraStriping-replacementGlow);
+}
+
+/* editor-stepping and debugger compatibility */
+/* this is pretty horrible */
+.sa-zebra-stripe[style*="#sa_glower_filter0"] {
+  /* !important because editor-stepping Highlighter uses inline styles */
+  filter: var(--zebraStriping-filter) url("#sa_glower_filter0") !important;
+}
+.sa-zebra-stripe[style*="#sa_glower_filter1"] {
+  filter: var(--zebraStriping-filter) url("#sa_glower_filter1") !important;
+}