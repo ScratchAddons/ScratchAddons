6ac604ed6dcd94cffc76f6b3cda9d1f4a4aca42a
Error <error2000gamer@gmail.com>
2022-08-17 16:20:04 -0700

    Color-Picker

--- addons/color-picker/userscript.js
+++ addons/color-picker/userscript.js
@@ -1,11 +1,10 @@
+import "../../libraries/thirdparty/cs/tinycolor-min.js";
 import codeEditorHandler from "./code-editor.js";
 import paintEditorHandler from "./paint-editor.js";
 
 // Load tinycolor here, and execute code after that
 // Note that we don't await other scripts (they block!)
 export default async (api) => {
-  const { addon } = api;
-  await addon.tab.loadScript(addon.self.lib + "/thirdparty/cs/tinycolor-min.js");
   codeEditorHandler(api);
   paintEditorHandler(api);
 };
