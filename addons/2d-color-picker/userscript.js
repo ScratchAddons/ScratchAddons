72def2b66674de7c34bd61e931d32e9346e02512
Error <error2000gamer@gmail.com>
2022-08-17 16:06:49 -0700

    Consolidate. Why does there need to be two files?

--- addons/2d-color-picker/userscript.js
+++
@@ -1,7 +0,0 @@
-import paintEditorHandler from "./paint-editor.js";
-
-export default async (api) => {
-  const { addon } = api;
-  await addon.tab.loadScript(addon.self.lib + "/thirdparty/cs/tinycolor-min.js");
-  paintEditorHandler(api);
-};
