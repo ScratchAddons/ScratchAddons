a989317214696b0829a3082a984b5f1864cafe86
Error <error2000gamer@gmail.com>
2022-08-17 16:28:17 -0700

    performance addon

--- addons/debugger/performance.js
+++ addons/debugger/performance.js
@@ -4,8 +4,6 @@ import { onPauseChanged, isPaused } from "./module.js";
 export default async function createPerformanceTab({ debug, addon, console, msg }) {
   const vm = addon.tab.traps.vm;
 
-  await addon.tab.loadScript(addon.self.lib + "/thirdparty/cs/chart.min.js");
-
   const tab = debug.createHeaderTab({
     text: msg("tab-performance"),
     icon: addon.self.dir + "/icons/performance.svg",
