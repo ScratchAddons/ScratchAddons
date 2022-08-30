1123ff7c32b0931ae2ae7b9aa5db43d862d3698c
CST1229 <68464103+CST1229@users.noreply.github.com>
2022-08-30 14:44:22 +0200

    square text and color inputs addon

---
+++ addons/editor-square-inputs/userscript.js
@@ -0,0 +1,47 @@
+export default async function ({ addon, msg, global, console }) {
+	const ScratchBlocks = await addon.tab.traps.getBlockly();
+	const vm = addon.tab.traps.vm;
+	
+	const opcodeToSettings = {
+		text: "text",
+		argument_editor_string_number: "text",
+		colour_picker: "color",
+	};
+	
+	const originalJsonInit = ScratchBlocks.BlockSvg.prototype.jsonInit;
+	
+	ScratchBlocks.BlockSvg.prototype.jsonInit = function(json) {
+		if (
+			!addon.self.disabled &&
+			opcodeToSettings[this.type] && addon.settings.get(opcodeToSettings[this.type])
+		) {
+			originalJsonInit.call(this, {
+				...json,
+				outputShape: ScratchBlocks.OUTPUT_SHAPE_SQUARE,
+			});
+		} else {
+			originalJsonInit.call(this, json);
+		}
+	}
+	
+	// taken from custom-block-shape
+	function updateAllBlocks() {
+      const workspace = ScratchBlocks.getMainWorkspace();
+      if (workspace) {
+        if (vm.editingTarget) {
+          vm.emitWorkspaceUpdate();
+        }
+        const flyout = workspace.getFlyout();
+        if (flyout) {
+          const flyoutWorkspace = flyout.getWorkspace();
+          ScratchBlocks.Xml.clearWorkspaceAndLoadFromXml(ScratchBlocks.Xml.workspaceToDom(flyoutWorkspace), flyoutWorkspace);
+          workspace.getToolbox().refreshSelection();
+          workspace.toolboxRefreshEnabled_ = true;
+        }
+      }
+    }
+	
+	addon.self.addEventListener("disabled", updateAllBlocks);
+	addon.self.addEventListener("reenabled", updateAllBlocks);
+	addon.settings.addEventListener("change", updateAllBlocks);
+}
