0aadf299b21dd21565cf21d8d89cf0bcf0ef2bb4
TheColaber <72760579+TheColaber@users.noreply.github.com>
2022-08-31 21:04:34 -0400

    comment-linebreaks: make cache code better

--- addons/comments-linebreaks/userscript.js
+++ addons/comments-linebreaks/userscript.js
@@ -1,28 +1,27 @@
 import formatProfileComments from "../../libraries/common/cs/format-profile-comments.js";
 
 export default async function ({ addon, global, console }) {
-  const cache = Object.create(null);
+  const cache = new Map();
   addon.self.addEventListener("disabled", () => {
-    for (const comment of Object.keys(cache)) {
-      const cached = cache[comment];
-      cached.comment.innerHTML = "";
-      cached.comment.style.whiteSpace = "nowrap";
+    cache.forEach((content, comment) => {
+      comment.innerHTML = "";
+      comment.style.whiteSpace = "nowrap";
 
-      for (const elm of cached.content.childNodes) {
+      for (const elm of content.childNodes) {
         const cloned = elm.cloneNode(true);
-        cached.comment.appendChild(cloned);
+        comment.appendChild(cloned);
       }
-    }
+    });
   });
   addon.self.addEventListener("reenabled", () => {
-    for (const comment of Object.keys(cache)) {
-      cache[comment].comment.style.whiteSpace = "break-spaces";
-      formatProfileComments(cache[comment].comment);
-    }
+    cache.forEach((content, comment) => {
+      comment.style.whiteSpace = "break-spaces";
+      formatProfileComments(comment);
+    });
   });
   while (true) {
     const comment = await addon.tab.waitForElement(".comment .content", { markAsSeen: true });
-    cache[comment.parentElement.parentElement.id] = { comment, content: comment.cloneNode(true) };
+    cache.set(comment, comment.cloneNode(true));
     if (!addon.self.disabled) {
       comment.style.whiteSpace = "break-spaces";
       formatProfileComments(comment);
