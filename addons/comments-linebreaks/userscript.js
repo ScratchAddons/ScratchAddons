8820926a60f6f844d8f3b337ada467295624f9c9
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-09-10 21:08:34 -0400

    Make almost everything dynamic
    except enabling/disabling scrollbars on scratch-www

--- addons/comments-linebreaks/userscript.js
+++ addons/comments-linebreaks/userscript.js
@@ -15,20 +15,32 @@ export default async function ({ addon, global, console }) {
   });
   addon.self.addEventListener("reenabled", () => {
     cache.forEach((content, comment) => {
-      comment.style.whiteSpace = "break-spaces";
-      formatProfileComments(comment);
+      fixComment(comment);
     });
   });
+  addon.settings.addEventListener("change", () => {
+    cache.forEach((content, comment) => {
+      fixComment(comment);
+    });
+  });
+
+  function fixComment(c) {
+    if (!addon.self.disabled) {
+      if (addon.settings.get("scrollbars")) {
+        let height = addon.settings.get("height") * 20 + "px";
+        c.style.maxHeight = height;
+        c.style.overflow = "auto";
+      } else {
+        c.style.maxHeight = "none";
+      }
+      c.style.whiteSpace = "break-spaces";
+      formatProfileComments(c);
+    }
+  }
+
   while (true) {
     const comment = await addon.tab.waitForElement(".comment .content", { markAsSeen: true });
     cache.set(comment, comment.cloneNode(true));
-    if (!addon.self.disabled) {
-      if (addon.settings.get("scrollbars")) {
-        comment.style.maxHeight = addon.settings.get("height") * 20 + "px";
-        comment.style.overflow = "auto";
-      }
-      comment.style.whiteSpace = "break-spaces";
-      formatProfileComments(comment);
-    }
+    fixComment(comment);
   }
 }
