71d2bd1e7d7637ea6b03b067668679bd814843e5
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2022-09-28 22:00:58 +0000

    Format code

--- addons/isonline/profile.js
+++ addons/isonline/profile.js
@@ -1,8 +1,8 @@
 import { isOnline, getProjectId } from "./common.js";
 export default async function ({ addon, global, console, msg }) {
   console.log("hi");
-  let username = window.location.pathname.split("/users/")[1].split('/')[0];
-    let projectId = await getProjectId(username);
+  let username = window.location.pathname.split("/users/")[1].split("/")[0];
+  let projectId = await getProjectId(username);
   if (!projectId) return;
   let online = await isOnline(projectId);
   let usernameText = document.querySelector(".header-text > h2");
