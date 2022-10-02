8e93b1b585531155b11c4f264c07d9a1b7a66bc1
emir4169 <emir4169@users.noreply.github.com>
2022-10-02 11:06:59 +0000

    Format code

--- addons/lookatrandom/oldcode.js
+++ addons/lookatrandom/oldcode.js
@@ -1,22 +1,22 @@
 export default async function ({ addon, global, console }) {
-    const ScratchBlocks = await addon.tab.traps.getBlockly();
-    ScratchBlocks.Blocks.motion_pointtowards_menu.init = function() {
-        this.jsonInit({
-          "message0": "%1",
-          "args0": [
-            {
-              "type": "field_dropdown",
-              "name": "TOWARDS",
-              "options": [
-                [ScratchBlocks.Msg.MOTION_POINTTOWARDS_POINTER, '_mouse_'],
-                [ScratchBlocks.Msg.MOTION_POINTTOWARDS_RANDOM, '_random_']
-              ]
-            }
+  const ScratchBlocks = await addon.tab.traps.getBlockly();
+  ScratchBlocks.Blocks.motion_pointtowards_menu.init = function () {
+    this.jsonInit({
+      message0: "%1",
+      args0: [
+        {
+          type: "field_dropdown",
+          name: "TOWARDS",
+          options: [
+            [ScratchBlocks.Msg.MOTION_POINTTOWARDS_POINTER, "_mouse_"],
+            [ScratchBlocks.Msg.MOTION_POINTTOWARDS_RANDOM, "_random_"],
           ],
-          "colour": ScratchBlocks.Colours.motion.secondary,
-          "colourSecondary": ScratchBlocks.Colours.motion.secondary,
-          "colourTertiary": ScratchBlocks.Colours.motion.tertiary,
-          "extensions": ["output_string"]
-        });
-      }
-  }
+        },
+      ],
+      colour: ScratchBlocks.Colours.motion.secondary,
+      colourSecondary: ScratchBlocks.Colours.motion.secondary,
+      colourTertiary: ScratchBlocks.Colours.motion.tertiary,
+      extensions: ["output_string"],
+    });
+  };
+}
