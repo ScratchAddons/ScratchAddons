50007524ae16a16d258e54e690bd454bc36fe35c
lisa-wolfgang <43426138+lisa-wolfgang@users.noreply.github.com>
2022-10-03 17:35:39 -0500

    Setting to auto-collapse on mouse exit

--- addons/sprite-properties/userscript.js
+++ addons/sprite-properties/userscript.js
@@ -1,4 +1,6 @@
 export default async function ({ addon, global, console }) {
+  const showPropsClass = "sa-show-sprite-properties";
+
   let propertiesPanel = await addon.tab.waitForElement('[class^="sprite-info_sprite-info_"]');
   let spriteContainer = propertiesPanel.parentElement; // also contains sprite grid
   let spriteGrid = await addon.tab.waitForElement('[class^="sprite-selector_items-wrapper_"]');
@@ -10,7 +12,10 @@ export default async function ({ addon, global, console }) {
     let doubleClick = e.detail === 2;
     if (doubleClick) togglePropertiesPanel();
   });
+  // Close properties panel when mouse leaves the entire sprite panel
+  spriteContainer.addEventListener("mouseleave", () => autoHidePanel());
 
+  addon.settings.addEventListener("change", () => autoHidePanel());
   addon.self.addEventListener("disabled", () => togglePropertiesPanel());
   addon.self.addEventListener("reenabled", () => toggleOnLoad());
 
@@ -19,8 +24,13 @@ export default async function ({ addon, global, console }) {
   }
 
   function togglePropertiesPanel() {
-    const showPropsClass = "sa-show-sprite-properties";
     if (!addon.self.disabled) spriteContainer.classList.toggle(showPropsClass);
     else spriteContainer.classList.remove(showPropsClass);
+  }
+
+  function autoHidePanel() {
+    if (addon.settings.get("autoCollapse") && spriteContainer.classList.contains(showPropsClass)) {
+      togglePropertiesPanel();
+    }
   }
 }
