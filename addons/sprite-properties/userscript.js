89f50dc89cb4969af5c03dd7ccdb6e3923de1a06
lisa-wolfgang <43426138+lisa-wolfgang@users.noreply.github.com>
2022-10-03 16:55:00 -0500

    Double-click to toggle panel

---
+++ addons/sprite-properties/userscript.js
@@ -0,0 +1,15 @@
+export default async function ({ addon, global, console }) {
+  let propertiesPanel = await addon.tab.waitForElement('[class^="sprite-info_sprite-info_"]');
+  let spriteContainer = propertiesPanel.parentElement;
+  let spriteGrid = await addon.tab.waitForElement('[class^="sprite-selector_items-wrapper_"]');
+
+  // Add a single event listener on the entire grid to take advantage of event bubbling
+  spriteGrid.addEventListener("click", (e) => {
+    let doubleClick = e.detail === 2;
+    if (doubleClick) togglePropertiesPanel();
+  });
+
+  function togglePropertiesPanel() {
+    spriteContainer.classList.toggle("sa-show-sprite-properties");
+  }
+}
