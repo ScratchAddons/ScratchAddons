3c32186f77c122a42d972eb1ff92788b67b28bfa
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-10-15 14:43:58 -0400

    Volume slider addon (#4734)
    * mute-project: add slider option
    
    * Format code
    
    * rewrite slider code, add a little css
    
    * Format code
    
    * Update description, enable setting by default
    
    * style slider (untested on ff)
    
    * Format code
    
    * Adjust css, set initial vol back to 1
    
    * Format code
    
    * Fix setting dynamic enable bug
    
    * Format code
    
    * Split slider into sepertae addon
    
    Untested with mute-project. Probably incompatible.
    
    * Default volume setting
    
    * Format code
    
    * Update while dragging
    
    This was way easier than I though!
    
    * Remove duplicate icon
    
    * Hide icon with css
    
    * Format code
    
    * Use module
    
    * Fix bugs
    
    * Format code
    
    * Update addons/vol-slider/userscript.js
    
    * Hide slider in small stage
    
    mute icon is never shown in small stage if vol-slider is enabled.
    
    * Format code
    
    * Mute/unmute when clicking speaker icon (like YT)
    
    * Add recommended tag to addon
    
    * Update versionAdded
    
    Co-authored-by: Samq64 <Samq64@users.noreply.github.com>
    Co-authored-by: apple502j <33279053+apple502j@users.noreply.github.com>
    Co-authored-by: WorldLanguages <mundofinky@gmail.com>

--- addons/mute-project/userscript.js
+++ addons/mute-project/userscript.js
@@ -1,27 +1,27 @@
+import { setup, isMuted, setVol, getDefVol } from "../vol-slider/module.js";
+
 export default async function ({ addon, global, console }) {
   const vm = addon.tab.traps.vm;
-  let muted = false;
   let icon = document.createElement("img");
   icon.src = "/static/assets/e21225ab4b675bc61eed30cfb510c288.svg";
   icon.loading = "lazy";
   icon.style.display = "none";
+  icon.className = "sa-mute-icon";
   const toggleMute = (e) => {
     if (!addon.self.disabled && (e.ctrlKey || e.metaKey)) {
       e.cancelBubble = true;
       e.preventDefault();
-      muted = !muted;
-      if (muted) {
-        vm.runtime.audioEngine.inputNode.gain.value = 0;
-        icon.style.display = "block";
-      } else {
-        vm.runtime.audioEngine.inputNode.gain.value = 1;
+      if (isMuted()) {
+        setVol(getDefVol());
         icon.style.display = "none";
+      } else {
+        setVol(0);
+        icon.style.display = "block";
       }
     }
   };
   addon.self.addEventListener("disabled", () => {
-    muted = false;
-    vm.runtime.audioEngine.inputNode.gain.value = 1;
+    setVol(1);
     icon.style.display = "none";
   });
 
@@ -31,6 +31,7 @@ export default async function ({ addon, global, console }) {
       reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
     });
     addon.tab.appendToSharedSpace({ space: "afterStopButton", element: icon, order: 0 });
+    setup(vm);
     button.addEventListener("click", toggleMute);
     button.addEventListener("contextmenu", toggleMute);
   }
