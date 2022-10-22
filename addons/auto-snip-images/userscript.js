24a4af3d40e67d4911c27967d2d78ee1b3eb2df9
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-10-22 20:57:44 +0530

    Create auto-snip-images

---
+++ addons/auto-snip-images/userscript.js
@@ -0,0 +1,21 @@
+export default async function ({ addon }) {
+  const query = addon.settings.get("allPosts") == true ? ".post_body_html > img, blockquote > img" : "blockquote > img";
+
+  document.querySelectorAll(query).forEach(image => {
+
+    let details = document.createElement("details");
+    let summary = document.createElement("summary");
+    let newImage = document.createElement("img");
+
+    newImage.src = image.src;
+    details.appendChild(summary);
+    details.appendChild(newImage);
+    summary.innerText = "Expand image";
+    image.parentElement.insertBefore(details, image);
+    image.remove();
+
+    summary.style.fontStyle = "italic";
+    details.style.cursor = "pointer";
+    details.style.userSelect = "none";
+  })
+}
