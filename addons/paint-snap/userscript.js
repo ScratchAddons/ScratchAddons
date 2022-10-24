4d606d5d2577090f34f387cdbe79cc3d7fce46f1
ErrorGamer2000 <errorgamer2000@outlook.com>
2022-10-24 13:45:12 -0600

    Scale snapping

--- addons/paint-snap/userscript.js
+++ addons/paint-snap/userscript.js
@@ -1,6 +1,7 @@
 import { isSelectTool, updateSelectTool } from "./updateSelectTool.js";
-import { enable, disable, loadSettings } from "./state.js";
+import { enable, disable, loadSettings, setGuideColor, toggle } from "./state.js";
 import { initUI } from "./ui.js";
+import { updateScaleTool } from "./updateScaleTool.js";
 
 /** @type {(api: import("../../addon-api/content-script/typedef").UserscriptUtilities) => Promise<void>} */
 export default async function (api) {
@@ -10,8 +11,14 @@ export default async function (api) {
   loadSettings(addon);
   const paper = await addon.tab.traps.getPaper();
   const [tool] = paper.tools;
+
+  toggle(addon.settings.get("enable-default"));
+  setGuideColor(addon.settings.get("guide-color"));
+  addon.settings.addEventListener("change", () => setGuideColor(addon.settings.get("guide-color")));
+
   if (isSelectTool(tool)) {
-    updateSelectTool(paper, tool, addon.settings);
+    updateSelectTool(paper, tool);
+    updateScaleTool(paper, tool);
   }
   initUI(api);
 }
