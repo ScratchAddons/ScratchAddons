5953127a69300e53fd389133b7f206db23918115
Steve0Greatness <Steve0Greatness@users.noreply.github.com>
2022-11-01 18:16:14 +0000

    Format code

--- addons/share-through-mystuff/userscript.js
+++ addons/share-through-mystuff/userscript.js
@@ -5,7 +5,8 @@ export default async function ({ addon, global, console }) {
     if (!confirmation) return;
     e.target.parentElement.querySelector("media-share").click();
   }
-  function make(display) { // function that creates the share button
+  function make(display) {
+    // function that creates the share button
     let share_link = document.createElement("a");
     share_link.href = "#";
     share_link.classList.add("media-share");
@@ -23,27 +24,27 @@ export default async function ({ addon, global, console }) {
     return container;
   }
   var next_button = document.querySelector(next_button_selector);
-  async function run(runs, elements) { // main script
+  async function run(runs, elements) {
+    // main script
     let not_shared = document.querySelectorAll(".media-item-content.not-shared .media-action div")[runs];
-    if (!not_shared.hasAttribute("data-sa_mystuff_sharing_added"))
-      not_shared.appendChild(make("Share"));
+    if (!not_shared.hasAttribute("data-sa_mystuff_sharing_added")) not_shared.appendChild(make("Share"));
     not_shared.dataset.sa_mystuff_sharing_added = 1;
-    if (runs + 1 !== elements)
-      run(runs + 1, elements);
+    if (runs + 1 !== elements) run(runs + 1, elements);
   }
-  async function basics() { // setup for new runs of the script
+  async function basics() {
+    // setup for new runs of the script
     await addon.tab.waitForElement(".media-item-content.not-shared");
     let elements = document.querySelectorAll(".media-item-content.not-shared").length;
     next_button = document.querySelector(next_button_selector);
-    next_button.addEventListener("click", event)
+    next_button.addEventListener("click", event);
     addon.tab.addEventListener("urlChange", event);
     run(0, elements);
   }
   function event(wait = 500) {
-    console.log("Event Triggered")
+    console.log("Event Triggered");
     setTimeout(basics, wait); // allows for time to load
   }
   addon.tab.addEventListener("urlChange", event);
-  next_button.addEventListener("click", () => event(1500))
+  next_button.addEventListener("click", () => event(1500));
   basics();
 }
