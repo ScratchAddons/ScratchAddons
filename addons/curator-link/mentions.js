d78bf0a56ca5eade2f30717e2f122e1df977a784
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-11-30 18:45:53 -0500

    merge into one userscript

--- addons/curator-link/mentions.js
+++
@@ -1,8 +0,0 @@
-import { pingifyTextNode } from "../../libraries/common/cs/fast-linkify.js";
-
-export default async function ({ addon, console }) {
-  while (true) {
-    let post = await addon.tab.waitForElement(".post_body_html", { markAsSeen: true });
-    pingifyTextNode(post);
-  }
-}
--- addons/curator-link/script.js
+++ addons/curator-link/script.js
@@ -1,15 +1,26 @@
 import { escapeHTML } from "../../libraries/common/cs/autoescaper.js";
-export default async function ({ addon }) {
-  const heading = await addon.tab.waitForElement(".inner.mod-splash:nth-child(2) h4", {
-    reduxCondition: (state) => state.splash.featured.status === "FETCHED",
-    reduxEvents: ["SET_FETCH_STATUS", "SET_ROWS"],
-  });
+import { pingifyTextNode } from "../../libraries/common/cs/fast-linkify.js";
 
-  const curator = addon.tab.redux.state.splash.featured.rows.curator_top_projects[0].curator_name;
-  const link = document.createElement("a");
-  link.textContent = curator;
-  link.href = `https://scratch.mit.edu/users/${curator}/`;
-  link.id = "curator-link";
+export default async function ({ addon }) {
+  if (addon.settings.curator) {
+    const heading = await addon.tab.waitForElement(".inner.mod-splash:nth-child(2) h4", {
+      reduxCondition: (state) => state.splash.featured.status === "FETCHED",
+      reduxEvents: ["SET_FETCH_STATUS", "SET_ROWS"],
+    });
+  
+    const curator = addon.tab.redux.state.splash.featured.rows.curator_top_projects[0].curator_name;
+    const link = document.createElement("a");
+    link.textContent = curator;
+    link.href = `https://scratch.mit.edu/users/${curator}/`;
+    link.id = "curator-link";
+  }
+
+  if (addon.settings.mentions) {
+    while (true) {
+      let post = await addon.tab.waitForElement(".post_body_html", { markAsSeen: true });
+      pingifyTextNode(post);
+    }
+  }
 
   if (!addon.self.disabled) {
     heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
