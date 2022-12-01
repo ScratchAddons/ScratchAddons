6fb541a02929543749098597d61ecbf837732eb4
Samq64 <81489795+Samq64@users.noreply.github.com>
2022-11-30 19:31:05 -0500

    Fix errors

--- addons/curator-link/script.js
+++ addons/curator-link/script.js
@@ -2,7 +2,8 @@ import { escapeHTML } from "../../libraries/common/cs/autoescaper.js";
 import { pingifyTextNode } from "../../libraries/common/cs/fast-linkify.js";
 
 export default async function ({ addon }) {
-  if (addon.settings.curator) {
+  if (window.location.href === "https://scratch.mit.edu/" && addon.settings.get("curator")) {
+    console.log("ak")
     const heading = await addon.tab.waitForElement(".inner.mod-splash:nth-child(2) h4", {
       reduxCondition: (state) => state.splash.featured.status === "FETCHED",
       reduxEvents: ["SET_FETCH_STATUS", "SET_ROWS"],
@@ -13,31 +14,29 @@ export default async function ({ addon }) {
     link.textContent = curator;
     link.href = `https://scratch.mit.edu/users/${curator}/`;
     link.id = "curator-link";
-  }
 
-  if (addon.settings.mentions) {
+    if (!addon.self.disabled) {
+      heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
+        "{curatorId}",
+        link.outerHTML
+      );
+    }
+    addon.self.addEventListener("disabled", () => {
+      heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
+        "{curatorId}",
+        curator
+      );
+    });
+    addon.self.addEventListener("reenabled", () => {
+      heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
+        "{curatorId}",
+        link.outerHTML
+      );
+    });
+  } else if (addon.settings.get("mentions")) {
     while (true) {
       let post = await addon.tab.waitForElement(".post_body_html", { markAsSeen: true });
       pingifyTextNode(post);
     }
   }
-
-  if (!addon.self.disabled) {
-    heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
-      "{curatorId}",
-      link.outerHTML
-    );
-  }
-  addon.self.addEventListener("disabled", () => {
-    heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
-      "{curatorId}",
-      curator
-    );
-  });
-  addon.self.addEventListener("reenabled", () => {
-    heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
-      "{curatorId}",
-      link.outerHTML
-    );
-  });
 }
