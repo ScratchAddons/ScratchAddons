0b8fd3c4780408b96aec44c4bed17f0ba50eb523
TheColaber <72760579+TheColaber@users.noreply.github.com>
2022-12-05 16:04:24 -0500

    Remove default values in flyout

---
+++ addons/remove-default-flyout/userscript.js
@@ -0,0 +1,20 @@
+export default async function ({ addon, msg, global, console }) {
+  const ScratchBlocks = await addon.tab.traps.getBlockly();
+  const oldSetValue = ScratchBlocks.Field.prototype.setValue;
+  ScratchBlocks.Field.prototype.setValue = function (newValue) {
+    if (!addon.self.disabled && this instanceof ScratchBlocks.FieldTextInput) {
+      newValue = "";
+    }
+    oldSetValue.call(this, newValue);
+  };
+
+  if (addon.self.enabledLate) {
+    updateToolbox();
+  }
+  addon.self.addEventListener("disabled", updateToolbox);
+  addon.self.addEventListener("reenabled", updateToolbox);
+
+  function updateToolbox() {
+    // todo, idk how to do this :(
+  }
+}
