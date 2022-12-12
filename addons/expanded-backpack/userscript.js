2fe716ef124f6b6ca56f3a9e33926e9534fb8950
pumpkin <113677241+pumpkinhasapatch@users.noreply.github.com>
2022-12-12 03:55:21 +0000

    Update userscript.js

--- addons/expanded-backpack/userscript.js
+++ addons/expanded-backpack/userscript.js
@@ -1,9 +1,10 @@
 export default async function ({ addon, global, console }) {
-  // resize editor to match backpack height when settings changed or addon disabled
-  addon.settings.addEventListener("change", function() {
-    window.dispatchEvent(new Event("resize"))
-  });
-  addon.self.addEventListener("disabled", function() {
-    window.dispatchEvent(new Event("resize"))
-  });
+  // fix area above the backpack displaying incorrectly when height changed
+  addon.settings.addEventListener("change", heightFix);
+  addon.self.addEventListener("disabled", heightFix);
+  addon.self.addEventListener("reenabled", heightFix);
+
+  async function heightFix() {
+    window.dispatchEvent(new Event("resize")); // resizes the code area and paint editor canvas
+  }
 }
