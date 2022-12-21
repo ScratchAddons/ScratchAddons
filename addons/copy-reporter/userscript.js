8e941a44f96398269ae6f960c79a90a91b52d89f
TheKodeToad <TheKodeToad@users.noreply.github.com>
2022-12-21 17:24:29 +0000

    Format code

--- addons/copy-reporter/userscript.js
+++ addons/copy-reporter/userscript.js
@@ -1,35 +1,36 @@
 export default async function ({ addon, console, msg }) {
   addon.tab.createEditorContextMenu(
-      (ctx) => {
-        // ugly hack to get the string value
-        let element = ctx.target.lastChild.lastChild;
+    (ctx) => {
+      // ugly hack to get the string value
+      let element = ctx.target.lastChild.lastChild;
 
-        if (ctx.type === "monitor_slider") {
-          element = element.children[element.children.length - 2].lastChild;
-        } else {
+      if (ctx.type === "monitor_slider") {
+        element = element.children[element.children.length - 2].lastChild;
+      } else {
+        element = element.lastChild;
+        if (ctx.type === "monitor_default") {
           element = element.lastChild;
-          if (ctx.type === "monitor_default") {
-            element = element.lastChild;
-          }
         }
+      }
 
-        if (element.innerText.length !== 0) {
-          navigator.clipboard.writeText(element.innerText);
-        }
-      },
-      {
-        className: "copy",
-        types: ["monitor_default", "monitor_large", "monitor_slider"],
-        position: "monitor",
-        order: 0,
-        label: msg("copy-value")
-      });
+      if (element.innerText.length !== 0) {
+        navigator.clipboard.writeText(element.innerText);
+      }
+    },
+    {
+      className: "copy",
+      types: ["monitor_default", "monitor_large", "monitor_slider"],
+      position: "monitor",
+      order: 0,
+      label: msg("copy-value"),
+    }
+  );
 
   // add button to reporter bubble
   const ScratchBlocks = await addon.tab.traps.getBlockly();
 
   // https://github.com/LLK/scratch-blocks/blob/893c7e7ad5bfb416eaed75d9a1c93bdce84e36ab/core/workspace_svg.js#L979
-  ScratchBlocks.WorkspaceSvg.prototype.reportValue = function(id, value) {
+  ScratchBlocks.WorkspaceSvg.prototype.reportValue = function (id, value) {
     let block = this.getBlockById(id);
     if (!block) {
       throw "Tried to report value on block that does not exist.";
@@ -74,7 +75,10 @@ export default async function ({ addon, console, msg }) {
 
     contentDiv.appendChild(valueReportBox);
 
-    ScratchBlocks.DropDownDiv.setColour(ScratchBlocks.Colours.valueReportBackground, ScratchBlocks.Colours.valueReportBorder);
+    ScratchBlocks.DropDownDiv.setColour(
+      ScratchBlocks.Colours.valueReportBackground,
+      ScratchBlocks.Colours.valueReportBorder
+    );
     ScratchBlocks.DropDownDiv.showPositionedByBlock(this, block);
   };
 }
