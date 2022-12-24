c1a96bff12507acd17ac7d053b3fe0e36b2fe23f
TheKodeToad <TheKodeToad@proton.me>
2022-12-24 09:37:29 +0000

    Replace hardcoded string with fetch to internal resource - potentially unsafe

--- addons/copy-reporter/userscript.js
+++ addons/copy-reporter/userscript.js
@@ -1,4 +1,6 @@
 export default async function ({ addon, console, msg }) {
+  const copySvg = await fetch(addon.self.dir + "/copy.svg").then((response) => response.text());
+
   addon.tab.createEditorContextMenu(
     (ctx) => {
       // ugly hack to get the string value
@@ -68,7 +70,7 @@ export default async function ({ addon, console, msg }) {
       copyButton.style.display = "block";
       copyButton.style.userSelect = "none";
 
-      copyButton.innerHTML = `<path fill="currentColor" d="M21 8.94a1.31 1.31 0 0 0-.06-.27v-.09a1.07 1.07 0 0 0-.19-.28l-6-6a1.07 1.07 0 0 0-.28-.19a.32.32 0 0 0-.09 0a.88.88 0 0 0-.33-.11H10a3 3 0 0 0-3 3v1H6a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3v-1h1a3 3 0 0 0 3-3V8.94Zm-6-3.53L17.59 8H16a1 1 0 0 1-1-1ZM15 19a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h1v7a3 3 0 0 0 3 3h5Zm4-4a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3v3a3 3 0 0 0 3 3h3Z"/>`;
+      copyButton.innerHTML = copySvg;
       copyButton.onclick = () => navigator.clipboard.writeText(value);
       valueReportBox.appendChild(copyButton);
     }
