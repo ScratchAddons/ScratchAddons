d85d1f5bf1f9085d42c9ea1afd7b18939f983438
WorldLanguages <mundofinky@gmail.com>
2022-12-26 20:15:16 -0300

    Add v1.30 update notification

--- content-scripts/cs.js
+++ content-scripts/cs.js
@@ -649,7 +649,7 @@ const showBanner = () => {
   });
   const notifInnerText1 = Object.assign(document.createElement("span"), {
     style: NOTIF_TEXT_STYLE,
-    innerHTML: escapeHTML(chrome.i18n.getMessage("extensionUpdateInfo1_v1_29", DOLLARS)).replace(
+    innerHTML: escapeHTML(chrome.i18n.getMessage("extensionUpdateInfo1_v1_30", DOLLARS)).replace(
       /\$(\d+)/g,
       (_, i) =>
         [
@@ -668,7 +668,7 @@ const showBanner = () => {
   });
   const notifInnerText2 = Object.assign(document.createElement("span"), {
     style: NOTIF_TEXT_STYLE,
-    textContent: chrome.i18n.getMessage("extensionUpdateInfo2_v1_29"),
+    textContent: chrome.i18n.getMessage("extensionUpdateInfo2_v1_30"),
   });
   const notifFooter = Object.assign(document.createElement("span"), {
     style: NOTIF_TEXT_STYLE,
