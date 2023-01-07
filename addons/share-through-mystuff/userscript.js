3d24bc6d959f5f4470ae8b35e220e897bff35cfe
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-01-06 18:49:33 -0800

    Full rewrite

--- addons/share-through-mystuff/userscript.js
+++ addons/share-through-mystuff/userscript.js
@@ -1,48 +1,24 @@
 export default async function ({ addon, global, console }) {
-  const next_button_selector = ".grey.button:not(.small)";
-  async function prompt(e) {
-    let confirmation = confirm("Would you like to share this project?");
-    if (!confirmation) return;
-    e.target.parentElement.querySelector(".media-share").click();
-  }
-  function make(display) {
-    // function that creates the share button
-    let share_link = document.createElement("a");
-    share_link.href = "#";
-    share_link.classList.add("media-share");
-    share_link.dataset.control = "share";
-    share_link.addEventListener("click", check_shared);
-    let prompt_link = document.createElement("a");
-    prompt_link.href = "#";
-    prompt_link.innerText = display;
-    prompt_link.addEventListener("click", prompt);
-    let container = document.createElement("span");
-    container.append(prompt_link, share_link);
-    container.classList.add("sa_mystuff_sharing_button");
-    return container;
-  }
-  var next_button = document.querySelector(next_button_selector);
-  async function run(runs, elements) {
-    // main script
-    let not_shared = document.querySelectorAll(".media-item-content.not-shared .media-action div")[runs];
-    if (!not_shared.hasAttribute("data-sa_mystuff_sharing_added")) not_shared.appendChild(make("Share"));
-    not_shared.dataset.sa_mystuff_sharing_added = 1;
-    if (runs + 1 !== elements) run(runs + 1, elements);
-  }
-  async function basics() {
-    // setup for new runs of the script
-    await addon.tab.waitForElement(".media-item-content.not-shared");
-    let elements = document.querySelectorAll(".media-item-content.not-shared").length;
-    next_button = document.querySelector(next_button_selector);
-    next_button.addEventListener("click", event);
-    addon.tab.addEventListener("urlChange", event);
-    run(0, elements);
-  }
-  function event(wait = 500) {
-    console.log("Event Triggered");
-    setTimeout(basics, wait); // allows for time to load
-  }
-  addon.tab.addEventListener("urlChange", event);
-  next_button.addEventListener("click", () => event(1500));
-  basics();
+    const share_function = document.createElement("a");
+    share_function.classList.add("media-share");
+    share_function.dataset.control = "share";
+    share_function.href = "#";
+
+    const share_button = document.createElement("a");
+    share_button.href = "#";
+    share_button.classList.add("__share_button__");
+    share_button.innerText = "Share";
+    share_button.addEventListener("click", event => {
+        let confirmation = confirm("Would you like to share this project?");
+        if (confirmation)
+            event.target.parentElement.querySelector(".media-share").click();
+    })
+
+    while(true) {
+        const project = await addon.tab.waitForElement("div.media-item-content.not-shared", {
+            markAsSeen: true
+        });
+        project.querySelector(".media-action div").appendChild(share_function.cloneNode());
+        project.querySelector(".media-action div").appendChild(share_button.cloneNode(true));
+    }
 }
