f698eae3ef4a5d28a4d5f4841e7d2259829fbf15
Steve0Greatness <Steve0Greatness@users.noreply.github.com>
2023-01-07 02:50:07 +0000

    Format code

--- addons/share-through-mystuff/userscript.js
+++ addons/share-through-mystuff/userscript.js
@@ -1,24 +1,23 @@
 export default async function ({ addon, global, console }) {
-    const share_function = document.createElement("a");
-    share_function.classList.add("media-share");
-    share_function.dataset.control = "share";
-    share_function.href = "#";
+  const share_function = document.createElement("a");
+  share_function.classList.add("media-share");
+  share_function.dataset.control = "share";
+  share_function.href = "#";
 
-    const share_button = document.createElement("a");
-    share_button.href = "#";
-    share_button.classList.add("__share_button__");
-    share_button.innerText = "Share";
-    share_button.addEventListener("click", event => {
-        let confirmation = confirm("Would you like to share this project?");
-        if (confirmation)
-            event.target.parentElement.querySelector(".media-share").click();
-    })
+  const share_button = document.createElement("a");
+  share_button.href = "#";
+  share_button.classList.add("__share_button__");
+  share_button.innerText = "Share";
+  share_button.addEventListener("click", (event) => {
+    let confirmation = confirm("Would you like to share this project?");
+    if (confirmation) event.target.parentElement.querySelector(".media-share").click();
+  });
 
-    while(true) {
-        const project = await addon.tab.waitForElement("div.media-item-content.not-shared", {
-            markAsSeen: true
-        });
-        project.querySelector(".media-action div").appendChild(share_function.cloneNode());
-        project.querySelector(".media-action div").appendChild(share_button.cloneNode(true));
-    }
+  while (true) {
+    const project = await addon.tab.waitForElement("div.media-item-content.not-shared", {
+      markAsSeen: true,
+    });
+    project.querySelector(".media-action div").appendChild(share_function.cloneNode());
+    project.querySelector(".media-action div").appendChild(share_button.cloneNode(true));
+  }
 }
