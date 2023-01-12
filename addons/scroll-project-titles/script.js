a6ebf464ef9cab1e617ab77883dd8bc34cda5908
Ronan <122339056+actuallyronan@users.noreply.github.com>
2023-01-12 12:15:40 -0800

    Fix disabled listener function

--- addons/scroll-project-titles/script.js
+++ addons/scroll-project-titles/script.js
@@ -1,5 +1,7 @@
 export default async function ({ addon, console, msg }) {
   addon.self.addEventListener("disabled", function() {
-   document.querySelector(".project-title.no-edit")?.scrollLeft = 0
+    if (document.querySelector(".project-title.no-edit")) {
+      document.querySelector(".project-title.no-edit").scrollLeft = 0
+    }
   });
 }
