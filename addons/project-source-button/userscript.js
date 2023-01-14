3421d33840e5b59eff8f5fe1830f0ed658a2de86
nakasyou <nakasyou1103@gmail.com>
2023-01-14 18:32:15 +0900

    Add a "Project source button" addon.

---
+++ addons/project-source-button/userscript.js
@@ -0,0 +1,53 @@
+export default async function ({ addon, global, console, msg}) {
+    function getFilePath(path){
+        let addonPath=addon.__path;
+        if(addonPath.at(-1)!=="/"){
+            addonPath+="/";
+        }
+        if(path[0]!=="/"){
+            path="/"+path;
+        }
+        return addonPath+"addons/"+addon._addonId+path;
+    }
+    //get project's public status
+    const isPublic=addon.tab.redux.state.preview.projectInfo.public;
+    //get project id
+    const projectId = window.location.pathname.split("/")[2];
+    async function getToken(){
+        let projectToken;
+        if(!isPublic){
+            projectToken = (
+                await (
+                    await fetch(`https://api.scratch.mit.edu/projects/${projectId}?nocache=${Date.now()}`, {
+                        headers: {"x-token": await addon.auth.fetchXToken()}
+                    })
+                ).json()
+            ).project_token;
+        }else{
+            projectToken = (
+                await (
+                    await fetch(`https://api.scratch.mit.edu/projects/${projectId}?nocache=${Date.now()}`, {})
+                ).json()
+            ).project_token;
+        }
+        return projectToken;
+    }
+
+    const prjBtnEvt=setInterval(e=>{
+        if(document.getElementsByClassName("project-buttons")[0]){
+            clearInterval(prjBtnEvt)
+            setup()
+        }
+    },100);
+    async function setup(){
+        const icon=document.createElement("div");
+        icon.id="view-json-btn";
+        icon.title=msg("hover");
+        const pjtBtns=document.getElementsByClassName("project-buttons")[0];
+        pjtBtns.prepend(icon);
+        document.getElementById("view-json-btn").addEventListener("click",async e=>{
+            const projectToken=await getToken();
+            const viewer=window.open(getFilePath("viewer.html")+`?id=${projectId}&token=${projectToken}`);
+        });
+    }
+}
---
+++ addons/project-source-button/viewer.js
@@ -0,0 +1,70 @@
+const queries=(()=>{
+    const query=window.location.search.slice(1);
+    const queries = {};
+    if (!query) return {};
+    for(let text of query.split('&')){
+        text=text.split('=');
+        queries[text[0]]=text[1];
+    }
+    return queries;
+})();
+(async function(){
+    const jsonData=await (await fetch(`https://projects.scratch.mit.edu/${queries.id}?token=${queries.token}`)).json();
+    document.getElementsByClassName("language-json")[0].textContent=JSON.stringify(jsonData,null,"  ");
+
+    const prismScript=document.createElement("script");
+    prismScript.src="prism.js";
+    document.body.appendChild(prismScript);
+
+    let assets=[];
+    function assetsSearch(arg){
+        if(arg===null){
+            return;
+        }
+        Object.keys(arg).forEach(key=>{
+            if(typeof arg[key]==="object"){
+                if(key==="sounds" || key==="costumes"){
+                    const data=arg[key];
+                    data.forEach(elem=>{elem.type=key});
+                    assets=assets.concat(data)
+                }else{
+                    assetsSearch(arg[key]);
+                }
+                
+            }/*else if(typeof arg[key]==="string"){
+                if(key==="md5ext"){
+                    assets.push(arg[key]);
+                }
+            }*/
+        });
+    }
+    assetsSearch(jsonData);
+    const assetsElem=document.getElementById("assets");
+    function assetsInfo(obj){
+        const tr=document.createElement("tr");
+        function addtd(arr){
+            arr.forEach(elem=>{
+                const td=document.createElement("td");
+                td.innerHTML=elem;
+                tr.appendChild(td);
+            })
+        }
+        addtd([
+            obj.type,
+            obj.name,
+            obj.dataFormat,
+            `<a href="https://cdn.assets.scratch.mit.edu/internalapi/asset/${obj.md5ext}/get/">URL</a>`,
+            obj.assetId,
+            (rate=>{if(rate==null){return "--"}return rate+"Hz"})(obj.rate)
+        ]);
+        return tr;
+    }
+    const assetsTable=document.createElement("table");
+    const ths=document.createElement("tr");
+    ths.innerHTML=`<th>Type</th><th>Name</th><th>Format</th><th>CDN</th><th>MD5</th><th>Sound rate</th>`
+    assetsTable.appendChild(ths);
+    assets.forEach(elem=>assetsTable.appendChild(assetsInfo(elem)));
+    assetsElem.appendChild(assetsTable);
+
+})()
+
