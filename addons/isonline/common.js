4e5a7b24d79225ec9333c0b7561750151ec9e33c
GrahamSH <grahamshllk@gmail.com>
2023-01-16 11:28:27 -0500

    fix some buggies

--- addons/isonline/common.js
+++ addons/isonline/common.js
@@ -1,5 +1,8 @@
 export const isOnline = async (user) => {
   let res = await fetch(`https://isoffline.grahamsh.com/status/${user}`);
   let data = await res.json();
+  if (data.statusCode == 404) {
+    throw new Error();
+  } 
   return data.online;
 };
--- addons/isonline/profile.js
+++ addons/isonline/profile.js
@@ -1,9 +1,13 @@
 import { isOnline } from "./common.js";
 async function updateProfile() {
   let username = window.location.pathname.split("/users/")[1].split("/")[0];
+  try {
   let online = await isOnline(username);
   let image = document.querySelector(".avatar");
   image.className = online ? "avatar online" : "avatar offline";
+  } catch (e) {
+    console.log('User does not have isonline enabled');
+  }
 }
 export default async function ({ addon, global, console, msg }) {
   await updateProfile();
