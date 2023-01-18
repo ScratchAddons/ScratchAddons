5efdb75a9211cc24ef2e13d0688a7cb6ffaf803c
GarboMuffin <muffin@mailbox.org>
2023-01-17 21:01:14 -0600

    sprite-properties: Fix some bugs (#5563)
    * Add support for "wide" languages
    
    These ones:
    https://github.com/LLK/scratch-gui/blob/7e2430eb28aed90f42c0c415969c8e285f43084e/src/lib/locale-utils.js#L6
    
    * Fix sprite properties disappearing after changing language
    
    https://github.com/ScratchAddons/ScratchAddons/issues/5561
    
    * Convert to typical while (true) { ... }
    
    * Make wide mode detection work in small stage mode
    
    * Make drag-drop lose specificity to sprite-properties
    
    * Disable icon dragging
    
    * Revert unnecessary change
    
    * Add redux condition too
    
    * Remove drag-drop's sprite info panel animation
    
    * Format code
    
    * Revert drag-drop changes
    
    * sa-sprite-properties-btn -> sa-sprite-properties-info-btn
    
    * RTL support
    
    * Move show and hide classes to the <body> element
    
    * Comments and code style
    
    * Remove unnecessary async
    
    * Apply setting classes earlier
    
    Fixes layout shifts when auto collapse is disabled
    
    * Make dynamic disable and reenable work better
    
    * Revert changes to setting change handlers
    
    * Wait for React to finish updating DOM before injecting button
    
    * Simplify button-related logic
    
    * Unbreak hiding info button
    
    This would not be an issue with https://github.com/ScratchAddons/ScratchAddons/pull/4924
    
    * Fix compatibility with folders
    
    * Remove `global`
    
    * make wide locales work again, whoops
    
    * Prevent double clicking from highlighting images
    
    * Update comments
    
    * Minimize MutationObserver events
    
    * Always disable collapse by default when collapse on mouse leave is enabled
    
    * Simplify
    
    Co-authored-by: GarboMuffin <GarboMuffin@users.noreply.github.com>

--- addons/sprite-properties/userscript.js
+++ addons/sprite-properties/userscript.js
@@ -1,35 +1,40 @@
-export default async function ({ addon, global, console, msg }) {
+export default async function ({ addon, console, msg }) {
   const SHOW_PROPS_CLASS = "sa-show-sprite-properties";
   const HIDE_PROPS_CLASS = "sa-hide-sprite-properties";
-  const PROPS_BTN_CLASS = "sa-sprite-properties-btn";
+  const PROPS_INFO_BTN_CLASS = "sa-sprite-properties-info-btn";
   const PROPS_CLOSE_BTN_CLASS = "sa-sprite-properties-close-btn";
 
+  /** @type {HTMLElement} */
   let propertiesPanel;
-  let spriteContainer; // also contains sprite grid
 
-  await init();
-
-  // Inject info bubble into current editing target
-  addon.tab.redux.initialize();
-  addon.tab.redux.addEventListener("statechanged", (e) => {
-    if (e.detail.action.type === "scratch-gui/targets/UPDATE_TARGET_LIST") {
-      let spriteId = e.detail.action.editingTarget;
-      if (!spriteId) return;
-      let spriteIndex = e.detail.action.targets.findIndex((el) => el.id === spriteId);
-      // The focused sprite might not be in the target list if, for example, we are editing a clone.
-      if (spriteIndex !== -1) {
-        injectInfoButton(spriteIndex);
-      }
-    }
+  // A mutation observer is the only reliable way to detect when a different sprite has
+  // been selected or when the folder that contains the focused sprite has been opened.
+  const observer = new MutationObserver(() => {
+    injectInfoButton();
   });
 
-  // Open the properties panel when double clicking in the sprite grid
+  // Toggle the properties panel when double clicking in the sprite grid
   document.addEventListener("click", (e) => {
     if (e.detail === 2 && e.target.closest('[class^="sprite-selector_scroll-wrapper_"]')) {
       togglePropertiesPanel();
     }
   });
 
+  function setPropertiesPanelVisible(visible) {
+    document.body.classList.toggle(SHOW_PROPS_CLASS, visible);
+    document.body.classList.toggle(HIDE_PROPS_CLASS, !visible);
+  }
+
+  function togglePropertiesPanel() {
+    const isCurrentlyOpen = document.body.classList.contains(SHOW_PROPS_CLASS);
+    setPropertiesPanelVisible(!isCurrentlyOpen);
+  }
+
+  function autoHidePanel() {
+    if (addon.settings.get("autoCollapse")) {
+      setPropertiesPanelVisible(false);
+    }
+  }
   // Close properties panel when mouse leaves the entire sprite panel
   document.body.addEventListener(
     "mouseleave",
@@ -42,73 +47,100 @@ export default async function ({ addon, global, console, msg }) {
       capture: true,
     }
   );
+  addon.settings.addEventListener("change", autoHidePanel);
 
-  addon.settings.addEventListener("change", () => autoHidePanel());
+  function applySettings() {
+    const visibleByDefault = !addon.settings.get("autoCollapse") && !addon.settings.get("hideByDefault");
+    setPropertiesPanelVisible(visibleByDefault);
+  }
+  addon.self.addEventListener("reenabled", applySettings);
+  applySettings();
+
   addon.self.addEventListener("disabled", () => {
     setPropertiesPanelVisible(true);
-    removeCloseButton();
-  });
-  addon.self.addEventListener("reenabled", () => init());
-  addon.tab.addEventListener("urlChange", () => {
-    if (addon.tab.editorMode === "editor") init();
   });
 
-  async function init() {
-    propertiesPanel = await addon.tab.waitForElement('[class^="sprite-info_sprite-info_"]');
-    spriteContainer = propertiesPanel.parentElement; // also contains sprite grid
-    setPropertiesPanelVisible(!addon.settings.get("hideByDefault"));
+  function createButton(className, iconPath, tooltip) {
+    const buttonIcon = document.createElement("img");
+    buttonIcon.setAttribute("src", addon.self.dir + iconPath);
+    buttonIcon.draggable = false;
+    const button = document.createElement("button");
+    button.classList.add(className);
+    button.title = tooltip;
+    button.addEventListener("click", () => togglePropertiesPanel());
+    button.appendChild(buttonIcon);
+    addon.tab.displayNoneWhileDisabled(button, { display: "flex" });
+    return button;
+  }
+
+  /** @type {HTMLElement} */
+  let infoButton;
+  /** @type {HTMLElement} */
+  let closeButton;
+
+  function injectInfoButton() {
+    if (!infoButton) {
+      infoButton = createButton(PROPS_INFO_BTN_CLASS, "/info.svg", msg("open-properties-panel-tooltip"));
+    }
+    const selectedSprite = propertiesPanel.parentNode.querySelector('[class*="sprite-selector-item_is-selected"]');
+    if (infoButton.parentNode !== selectedSprite) {
+      if (selectedSprite) {
+        selectedSprite.appendChild(infoButton);
+      } else {
+        infoButton.remove();
+      }
+    }
+  }
+
+  function injectCloseButton() {
+    if (!closeButton) {
+      closeButton = createButton(PROPS_CLOSE_BTN_CLASS, "/collapse.svg", msg("close-properties-panel-tooltip"));
+    }
+    propertiesPanel.appendChild(closeButton);
+  }
+
+  function updateWideLocaleMode() {
+    // Certain "wide" languages such as Japanese use a different layout for the sprite info panel
+    // Easiest way to detect this is with this selector that only exists when the sprite info panel
+    // is using the layout with text above the input.
+    // Note that when the stage is in small mode, "wide" languages use the same info panel as other
+    // languages.
+    // List of languages is here:
+    // https://github.com/LLK/scratch-gui/blob/e15b2dfa3a2e58e80fae8d1586c7f56aa0cc0ede/src/lib/locale-utils.js#L6-L18
+    const isWideLocale = !!propertiesPanel.querySelector("[class^=label_input-group-column_]");
+    document.body.classList.toggle("sa-sprite-properties-wide-locale", isWideLocale);
+  }
+
+  document.addEventListener(
+    "click",
+    (e) => {
+      if (
+        e.target.closest("[class*='stage-header_stage-button-first']") ||
+        e.target.closest("[class*='stage-header_stage-button-last']")
+      ) {
+        setTimeout(updateWideLocaleMode);
+      }
+    },
+    { capture: true }
+  );
+
+  while (true) {
+    propertiesPanel = await addon.tab.waitForElement('[class^="sprite-info_sprite-info_"]', {
+      markAsSeen: true,
+      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
+      reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
+    });
+
+    const spriteSelector = propertiesPanel.parentNode;
+    const scrollWrapper = spriteSelector.querySelector('[class*="sprite-selector_scroll-wrapper_"]');
+    const itemsWrapper = scrollWrapper.firstChild;
+    observer.observe(itemsWrapper, {
+      childList: true,
+      subtree: true,
+    });
+
+    updateWideLocaleMode();
     injectInfoButton();
     injectCloseButton();
-  }
-
-  function setPropertiesPanelVisible(visible) {
-    spriteContainer.classList.toggle(SHOW_PROPS_CLASS, visible);
-    spriteContainer.classList.toggle(HIDE_PROPS_CLASS, !visible);
-  }
-
-  function togglePropertiesPanel() {
-    const isCurrentlyOpen = spriteContainer.classList.contains(SHOW_PROPS_CLASS);
-    setPropertiesPanelVisible(!isCurrentlyOpen);
-  }
-
-  function autoHidePanel() {
-    if (addon.settings.get("autoCollapse")) {
-      setPropertiesPanelVisible(false);
-    }
-  }
-
-  async function injectInfoButton(spriteIndex) {
-    let selectedSprite;
-    if (spriteIndex) {
-      selectedSprite = document.querySelector(
-        `[class*='sprite-selector_sprite-wrapper_']:nth-child(${spriteIndex}) [class*="sprite-selector_sprite_"]`
-      );
-    } else {
-      selectedSprite = await addon.tab.waitForElement(`[class*="sprite-selector-item_is-selected"]`);
-    }
-    injectButton(selectedSprite, PROPS_BTN_CLASS, "/info.svg", msg("open-properties-panel-tooltip"));
-  }
-
-  async function injectCloseButton() {
-    injectButton(propertiesPanel, PROPS_CLOSE_BTN_CLASS, "/collapse.svg", msg("close-properties-panel-tooltip"));
-  }
-
-  async function injectButton(container, className, iconPath, tooltip) {
-    if (container.querySelector("." + className)) return;
-    let btnIcon = document.createElement("img");
-    btnIcon.setAttribute("src", addon.self.dir + iconPath);
-    let btn = document.createElement("button");
-    btn.classList.add(className);
-    btn.setAttribute("title", tooltip);
-    btn.setAttribute("tabindex", 0);
-    btn.addEventListener("click", () => togglePropertiesPanel());
-    btn.appendChild(btnIcon);
-    container.appendChild(btn);
-    addon.tab.displayNoneWhileDisabled(btn, { display: "flex" });
-  }
-
-  function removeCloseButton() {
-    let closeBtn = document.querySelector("." + PROPS_CLOSE_BTN_CLASS);
-    if (closeBtn) closeBtn.remove();
   }
 }
