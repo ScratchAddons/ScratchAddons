202a83c08c9951556c79da10013cc03ee439d0ff
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-01-20 20:00:51 -0800

    Camel case is now used.

--- addons/share-through-mystuff/userscript.js
+++ addons/share-through-mystuff/userscript.js
@@ -1,25 +1,25 @@
 export default async function ({ addon, global }) {
-  const share_function = document.createElement("a");
-  share_function.classList.add("media-share");
-  share_function.dataset.control = "share";
-  share_function.href = "#";
+  const shareFunction = document.createElement("a");
+  shareFunction.classList.add("media-share");
+  shareFunction.dataset.control = "share";
+  shareFunction.href = "#";
 
-  const share_button = document.createElement("a");
-  function share_confirmation(event) {
+  const shareButton = document.createElement("a");
+  function shareConfirmation(event) {
     let confirmation = confirm("Would you like to share this project?");
     if (confirmation) event.target.parentElement.querySelector(".media-share").click();
   }
-  share_button.href = "#";
-  share_button.classList.add("sa-share-button");
-  share_button.innerText = "Share";
+  shareButton.href = "#";
+  shareButton.classList.add("sa-share-button");
+  shareButton.innerText = "Share";
 
   while (true) {
     const project = await addon.tab.waitForElement("div.media-item-content.not-shared", {
       markAsSeen: true,
     });
-    project.querySelector(".media-action div").appendChild(share_function.cloneNode());
-    let local_share = share_button.cloneNode(true);
-    local_share.addEventListener("click", share_confirmation);
-    project.querySelector(".media-action div").appendChild(local_share);
+    project.querySelector(".media-action div").appendChild(shareFunction.cloneNode());
+    let localShare = shareButton.cloneNode(true);
+    localShare.addEventListener("click", shareConfirmation);
+    project.querySelector(".media-action div").appendChild(localShare);
   }
 }
