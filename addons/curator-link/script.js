7dce415af0f831a36a545eaea0812b4c1214f00e
Samq64 <81489795+Samq64@users.noreply.github.com>
2023-01-30 16:57:29 -0500

    dynamic enable/disable code from more-links

--- addons/curator-link/script.js
+++ addons/curator-link/script.js
@@ -38,9 +38,27 @@ export default async function ({ addon, console }) {
   }
   
   if (pageType === "discuss" && addon.settings.get("mentions")) {
+    const linkified = [];
+    addon.self.addEventListener("disabled", () => {
+      for (const { element, original } of linkified) {
+        element.innerHTML = original;
+      }
+    });
+    addon.self.addEventListener("reenabled", () => {
+      for (const { element } of linkified) {
+        pingifyTextNode(element);
+      }
+    });
+    function pingify(element) {
+      linkified.push({
+        element,
+        original: element.innerHTML,
+      });
+      return pingifyTextNode(element);
+    }
     while (true) {
       let post = await addon.tab.waitForElement(".post_body_html", { markAsSeen: true });
-      if (!addon.self.disabled) pingifyTextNode(post);
+      if (!addon.self.disabled) pingify(post);
     }
   }
 }
