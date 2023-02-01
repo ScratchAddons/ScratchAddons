664a5f3b502a81a8b2950c5aa119a1ee54c62dc8
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-02-01 13:20:32 -0800

    Bug fix & code rearrangement

--- addons/share-through-mystuff/userscript.js
+++ addons/share-through-mystuff/userscript.js
@@ -2,26 +2,27 @@ export default async function ({ addon }) {
   const shareFunction = document.createElement("a");
   shareFunction.classList.add("media-share");
   shareFunction.dataset.control = "share";
-  shareFunction.href = "#";
 
   const shareButton = document.createElement("a");
-  async function shareConfirmation(event) {
-    let confirmation = await addon.tab.confirm("Share this project?", "Are you sure you want to share this project?");
-    if (confirmation) event.target.parentElement.querySelector(".media-share").click();
-  }
-  shareButton.href = "#";
   shareButton.classList.add("sa-share-button");
   shareButton.innerText = "Share";
   addon.tab.displayNoneWhileDisabled(shareButton, {
     display: "block",
   });
+  async function shareConfirmation(event) {
+    let confirmation = await addon.tab.confirm("Share this project?", "Are you sure you want to share this project?");
+    if (confirmation) event.target.parentElement.querySelector(".media-share").click();
+  }
 
   while (true) {
     const project = await addon.tab.waitForElement("div.media-item-content.not-shared", {
       markAsSeen: true,
     });
-    project.querySelector(".media-action div").appendChild(shareFunction.cloneNode());
+    let localShareFunction = shareFunction.cloneNode();
     let localShare = shareButton.cloneNode(true);
+    localShareFunction.href = `${location.hash}`;
+    localShare.href = localShareFunction.href;
+    project.querySelector(".media-action div").appendChild(localShareFunction);
     localShare.addEventListener("click", shareConfirmation);
     project.querySelector(".media-action div").appendChild(localShare);
   }
