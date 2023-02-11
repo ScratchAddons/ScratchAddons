4cad1c7258247f3eaf80d845505a8c28d6be1284
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-02-10 20:28:26 -0800

    Extending cookie length

--- addons/remember-collapsed-categories/userscript.js
+++ addons/remember-collapsed-categories/userscript.js
@@ -3,21 +3,52 @@ export default async function ({ console }) {
   for (let i = 0; i < categories.length; i++) {
     let categoryBody = categories[i];
 
+    let categoryCollapse = categoryBody.querySelector(".toggle");
+    categoryCollapse.addEventListener("click", event => {
+      event.preventDefault();
+      let categoryHead = event.target.parentElement;
+      if (categoryHead.classList.contains("sa-collapsed")) {
+        categoryHead.classList.remove("sa-collapsed");
+        console.log(`${categoryHead.id.split("_")[2]} is being opened`);
+        return;
+      } else {
+        categoryHead.classList.add("sa-collapsed");
+        changeCategoryCookieExistenceLength(categoryNumber);
+      }
+    });
+
     let categoryNumber = categoryBody.id.split("_").pop();
-    if (!shouldCollapseCategory(categoryNumber)) break;
+    if (!shouldCollapseCategory(categoryNumber)) {
+        removeCategoryCookie(categoryNumber)
+        continue;
+    }
+    changeCategoryCookieExistenceLength(categoryNumber);
 
     let categoryContent = categoryBody.querySelector(".box-content");
     categoryContent.style.display = "none";
 
-    let categoryHead = categoryBody.querySelector(`.box-head`);
+    let categoryHead = categoryBody.querySelector(`.box-head h4`);
     categoryHead.classList.add("collapsed");
+    categoryHead.classList.add("sa-collapsed");
 
     let categoryName = categoryHead.innerText.split("\n")[1]; // This is exclusively for debug purposes
     console.log(`Category ${categoryName}(#${categoryNumber}) should be hidden`);
   }
 
-  function shouldCollapseCategory(catNumber) {
-    let cookieName = `category_body_${catNumber}`;
-    return document.cookie.includes(`${cookieName}=collapsed;`);
+  function shouldCollapseCategory(categoryNumber) {
+    let cookieName = `category_body_${categoryNumber}`;
+    let storedInCookie = document.cookie.includes(`${cookieName}=collapsed`)
+    return storedInCookie;
+  }
+
+  function changeCategoryCookieExistenceLength(categoryNumber) {
+    let cookieName = `category_body_${categoryNumber}`;
+    console.log(`Extending ${cookieName} for an extra 30 days`)
+    document.cookie = `${cookieName}=collapsed;max-age=${30*24*60*60}`;
+  }
+
+  function removeCategoryCookie(categoryNumber) {
+    let cookieName = `category_body_${categoryNumber}`;
+    document.cookie = `${cookieName}=;max-age=0;`;
   }
 }
