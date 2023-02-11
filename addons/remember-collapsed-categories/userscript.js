94a083b7bb13b1d2b74befd1adf0a7e94ee47fcc
Steve0Greatness <Steve0Greatness@users.noreply.github.com>
2023-02-11 02:28:58 +0000

    Format code

--- addons/remember-collapsed-categories/userscript.js
+++ addons/remember-collapsed-categories/userscript.js
@@ -1,25 +1,23 @@
 export default async function ({ console }) {
-    const categories = document.querySelectorAll('[id^="category_body_"]');
-    for (let i = 0; i < categories.length; i++) {
-      let categoryBody = categories[i];
+  const categories = document.querySelectorAll('[id^="category_body_"]');
+  for (let i = 0; i < categories.length; i++) {
+    let categoryBody = categories[i];
 
-      let categoryNumber = categoryBody.id.split("_").pop();
-      if (!shouldCollapseCategory(categoryNumber))
-        break;
-      
-      let categoryContent = categoryBody.querySelector(".box-content");
-      categoryContent.style.display = "none";
+    let categoryNumber = categoryBody.id.split("_").pop();
+    if (!shouldCollapseCategory(categoryNumber)) break;
 
-      let categoryHead = categoryBody.querySelector(`.box-head`);
-      categoryHead.classList.add("collapsed");
-      
-      let categoryName = categoryHead.innerText.split("\n")[1]; // This is exclusively for debug purposes
-      console.log(`Category ${categoryName}(#${categoryNumber}) should be hidden`);
-    }
+    let categoryContent = categoryBody.querySelector(".box-content");
+    categoryContent.style.display = "none";
 
-    function shouldCollapseCategory(catNumber) {
-      let cookieName = `category_body_${catNumber}`;
-      return document.cookie.includes(`${cookieName}=collapsed;`);
-    }
+    let categoryHead = categoryBody.querySelector(`.box-head`);
+    categoryHead.classList.add("collapsed");
+
+    let categoryName = categoryHead.innerText.split("\n")[1]; // This is exclusively for debug purposes
+    console.log(`Category ${categoryName}(#${categoryNumber}) should be hidden`);
   }
-  
+
+  function shouldCollapseCategory(catNumber) {
+    let cookieName = `category_body_${catNumber}`;
+    return document.cookie.includes(`${cookieName}=collapsed;`);
+  }
+}
