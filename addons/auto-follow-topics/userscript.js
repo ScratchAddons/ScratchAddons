c03249a1deabe28482b512df078f210484e40be5
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2023-02-18 10:53:35 +0530

    Use sessionStorage

--- addons/auto-follow-topics/userscript.js
+++ addons/auto-follow-topics/userscript.js
@@ -1,12 +1,20 @@
 export default async function ({ addon, console, msg }) {
+  const getTopics = () => JSON.parse(sessionStorage.getItem("auto-follow-topics") || "[]");
+  const setTopics = (topics) => sessionStorage.setItem("auto-follow-topics", JSON.stringify(topics));
+  const topicID = location.href.split("/")[5];
+
   document.querySelector("[name=AddPostForm]").addEventListener("click", (event) => {
-    localStorage.setItem(location.href.split("/")[5], true);
+    let ids = getTopics();
+    ids.push(topicID);
+    setTopics(ids);
   });
 
-  if (localStorage.getItem(location.href.split("/")[5])) {
+  let topics = getTopics();
+  if (topics.includes(topicID)) {
     const followBtn = document.querySelectorAll(".follow-button")[1];
     followBtn.focus();
     followBtn.click();
-    localStorage.clear();
+    // Remove the topic we've followed now
+    setTopics(topics.filter((topic) => topic !== topicID));
   }
 }
