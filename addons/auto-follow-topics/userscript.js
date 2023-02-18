b190a8a7f20680bf8ccc936fce7c27d666e235ad
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2023-02-18 10:10:06 +0530

    Remove debug console.log
    Co-authored-by: BroJac5246 <104146239+BroJac5246@users.noreply.github.com>

--- addons/auto-follow-topics/userscript.js
+++ addons/auto-follow-topics/userscript.js
@@ -4,7 +4,6 @@ export default async function ({ addon, console, msg }) {
   });
 
   if (localStorage.getItem(location.href.split("/")[5])) {
-    console.log("Following topic");
     const followBtn = document.querySelectorAll(".follow-button")[1];
     followBtn.focus();
     followBtn.click();
