60e38ebf6529410256037427a5d273fd477deafb
iqnite <iqnite@users.noreply.github.com>
2023-02-26 19:44:19 +0000

    Format code

--- addons/compact-menu-bar/userscript.js
+++ addons/compact-menu-bar/userscript.js
@@ -1,16 +1,16 @@
 export default async function ({ addon }) {
-    let span;
-    let li;
-    while (true) {
-        if (await addon.auth.fetchIsLoggedIn()) {
-            const username = await addon.auth.fetchUsername();
-            const dropdown = await addon.tab.waitForElement(".account-nav_profile-name_2oRiV");
-            span = dropdown;
-            span?.remove();
-            const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt");
-            li = profileSpans.appendChild(document.createElement("span"));
-            li.className = "sa-profile-name";
-            li.textContent = username;
-        }
+  let span;
+  let li;
+  while (true) {
+    if (await addon.auth.fetchIsLoggedIn()) {
+      const username = await addon.auth.fetchUsername();
+      const dropdown = await addon.tab.waitForElement(".account-nav_profile-name_2oRiV");
+      span = dropdown;
+      span?.remove();
+      const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt");
+      li = profileSpans.appendChild(document.createElement("span"));
+      li.className = "sa-profile-name";
+      li.textContent = username;
     }
+  }
 }
