fafad5cc3fdab03f6729462d449cb13f0d215393
IQBit <iqnitetechnologies@gmail.com>
2023-02-26 20:46:31 +0100

    Update userscript.js

--- addons/compact-menu-bar/userscript.js
+++ addons/compact-menu-bar/userscript.js
@@ -3,10 +3,12 @@ export default async function ({ addon }) {
     let li;
     while (true) {
         if (await addon.auth.fetchIsLoggedIn()) {
+            // Remove username from bar
             const username = await addon.auth.fetchUsername();
             const dropdown = await addon.tab.waitForElement(".account-nav_profile-name_2oRiV");
             span = dropdown;
             span?.remove();
+            // Add username to "Profile" menu option
             const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt");
             li = profileSpans.appendChild(document.createElement("span"));
             li.className = "sa-profile-name";
