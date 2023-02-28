cbc316a0e15d1a994d39e0193dc548be6fac9244
IQBit <iqnitetechnologies@gmail.com>
2023-02-28 22:05:33 +0100

    Move to userstyle

--- addons/compact-menu-bar/userscript.js
+++ addons/compact-menu-bar/userscript.js
@@ -1,18 +1,14 @@
 export default async function ({ addon }) {
-  let span;
-  let li;
-  if (await addon.auth.fetchIsLoggedIn()) {
-    while (true) {
-      // Remove username from bar
-      const username = await addon.auth.fetchUsername();
-      const dropdown = await addon.tab.waitForElement(".account-nav_profile-name_2oRiV");
-      span = dropdown;
-      span?.remove();
-      // Add username to "Profile" menu option
-      const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt");
-      li = profileSpans.appendChild(document.createElement("span"));
-      li.className = "sa-profile-name";
-      li.textContent = username;
-    }
+  let profile;
+
+  async function addSpan() {
+    // Add username to "Profile" menu option
+    const username = await addon.auth.fetchUsername();
+    const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt");
+    profile = profileSpans.appendChild(document.createElement("span"));
+    profile.textContent = username;
+    profile.className = "sa-profile-name";
   }
+
+  if (await addon.auth.fetchIsLoggedIn()) setInterval(addSpan(), 1);
 }
