697bc1b20a7f474ce2858370b864cd4c6be2eafb
Platformer444 <shreyanshupanda44@gmail.com>
2023-02-28 15:12:31 +0530

    complete rewrite of addon userscript

--- addons/project-folders/userscript.js
+++ addons/project-folders/userscript.js
@@ -1,108 +1,48 @@
 export default async function ({ addon, global, console, msg }) {
-  let projectColumns;
-  projectColumns = await addon.tab.waitForElement("ul.media-list", {
-    markAsSeen: true,
-  });
+  const projects = await (await fetch('https://scratch.mit.edu/site-api/projects/all')).json();
 
-  let foldersArray;
-  const projectTab = document.querySelectorAll("li.first")[0];
-  projectTab.childNodes[0].onclick = async () => {
-    projectColumns = await addon.tab.waitForElement("ul.media-list", {
-      markAsSeen: true,
-    });
-    createFolderAreaAndButton();
-    foldersArray = await load();
-  };
-
-  if (document.querySelectorAll("div.folders-container")[0] === undefined) {
-    createFolderAreaAndButton();
-    foldersArray = await load();
-  }
-
-  async function getProjectDetails(projectID) {
-    const token = await addon.auth.fetchXToken();
-    const projectDetails = await (
-      await fetch(`https://api.scratch.mit.edu/projects/${projectID}/`, {
+  async function loadFolders() {
+    const folders = [];
+    for (let i = 0; i < projects.length; i++) {
+      const projectID = projects[i].pk;
+      const token = await addon.auth.fetchXToken();
+      const projectDetails = await (await fetch(`https://api.scratch.mit.edu/projects/${projectID}`, {
         headers: {
           "content-type": "application/json",
           "x-csrftoken": addon.auth.crsfToken,
-          "x-token": token,
-        },
-      })
-    ).json();
+          "x-token": token
+      }})).json();
+      const instructions = projectDetails.instructions.split('\n');
+      const folder = instructions.filter(element => {
+        if (element.includes('#_')) {
+          return true;
+        }
+      });
+      if (folder.length === 0) continue;
 
-    return projectDetails;
-  }
-
-  async function load() {
-    const folders = [];
-
-    const folderDiv = document.querySelectorAll(".folders")[0];
-
-    const noFolderSpan = document.createElement("span");
-    noFolderSpan.textContent = msg("loading");
-    folderDiv.appendChild(noFolderSpan);
-
-    const tempBR = document.createElement("br");
-    folderDiv.appendChild(tempBR);
-
-    const loader = document.createElement("progress");
-    loader.setAttribute("value", "0");
-    loader.setAttribute("max", "100");
-    folderDiv.appendChild(loader);
-
-    for (let i = 0; i < projectColumns.childNodes.length - 2; i++) {
-      loader.setAttribute("value", (i / (projectColumns.childNodes.length - 2)) * 100);
-
-      const link = projectColumns.childNodes[i + 2].childNodes[1].childNodes[3].childNodes[1].childNodes[0].href;
-      const projectID = link.replace("https://scratch.mit.edu/projects/", "").replace("/", "");
-      const projectDetails = await getProjectDetails(projectID);
-      let instructions = projectDetails.instructions;
-
-      if (instructions.includes("#_")) {
-        instructions = instructions.split("\n");
-
-        for (let j = 0; j < instructions.length; j++) {
-          if (instructions[j].includes("#_")) {
-            let folderExists = false;
-            for (let k = 0; k < folders.length; k++) {
-              if (folders[k].name === instructions[j].replace("#_", "")) {
-                folders[k].projects.push({ link: link, name: projectDetails.title });
-                folderExists = true;
-                break;
-              }
-            }
-
-            if (folderExists === false) {
-              folders.push({
-                name: instructions[j].replace("#_", ""),
-                projects: [{ link: link, name: projectDetails.title }],
-              });
-            }
+      for (let j = 0; j < folder.length; j++) {
+        const matches = folders.filter(element => {
+          if (element.name === folder[j].replace('#_', '')) {
+            return true;
+          } else {
+            return false;
           }
+        });
+
+        if (matches.length === 0) {
+          folders.push({ name: folder[j].replace('#_', ''), projects: [{ name: projectDetails.title, id: projectDetails.id, thumbnail: projectDetails.image }] });
+        } else {
+          folders[folders.indexOf(matches[0])].projects.push({ name: projectDetails.title, id: projectDetails.id, thumbnail: projectDetails.image });
         }
       }
     }
 
-    loader.setAttribute("value", "100");
-
-    loader.remove();
-    tempBR.remove();
-
-    if (folders.length === 0) {
-      noFolderSpan.textContent = msg("noFolder");
-    } else {
-      noFolderSpan.remove();
+    for (let i = 0; i < folders.length; i++) {
+      createFolder(folders[i].name, JSON.stringify(folders[i]));
     }
-
-    for (let k = 0; k < folders.length; k++) {
-      createFolder(folders[k].name);
-    }
-
-    return folders;
   }
 
-  function createFolderAreaAndButton() {
+  function createFolderAreaAndButton(projectColumns) {
     const folderDiv = document.createElement("div");
     folderDiv.className = "folders-container";
     projectColumns.insertBefore(folderDiv, projectColumns.childNodes[0]);
@@ -121,7 +61,7 @@ export default async function ({ addon, global, console, msg }) {
     projectColumns.insertBefore(projectHeader, projectColumns.childNodes[1]);
   }
 
-  function createFolder(name) {
+  function createFolder(name, folderdata) {
     const folderDiv = document.querySelectorAll(".folders")[0];
 
     const noFolderSpan = document.querySelectorAll(".folders>.no-folder")[0];
@@ -131,6 +71,7 @@ export default async function ({ addon, global, console, msg }) {
 
     const folder = document.createElement("button");
     folder.className = "folder";
+    folder.setAttribute('folder-data', folderdata);
     folderDiv.appendChild(folder);
 
     const image = document.createElement("img");
@@ -145,10 +86,12 @@ export default async function ({ addon, global, console, msg }) {
     folder.appendChild(folderName);
 
     folder.addEventListener("click", async () => {
-      const { backdrop, container, content, closeButton, remove } = addon.tab.createModal(folderName.textContent, {
+      const { backdrop, content, closeButton, remove } = addon.tab.createModal(folderName.textContent, {
         isOpen: true,
         useEditorClasses: true,
       });
+
+      const folderData = JSON.parse(folder.getAttribute('folder-data'));
 
       closeButton.addEventListener("click", remove);
       backdrop.addEventListener("click", remove);
@@ -157,34 +100,29 @@ export default async function ({ addon, global, console, msg }) {
       projectDiv.classList.add("sa-folder-projects");
       content.appendChild(projectDiv);
 
-      const folders = foldersArray;
-      let projects;
-      for (let i = 0; i < folders.length; i++) {
-        if (folders[i].name === folderName.textContent) {
-          projects = folders[i].projects;
-          break;
-        }
-      }
-
-      for (let i = 0; i < projects.length; i++) {
-        const projectID = projects[i].link.replace("https://scratch.mit.edu/projects/", "").replace("/", "");
-
-        const project = document.createElement("div");
-        project.classList.add("project");
+      for (let i = 0; i < folderData.projects.length; i++) {
+        const project = document.createElement('div');
+        project.classList.add('project');
         projectDiv.appendChild(project);
 
-        const img = document.createElement("img");
-        img.src = `https://uploads.scratch.mit.edu/get_image/project/${projectID}_200x160.png`;
-        img.classList.add("sa-folder-project-img");
-        project.appendChild(img);
+        const projectIMG = document.createElement('img');
+        projectIMG.classList.add('sa-folder-project-img');
+        projectIMG.src = folderData.projects[i].thumbnail;
+        project.appendChild(projectIMG);
 
-        project.appendChild(document.createElement("br"));
-
-        const projectLink = document.createElement("a");
-        projectLink.textContent = projects[i].name;
-        projectLink.href = projects[i].link;
+        const projectLink = document.createElement('a');
+        projectLink.href = `https://scratch.mit.edu/project/${folderData.projects[i].id}`;
+        projectLink.textContent = folderData.projects[i].name;
         project.appendChild(projectLink);
       }
     });
+  }
+
+  while (true) {
+    const projectColumns = await addon.tab.waitForElement("ul.media-list", {
+      markAsSeen: true,
+    });
+    createFolderAreaAndButton(projectColumns);
+    loadFolders();
   }
 }
