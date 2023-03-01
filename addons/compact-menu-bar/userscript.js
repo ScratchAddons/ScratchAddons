3dd32d53ab114a7478e96b9462f118ea0a5462d4
IQBit <iqnitetechnologies@gmail.com>
2023-03-01 09:14:21 +0100

    Fix span adding

--- addons/compact-menu-bar/userscript.js
+++ addons/compact-menu-bar/userscript.js
@@ -1,14 +1,18 @@
 export default async function ({ addon }) {
   let profile;
 
-  async function addSpan() {
+  async function addSpan(username, profileSpans) {
     // Add username to "Profile" menu option
-    const username = await addon.auth.fetchUsername();
-    const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt");
     profile = profileSpans.appendChild(document.createElement("span"));
     profile.textContent = username;
     profile.className = "sa-profile-name";
   }
 
-  if (await addon.auth.fetchIsLoggedIn()) setInterval(addSpan(), 1);
+  if (await addon.auth.fetchIsLoggedIn()) {
+    const username = await addon.auth.fetchUsername();
+    while (true) {
+      const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt", { markAsSeen: true });
+      addSpan(username, profileSpans);
+    }
+  }
 }
