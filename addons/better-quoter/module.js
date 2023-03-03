ScratchAddons/ScratchAddons

commit 9558611e16e0b3e30fde7cdbf2964aa7979585e7

`better-quoter`: fix compatibility issues (#5240)
* better-quoter: fix forum-id compatability & quote author bug

* Format code

* better-quoter: fix compatability with forums-copy-code

* Format code

* remove unnecessary comments

* set isSetup to true, move globals comment

* Format code

* don't show 'quote post number' on closed topics

* Revert "don't show 'quote post number' on closed topics"

This reverts commit e3e5df27afa62f90e59f3b3f7150bdaa2639e17f.

revert last commit

* fix git

* Revert "fix git"

This reverts commit 6a19130948bc94daeb645f3d4475cb7af90a424c.

oops

* forum-id: early return on closed topics

* Format code

* Remove `global` from addon func

* fix better-quoter dynamic disabke

---------

Co-authored-by: pufferfish101007 <pufferfish101007@users.noreply.github.com>

addons/better-quoter/module.js
@@ -1,0 +1,206 @@
+/* global $, paste */
+
+let forumIdAddon = null;
+let betterQuoterAddon = null;
+let isSetup = false;
+
+export function setupForumId(addon) {
+  if (!forumIdAddon) {
+    forumIdAddon = addon;
+  }
+  setup();
+}
+
+export function setupBetterQuoter(addon) {
+  if (!betterQuoterAddon) {
+    betterQuoterAddon = addon;
+  }
+  setup();
+}
+
+export function getPostText(id, post, selection) {
+  return new Promise((resolve) => {
+    const selectionStr = selection.toString();
+    if (
+      !betterQuoterAddon ||
+      betterQuoterAddon?.self?.disabled ||
+      !(
+        selectionStr &&
+        selection.anchorNode &&
+        post.contains(selection.anchorNode) &&
+        selection.focusNode &&
+        post.contains(selection.focusNode)
+      )
+    ) {
+      $.ajax("/discuss/post/" + id.substr(1) + "/source/").done(function (data) {
+        resolve(data);
+      });
+    } else {
+      resolve(getSelectionBBCode(selection));
+    }
+  });
+}
+
+export function getIDLink(id, name, addSpace) {
+  return `[url=https://scratch.mit.edu/discuss/post/${id}/]${name}[/url]${addSpace ? " " : ""}`;
+}
+
+function getSelectionBBCode(selection) {
+  const html = document.createElement("div");
+  if (selection.rangeCount > 0) {
+    // if something is selected
+    const range = selection.getRangeAt(0);
+    const clonedSelection = range.cloneContents();
+    html.appendChild(clonedSelection);
+  } else {
+    // nothing is selected
+    return "";
+  }
+
+  // new lines
+  const lineBreaks = html.querySelectorAll("br");
+  for (const br of lineBreaks) br.insertAdjacentText("afterend", "\n");
+
+  // images and smileys
+  const smilieReplaces = Object.assign(Object.create(null), {
+    smile: ":)",
+    neutral: ":|",
+    sad: ":(",
+    big_smile: ":D",
+    yikes: ":o",
+    wink: ";)",
+    hmm: ":/",
+    tongue: ":P",
+    lol: ":lol:",
+    mad: ":mad:",
+    roll: ":rolleyes",
+    cool: ":cool:",
+  });
+
+  const imgs = html.querySelectorAll("img");
+  for (const img of imgs) {
+    if (
+      /\/\/cdn\.scratch\.mit\.edu\/scratchr2\/static\/__[a-z0-9]{32}__\/djangobb_forum\/img\/smilies\/[a-z_]{3,9}\.png/.test(
+        img.src
+      )
+    ) {
+      if (smilieReplaces[img.src.split("smilies/")[1].split(".")[0]]) {
+        img.parentNode.insertBefore(
+          document.createTextNode(smilieReplaces[img.src.split("smilies/")[1].split(".")[0]]),
+          img
+        );
+      } else img.parentNode.insertBefore(document.createTextNode(`[img${img.src}[/img]`), img);
+    } else img.parentNode.insertBefore(document.createTextNode(`[img]${img.src}[/img]`), img);
+  }
+
+  // bold, italic, underline, strikethrough, big, small and color
+  const bbReplaces = {
+    italic: "i",
+    bold: "b",
+    big: "big",
+    small: "small",
+    underline: "u",
+    strikethrough: "s",
+  };
+  const spans = html.querySelectorAll("span");
+  for (const span of spans) {
+    if (span.className.startsWith("bb-")) {
+      span.insertAdjacentText("afterbegin", `[${bbReplaces[span.className.slice(3)]}]`);
+      span.insertAdjacentText("beforeend", `[/${bbReplaces[span.className.slice(3)]}]`);
+    } else if (span.style.color) {
+      const color = span.style.color;
+
+      function componentToHex(c) {
+        const hex = c.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+      }
+
+      function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+      }
+
+      if (color.startsWith("rgb")) {
+        const rgbValues = color
+          .slice(4, color.length - 1)
+          .split(/, ?/)
+          .map((x) => Number(x));
+
+        span.insertAdjacentText("afterbegin", `[color=${rgbToHex(...rgbValues).toUpperCase()}]`);
+      } else span.insertAdjacentText("afterbegin", `[color=${color}]`);
+      span.insertAdjacentText("beforeend", "[/color]");
+    }
+  }
+
+  // links
+  // todo: try and guess where dictionary/wiki/wp etc. tags are being used?
+  const links = html.querySelectorAll("a");
+  for (const link of links) {
+    link.insertAdjacentText("afterbegin", `[url=${link.href}]`);
+    link.insertAdjacentText("beforeend", "[/url]");
+  }
+
+  // center
+  const divs = html.querySelectorAll("div");
+  for (const div of divs) {
+    if (div.style.textAlign === "center") {
+      div.insertAdjacentText("afterbegin", "[center]");
+      div.insertAdjacentText("beforeend", "[/center]");
+    }
+  }
+
+  // lists
+  const lis = html.querySelectorAll("li");
+  for (const li of lis) li.textContent = `[*]${li.textContent}`;
+  const uls = html.querySelectorAll("ul");
+  for (let ul of uls) ul.textContent = `[list]\n${ul.textContent}[/list]\n`;
+  const ols = html.querySelectorAll("ol");
+  for (const ol of ols) ol.textContent = `[list=1]\n${ol.textContent}[/list]\n`;
+
+  // scratchblocks
+  const scratchBlocksPres = html.getElementsByClassName("blocks");
+  for (const pre of scratchBlocksPres) {
+    pre.textContent = `[scratchblocks]\n${pre.getAttribute("data-original")}\n[/scratchblocks]`; // cs.js manages data-original because 3.0 scratchblocks
+  }
+
+  // code blocks
+  const codeBlocks = html.querySelectorAll("div.code");
+  for (const codeBlock of codeBlocks) codeBlock.textContent = `[code]\n${codeBlock.textContent}[/code]\n`;
+
+  // quotes
+  const quotes = html.querySelectorAll("blockquote");
+  for (const quote of quotes) {
+    const author = quote.querySelector("p.bb-quote-author");
+    const authorText = !author || author?.textContent?.length < 8 ? "" : author.textContent;
+    if (author) author.textContent = "";
+    quote.insertAdjacentText(
+      "afterbegin",
+      "[quote" + (authorText ? `=${authorText.slice(0, authorText.length - 7)}]\n` : "]\n")
+    );
+    quote.insertAdjacentText("beforeend", "[/quote]\n");
+  }
+
+  // remove any 'copy code' buttons added by forums-copy-code
+  const copyCodeBtns = html.querySelectorAll("div.sa-copyCodeDiv");
+  for (const ccbtn of copyCodeBtns) {
+    ccbtn.textContent = "";
+  }
+
+  return html.textContent;
+}
+
+function setup() {
+  if (isSetup) return;
+  isSetup = true;
+  const originalCopyPaste = window.copy_paste;
+  window.copy_paste = function (id) {
+    const post = $("#" + id);
+    const username = post.find(".username").text();
+    const idText =
+      !forumIdAddon?.self?.disabled && forumIdAddon?.settings?.get?.("auto_add")
+        ? getIDLink(id.substring(1), post["0"].querySelector(".box-head > .conr").textContent, false)
+        : "";
+    getPostText(id, post[0], window.getSelection()).then((text) => {
+      paste(`[quote=${username}]${idText}\n${text}\n[/quote]\n`);
+    });
+  };
+}

addons/better-quoter/userscript.js
@@ -1,163 +1,4 @@
-/* global copy_paste */
+import { setupBetterQuoter } from "./module.js";
 export default async function ({ addon, console }) {
-  function getSelectionBBCode() {
-    var selection = window.getSelection();
-    if (selection.rangeCount > 0) {
-      // if something is selected
-      let range = selection.getRangeAt(0);
-      var clonedSelection = range.cloneContents();
-      var html = document.createElement("div");
-      html.appendChild(clonedSelection);
-    } else {
-      // nothing is selected
-      return "";
-    }
-
-    // new lines
-    let lineBreaks = html.querySelectorAll("br");
-    for (let br of lineBreaks) br.insertAdjacentText("afterend", "\n");
-
-    // images and smileys
-    let smilieReplaces = Object.assign(Object.create(null), {
-      smile: ":)",
-      neutral: ":|",
-      sad: ":(",
-      big_smile: ":D",
-      yikes: ":o",
-      wink: ";)",
-      hmm: ":/",
-      tongue: ":P",
-      lol: ":lol:",
-      mad: ":mad:",
-      roll: ":rolleyes",
-      cool: ":cool:",
-    });
-
-    let imgs = html.querySelectorAll("img");
-    for (let img of imgs) {
-      if (
-        /\/\/cdn\.scratch\.mit\.edu\/scratchr2\/static\/__[a-z0-9]{32}__\/djangobb_forum\/img\/smilies\/[a-z_]{3,9}\.png/.test(
-          img.src
-        )
-      ) {
-        if (smilieReplaces[img.src.split("smilies/")[1].split(".")[0]]) {
-          img.parentNode.insertBefore(
-            document.createTextNode(smilieReplaces[img.src.split("smilies/")[1].split(".")[0]]),
-            img
-          );
-        } else img.parentNode.insertBefore(document.createTextNode(`[img${img.src}[/img]`), img);
-      } else img.parentNode.insertBefore(document.createTextNode(`[img]${img.src}[/img]`), img);
-    }
-
-    // bold, italic, underline, strikethrough, big, small and color
-    let bbReplaces = {
-      italic: "i",
-      bold: "b",
-      big: "big",
-      small: "small",
-      underline: "u",
-      strikethrough: "s",
-    };
-    let spans = html.querySelectorAll("span");
-    for (let span of spans) {
-      if (span.className.startsWith("bb-")) {
-        span.insertAdjacentText("afterbegin", `[${bbReplaces[span.className.slice(3)]}]`);
-        span.insertAdjacentText("beforeend", `[/${bbReplaces[span.className.slice(3)]}]`);
-      } else if (span.style.color) {
-        let color = span.style.color;
-
-        function componentToHex(c) {
-          var hex = c.toString(16);
-          return hex.length === 1 ? "0" + hex : hex;
-        }
-
-        function rgbToHex(r, g, b) {
-          return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-        }
-
-        if (color.startsWith("rgb")) {
-          let rgbValues = color
-            .slice(4, color.length - 1)
-            .split(/, ?/)
-            .map((x) => Number(x));
-
-          span.insertAdjacentText("afterbegin", `[color=${rgbToHex(...rgbValues).toUpperCase()}]`);
-        } else span.insertAdjacentText("afterbegin", `[color=${color}]`);
-        span.insertAdjacentText("beforeend", "[/color]");
-      }
-    }
-
-    // links
-    // todo: try and gues where dictionary/wiki/wp etc. tags are being used?
-    let links = html.querySelectorAll("a");
-    for (let link of links) {
-      link.insertAdjacentText("afterbegin", `[url=${link.href}]`);
-      link.insertAdjacentText("beforeend", "[/url]");
-    }
-
-    // center
-    let divs = html.querySelectorAll("div");
-    for (let div of divs) {
-      if (div.style.textAlign === "center") {
-        div.insertAdjacentText("afterbegin", "[center]");
-        div.insertAdjacentText("beforeend", "[/center]");
-      }
-    }
-
-    // lists
-    let lis = html.querySelectorAll("li");
-    for (let li of lis) li.textContent = `[*]${li.textContent}`;
-    let uls = html.querySelectorAll("ul");
-    for (let ul of uls) ul.textContent = `[list]\n${ul.textContent}[/list]\n`;
-    let ols = html.querySelectorAll("ol");
-    for (let ol of ols) ol.textContent = `[list=1]\n${ol.textContent}[/list]\n`;
-
-    // scratchblocks
-    let scratchBlocksPres = html.getElementsByClassName("blocks");
-    for (let pre of scratchBlocksPres) {
-      pre.textContent = `[scratchblocks]\n${pre.getAttribute("data-original")}\n[/scratchblocks]`; // cs.js manages data-original because 3.0 scratchblocks
-    }
-
-    // code blocks
-    let codeBlocks = html.querySelectorAll("div.code");
-    for (let codeBlock of codeBlocks) codeBlock.textContent = `[code]\n${codeBlock.textContent}[/code]\n`;
-
-    // quotes
-    let quotes = html.querySelectorAll("blockquote");
-    for (let quote of quotes) {
-      let author = quote.querySelector("p.bb-quote-author");
-      if (author)
-        quote.textContent = `[quote=${author.textContent.slice(0, author.textContent.length - 7)}]\n${
-          quote.textContent
-        }[/quote]\n`;
-      else quote.textContent = `[quote]\n${quote.textContent}[/quote]\n`;
-    }
-
-    return html.textContent;
-  }
-
-  let textarea = document.querySelector(".markItUpEditor");
-  while (true) {
-    let quoteButton = await addon.tab.waitForElement(".postquote a", { markAsSeen: true });
-    quoteButton.setAttribute("onclick", "return false");
-    quoteButton.addEventListener("mouseup", (e) => {
-      let blockpost = quoteButton.closest(".blockpost");
-      if (addon.self.disabled) return copy_paste(blockpost.id);
-      let selection = window.getSelection();
-      let selectionStr = selection.toString();
-      if (
-        selectionStr &&
-        selection.anchorNode &&
-        blockpost.contains(selection.anchorNode) &&
-        selection.focusNode &&
-        blockpost.contains(selection.focusNode)
-      )
-        textarea.value += `[quote=${
-          blockpost.querySelector(".black.username").innerText
-        }]${getSelectionBBCode()}[/quote]`;
-      else copy_paste(blockpost.id);
-      window.location.hash = "#reply";
-      textarea.focus();
-    });
-  }
+  setupBetterQuoter(addon);
 }

addons/forum-id/userscript.js
@@ -1,5 +1,7 @@
-/* global $, paste */
+import { setupForumId, getIDLink } from "../better-quoter/module.js";
 export default async function ({ addon, console, msg }) {
+  if (!document.querySelector("textarea")) return;
+  setupForumId(addon);
   const buttons = document.querySelectorAll(".postfootright");
   buttons.forEach(function (elm) {
     const addBtn = document.createElement("li");
@@ -16,28 +18,4 @@ export default async function ({ addon, console, msg }) {
     let id = e.target.closest(".blockpost").id.substring(1);
     window.paste(getIDLink(id, idName, true));
   }
-  function getIDLink(id, name, addSpace) {
-    return `[url=https://scratch.mit.edu/discuss/post/${id}/]${name}[/url]${addSpace ? " " : ""}`;
-  }
-  //Auto-adding IDs to quotes
-  const originalCopyPaste = window.copy_paste;
-  window.copy_paste = function (id) {
-    if (addon.self.disabled || !addon.settings.get("auto_add")) {
-      originalCopyPaste(id);
-      return;
-    }
-    var post = $("#" + id);
-    var username = post.find(".username").text();
-    $.ajax("/discuss/post/" + id.substr(1) + "/source/").done(function (data) {
-      paste(
-        "[quote=" +
-          username +
-          "][small](" +
-          getIDLink(id.substring(1), post["0"].querySelector(".box-head > .conr").textContent, false) +
-          ")[/small]\n" +
-          data +
-          "[/quote]\n"
-      );
-    });
-  };
 }

commit 852269aee0cdf5dffc00e110bd366325f4dffb2c

forum-id: auto-add post number small & in brackets
Fixes a regression caused by #5240 that made the link added ny `forum-id` with the 'auto add post number' setting enabled large and without brackets

addons/better-quoter/module.js
@@ -197,7 +197,7 @@ function setup() {
     const username = post.find(".username").text();
     const idText =
       !forumIdAddon?.self?.disabled && forumIdAddon?.settings?.get?.("auto_add")
-        ? getIDLink(id.substring(1), post["0"].querySelector(".box-head > .conr").textContent, false)
+        ? `[small](${getIDLink(id.substring(1), post["0"].querySelector(".box-head > .conr").textContent, false)})[/small]`
         : "";
     getPostText(id, post[0], window.getSelection()).then((text) => {
       paste(`[quote=${username}]${idText}\n${text}\n[/quote]\n`);