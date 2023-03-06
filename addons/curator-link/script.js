3660c84a53a142162fe5dfd9751378a8da8ed260
Samq64 <81489795+Samq64@users.noreply.github.com>
2023-03-06 16:29:16 -0500

    Dynamic settings

--- addons/curator-link/script.js
+++ addons/curator-link/script.js
@@ -17,38 +17,45 @@ export default async function ({ addon, console }) {
     link.href = `https://scratch.mit.edu/users/${curator}/`;
     link.id = "curator-link";
 
-    if (!addon.self.disabled) {
+    if (!addon.self.disabled) linkifyCurator();
+
+    addon.self.addEventListener("disabled", () => unLinkifyCurator());
+    addon.self.addEventListener("reenabled", () => linkifyCurator());
+    addon.settings.addEventListener("change", () => {
+      (addon.settings.get("curator")) ? linkifyCurator() : unLinkifyCurator();
+    });
+
+    function linkifyCurator() {
       heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
         "{curatorId}",
         link.outerHTML
       );
     }
-    addon.self.addEventListener("disabled", () => {
+    function unLinkifyCurator() {
       heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
         "{curatorId}",
         curator
       );
-    });
-    addon.self.addEventListener("reenabled", () => {
-      heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
-        "{curatorId}",
-        link.outerHTML
-      );
-    });
+    }
   }
 
   if (pageType === "discuss" && addon.settings.get("mentions")) {
     const linkified = [];
-    addon.self.addEventListener("disabled", () => {
-      for (const { element, original } of linkified) {
-        element.innerHTML = original;
-      }
+    addon.self.addEventListener("disabled", () => unLinkifyMentions());
+    addon.self.addEventListener("reenabled", () => linkifyMentions());
+    addon.settings.addEventListener("change", () => {
+      (addon.settings.get("mentions")) ? linkifyMentions() : unLinkifyMentions();
     });
-    addon.self.addEventListener("reenabled", () => {
+    function linkifyMentions() {
       for (const { element } of linkified) {
         pingifyTextNode(element);
       }
-    });
+    }
+    function unLinkifyMentions() {
+      for (const { element, original } of linkified) {
+        element.innerHTML = original;
+      }
+    }
     function pingify(element) {
       linkified.push({
         element,
