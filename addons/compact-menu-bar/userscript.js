ce310510f5afaaaf17357b05d201518907993691
IQBit <iqnitetechnologies@gmail.com>
2023-03-10 20:19:10 +0100

    Fixed multiple showing bug

--- addons/compact-menu-bar/userscript.js
+++ addons/compact-menu-bar/userscript.js
@@ -1,20 +1,19 @@
 export default async function ({ addon }) {
-  let profile;
-
   async function addSpan(username, profileSpans) {
     // Add username to "Profile" menu option
-    profile = profileSpans.appendChild(document.createElement("span"));
+    const profile = profileSpans.appendChild(document.createElement("span"));
     profile.textContent = username;
     profile.className = "sa-profile-name";
   }
 
   if (await addon.auth.fetchIsLoggedIn()) {
     const username = await addon.auth.fetchUsername();
-    while (true) {
-      const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt", {
-        markAsSeen: true,
-      });
+    addon.tab.redux.initialize();
+    addon.tab.redux.addEventListener("statechanged", async (e) => {
+      if (e.detail.action.type !== "scratch-gui/menus/OPEN_MENU") return;
+      if (e.detail.action.menu !== "accountMenu") return;
+      const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt");
       addSpan(username, profileSpans);
-    }
+    })
   }
 }
