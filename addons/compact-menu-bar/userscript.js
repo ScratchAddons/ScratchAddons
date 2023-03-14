b8167c6d3a51afaf10c15ac7c1545b2f2376c394
IQBit <iqnitetechnologies@gmail.com>
2023-03-14 12:21:01 +0100

    Add support for dynamic enable

--- addons/compact-menu-bar/userscript.js
+++ addons/compact-menu-bar/userscript.js
@@ -1,17 +1,14 @@
-export default async function ({ addon }) {
-  async function addSpan(username, profileSpans) {
-    const profile = profileSpans.appendChild(document.createElement("span")); // Add username to "Profile" menu option
-    profile.textContent = username;
-    profile.className = "sa-profile-name";
-  }
-
+export default async function ({ addon }) {  
   if (await addon.auth.fetchIsLoggedIn()) {
     const username = await addon.auth.fetchUsername();
     addon.tab.redux.initialize(); // Start listening to Redux events
     addon.tab.redux.addEventListener("statechanged", async (e) => {
-      if (e.detail.action.type !== "scratch-gui/menus/OPEN_MENU" || e.detail.action.menu !== "accountMenu") return; // Leave if another Redux event occurs
+      if (addon.self.disabled) return; // Skip if the addon is disabled
+      if (e.detail.action.type !== "scratch-gui/menus/OPEN_MENU" || e.detail.action.menu !== "accountMenu") return; // Skip if another Redux event occurs
       const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt"); // Do this if the menu is opened
-      addSpan(username, profileSpans);
+      const profile = profileSpans.appendChild(document.createElement("span")); // Add username to "Profile" menu option
+      profile.textContent = username;
+      profile.className = "sa-profile-name";
     });
   }
 }
