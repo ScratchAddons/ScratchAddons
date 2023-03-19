8bf9f52d1b72534b1bf374f80581bbb8eb8f7eae
WorldLanguages <mundofinky@gmail.com>
2023-03-19 15:55:37 -0300

    Use "const" instead of "let"

--- addons/remember-collapsed-categories/userscript.js
+++ addons/remember-collapsed-categories/userscript.js
@@ -5,10 +5,10 @@ export default async function ({ console, addon }) {
     });
     console.log(`Found category ${categoryBody.id}`);
 
-    let categoryCollapse = categoryBody.querySelector(".toggle");
+    const categoryCollapse = categoryBody.querySelector(".toggle");
     categoryCollapse.addEventListener("click", (event) => {
       event.preventDefault();
-      let categoryHead = event.target.parentElement;
+      const categoryHead = event.target.parentElement;
       categoryHead.classList.toggle("sa-collapsed");
       if (categoryHead.classList.contains("sa-collapsed")) {
         console.log(`${categoryHead.id.split("_")[2]} is being opened`);
@@ -18,38 +18,38 @@ export default async function ({ console, addon }) {
       }
     });
 
-    let categoryNumber = categoryBody.id.split("_").pop();
+    const categoryNumber = categoryBody.id.split("_").pop();
     if (!shouldCollapseCategory(categoryNumber)) {
       removeCategoryCookie(categoryNumber);
       continue;
     }
     extendCategoryCookieExistenceLength(categoryNumber);
 
-    let categoryContent = categoryBody.querySelector(".box-content");
+    const categoryContent = categoryBody.querySelector(".box-content");
     categoryContent.style.display = "none";
 
-    let categoryHead = categoryBody.querySelector(`.box-head h4`);
+    const categoryHead = categoryBody.querySelector(`.box-head h4`);
     categoryHead.classList.add("collapsed");
     categoryHead.classList.add("sa-collapsed");
 
-    let categoryName = categoryHead.innerText.split("\n")[1]; // This is exclusively for debug purposes
+    const categoryName = categoryHead.innerText.split("\n")[1]; // This is exclusively for debug purposes
     console.log(`Category ${categoryName}(#${categoryNumber}) should be hidden`);
   }
 
   function shouldCollapseCategory(categoryNumber) {
-    let cookieName = `category_body_${categoryNumber}`;
-    let storedInCookie = document.cookie.includes(`${cookieName}=collapsed`);
+    const cookieName = `category_body_${categoryNumber}`;
+    const storedInCookie = document.cookie.includes(`${cookieName}=collapsed`);
     return storedInCookie;
   }
 
   function extendCategoryCookieExistenceLength(categoryNumber) {
-    let cookieName = `category_body_${categoryNumber}`;
+    const cookieName = `category_body_${categoryNumber}`;
     console.log(`Extending ${cookieName} for an extra 30 days`);
     document.cookie = `${cookieName}=collapsed;max-age=${30 * 24 * 60 * 60}`;
   }
 
   function removeCategoryCookie(categoryNumber) {
-    let cookieName = `category_body_${categoryNumber}`;
+    const cookieName = `category_body_${categoryNumber}`;
     console.log(`Removing ${cookieName} due to the category being closed.`);
     document.cookie = `${cookieName}=;max-age=0;`;
   }
