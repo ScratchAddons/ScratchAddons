4abf3dd354e2c2483dca7c31d8859b30a8231e66
StioStudio <simon.s.johanssen@gmail.com>
2023-03-23 14:04:30 +0100

    compact buttons done

--- addons/compact-buttons/button.js
+++ addons/compact-buttons/button.js
@@ -1,8 +1,31 @@
 
 export default async function ({ addon, console, msg }) {
+    // let rem = document.querySelector(".project-buttons")
+    // let button = (await (await addon.tab.waitForElement(".banner-button")))
+    // rem.insertBefore(button, rem.firstChild)
+    // button.classList = ["banner-outer", "flex-row inner", "banner-inner", "button", "banner-button"]
+    // await (await addon.tab.waitForElement(".banner-outer")).remove()
 
-    let rem = document.querySelector(".project-buttons")
-    await rem.insertBefore((await (await addon.tab.waitForElement(".banner-button"))), rem.firstChild)
-    await (await addon.tab.waitForElement(".banner-outer")).remove()
+    // let addon = {tab:{waitForElement(e){document.querySelector(e)}}} 
+
+    // function loadImage(_url) {
+    //     let img;
+    //     new Promise((resolve) => {
+    //         img = new Image();
+    //         img.onload = () => resolve(img);
+    //         img.src = _url
+    //     });
+    //     return img
+    // }
+
+    // let rem = (await (await addon.tab.waitForElement(".menu-bar_main-menu_3wjWH")))
+    // console.log(rem)
+
+    // let rem = (await (await addon.tab.waitForElement(".menu-bar_main-menu_3wjWH"))).children
+    // let hi = rem[5].querySelector("span")
+    // hi.classList.add(".banner-button")
+    // let a = document.createElement("div")
+    // a.classList.add("before")
+    // hi.appendChild(a)
 
 }
