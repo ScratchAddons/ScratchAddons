e959dc1de38a217c04540a0e5ef9119ab4f71bb5
Emma <128169269+emma144@users.noreply.github.com>
2023-04-02 20:52:55 +0300

    Debug & fix

--- addons/auto-bumper/userscript.js
+++ addons/auto-bumper/userscript.js
@@ -4,10 +4,10 @@ export default async function ({ addon, console }) {
   const button = document.createElement('button');
   button.textContent = 'Bump';
   button.style.float = 'right';
-  document.querySelector('.form-submit').append(span);
+  document.querySelector('.form-submit').append(button);
   
-  let textarea = document.querySelector(type === "settings" ? "#id_signature" : "#id_body");
-  let postButton = document.querySelector(type === "topic" ? ".button.grey:nth-child(1)" : "button");
+  let textarea = document.querySelector('.markItUpEditor');
+  let postButton = document.querySelector('.form-submit').firstElementChild;
   
   // listen to the bump button click
   button.addEventListener("click", async () => {
