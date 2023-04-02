0626ad7948ee3345cb4aa167f5a05e666418c7f7
Emma <128169269+emma144@users.noreply.github.com>
2023-04-02 21:00:31 +0300

    Update userscript.js

--- addons/auto-bumper/userscript.js
+++ addons/auto-bumper/userscript.js
@@ -2,9 +2,15 @@ export default async function ({ addon, console }) {
   
   // create a button and append it
   const button = document.createElement('button');
-  button.textContent = 'Bump';
   button.style.float = 'right';
+  button.classList.add('grey');
+  button.classList.add('button');
   document.querySelector('.form-submit').append(button);
+  
+  // add the button content
+  const span = document.createElement('span');
+  span.textContent = 'Bump';
+  button.append(span);
   
   let textarea = document.querySelector('.markItUpEditor');
   let postButton = document.querySelector('.form-submit').firstElementChild;
