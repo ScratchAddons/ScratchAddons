3f3164ab2180f1df463774ad645743043d7136dd
mdwalters <mail@mdwalters.ml>
2023-04-16 03:25:19 -0400

    chore: resolve some reviews - Use `div` instead of `br` and `span` - Rename badge from "OP" to "Original poster" - Add localization files

--- addons/op-badge/userscript.js
+++ addons/op-badge/userscript.js
@@ -1,16 +1,14 @@
-export default async function({ addon, console }) {
+export default async function({ addon, console, msg }) {
     const posts = document.querySelectorAll(".blockpost");
 
     const op = await fetch(`https://scratchdb.lefty.one/v3/forum/topic/posts/${window.location.href.split("/")[5]}/0?o=oldest`).then(res => res.json());
 
     for (const i of posts) {
         if (i.querySelector(".username").innerText == op[0].username) {
-            let br = document.createElement("br");
-            let op_badge = document.createElement("span");
+            let op_badge = document.createElement("div");
 
-            op_badge.innerText = "OP";
+            op_badge.innerText = msg("op");
 
-            i.querySelector(".postleft").querySelector("dl").appendChild(br);
             i.querySelector(".postleft").querySelector("dl").appendChild(op_badge);
         }
     }
