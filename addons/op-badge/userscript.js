547094ce15fffb9279d6fecdfacae7445802c313
mdwalters <mail@mdwalters.ml>
2023-04-17 16:37:24 -0400

    fix: make the code shorter

--- addons/op-badge/userscript.js
+++ addons/op-badge/userscript.js
@@ -1,12 +1,12 @@
 export default async function({ addon, console, msg }) {
     const posts = document.querySelectorAll(".blockpost");
 
-    if (window.location.href.split("?").join("").split("&")[0] == "page=1" || window.location.href.split("?").join("").split("&").length == 0) {
+    if (window.location.href.split("?")[1] == "page=1" || window.location.href.split("?").length == 1) {
         const html = document.querySelectorAll(".blockpost");
-        const op = html[0].querySelector(".username").innerText;
+        var op = html[0].querySelector(".username").innerText;
     } else {
         const html = new DOMParser().parseFromString(await fetch(`https://scratch.mit.edu/discuss/topic/${window.location.href.split("/")[5]}/`).then(res => res.text()), "text/html");
-        const op = html.querySelectorAll(".blockpost")[0].querySelector(".username").innerText;
+        var op = html.querySelectorAll(".blockpost")[0].querySelector(".username").innerText;
     }
 
     for (const i of posts) {
