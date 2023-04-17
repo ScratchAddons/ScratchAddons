be7fe3c9e9aadf71967bf60513bb599c93d41a1d
M.D. Walters <popularvsofficial@gmail.com>
2023-04-17 13:08:46 -0400

    fix: do `document` on first page instead of `DOMParser` Howerver, I cannot resolve it, as I never tested it yet

--- addons/op-badge/userscript.js
+++ addons/op-badge/userscript.js
@@ -1,8 +1,13 @@
 export default async function({ addon, console, msg }) {
     const posts = document.querySelectorAll(".blockpost");
 
-    const html = new DOMParser().parseFromString(await fetch(`https://scratch.mit.edu/discuss/topic/${window.location.href.split("/")[5]}/`).then(res => res.text()), "text/html");
-    const op = html.querySelectorAll(".blockpost")[0].querySelector(".username").innerText;
+    if (window.location.href.split("?").join("").split("&")[0] == "page=1" || window.location.href.split("?").join("").split("&").length == 0) {
+        const html = document.querySelectorAll(".blockpost");
+        const op = html[0].querySelector(".username").innerText;
+    } else {
+        const html = new DOMParser().parseFromString(await fetch(`https://scratch.mit.edu/discuss/topic/${window.location.href.split("/")[5]}/`).then(res => res.text()), "text/html");
+        const op = html.querySelectorAll(".blockpost")[0].querySelector(".username").innerText;
+    }
 
     for (const i of posts) {
         if (op == i.querySelectorAll(".username")[0].innerText) {
