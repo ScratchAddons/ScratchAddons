1ea4fc4b5c4d2e9d80ad21c7ca9cf973261aac40
WorldLanguages <mundofinky@gmail.com>
2023-04-28 12:01:32 -0300

    Use GarboMuffin's solution instead of waitForElement

--- addons/move-to-top-layer/userscript.js
+++ addons/move-to-top-layer/userscript.js
@@ -2,22 +2,14 @@
 
 export default async function ({ addon, console }) {
   const vm = addon.tab.traps.vm;
-
-  while (true) {
-    const spriteList = await addon.tab.waitForElement("div[class^='sprite-selector_items-wrapper']", {
-      markAsSeen: true,
-      reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
-      reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
-    });
-
-    spriteList.addEventListener("click", (e) => {
-      if (e.shiftKey && !addon.self.disabled) {
-        // get the sprite thumbnail closest to the click
-        const parentDiv = e.target.closest("div[class^='sprite-selector_sprite-wrapper']");
+  document.body.addEventListener("click", (e) => {
+    if (e.shiftKey && !addon.self.disabled) {
+      const parentDiv = e.target.closest("div[class^='sprite-selector_sprite-wrapper']");
+      if (parentDiv) {
         const spriteName = parentDiv.querySelector("div[class^='sprite-selector-item_sprite-name']").innerText;
         // move the sprite with that name to front
         vm.runtime.getSpriteTargetByName(spriteName).goToFront();
       }
-    });
-  }
+    }
+  });
 }
