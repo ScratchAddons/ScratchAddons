994a0481e1a972ccf6c3899fb275c44f3b69c491
Tacodiva <27910867+Tacodiva@users.noreply.github.com>
2023-04-30 11:31:22 +1000

    Clean up

--- addons/faster-project-loading/userscript.js
+++ addons/faster-project-loading/userscript.js
@@ -1,9 +1,10 @@
 // This is a fix for https://github.com/LLK/scratch-gui/issues/8805
 
-export default async function ({ addon, console, msg }) {
+export default async function ({ addon }) {
   const BACKPACK_URL = "https://backpack.scratch.mit.edu/";
+
   const originalOpen = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function (method, url) {
+  XMLHttpRequest.prototype.open = function (method, url, ...moreArgs) {
     if (!addon.self.disabled && method === "GET" && url.startsWith(BACKPACK_URL)) {
       /*
               We don't want to block actual requests for backpack assets.
@@ -12,19 +13,18 @@ export default async function ({ addon, console, msg }) {
               We can check for the '?' to differentiate them from wrong requests which look like:
                 https://backpack.scratch.mit.edu/de9e1ed35f087dcb297f1339b16deaf6.svg
             */
-
-      if (url.indexOf("?") == -1) throw new Error("Request blocked by Scratch Addons faster project loading. ");
+      if (!url.includes("?")) throw new Error("Request blocked by Scratch Addons faster project loading. ");
     }
-    return originalOpen.call(this, method, url);
+    originalOpen.call(this, method, url, ...moreArgs);
   };
 
   const originalPostMessage = Worker.prototype.postMessage;
-  Worker.prototype.postMessage = function (message, options) {
+  Worker.prototype.postMessage = function (message, options, ...moreArgs) {
     if (!addon.self.disabled && message && typeof message.id === "string" && typeof message.url === "string") {
       if (message.url.startsWith(BACKPACK_URL)) {
         throw new Error("Request blocked by Scratch Addons faster project loading.");
       }
     }
-    originalPostMessage.call(this, message, options);
+    originalPostMessage.call(this, message, options, ...moreArgs);
   };
 }
