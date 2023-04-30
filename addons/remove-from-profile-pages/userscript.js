f0b6572ad55b7671447d56f3feb0f0b17558480b
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-04-30 16:45:37 +0200

    Remove unnecessary console import

--- addons/remove-from-profile-pages/userscript.js
+++ addons/remove-from-profile-pages/userscript.js
@@ -1,4 +1,4 @@
-export default async function ({ addon, console, msg }) {
+export default async function ({ addon, msg }) {
   const lastPathFromURL = /\/([^\/]*)\/$/;
   if (
     !(await addon.auth.fetchIsLoggedIn()) ||
