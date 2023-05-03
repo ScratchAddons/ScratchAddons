c86821822924a8b550f36a5e6a839ce9db05b86a
Samq64 <81489795+Samq64@users.noreply.github.com>
2023-05-03 13:39:44 -0400

    format code

--- addons/scroll-project-titles/script.js
+++ addons/scroll-project-titles/script.js
@@ -1,7 +1,7 @@
 export default async function ({ addon, console, msg }) {
   addon.self.addEventListener("disabled", function() {
     if (document.querySelector(".project-title.no-edit")) {
-      document.querySelector(".project-title.no-edit").scrollLeft = 0
+      document.querySelector(".project-title.no-edit").scrollLeft = 0;
     }
   });
 }
