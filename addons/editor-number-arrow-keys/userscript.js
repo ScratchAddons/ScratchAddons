60092b6cb8fe129803283765042ffd85313deafa
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-05-07 11:46:42 +0200

    Fix  0.1+0.2=0.30000000000000004

--- addons/editor-number-arrow-keys/userscript.js
+++ addons/editor-number-arrow-keys/userscript.js
@@ -9,9 +9,14 @@ export default async function () {
           return;
         }
         e.preventDefault();
-        elt.value =
-          Number(elt.value) +
-          (e.altKey ? 0.1 : e.shiftKey ? 10 : e.ctrlKey ? 100 : 1) * (e.code === "ArrowUp" ? 1 : -1);
+        if (e.altKey) {
+          // Because of JavaScript math like 0.1+0.2=0.30000000000000004
+          elt.value = (Number(elt.value) * 10 + 1) / 10;
+        } else {
+          elt.value =
+            Number(elt.value) +
+            (e.altKey ? 0.1 : e.shiftKey ? 10 : e.ctrlKey ? 100 : 1) * (e.code === "ArrowUp" ? 1 : -1);
+        }
       });
       elt.classList.add("sa-editor-number-arrow-keys");
     });
