e71e694b88b5690f3652f5df5af9de7554c4dded
DNin01 <106490990+DNin01@users.noreply.github.com>
2023-05-10 18:57:11 -0700

    Fixes

--- libraries/common/global-theme.js
+++ libraries/common/global-theme.js
@@ -1,8 +1,8 @@
 export default function () {
   const prerelease = chrome.runtime.getManifest().version_name.includes("-prerelease");
   if (prerelease) {
-    const blue = getComputedStyle(document.documentElement).getPropertyValue("--prerelease-brand-color");
-    document.documentElement.style.setProperty("--brand-color", blue);
+    document.documentElement.style.setProperty("--brand-color", "var(--prerelease-brand-color)");
+    document.documentElement.style.setProperty("--brand-color-light", "var(--prerelease-brand-color-light)");
     const favicon = document.getElementById("favicon");
     if (favicon) favicon.href = chrome.runtime.getURL("/images/icon-blue.png");
   }
