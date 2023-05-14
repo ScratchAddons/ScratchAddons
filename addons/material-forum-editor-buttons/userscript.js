81922d754524dea33d1232d3c5fb7aba3fced414
just-a-hriday <just.a.hriday@gmail.com>
2023-05-14 08:32:50 +0530

    minimum viable product

--- addons/material-forum-editor-buttons/userscript.js
+++ addons/material-forum-editor-buttons/userscript.js
@@ -1,33 +1,49 @@
 export default async function ({ addon, console, msg }) {
     let style = addon.settings.get("style") + "";
+    document.querySelector(":root").style.setProperty("--filledMaterialIcons", style === "fill" ? 1 : 0);
 
-    let buttons = document.querySelectorAll(".markItUpHeader")[0].getElementsByTagName("ul")[0].querySelectorAll(":scope > .markItUpButton");;
+    let buttons = document.querySelectorAll(".markItUpHeader .markItUpButton");
 
     buttons.forEach(button => {
-        // console.log(button.innerHTML);
-
         let span = document.createElement("span");
         span.textContent = getIconName(button, 1);
         span.classList.add("materialIcon");
         span.id = "buttonIcon";
 
-        let aTag = button.getElementsByTagName("a")[0];
+        let aElement = button.getElementsByTagName("a")[0];
 
-        aTag.childNodes[0].remove();
-        aTag.appendChild(span);
+        if (!isDropMenuOption(button)) {
+            aElement.childNodes[0].remove();
+            aElement.prepend(span);
+        }
+        else {
+            button.prepend(span);
+        }
 
         if (button.classList.contains("markItUpDropMenu")) {
             span = document.createElement("span");
-            span.innerHTML = "expand_more";
-            span.classList.add("materialIcon", "dropMenuExpandButton");
+            span.classList.add("materialIcon", `${isDropMenuOption(button) ? "subD" : "d"}ropMenuExpandButton`);
+            span.textContent = getIconName(span, 1);
 
-            aTag.appendChild(span);
+            aElement.prepend(span);
         }
     });
 }
 
-function getIconName(button, classIndex) {
-    switch (button.classList[classIndex]) {
+function isDropMenuOption(button) {
+    return button.parentElement.parentElement.classList.contains("markItUpDropMenu");
+}
+
+function isEmojiOption(button) {
+    return button.parentElement.parentElement.classList.contains("markItUpButton12");
+}
+
+function getIconName(element, classIndex) {
+    switch (element.classList[classIndex]) {
+        case "dropMenuExpandButton":
+            return "expand_more";
+        case "subDropMenuExpandButton":
+            return "chevron_right";
         case "markItUpButton1":
             return "format_bold";
         case "markItUpButton2":
@@ -43,6 +59,10 @@ function getIconName(button, classIndex) {
             return "add_link";
         case "markItUpButton7":
             return "format_size";
+        case "markItUpButton7-1":
+            return "text_increase";
+        case "markItUpButton7-2":
+            return "text_decrease";
         case "markItUpButton8":
             return "format_list_bulleted";
         case "markItUpButton9":
@@ -55,21 +75,23 @@ function getIconName(button, classIndex) {
             return "add_reaction";
         case "markItUpButton13":
             return "computer";
-        case "markItUpButton14":
-            return "question_mark";
+        // case "markItUpButton14": // (emoji dropdown menu)
+        //     return "";
         case "markItUpButton15":
-            return "cleaning_services";
+            return "mop";
         case "markItUpButton16":
-            return "spellcheck";
+            return "visibility";
         case "sa-forum-toolbar-color":
             return "format_color_fill";
         case "sa-forum-toolbar-center":
             return "format_align_center"
+        case "sa-forum-toolbar-code":
+            return "code";
         case "markItUpDropMenu":
             console.log("not found, increasing index");
-            return getIconName(button, ++classIndex);
+            return getIconName(element, ++classIndex);
         default:
             console.log("default :(");
-            break;
+            return "question_mark";
     }
 }
