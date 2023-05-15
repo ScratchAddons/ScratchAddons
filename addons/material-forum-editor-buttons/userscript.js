842701c8125935d2cefcdbffc339f78565b664e0
just-a-hriday <just.a.hriday@gmail.com>
2023-05-15 14:51:24 +0530

    decrease dependency on userscript

--- addons/material-forum-editor-buttons/userscript.js
+++ addons/material-forum-editor-buttons/userscript.js
@@ -1,72 +1,27 @@
 export default async function ({ addon, console, msg }) {
-    setIconStyle(addon.settings.get("iconStyle"));
-
     // await addon.tab.waitForElement(".sa-forum-toolbar-code");
     // await addon.tab.waitForElement(".markItUpButton17");
 
     // All buttons in the forum toolbar, including the options on dropdown menus.
-    let buttons = document.querySelectorAll(".markItUpHeader .markItUpButton");
+    let buttons = document.querySelectorAll(".markItUpHeader ul > .markItUpButton");
+
+    console.log(`queryselector done, ${buttons.length} elements`);
 
     buttons.forEach(button => {
-        // This span contains the icon.
-        let span = document.createElement("span");
-
-        span.textContent = getIconName(button, 1);
-        span.classList.add("materialIcon");
-        span.id = "buttonIcon";
-
-        // The <a> link tag inside the button.
-        let buttonLink = button.getElementsByTagName("a")[0];
-
-        if (isDropMenuOption(button)) {
-            // Option inside a dropdown menu.
-            if (!(button.parentElement.parentElement.classList.contains("markItUpButton12") || isScratchblocksubOption(button)))
-                button.prepend(span); // Don't prepend the child if it's inside the emoji or scratchblocks menu.
-        }
-        else {
-            // Regular button, not a dropdown menu option.
-            buttonLink.childNodes[0]?.remove(); // Remove the text inside the button, which otherwise interferes with the icon span.
-            buttonLink.prepend(span); // Add the span before all the other children of the butten (required for the dropdown menus).
-        }
-
-        // Adds the expand arrow to dropdown menus.
-        if (button.classList.contains("markItUpDropMenu")) {
-            span = document.createElement("span"); // This span contains the expand arrow.
-
-            // Adds a "subDropMenuExpandButton" or "dropMenuExpandButton" class. This is required for the emoji menu, which has sub-dropdowns.
-            // The main dropdown buttons get arrows pointing downwards, the sub dropdown buttons get arrows pointing to the right.
-            span.classList.add("materialIcon", `${isDropMenuOption(button) ? "subD" : "d"}ropMenuExpandButton`);
-            span.textContent = getIconName(span, 1);
-
-            buttonLink.prepend(span);
-        }
+        button.querySelector("A").setAttribute("iconName", getIconName(button, 1));
     });
 }
 
-function setIconStyle (style) {
-    // The icon font needs a value of 1 for filled icons and 0 for outlined - basically a boolean.
-    document.querySelector(":root").style.setProperty("--filledMaterialIcons", style === "fill" ? 1 : 0);
-}
-
-// Checks if a button is an option inside a dropdown menu.
-function isDropMenuOption(button) {
-    return button.parentElement.parentElement.classList.contains("markItUpDropMenu");
-}
-
-function isScratchblocksubOption(button) {
-    let scratchblocksButton = document.querySelector(".scratchblocks-button");
-    return scratchblocksButton.contains(button); // Returns true if button is a descendant (at any level) of the scratchblocks button.
-}
+// function setIconStyle (style) {
+//     // The icon font needs a value of 1 for filled icons and 0 for outlined - basically a boolean.
+//     document.querySelector(":root").style.setProperty("--filledMaterialIcons", style === "fill" ? 1 : 0);
+// }
 
 // This function gets the name of the icon glyph based on the element's class.
 // If the first class it checks doesn't have a match on the switch statement, it tries the next class.
 // If no class names match and the class name is empty, a question mark icon is returned.
 function getIconName(element, classIndex) {
     switch (element.classList[classIndex]) {
-        case "dropMenuExpandButton":
-            return "expand_more";
-        case "subDropMenuExpandButton":
-            return "chevron_right";
         case "markItUpButton1":
             return "format_bold";
         case "markItUpButton2":
@@ -97,7 +52,7 @@ function getIconName(element, classIndex) {
         case "markItUpButton12":
             return "add_reaction";
         case "markItUpButton13":
-            return "computer";
+            return "devices";
         // case "markItUpButton14": // (emoji dropdown menu)
         //     return "";
         case "markItUpButton15":
@@ -107,7 +62,7 @@ function getIconName(element, classIndex) {
         case "markItUpButton17": // image uploader
             return "file_upload";
         case "sa-forum-toolbar-color":
-            return "format_color_fill";
+            return "palette";
         case "sa-forum-toolbar-center":
             return "format_align_center"
         case "sa-forum-toolbar-code":
