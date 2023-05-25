ec5c6c7062170646087ab92d1d5643ac0d8c8b2e
WorldLanguages <mundofinky@gmail.com>
2023-05-24 21:01:02 -0300

    Add missing `!` operator (get opposite bool)

--- background/get-addon-settings.js
+++ background/get-addon-settings.js
@@ -59,7 +59,7 @@ chrome.storage.sync.get([...ADDON_SETTINGS_KEYS, "addonsEnabled"], (storageItems
     return;
   }
   const addonsEnabled = storageItems.addonsEnabled || {};
-  const areAddonSettingsEmpty = storageItems["addonSettings3"];
+  const areAddonSettingsEmpty = !storageItems["addonSettings3"];
   const addonSettings = areAddonSettingsEmpty
     ? {} // Default value
     : { ...storageItems.addonSettings1, ...storageItems.addonSettings2, ...storageItems.addonSettings3 };
