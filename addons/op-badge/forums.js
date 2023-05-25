7a3b1c7cb57ccc52d9d2b6538c2ff0ae67f523fa
WorldLanguages <mundofinky@gmail.com>
2023-05-25 18:21:41 -0300

    Clean up forums.js code

--- addons/op-badge/forums.js
+++ addons/op-badge/forums.js
@@ -1,26 +1,22 @@
 export default async function ({ addon, console, msg }) {
   const posts = document.querySelectorAll(".blockpost");
+  let opUsername;
 
-  if (new URLSearchParams(location.search).get("page") || 1 == 1) {
-    const html = document.querySelectorAll(".blockpost");
-    var op = html[0].querySelector(".username").innerText;
+  if (new URLSearchParams(location.search).get("page") || 1 === 1) {
+    opUsername = posts[0].querySelector(".username").innerText;
   } else {
-    const html = new DOMParser().parseFromString(
-      await fetch(`https://scratch.mit.edu/discuss/topic/${window.location.href.split("/")[5]}/`).then((res) =>
-        res.text()
-      ),
+    const firstPageDocument = new DOMParser().parseFromString(
+      await fetch(`https://scratch.mit.edu/discuss/topic/${location.href.split("/")[5]}/`).then((res) => res.text()),
       "text/html"
     );
-    var op = html.querySelectorAll(".blockpost")[0].querySelector(".username").innerText;
+    opUsername = firstPageDocument.querySelectorAll(".blockpost")[0].querySelector(".username").innerText;
   }
 
   for (const post of posts) {
-    if (op == post.querySelectorAll(".username")[0].innerText) {
-      let op_badge = document.createElement("div");
-
-      op_badge.innerText = msg("op");
-
-      post.querySelector(".postleft").querySelector("dl").appendChild(op_badge);
+    if (opUsername === post.querySelector(".username").innerText) {
+      let opBadge = document.createElement("div");
+      opBadge.innerText = msg("op");
+      post.querySelector(".postleft dl").appendChild(opBadge);
     }
   }
 }
