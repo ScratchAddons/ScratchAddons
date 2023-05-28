f1fd595995f8df068baa26a4e98add601587b164
DNin01 <106490990+DNin01@users.noreply.github.com>
2023-05-28 12:17:11 -0700

    Add some comments to userscript

--- addons/remix-tree-button/main.js
+++ addons/remix-tree-button/main.js
@@ -1,5 +1,5 @@
 export default async function ({ addon, console, msg }) {
-  //define remix tree button elements
+  // Functions (these are where the magic happens):
   function loadRemixButton() {
     if (document.querySelector("#scratchAddonsRemixTreeBtn") && addon.settings.get("type") === "new") {
       document.querySelector("#scratchAddonsRemixTreeBtn").style.display = "block";
@@ -38,6 +38,7 @@ export default async function ({ addon, console, msg }) {
   }
 
   function goToRemixTree() {
+    // Follow the link to the remix tree after clicking the button.
     if (addon.settings.get("open-with-icon") && !addon.self.disabled) {
       window.location.href = `https://scratch.mit.edu/projects/${
         window.location.href.split("projects")[1].split("/")[1]
@@ -46,22 +47,56 @@ export default async function ({ addon, console, msg }) {
   }
 
   function undoLoadRemixButton() {
+    // Removes the button.
     if (document.querySelector("#scratchAddonsRemixTreeBtn")) {
       document.querySelector("#scratchAddonsRemixTreeBtn").style.display = "none";
     }
   }
 
   function undoChangeLink() {
+    // Changes the link back to /remixes.
     if (document.querySelector(".remix-list")) {
       document.querySelector(".remix-list a").href = window.location.href + "remixes";
       document.querySelector(".remix-list a span").textContent = "View all";
     }
   }
 
+  function addIconLink() {
+    // Turns the remixes icon on the project statistics row into a link to the remix tree.
+    if (addon.settings.get("open-with-icon") && !addon.self.disabled) {
+      if (addon.tab.editorMode === "projectpage") {
+        addon.tab
+          .waitForElement(".flex-row.subactions", {
+            reduxCondition: (state) => state.scratchGui.mode.isPlayerOnly,
+          })
+          .then(() => {
+            const remixesIcon = document.querySelector(".project-remixes");
+            remixesIcon.style.cursor = "pointer";
+            remixesIcon.title = msg("go-to-remix-tree");
+            remixesIcon.addEventListener("click", () => {
+              goToRemixTree();
+            });
+          });
+      }
+    }
+  }
+
+  function removeIconLink() {
+    // Removes the link from the remixes statistic icon.
+    if (addon.tab.editorMode === "projectpage") {
+      const remixesIcon = document.querySelector(".project-remixes");
+      remixesIcon.removeAttribute("style");
+      remixesIcon.removeAttribute("title");
+    }
+  }
+
+  // Initialize:
   loadRemixButton();
   addIconLink();
 
+  // Events:
   addon.tab.addEventListener("urlChange", () => {
+    // When the user comes back to the project page from the editor (or vice versa), we need to re-add the links.
     loadRemixButton();
     addIconLink();
   });
@@ -96,30 +131,4 @@ export default async function ({ addon, console, msg }) {
       removeIconLink();
     }
   });
-
-  function addIconLink() {
-    if (addon.settings.get("open-with-icon")) {
-      if (addon.self.disabled) return;
-      if (addon.tab.editorMode === "projectpage") {
-        addon.tab
-          .waitForElement(".flex-row.subactions", {
-            reduxCondition: (state) => state.scratchGui.mode.isPlayerOnly,
-          })
-          .then(() => {
-            const remixesIcon = document.querySelector(".project-remixes");
-            remixesIcon.style.cursor = "pointer";
-            remixesIcon.title = msg("go-to-remix-tree");
-            remixesIcon.addEventListener("click", () => {
-              goToRemixTree();
-            });
-          });
-      }
-    }
-  }
-
-  function removeIconLink() {
-    const remixesIcon = document.querySelector(".project-remixes");
-    remixesIcon.removeAttribute("style");
-    remixesIcon.removeAttribute("title");
-  }
 }
