09ba27915e4c5daf8448efe6ab1a5fc66ae42fb2
pufferfish101007 <pufferfish101007@users.noreply.github.com>
2023-05-31 16:27:28 +0000

    Format code

--- addons/custom-reporters/userscript.js
+++ addons/custom-reporters/userscript.js
@@ -1,194 +1,215 @@
 export default async function ({ addon, msg, console }) {
-    const ScratchBlocks = await addon.tab.traps.getBlockly();
-    addon.tab.redux.initialize();
-    while (true) {
-        const modal = (await addon.tab.waitForElement("div[class*=custom-procedures_modal-content_]", { markAsSeen: true })).querySelector("div");
-        const header = modal.querySelector("div[class*=modal_header]");
-        const selectTypeDiv = Object.assign(document.createElement("div"), {
-            className: addon.tab.scratchClass("custom-procedures_options-row", "custom-procedures4body box_box"),
-            innerHTML: `
-            <div id="sa-custom-reporter_select-block-type_stack" class="${addon.tab.scratchClass("custom-procedures_option-card")}" role="button" tabindex="0">
-                <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${addon.self.dir}/stack.svg">
+  const ScratchBlocks = await addon.tab.traps.getBlockly();
+  addon.tab.redux.initialize();
+  while (true) {
+    const modal = (
+      await addon.tab.waitForElement("div[class*=custom-procedures_modal-content_]", { markAsSeen: true })
+    ).querySelector("div");
+    const header = modal.querySelector("div[class*=modal_header]");
+    const selectTypeDiv = Object.assign(document.createElement("div"), {
+      className: addon.tab.scratchClass("custom-procedures_options-row", "custom-procedures4body box_box"),
+      innerHTML: `
+            <div id="sa-custom-reporter_select-block-type_stack" class="${addon.tab.scratchClass(
+              "custom-procedures_option-card"
+            )}" role="button" tabindex="0">
+                <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${
+        addon.self.dir
+      }/stack.svg">
                 <div class="${addon.tab.scratchClass("custom-procedures_option-title")}">
                     <span>${msg("stack")}</span>
                 </div>
             </div>
-            <div id="sa-custom-reporter_select-block-type_number" class="${addon.tab.scratchClass("custom-procedures_option-card")}" role="button" tabindex="0">
-                <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${addon.self.dir}/reporter.svg">
+            <div id="sa-custom-reporter_select-block-type_number" class="${addon.tab.scratchClass(
+              "custom-procedures_option-card"
+            )}" role="button" tabindex="0">
+                <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${
+        addon.self.dir
+      }/reporter.svg">
                 <div class="${addon.tab.scratchClass("custom-procedures_option-title")}">
                     <span>${msg("numortext")}</span>
                 </div>
             </div>
-            <div id="sa-custom-reporter_select-block-type_predicate" class="${addon.tab.scratchClass("custom-procedures_option-card")}" role="button" tabindex="0">
-                <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${addon.self.dir}/predicate.svg">
+            <div id="sa-custom-reporter_select-block-type_predicate" class="${addon.tab.scratchClass(
+              "custom-procedures_option-card"
+            )}" role="button" tabindex="0">
+                <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${
+        addon.self.dir
+      }/predicate.svg">
                 <div class="${addon.tab.scratchClass("custom-procedures_option-title")}">
                     <span>${msg("boolean")}</span>
                 </div>
             </div>
-            `
-        });
-        
-        let selectedType = "stack";
-        const blockExtensions = {
-            stack: ["shape_statement"],
-            number: ["output_number", "output_string"],
-            predicate: ["output_boolean"],
-        };
-        
-        // make the custom block editor thing the right shape
-        const originalInit = ScratchBlocks.Blocks.procedures_declaration.init;
-        ScratchBlocks.Blocks.procedures_declaration.init = function(...args) {
-            const originalJsonInit = this.jsonInit;
-            this.jsonInit = function(obj) {
-                return originalJsonInit.call(this, Object.assign(obj, {
-                    extensions: ["colours_more", ...blockExtensions[selectedType]],
-                }));
-            }
-            return originalInit.call(this, ...args);
+            `,
+    });
+
+    let selectedType = "stack";
+    const blockExtensions = {
+      stack: ["shape_statement"],
+      number: ["output_number", "output_string"],
+      predicate: ["output_boolean"],
+    };
+
+    // make the custom block editor thing the right shape
+    const originalInit = ScratchBlocks.Blocks.procedures_declaration.init;
+    ScratchBlocks.Blocks.procedures_declaration.init = function (...args) {
+      const originalJsonInit = this.jsonInit;
+      this.jsonInit = function (obj) {
+        return originalJsonInit.call(
+          this,
+          Object.assign(obj, {
+            extensions: ["colours_more", ...blockExtensions[selectedType]],
+          })
+        );
+      };
+      return originalInit.call(this, ...args);
+    };
+
+    // ignore any calls from scratch to add labels/inputs, as it tries to add them to a block that probably won't exist
+    const oldAddLabel = ScratchBlocks.Blocks.procedures_declaration.addLabelExternal;
+    ScratchBlocks.Blocks.procedures_declaration.addLabelExternal = function (isSA) {
+      if (isSA) {
+        return oldAddLabel.call(this);
+      }
+    };
+    const oldAddBool = ScratchBlocks.Blocks.procedures_declaration.addBooleanExternal;
+    ScratchBlocks.Blocks.procedures_declaration.addBooleanExternal = function (isSA) {
+      if (isSA) {
+        return oldAddBool.call(this);
+      }
+    };
+    const oldAddStringNum = ScratchBlocks.Blocks.procedures_declaration.addStringNumberExternal;
+    ScratchBlocks.Blocks.procedures_declaration.addStringNumberExternal = function (isSA) {
+      if (isSA) {
+        return oldAddStringNum.call(this);
+      }
+    };
+
+    const workspace = ScratchBlocks.getMainWorkspace();
+    let mutationRoot = workspace.topBlocks_[0];
+
+    let rtlOffset = 0;
+
+    workspace.listeners_[0] = () => null; // stop scratch from trying to move its potentially deleted block around
+
+    workspace.addChangeListener(() => {
+      // from https://github.com/LLK/scratch-gui/blob/c5d4aea493b58c15570915753e1c0f9e7e812ec1/src/containers/custom-procedures.jsx
+      mutationRoot.onChangeFn();
+      // Keep the block centered on the workspace
+      const metrics = workspace.getMetrics();
+      const { x, y } = mutationRoot.getRelativeToSurfaceXY();
+      const dy = metrics.viewHeight / 2 - mutationRoot.height / 2 - y;
+      let dx;
+      if (addon.tab.direction === "rtl") {
+        // // TODO: https://github.com/LLK/scratch-gui/issues/2838
+        // This is temporary until we can figure out what's going on width
+        // block positioning on the workspace for RTL.
+        // Workspace is always origin top-left, with x increasing to the right
+        // Calculate initial starting offset and save it, every other move
+        // has to take the original offset into account.
+        // Calculate a new left postion based on new width
+        // Convert current x position into LTR (mirror) x position (uses original offset)
+        // Use the difference between ltrX and mirrorX as the amount to move
+        const ltrX = metrics.viewWidth / 2 - mutationRoot.width / 2 + 25;
+        const mirrorX = x - (x - rtlOffset) * 2;
+        if (mirrorX === ltrX) {
+          return;
         }
-        
-        // ignore any calls from scratch to add labels/inputs, as it tries to add them to a block that probably won't exist
-        const oldAddLabel = ScratchBlocks.Blocks.procedures_declaration.addLabelExternal;
-        ScratchBlocks.Blocks.procedures_declaration.addLabelExternal = function(isSA) {
-            if (isSA) {
-                return oldAddLabel.call(this);
-            }
+        dx = mirrorX - ltrX;
+        const midPoint = metrics.viewWidth / 2;
+        if (x === 0) {
+          // if it's the first time positioning, it should always move right
+          if (mutationRoot.width < midPoint) {
+            dx = ltrX;
+          } else if (mutationRoot.width < metrics.viewWidth) {
+            dx = midPoint - (metrics.viewWidth - mutationRoot.width) / 2;
+          } else {
+            dx = midPoint + (mutationRoot.width - metrics.viewWidth);
+          }
+          mutationRoot.moveBy(dx, dy);
+          rtlOffset = mutationRoot.getRelativeToSurfaceXY().x;
+          return;
         }
-        const oldAddBool = ScratchBlocks.Blocks.procedures_declaration.addBooleanExternal;
-        ScratchBlocks.Blocks.procedures_declaration.addBooleanExternal = function(isSA) {
-            if (isSA) {
-                return oldAddBool.call(this);
-            }
+        if (mutationRoot.width > metrics.viewWidth) {
+          dx = dx + mutationRoot.width - metrics.viewWidth;
         }
-        const oldAddStringNum = ScratchBlocks.Blocks.procedures_declaration.addStringNumberExternal;
-        ScratchBlocks.Blocks.procedures_declaration.addStringNumberExternal = function(isSA) {
-            if (isSA) {
-                return oldAddStringNum.call(this);
-            }
+      } else {
+        dx = metrics.viewWidth / 2 - mutationRoot.width / 2 - x;
+        // If the procedure declaration is wider than the view width,
+        // keep the right-hand side of the procedure in view.
+        if (mutationRoot.width > metrics.viewWidth) {
+          dx = metrics.viewWidth - mutationRoot.width - x;
         }
-        
-        const workspace = ScratchBlocks.getMainWorkspace();
-        let mutationRoot = workspace.topBlocks_[0];
-        
-        let rtlOffset = 0;
-        
-        workspace.listeners_[0] = () => null; // stop scratch from trying to move its potentially deleted block around
-        
-        workspace.addChangeListener(() => { // from https://github.com/LLK/scratch-gui/blob/c5d4aea493b58c15570915753e1c0f9e7e812ec1/src/containers/custom-procedures.jsx
-            mutationRoot.onChangeFn();
-            // Keep the block centered on the workspace
-            const metrics = workspace.getMetrics();
-            const {x, y} = mutationRoot.getRelativeToSurfaceXY();
-            const dy = (metrics.viewHeight / 2) - (mutationRoot.height / 2) - y;
-            let dx;
-            if (addon.tab.direction === "rtl") {
-                // // TODO: https://github.com/LLK/scratch-gui/issues/2838
-                // This is temporary until we can figure out what's going on width
-                // block positioning on the workspace for RTL.
-                // Workspace is always origin top-left, with x increasing to the right
-                // Calculate initial starting offset and save it, every other move
-                // has to take the original offset into account.
-                // Calculate a new left postion based on new width
-                // Convert current x position into LTR (mirror) x position (uses original offset)
-                // Use the difference between ltrX and mirrorX as the amount to move
-                const ltrX = ((metrics.viewWidth / 2) - (mutationRoot.width / 2) + 25);
-                const mirrorX = x - ((x - rtlOffset) * 2);
-                if (mirrorX === ltrX) {
-                    return;
-                }
-                dx = mirrorX - ltrX;
-                const midPoint = metrics.viewWidth / 2;
-                if (x === 0) {
-                    // if it's the first time positioning, it should always move right
-                    if (mutationRoot.width < midPoint) {
-                        dx = ltrX;
-                    } else if (mutationRoot.width < metrics.viewWidth) {
-                        dx = midPoint - ((metrics.viewWidth - mutationRoot.width) / 2);
-                    } else {
-                        dx = midPoint + (mutationRoot.width - metrics.viewWidth);
-                    }
-                    mutationRoot.moveBy(dx, dy);
-                    rtlOffset = mutationRoot.getRelativeToSurfaceXY().x;
-                    return;
-                }
-                if (mutationRoot.width > metrics.viewWidth) {
-                    dx = dx + mutationRoot.width - metrics.viewWidth;
-                }
-            } else {
-                dx = (metrics.viewWidth / 2) - (mutationRoot.width / 2) - x;
-                // If the procedure declaration is wider than the view width,
-                // keep the right-hand side of the procedure in view.
-                if (mutationRoot.width > metrics.viewWidth) {
-                    dx = metrics.viewWidth - mutationRoot.width - x;
-                }
-            }
-            mutationRoot.moveBy(dx, dy);
-        });
-        
-        const setupMutationRoot = () => {
-            // from https://github.com/LLK/scratch-gui/blob/c5d4aea493b58c15570915753e1c0f9e7e812ec1/src/containers/custom-procedures.jsx
-            mutationRoot.setMovable(false);
-            mutationRoot.setDeletable(false);
-            mutationRoot.contextMenu = false;
-            mutationRoot.domToMutation(addon.tab.redux.state.scratchGui.customProcedures.mutator);
-            mutationRoot.initSvg();
-            mutationRoot.render();
-            // Allow the initial events to run to position this block, then focus.
-            setTimeout(() => {
-                mutationRoot.focusLastEditor_();
-            });
-        };
-        
-        const selectBlockTypeFactory = (type) => {
-            return () => {
-                selectedType = type;
-                rtlOffset = 0;
-                ScratchBlocks.duplicate_(workspace.topBlocks_[0]);
-                workspace.topBlocks_[0].dispose();
-                mutationRoot = workspace.topBlocks_[0];
-                setupMutationRoot();
-            };
-        };
-        
-        header.insertAdjacentElement("afterend", selectTypeDiv);
-        for (const blockType of ["stack", "number", "predicate"]) {
-            document.getElementById(`sa-custom-reporter_select-block-type_${blockType}`).addEventListener("click", selectBlockTypeFactory(blockType));
-        }
-        
-        const oldReduxCb = addon.tab.redux.state.scratchGui.customProcedures.callback;
-        console.log(oldReduxCb.toString());
-        addon.tab.redux.dispatch({
-            type: "scratch-gui/custom-procedures/SET_CALLBACK",
-            callback: function(t){
-              if(t){
-                t = '<xml><block type="procedures_definition"><statement name="custom_block"><shadow type="procedures_prototype">'
-                  + Blockly.Xml.domToText(t) 
-                  + "</shadow></statement></block></xml>",
-                  t = Blockly.Xml.textToDom(t).firstChild,
-                  Blockly.Events.setGroup(!0),
-                  t = Blockly.Xml.domToBlock(t,e);
-                var o=e.scale,
-                  n = -e.scrollX;
-                  n = e.RTL ? n + (e.getMetrics().contentWidth - 30) : n + 30,
-                  t.moveBy(n / o,(30 - e.scrollY) / o),
-                  t.scheduleSnapAndBump(),
-                  Blockly.Events.setGroup(!1)
-              }
-            },
-        });
-        
-        modal.querySelector("div[class*=custom-procedures_checkbox-row] input").addEventListener("change", (e) => {
-            mutationRoot.setWarp(e.target.checked);
-        });
-        
-        const inputAddButtons = modal.querySelectorAll("div[class*=custom-procedures_body] > div > div");
-        inputAddButtons[0].addEventListener("click", () => mutationRoot.addStringNumberExternal(true));
-        inputAddButtons[1].addEventListener("click", () => mutationRoot.addBooleanExternal(true));
-        inputAddButtons[2].addEventListener("click", () => mutationRoot.addLabelExternal(true));
-        
-        modal.querySelector("button[class*=custom-procedures_ok-button]").addEventListener("click", () => {
-            console.log(mutationRoot.mutationToDom(true));
-            oldReduxCb(mutationRoot.mutationToDom(true));
-        });
+      }
+      mutationRoot.moveBy(dx, dy);
+    });
+
+    const setupMutationRoot = () => {
+      // from https://github.com/LLK/scratch-gui/blob/c5d4aea493b58c15570915753e1c0f9e7e812ec1/src/containers/custom-procedures.jsx
+      mutationRoot.setMovable(false);
+      mutationRoot.setDeletable(false);
+      mutationRoot.contextMenu = false;
+      mutationRoot.domToMutation(addon.tab.redux.state.scratchGui.customProcedures.mutator);
+      mutationRoot.initSvg();
+      mutationRoot.render();
+      // Allow the initial events to run to position this block, then focus.
+      setTimeout(() => {
+        mutationRoot.focusLastEditor_();
+      });
+    };
+
+    const selectBlockTypeFactory = (type) => {
+      return () => {
+        selectedType = type;
+        rtlOffset = 0;
+        ScratchBlocks.duplicate_(workspace.topBlocks_[0]);
+        workspace.topBlocks_[0].dispose();
+        mutationRoot = workspace.topBlocks_[0];
+        setupMutationRoot();
+      };
+    };
+
+    header.insertAdjacentElement("afterend", selectTypeDiv);
+    for (const blockType of ["stack", "number", "predicate"]) {
+      document
+        .getElementById(`sa-custom-reporter_select-block-type_${blockType}`)
+        .addEventListener("click", selectBlockTypeFactory(blockType));
     }
+
+    const oldReduxCb = addon.tab.redux.state.scratchGui.customProcedures.callback;
+    console.log(oldReduxCb.toString());
+    addon.tab.redux.dispatch({
+      type: "scratch-gui/custom-procedures/SET_CALLBACK",
+      callback: function (t) {
+        if (t) {
+          (t =
+            '<xml><block type="procedures_definition"><statement name="custom_block"><shadow type="procedures_prototype">' +
+            Blockly.Xml.domToText(t) +
+            "</shadow></statement></block></xml>"),
+            (t = Blockly.Xml.textToDom(t).firstChild),
+            Blockly.Events.setGroup(!0),
+            (t = Blockly.Xml.domToBlock(t, e));
+          var o = e.scale,
+            n = -e.scrollX;
+          (n = e.RTL ? n + (e.getMetrics().contentWidth - 30) : n + 30),
+            t.moveBy(n / o, (30 - e.scrollY) / o),
+            t.scheduleSnapAndBump(),
+            Blockly.Events.setGroup(!1);
+        }
+      },
+    });
+
+    modal.querySelector("div[class*=custom-procedures_checkbox-row] input").addEventListener("change", (e) => {
+      mutationRoot.setWarp(e.target.checked);
+    });
+
+    const inputAddButtons = modal.querySelectorAll("div[class*=custom-procedures_body] > div > div");
+    inputAddButtons[0].addEventListener("click", () => mutationRoot.addStringNumberExternal(true));
+    inputAddButtons[1].addEventListener("click", () => mutationRoot.addBooleanExternal(true));
+    inputAddButtons[2].addEventListener("click", () => mutationRoot.addLabelExternal(true));
+
+    modal.querySelector("button[class*=custom-procedures_ok-button]").addEventListener("click", () => {
+      console.log(mutationRoot.mutationToDom(true));
+      oldReduxCb(mutationRoot.mutationToDom(true));
+    });
+  }
 }
