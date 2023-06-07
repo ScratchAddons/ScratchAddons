f947b9b4a4e7c08fe72a09b1d9f8441c18860df6
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-06-07 20:10:06 +0200

    Add dynamic disable to true-youtube-links

--- addons/true-youtube-links/userscript.js
+++ addons/true-youtube-links/userscript.js
@@ -1,4 +1,11 @@
 ﻿export default async function ({ addon, console }) {
+  let changedLinks = [];
+  addon.self.addEventListener("disabled", () => {
+    changedLinks.forEach((link) => {
+      link.href = link.href.replace("https://www.youtube.com/watch?v=", "https://scratch.mit.edu/discuss/youtube/");
+    });
+    changedLinks = [];
+  });
   while (true) {
     const el = await addon.tab.waitForElement(
       'a[href^="https://scratch.mit.edu/discuss/youtube/"], a[href^="/discuss/youtube/"]',
@@ -9,6 +16,10 @@
         },
       }
     );
+    if (addon.self.disabled) {
+      continue;
+    }
     el.href = el.href.replace("https://scratch.mit.edu/discuss/youtube/", "https://www.youtube.com/watch?v=");
+    changedLinks.push(el);
   }
 }
