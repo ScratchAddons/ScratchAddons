c18a03d7a053b8b3e6600d105ab32ee4c8251515
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-06-08 09:22:36 +0200

    Don't always run the loop

--- addons/true-youtube-links/userscript.js
+++ addons/true-youtube-links/userscript.js
@@ -1,4 +1,23 @@
 ﻿export default async function ({ addon, console }) {
+  const main = async () => {
+    while (true) {
+      const el = await addon.tab.waitForElement(
+        'a[href^="https://scratch.mit.edu/discuss/youtube/"], a[href^="/discuss/youtube/"]',
+        {
+          reduxCondition: (state) => {
+            if (!state.scratchGui) return true;
+            return state.scratchGui.mode.isPlayerOnly;
+          },
+        }
+      );
+      if (addon.self.disabled) {
+        return;
+      }
+      el.href = el.href.replace("https://scratch.mit.edu/discuss/youtube/", "https://www.youtube.com/watch?v=");
+      changedLinks.push(el);
+    }
+  };
+
   let changedLinks = [];
   addon.self.addEventListener("disabled", () => {
     changedLinks.forEach((link) => {
@@ -6,20 +25,6 @@
     });
     changedLinks = [];
   });
-  while (true) {
-    const el = await addon.tab.waitForElement(
-      'a[href^="https://scratch.mit.edu/discuss/youtube/"], a[href^="/discuss/youtube/"]',
-      {
-        reduxCondition: (state) => {
-          if (!state.scratchGui) return true;
-          return state.scratchGui.mode.isPlayerOnly;
-        },
-      }
-    );
-    if (addon.self.disabled) {
-      continue;
-    }
-    el.href = el.href.replace("https://scratch.mit.edu/discuss/youtube/", "https://www.youtube.com/watch?v=");
-    changedLinks.push(el);
-  }
+  addon.self.addEventListener("reenabled", main);
+  main();
 }
