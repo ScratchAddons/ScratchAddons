d74d122b3a78ee86b94f61580b8ff1f9fb87cdcf
Valmontechno <108832011+valmontechno@users.noreply.github.com>
2023-06-22 10:48:49 +0200

    Add files via upload

--- addons/search-profile/userscript.js
+++ addons/search-profile/userscript.js
@@ -5,14 +5,13 @@ export default async function ({ addon, console, msg }) {
     img = tab.appendChild(document.createElement("img")),
     span = tab.appendChild(document.createElement("span")),
     user = document.querySelector('[name="q"]').value.trim(),
-    valid = /^[\w-]{3,20}$/g.test(user);
+    valid = (/^[\w-]{3,20}$/g.test(user) && (await fetch(`https://scratch.mit.edu/users/${user}/`)).status == 200);
   tab.type = "button";
   tab.role = "tab";
+  tab.className = "user-tab";
   tab.tabIndex = -1; // unselected tabs should only be focusable using arrow keys
   tab.ariaSelected = false;
-  img.src = addon.self.dir + "/user.svg";
   img.className = "tab-icon";
-  span.innerText = msg("profile");
   addon.tab.displayNoneWhileDisabled(tab);
 
   // Check if a valid username is entered
@@ -60,4 +59,18 @@ export default async function ({ addon, console, msg }) {
     tab.disabled = true;
     tab.title = msg("invalid-username", { username: user });
   }
+  
+  // Show user icon only if user is valid and option is enabled
+  async function showIcon() {
+    if (valid && addon.settings.get('icon')) {
+      const userId = (await (await fetch(`https://api.scratch.mit.edu/users/${user}/`)).json()).id;
+      img.src = `https://uploads.scratch.mit.edu/get_image/user/${userId}_24x24.png`;
+      span.innerText = user;
+    } else {
+      img.src = addon.self.dir + "/user.svg";
+      span.innerText = msg("profile");
+    }
+  }
+  showIcon();
+  addon.settings.addEventListener("change", showIcon);
 }
