8821c2ce49c5a1a21eef86e83ed5cbca5805f8a6
Maximouse <51849865+mxmou@users.noreply.github.com>
2023-06-26 10:34:44 +0200

    Update remaining uses of blue accent color

--- addon-api/content-script/modal.js
+++ addon-api/content-script/modal.js
@@ -89,8 +89,8 @@ export const createScratchWwwModal = (title, { isOpen = false, useSizesClass = t
       height: 3rem;
       box-sizing: border-box;
       padding-top: 0.75rem;
-      background-color: var(--darkWww-navbar, #4d97ff);
-      box-shadow: 0 -1px 0 0 inset var(--darkWww-navbar-variant, #4280d7);
+      background-color: var(--darkWww-navbar, #855cd6);
+      box-shadow: 0 -1px 0 0 inset var(--darkWww-navbar-variant, #7854c0);
       color: var(--darkWww-navbar-text, white);
       text-align: center;
       font-weight: bold;
