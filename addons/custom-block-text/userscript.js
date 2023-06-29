d0656a927cb5d501b0817d3eda285dda0fa85781
World_Languages <mundofinky@gmail.com>
2023-06-29 14:38:38 -0300

    Fix race condition bugs

--- addons/custom-block-text/userscript.js
+++ addons/custom-block-text/userscript.js
@@ -1,44 +1,89 @@
 import { updateAllBlocks } from "../custom-block-shape/update-all-blocks.js";
 
 export default async function ({ addon, console }) {
-  let currentSize = 100;
+  let currentTextSize = 100;
 
   const vm = addon.tab.traps.vm;
   const blocklyInstance = await addon.tab.traps.getBlockly();
 
   // Handling the CSS from here instead of a userstyle is much more stable, as
   // there's no code outside of this addon dynamically toggling the styles.
-  let stylesheet = document.createElement("style");
-  stylesheet.textContent = `
+  // This way, we can clearly control the execution order of style operations.
+  // For example, we always want to call updateAllBlocks() after the styles
+  // were updated according to the user's settings, not before.
+  const fontSizeCss = document.createElement("style");
+  fontSizeCss.textContent = `
     .blocklyText,
     .blocklyHtmlInput {
       font-size: calc(var(--customBlockText-sizeSetting) * 0.12pt) !important;
     }`;
-  let stylesheetOnDocument = false;
+  fontSizeCss.disabled = true;
+  document.head.appendChild(fontSizeCss);
+  //
+  const boldCss = document.createElement("style");
+  boldCss.textContent = `
+    .blocklyText,
+    .blocklyHtmlInput {
+      font-weight: bold;
+    }`;
+  boldCss.disabled = true;
+  document.head.appendChild(boldCss);
+  //
+  const textShadowCss = document.createElement("style");
+  textShadowCss.textContent = `
+    .blocklyDraggable > .blocklyText,
+    .blocklyDraggable > g > text {
+      text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.4);
+    }`;
+  textShadowCss.disabled = true;
+  document.head.appendChild(textShadowCss);
 
-  const changeFontSize = (wantedSize) => {
-    document.documentElement.style.setProperty("--customBlockText-sizeSetting", wantedSize);
-
-    if (wantedSize === 100) {
-      stylesheet.remove();
-      stylesheetOnDocument = false;
-      return;
-    } else if (wantedSize === currentSize) return;
-    currentSize = wantedSize;
-
-    if (!stylesheetOnDocument) {
-      document.head.appendChild(stylesheet);
-      stylesheetOnDocument = true;
-    }
-
+  const updateBlockly = () => {
     blocklyInstance.Field.cacheWidths_ = {}; // Clear text width cache
     updateAllBlocks(vm);
   };
 
-  addon.settings.addEventListener("change", () => changeFontSize(addon.settings.get("size")));
+  const setFontSize = (wantedSize) => {
+    if (wantedSize !== 100) document.documentElement.style.setProperty("--customBlockText-sizeSetting", wantedSize);
 
-  addon.self.addEventListener("disabled", () => changeFontSize(100));
-  addon.self.addEventListener("reenabled", () => changeFontSize(addon.settings.get("size")));
+    if (wantedSize === 100) {
+      fontSizeCss.disabled = true;
+      currentTextSize = 100;
+      return;
+    } else if (wantedSize === currentTextSize) return;
 
-  changeFontSize(addon.settings.get("size"));
+    currentTextSize = wantedSize;
+    fontSizeCss.disabled = false;
+  };
+  const setBold = (bool) => {
+    boldCss.disabled = !bool;
+  };
+  const setTextShadow = (bool) => {
+    textShadowCss.disabled = !bool;
+  };
+
+  addon.settings.addEventListener("change", () => {
+    setFontSize(addon.settings.get("size"));
+    setBold(addon.settings.get("bold"));
+    setTextShadow(addon.settings.get("shadow"));
+    updateBlockly();
+  });
+
+  addon.self.addEventListener("disabled", () => {
+    setFontSize(100);
+    setBold(false);
+    setTextShadow(false);
+    updateBlockly();
+  });
+  addon.self.addEventListener("reenabled", () => {
+    setFontSize(addon.settings.get("size"));
+    setBold(addon.settings.get("bold"));
+    setTextShadow(addon.settings.get("shadow"));
+    updateBlockly();
+  });
+
+  setFontSize(addon.settings.get("size"));
+  setBold(addon.settings.get("bold"));
+  setTextShadow(addon.settings.get("shadow"));
+  updateBlockly();
 }
