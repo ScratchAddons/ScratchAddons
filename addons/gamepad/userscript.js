f2ef6826201d67c067370b0960a5517c9957485a
Maximouse <51849865+mxmou@users.noreply.github.com>
2023-07-01 21:32:15 +0200

    Remove outdated code (#6320)

--- addons/gamepad/userscript.js
+++ addons/gamepad/userscript.js
@@ -418,7 +418,8 @@ export default async function ({ addon, console, msg }) {
 
   while (true) {
     const target = await addon.tab.waitForElement(
-      '[class^="stage-header_stage-size-row"] [class^="button_outlined-button"], [class^="stage-header_stage-menu-wrapper"] > [class^="button_outlined-button"], [class*="stage-header_unselect-wrapper_"] > [class^="button_outlined-button"]',
+      // Full screen button
+      '[class^="stage-header_stage-size-row"] [class^="button_outlined-button"], [class*="stage-header_unselect-wrapper_"] > [class^="button_outlined-button"]',
       {
         markAsSeen: true,
         reduxEvents: [
