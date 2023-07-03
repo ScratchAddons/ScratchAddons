ac7210c3c95ddc8a293b8176301374acd7d5b187
Maximouse <51849865+mxmou@users.noreply.github.com>
2023-07-03 14:35:24 +0200

    Update editor dark mode preview

--- webpages/settings/components/previews/editor-dark-mode.js
+++ webpages/settings/components/previews/editor-dark-mode.js
@@ -51,8 +51,8 @@ export default async function ({ template }) {
           menuBarBorder: textColor(this.settings.menuBar, "rgba(0, 0, 0, 0.15)", "rgba(255, 255, 255, 0.15)", 60),
           accentTransparentText: textColor(
             this.settings.accent,
-            "rgba(87, 94, 117, 0.75)",
-            "rgba(255, 255, 255, 0.75)"
+            "rgba(87, 94, 117, 0.5)",
+            "rgba(255, 255, 255, 0.3)"
           ),
           accentArtboard: this.settings.affectPaper ? this.settings.accent : "#ffffff",
           accentCheckerboard: this.settings.affectPaper
@@ -60,8 +60,8 @@ export default async function ({ template }) {
                 textColor(
                   // see addons/editor-dark-mode/paper.js
                   this.settings.accent,
-                  alphaBlend(this.settings.accent, multiply(makeHsv(this.settings.primary, 1, 0.67), { a: 0.15 })),
-                  alphaBlend(this.settings.accent, multiply(makeHsv(this.settings.primary, 0.5, 1), { a: 0.15 })),
+                  alphaBlend(this.settings.accent, multiply(makeHsv(this.settings.page, 1, 0.67), { a: 0.15 })),
+                  alphaBlend(this.settings.accent, multiply(makeHsv(this.settings.page, 0.5, 1), { a: 0.15 })),
                   112 // threshold: #707070
                 ),
                 { a: 0.55 }
@@ -73,7 +73,8 @@ export default async function ({ template }) {
             "rgba(87, 124, 155, 0.13)",
             "rgba(255, 255, 255, 0.05)"
           ),
-          primaryTransparent: multiply(this.settings.primary, { a: 0.35 }),
+          primaryTransparent15: multiply(this.settings.primary, { a: 0.15 }),
+          primaryTransparent35: multiply(this.settings.primary, { a: 0.35 }),
           inputTransparent: multiply(this.settings.input, { a: 0.25 }),
         };
       },
