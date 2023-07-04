267c0f5f138276d1884c780e0733ea81ae723822
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-07-04 07:01:43 +0200

    use body.addEventListener

--- addons/editor-number-arrow-keys/userscript.js
+++ addons/editor-number-arrow-keys/userscript.js
@@ -1,31 +1,24 @@
 export default async function ({ addon }) {
-  setInterval(() => {
-    [...document.querySelectorAll(".blocklyHtmlInput:not(.sa-editor-number-arrow-keys)")].forEach((elt) => {
-      elt.addEventListener("keydown", (e) => {
-        if (
-          !["ArrowUp", "ArrowDown"].includes(e.code) ||
-          Number(elt.value).toString().replace(/^0*/, "") !== elt.value.replace(/^0*/, "")
-        ) {
-          return;
-        }
-        e.preventDefault();
-        // if (e.altKey) {
-        //   // Because of JavaScript math like 0.1+0.2=0.30000000000000004
-        //   elt.value = (Number(elt.value) * 10 + (e.code === "ArrowUp" ? 1 : -1)) / 10;
-        // } else {
-        elt.value =
-          Number(elt.value) +
-          (e.shiftKey
-            ? addon.settings.get("shift")
-            : e.ctrlKey
-            ? addon.settings.get("ctrl")
-            : e.altKey
-            ? addon.settings.get("alt")
-            : addon.settings.get("regular")) *
-            (e.code === "ArrowUp" ? 1 : -1);
-        // }
-      });
-      elt.classList.add("sa-editor-number-arrow-keys");
-    });
+  document.body.addEventListener("keydown", (e) => {
+    if (!e.target.classList.add("blocklyHtmlInput")) {
+      return;
+    }
+    if (
+      !["ArrowUp", "ArrowDown"].includes(e.code) ||
+      Number(e.target.value).toString().replace(/^0*/, "") !== e.target.value.replace(/^0*/, "")
+    ) {
+      return;
+    }
+    e.preventDefault();
+    e.target.value =
+      Number(e.target.value) +
+      (e.shiftKey
+        ? addon.settings.get("shift")
+        : e.ctrlKey
+        ? addon.settings.get("ctrl")
+        : e.altKey
+        ? addon.settings.get("alt")
+        : addon.settings.get("regular")) *
+        (e.code === "ArrowUp" ? 1 : -1);
   });
 }
