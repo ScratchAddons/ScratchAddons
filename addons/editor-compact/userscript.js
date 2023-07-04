c34991c8d3dac5c23b221e439365c1ad18ae5839
Maximouse <51849865+mxmou@users.noreply.github.com>
2023-07-04 13:08:31 +0200

    Add tooltips if labels are hidden

--- addons/editor-compact/userscript.js
+++ addons/editor-compact/userscript.js
@@ -12,4 +12,26 @@ export default async function ({ addon, global, console }) {
   async function resizeWorkspace() {
     window.dispatchEvent(new Event("resize"));
   }
+
+  // Icons in the sound editor don't have tooltips. Add them if labels are hidden.
+  const updateTooltips = () => {
+    for (const button of document.querySelectorAll(
+      "[class*='sound-editor_tool-button_'], [class*='sound-editor_effect-button_']"
+    )) {
+      const icon = button.querySelector("img");
+      if (!addon.self.disabled && addon.settings.get("hideLabels")) icon.title = button.textContent;
+      else icon.removeAttribute("title");
+    }
+  };
+  updateTooltips();
+  addon.settings.addEventListener("change", updateTooltips);
+  addon.self.addEventListener("disabled", updateTooltips);
+  addon.self.addEventListener("reenabled", updateTooltips);
+  while (true) {
+    await addon.tab.waitForElement("[class*='sound-editor_editor-container_']", {
+      markAsSeen: true,
+      reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly && state.scratchGui.editorTab.activeTabIndex === 2,
+    });
+    updateTooltips();
+  }
 }
