abe1ae5140f989739d3c043130e73bc796310ca1
IQBit <iqnitetechnologies@gmail.com>
2023-07-05 21:34:48 +0200

    Move ´editor-messages´ back to separate addon Because W_L was about to cry :)

---
+++ addons/editor-messages/userscript.js
@@ -0,0 +1,52 @@
+export default async function ({ addon, console }) {
+    const messages = document.createElement("a");
+    messages.href = "/messages/";
+    messages.className = addon.tab.scratchClass("menu-bar_menu-bar-item", "menu-bar_hoverable", {
+      others: "sa-editormessages",
+    });
+    let messageCount = document.createElement("span");
+    messageCount.classList.add("sa-editormessages-count");
+    messages.appendChild(messageCount);
+    const setMessages = async () => {
+      const msgCount = Number(await addon.account.getMsgCount());
+      messageCount.innerText = msgCount;
+      if (msgCount === 0) {
+        messageCount.setAttribute("style", `display: none;`);
+      } else {
+        messageCount.setAttribute("style", "");
+      }
+    };
+  
+    let interval;
+    function createInterval() {
+      if (addon.tab.editorMode === "editor") {
+        setMessages();
+        interval = setInterval(setMessages, 5000);
+      } else {
+        addon.tab.addEventListener("urlChange", function thisFunction() {
+          if (addon.tab.editorMode === "editor") {
+            setMessages();
+            interval = setInterval(setMessages, 5000);
+            addon.tab.removeEventListener("urlChange", thisFunction);
+          }
+        });
+      }
+    }
+    createInterval();
+  
+    addon.self.addEventListener("disabled", () => {
+      clearInterval(interval);
+    });
+    addon.self.addEventListener("reenabled", createInterval);
+  
+    addon.tab.displayNoneWhileDisabled(messages);
+    while (true) {
+      let nav = await addon.tab.waitForElement("[class^='menu-bar_account-info-group'] > [href^='/my']", {
+        markAsSeen: true,
+        reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
+        reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
+      });
+      document.querySelector("[class^='menu-bar_account-info-group']").insertBefore(messages, nav);
+    }
+  }
+  
