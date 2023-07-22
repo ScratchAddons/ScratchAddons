8f5b9eabe52bf8ef32b1a69fab8c705e9ffa5c78
World_Languages <mundofinky@gmail.com>
2023-07-22 20:16:01 -0300

    Code cleanup

--- addons/editor-number-arrow-keys/userscript.js
+++ addons/editor-number-arrow-keys/userscript.js
@@ -6,27 +6,25 @@ export default async function ({ addon }) {
     hundred: 100,
     thousand: 1000,
   };
+
   document.body.addEventListener("keydown", (e) => {
-    e.target.classList;
-    if (!e.target.classList.contains("blocklyHtmlInput")) {
-      return;
-    }
-    if (
-      !["ArrowUp", "ArrowDown"].includes(e.code) ||
-      Number(e.target.value).toString().replace(/^0*/, "") !== e.target.value.replace(/^0*/, "")
-    ) {
-      return;
-    }
+    if (!e.target.classList.contains("blocklyHtmlInput")) return;
+    if (!["ArrowUp", "ArrowDown"].includes(e.code)) return;
+    if (Number(e.target.value).toString().replace(/^0*/, "") !== e.target.value.replace(/^0*/, "")) return;
+    // TODO: why remove leading zeros to a result of Number().toString ?
+
     e.preventDefault();
-    e.target.value =
-      Number(e.target.value) +
+
+    const currentValue = Number(e.target.value);
+    const changeBy =
       (e.shiftKey
         ? settings[addon.settings.get("shift")]
         : e.ctrlKey
         ? settings[addon.settings.get("ctrl")]
         : e.altKey
         ? settings[addon.settings.get("alt")]
-        : settings[addon.settings.get("regular")]) *
-        (e.code === "ArrowUp" ? 1 : -1);
+        : settings[addon.settings.get("regular")]) * (e.code === "ArrowUp" ? 1 : -1);
+
+    e.target.value = currentValue + changeBy;
   });
 }
