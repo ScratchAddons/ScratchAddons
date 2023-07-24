04cef76f95f296166f04088f5ee199fb4ae088c8
World_Languages <mundofinky@gmail.com>
2023-07-24 12:39:48 -0300

    Remove misleading TODO
    Co-authored-by: mybearworld <130385691+mybearworld@users.noreply.github.com>

--- addons/editor-number-arrow-keys/userscript.js
+++ addons/editor-number-arrow-keys/userscript.js
@@ -32,7 +32,6 @@ export default async function ({ addon }) {
     if (!e.target.classList.contains("blocklyHtmlInput")) return;
     if (!["ArrowUp", "ArrowDown"].includes(e.code)) return;
     if (Number(e.target.value).toString().replace(/^0*/, "") !== e.target.value.replace(/^0*/, "")) return;
-    // TODO: why remove leading zeros to a result of Number().toString ?
     // TODO: should an empty input be considered a zero?
 
     if (e.target.value.length > 10) return;
