c56bd6e767f2110b9f98ce11dfc7b991e7205844
Samq64 <81489795+Samq64@users.noreply.github.com>
2023-07-29 16:29:45 -0400

    add option to show as loading screen

--- addons/player-thumb/userscript.js
+++ addons/player-thumb/userscript.js
@@ -1,9 +1,5 @@
 export default async function ({ addon, console }) {
   if (addon.tab.editorMode === "editor") return;
-  const stage = await addon.tab.waitForElement('div[class*="stage_stage"]', {
-    markAsSeen: true,
-  });
-  const greenFlagOverlay = stage.querySelector('[class*="stage_green-flag-overlay-wrapper_"]');
 
   const projectId = window.location.pathname.split("/")[2];
   const thumbUrl = `https://uploads.scratch.mit.edu/get_image/project/${projectId}_480x360.png`;
@@ -11,14 +7,37 @@ export default async function ({ addon, console }) {
   thumb.src = thumbUrl;
   thumb.id = "sa-project-thumb";
   thumb.classList.add("sa-project-thumb");
-  stage.insertBefore(thumb, greenFlagOverlay);
+
+  const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_2bejr box_box_2jjDp"]', {
+    markAsSeen: true,
+  });
+
+  if (addon.settings.get("loading")) {
+    const LoaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
+    stageWrapper.insertBefore(thumb, LoaderBackground);
+  }
 
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", (e) => {
+    console.log(e.detail.action.type);
+    if (e.detail.action.type == "scratch-gui/project-changed/SET_PROJECT_CHANGED") {
+      moveThumb();
+      // TODO: Remove listener
+    }
     if (e.detail.action.type == "scratch-gui/vm-status/SET_STARTED_STATE") {
       document.getElementById("sa-project-thumb").remove();
     }
   });
+
+  function moveThumb() {
+    console.log("moveThumb activated")
+    // Since the loading screen has a different size and layer
+    // just remove the thumbnail and re-add it in the correct place
+    document.getElementById("sa-project-thumb").remove();
+    const stage = document.querySelector('div[class*="stage_stage"]');
+    const greenFlagOverlay = stage.querySelector('[class*="stage_green-flag-overlay-wrapper_"]');
+    stage.insertBefore(thumb, greenFlagOverlay);
+  }
 
   addon.tab.displayNoneWhileDisabled(thumb, {
     display: "inline",
