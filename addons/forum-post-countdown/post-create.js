4b59ac6ff4db1100c4ee40d3dbcac6733d562350
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-08-02 20:45:09 +0200

    Implement Samq64's review

--- addons/forum-post-countdown/post-create.js
+++
@@ -1,9 +0,0 @@
-export default async function () {
-  const submitButton = document.querySelector("#djangobbwrap .form-submit [type=submit]");
-
-  submitButton.addEventListener("click", () => {
-    if (!localStorage.getItem("sa-forum-post-countdown")) {
-      localStorage.setItem("sa-forum-post-countdown", Date.now());
-    }
-  });
-}
--- addons/forum-post-countdown/userscript.js
+++ addons/forum-post-countdown/userscript.js
@@ -4,6 +4,16 @@ export default async function ({ addon, msg }) {
     return;
   }
 
+  const loggedIn = await addon.auth.fetchIsLoggedIn();
+
+  const submitButton = document.querySelector("#djangobbwrap .form-submit [type=submit]");
+
+  submitButton.addEventListener("click", () => {
+    if (!localStorage.getItem("sa-forum-post-countdown")) {
+      localStorage.setItem("sa-forum-post-countdown", Date.now());
+    }
+  });
+
   let secondCount = (
     await fetch("/session", { headers: { "x-requested-with": "XMLHttpRequest" } }).then((resp) => resp.json())
   ).permissions.new_scratcher
@@ -14,15 +24,17 @@ export default async function ({ addon, msg }) {
   elt.id = "sa-forum-post-countdown";
   document.body.appendChild(elt);
 
+  const seconds = secondCount - Math.floor((now - countdown) / 100);
+
   setInterval(async () => {
     const now = Date.now();
-    if (now > Number(countdown) + secondCount * 1000 || !(await addon.auth.fetchIsLoggedIn())) {
+    if (now > Number(countdown) + secondCount * 1000 || !loggedIn) {
       localStorage.removeItem("sa-forum-post-countdown");
       elt.remove();
       return;
     }
-    elt.textContent = msg("seconds-left", { seconds: secondCount - Math.floor((now - countdown) / 1000) });
-  });
-
+    elt.textContent = msg("seconds-left", { seconds });
+  }, 250);
 
   addon.tab.displayNoneWhileDisabled(elt);
+}
