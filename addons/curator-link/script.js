f2f68b4875ba35e5da03a5703a4be8ddba6e1c92
Samq64 <81489795+Samq64@users.noreply.github.com>
2023-08-02 10:47:14 -0400

    Make it a new addon again

--- addons/curator-link/script.js
+++ addons/curator-link/script.js
@@ -1,71 +1,32 @@
 import { escapeHTML } from "../../libraries/common/cs/autoescaper.js";
-import { pingifyTextNode } from "../../libraries/common/cs/fast-linkify.js";
+export default async function ({ addon }) {
+  const heading = await addon.tab.waitForElement(".inner.mod-splash + .inner.mod-splash h4", {
+    reduxCondition: (state) => state.splash.featured.status === "FETCHED",
+    reduxEvents: ["SET_FETCH_STATUS", "SET_ROWS"],
+  });
 
-export default async function ({ addon, console }) {
-  const pageType = document.location.pathname.substring(1).split("/")[0];
+  const curator = addon.tab.redux.state.splash.featured.rows.curator_top_projects[0].curator_name;
+  const link = document.createElement("a");
+  link.textContent = curator;
+  link.href = `https://scratch.mit.edu/users/${curator}/`;
+  link.id = "curator-link";
 
-  if (pageType === "" && addon.settings.get("curator")) {
-    // Empty string means the front page
-    const heading = await addon.tab.waitForElement(".inner.mod-splash + .inner.mod-splash h4", {
-      reduxCondition: (state) => state.splash.featured.status === "FETCHED",
-      reduxEvents: ["SET_FETCH_STATUS", "SET_ROWS"],
-    });
-
-    const curator = addon.tab.redux.state.splash.featured.rows.curator_top_projects[0].curator_name;
-    const link = document.createElement("a");
-    link.textContent = curator;
-    link.href = `https://scratch.mit.edu/users/${curator}/`;
-    link.id = "curator-link";
-
-    if (!addon.self.disabled) linkifyCurator();
-
-    addon.self.addEventListener("disabled", () => unLinkifyCurator());
-    addon.self.addEventListener("reenabled", () => linkifyCurator());
-    addon.settings.addEventListener("change", () => {
-      addon.settings.get("curator") ? linkifyCurator() : unLinkifyCurator();
-    });
-
-    function linkifyCurator() {
-      heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
-        "{curatorId}",
-        link.outerHTML
-      );
-    }
-    function unLinkifyCurator() {
-      heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
-        "{curatorId}",
-        curator
-      );
-    }
+  if (!addon.self.disabled) {
+    heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
+      "{curatorId}",
+      link.outerHTML
+    );
   }
-
-  if (pageType === "discuss" && addon.settings.get("mentions")) {
-    const linkified = [];
-    addon.self.addEventListener("disabled", () => unLinkifyMentions());
-    addon.self.addEventListener("reenabled", () => linkifyMentions());
-    addon.settings.addEventListener("change", () => {
-      addon.settings.get("mentions") ? linkifyMentions() : unLinkifyMentions();
-    });
-    function linkifyMentions() {
-      for (const { element } of linkified) {
-        pingifyTextNode(element);
-      }
-    }
-    function unLinkifyMentions() {
-      for (const { element, original } of linkified) {
-        element.innerHTML = original;
-      }
-    }
-    function pingify(element) {
-      linkified.push({
-        element,
-        original: element.innerHTML,
-      });
-      return pingifyTextNode(element);
-    }
-    while (true) {
-      let post = await addon.tab.waitForElement(".post_body_html", { markAsSeen: true });
-      if (!addon.self.disabled) pingify(post);
-    }
-  }
+  addon.self.addEventListener("disabled", () => {
+    heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
+      "{curatorId}",
+      curator
+    );
+  });
+  addon.self.addEventListener("reenabled", () => {
+    heading.innerHTML = escapeHTML(addon.tab.scratchMessage("splash.projectsCuratedBy")).replace(
+      "{curatorId}",
+      link.outerHTML
+    );
+  });
 }
---
+++ addons/linkify-mentions/script.js
@@ -0,0 +1,31 @@
+import { pingifyTextNode } from "../../libraries/common/cs/fast-linkify.js";
+
+export default async function ({ addon, console }) {
+const linkified = [];
+    addon.self.addEventListener("disabled", () => unLinkifyMentions());
+    addon.self.addEventListener("reenabled", () => linkifyMentions());
+    addon.settings.addEventListener("change", () => {
+      addon.settings.get("mentions") ? linkifyMentions() : unLinkifyMentions();
+    });
+    function linkifyMentions() {
+      for (const { element } of linkified) {
+        pingifyTextNode(element);
+      }
+    }
+    function unLinkifyMentions() {
+      for (const { element, original } of linkified) {
+        element.innerHTML = original;
+      }
+    }
+    function pingify(element) {
+      linkified.push({
+        element,
+        original: element.innerHTML,
+      });
+      return pingifyTextNode(element);
+    }
+    while (true) {
+      let post = await addon.tab.waitForElement(".post_body_html", { markAsSeen: true });
+      if (!addon.self.disabled) pingify(post);
+    }
+}
