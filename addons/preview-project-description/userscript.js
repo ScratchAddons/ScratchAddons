3a12797fb6a9fb7cfabc366e489276b38147e439
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-08-04 01:00:11 -0700

    finish addon

--- addons/preview-project-description/userscript.js
+++ addons/preview-project-description/userscript.js
@@ -1,13 +1,8 @@
 export default async function ({ addon, console, msg }) {
     const loggedInUser = await addon.auth.fetchUsername();
-    const projectOwner = document.querySelector(".project-header .title a")?.innerText;
+    const projectOwner = document.querySelector(".project-header a img").alt;
     if (loggedInUser == null || loggedInUser != projectOwner)
         return;
-
-    var currentlyEnabled = addon.settings.get("enable-default");
-    
-    if (currentlyEnabled)
-        togglePreview(override=true);
 
     const actionsContainer = document.querySelector(".action-buttons");
     const enableSwitcher = document.createElement("button");
@@ -18,38 +13,43 @@ export default async function ({ addon, console, msg }) {
     const instructionPreview = document.createElement("div");
     const instructionEditor = document.querySelector("textarea[name=\"description\"]");
     const instructionContainer = instructionEditor.closest(".description-block");
+    const instructionForm = instructionEditor.closest(".project-description-form");
     instructionPreview.classList.add("project-description");
-    instructionPreview.innerText = instructionEditor?.value;
+    instructionPreview.innerText = instructionEditor.value;
 
     const notesCreditPreview = document.createElement("div");
     const notesCreditEditor = document.querySelector("textarea[name=\"instructions\"]");
     const notesCreditContainer = notesCreditEditor.closest(".description-block");
+    const notesCreditForm = notesCreditEditor.closest(".project-description-form");
     notesCreditPreview.classList.add("project-description");
-    notesCreditPreview.innerText = notesCreditEditor?.value;
+    notesCreditPreview.innerText = notesCreditEditor.value;
 
-    notesCreditContainer?.appendChild(notesCreditPreview);
-    instructionContainer?.appendChild(instructionPreview);
-    actionsContainer?.appendChild(enableSwitcher);
+    notesCreditContainer.appendChild(notesCreditPreview);
+    instructionContainer.appendChild(instructionPreview);
+    actionsContainer.appendChild(enableSwitcher);
+
+    var currentlyEnabled = addon.settings.get("enable-default");
+    togglePreview(null, currentlyEnabled)
 
     function togglePreview(_ = null, override = !currentlyEnabled) {
         enableSwitcher.innerHTML = `<span>${override ? "Disable" : "Enable"} Preview</span>`;
         currentlyEnabled = override;
 
         if (override) {
-            instructionEditor?.style.display = "none";
-            instructionPreview.innerText = instructionEditor?.value;
+            instructionForm.style.display = "none";
+            instructionPreview.innerText = instructionEditor.value;
             instructionPreview.style.removeProperty("display");
 
-            notesCreditEditor?.style.display = "none";
-            notesCreditPreview.innerText = notesCreditEditor?.value;
+            notesCreditForm.style.display = "none";
+            notesCreditPreview.innerText = notesCreditEditor.value;
             notesCreditPreview.style.removeProperty("display");
             return;
         }
 
-        instructionEditor?.style.removeProperty("display");
+        instructionForm.style.removeProperty("display");
         instructionPreview.style.display = "none";
 
-        notesCreditEditor?.style.removeProperty("display");
+        notesCreditForm.style.removeProperty("display");
         notesCreditPreview.style.display = "none";
     }
 }
