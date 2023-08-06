0ec5cdba7c688f7215805c21ebd133864caaeee5
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-08-06 15:23:52 -0700

    More links integration

--- addons/preview-project-description/userscript.js
+++ addons/preview-project-description/userscript.js
@@ -1,9 +1,11 @@
-export default async function ({ addon, console, msg }) {
+export default async function ({ addon, console, msg }) {  
   const loggedInUser = await addon.auth.fetchUsername();
   const projectOwner = addon.tab.redux.state.preview.projectInfo.author.username;
   if (loggedInUser == null || loggedInUser != projectOwner) return;
 
   const matchUsername = /\@([A-Z]|[a-z]|[0-9]|\-|\_){1,20}/gm;
+  const moreLinksEnabled = (await addon.self.getEnabledAddons()).includes("more-links");
+  const matchHyperLinks = /(https?\:\/\/)?.*\.([A-Z]|[a-z]|-){2,24}((\/.*)*)?/gm
 
   const actionsContainer = document.querySelector(".action-buttons");
   const enableSwitcher = document.createElement("button");
@@ -66,6 +68,12 @@ export default async function ({ addon, console, msg }) {
     let rendered = input.replace(matchUsername, (matched) => {
       return `<a href="/users/${matched.slice(1)}">${matched}</a>`;
     });
+    if (moreLinksEnabled)
+      rendered.replace(matchHyperLinks, (matched) => {
+        if (matched.startswith("https://") || matched.startswith("http://"))
+          matched = "//" + matched;
+        return `<a href="${matched}">${matched}</a>`;
+      })
     preview.innerHTML = rendered;
   }
 
