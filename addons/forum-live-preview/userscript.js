d517bdba6260025efbadaaea71e13d482161aba1
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-09-23 22:04:58 +0200

    Forum live preview: Be affected by toolbar buttons

--- addons/forum-live-preview/userscript.js
+++ addons/forum-live-preview/userscript.js
@@ -1,6 +1,7 @@
 export default async function ({ addon, console }) {
   const textarea = await addon.tab.waitForElement(".markItUpEditor");
   const previewButton = await addon.tab.waitForElement(".markItUpButton.preview");
+  const markItUpButtons = document.querySelectorAll(".markItUpButton:not(.markItUpDropMenu)");
   let previewIframe;
   let delay;
   switch (addon.settings.get("rate")) {
@@ -32,9 +33,14 @@ export default async function ({ addon, console }) {
   };
 
   let timeout;
-  textarea.addEventListener("input", () => {
+  const delayedPreview = () => {
     if (timeout !== undefined) clearTimeout(timeout);
     timeout = setTimeout(updatePreview, delay);
+  };
+
+  textarea.addEventListener("input", delayedPreview);
+  markItUpButtons.forEach((el) => {
+    el.addEventListener("click", delayedPreview);
   });
   addon.self.addEventListener("disabled", () => {
     showPreview();
