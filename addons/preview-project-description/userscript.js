5f1fdb4435516136126b1ec02c9c46df751398de
World_Languages <mundofinky@gmail.com>
2023-09-24 18:40:04 -0300

    preview-project-description: Support dynamicDisable properly

--- addons/preview-project-description/userscript.js
+++ addons/preview-project-description/userscript.js
@@ -18,6 +18,10 @@ export default async function ({ addon, console, msg }) {
   });
   label.append(checkboxInput, sliderSpan);
   divElement.append(span, label);
+
+  addon.tab.displayNoneWhileDisabled(divElement, { display: "flex" });
+  addon.self.addEventListener("disabled", () => togglePreview(false));
+
   checkboxInput.addEventListener("change", () => {
     togglePreview(checkboxInput.checked);
   });
