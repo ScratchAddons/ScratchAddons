e7064a45987c7e2e83e4e138b2f5ca4905d79bc0
fath11 <fath11@users.noreply.github.com>
2023-09-25 12:37:17 +0000

    Format code

--- addons/open-in-gandi/userscript.js
+++ addons/open-in-gandi/userscript.js
@@ -1,14 +1,14 @@
 export default async function ({ addon, console, msg }) {
-    const button = document.createElement("button");
-    button.className = "button sa-gd-button";
-    button.title = msg("open");
+  const button = document.createElement("button");
+  button.className = "button sa-gd-button";
+  button.title = msg("open");
 
-    button.onclick = async (e) => {
-        const projectId = window.location.pathname.split("/")[2];
-        window.location.href = `https://scratch.mit.edu.host/projects/${projectId}/`;
-    }
+  button.onclick = async (e) => {
+    const projectId = window.location.pathname.split("/")[2];
+    window.location.href = `https://scratch.mit.edu.host/projects/${projectId}/`;
+  };
 
-    await addon.tab.waitForElement("div.project-buttons");
-  
-    addon.tab.appendToSharedSpace({ space: "beforeRemixButton", element: button, order: 2 });
-  }
+  await addon.tab.waitForElement("div.project-buttons");
+
+  addon.tab.appendToSharedSpace({ space: "beforeRemixButton", element: button, order: 2 });
+}
