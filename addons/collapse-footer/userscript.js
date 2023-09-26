7707555cdaeae2eed2ff1c7cc635033355d11d9a
Samq64 <81489795+Samq64@users.noreply.github.com>
2023-09-26 12:01:17 -0400

    Check for addon & fix icon

--- addons/collapse-footer/userscript.js
+++ addons/collapse-footer/userscript.js
@@ -1,15 +1,16 @@
 export default async function ({ addon, console }) {
+  const enabledAddons = await addon.self.getEnabledAddons("community");
   const footer = await addon.tab.waitForElement("#footer");
+
   const icon = document.createElement("img");
   icon.src = addon.self.dir + "/icon.svg";
   icon.height = 16;
   icon.width = 16;
   icon.className = "sa-footer-arrow";
   footer.insertBefore(icon, footer.firstChild);
-  addon.tab.displayNoneWhileDisabled(icon, { display: "block" });
 
-  if (!addon.settings.get("infiniteScroll")) {
-    // If the setting is enabled infinate-scroll
+  if (!(addon.settings.get("infiniteScroll") && enabledAddons.includes("infinite-scroll"))) {
+    // If the setting is enabled, infinate-scroll
     // adds the class on the pages it runs on instead
     document.body.classList.add("sa-collapse-footer");
   }
