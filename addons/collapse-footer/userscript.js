a5cabf0735c53f96b7c685972bb3f720c8186d6a
Samq64 <81489795+Samq64@users.noreply.github.com>
2023-09-26 10:56:52 -0400

    Use arrow icon

--- addons/collapse-footer/userscript.js
+++ addons/collapse-footer/userscript.js
@@ -1,4 +1,13 @@
 export default async function ({ addon, console }) {
+  const footer = await addon.tab.waitForElement("#footer");
+  const icon = document.createElement("img");
+  icon.src = addon.self.dir + "/icon.svg"
+  icon.height = 16;
+  icon.width = 16;
+  icon.className = "sa-footer-arrow";
+  footer.insertBefore(icon, footer.firstChild);
+  addon.tab.displayNoneWhileDisabled(icon, { display: "block" });
+
   if (!addon.settings.get("infiniteScroll")) {
     // If the setting is enabled infinate-scroll
     // adds the class on the pages it runs on instead
@@ -8,7 +17,6 @@ export default async function ({ addon, console }) {
   if (location.pathname.split("/")[1] === "") {
     // Moves the donor text into the footer on the front page
     const root = document.documentElement;
-    const footer = await addon.tab.waitForElement("#footer");
     const donor = await addon.tab.waitForElement("#donor");
     footer.appendChild(donor);
     root.style.setProperty("--footer-hover-height", "426px");
