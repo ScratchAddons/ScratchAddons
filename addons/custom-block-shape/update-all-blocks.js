d680f49876e9b865f37c15f30f4c9b33bab7ce10
World_Languages <mundofinky@gmail.com>
2023-10-01 19:42:23 -0300

    Fix workspace undo bugs after workspace reload

--- addons/custom-block-shape/update-all-blocks.js
+++ addons/custom-block-shape/update-all-blocks.js
@@ -1,4 +1,7 @@
-export function updateAllBlocks(vm, workspace) {
+export function updateAllBlocks(vm, workspace, blockly) {
+  const eventsOriginallyEnabled = blockly.Events.isEnabled();
+  blockly.Events.disable(); // Clears workspace right-click→undo (see SA/SA#6691)
+
   if (workspace) {
     if (vm.editingTarget) {
       vm.emitWorkspaceUpdate();
@@ -14,4 +17,8 @@ export function updateAllBlocks(vm, workspace) {
       workspace.toolboxRefreshEnabled_ = true;
     }
   }
+
+  // There's no particular reason for checking whether events were originally enabled.
+  // Unconditionally enabling events at this point could, in theory, cause bugs in the future,
+  if (eventsOriginallyEnabled) blockly.Events.enable(); // Re-enable events
 }
