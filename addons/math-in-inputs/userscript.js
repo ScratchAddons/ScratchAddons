33d533b6913da6c4f2f43c81a9e23d4d737e34a3
Jazza <greeny-231@outlook.com>
2023-10-12 16:24:38 +0800

    Eval is evil and we dont like evil

--- addons/math-in-inputs/userscript.js
+++ addons/math-in-inputs/userscript.js
@@ -22,22 +22,71 @@ export default async function ({ addon }) {
     return false;
   };
 
-  function power(match, x, y) {
-    return `Math.pow(${x}, ${y})`;
+  // And this is stolen from SO, I love not having to code stuff
+  let parens = /\(([0-9+\-*/\^ .]+)\)/; // Regex for identifying parenthetical expressions
+  let exp = /(\d+(?:\.\d+)?) ?\^ ?(\d+(?:\.\d+)?)/; // Regex for identifying exponentials (x ^ y)
+  let expAlt = /(\d+(?:\.\d+)?) ?\*\* ?(\d+(?:\.\d+)?)/;
+  let mul = /(\d+(?:\.\d+)?) ?\* ?(\d+(?:\.\d+)?)/; // Regex for identifying multiplication (x * y)
+  let div = /(\d+(?:\.\d+)?) ?\/ ?(\d+(?:\.\d+)?)/; // Regex for identifying division (x / y)
+  let add = /(\d+(?:\.\d+)?) ?\+ ?(\d+(?:\.\d+)?)/; // Regex for identifying addition (x + y)
+  let sub = /(\d+(?:\.\d+)?) ?- ?(\d+(?:\.\d+)?)/; // Regex for identifying subtraction (x - y)
+
+  /**
+   * Evaluates a numerical expression as a string and returns a Number
+   * Follows standard PEMDAS operation ordering
+   * @param {String} expr Numerical expression input
+   * @returns {Number} Result of expression
+   */
+  function evaluate(expr) {
+    if (isNaN(Number(expr))) {
+      if (parens.test(expr)) {
+        let newExpr = expr.replace(parens, function (match, subExpr) {
+          return evaluate(subExpr);
+        });
+        return evaluate(newExpr);
+      } else if (exp.test(expr)) {
+        let newExpr = expr.replace(exp, function (match, base, pow) {
+          return Math.pow(Number(base), Number(pow));
+        });
+        return evaluate(newExpr);
+      } else if (expAlt.test(expr)) {
+        let newExpr = expr.replace(expAlt, function (match, base, pow) {
+          return Math.pow(Number(base), Number(pow));
+        });
+        return evaluate(newExpr);
+      } else if (mul.test(expr)) {
+        let newExpr = expr.replace(mul, function (match, a, b) {
+          return Number(a) * Number(b);
+        });
+        return evaluate(newExpr);
+      } else if (div.test(expr)) {
+        let newExpr = expr.replace(div, function (match, a, b) {
+          if (b != 0) return Number(a) / Number(b);
+          else throw new Error("Division by zero");
+        });
+        return evaluate(newExpr);
+      } else if (add.test(expr)) {
+        let newExpr = expr.replace(add, function (match, a, b) {
+          return Number(a) + Number(b);
+        });
+        return evaluate(newExpr);
+      } else if (sub.test(expr)) {
+        let newExpr = expr.replace(sub, function (match, a, b) {
+          return Number(a) - Number(b);
+        });
+        return evaluate(newExpr);
+      } else {
+        return expr;
+      }
+    }
+    return Number(expr);
   }
 
   function parseMath(value) {
     if (!/^[0-9+\-*/().^]+$/.test(value)) {
       return value;
     }
-
-    const processedValue = value.replace(/(\d+)\^(\d+)/g, power);
-
-    try {
-      return eval(processedValue) || 0;
-    } catch (error) {
-      return 0;
-    }
+    return evaluate(value);
   }
 
   // #Garboism
