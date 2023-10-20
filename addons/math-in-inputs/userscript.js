9b369db98623e430343f7216ea209ced435faaff
Tacodiva <27910867+Tacodiva@users.noreply.github.com>
2023-10-20 18:27:32 +1100

    Misc stuff

--- addons/math-in-inputs/userscript.js
+++ addons/math-in-inputs/userscript.js
@@ -1,39 +1,31 @@
 export default async function ({ addon }) {
   function getRegexFromSettings() {
     let textInInputs = addon.settings.get("textInInputs");
-    return textInInputs ? /^[0-9a-z+\-*^/().]+$/i : /^[0-9+\-*^/().]+$/;
+    return textInInputs ? /^.*$/i : /^[0-9+\-*^/(). ]+$/;
   }
 
   // I 100% stole this part of the code from "editor-number-arrow-keys"
   const isSupportedElement = (el) => {
+    if (!el.classList) return false;
     if (el.classList.contains("blocklyHtmlInput")) return true;
     else if (el.matches(".mediaRecorderPopupContent input[type=text]")) {
       // Number inputs in `mediarecorder` addon modal
       // The inputs should be type=text because I change that below to allow math symbols
       return true;
     } else if (el.className.includes("input_input-form_")) {
-      if (el.matches("[class*=sprite-info_sprite-info_] [class*=input_input-small_]")) {
-        // Sprite X/Y coordinates, size and direction (excludes sprite name)
-        return true;
-      } else if (el.matches("[class*=paint-editor_editor-container-top_] input[type=text")) {
-        // Number inputs in costume editor
-        return true;
-      } else if (el.className.includes("input_input-form_l9eYg")) {
-        // Colour picker?
-        return true;
-      } else return false;
+      return true;
     }
     return false;
   };
 
   // And this is stolen from SO, I love not having to code stuff
   let parens = /\(([0-9+\-*/\^ .]+)\)/; // Regex for identifying parenthetical expressions
-  let exp = /(\d+(?:\.\d+)?) ?\^ ?(\d+(?:\.\d+)?)/; // Regex for identifying exponentials (x ^ y)
-  let expAlt = /(\d+(?:\.\d+)?) ?\*\* ?(\d+(?:\.\d+)?)/; //Regex for identifying **
-  let mul = /(\d+(?:\.\d+)?) ?\* ?(\d+(?:\.\d+)?)/; // Regex for identifying multiplication (x * y)
-  let div = /(\d+(?:\.\d+)?) ?\/ ?(\d+(?:\.\d+)?)/; // Regex for identifying division (x / y)
-  let add = /(\d+(?:\.\d+)?) ?\+ ?(\d+(?:\.\d+)?)/; // Regex for identifying addition (x + y)
-  let sub = /(\d+(?:\.\d+)?) ?- ?(\d+(?:\.\d+)?)/; // Regex for identifying subtraction (x - y)
+  let exp = /(\d+(?:\.\d+)?) *\^ *(\d+(?:\.\d+)?)/; // Regex for identifying exponentials (x ^ y)
+  let expAlt = /(\d+(?:\.\d+)?) *\*\* *(\d+(?:\.\d+)?)/; //Regex for identifying **
+  let mul = /(\d+(?:\.\d+)?) *\* *(\d+(?:\.\d+)?)/; // Regex for identifying multiplication (x * y)
+  let div = /(\d+(?:\.\d+)?) *\/ *(\d+(?:\.\d+)?)/; // Regex for identifying division (x / y)
+  let add = /(\d+(?:\.\d+)?) *\+ *(\d+(?:\.\d+)?)/; // Regex for identifying addition (x + y)
+  let sub = /(\d+(?:\.\d+)?) *- *(\d+(?:\.\d+)?)/; // Regex for identifying subtraction (x - y)
 
   /**
    * Evaluates a numerical expression as a string and returns a Number
@@ -65,8 +57,7 @@ export default async function ({ addon }) {
         return evaluate(newExpr);
       } else if (div.test(expr)) {
         let newExpr = expr.replace(div, function (match, a, b) {
-          if (b != 0) return Number(a) / Number(b);
-          else throw new Error("Division by zero");
+          return Number(a) / Number(b);
         });
         return evaluate(newExpr);
       } else if (add.test(expr)) {
@@ -87,7 +78,7 @@ export default async function ({ addon }) {
   }
 
   function parseMath(value) {
-    if (!/^[0-9+\-*/().^]+$/.test(value)) {
+    if (!/^[0-9+\-*/().^ ]+$/.test(value)) {
       return value;
     }
     return evaluate(value);
@@ -100,31 +91,32 @@ export default async function ({ addon }) {
     if (!addon.self.disabled) this.restrictor_ = getRegexFromSettings();
     return original.apply(this, args);
   };
-
+  /**
+   *
+   * @param {Event} e
+   * @returns
+   */
   function handleParseInput(e) {
     if (addon.self.disabled) return;
     if (!isSupportedElement(e.target)) return;
     if (!e.target.value) return;
     const newValue = parseMath(e.target.value);
     Object.getOwnPropertyDescriptor(e.target.constructor.prototype, "value").set.call(e.target, newValue.toString());
-    e.target.dispatchEvent(new Event("input", { bubbles: true }));
-    e.target.blur();
   }
-
-  document.body.addEventListener(
+  document.addEventListener(
     "keydown",
     (e) => {
       if (e.key !== "Enter") return;
       handleParseInput(e);
     },
-    { capture: true }
+    true
   );
-  document.body.addEventListener(
-    "blur",
+  document.addEventListener(
+    "change",
     (e) => {
       handleParseInput(e);
     },
-    { capture: true }
+    true
   );
 
   function handleInputTypeChanges(input) {
@@ -133,7 +125,7 @@ export default async function ({ addon }) {
 
       input.addEventListener("focusin", function () {
         input.type = "text";
-        const inputLength = input.value.length;
+        const inputLength = input.value?.length ?? 0;
         input.setSelectionRange(inputLength, inputLength);
       });
 
@@ -149,9 +141,9 @@ export default async function ({ addon }) {
         mutation.addedNodes.forEach(function (node) {
           if (
             node instanceof HTMLElement &&
-            (node.className.includes("input-group_input-group_plJaJ") ||
-              node.classList.contains("input_input-form_l9eYg") ||
-              node.classList.contains("mediaRecorderPopupContent"))
+            (node.classList.contains(addon.tab.scratchClass("input-group_input-group")) ||
+            node.classList.contains(addon.tab.scratchClass("input_input-form")) ||
+            node.matches("input.mediaRecorderPopupContent"))
           ) {
             handleInputTypeChanges(node);
           }
