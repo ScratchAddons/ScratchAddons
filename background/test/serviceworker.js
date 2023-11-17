51b1996a41a8451a35858e83cc52c9f03492afd0
World_Languages <mundofinky@gmail.com>
2023-11-17 20:23:05 -0300

    Draft - MV3 manifest supported by Chrome & Firefox

---
+++ background/test/serviceworker.js
@@ -0,0 +1,8 @@
+// For Chrome
+
+import x from "./_module.js";
+
+console.log(x);
+console.log("I am a service worker!");
+console.log("window is", typeof window);
+console.log("My `self` object is:", self);
