ed10b3c169750b67b104afdee48d4f9c2662cfc7
Maximouse <51849865+mxmou@users.noreply.github.com>
2023-11-26 16:35:30 +0100

    discuss-button: hide username using CSS (#6889)

--- addons/discuss-button/r2.js
+++ addons/discuss-button/r2.js
@@ -5,24 +5,14 @@ export default async function ({ addon }) {
   const add = async () => {
     if (!span && (await addon.auth.fetchIsLoggedIn())) {
       const username = await addon.auth.fetchUsername();
-      const container = await addon.tab.waitForElement(".dropdown");
-      const dropdown = await addon.tab.waitForElement(".dropdown-menu .user-nav");
       (await addon.tab.waitForElement(".user-icon")).classList.add("sa-compact-profile-icon");
-      const profileSpans = dropdown.childNodes[0].childNodes[0];
+      const profileSpans = await addon.tab.waitForElement(".dropdown-menu .user-nav > :first-child > :first-child");
       span = profileSpans.appendChild(document.createElement("span"));
       span.className = "sa-profile-name";
       span.textContent = username;
-
-      // Remove username next to icon.
-      container.firstChild.childNodes[1].textContent = "";
     }
   };
   const remove = async () => {
-    const container = document.querySelector(".dropdown");
-    if (container) {
-      const username = await addon.auth.fetchUsername();
-      container.firstChild.childNodes[1].textContent = username;
-    }
     span?.remove();
     span = null;
     const icon = document.querySelector(".user-icon");
