1b1245a29e86d46b1670aa18f6db4bcfcd1c8265
DNin01 <106490990+DNin01@users.noreply.github.com>
2023-11-29 15:29:49 -0800

    Option to execute double-clicked scripts

--- addons/ctrl-click-run-scripts/userscript.js
+++ addons/ctrl-click-run-scripts/userscript.js
@@ -17,10 +17,30 @@ export default async function ({ addon, console }) {
     }
   );
 
-  // Limits all script running to CTRL + click
+  let doubleClick = false;
+  let doubleClickTimeout;
+  function startDoubleClick() {
+    if (addon.settings.get("double-click")) {
+      doubleClick = true;
+      doubleClickTimeout = setTimeout(() => {
+        doubleClick = false;
+      }, 400);
+    }
+  }
+
+  // Capture clicks on scripts
   const newBlocklyListen = function (e) {
     if (!addon.self.disabled && e.element === "stackclick" && !ctrlKeyPressed) {
-      return;
+      if (doubleClick) {
+        // Allow script to execute on double click
+        clearTimeout(doubleClickTimeout);
+        startDoubleClick();
+        originalBlocklyListen.call(this, e);
+      } else {
+        // Start a double-click timeout and prevent script execution
+        startDoubleClick();
+        return;
+      }
     } else {
       originalBlocklyListen.call(this, e);
     }
