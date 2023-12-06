64d8cf59ab1ce05eae9cc5308d651e82b80704f1
Jazza <greeny-231@outlook.com>
2023-12-06 10:28:29 +0800

    format

--- addons/ctrl-enter-ftso/userscript.js
+++ addons/ctrl-enter-ftso/userscript.js
@@ -4,7 +4,8 @@ To do:
 Add support for the command key
 Find way to make ctrl + enter work like enter, variables cant be made while ctrl is pressed (at least on tw desktop)
 
-*/ await addon.tab.waitForElement("input[type=radio][value=local]");
+*/
+  await addon.tab.waitForElement("input[type=radio][value=local]");
   const local = document.querySelector("input[type=radio][value=local]");
   const global = document.querySelector("input[type=radio][value=global]");
   let toClick;
@@ -13,7 +14,7 @@ Find way to make ctrl + enter work like enter, variables cant be made while ctrl
     if (!(e.ctrlKey || e.metaKey)) return;
     if (!toClick) {
       toClick = local.checked ? local : global;
-      local.click();
+      local?.click();
     }
   });
 
