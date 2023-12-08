d60ac75a2484524d00d93ec38f1cc0761d3ecb3f
Jazza <greeny-231@outlook.com>
2023-12-08 12:58:09 +0800

    fix for own projects

--- addons/download-button/userscript.js
+++ addons/download-button/userscript.js
@@ -2,11 +2,24 @@ import downloadBlob from "../../libraries/common/cs/download-blob.js";
 
 export default async function ({ addon, console }) {
   const vm = addon.tab.traps.vm;
+  const { redux } = addon.tab;
+
+  await redux.waitForState((state) => typeof state.session.session?.user === "object");
+  const username = await addon.auth.fetchUsername();
+
+  const projectAuthor =
+    (await redux.state.preview.status.project) === "FETCHED"
+      ? redux.state.preview.projectInfo.author?.username
+      : "Can not find project author";
+
+  const isOwn = username === projectAuthor;
 
   async function download() {
     const project = await vm.saveProjectSb3();
-    const title = await addon.tab.waitForElement(".project-title");
-    downloadBlob(`${title.innerText}.sb3`, project);
+    const title = isOwn
+      ? await addon.tab.waitForElement(".project-title input")
+      : await addon.tab.waitForElement(".project-title");
+    downloadBlob(`${isOwn ? title.value : title.innerText}.sb3`, project);
   }
 
   const downloadButton = document.createElement("button");
