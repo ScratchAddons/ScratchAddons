5bf50c63e6cd81cd687c9d6e567613ec7f70e7b8
iqnite <iqnite@users.noreply.github.com>
2023-12-11 12:07:21 +0000

    Format code

--- addons/redirect-mobile-forums/userscript.js
+++ addons/redirect-mobile-forums/userscript.js
@@ -1,3 +1,4 @@
 export default async function ({ addon, console, msg }) {
-  if (window.location.href.includes("https://scratch.mit.edu/discuss/m")) window.location.replace(window.location.href.replace("m/", ""));
+  if (window.location.href.includes("https://scratch.mit.edu/discuss/m"))
+    window.location.replace(window.location.href.replace("m/", ""));
 }
