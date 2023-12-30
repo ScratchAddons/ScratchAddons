8cadba6153eff8e593dc9827c0543ebadf210953
Joeclinton1 <Joeclinton1@users.noreply.github.com>
2023-12-30 20:17:28 +0000

    Format code

--- addons/reorder-custom-inputs/userscript.js
+++ addons/reorder-custom-inputs/userscript.js
@@ -1,66 +1,70 @@
 export default async function ({ addon, console }) {
-    const Blockly = await addon.tab.traps.getBlockly();
+  const Blockly = await addon.tab.traps.getBlockly();
 
-    function createArrow(direction, callback) {
-        const path = direction === "left" ? "M 17 13 L 9 21 L 17 30" : "M 9 13 L 17 21 L 9 30"
-        const arrowDiv = Blockly.WidgetDiv.DIV.appendChild(document.createElement('div'));
-        arrowDiv.innerHTML = `
+  function createArrow(direction, callback) {
+    const path = direction === "left" ? "M 17 13 L 9 21 L 17 30" : "M 9 13 L 17 21 L 9 30";
+    const arrowDiv = Blockly.WidgetDiv.DIV.appendChild(document.createElement("div"));
+    arrowDiv.innerHTML = `
         <svg width="20px" height="40px" 
              style="left: ${direction === "left" ? "calc(50% - 20px)" : "calc(50% + 20px)"}" 
              class="blocklyTextShiftArrow">
             <path d="${path}" fill="none" stroke="#FF661A" stroke-width="2"></path>
         </svg>`;
 
-        arrowDiv.addEventListener('click', callback);
+    arrowDiv.addEventListener("click", callback);
+  }
+
+  Blockly.ScratchBlocks.ProcedureUtils.shiftFieldCallback = function (field, direction) {
+    const proc = this.parentBlock_ ? this.parentBlock_ : this;
+    if (proc.inputList.length <= 1) return;
+
+    let inputNameToShift = null;
+    let newPosition;
+    for (const [i, input] of proc.inputList.entries()) {
+      const isTargetField = input.connection
+        ? input.connection.targetBlock()?.getField(field.name) === field
+        : input.fieldRow.includes(field);
+
+      if (isTargetField) {
+        inputNameToShift = input.name;
+        newPosition = direction === "left" ? i - 1 : i + 1;
+        break;
+      }
     }
 
-    Blockly.ScratchBlocks.ProcedureUtils.shiftFieldCallback = function (field, direction) {
-        const proc = this.parentBlock_ ? this.parentBlock_ : this;
-        if (proc.inputList.length <= 1) return;
+    if (inputNameToShift && newPosition >= 0 && newPosition < proc.inputList.length) {
+      const itemToMove = proc.inputList.splice(
+        proc.inputList.findIndex((input) => input.name === inputNameToShift),
+        1
+      )[0];
+      proc.inputList.splice(newPosition, 0, itemToMove);
 
-        let inputNameToShift = null;
-        let newPosition;
-        for (const [i, input] of proc.inputList.entries()) {
-            const isTargetField = input.connection
-                ? input.connection.targetBlock()?.getField(field.name) === field
-                : input.fieldRow.includes(field);
+      proc.onChangeFn();
+      proc.updateDisplay_();
 
-            if (isTargetField) {
-                inputNameToShift = input.name;
-                newPosition = direction === "left" ? i - 1 : i + 1;
-                break;
-            }
-        }
-
-        if (inputNameToShift && newPosition >= 0 && newPosition < proc.inputList.length) {
-            const itemToMove = proc.inputList.splice(proc.inputList.findIndex((input) => input.name === inputNameToShift), 1)[0];
-            proc.inputList.splice(newPosition, 0, itemToMove);
-
-            proc.onChangeFn();
-            proc.updateDisplay_();
-
-            focusOnInput(proc.inputList[newPosition]);
-        }
-    };
-
-    function focusOnInput(input) {
-        if (!input) return;
-        if (input.type === Blockly.DUMMY_INPUT) {
-            input.fieldRow[0].showEditor_();
-        } else if (input.type === Blockly.INPUT_VALUE) {
-            const target = input.connection.targetBlock();
-            target.getField("TEXT").showEditor_();
-        }
+      focusOnInput(proc.inputList[newPosition]);
     }
+  };
 
-    ["procedures_declaration", "argument_editor_boolean", "argument_editor_string_number"].forEach((blockType) => {
-        if (Blockly.Blocks[blockType]) Blockly.Blocks[blockType].shiftFieldCallback = Blockly.ScratchBlocks.ProcedureUtils.shiftFieldCallback;
-    });
+  function focusOnInput(input) {
+    if (!input) return;
+    if (input.type === Blockly.DUMMY_INPUT) {
+      input.fieldRow[0].showEditor_();
+    } else if (input.type === Blockly.INPUT_VALUE) {
+      const target = input.connection.targetBlock();
+      target.getField("TEXT").showEditor_();
+    }
+  }
 
-    const originalShowEditor = Blockly.FieldTextInputRemovable.prototype.showEditor_;
-    Blockly.FieldTextInputRemovable.prototype.showEditor_ = function () {
-        originalShowEditor.call(this);
-        createArrow("left", () => this.sourceBlock_.shiftFieldCallback(this, "left"));
-        createArrow("right", () => this.sourceBlock_.shiftFieldCallback(this, "right"));
-    };
+  ["procedures_declaration", "argument_editor_boolean", "argument_editor_string_number"].forEach((blockType) => {
+    if (Blockly.Blocks[blockType])
+      Blockly.Blocks[blockType].shiftFieldCallback = Blockly.ScratchBlocks.ProcedureUtils.shiftFieldCallback;
+  });
+
+  const originalShowEditor = Blockly.FieldTextInputRemovable.prototype.showEditor_;
+  Blockly.FieldTextInputRemovable.prototype.showEditor_ = function () {
+    originalShowEditor.call(this);
+    createArrow("left", () => this.sourceBlock_.shiftFieldCallback(this, "left"));
+    createArrow("right", () => this.sourceBlock_.shiftFieldCallback(this, "right"));
+  };
 }
