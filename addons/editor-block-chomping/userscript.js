ba3ce1c94e3644399bc13539a9d173b2f9e1853a
CST1229 <68464103+CST1229@users.noreply.github.com>
2024-01-06 23:59:38 +0100

    fixes and comments

--- addons/editor-block-chomping/userscript.js
+++ addons/editor-block-chomping/userscript.js
@@ -1,18 +1,20 @@
 export default async function ({ addon, msg, console }) {
 	const ScratchBlocks = await addon.tab.traps.getBlockly();
 
+	// Rerender the dragged block when updating the insertion marker
 	const ogConnectMarker = ScratchBlocks.InsertionMarkerManager.prototype.connectMarker_;
 	ScratchBlocks.InsertionMarkerManager.prototype.connectMarker_ = function() {
 		ogConnectMarker.call(this);
-		if (this.firstMarker_) {
+		if (!addon.self.disabled && this.firstMarker_) {
 			const block = this?.workspace_?.currentGesture_?.blockDragger_?.draggingBlock_;
+			block.noMoveConnection = true;
 			if (block) block.render(false);
 		}
 	}
 	const ogDisconnectMarker = ScratchBlocks.InsertionMarkerManager.prototype.disconnectMarker_;
 	ScratchBlocks.InsertionMarkerManager.prototype.disconnectMarker_ = function() {
 		ogDisconnectMarker.call(this);
-		if (this.firstMarker_) {
+		if (!addon.self.disabled && this.firstMarker_) {
 			const block = this?.workspace_?.currentGesture_?.blockDragger_?.draggingBlock_;
 			block.noMoveConnection = true;
 			if (block) block.render(false);
@@ -24,32 +26,44 @@ export default async function ({ addon, msg, console }) {
 	ScratchBlocks.BlockSvg.prototype.renderDraw_ = function(iconWidth, inputRows) {
 		if (addon.self.disabled) return ogDraw.call(this, iconWidth, inputRows);
 
+		// If the block contains a statement (C) input and has an insertion marker,
+		// use that to calculate the height of the statement inputs
 		let computeBlock = this;
 		if (this?.workspace?.currentGesture_?.blockDragger_?.draggedConnectionManager_) {
 			const dragger = this.workspace.currentGesture_.blockDragger_;
 			const manager = dragger.draggedConnectionManager_;
 			if (manager.markerConnection_ && manager.firstMarker_ && dragger.draggingBlock_ == this && dragger.draggingBlock_.type == manager.firstMarker_.type) {
-				computeBlock = manager.firstMarker_;
+				if (inputRows.some(row => row.some(input => input.type === ScratchBlocks.NEXT_STATEMENT))) {
+					computeBlock = manager.firstMarker_;
+				}
 			}
 		}
-		// change the height of substacks
-		// (if we set inputRows to computeBlock.renderCompute_,
+
+		// Change the height of substacks
+		// (If we set inputRows to computeBlock.renderCompute_,
 		// the references to the inputs would be wrong
 		// so they just won't update properly)
 		if (computeBlock !== this) {
 			const _inputRows = computeBlock.renderCompute_(iconWidth);
 			for (let i = 0; i < inputRows.length; i++) {
 				const row = inputRows[i];
-				row.height = _inputRows[i].height;
+				let update = false;
+				for (const input of row) {
+					if (input.type === ScratchBlocks.NEXT_STATEMENT) update = true;
+				}
+				if (update) row.height = Math.max(row.height, _inputRows[i].height);
 			}
 		}
 
 		ogDraw.call(this, iconWidth, inputRows);
+
+		// Moving the connections of a block while it's being dragged breaks it,
+		// so don't
 		if (computeBlock === this && !this.noMoveConnection) ogMoveConnections.call(this);
 		this.noMoveConnection = false;
 	};
 	ScratchBlocks.BlockSvg.prototype.renderMoveConnections_ = function() {
-		if (addon.self.disabled) return ogDraw.call(this);
-		// do nothing (this function is instead called by renderDraw_)
+		if (addon.self.disabled) return ogMoveConnections.call(this);
+		// Do nothing (this function is instead called by renderDraw_)
 	};
 }
