d6a33ca11e85551be2f5a12d6913fea66bd3d257
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-01-14 19:03:36 +0100

    Keep reset button, repurpose percentage as an input

--- addons/zoom-slider/userscript.js
+++ addons/zoom-slider/userscript.js
@@ -10,8 +10,8 @@ export default async function ({ addon, msg }) {
 
     const foreignObject = document.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
     foreignObject.setAttribute("height", "500");
-    foreignObject.setAttribute("width", "500");
-    foreignObject.setAttribute("x", "-80");
+    foreignObject.setAttribute("width", "120");
+    foreignObject.setAttribute("x", "-126");
     foreignObject.setAttribute("y", "95");
     const wrapperDiv = document.createElement("div");
     wrapperDiv.classList.add("sa-zoom-slider-wrapper");
@@ -22,9 +22,8 @@ export default async function ({ addon, msg }) {
     slider.step = "0.01";
     slider.value = blockly.mainWorkspace.scale;
     slider.classList.add("sa-zoom-slider");
-    const count = document.createElement("button");
+    const count = document.createElement("input");
     count.classList.add("sa-zoom-slider-count");
-    count.title = msg("reset");
     count.textContent = percentScale(blockly.mainWorkspace.scale);
     wrapperDiv.append(count, slider);
     foreignObject.append(wrapperDiv);
@@ -35,14 +34,17 @@ export default async function ({ addon, msg }) {
     });
     slider.addEventListener("input", async () => {
       blockly.mainWorkspace.setScale(parseFloat(slider.value));
-      count.textContent = percentScale(slider.value);
+      if (!countInput) {
+        count.value = percentScale(slider.value);
+      }
     });
-    count.addEventListener("mousedown", (e) => {
-      e.stopPropagation();
-    });
-    count.addEventListener("click", () => {
-      slider.value = blockly.mainWorkspace.options.zoomOptions.startScale;
+    let countInput = false;
+    count.addEventListener("input", async () => (countInput = true));
+    count.addEventListener("blur", async () => (countInput = false));
+    count.addEventListener("change", async () => {
+      slider.value = count.value / 100;
       slider.dispatchEvent(new Event("input"));
+      countInput = false;
     });
 
     setInterval(() => {
@@ -57,7 +59,7 @@ export default async function ({ addon, msg }) {
     });
   };
 
-  const percentScale = (scale) => Math.round(parseFloat(scale) * 100) + "%";
+  const percentScale = (scale) => Math.round(parseFloat(scale) * 100);
 
   addSlider();
   addon.tab.addEventListener("urlChange", addSlider);
