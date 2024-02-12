4d116402733acd16dfade1bb8d2a9dc5464acf74
DNin01 <106490990+DNin01@users.noreply.github.com>
2024-02-12 15:49:50 -0800

    Check installType instead of version_name in some places

--- libraries/common/load-vue-components.js
+++ libraries/common/load-vue-components.js
@@ -17,19 +17,22 @@ export default (filenames) =>
           const dom = new DOMParser().parseFromString(text, "text/html");
           const css = dom.querySelector("style")?.textContent;
           if (css) {
-            if (chrome.runtime.getManifest().version_name.includes("-prerelease")) {
-              const normalizedCss = css.replace("\n", "").trimEnd();
-              const normalizedText = text.replace(/\r/g, "");
-              const cssFirstLine = normalizedCss.substring(0, normalizedCss.indexOf("\n"));
-              const linesToAdd = normalizedText.split("\n").findIndex((line) => line === cssFirstLine);
-              const newLines = Object.assign(Array(linesToAdd), {
-                0: "/*",
-                [linesToAdd - 1]: "<style> */",
-              }).join("\n");
-              styles[filename] = `${newLines}\n${normalizedCss}/* \n</style> */\n/*# sourceURL=${htmlUrl} */`;
-            } else {
-              styles[filename] = css;
-            }
+            chrome.management.getSelf((result) => {
+              const developerMode = result.installType === "development";
+              if (developerMode) {
+                const normalizedCss = css.replace("\n", "").trimEnd();
+                const normalizedText = text.replace(/\r/g, "");
+                const cssFirstLine = normalizedCss.substring(0, normalizedCss.indexOf("\n"));
+                const linesToAdd = normalizedText.split("\n").findIndex((line) => line === cssFirstLine);
+                const newLines = Object.assign(Array(linesToAdd), {
+                  0: "/*",
+                  [linesToAdd - 1]: "<style> */",
+                }).join("\n");
+                styles[filename] = `${newLines}\n${normalizedCss}/* \n</style> */\n/*# sourceURL=${htmlUrl} */`;
+              } else {
+                styles[filename] = css;
+              }
+            });
           }
           return dom.querySelector("template").innerHTML;
         })
