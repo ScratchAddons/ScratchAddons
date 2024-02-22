7b667910b60d28103482871e34c1c78137ada69b
IQBit <iqnitetechnologies@gmail.com>
2024-02-22 17:33:24 +0100

    Fetch username late Better for when the user switches accounts or logs out.

--- addons/custom-menu-bar/username.js
+++ addons/custom-menu-bar/username.js
@@ -1,14 +1,13 @@
 export default async function ({ addon }) {
-  if (await addon.auth.fetchIsLoggedIn()) {
+  if (!(await addon.auth.fetchIsLoggedIn())) return; // Skip if not logged in
+  addon.tab.redux.initialize(); // Start listening to Redux events
+  addon.tab.redux.addEventListener("statechanged", async (e) => {
+    if (!addon.settings.get("compact-username")) return; // Skip if the option is disabled
+    if (e.detail.action.type !== "scratch-gui/menus/OPEN_MENU" || e.detail.action.menu !== "accountMenu") return; // Skip if another Redux event occurs
+    const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt"); // Do this if the menu is opened
+    const profile = profileSpans.appendChild(document.createElement("span")); // Add username to "Profile" menu option
     const username = await addon.auth.fetchUsername();
-    addon.tab.redux.initialize(); // Start listening to Redux events
-    addon.tab.redux.addEventListener("statechanged", async (e) => {
-      if (!addon.settings.get("compact-username")) return; // Skip if the option is disabled
-      if (e.detail.action.type !== "scratch-gui/menus/OPEN_MENU" || e.detail.action.menu !== "accountMenu") return; // Skip if another Redux event occurs
-      const profileSpans = await addon.tab.waitForElement(".menu_menu-item_3EwYA.menu_hoverable_3u9dt"); // Do this if the menu is opened
-      const profile = profileSpans.appendChild(document.createElement("span")); // Add username to "Profile" menu option
-      profile.textContent = username;
-      profile.className = "sa-profile-name";
-    });
-  }
+    profile.textContent = username;
+    profile.className = "sa-profile-name";
+  });
 }
