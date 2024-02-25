d9f9584d8a972c91863427df8b1927c65841bae0
Samq64 <Samq64@users.noreply.github.com>
2024-02-25 20:49:50 +0000

    Format code

--- addons/custom-menu-bar/username.js
+++ addons/custom-menu-bar/username.js
@@ -1,6 +1,6 @@
 export default async function ({ addon }) {
   while (true) {
-    const menuItem = await addon.tab.waitForElement('[class^=account-nav_user-info_] + div > ul > :first-child', {
+    const menuItem = await addon.tab.waitForElement("[class^=account-nav_user-info_] + div > ul > :first-child", {
       markAsSeen: true,
       reduxEvents: ["scratch-gui/menus/OPEN_MENU"],
       reduxCondition: (state) => state.scratchGui.menus.accountMenu,
