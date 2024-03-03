adb6b76984cc7a7773e4652d015db407545cf125
Jazza-231 <greeny-231@outlook.com>
2024-03-03 21:43:06 +0800

    thanks joe

--- addons/alphabetical-order/userscript.js
+++ addons/alphabetical-order/userscript.js
@@ -2,26 +2,7 @@ export default async function ({ addon, msg, console }) {
   const vm = addon.tab.traps.vm;
 
   Array.prototype.naturalSort = function () {
-    var a,
-      b,
-      a1,
-      b1,
-      rx = /(\d+)|(\D+)/g,
-      rd = /\d+/;
-    return this.sort(function (as, bs) {
-      a = String(as).toLowerCase().match(rx);
-      b = String(bs).toLowerCase().match(rx);
-      while (a.length && b.length) {
-        a1 = a.shift();
-        b1 = b.shift();
-        if (rd.test(a1) || rd.test(b1)) {
-          if (!rd.test(a1)) return 1;
-          if (!rd.test(b1)) return -1;
-          if (a1 != b1) return a1 - b1;
-        } else if (a1 != b1) return a1 > b1 ? 1 : -1;
-      }
-      return a.length - b.length;
-    });
+    return this.sort((a, b) => String(a).localeCompare(String(b), undefined, { numeric: true, sensitivity: "base" }));
   };
 
   let assets;
