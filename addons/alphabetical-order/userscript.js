c13fab498535cd6a45d426cb315d2c4873487281
Jazza-231 <greeny-231@outlook.com>
2024-03-03 19:23:45 +0800

    natural sort

--- addons/alphabetical-order/userscript.js
+++ addons/alphabetical-order/userscript.js
@@ -1,6 +1,29 @@
 export default async function ({ addon, msg, console }) {
   const vm = addon.tab.traps.vm;
 
+  Array.prototype.naturalSort = function () {
+    var a,
+      b,
+      a1,
+      b1,
+      rx = /(\d+)|(\D+)/g,
+      rd = /\d+/;
+    return this.sort(function (as, bs) {
+      a = String(as).toLowerCase().match(rx);
+      b = String(bs).toLowerCase().match(rx);
+      while (a.length && b.length) {
+        a1 = a.shift();
+        b1 = b.shift();
+        if (rd.test(a1) || rd.test(b1)) {
+          if (!rd.test(a1)) return 1;
+          if (!rd.test(b1)) return -1;
+          if (a1 != b1) return a1 - b1;
+        } else if (a1 != b1) return a1 > b1 ? 1 : -1;
+      }
+      return a.length - b.length;
+    });
+  };
+
   let assets;
   let sortedAssets;
   let isSorted;
@@ -19,7 +42,7 @@ export default async function ({ addon, msg, console }) {
     // Sort assets by name
     sortedAssets = assets
       .map((asset) => asset.name)
-      .sort()
+      .naturalSort()
       .map((sortedName) => assets.find((asset) => asset.name === sortedName));
 
     isSorted = JSON.stringify(assets) === JSON.stringify(sortedAssets);
