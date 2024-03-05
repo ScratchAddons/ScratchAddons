930ba0b444fba29a41aaa9666e631cba30898fe1
Jazza-231 <greeny-231@outlook.com>
2024-03-05 13:04:05 +0800

    censor

--- addons/hide-ads/userscript.js
+++ addons/hide-ads/userscript.js
@@ -6,6 +6,8 @@ export default async function ({ addon, console }) {
 
   url = window.location.href;
 
+  const advertisingContent = "[Marked as advertising]";
+
   /* 
   PROFILES
   top-level-reply
@@ -35,11 +37,45 @@ export default async function ({ addon, console }) {
   comment-content.lastChild.innerText (in replies there are two emoji-texts)
   */
 
+  let idToContent = {};
+
+  function handleComment(element) {
+    if (addon.settings.get("method") === "hide") {
+      element.style.display = "none";
+    } else {
+      let commentContent;
+
+      if (url.includes(settings["profiles"])) {
+        commentContent = element.querySelector(".info>.content");
+      } else {
+        commentContent = element.querySelector(".comment-content");
+      }
+
+      const key = element.querySelector("div").getAttribute("id");
+      const value = commentContent.innerText;
+
+      idToContent[key] = value;
+
+      commentContent.innerText = advertisingContent;
+      commentContent.classList.add("advertising");
+
+      commentContent.addEventListener("click", () => {
+        if (commentContent.innerText === advertisingContent) {
+          commentContent.innerText = idToContent[commentContent.closest(".comment").getAttribute("id")];
+          commentContent.classList.remove("advertising");
+        } else {
+          commentContent.innerText = advertisingContent;
+          commentContent.classList.add("advertising");
+        }
+      });
+    }
+  }
+
   if (url.includes("projects")) el = ".comment-container";
   else if (url.includes("studios")) el = ".comment-container";
   else if (url.includes("users")) el = ".top-level-reply";
 
-  //dsis = does string contain string
+  //dsis = does string include string
   function dsis(comment, string) {
     return typeof comment === "object"
       ? comment.innerHTML.toLowerCase().includes(string.toLowerCase())
@@ -51,37 +87,46 @@ export default async function ({ addon, console }) {
       if (url.includes(settings[key]) && addon.settings.get(key)) {
         while (true) {
           const commentsContainer = await addon.tab.waitForElement(el, { markAsSeen: true });
-          console.log("el found");
+          console.log("Checking main comments");
           matches.forEach((match) => {
             if (dsis(commentsContainer.innerHTML, match)) {
-              console.log("match found");
               //We have a match somewhere, now we need to figure out where
 
-              if (dsis(commentsContainer.querySelector("div.comment"), match)) {
-                //If this is true, the first comment is what contains the advertising, therefore we can hide them all
-                commentsContainer.style.display = "none";
-              } else {
-                //The first comment is not the offending comment, we search replies now
+              function handleReplies() {
                 if (url.includes(settings["profiles"])) {
                   //Projects and studios are handled below
                   const replies = commentsContainer.querySelectorAll("li.reply");
                   replies.forEach((reply) => {
                     if (dsis(reply, match)) {
-                      reply.style.display = "none";
+                      handleComment(reply);
                     }
                   });
                 }
+              }
+
+              if (dsis(commentsContainer.querySelector("div.comment"), match)) {
+                //If this is true, the first comment is what contains the advertising
+                handleComment(commentsContainer);
+
+                //If we are censoring, not hiding, then replies need to be checked too
+                if (addon.settings.get("method") === "censor") handleReplies();
+              } else {
+                //The first comment is not the offending comment, we search replies now
+                handleReplies();
               }
             }
           });
 
           //Project and studio replies are not loaded when the comment container is, which means we have to search for them separately
-          const commentReply = await addon.tab.waitForElement("div.replies>div.comment", { markAsSeen: true });
-          matches.forEach((match) => {
-            if (dsis(commentReply, match)) {
-              commentReply.style.display = "none";
-            }
-          });
+          // if (addon.settings.get("method") === "censor" && !url.includes(settings["profiles"])) {
+          //   const commentReply = await addon.tab.waitForElement("div.replies>div.comment", { markAsSeen: true });
+          //   console.log("Checing replies");
+          //   matches.forEach((match) => {
+          //     if (dsis(commentReply, match)) {
+          //       handleComment(commentReply);
+          //     }
+          //   });
+          // }
         }
       }
     }
