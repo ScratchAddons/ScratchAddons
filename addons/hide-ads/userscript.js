fb31b9de2aa792c16184fd34bcbf137d02773678
Jazza-231 <greeny-231@outlook.com>
2024-03-07 17:14:55 +0800

    various changes

--- addons/hide-ads/userscript.js
+++ addons/hide-ads/userscript.js
@@ -2,11 +2,12 @@ export default async function ({ addon, console }) {
   let el;
   let url;
   const settings = { projects: "projects", studios: "studios", profiles: "users" };
-  const matches = addon.settings.get("matchingStrings").map((obj) => obj.match.toLowerCase());
+  const matches = addon.settings.get("matches");
 
   url = window.location.href;
 
   const advertisingContent = "[Marked as advertising]";
+  const advertisingUser = "[User's messages blocked]";
 
   /* 
   PROFILES
@@ -39,10 +40,13 @@ export default async function ({ addon, console }) {
 
   let idToContent = {};
 
-  function handleComment(element) {
+  function handleComment(element, blockType) {
     if (addon.settings.get("method") === "hide") {
       element.style.display = "none";
     } else {
+      const comment = element.classList.contains("comment") ? element : element.querySelector(".comment");
+      comment.classList.add("contains-advertising");
+
       let commentContent;
 
       if (url.includes(settings["profiles"])) {
@@ -51,21 +55,24 @@ export default async function ({ addon, console }) {
         commentContent = element.querySelector(".comment-content");
       }
 
-      const key = element.querySelector("div").getAttribute("id");
-      const value = commentContent.innerText;
+      const key = comment.getAttribute("id");
+      const value = commentContent.innerHTML;
 
       idToContent[key] = value;
 
-      commentContent.innerText = advertisingContent;
+      commentContent.innerHTML = blockType === "content" ? advertisingContent : advertisingUser;
       commentContent.classList.add("advertising");
+      commentContent.style.cursor = "pointer";
 
       commentContent.addEventListener("click", () => {
-        if (commentContent.innerText === advertisingContent) {
-          commentContent.innerText = idToContent[commentContent.closest(".comment").getAttribute("id")];
+        if (commentContent.classList.contains("advertising")) {
+          commentContent.innerHTML = idToContent[commentContent.closest(".comment").getAttribute("id")];
           commentContent.classList.remove("advertising");
+          comment.classList.remove("contains-advertising");
         } else {
-          commentContent.innerText = advertisingContent;
+          commentContent.innerHTML = blockType === "content" ? advertisingContent : advertisingUser;
           commentContent.classList.add("advertising");
+          comment.classList.add("contains-advertising");
         }
       });
     }
@@ -82,50 +89,74 @@ export default async function ({ addon, console }) {
       : comment.toLowerCase().includes(string.toLowerCase());
   }
 
+  function handleMatch(element, match, typeOfBlock) {
+    function handleReplies(element, match, typeOfBlock) {
+      if (url.includes(settings["profiles"])) {
+        //Projects and studios are handled below
+        const replies = element.querySelectorAll("li.reply");
+        replies.forEach((reply) => {
+          if (dsis(reply, match)) {
+            handleComment(reply, typeOfBlock);
+          }
+        });
+      }
+    }
+
+    if (dsis(element.querySelector("div.comment"), match)) {
+      //If this is true, the first comment is what contains the advertising
+      handleComment(element, typeOfBlock);
+
+      // //If we are censoring, not hiding, then replies need to be checked too
+      // if (addon.settings.get("method") === "censor") handleReplies();
+    } else {
+      //The first comment is not the offending comment, we search replies now
+      handleReplies(element, match, typeOfBlock);
+    }
+  }
+
   for (const key in settings) {
     if (settings.hasOwnProperty.call(settings, key)) {
       if (url.includes(settings[key]) && addon.settings.get(key)) {
         while (true) {
           const commentsContainer = await addon.tab.waitForElement(el, { markAsSeen: true });
           matches.forEach((match) => {
-            if (dsis(commentsContainer.innerHTML, match)) {
-              //We have a match somewhere, now we need to figure out where
-
-              function handleReplies() {
-                if (url.includes(settings["profiles"])) {
-                  //Projects and studios are handled below
-                  const replies = commentsContainer.querySelectorAll("li.reply");
-                  replies.forEach((reply) => {
-                    if (dsis(reply, match)) {
-                      handleComment(reply);
-                    }
-                  });
-                }
+            if (match.matchType === "link" || match.matchType === "string") {
+              const commentContent = commentsContainer.querySelector(".comment-content");
+              if (dsis(commentContent.innerText, match.matchValue)) {
+                //We have a match somewhere, now we need to figure out where
+                handleMatch(commentsContainer, match.matchValue, "content");
               }
+            } else if (match.matchType === "user") {
+              const userName = commentsContainer.querySelector(".username");
 
-              if (dsis(commentsContainer.querySelector("div.comment"), match)) {
-                //If this is true, the first comment is what contains the advertising
-                handleComment(commentsContainer);
-
-                //If we are censoring, not hiding, then replies need to be checked too
-                if (addon.settings.get("method") === "censor") handleReplies();
-              } else {
-                //The first comment is not the offending comment, we search replies now
-                handleReplies();
+              if (dsis(userName.innerText, match.matchValue)) {
+                //We have a match to the username, now we hide or censor it
+                handleComment(commentsContainer, "user");
               }
             }
           });
 
           //Project and studio replies are not loaded when the comment container is, which means we have to search for them separately
-          if (!url.includes(settings["profiles"])) {
-            const commentReply = await addon.tab.waitForElement("div.replies>div.comment", { markAsSeen: true });
-            console.log(commentReply);
-            matches.forEach((match) => {
-              if (dsis(commentReply, match)) {
-                handleComment(commentReply);
-              }
-            });
+          async function replies() {
+            if (!url.includes(settings["profiles"])) {
+              const commentReply = await addon.tab.waitForElement("div.replies>div.comment", { markAsSeen: true });
+
+              matches.forEach((match) => {
+                if (match.matchType === "link" || match.matchType === "string") {
+                  if (dsis(commentReply, match.matchValue)) {
+                    handleComment(commentReply, "content");
+                  }
+                } else if (match.matchType === "user") {
+                  const userName = commentReply.querySelector(".username");
+                  if (dsis(userName.innerText, match.matchValue)) {
+                    handleComment(commentReply, "user");
+                  }
+                }
+              });
+            }
           }
+
+          // replies();
         }
       }
     }
