78ed1cb0068832e11cef6cd8ab72602cc53a5025
World_Languages <mundofinky@gmail.com>
2024-03-10 17:47:13 -0300

    Reorganize code (logic untouched)

--- addons/default-bitmap/userscript.js
+++ addons/default-bitmap/userscript.js
@@ -1,27 +1,24 @@
 export default async function ({ addon, console }) {
   const vm = addon.tab.traps.vm;
-  const _addCostume = vm.addCostume;
-  vm.addCostume = (md5, costume, ...args) => {
+
+  const EMPTY_BITMAP = {
+    md5: "8ac8569502a1fc1a33aad0ae5786f20b.png",
+    assetId: "8ac8569502a1fc1a33aad0ae5786f20b",
+    bitmapResolution: 2,
+    rotationCenterX: 1,
+    rotationCenterY: 1,
+    skinId: null,
+  };
+
+  const originalAddCostume = vm.addCostume;
+  vm.addCostume = function (md5, costume, ...args) {
     if (md5 === "cd21514d0531fdffb22204e0ec5ed84a.svg" && !addon.self.disabled) {
-      return _addCostume.call(
-        vm,
-        emptyBitmap.md5,
-        {
-          ...emptyBitmap,
-          name: costume.name,
-        },
-        ...args
-      );
+      const newCostumeArg = {
+        ...EMPTY_BITMAP,
+        name: costume.name,
+      };
+      return originalAddCostume.call(this, EMPTY_BITMAP.md5, newCostumeArg, ...args);
     }
-    return _addCostume.call(vm, md5, costume, ...args);
+    return originalAddCostume.call(this, md5, costume, ...args);
   };
 }
-
-const emptyBitmap = {
-  md5: "8ac8569502a1fc1a33aad0ae5786f20b.png",
-  assetId: "8ac8569502a1fc1a33aad0ae5786f20b",
-  bitmapResolution: 2,
-  rotationCenterX: 1,
-  rotationCenterY: 1,
-  skinId: null,
-};
