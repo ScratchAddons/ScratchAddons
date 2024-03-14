67884362f72271024b10ed7f6f41b87abff913e3
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-03-14 19:06:27 -0400

    Don't run in editor + minor cleanup

--- addons/player-thumb/userscript.js
+++ addons/player-thumb/userscript.js
@@ -8,9 +8,7 @@ export default async function ({ addon, console }) {
   thumb.id = "sa-project-thumb";
   thumb.classList.add("sa-project-thumb");
 
-  const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_2bejr box_box_2jjDp"]', {
-    markAsSeen: true,
-  });
+  const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]');
 
   if (addon.settings.get("loading")) {
     const LoaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
@@ -21,23 +19,16 @@ export default async function ({ addon, console }) {
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", (e) => {
     if (e.detail.action.type == "scratch-gui/project-changed/SET_PROJECT_CHANGED") {
-      moveThumb();
-      // TODO: Remove listener
+      // Move the thumbnail after the project loads
+      thumb.classList.remove("sa-project-thumb-loading");
+      const stage = document.querySelector('div[class*="stage_stage"]');
+      const greenFlagOverlay = stage.querySelector('[class*="stage_green-flag-overlay-wrapper_"]');
+      stage.insertBefore(thumb, greenFlagOverlay);
     }
     if (e.detail.action.type == "scratch-gui/vm-status/SET_STARTED_STATE") {
       document.getElementById("sa-project-thumb").remove();
     }
   });
-
-  function moveThumb() {
-    // Since the loading screen has a different size and layer
-    // just remove the thumbnail and re-add it in the correct place
-    thumb.classList.remove("sa-project-thumb-loading");
-    document.getElementById("sa-project-thumb").remove();
-    const stage = document.querySelector('div[class*="stage_stage"]');
-    const greenFlagOverlay = stage.querySelector('[class*="stage_green-flag-overlay-wrapper_"]');
-    stage.insertBefore(thumb, greenFlagOverlay);
-  }
 
   addon.tab.displayNoneWhileDisabled(thumb, {
     display: "inline",
