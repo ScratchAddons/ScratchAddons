2921475f7b608b407b355fcb0d3556d390df477a
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-03-24 15:58:05 +0100

    Add type annotation to new addons

--- addons/redirect-mobile-forums-v2/userscript.js
+++ addons/redirect-mobile-forums-v2/userscript.js
@@ -1,3 +1,4 @@
+/** @param {import("addonAPI").AddonAPI} */
 export default async function ({ addon, console, msg }) {
   if (window.location.pathname.startsWith("/discuss/m/"))
     window.location.replace(window.location.href.replace("m/", ""));
