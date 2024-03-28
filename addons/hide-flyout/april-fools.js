727aeb7ced0df71c1fb099c0ba309229ff991a99
World_Languages <mundofinky@gmail.com>
2024-03-28 18:41:24 -0300

    Small changes to April Fools detection

--- addons/hide-flyout/april-fools.js
+++ addons/hide-flyout/april-fools.js
@@ -1,11 +1,12 @@
 export const isScratchAprilFools23 = () => {
   let val = false;
-  if (!__scratchAddonsRedux.state) val = true;
+  const now = Date.now() / 1000;
+  if (!__scratchAddonsRedux.state && now < 1712059200 && now > 1711886400) val = true;
   else {
     val = Object.entries(__scratchAddonsRedux.state.session.session.flags).some(
       ([k, v]) => k.includes("normal") && v === true
     );
   }
-  if (val === true) document.body.classList.toggle("totally-normal", true); // Only add once
+  if (val === true && document.body) document.body.classList.toggle("totally-normal", true); // Only add once
   return val;
 };
