53e716cc114ca29014bf4415add7d1e76920ce77
World_Languages <mundofinky@gmail.com>
2024-03-28 18:41:10 -0300

    Remove styles without messing specificity

--- addons/columns/userscript.js
+++ addons/columns/userscript.js
@@ -1,7 +1,13 @@
 import { isScratchAprilFools23 } from "../hide-flyout/april-fools.js";
 
 export default async function ({ addon, msg, console }) {
-  if (isScratchAprilFools23()) return;
+  if (isScratchAprilFools23()) {
+    const removeStyles = () =>
+      document.querySelector(":root > head > link[rel=stylesheet][data-addon-id=columns]").remove();
+    removeStyles();
+    addon.self.addEventListener("reenabled", () => setTimeout(removeStyles, 500));
+    return;
+  }
 
   const Blockly = await addon.tab.traps.getBlockly();
 
