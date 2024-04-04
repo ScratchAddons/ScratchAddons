e8a47f2597a1648a2771bcdfbd52622b13dd9903
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-04-04 21:24:33 +0200

    Move slider creation to a separate function

--- addons/zoom-slider/userscript.js
+++ addons/zoom-slider/userscript.js
@@ -1,4 +1,58 @@
 export default async function ({ addon, msg }) {
+  const createSlider = ({ wrapperElement, maxScale, minScale, setScale, currentScale }) => {
+    const wrapperDiv = document.createElement("div");
+    wrapperDiv.classList.add("sa-zoom-slider-wrapper");
+    const slider = document.createElement("input");
+    slider.type = "range";
+    slider.min = minScale();
+    slider.max = maxScale();
+    slider.step = "0.01";
+    slider.value = currentScale();
+    slider.classList.add("sa-zoom-slider");
+    const count = document.createElement("input");
+    count.className = addon.tab.scratchClass("input_input-form", "input_input-small", {
+      others: "sa-zoom-slider-count",
+    });
+    count.value = percentScale(currentScale());
+    wrapperDiv.append(count, slider);
+    if (wrapperElement) wrapperElement.append(wrapperDiv);
+
+    function updateWorkspaceZoomLevel() {
+      setScale(parseFloat(slider.value));
+    }
+
+    slider.addEventListener("mousedown", (e) => {
+      e.stopPropagation();
+    });
+    slider.addEventListener("input", async () => {
+      count.value = percentScale(slider.value);
+      updateWorkspaceZoomLevel();
+    });
+    const changeListener = async () => {
+      const newScale = parseFloat(count.value / 100);
+      if (newScale > maxScale()) count.value = maxScale() * 100;
+      if (newScale < minScale()) count.value = minScale() * 100;
+      slider.value = count.value / 100;
+      updateWorkspaceZoomLevel();
+    };
+    count.addEventListener("change", changeListener);
+    count.addEventListener("blur", changeListener);
+
+    setInterval(() => {
+      slider.min = minScale();
+      slider.max = maxScale();
+      if (parseFloat(slider.value) !== currentScale()) {
+        slider.value = currentScale();
+        count.value = percentScale(slider.value);
+        updateWorkspaceZoomLevel();
+      }
+    }, 200);
+
+    addon.self.addEventListener("disabled", () => {
+      (wrapperElement ?? wrapperDiv).remove();
+    });
+  };
+
   const addSlider = async () => {
     if (addon.tab.editorMode !== "editor" || addon.self.disabled) {
       return;
@@ -12,58 +66,13 @@ export default async function ({ addon, msg }) {
     foreignObject.setAttribute("width", "120");
     foreignObject.setAttribute("x", "-126");
     foreignObject.setAttribute("y", "79");
-    const wrapperDiv = document.createElement("div");
-    wrapperDiv.classList.add("sa-zoom-slider-wrapper");
-    const slider = document.createElement("input");
-    slider.type = "range";
-    slider.min = blockly.mainWorkspace.options.zoomOptions.minScale;
-    slider.max = blockly.mainWorkspace.options.zoomOptions.maxScale;
-    slider.step = "0.01";
-    slider.value = blockly.mainWorkspace.scale;
-    slider.classList.add("sa-zoom-slider");
-    const count = document.createElement("input");
-    count.className = addon.tab.scratchClass("input_input-form", "input_input-small", {
-      others: "sa-zoom-slider-count",
-    });
-    count.value = percentScale(blockly.mainWorkspace.scale);
-    wrapperDiv.append(count, slider);
-    foreignObject.append(wrapperDiv);
     zoom.append(foreignObject);
-
-    function updateWorkspaceZoomLevel() {
-      blockly.mainWorkspace.setScale(parseFloat(slider.value));
-    }
-
-    slider.addEventListener("mousedown", (e) => {
-      e.stopPropagation();
-    });
-    slider.addEventListener("input", async () => {
-      count.value = percentScale(slider.value);
-      updateWorkspaceZoomLevel();
-    });
-    const changeListener = async () => {
-      const newScale = parseFloat(count.value / 100);
-      const { maxScale, minScale } = blockly.mainWorkspace.options.zoomOptions;
-      if (newScale > maxScale) count.value = maxScale * 100;
-      if (newScale < minScale) count.value = minScale * 100;
-      slider.value = count.value / 100;
-      updateWorkspaceZoomLevel();
-    };
-    count.addEventListener("change", changeListener);
-    count.addEventListener("blur", changeListener);
-
-    setInterval(() => {
-      slider.min = blockly.mainWorkspace.options.zoomOptions.minScale;
-      slider.max = blockly.mainWorkspace.options.zoomOptions.maxScale;
-      if (parseFloat(slider.value) !== blockly.mainWorkspace.scale) {
-        slider.value = blockly.mainWorkspace.scale;
-        count.value = percentScale(slider.value);
-        updateWorkspaceZoomLevel();
-      }
-    }, 200);
-
-    addon.self.addEventListener("disabled", () => {
-      foreignObject.remove();
+    createSlider({
+      wrapperElement: foreignObject,
+      maxScale: () => blockly.mainWorkspace.options.zoomOptions.maxScale,
+      minScale: () => blockly.mainWorkspace.options.zoomOptions.minScale,
+      setScale: (scale) => blockly.mainWorkspace.setScale(scale),
+      currentScale: () => blockly.mainWorkspace.scale,
     });
   };
 
