9be1d6b619dfc340a2a13c389aa9bedd2ccb6eeb
just-a-hriday <just.a.hriday@gmail.com>
2024-04-17 13:13:51 +0530

    add hidden link option

--- addons/clickable-quote-authors/userscript.js
+++ addons/clickable-quote-authors/userscript.js
@@ -3,11 +3,14 @@ export default async function ({ addon, console }) {
 
     for (const author of authors) {
         const authorName = author.textContent.match(/.+(?= wrote:)/);
-        
+
         const link = document.createElement("a");
         link.textContent = authorName;
         link.href = `https://scratch.mit.edu/users/${authorName}/`
-        
+
+        if (addon.settings.get("hidden"))
+            link.style.color = "inherit";
+
         author.textContent = " wrote:";
         author.prepend(link);
     };
