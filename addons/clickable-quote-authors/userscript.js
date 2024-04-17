276ee9b9d9960cb549a28d611b40be9b0dc548f7
just-a-hriday <just.a.hriday@gmail.com>
2024-04-17 12:42:58 +0530

    remove `innerHTML` usage

--- addons/clickable-quote-authors/userscript.js
+++ addons/clickable-quote-authors/userscript.js
@@ -2,8 +2,14 @@ export default async function ({ addon, console }) {
     const authors = document.getElementsByClassName("bb-quote-author");
     console.log(authors.length);
 
-    for (let author of authors) {
-        const authorName = author.innerText.match(/.+(?= wrote:)/);
-        author.innerHTML = `<a href = "https://scratch.mit.edu/users/${authorName}/">${authorName}</a> wrote:`;
+    for (const author of authors) {
+        const authorName = author.textContent.match(/.+(?= wrote:)/);
+        
+        const link = document.createElement("a");
+        link.textContent = authorName;
+        link.href = `https://scratch.mit.edu/users/${authorName}/`
+        
+        author.textContent = " wrote:";
+        author.prepend(link);
     };
 }
