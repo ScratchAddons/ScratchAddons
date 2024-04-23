2dad919b739b63cd10377534ae9dff328273566a
World_Languages <mundofinky@gmail.com>
2024-04-23 18:47:30 -0300

    Prettier lint

--- content-scripts/prototype-handler.js
+++ content-scripts/prototype-handler.js
@@ -14,27 +14,27 @@ function immediatelyRunFunctionInMainWorld(fn) {
 const isLocal = location.origin === "https://scratchfoundation.github.io" || location.port === "8601";
 if ((!(document.documentElement instanceof SVGElement) && location.pathname.split("/")[1] === "projects") || isLocal) {
   immediatelyRunFunctionInMainWorld(() => {
-  const oldBind = Function.prototype.bind;
-  // Use custom event target
-  window.__scratchAddonsTraps = new EventTarget();
-  const onceMap = (__scratchAddonsTraps._onceMap = Object.create(null));
+    const oldBind = Function.prototype.bind;
+    // Use custom event target
+    window.__scratchAddonsTraps = new EventTarget();
+    const onceMap = (__scratchAddonsTraps._onceMap = Object.create(null));
 
-  Function.prototype.bind = function (...args) {
-    if (Function.prototype.bind === oldBind) {
-      // Just in case some code stores the bind function once on startup, then always uses it.
-      return oldBind.apply(this, args);
-    } else if (
-      args[0] &&
-      Object.prototype.hasOwnProperty.call(args[0], "editingTarget") &&
-      Object.prototype.hasOwnProperty.call(args[0], "runtime")
-    ) {
-      onceMap.vm = args[0];
-      // After finding the VM, return to previous Function.prototype.bind
-      Function.prototype.bind = oldBind;
-      return oldBind.apply(this, args);
-    } else {
-      return oldBind.apply(this, args);
-    }
-  };
+    Function.prototype.bind = function (...args) {
+      if (Function.prototype.bind === oldBind) {
+        // Just in case some code stores the bind function once on startup, then always uses it.
+        return oldBind.apply(this, args);
+      } else if (
+        args[0] &&
+        Object.prototype.hasOwnProperty.call(args[0], "editingTarget") &&
+        Object.prototype.hasOwnProperty.call(args[0], "runtime")
+      ) {
+        onceMap.vm = args[0];
+        // After finding the VM, return to previous Function.prototype.bind
+        Function.prototype.bind = oldBind;
+        return oldBind.apply(this, args);
+      } else {
+        return oldBind.apply(this, args);
+      }
+    };
   });
 }
