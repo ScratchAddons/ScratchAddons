74c176f56b08afef5fa4273dbf13703a217187a4
World_Languages <mundofinky@gmail.com>
2024-04-28 20:54:25 -0300

    Only respond to settings page if ready

--- background/handle-settings-page.js
+++ background/handle-settings-page.js
@@ -2,28 +2,37 @@ import changeAddonState from "./imports/change-addon-state.js";
 import minifySettings from "../libraries/common/minify-settings.js";
 import { updateBadge } from "./message-cache.js";
 
+const onReady = (fn) => {
+  if (scratchAddons.localState.allReady) {
+    fn();
+    return undefined;
+  } else {
+    scratchAddons.localEvents.addEventListener("ready", () => fn(), { once: true });
+    // https://developer.chrome.com/docs/extensions/develop/concepts/messaging#simple
+    // "The sendResponse() callback is only valid if used synchronously, or if the event
+    // handler returns true to indicate that it will respond asynchronously"
+    return true;
+  }
+};
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   // Message used to load popups as well
   if (request === "getSettingsInfo") {
-    const sendRes = () =>
+    return onReady(() => {
       sendResponse({
         manifests: scratchAddons.manifests,
         // Firefox breaks if we send proxies
         addonsEnabled: scratchAddons.localState._target.addonsEnabled,
         addonSettings: scratchAddons.globalState._target.addonSettings,
       });
-    // Data might have not loaded yet, or be partial.
-    // Only respond when all data is ready
-    if (scratchAddons.localState.allReady) {
-      sendRes();
-    } else {
-      scratchAddons.localEvents.addEventListener("ready", sendRes);
-      return true;
-    }
+    });
   } else if (request.changeEnabledState) {
+    return onReady(() => {
     const { addonId, newState } = request.changeEnabledState;
     changeAddonState(addonId, newState);
+    });
   } else if (request.changeAddonSettings) {
+    return onReady(() => {
     const { addonId, newSettings } = request.changeAddonSettings;
     scratchAddons.globalState.addonSettings[addonId] = newSettings;
     const prerelease = chrome.runtime.getManifest().version_name.endsWith("-prerelease");
@@ -39,5 +48,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         new CustomEvent("updateUserstylesSettingsChange", { detail: { addonId, manifest, newSettings } })
       );
     if (addonId === "msg-count-badge") updateBadge(scratchAddons.cookieStoreId);
+    });
   }
 });
