eb6aac9d856b59980a1d046d50e04178c3c91ef4
just-a-hriday <just.a.hriday@gmail.com>
2024-04-30 07:33:18 +0530

    add dynamic disable

--- addons/clickable-quote-authors/userscript.js
+++ addons/clickable-quote-authors/userscript.js
@@ -1,16 +1,31 @@
 export default async function ({ addon, console }) {
   const authors = document.getElementsByClassName("bb-quote-author");
+  addLinks();
 
-  for (const author of authors) {
-    const authorName = author.textContent.match(/(?<=^)[\w-]{2,30}(?= wrote:$)/);
+  function addLinks() {
+    for (const author of authors) {
+      const authorName = author.textContent.match(/(?<=^)[\w-]{2,30}(?= wrote:$)/);
 
-    if (!authorName) continue;
+      if (!authorName) continue;
 
-    const link = document.createElement("a");
-    link.textContent = authorName;
-    link.href = `https://scratch.mit.edu/users/${authorName}/`;
+      const link = document.createElement("a");
+      link.textContent = authorName;
+      link.href = `https://scratch.mit.edu/users/${authorName}/`;
 
-    author.textContent = " wrote:";
-    author.prepend(link);
+      author.textContent = " wrote:";
+      author.prepend(link);
+    }
   }
+
+  addon.self.addEventListener("disabled", () => {
+    for (const author of authors) {
+      const authorLink = author.children[0];
+
+      if (!authorLink) continue;
+
+      author.textContent = `${authorLink.textContent} wrote:`;
+    }
+  });
+
+  addon.self.addEventListener("reenabled", addLinks);
 }
