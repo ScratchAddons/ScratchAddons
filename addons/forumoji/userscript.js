a19d546c7ae2fe325dc36fd2841570ac281a02d4
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-05-27 10:49:49 +0200

    Make addon work with updated forumoji

--- addons/forumoji/userscript.js
+++ addons/forumoji/userscript.js
@@ -1,37 +1,27 @@
 export default async function ({ addon, msg }) {
   const postButton = document.querySelector("#djangobbwrap [method=post] [type=submit]");
-  const forumoji = await fetch("https://raw.githubusercontent.com/lopste/forumoji/main/resources/forumoji.json").then(
-    (response) => response.json()
-  );
+  const forumoji = Object.keys(
+    await (await fetch("https://raw.githubusercontent.com/vercte/forumoji/main/assets/emoji.json")).json()
+  ).map((codepoint) => [
+    codepoint,
+    codepoint
+      .split("-")
+      .map((part) => String.fromCodePoint(parseInt(part, 16)))
+      .join(""),
+  ]);
   const editor = document.querySelector(".markItUpEditor");
-
-  const emojiList = {};
-  window.emojiList = emojiList;
-  forumoji.emoji.forEach((emoji) => {
-    emojiList[
-      emoji.codepoint
-        .replaceAll("U+", "")
-        .split(" ")
-        .map((codepoint) => String.fromCodePoint(parseInt(codepoint, 16)))
-        .join("")
-    ] = emoji.url;
-  });
 
   postButton.addEventListener("click", (e) => {
     if (addon.self.disabled) {
       return;
     }
-    Object.entries(emojiList).forEach(([emoji, image]) => {
-      editor.value = editor.value.replaceAll(emoji, `[img]${image}[/img]`);
+    forumoji.forEach(([codepoint, char]) => {
+      editor.value = editor.value.replaceAll(char, `[img https://u.cubeupload.com/fmji/${codepoint}.png]`);
     });
-    if (/\p{Extended_Pictographic}/u.test(editor.value)) {
-      if (confirm(msg("not-in-forumoji"))) {
-        editor.value = editor.value.replaceAll(/\p{Extended_Pictographic}/gu, "");
-      } else {
-        e.preventDefault();
-      }
-    } else {
-      e.preventDefault();
+    if (/\p{Extended_Pictographic}/u.test(editor.value) && confirm(msg("not-in-forumoji"))) {
+      editor.value = editor.value.replaceAll(/\p{Extended_Pictographic}/gu, "");
+      return;
     }
+    e.preventDefault();
   });
 }
