03e8dda658f905f1df72fb11eecc40ba7e1c3027
Da_Person <hueychen27@outlook.com>
2024-06-08 20:38:00 -0700

    Prettify

--- addons/hide-flyout/userscript.js
+++ addons/hide-flyout/userscript.js
@@ -85,8 +85,7 @@ export default async function ({ addon, console, msg }) {
     if (
       flyoutLock ||
       document.activeElement.className == "blocklyHtmlInput" ||
-      (Blockly.DropDownDiv.DIV_.style.display !== "none" &&
-        Blockly.DropDownDiv.DIV_.style.display !== "")
+      (Blockly.DropDownDiv.DIV_.style.display !== "none" && Blockly.DropDownDiv.DIV_.style.display !== "")
     )
       return;
     if (e && e.buttons) {
