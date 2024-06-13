9b1afadb0b9fc541d04ba51edf916656ac7f7e56
Jazza231 <greeny-231@outlook.com>
2024-06-13 19:20:53 +0800

    Restructure

--- addons/sprite-clone-count/userscript.js
+++ addons/sprite-clone-count/userscript.js
@@ -1,41 +1,47 @@
 export default async function ({ addon, console }) {
   const vm = addon.tab.traps.vm;
-  const OGfunction = vm.runtime.changeCloneCounter;
+  const ogCloneCounter = vm.runtime.changeCloneCounter;
+
+  function updateCounts() {
+    let counts = {};
+
+    vm.runtime.targets
+      .filter((target) => !target.isOriginal)
+      .map((target) => target.sprite.name)
+      .forEach((target) => {
+        if (!counts[target]) counts[target] = 1;
+        else counts[target] += 1;
+      });
+
+    const spriteNames = Array.from(
+      document
+        .querySelector("[class*=sprite-selector_items-wrapper]")
+        .querySelectorAll("[class*=sprite-selector-item_sprite-name]")
+    );
+
+    spriteNames.forEach((spriteName) => {
+      spriteName.querySelector(".clone-count")?.remove();
+      if (counts[spriteName.innerText.split("\n")[0]] !== undefined) {
+        const count = document.createElement("div");
+
+        count.classList.add("clone-count");
+        count.innerText = `(${counts[spriteName.innerText.split("\n")[0]]} clones)`;
+        spriteName.appendChild(count);
+      }
+    });
+  }
 
   vm.runtime.changeCloneCounter = (e) => {
-    OGfunction.call(vm.runtime, e);
+    ogCloneCounter.call(vm.runtime, e);
 
     setTimeout(() => {
-      let counts = {};
-
-      vm.runtime.targets
-        .filter((target) => !target.isOriginal)
-        .map((target) => target.sprite.name)
-        .forEach((target) => {
-          if (!counts[target]) counts[target] = 1;
-          else counts[target] += 1;
-        });
-
-      console.log(counts);
-
-      const spriteNames = Array.from(
-        document
-          .querySelector("[class*=sprite-selector_items-wrapper]")
-          .querySelectorAll("[class*=sprite-selector-item_sprite-name]")
-      );
-
-      console.log(spriteNames);
-
-      spriteNames.forEach((spriteName) => {
-        if (counts[spriteName.innerText.split("\n")[0]]) {
-          spriteName.querySelector("div")?.remove();
-
-          const count = document.createElement("div");
-
-          count.innerText = `(${counts[spriteName.innerText.split("\n")[0]]} clones)`;
-          spriteName.appendChild(count);
-        }
-      });
+      updateCounts();
     }, 100);
   };
+
+  vm.addListener("targetsUpdate", () => {
+    setTimeout(() => {
+      updateCounts();
+    }, 0);
+  });
 }
