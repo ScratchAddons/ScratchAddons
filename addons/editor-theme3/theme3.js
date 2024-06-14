5a6606594c669c530ac5d38a4cc22f329bce12d8
shock59 <42130427+shock59@users.noreply.github.com>
2024-06-14 18:25:04 +1000

    Revert "Made the toggle actually work again"
    This reverts commit a8afe79c8adb416decc079be1cecd700fdddf4a9.

--- addons/editor-theme3/theme3.js
+++ addons/editor-theme3/theme3.js
@@ -374,7 +374,7 @@ export default async function ({ addon, console, msg }) {
   const oldInsertionMarkerCreateMarkerBlock = Blockly.InsertionMarkerManager.prototype.createMarkerBlock_;
   Blockly.InsertionMarkerManager.prototype.createMarkerBlock_ = function (originalBlock) {
     const markerBlock = oldInsertionMarkerCreateMarkerBlock.call(this, originalBlock);
-    if (addon.settings.get("insertion-markers")) markerBlock.svgPath_.style.fill = originalBlock.getColour();
+    markerBlock.svgPath_.style.fill = originalBlock.getColour();
     return markerBlock;
   };
 
