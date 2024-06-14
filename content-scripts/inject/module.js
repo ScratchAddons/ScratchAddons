955056151aa63144e57bf9d215126565a1604749
Niko <150537842+OneShot-Niko@users.noreply.github.com>
2024-06-14 15:56:04 +0000

    maybe getting somewhere with changing to regexp
    this errors atm and doesn't work

--- content-scripts/inject/module.js
+++ content-scripts/inject/module.js
@@ -302,7 +302,7 @@ if (isScratchGui || isProject) {
   }
 }
 
-if (location.pathname.includes("topic") || location.pathname.includes("discuss/settings/")) {
+if (location.pathname.match((?:https?:\/\/scratch\.mit\.edu)?\/(topic|discuss\/settings)\/;)) {
   const checkUA = () => {
     if (!window.mySettings) return false;
     const ua = window.mySettings.markupSet.find((x) => x.className);
