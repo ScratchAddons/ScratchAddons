01853f43d19c319626c7c3bee198678b7a96cd68
Jazza231 <greeny-231@outlook.com>
2024-06-15 10:47:16 +0800

    Misc and non destructive handling of disabling

--- addons/workspace-dots/userscript.js
+++ addons/workspace-dots/userscript.js
@@ -9,22 +9,20 @@ export default async function ({ addon, console }) {
     let spacingAmount = addon.settings.get("spacing");
 
     blockly.getMainWorkspace().grid_.update = function (scale) {
-      console.log(themeSetting, spacingSetting, spacingAmount);
-
       this.scale_ = scale;
 
       let spacing = this.spacing_;
-      if (spacingSetting) spacing = spacingAmount;
+      if (spacingSetting & !addon.self.disabled) spacing = spacingAmount;
 
       // MSIE freaks if it sees a 0x0 pattern, so set empty patterns to 100x100.
-      var safeSpacing = spacing * scale || 100;
+      let safeSpacing = spacing * scale || 100;
 
       this.gridPattern_.setAttribute("width", safeSpacing);
       this.gridPattern_.setAttribute("height", safeSpacing);
 
-      var half = Math.floor(spacing / 2) + 0.5;
-      var start = half - this.length_ / 2;
-      var end = half + this.length_ / 2;
+      let half = Math.floor(spacing / 2) + 0.5;
+      let start = half - this.length_ / 2;
+      let end = half + this.length_ / 2;
 
       half *= scale;
       start *= scale;
@@ -33,31 +31,36 @@ export default async function ({ addon, console }) {
       let strokeWidthY;
       let strokeWidthX;
 
-      switch (themeSetting) {
-        case "dots":
-          strokeWidthY = scale;
-          strokeWidthX = scale;
-          break;
-        case "lines":
-          strokeWidthX = scale * (spacing + 1);
-          strokeWidthY = scale * (spacing + 1);
-          break;
-        case "crosshairs":
-          strokeWidthX = scale * 15;
-          strokeWidthY = scale * 15;
-          break;
-        case "none":
-          strokeWidthX = 0;
-          strokeWidthY = 0;
-          break;
-        case "vertical":
-          strokeWidthX = 0;
-          strokeWidthY = scale * (spacing + 1);
-          break;
-        case "horizontal":
-          strokeWidthX = scale * (spacing + 1);
-          strokeWidthY = 0;
-          break;
+      if (!addon.self.disabled) {
+        switch (themeSetting) {
+          case "dots":
+            strokeWidthY = scale;
+            strokeWidthX = scale;
+            break;
+          case "lines":
+            strokeWidthX = scale * (spacing + 1);
+            strokeWidthY = scale * (spacing + 1);
+            break;
+          case "crosshairs":
+            strokeWidthX = scale * 15;
+            strokeWidthY = scale * 15;
+            break;
+          case "none":
+            strokeWidthX = 0;
+            strokeWidthY = 0;
+            break;
+          case "vertical":
+            strokeWidthX = 0;
+            strokeWidthY = scale * (spacing + 1);
+            break;
+          case "horizontal":
+            strokeWidthX = scale * (spacing + 1);
+            strokeWidthY = 0;
+            break;
+        }
+      } else {
+        strokeWidthY = scale;
+        strokeWidthX = scale;
       }
 
       this.setLineAttributes_(this.line1_, strokeWidthY, start, end, half, half);
@@ -72,8 +75,7 @@ export default async function ({ addon, console }) {
   addon.settings.addEventListener("change", updateGrid);
 
   addon.self.addEventListener("disabled", () => {
-    blockly.getMainWorkspace().grid_.update = oldFunction;
-    blockly.getMainWorkspace().grid_.update(blockly.getMainWorkspace().grid_.scale_);
+    updateGrid.call(this);
   });
   addon.self.addEventListener("reenabled", () => {
     updateGrid.call(this);
