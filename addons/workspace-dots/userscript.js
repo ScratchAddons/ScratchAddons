9cb20642e497fe3fd130f113c9cc311ed887abd5
Jazza231 <greeny-231@outlook.com>
2024-06-19 00:47:57 +0800

    clobr help me

--- addons/workspace-dots/userscript.js
+++ addons/workspace-dots/userscript.js
@@ -1,84 +1,80 @@
 export default async function ({ addon, console }) {
   const blockly = await addon.tab.traps.getBlockly();
+  window.blockly = blockly;
 
-  const oldFunction = blockly.getMainWorkspace().grid_.update;
+  const workspace = blockly.getMainWorkspace();
 
-  function updateGrid() {
+  // https://github.com/scratchfoundation/scratch-blocks/blob/develop/core/grid.js#L136
+  workspace.grid_.update = function (scale) {
     let themeSetting = addon.settings.get("theme");
     let spacingSetting = addon.settings.get("useSpacing");
     let spacingAmount = addon.settings.get("spacing");
 
-    // https://github.com/scratchfoundation/scratch-blocks/blob/develop/core/grid.js#L136
-    blockly.getMainWorkspace().grid_.update = function (scale) {
-      this.scale_ = scale;
+    this.scale_ = scale;
 
-      let spacing = this.spacing_;
-      if (spacingSetting & !addon.self.disabled) spacing = spacingAmount;
+    let spacing = this.spacing_;
+    if (spacingSetting & !addon.self.disabled) spacing = spacingAmount;
 
-      // MSIE freaks if it sees a 0x0 pattern, so set empty patterns to 100x100.
-      let safeSpacing = spacing * scale || 100;
+    // MSIE freaks if it sees a 0x0 pattern, so set empty patterns to 100x100.
+    let safeSpacing = spacing * scale || 100;
 
-      this.gridPattern_.setAttribute("width", safeSpacing);
-      this.gridPattern_.setAttribute("height", safeSpacing);
+    this.gridPattern_.setAttribute("width", safeSpacing);
+    this.gridPattern_.setAttribute("height", safeSpacing);
 
-      let half = Math.floor(spacing / 2) + 0.5;
-      let start = half - this.length_ / 2;
-      let end = half + this.length_ / 2;
+    let half = Math.floor(spacing / 2) + 0.5;
+    let start = half - this.length_ / 2;
+    let end = half + this.length_ / 2;
 
-      half *= scale;
-      start *= scale;
-      end *= scale;
+    half *= scale;
+    start *= scale;
+    end *= scale;
 
-      let strokeWidthY;
-      let strokeWidthX;
+    let strokeWidthY;
+    let strokeWidthX;
 
-      if (!addon.self.disabled) {
-        switch (themeSetting) {
-          case "dots":
-            strokeWidthY = scale;
-            strokeWidthX = scale;
-            break;
-          case "lines":
-            strokeWidthX = scale * (spacing + 1);
-            strokeWidthY = scale * (spacing + 1);
-            break;
-          case "crosshairs":
-            strokeWidthX = scale * 15;
-            strokeWidthY = scale * 15;
-            break;
-          case "none":
-            strokeWidthX = 0;
-            strokeWidthY = 0;
-            break;
-          case "vertical":
-            strokeWidthX = 0;
-            strokeWidthY = scale * (spacing + 1);
-            break;
-          case "horizontal":
-            strokeWidthX = scale * (spacing + 1);
-            strokeWidthY = 0;
-            break;
-        }
-      } else {
-        strokeWidthY = scale;
-        strokeWidthX = scale;
+    if (!addon.self.disabled) {
+      switch (themeSetting) {
+        case "dots":
+          strokeWidthY = scale;
+          strokeWidthX = scale;
+          break;
+        case "lines":
+          strokeWidthX = scale * (spacing + 1);
+          strokeWidthY = scale * (spacing + 1);
+          break;
+        case "crosshairs":
+          strokeWidthX = scale * 15;
+          strokeWidthY = scale * 15;
+          break;
+        case "none":
+          strokeWidthX = 0;
+          strokeWidthY = 0;
+          break;
+        case "vertical":
+          strokeWidthX = 0;
+          strokeWidthY = scale * (spacing + 1);
+          break;
+        case "horizontal":
+          strokeWidthX = scale * (spacing + 1);
+          strokeWidthY = 0;
+          break;
       }
+    } else {
+      strokeWidthY = scale;
+      strokeWidthX = scale;
+    }
 
-      this.setLineAttributes_(this.line1_, strokeWidthY, start, end, half, half);
-      this.setLineAttributes_(this.line2_, strokeWidthX, half, half, start, end);
-    };
+    this.setLineAttributes_(this.line1_, strokeWidthY, start, end, half, half);
+    this.setLineAttributes_(this.line2_, strokeWidthX, half, half, start, end);
+  };
 
-    blockly.getMainWorkspace().grid_.update(blockly.getMainWorkspace().grid_.scale_);
+  function updateGrid() {
+    workspace.grid_.update(workspace.grid_.scale_);
   }
 
   updateGrid();
 
   addon.settings.addEventListener("change", updateGrid);
-
-  addon.self.addEventListener("disabled", () => {
-    updateGrid.call(this);
-  });
-  addon.self.addEventListener("reenabled", () => {
-    updateGrid.call(this);
-  });
+  addon.self.addEventListener("disabled", updateGrid);
+  addon.self.addEventListener("reenabled", updateGrid);
 }
