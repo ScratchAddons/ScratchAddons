5901ccd45138b42181f1a9601e3b2b5cda955239
TheColaber <thecolaber@gmail.com>
2024-06-24 16:01:02 -0700

    clean up

--- addons/zebra-striping/userscript.js
+++ addons/zebra-striping/userscript.js
@@ -1,7 +1,6 @@
 export default async function ({ addon, msg, console }) {
   await addon.tab.loadScript("/libraries/thirdparty/cs/tinycolor-min.js");
 
-  const vm = addon.tab.traps.vm;
   const ScratchBlocks = await addon.tab.traps.getBlockly();
   const originalRender = ScratchBlocks.BlockSvg.prototype.render;
   ScratchBlocks.BlockSvg.prototype.render = function (opt_bubble) {
@@ -9,6 +8,9 @@ export default async function ({ addon, msg, console }) {
     // The top block of the script is responsible for striping all of its children.
     // This way stripes are computed exactly once.
     if (!addon.self.disabled && !this.isInFlyout && !this.isShadow() && this.getParent() === null) {
+      const shade = addon.settings.get("shade");
+      const intensity = addon.settings.get("intensity");
+      const amount = ((shade === "lighter" ? 1 : -1) * intensity) / 2;
       // Conveniently getDescendants() returns blocks in an order such that each block's
       // parent will always come before that block (except the first block which has no
       // parent).
@@ -27,17 +29,14 @@ export default async function ({ addon, msg, console }) {
           parent.getCategory() === block.getCategory() &&
           // block has a substack (wrap blocks)
           (block.inputList.some((i) => i.name === "SUBSTACK") ||
-            // block has a output shape (reporter/boolean) and is same as parent's
-            (block.outputShape_ > 0 && block.outputShape_ === parent.outputShape_));
+            // block has a output connection (reporter/boolean) and is same shape as parent
+            (this.outputConnection && block.outputShape_ === parent.outputShape_));
         if (!block.striped && block.orginalColour_) {
           block.setColour(block.orginalColour_);
           block.orginalColour_ = null;
         } else if (block.striped && !block.orginalColour_) {
           const color = block.getColour();
           block.orginalColour_ = color;
-          const shade = addon.settings.get("shade");
-          const intensity = addon.settings.get("intensity");
-          const amount = ((shade === "lighter" ? 1 : -1) * intensity) / 2;
           block.setColour("#" + tinycolor(color).lighten(amount).toHex());
         }
       }
