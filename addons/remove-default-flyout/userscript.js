3abf06875720500b2ce4aaf3a624645ba34fd819
TheColaber <thecolaber@gmail.com>
2024-06-28 11:07:49 -0400

    still cant figure it out...

--- addons/remove-default-flyout/userscript.js
+++ addons/remove-default-flyout/userscript.js
@@ -2,6 +2,7 @@ export default async function ({ addon, msg, global, console }) {
   const ScratchBlocks = await addon.tab.traps.getBlockly();
   const oldSetValue = ScratchBlocks.Field.prototype.setValue;
   ScratchBlocks.Field.prototype.setValue = function (newValue) {
+    // console.trace();
     if (!addon.self.disabled && this instanceof ScratchBlocks.FieldTextInput && this.sourceBlock_?.isInFlyout) {
       newValue = "";
     }
@@ -15,6 +16,15 @@ export default async function ({ addon, msg, global, console }) {
   addon.self.addEventListener("reenabled", updateToolbox);
 
   function updateToolbox() {
-    // todo, idk how to do this :(
+    const workspace = addon.tab.traps.getWorkspace();
+    if (!workspace) return;
+    const toolbox = Blockly.getMainWorkspace().getToolbox();
+    if (!toolbox) return;
+    const flyout = toolbox.flyout_;
+    if (!flyout) return;
+    console.log("showing and hiding");
+    flyout.hide();
+    console.log(toolbox);
+    // toolbox.showAll_();
   }
 }
