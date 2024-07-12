440e602d7736c37c32707d77affb28b63a042215
World_Languages <mundofinky@gmail.com>
2024-07-12 17:49:42 -0300

    Undo changes in functions selectedBlocks & allBlocks

--- addons/blocks2image/userscript.js
+++ addons/blocks2image/userscript.js
@@ -236,34 +236,43 @@ export default async function ({ addon, console, msg }) {
     let yArr = [];
 
     const scale = isExportPNG ? 2 : 1;
+    if (dataShapes === "c-block c-1 hat") {
+      translateY = 20; // for My block
+    }
+    if (dataShapes === "hat") {
+      translateY = 16; // for Events
+      if (enabledAddons.includes("cat-blocks")) {
+        translateY += 16; // for cat ears
+      }
+    }
+    svgchild.setAttribute("transform", `translate(0,${scale * translateY}) scale(${scale})`);
+    setCSSVars(svg);
+    svg.append(makeStyle());
+    svg.append(svgchild);
+    return svg;
+  }
 
-    svgchild.childNodes.forEach((g, i) => {
+  function allBlocks(isExportPNG) {
+    let svg = exSVG.cloneNode();
+
+    let svgchild = document.querySelector("svg.blocklySvg g.blocklyBlockCanvas");
+    svgchild = svgchild.cloneNode(true);
+
+    let xArr = [];
+    let yArr = [];
+
+    svgchild.childNodes.forEach((g) => {
       let x = g.getAttribute("transform").match(/translate\((.*?),(.*?)\)/)[1] || 0;
       let y = g.getAttribute("transform").match(/translate\((.*?),(.*?)\)/)[2] || 0;
-
-      let dataShapes = g.getAttribute("data-shapes");
-
-      // Jazza here: do not ask me why these numbers work. I do not know.
-      if (dataShapes === "c-block c-1 hat") {
-        y -= 20;
-        if (enabledAddons.includes("cat-blocks")) {
-          y -= 11;
-        }
-      }
-      if (dataShapes === "hat") {
-        y -= 16;
-        if (enabledAddons.includes("cat-blocks")) {
-          y -= 15;
-        }
-      }
-
-      xArr.push(x * scale);
-      yArr.push(y * scale);
+      xArr.push(x * (isExportPNG ? 2 : 1));
+      yArr.push(y * (isExportPNG ? 2 : 1));
     });
 
     svgchild.setAttribute(
       "transform",
-      `translate(${-Math.min(...xArr) + scale},${-Math.min(...yArr) + scale}) scale(${scale})`
+      `translate(${-Math.min(...xArr)},${-Math.min(...yArr) + 18 * (isExportPNG ? 2 : 1)}) ${
+        isExportPNG ? "scale(2)" : ""
+      }`
     );
     setCSSVars(svg);
     svg.append(makeStyle());
