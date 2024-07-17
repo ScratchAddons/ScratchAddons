ae0bf1aeaa491bd3d92eb24692ad40fc4fffa78c
StickmanRed <StickmanRed@users.noreply.github.com>
2024-07-17 04:21:57 +0000

    Format code

--- addons/fix-uploaded-svgs/userscript.js
+++ addons/fix-uploaded-svgs/userscript.js
@@ -1,25 +1,25 @@
 export default async function ({ addon, console }) {
   function fixEditorSVG(element) {
     const svg = element.cloneNode(true);
-    
+
     // Fixes the previous issue of SVGs imported from Affinity Designer
     if (svg.height.baseVal.valueAsString === "100%" && svg.width.baseVal.valueAsString === "100%") {
       svg.removeAttribute("height");
       svg.removeAttribute("width");
     }
-    
+
     if (addon.settings.get("removeUnsupported")) {
       // "dominant-baseline" needs this iframe to shift accordingly
       const iframe = document.createElement("iframe");
       iframe.setAttribute("src", "about:blank");
       document.body.append(iframe);
       iframe.contentDocument.body.appendChild(svg);
-      
+
       for (const textElement of svg.getElementsByTagName("text")) {
         // "x" and "y" aren't accounted for by Scratch
         let x = Number.parseFloat(textElement.getAttribute("x") ?? "0");
         let y = Number.parseFloat(textElement.getAttribute("y") ?? "0");
-        
+
         // The lack of "tspan" creates a vertical shift
         const innerTextElement = textElement.firstChild;
         if (innerTextElement.tagName === undefined) {
@@ -27,7 +27,7 @@ export default async function ({ addon, console }) {
           innerTextSpan.appendChild(innerTextElement);
           textElement.appendChild(innerTextSpan);
         }
-        
+
         // "dominant-baseline" adjustment
         const baselineAdjust = textElement.cloneNode(true);
         textElement.after(baselineAdjust);
@@ -35,14 +35,14 @@ export default async function ({ addon, console }) {
         const difference = textElement.getBoundingClientRect().top - baselineAdjust.getBoundingClientRect().top;
         textElement.style.dominantBaseline = "auto";
         baselineAdjust.remove();
-        
+
         let transform = textElement.getAttribute("transform") ?? "";
         transform += ` translate(${x}, ${y + difference})`;
         textElement.setAttribute("transform", transform);
         textElement.setAttribute("x", "0");
         textElement.setAttribute("y", "0");
       }
-      
+
       // "use" tags are ignored by Scratch
       for (const useElement of svg.querySelectorAll("use")) {
         const referElement = svg.querySelector(useElement.getAttribute("href")).cloneNode(true);
@@ -53,12 +53,12 @@ export default async function ({ addon, console }) {
         useElement.replaceWith(referElement);
         useElement.remove();
       }
-      
+
       iframe.remove();
     }
     return svg;
   }
-  
+
   const originalFileReader = window.FileReader;
   window.FileReader = function () {
     const realFileReader = new originalFileReader();
