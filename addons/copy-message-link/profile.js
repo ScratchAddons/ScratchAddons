e1843613d17117227ef39c1e4ead42e34ef7a81e
rgantzos <86856959+rgantzos@users.noreply.github.com>
2024-07-17 16:01:23 -0700

    Use markAsSeen and don't reuse comments

--- addons/copy-message-link/profile.js
+++ addons/copy-message-link/profile.js
@@ -2,19 +2,11 @@ export default async function ({ addon, console, msg }) {
   let amtOfComments = 0;
   let pass = 0;
   while (true) {
-    const newAmtOfComments = document.querySelectorAll("div.comment").length;
-    if (amtOfComments !== newAmtOfComments) {
-      pass++;
-      amtOfComments = newAmtOfComments;
-    }
-    const comment = await addon.tab.waitForElement(`div.comment:not([data-sa-copy-link-pass='${pass}'])`);
-    comment.dataset.saCopyLinkPass = pass;
-    if (comment.querySelector(".sa-copy-link-btn")) {
-      // This will to all comments after posting a new one,
-      // and to the first comment on every loaded page.
-      // Do not readd button if we already added it
-      continue;
-    }
+    const comment = await addon.tab.waitForElement("div.comment:not(.sa-copy-link)", {
+      markAsSeen: true,
+    })
+    comment.classList.add("sa-copy-link")
+
     const newElem = document.createElement("span");
     addon.tab.displayNoneWhileDisabled(newElem);
     newElem.className = "actions report sa-copy-link-btn";
