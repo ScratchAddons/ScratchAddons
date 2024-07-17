1f24586cd21235965b6c001f16fc4c620411e50e
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-07-16 21:15:36 -0700

    Update userscript.js

--- addons/fix-uploaded-svgs/userscript.js
+++ addons/fix-uploaded-svgs/userscript.js
@@ -9,51 +9,53 @@ export default async function ({ addon, console }) {
       svg.removeAttribute("width");
     }
 
-    // This iframe is needed to correct "dominant-baseline"
-    const iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "about:blank");
-    document.body.append(iframe);
-    iframe.contentDocument.body.appendChild(svg);
+    if (addon.settings.get("removeUnsupported")) {
+      // This iframe is needed to correct "dominant-baseline"
+      const iframe = document.createElement("iframe");
+      iframe.setAttribute("src", "about:blank");
+      document.body.append(iframe);
+      iframe.contentDocument.body.appendChild(svg);
 
-    for (const textElement of svg.getElementsByTagName("text")) {
-      let x = Number.parseFloat(textElement.getAttribute("x") ?? "0");
-      let y = Number.parseFloat(textElement.getAttribute("y") ?? "0");
+      for (const textElement of svg.getElementsByTagName("text")) {
+        let x = Number.parseFloat(textElement.getAttribute("x") ?? "0");
+        let y = Number.parseFloat(textElement.getAttribute("y") ?? "0");
 
-      // Appends "tspan" instead of Scratch
-      const innerTextElement = textElement.firstChild;
-      if (innerTextElement.tagName === undefined) {
-        const innerTextSpan = document.createElementNS("http://www.w3.org/2000/svg", "tspan");
-        innerTextSpan.appendChild(innerTextElement);
-        textElement.appendChild(innerTextSpan);
+        // Appends "tspan" instead of Scratch
+        const innerTextElement = textElement.firstChild;
+        if (innerTextElement.tagName === undefined) {
+          const innerTextSpan = document.createElementNS("http://www.w3.org/2000/svg", "tspan");
+          innerTextSpan.appendChild(innerTextElement);
+          textElement.appendChild(innerTextSpan);
+        }
+
+        // Accounts for "dominant-baseline" removal
+        const baselineAdjust = textElement.cloneNode(true);
+        textElement.after(baselineAdjust);
+        baselineAdjust.style.dominantBaseline = "auto";
+        const difference = textElement.getBoundingClientRect().top - baselineAdjust.getBoundingClientRect().top;
+        textElement.style.dominantBaseline = "auto";
+        baselineAdjust.remove();
+
+        let transform = textElement.getAttribute("transform") ?? "";
+        transform += ` translate(${x}, ${y + difference})`;
+        textElement.setAttribute("transform", transform);
+        textElement.setAttribute("x", "0");
+        textElement.setAttribute("y", "0");
       }
 
-      // Accounts for "dominant-baseline" removal
-      const baselineAdjust = textElement.cloneNode(true);
-      textElement.after(baselineAdjust);
-      baselineAdjust.style.dominantBaseline = "auto";
-      const difference = textElement.getBoundingClientRect().top - baselineAdjust.getBoundingClientRect().top;
-      textElement.style.dominantBaseline = "auto";
-      baselineAdjust.remove();
-
-      let transform = textElement.getAttribute("transform") ?? "";
-      transform += ` translate(${x}, ${y + difference})`;
-      textElement.setAttribute("transform", transform);
-      textElement.setAttribute("x", "0");
-      textElement.setAttribute("y", "0");
-    }
-
-    // Fixes scratchblocks images removal
-    for (const useElement of svg.querySelectorAll("use")) {
-      const referElement = svg.querySelector(useElement.getAttribute("href")).cloneNode(true);
-      useElement.removeAttribute("href");
-      for (let useAttribute of useElement.attributes) {
-        referElement.setAttribute(useAttribute.name, useAttribute.value);
+      // Fixes scratchblocks images removal
+      for (const useElement of svg.querySelectorAll("use")) {
+        const referElement = svg.querySelector(useElement.getAttribute("href")).cloneNode(true);
+        useElement.removeAttribute("href");
+        for (let useAttribute of useElement.attributes) {
+          referElement.setAttribute(useAttribute.name, useAttribute.value);
+        }
+        useElement.replaceWith(referElement);
+        useElement.remove();
       }
-      useElement.replaceWith(referElement);
-      useElement.remove();
-    }
 
-    iframe.remove();
+      iframe.remove();
+    }
     return svg;
   }
 
