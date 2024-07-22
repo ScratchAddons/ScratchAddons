e7520680daa909c448ea010cbe938319045c8efe
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-07-22 12:56:55 -0400

    Add BBCode and signature support

--- addons/linkify-mentions/script.js
+++ addons/linkify-mentions/script.js
@@ -2,21 +2,7 @@ import { pingifyTextNode } from "../../libraries/common/cs/fast-linkify.js";
 
 export default async function ({ addon, console }) {
   const linkified = [];
-  addon.self.addEventListener("disabled", () => unLinkifyMentions());
-  addon.self.addEventListener("reenabled", () => linkifyMentions());
-  addon.settings.addEventListener("change", () => {
-    addon.settings.get("mentions") ? linkifyMentions() : unLinkifyMentions();
-  });
-  function linkifyMentions() {
-    for (const { element } of linkified) {
-      pingifyTextNode(element);
-    }
-  }
-  function unLinkifyMentions() {
-    for (const { element, original } of linkified) {
-      element.innerHTML = original;
-    }
-  }
+
   function pingify(element) {
     linkified.push({
       element,
@@ -24,8 +10,24 @@ export default async function ({ addon, console }) {
     });
     return pingifyTextNode(element);
   }
-  while (true) {
-    let post = await addon.tab.waitForElement(".post_body_html", { markAsSeen: true });
-    if (!addon.self.disabled) pingify(post);
+
+  function linkifyAll() {
+    document.querySelectorAll(".post_body_html, .postsignature").forEach(post => {
+      pingify(post);
+      post.querySelectorAll("span, li, blockquote").forEach(el => pingifyTextNode(el));
+    });
+    console.log(linkified)
   }
+
+  addon.self.addEventListener("disabled", () => {
+    for (const { element, original } of linkified) {
+      element.innerHTML = original;
+    }
+  });
+
+  addon.self.addEventListener("reenabled", () => {
+    linkifyAll();
+  });
+
+  linkifyAll();
 }
