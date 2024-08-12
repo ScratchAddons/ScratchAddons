691bd62006f38592de3c77e4ea81c077437c9f78
Samq64 <Samq64@users.noreply.github.com>
2024-08-12 19:15:01 +0000

    Format code

--- addons/full-signature/disable.js
+++ addons/full-signature/disable.js
@@ -1,8 +1,12 @@
 export default async function ({ addon, console }) {
   addon.self.addEventListener("disabled", () => {
-    document.querySelectorAll(".activity-stream, .activity-ul, .postsignature, .project-title.no-edit, .scratchblocks, .blocks3").forEach((el) => {
-      el.scrollTop = 0;
-      el.scrollLeft = 0;
-    });
+    document
+      .querySelectorAll(
+        ".activity-stream, .activity-ul, .postsignature, .project-title.no-edit, .scratchblocks, .blocks3"
+      )
+      .forEach((el) => {
+        el.scrollTop = 0;
+        el.scrollLeft = 0;
+      });
   });
 }
