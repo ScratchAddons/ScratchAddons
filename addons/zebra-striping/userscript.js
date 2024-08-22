964f01eeb022c030fcd995f42d30d6b9ae3c2b61
TheColaber <thecolaber@gmail.com>
2024-08-22 17:14:16 -0400

    fix shadow block dropdowns, fix var name, push old code improvements

--- addons/zebra-striping/userscript.js
+++ addons/zebra-striping/userscript.js
@@ -6,73 +6,70 @@ export default async function ({ addon, msg, console }) {
   const ScratchBlocks = await addon.tab.traps.getBlockly();
   const originalRender = ScratchBlocks.BlockSvg.prototype.render;
   ScratchBlocks.BlockSvg.prototype.render = function (opt_bubble) {
+    const shade = addon.settings.get("shade");
+    const intensity = addon.settings.get("intensity");
+    const amount = ((shade === "lighter" ? 1 : -1) * intensity) / 2;
+
     // Any changes that affect block striping should bubble to the top block of the script.
     // The top block of the script is responsible for striping all of its children.
     // This way stripes are computed exactly once.
     if (
+      // addon is enabled
       !addon.self.disabled &&
+      // not in the flyout
       !this.isInFlyout &&
+      // not a shadow
       !this.isShadow() &&
+      // not an insertion marker
       !this.isInsertionMarker() &&
+      // does not have parent
       this.getParent() === null
     ) {
-      const shade = addon.settings.get("shade");
-      const intensity = addon.settings.get("intensity");
-      const amount = ((shade === "lighter" ? 1 : -1) * intensity) / 2;
-      // Conveniently getDescendants() returns blocks in an order such that each block's
-      // parent will always come before that block (except the first block which has no
-      // parent).
+      // getDescendants() returns blocks in an order such that each block's parent
+      // will always come before that block (except the first block which has no parent).
       for (const block of this.getDescendants()) {
         const parent = block.getSurroundParent();
-        let wasStripped = !!block.sa_striped;
-        // Ensure the shadow block has the correct stroke by updating it's color to match parent colors.
-        if (block.isShadow() && !parent.sa_striped) {
-          block.updateColour();
-          continue;
-        }
-        block.sa_striped =
-          // not a shadow
-          !block.isShadow() &&
+
+        const shouldStripe =
           // not a insertion marker
           !block.isInsertionMarker() &&
           // has a parent
           parent &&
-          // parent is not striped
-          !parent.sa_striped &&
-          // parent and child are same color
+          // shadow blocks should be striped if it's parent is striped. otherwise, it should be opposite of parent.
+          (block.isShadow() ? parent.saStriped : !parent.saStriped) &&
+          // parent and child are same color (even shadows match block color)
           // we dont check category because other addons can make blocks the same color.
-          // if the block was stripped we need to look at it's original color in order to get it's real color
-          parent.getColour() === (block.sa_orginalColour ? block.sa_orginalColour[0] : block.getColour());
+          // if the block was stripped we need to look at it's original color in order to get it's real color.
+          parent.getColour() === (block.saOrginalColour ? block.saOrginalColour[0] : block.getColour());
 
-        if (!block.sa_striped && block.sa_orginalColour) {
-          block.setColour(...block.sa_orginalColour);
-          block.sa_orginalColour = null;
-        } else if (block.sa_striped && !block.sa_orginalColour) {
+        // if the block's stripe state is correct, no need to updaye its state.
+        if (shouldStripe === block.saStriped) continue;
+
+        if (!shouldStripe && block.saOrginalColour) {
+          block.setColour(...block.saOrginalColour);
+          block.saOrginalColour = null;
+        } else if (shouldStripe && !block.saOrginalColour) {
           const color = block.getColour();
           const secondary = block.getColourSecondary();
           const tertiary = block.getColourTertiary();
           const quaternary = block.getColourQuaternary();
+          block.saOrginalColour = [color, secondary, tertiary, quaternary];
 
-          block.sa_orginalColour = [color, secondary, tertiary, quaternary];
-          block.setColour(
-            "#" + tinycolor(color).lighten(amount).toHex(),
-            "#" + tinycolor(secondary).lighten(amount).toHex(),
-            "#" + tinycolor(tertiary).lighten(amount).toHex(),
-            "#" + tinycolor(quaternary).lighten(amount).toHex()
-          );
+          const stripedColors = block.saOrginalColour.map((c) => "#" + tinycolor(c).lighten(amount).toHex());
+          block.setColour(...stripedColors);
         }
 
-        if (wasStripped !== block.sa_striped) {
-          // Update field dropdowns to match parent color
-          for (const input of block.inputList) {
-            for (const field of input.fieldRow) {
-              if (field instanceof ScratchBlocks.FieldDropdown) {
-                field.box_.setAttribute("fill", field.sourceBlock_.getColour());
-                field.box_.setAttribute("stroke", field.sourceBlock_.getColourTertiary());
-              }
+        // field strokes don't automatically update when setColour is called.
+        // so manually update field dropdowns to match parent color
+        for (const input of block.inputList) {
+          for (const field of input.fieldRow) {
+            if (field instanceof ScratchBlocks.FieldDropdown) {
+              field.box_.setAttribute("fill", field.sourceBlock_.getColour());
+              field.box_.setAttribute("stroke", field.sourceBlock_.getColourTertiary());
             }
           }
         }
+        block.saStriped = shouldStripe;
       }
     }
     return originalRender.call(this, opt_bubble);
