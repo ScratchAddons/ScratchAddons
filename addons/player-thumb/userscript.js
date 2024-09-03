86735fcae7440c1e1fb4151f08272169e51751b4
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-09-03 10:34:43 -0400

    editor and fullscreen compat

--- addons/player-thumb/userscript.js
+++ addons/player-thumb/userscript.js
@@ -1,6 +1,4 @@
 export default async function ({ addon, console }) {
-  if (addon.tab.editorMode === "editor") return;
-
   const projectId = window.location.pathname.split("/")[2];
   const thumbUrl = `https://uploads.scratch.mit.edu/get_image/project/${projectId}_480x360.png`;
   const thumb = document.createElement("img");
@@ -10,22 +8,28 @@ export default async function ({ addon, console }) {
   const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]');
   const alerts = document.querySelector(".project-info-alerts");
 
-  if (addon.tab.editorMode === "projectpage") {
-    const LoaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
-    thumb.classList.add("loading");
-    stageWrapper.insertBefore(thumb, LoaderBackground);
-    alerts.style.display = "none";
+  const loaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
+  if (addon.tab.editorMode === "fullscreen") {
+    document.body.classList.add("sa-body-fullscreen");
   }
+
+  thumb.classList.add("loading");
+  stageWrapper.insertBefore(thumb, loaderBackground);
+  if (alerts) alerts.style.display = "none";
 
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", (e) => {
     if (e.detail.action.type == "scratch-gui/project-changed/SET_PROJECT_CHANGED") {
-      // Move the thumbnail after the project loads
-      thumb.classList.remove("loading");
-      const stage = document.querySelector('div[class*="stage_stage"]');
-      const greenFlagOverlay = stage.querySelector('[class*="stage_green-flag-overlay-wrapper_"]');
-      stage.insertBefore(thumb, greenFlagOverlay);
-      alerts.style.display = "flex";
+      if (addon.tab.editorMode !== "editor") {
+        // Move the thumbnail after the project loads
+        thumb.classList.remove("loading");
+        const stage = document.querySelector('div[class*="stage_stage"]');
+        const greenFlagOverlay = stage.querySelector('[class*="stage_green-flag-overlay-wrapper_"]');
+        stage.insertBefore(thumb, greenFlagOverlay);
+        if (alerts) alerts.style.display = "flex";
+      } else if (addon.tab.redux.state.scratchGui.projectState.loadingState === "SHOWING_WITH_ID") {
+        thumb.remove();
+      }
     }
     if (e.detail.action.type == "scratch-gui/vm-status/SET_STARTED_STATE") {
       thumb.remove();
@@ -33,4 +37,12 @@ export default async function ({ addon, console }) {
   });
 
   addon.tab.displayNoneWhileDisabled(thumb);
+
+  addon.tab.addEventListener("urlChange", (e) => {
+    if (addon.tab.editorMode === "fullscreen") {
+      document.body.classList.add("sa-body-fullscreen");
+    } else {
+      document.body.classList.remove("sa-body-fullscreen");
+    }
+  });
 }
