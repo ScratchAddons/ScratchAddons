4f1cfa461c844b79e790d1a18d93a8e57ffc345e
Masaabu <Masa2525ym@gmail.com>
2024-09-08 22:11:18 +0900

    svg-path-editor

---
+++ addons/svg-path-editor/userscript.js
@@ -0,0 +1,103 @@
+export default async function ({ addon, console }) {
+  let button;
+  let lastPaintMode;
+
+  await addon.tab.waitForElement("div.mode-tools_mode-tools_kuBCO", {  markAsSeen: true });
+  addon.tab.redux.initialize()
+  addon.tab.redux.addEventListener("statechanged", async function() {
+    button = document.querySelector(".sa-svg-path-editor-button")
+    const paintMode = addon.tab.redux.state.scratchPaint.mode;
+    if (lastPaintMode !== paintMode) { if (button) { button.remove() } };
+    lastPaintMode = paintMode;
+    if (
+      paintMode !== "SELECT" &&
+      paintMode !== "RESHAPE"
+    ) return;
+    if (!button) {
+      button = document.createElement("span")
+      button.className = "button_button_LhMbA labeled-icon-button_mod-edit-field_wieqn sa-svg-path-editor-button";
+      button.role = "button";
+
+      const img = document.createElement("img");
+      img.className = "labeled-icon-button_edit-field-icon_YEtkK";
+      img.src = addon.self.dir + "/image.svg";
+      img.alt = "Edit Path";
+      img.title = "Edit Path";
+
+      button.addEventListener("click", async function () {
+        if (button.classList.contains("button_mod-disabled_HvX8y")) return;
+        const { content, closeButton, remove } = addon.tab.createModal("Edit Path", {
+          isOpen: true
+        })
+        try {
+          const pathData = addon.tab.redux.state.scratchPaint.selectedItems[0].pathData;
+
+          const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+          svg.classList.add("sa-svg-path-editor-svg");
+          svg.setAttribute("height", "200px");
+
+          const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+          path.setAttribute("stroke", "#009dec");
+          path.setAttribute("stroke-width", "3");
+          path.setAttribute("fill", "none");
+
+          svg.appendChild(path)
+          content.appendChild(svg)
+
+          async function setPath(d) {
+            path.setAttribute("d", d)
+            const bbox = path.getBBox();
+            svg.setAttribute("viewBox", `${bbox.x-3} ${bbox.y-3} ${bbox.width+3} ${bbox.height+3}`);
+          }
+
+          const input = document.createElement("input");
+          input.className = "input_input-form_w8QAP sa-svg-path-editor-input";
+          input.value = pathData;
+          input.addEventListener("input", () => {
+            setPath(input.value)
+          });
+          content.appendChild(input);
+
+          setPath(input.value)
+
+          const footer = document.createElement("div");
+          footer.className = "action-buttons";
+
+          const cancelButton = document.createElement("button");
+          cancelButton.className = "button action-button close-button white";
+          cancelButton.textContent = "cancel";
+          cancelButton.addEventListener("click", () => {remove()});
+          footer.appendChild(cancelButton);
+
+          const saveButton = document.createElement("button");
+          saveButton.className = "button action-button submit-button";
+          saveButton.textContent = "save";
+          saveButton.addEventListener("click", () => {
+            addon.tab.redux.state.scratchPaint.selectedItems[0].pathData = input.value;
+            remove();
+          });
+          footer.appendChild(saveButton);
+
+          content.appendChild(footer)
+
+          closeButton.addEventListener("click", () => {
+            remove();
+          });
+        } catch (e) {
+          remove()
+        }
+      });
+      if (addon.tab.redux.state.scratchPaint.selectedItems.length !== 1) button.classList.add("button_mod-disabled_HvX8y")
+      else if (!addon.tab.redux.state.scratchPaint.selectedItems[0].pathData) button.classList.add("button_mod-disabled_HvX8y")
+      button.appendChild(img);
+
+      await addon.tab.waitForElement("div.mode-tools_mode-tools_kuBCO")
+      const toolbar = document.querySelector("div.mode-tools_mode-tools_kuBCO");
+      toolbar.appendChild(button);
+    } else {
+      if (addon.tab.redux.state.scratchPaint.selectedItems.length !== 1) button.classList.add("button_mod-disabled_HvX8y")
+      else if (!addon.tab.redux.state.scratchPaint.selectedItems[0].pathData) button.classList.add("button_mod-disabled_HvX8y")
+      else button.classList.remove("button_mod-disabled_HvX8y")
+    }
+  })
+}
