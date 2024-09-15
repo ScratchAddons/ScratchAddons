f69ff51fabc31ce98d3883d267a6732783d78972
Maximouse <51849865+mxmou@users.noreply.github.com>
2024-09-15 22:48:34 +0300

    Fix text size

--- addons/blocks2image/userscript.js
+++ addons/blocks2image/userscript.js
@@ -29,7 +29,7 @@ export default async function ({ addon, console, msg }) {
 
   function setCSSVars(element) {
     for (let property of document.documentElement.style) {
-      if (property.startsWith("--editorTheme3-"))
+      if (property.startsWith("--editorTheme3-") || property.startsWith("--customBlockText-"))
         element.style.setProperty(property, document.documentElement.style.getPropertyValue(property));
     }
   }
