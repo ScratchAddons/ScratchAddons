c365824013b1713191d7c351c9fe36e68a7364d3
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-10-05 09:51:14 -0400

    Fix thumbnail disappearing when exiting editor

--- addons/player-thumb/userscript.js
+++ addons/player-thumb/userscript.js
@@ -1,23 +1,17 @@
 export default async function ({ addon, console }) {
-  if (addon.tab.editorMode === "editor") return;
+
+  let controls, alerts;
 
   const projectId = location.pathname.split("/")[2];
+  if (typeof(projectId) !== Number) return;
+
   const thumb = document.createElement("img");
   thumb.src = `https://uploads.scratch.mit.edu/get_image/project/${projectId}_480x360.png`;
   thumb.className = "sa-project-thumb";
 
-  const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]');
-  const alerts = document.querySelector(".project-info-alerts");
-  const controls = stageWrapper.querySelector('div[class^="controls_controls-container_"]');
-  controls.classList.add("sa-controls-disabled");
 
-  if (addon.tab.editorMode === "projectpage") {
-    const LoaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
-    thumb.classList.add("loading");
-    stageWrapper.insertBefore(thumb, LoaderBackground);
-    alerts.style.display = "none";
-  }
-
+  // TODO: Wait for Redux properly
+  await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]');
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", (e) => {
     if (e.detail.action.type == "scratch-gui/project-changed/SET_PROJECT_CHANGED") {
@@ -26,9 +20,36 @@ export default async function ({ addon, console }) {
       controls.classList.remove("sa-controls-disabled");
       const stage = document.querySelector('div[class*="stage_stage"]');
       const greenFlagOverlay = stage.querySelector('[class*="stage_green-flag-overlay-wrapper_"]');
-      stage.insertBefore(thumb, greenFlagOverlay);
-      alerts.style.display = "flex";
+      if (greenFlagOverlay) {
+        stage.insertBefore(thumb, greenFlagOverlay)
+        alerts.style.display = "flex";
+      } else {
+        thumb.remove();
+      }
     }
     if (e.detail.action.type == "scratch-gui/vm-status/SET_STARTED_STATE") thumb.remove();
   });
+
+
+  while (true) {
+    const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]', {
+      markAsSeen: true,
+      reduxCondition: (state) => state.scratchGui.mode.isPlayerOnly
+    });
+    if (addon.tab.redux.state?.scratchGui?.projectState?.loadingState === "SHOWING_WITH_ID") {
+      controls.classList.remove("sa-controls-disabled");
+      return;
+    }
+    alerts = document.querySelector(".project-info-alerts");
+    controls = stageWrapper.querySelector('div[class^="controls_controls-container_"]');
+    controls.classList.add("sa-controls-disabled");
+
+    if (addon.tab.editorMode === "projectpage") {
+      const LoaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
+      LoaderBackground.style.backgroundColor = "rgba(0, 0, 0, 0.25)"; // Prevent style from being flashed on /projects/editor/
+      thumb.classList.add("loading");
+      stageWrapper.insertBefore(thumb, LoaderBackground);
+      if (alerts) alerts.style.display = "none";
+    }
+  }
 }
