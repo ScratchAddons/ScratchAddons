5004e321047221a6b8fa5582a5afda5a9f084732
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-10-10 08:25:15 -0400

    Remove waitForElement loop

--- addons/player-thumb/userscript.js
+++ addons/player-thumb/userscript.js
@@ -1,14 +1,22 @@
 export default async function ({ addon, console }) {
-  const projectId = location.pathname.split("/")[2];
+  if (addon.tab.editorMode === "editor") return;
 
+  const projectId = location.pathname.split("/")[2];
   const thumb = document.createElement("img");
   thumb.src = `https://uploads.scratch.mit.edu/get_image/project/${projectId}_480x360.png`;
   thumb.classList = "sa-project-thumb loading";
 
-  let controls, alerts;
+  const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]');
+  const alerts = document.querySelector(".project-info-alerts");
+  const controls = stageWrapper.querySelector('div[class^="controls_controls-container_"]');
+  controls.classList.add("sa-controls-disabled");
 
-  // TODO: Wait for Redux properly
-  await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]');
+  const loaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
+  stageWrapper.insertBefore(thumb, loaderBackground);
+  alerts.style.display = "none";
+  // Ensure thumbnail is injected before adding transparency
+  loaderBackground.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
+
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", (e) => {
     if (e.detail.action.type == "scratch-gui/project-changed/SET_PROJECT_CHANGED") {
@@ -26,21 +34,4 @@ export default async function ({ addon, console }) {
     }
     if (e.detail.action.type == "scratch-gui/vm-status/SET_STARTED_STATE") thumb.remove();
   });
-
-  while (true) {
-    const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]', {
-      markAsSeen: true,
-      reduxCondition: (state) =>
-        state.scratchGui.mode.isPlayerOnly && state.scratchGui.projectState.loadingState !== "SHOWING_WITH_ID",
-    });
-    alerts = document.querySelector(".project-info-alerts");
-    controls = stageWrapper.querySelector('div[class^="controls_controls-container_"]');
-    controls.classList.add("sa-controls-disabled");
-
-    const loaderBackground = stageWrapper.querySelector('[class*="loader_background_"]');
-    stageWrapper.insertBefore(thumb, loaderBackground);
-    if (alerts) alerts.style.display = "none";
-    // Ensure thumbnail is injected at least once before adding transparency
-    loaderBackground.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
-  }
 }
