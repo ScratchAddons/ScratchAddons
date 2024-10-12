157cabe3687f550ac0f1a2d4c567e76e0ed3daff
mybearworld <mybearworld@users.noreply.github.com>
2024-10-12 15:29:51 +0000

    Format code

--- addon-api/content-script/Tab.js
+++ addon-api/content-script/Tab.js
@@ -47,8 +47,8 @@ export default class Tab extends Listenable {
       this._clientVersion = document.querySelector("meta[name='format-detection']")
         ? "scratch-www"
         : document.querySelector("script[type='text/javascript']")
-        ? "scratchr2"
-        : null;
+          ? "scratchr2"
+          : null;
     return this._clientVersion;
   }
   /**
