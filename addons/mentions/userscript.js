bba6ef57b0dbfe4c0f68fff77dfba031a001e45d
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-10-15 06:50:54 +0200

    Use addon.tab.waitForElement

--- addons/mentions/userscript.js
+++ addons/mentions/userscript.js
@@ -2,38 +2,32 @@ export default async function ({ addon }) {
   if (!(await addon.auth.fetchIsLoggedIn())) {
     return;
   }
-  const username = await addon.auth.fetchUsername();
-  setInterval(() => {
-    if (addon.self.disabled) {
-      return;
-    }
-    document
-      .querySelectorAll(
-        [
-          ".comment-content", // Project/studio comments
-          ".description-block", // Insturctions / Notes and Credits
-          ".comment .content", // Profile comments
-          "#user-details", // About me / What I'm working on
-          ".post_body_html", // Forum posts
-          ".postsignature", // Forum signatures
-        ]
-          .filter(Boolean)
-          .map(
-            (selector) =>
-              `${selector} a[href^="/users/${username}"]:not(.sa-mention)` +
-              (addon.settings.get("direct")
-                ? `, ${selector} a[href^="https://scratch.mit.edu/users/${username}"]:not(.sa-mention)`
-                : "")
-          )
-          .join(",")
-      )
-      .forEach((el) => {
-        el.classList.add("sa-mention");
-      });
-  });
   addon.settings.addEventListener("change", () => {
     document.querySelectorAll(".sa-mention").forEach((el) => {
       el.classList.remove("sa-mention");
     });
   });
+  const username = await addon.auth.fetchUsername();
+  while (true) {
+    const el = await addon.tab.waitForElement(
+      [
+        ".comment-content", // Project/studio comments
+        ".description-block", // Insturctions / Notes and Credits
+        ".comment .content", // Profile comments
+        "#user-details", // About me / What I'm working on
+        ".post_body_html", // Forum posts
+        ".postsignature", // Forum signatures
+      ]
+        .filter(Boolean)
+        .map(
+          (selector) =>
+            `${selector} a[href^="/users/${username}"]:not(.sa-mention)` +
+            (addon.settings.get("direct")
+              ? `, ${selector} a[href^="https://scratch.mit.edu/users/${username}"]:not(.sa-mention)`
+              : "")
+        )
+        .join(",")
+    );
+    el.classList.add("sa-mention");
+  }
 }
