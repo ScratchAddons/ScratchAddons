ddfb5a9527aa0f42ad248d0fba66571faf6eb7d5
Samq64 <Samq64@users.noreply.github.com>
2024-11-02 00:13:47 +0000

    Format code

--- addons/no-script-bumping/userscript.js
+++ addons/no-script-bumping/userscript.js
@@ -5,7 +5,7 @@ export default async function ({ addon, console }) {
     const originalBumpNeighbors = ScratchBlocks.BlockSvg.prototype.bumpNeighbours;
     ScratchBlocks.BlockSvg.prototype.bumpNeighbours = function () {
       if (addon.self.disabled) originalBumpNeighbors.call(this);
-    }
+    };
   } else {
     const originalBumpNeighbors = ScratchBlocks.BlockSvg.prototype.bumpNeighbours_;
     ScratchBlocks.BlockSvg.prototype.bumpNeighbours_ = function () {
