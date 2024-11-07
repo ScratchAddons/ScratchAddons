98a632503b07e1b43ce3a830908d5c41f27a27cf
Muffin <muffin@muffin.ink>
2024-11-06 22:06:00 -0600

    folders: fix compatibility with sprite delete confirmation

--- addons/folders/userscript.js
+++ addons/folders/userscript.js
@@ -297,7 +297,7 @@ export default async function ({ addon, console, msg }) {
       typeof SpriteSelectorItem.prototype.setRef === "function" &&
       typeof SpriteSelectorItem.prototype.handleDrag === "function" &&
       typeof SpriteSelectorItem.prototype.handleDragEnd === "function" &&
-      typeof SpriteSelectorItem.prototype.handleDelete === "function" &&
+      typeof SpriteSelectorItem.prototype.handleDeleteButtonClick === "function" &&
       typeof SpriteSelectorItem.prototype.handleDuplicate === "function" &&
       typeof SpriteSelectorItem.prototype.handleExport === "function"
     )
@@ -885,7 +885,7 @@ export default async function ({ addon, console, msg }) {
   });
 
   const patchSpriteSelectorItem = (SpriteSelectorItem) => {
-    for (const method of ["handleDelete", "handleDuplicate", "handleExport"]) {
+    for (const method of ["handleDeleteButtonClick", "handleDuplicate", "handleExport"]) {
       const original = SpriteSelectorItem.prototype[method];
       SpriteSelectorItem.prototype[method] = function (...args) {
         if (typeof this.props.id === "number") {
