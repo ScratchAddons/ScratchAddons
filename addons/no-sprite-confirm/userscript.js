34ceb69c44a40eba85714e160255bb4d65fc78a9
Samq64 <Samq64@users.noreply.github.com>
2024-12-06 17:44:41 +0000

    Format code

--- addons/no-sprite-confirm/userscript.js
+++ addons/no-sprite-confirm/userscript.js
@@ -1,7 +1,8 @@
 export default async ({ addon, console }) => {
   while (true) {
-    const confirmButton = await addon.tab.waitForElement("[class^='delete-confirmation-prompt_ok-button_']", { markAsSeen: true });
+    const confirmButton = await addon.tab.waitForElement("[class^='delete-confirmation-prompt_ok-button_']", {
+      markAsSeen: true,
+    });
     if (!addon.self.disabled) confirmButton.click();
   }
 };
-
