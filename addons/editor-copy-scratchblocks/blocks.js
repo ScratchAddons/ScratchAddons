6801b4eeaea980608e988aad27ba705dbcfd68e3
just-a-hriday <just.a.hriday@gmail.com>
2024-12-11 15:21:13 +0530

    fix blocks, add more blocks

--- addons/editor-copy-scratchblocks/blocks.js
+++ addons/editor-copy-scratchblocks/blocks.js
@@ -17,14 +17,15 @@ function overrideCheck(block) {
 
 function processComponent(component, block, script) {
   if (component.input) {
-    const inputBlock = block.getInputTargetBlock(component.input);
+    const inputConnection = block.getInput(component.input).connection;
+    const inputBlock = inputConnection.targetBlock();
     if (inputBlock) {
       if (component.substack) {
         return getBlockCode(inputBlock, true).replaceAll(/^/gm, "   ") + "\n";
       } else {
         return getBlockCode(inputBlock);
       }
-    } else if (component.boolean) return "<>";
+    } else if (inputConnection.check_.length === 1 && inputConnection.check_[0] === "Boolean") return "<>";
     else return "";
   }
 
@@ -80,10 +81,17 @@ const blockSanitizations = [
 
 const repSanitizations = [...blockSanitizations, { searchValue: ")", replacer: "\\)" }];
 
-const numBlock = (block) => {
-  const content = block.getFieldValue("NUM");
+const numBlock = (field) => (block) => {
+  const content = block.getFieldValue(field);
   return /^[0-9e.-]*$/.test(content) ? `(${content})` : `[${sanitize(content, strInpSanitizations)}]`;
 };
+
+const dropdown = (field) => (block) => `(${sanitize(block.getField(field).getText(), repSanitizations)} v)`;
+
+const simpleReporter = (text, category) => () => {
+  // TODO: override checking
+  return `(${text})`;
+}
 
 const procedure = (block) => {
   const sanitizations = [
@@ -97,8 +105,15 @@ const procedure = (block) => {
     output = "";
 
   for (const component of block.getProcCode().split(/(?<!\\)(%[nbs])/)) {
-    if (/^%[nbs]/.test(component.trim())) {
-      output += getBlockCode(block.getInputTargetBlock(block.argumentIds_[argumentCount++]));
+    if (/^%[nbs]$/.test(component)) {
+      const inputBlock = block.getInputTargetBlock(block.argumentIds_[argumentCount++]);
+      if (!inputBlock) {
+        if (component === "%b") {
+          output += "<>";
+        }
+      } else {
+        output += getBlockCode(inputBlock);
+      }
     } else {
       const labelText = sanitize(component, sanitizations);
       output += labelText;
@@ -113,12 +128,12 @@ const procedure = (block) => {
 };
 
 const blocks = {
-  math_angle: numBlock,
-  math_integer: numBlock,
-  math_whole_number: numBlock,
-  math_positive_number: numBlock,
-  math_number: numBlock,
-  note: numBlock,
+  math_angle: numBlock("NUM"),
+  math_integer: numBlock("NUM"),
+  math_whole_number: numBlock("NUM"),
+  math_positive_number: numBlock("NUM"),
+  math_number: numBlock("NUM"),
+  note: numBlock("NOTE"),
   text: build`[${{ field: "TEXT", sanitizations: strInpSanitizations }}]`,
   colour_picker: build`[${{ field: "COLOUR", sanitizations: strInpSanitizations }}]`,
 
@@ -126,13 +141,13 @@ const blocks = {
   motion_turnright: build`turn right ${{ input: "DEGREES" }} degrees`,
   motion_turnleft: build`turn left ${{ input: "DEGREES" }} degrees`,
   motion_pointindirection: build`point in direction ${{ input: "DIRECTION" }}`,
-  motion_pointtowards_menu: build`(${{ field: "TOWARDS", sanitizations: repSanitizations }} v)`,
+  motion_pointtowards_menu: dropdown("TOWARDS"),
   motion_pointtowards: build`point towards ${{ input: "TOWARDS" }}`,
-  motion_goto_menu: build`(${{ field: "TO", sanitizations: repSanitizations }} v)`,
+  motion_goto_menu: dropdown("TO"),
   motion_gotoxy: build`go to x: ${{ input: "X" }} y: ${{ input: "Y" }}`,
   motion_goto: build`go to ${{ input: "TO" }}`,
   motion_glidesecstoxy: build`glide ${{ input: "SECS" }} secs to x: ${{ input: "X" }} y: ${{ input: "Y" }}`,
-  motion_glideto_menu: build`(${{ field: "TO", sanitizations: repSanitizations }} v)`,
+  motion_glideto_menu: dropdown("TO"),
   motion_glideto: build`glide ${{ input: "SECS" }} secs to ${{ input: "TO" }}`,
   motion_changexby: build`change x by ${{ input: "DX" }}`,
   motion_setx: build`set x to ${{ input: "X" }}`,
@@ -140,9 +155,9 @@ const blocks = {
   motion_sety: build`set y to ${{ input: "Y" }}`,
   motion_ifonedgebounce: build`if on edge, bounce`,
   motion_setrotationstyle: build`set rotation style [${{ field: "STYLE", sanitizations: dropdownSanitizations }} v]`,
-  motion_xposition: build`(x position${{ override: "motion" }})`,
-  motion_yposition: build`(y position${{ override: "motion" }})`,
-  motion_direction: build`(direction${{ override: "motion" }})`,
+  motion_xposition: simpleReporter("x position", "motion"),
+  motion_yposition: simpleReporter("y position", "motion"),
+  motion_direction: simpleReporter("direction", "motion"),
   motion_scroll_right: build`scroll right ${{ input: "DISTANCE" }} :: motion`,
   motion_scroll_up: build`scroll up ${{ input: "DISTANCE" }} :: motion`,
   motion_align_scene: build`align scene [${{ field: "ALIGNMENT", sanitizations: dropdownSanitizations }} v] :: motion`,
@@ -165,12 +180,12 @@ const blocks = {
   looks_cleargraphiceffects: build`clear graphic effects`,
   looks_changesizeby: build`change size by ${{ input: "CHANGE" }}`,
   looks_setsizeto: build`set size to ${{ input: "SIZE" }} %`,
-  looks_size: build`(size${{ override: "looks" }})`,
-  looks_costume: build`(${{ field: "COSTUME", sanitizations: repSanitizations }} v)`,
+  looks_size: simpleReporter("size", "looks"),
+  looks_costume: dropdown("COSTUME"),
   looks_switchcostumeto: build`switch costume to ${{ input: "COSTUME" }}`,
   looks_nextcostume: build`next costume`,
   looks_switchbackdropto: build`switch backdrop to ${{ input: "BACKDROP" }}`,
-  looks_backdrops: build`(${{ field: "BACKDROP", sanitizations: repSanitizations }} v)`,
+  looks_backdrops: dropdown("BACKDROP"),
   looks_gotofrontback: build`go to [${{ field: "FRONT_BACK", sanitizations: dropdownSanitizations }} v] layer`,
   looks_goforwardbackwardlayers: build`go [${{ field: "FORWARD_BACKWARD", sanitizations: dropdownSanitizations }} v] ${{
     input: "NUM",
@@ -180,7 +195,7 @@ const blocks = {
   looks_switchbackdroptoandwait: build`switch backdrop to ${{ input: "BACKDROP" }} and wait`,
   looks_nextbackdrop: build`next backdrop`,
 
-  sound_sounds_menu: build`(${{ field: "SOUND_MENU", sanitizations: repSanitizations }} v)`,
+  sound_sounds_menu: dropdown("SOUND_MENU"),
   sound_play: build`start sound ${{ input: "SOUND_MENU" }}`,
   sound_playuntildone: build`play sound ${{ input: "SOUND_MENU" }} until done`,
   sound_stopallsounds: build`stop all sounds`,
@@ -193,10 +208,10 @@ const blocks = {
   sound_cleareffects: build`clear sound effects`,
   sound_changevolumeby: build`change volume by ${{ input: "VOLUME" }}`,
   sound_setvolumeto: build`set volume to ${{ input: "VOLUME" }} %`,
-  sound_volume: build`(volume${{ override: "sound" }})`,
+  sound_volume: simpleReporter("volume", "sound"),
 
   event_whentouchingobject: build`when this sprite touches ${{ input: "TOUCHINGOBJECTMENU" }}`,
-  event_touchingobjectmenu: build`(${{ field: "TOUCHINGOBJECTMENU", sanitizations: repSanitizations }} v)`,
+  event_touchingobjectmenu: dropdown("TOUCHINGOBJECTMENU"),
   event_whenflagclicked: build`when green flag clicked`,
   event_whenthisspriteclicked: build`when this sprite clicked`,
   event_whenstageclicked: build`when stage clicked`,
@@ -211,27 +226,27 @@ const blocks = {
   event_whengreaterthan: build`when [${{ field: "WHENGREATERTHANMENU", sanitizations: dropdownSanitizations }} v] > ${{
     input: "VALUE",
   }}`,
-  event_broadcast_menu: build`(${{ field: "BROADCAST_OPTION", sanitizations: repSanitizations }} v)`,
+  event_broadcast_menu: dropdown("BROADCAST_OPTION"),
   event_broadcast: build`broadcast ${{ input: "BROADCAST_INPUT" }}`,
   event_broadcastandwait: build`broadcast ${{ input: "BROADCAST_INPUT" }} and wait`,
   event_whenkeypressed: build`when [${{ field: "KEY_OPTION", sanitizations: dropdownSanitizations }} v] key pressed`,
 
   control_forever: build`forever\n${{ input: "SUBSTACK", substack: true }}end`,
   control_repeat: build`repeat ${{ input: "TIMES" }}\n${{ input: "SUBSTACK", substack: true }}end`,
-  control_if: build`if ${{ input: "CONDITION", boolean: true }} then\n${{ input: "SUBSTACK", substack: true }}end`,
-  control_if_else: build`if ${{ input: "CONDITION", boolean: true }} then\n${{
+  control_if: build`if ${{ input: "CONDITION" }} then\n${{ input: "SUBSTACK", substack: true }}end`,
+  control_if_else: build`if ${{ input: "CONDITION" }} then\n${{
     input: "SUBSTACK",
     substack: true,
   }}else\n${{ input: "SUBSTACK2", substack: true }}end`,
   control_stop: build`stop [${{ field: "STOP_OPTION", sanitizations: dropdownSanitizations }} v]`,
   control_wait: build`wait ${{ input: "DURATION" }} seconds`,
-  control_wait_until: build`wait until ${{ input: "CONDITION", boolean: true }}`,
-  control_repeat_until: build`repeat until ${{ input: "CONDITION", boolean: true }}\n${{
+  control_wait_until: build`wait until ${{ input: "CONDITION" }}`,
+  control_repeat_until: build`repeat until ${{ input: "CONDITION" }}\n${{
     input: "SUBSTACK",
     substack: true,
   }}end`,
 
-  control_while: build`while ${{ input: "CONDITION", boolean: true }} {\n${{
+  control_while: build`while ${{ input: "CONDITION" }} {\n${{
     input: "SUBSTACK",
     substack: true,
   }}} @loopArrow :: control`,
@@ -240,7 +255,7 @@ const blocks = {
     input: "VALUE",
   }} {\n${{ input: "SUBSTACK", substack: true }}} @loopArrow :: control`,
   control_start_as_clone: build`when I start as a clone`,
-  control_create_clone_of_menu: build`(${{ field: "CLONE_OPTION", sanitizations: repSanitizations }} v)`,
+  control_create_clone_of_menu: dropdown("CLONE_OPTION"),
   control_create_clone_of: build`create clone of ${{ input: "CLONE_OPTION" }}`,
   control_delete_this_clone: build`delete this clone`,
   control_get_counter: build`(counter :: control)`,
@@ -249,29 +264,29 @@ const blocks = {
   control_all_at_once: build`all at once {\n${{ input: "SUBSTACK" }}} :: control`,
 
   sensing_touchingobject: build`<touching ${{ input: "TOUCHINGOBJECTMENU" }} ?>`,
-  sensing_touchingobjectmenu: build`(${{ field: "TOUCHINGOBJECTMENU", sanitizations: repSanitizations }} v)`,
+  sensing_touchingobjectmenu: dropdown("TOUCHINGOBJECTMENU"),
   sensing_touchingcolor: build`<touching color ${{ input: "COLOR" }} ?>`,
-  sensing_coloristouchingcolor: build`<colour ${{ input: "COLOR" }} is touching ${{ input: "COLOR" }} ?>`,
+  sensing_coloristouchingcolor: build`<colour ${{ input: "COLOR" }} is touching ${{ input: "COLOR2" }} ?>`,
   sensing_distanceto: build`(distance to ${{ input: "DISTANCETOMENU" }})`,
-  sensing_distancetomenu: build`(${{ field: "DISTANCETOMENU", sanitizations: repSanitizations }} v)`,
+  sensing_distancetomenu: dropdown("DISTANCETOMENU"),
   sensing_askandwait: build`ask ${{ input: "QUESTION" }} and wait`,
-  sensing_answer: build`(answer${{ override: "sensing" }})`,
+  sensing_answer: simpleReporter("answer", "sensing"),
   sensing_keypressed: build`key ${{ input: "KEY_OPTION" }} pressed`,
-  sensing_keyoptions: build`(${{ field: "KEY_OPTION", sanitizations: repSanitizations }} v)`,
+  sensing_keyoptions: dropdown("KEY_OPTION"),
   sensing_mousedown: build`<mouse down?${{ override: "sensing" }}>`,
-  sensing_mousex: build`(mouse x${{ override: "sensing" }})`,
-  sensing_mousey: build`(mouse y${{ override: "sensing" }})`,
+  sensing_mousex: simpleReporter("mouse x", "sensing"),
+  sensing_mousey: simpleReporter("mouse y", "sensing"),
   sensing_setdragmode: build`set drag mode [${{ field: "DRAG_MODE", sanitizations: dropdownSanitizations }} v]`,
-  sensing_loudness: build`(loudness${{ override: "sensing" }})`,
+  sensing_loudness: simpleReporter("loudness", "sensing"),
   sensing_loud: build`<loud :: sensing>`,
-  sensing_timer: build`(timer${{ override: "sensing" }})`,
+  sensing_timer: simpleReporter("timer", "sensing"),
   sensing_resettimer: build`reset timer`,
-  sensing_of_object_menu: build`(${{ field: "OBJECT", sanitizations: repSanitizations }} v)`,
+  sensing_of_object_menu: dropdown("OBJECT"),
   sensing_of: build`([${{ field: "PROPERTY", sanitizations: dropdownSanitizations }} v] of ${{ input: "OBJECT" }})`,
   sensing_current: build`(current [${{ field: "CURRENTMENU", sanitizations: dropdownSanitizations }} v])`,
-  sensing_dayssince2000: build`(days since 2000${{ override: "sensing" }})`,
-  sensing_username: build`(username${{ override: "sensing" }})`,
-  sensing_userid: build`(user id${{ override: "sensing" }})`,
+  sensing_dayssince2000: simpleReporter("days since 2000", "sensing"),
+  sensing_username: simpleReporter("username", "sensing"),
+  sensing_userid: simpleReporter("user id", "sensing"),
 
   operator_add: build`(${{ input: "NUM1" }} + ${{ input: "NUM2" }})`,
   operator_subtract: build`(${{ input: "NUM1" }} - ${{ input: "NUM2" }})`,
@@ -302,8 +317,8 @@ const blocks = {
   data_showvariable: build`show variable [${{ field: "VARIABLE", sanitizations: dropdownSanitizations }} v]`,
   data_hidevariable: build`hide variable [${{ field: "VARIABLE", sanitizations: dropdownSanitizations }} v]`,
   data_listcontents: build`(${{ field: "LIST", sanitizations: repSanitizations }}${{ override: "list" }})`,
-  data_listindexall: build`(${{ field: "INDEX", sanitizations: repSanitizations }} v)`,
-  data_listindexrandom: build`(${{ field: "INDEX", sanitizations: repSanitizations }} v)`,
+  data_listindexall: dropdown("INDEX"),
+  data_listindexrandom: dropdown("INDEX"),
   data_addtolist: build`add ${{ input: "ITEM" }} to [${{ field: "LIST", sanitizations: dropdownSanitizations }} v]`,
   data_deleteoflist: build`delete ${{ input: "INDEX" }} of [${{
     field: "LIST",
@@ -346,7 +361,7 @@ const blocks = {
   pen_penDown: build`pen down`,
   pen_penUp: build`pen up`,
   pen_setPenColorToColor: build`set pen color to ${{ input: "COLOR" }}`,
-  pen_menu_colorParam: build`(${{ field: "colorParam", sanitizations: repSanitizations }} v)`,
+  pen_menu_colorParam: dropdown("colorParam"),
   pen_changePenColorParamBy: build`change pen ${{ input: "COLOR_PARAM" }} by ${{ input: "VALUE" }}`,
   pen_setPenColorParamTo: build`set pen ${{ input: "COLOR_PARAM" }} to ${{ input: "VALUE" }}`,
   pen_changePenSizeBy: build`change pen size by ${{ input: "SIZE" }}`,
@@ -356,38 +371,54 @@ const blocks = {
   pen_setPenHueToNumber: build`set pen hue to ${{ input: "HUE" }}`,
   pen_changePenHueBy: build`change pen hue by ${{ input: "HUE" }}`,
 
-  music_menu_DRUM: build`(${{ field: "DRUM", sanitizations: repSanitizations }} v)`,
-  music_menu_INSTRUMENT: build`(${{ field: "INSTRUMENT", sanitizations: repSanitizations }} v)`,
+  music_menu_DRUM: dropdown("DRUM"),
+  music_menu_INSTRUMENT: dropdown("INSTRUMENT"),
   music_playDrumForBeats: build`play drum ${{ input: "DRUM" }} for ${{ input: "BEATS" }} beats`,
   music_restForBeats: build`rest for ${{ input: "BEATS" }} beats`,
   music_playNoteForBeats: build`play note ${{ input: "NOTE" }} for ${{ input: "BEATS" }} beats`,
   music_setInstrument: build`set instrument to ${{ input: "INSTRUMENT" }}`,
   music_setTempo: build`set tempo to ${{ input: "TEMPO" }}`,
   music_changeTempo: build`change tempo by ${{ input: "TEMPO" }}`,
-  music_getTempo: build`(tempo ${{ override: "MUSIC" }})`,
+  music_getTempo: simpleReporter("tempo", "MUSIC"),
 
   videoSensing_whenMotionGreaterThan: build`when video motion > ${{ input: "REFERENCE" }}`,
   videoSensing_videoOn: build`(video ${{ input: "ATTRIBUTE" }} on ${{ input: "SUBJECT" }})`,
-  videoSensing_menu_ATTRIBUTE: build`(${{ field: "ATTRIBUTE", sanitizations: repSanitizations }} v)`,
-  videoSensing_menu_SUBJECT: build`(${{ field: "SUBJECT", sanitizations: repSanitizations }} v)`,
+  videoSensing_menu_ATTRIBUTE: dropdown("ATTRIBUTE"),
+  videoSensing_menu_SUBJECT: dropdown("SUBJECT"),
   videoSensing_videoToggle: build`turn video ${{ input: "VIDEO_STATE" }}`,
-  videoSensing_menu_VIDEO_STATE: build`(${{ field: "VIDEO_STATE", sanitizations: repSanitizations }} v)`,
+  videoSensing_menu_VIDEO_STATE: dropdown("VIDEO_STATE"),
   videoSensing_setVideoTransparency: build`set video transparency to ${{ input: "TRANSPARENCY" }}`,
 
   text2speech_speakAndWait: build`speak ${{ input: "WORDS" }}`,
   text2speech_setVoice: build`set voice to ${{ input: "VOICE" }}`,
-  text2speech_menu_voices: build`(${{ field: "voices", sanitizations: repSanitizations }} v)`,
+  text2speech_menu_voices: dropdown("voices"),
   text2speech_setLanguage: build`set language to ${{ input: "LANGUAGE" }}`,
-  text2speech_menu_languages: build`(${{ field: "languages", sanitizations: repSanitizations }} v)`,
+  text2speech_menu_languages: dropdown("languages"),
 
-  translate_menu_languages: build`(${{ field: "languages", sanitizations: repSanitizations }} v)`,
+  translate_menu_languages: dropdown("languages"),
   translate_getTranslate: build`(translate ${{ input: "WORDS" }} to ${{ input: "LANGUAGE" }})`,
-  translate_getViewerLanguage: build`(language${{ override: "TRANSLATE" }})`,
+  translate_getViewerLanguage: simpleReporter("language", "TRANSLATE"),
 
   makeymakey_whenMakeyKeyPressed: build`when ${{ input: "KEY" }} key pressed`,
-  makeymakey_menu_KEY: build`(${{ field: "KEY", sanitizations: repSanitizations }} v)`,
+  makeymakey_menu_KEY: dropdown("KEY"),
   makeymakey_whenCodePressed: build`when ${{ input: "SEQUENCE" }} pressed in order`,
-  makeymakey_menu_SEQUENCE: build`(${{ field: "SEQUENCE", sanitizations: repSanitizations }} v)`,
+  makeymakey_menu_SEQUENCE: dropdown("SEQUENCE"),
+
+  // TODO: microbit
+
+  ev3_menu_motorPorts: dropdown("motorPorts"),
+  ev3_motorTurnClockwise: build`motor ${{ input: "PORT" }} turn this way for ${{ input: "TIME" }} seconds`,
+  ev3_motorTurnCounterClockwise: build`motor ${{ input: "PORT" }} turn that way for ${{ input: "TIME" }} seconds`,
+  ev3_motorSetPower: build`motor ${{ input: "PORT" }} set power ${{ input: "POWER" }} %`,
+  ev3_getMotorPosition: build`(motor ${{ input: "PORT" }} position)`,
+  ev3_menu_sensorPorts: dropdown("sensorPorts"),
+  ev3_whenButtonPressed: build`when button ${{ input: "PORT" }} pressed`,
+  ev3_whenDistanceLessThan: build`when distance \\< ${{ input: "DISTANCE" }}`,
+  ev3_whenBrightnessLessThan: build`when brightness \\< ${{ input: "DISTANCE" }}`,
+  ev3_buttonPressed: build`<button ${{ input: "PORT" }} pressed?>`,
+  ev3_getDistance: simpleReporter("distance", "EV3"),
+  ev3_getBrightness: simpleReporter("brightness", "EV3"),
+  ev3_beep: build`beep note ${{input: "NOTE"}} for ${{input: "TIME"}} secs`
 
   // TODO: add remaining extensions
 };
