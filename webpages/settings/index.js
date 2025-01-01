5f2ed854e4c425ae42a46ec324dd07c5ba2c4973
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-01-01 16:06:42 -0500

    Keep settings sidebar full-height in small mode (#8013)
    * Keep settings sidebar full-height in small mode
    
    * Higher z-index
    
    Co-authored-by: Maximouse <51849865+mxmou@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Maximouse <51849865+mxmou@users.noreply.github.com>

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -83,7 +83,6 @@ let fuse;
         theme: initialTheme,
         forceEnglishSetting: null,
         forceEnglishSettingInitial: null,
-        switchPath: "../../images/icons/switch.svg",
         moreSettingsOpen: false,
         categoryOpen: true,
         loaded: false,
@@ -183,11 +182,6 @@ let fuse;
       },
       sidebarToggle: function () {
         this.categoryOpen = !this.categoryOpen;
-        if (this.categoryOpen) {
-          vue.switchPath = "../../images/icons/close.svg";
-        } else {
-          vue.switchPath = "../../images/icons/switch.svg";
-        }
       },
       msg(message, ...params) {
         return chrome.i18n.getMessage(message, ...params);
@@ -644,11 +638,9 @@ let fuse;
     if (window.innerWidth < 1100) {
       vue.smallMode = true;
       vue.categoryOpen = false;
-      vue.switchPath = "../../images/icons/switch.svg";
     } else if (vue.smallMode !== false) {
       vue.smallMode = false;
       vue.categoryOpen = true;
-      vue.switchPath = "../../images/icons/close.svg";
     }
   }
   window.onresize = resize;
