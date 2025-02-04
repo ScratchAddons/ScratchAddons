b475d307e97505e3613fc7a74687c89c8b06264a
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-02-04 14:21:29 +0100

    Remove useless statement

--- addons/live-read-topics/topics.js
+++ addons/live-read-topics/topics.js
@@ -1,5 +1,4 @@
 export default async function ({ addon, msg, console }) {
-  if (addon.self.disabled) return;
   const topic = location.pathname.split("/")[3];
   new BroadcastChannel("sa-live-read-topics").postMessage(topic);
 }
