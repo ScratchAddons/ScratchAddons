89a2a5af354860a8b6becc28930247972f0b3b6b
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-02-15 19:31:16 +0100

    Hide New Posts button (WIP)

--- addons/live-read-topics/subforums.js
+++ addons/live-read-topics/subforums.js
@@ -3,9 +3,14 @@ export default async function ({ addon, msg, console }) {
     const el = document.querySelector(`.tclcon a[href='/discuss/topic/${e.data}/']`);
     if (!el) return;
     el.parentElement.classList.add("topic_isread");
-    const icon = el.closest(".tcl").querySelector(".inew");
-    if (!icon) return;
-    icon.classList.remove("inew");
-    icon.classList.add("forumicon");
+    const tcl = el.closest(".tcl");
+    const icon = tcl.querySelector(".inew, .iclosed");
+    if (icon) {
+      icon.classList.remove("inew", "iclosed");
+      icon.classList.add("forumicon");
+    }
+    const newPostsLink = tcl.querySelector(".tclcon > a");
+    newPostsLink.classList.add("sa-live-read-topics-new-posts");
+    newPostsLink.removeAttribute("href");
   });
 }
