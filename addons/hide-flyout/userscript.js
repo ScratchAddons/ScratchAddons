ff383f6c55e37051e0e2d9fbccfe0ded8a0880dd
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-02-25 10:48:07 -0500

    Update comment and add newline

--- addons/hide-flyout/userscript.js
+++ addons/hide-flyout/userscript.js
@@ -409,6 +409,7 @@ export default async function ({ addon, console, msg }) {
     let toolbox;
     if (Blockly.registry) toolbox = document.querySelector(".blocklyToolbox");
     else toolbox = document.querySelector(".blocklyToolboxDiv");
+
     const addExtensionButton = document.querySelector("[class^=gui_extension-button-container_]");
 
     for (let element of [toolbox, addExtensionButton, flyOut, scrollBar]) {
