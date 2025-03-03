86096d3d4a691b4120d8c947e66ad2fa86d51cdc
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-03-03 12:03:10 +0100

    Remove unused file

--- addons/env.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Userscripts must default-export an async function
- * that takes this object as a sole argument.
- * Note that this is commonly done by destructuring.
- * @example
- * export default async function({ addon, console, msg }) {
- */
-declare type AddonAPI = import("../addon-api/content-script/typedef.js").AddonAPI;
