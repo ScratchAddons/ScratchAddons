e2ebd241c3a4ef8af5f9bb8f3493f4a09af75f4d
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-03-24 16:29:38 -0400

    Implement navigation history

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -86,6 +86,7 @@ let fuse;
         moreSettingsOpen: false,
         relatedAddonsOpen: false,
         relatedAddons: [],
+        relatedAddonsHistory: [],
         categoryOpen: true,
         loaded: false,
         searchLoaded: false,
@@ -182,12 +183,28 @@ let fuse;
         }
         location.hash = "";
       },
-      openRelatedAddons(addonManifest) {
+      openRelatedAddons(addonManifest, log = true) {
+        if (log) this.relatedAddonsHistory.push(addonManifest);
         this.relatedAddons.length = 0;
         for (const relatedManifest of addonManifest._relatedAddons) {
           this.relatedAddons.push(relatedManifest);
         }
         this.relatedAddonsOpen = true;
+      },
+      backRelatedAddon() {
+        const addon = this.relatedAddonsHistory.pop();
+        if (this.relatedAddonsHistory.length === 0) {
+          this.relatedAddonsOpen = false;
+          setTimeout(() => {
+            const addonElem = document.querySelector(`.addons-container #addon-${addon._addonId}`);
+            addonElem.scrollIntoView({ behavior: "smooth" });
+            addonElem.classList.add("addon-blink");
+            setTimeout(() => addonElem.classList.remove("addon-blink"), 2001);
+          }, 0);
+        } else {
+          console.log(this.relatedAddonsHistory.at(-1))
+          this.openRelatedAddons(this.relatedAddonsHistory.at(-1), false);
+        }
       },
       sidebarToggle: function () {
         this.categoryOpen = !this.categoryOpen;
