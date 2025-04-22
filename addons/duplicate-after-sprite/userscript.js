fa6cc2a80d0f1b83c4dfa9de3da476fe68a12a1e
mybearworld <130385691+mybearworld@users.noreply.github.com>
2025-04-22 10:46:30 +0200

    New addon: Put sprite duplicates next to original sprite

---
+++ addons/duplicate-after-sprite/userscript.js
@@ -0,0 +1,26 @@
+export default async function ({ addon, console }) {
+  await addon.tab.redux.waitForState((state) => state.scratchGui.projectState.loadingState === "SHOWING_WITH_ID");
+
+  const SA_DUPLICATE_OF = Symbol("SA_DUPLICATE_OF");
+
+  const duplicate = addon.tab.traps.vm.runtime.targets[0].sprite.constructor.prototype.duplicate;
+  addon.tab.traps.vm.runtime.targets[0].sprite.constructor.prototype.duplicate = function () {
+    return duplicate.call(this).then((sprite) => {
+      sprite[SA_DUPLICATE_OF] = this;
+      return sprite;
+    });
+  };
+
+  const addTarget = addon.tab.traps.vm.runtime.addTarget;
+  addon.tab.traps.vm.runtime.addTarget = function (target) {
+    addTarget.call(this, target);
+    if (!addon.self.disabled && SA_DUPLICATE_OF in target.sprite) {
+      addon.tab.traps.vm.reorderTarget(
+        addon.tab.traps.vm.runtime.targets.length - 1,
+        addon.tab.traps.vm.runtime.targets.findIndex(
+          (candidate) => candidate.sprite === target.sprite[SA_DUPLICATE_OF]
+        ) + 1
+      );
+    }
+  };
+}
