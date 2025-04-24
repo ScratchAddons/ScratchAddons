b5e4e4c7ff6f3301a98ee3a36d9a71c54dcad153
Valmontechno <valmontechno@gmail.com>
2025-04-24 10:52:18 +0200

    Shift shortcut only

--- addons/hide-delete-button/userscript.js
+++ addons/hide-delete-button/userscript.js
@@ -1,12 +1,12 @@
 export default async function ({ addon, console, msg }) {
   document.addEventListener("keydown", (event) => {
-    if (event.key == addon.settings.get("show-trash-shortcut") && ! document.activeElement.matches(':focus')) {
+    if (event.key == "Shift" && ! document.activeElement.matches(':focus')) {
       document.body.classList.add("sa-show-trash");
     }
   });
 
   document.addEventListener("keyup", (event) => {
-    if (event.key == addon.settings.get("show-trash-shortcut")) {
+    if (event.key == "Shift") {
       document.body.classList.remove("sa-show-trash");
     }
   });
