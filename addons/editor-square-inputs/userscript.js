8ae07c83d5dcb167f1f1aa5250793a3c457f6a2b
Maximouse <mxmou.mouse@gmail.com>
2025-07-12 13:50:37 +0200

    editor-square-inputs: new Blockly (#8441)

--- addons/editor-square-inputs/userscript.js
+++ addons/editor-square-inputs/userscript.js
@@ -15,18 +15,58 @@ export default async function ({ addon }) {
     colour_picker: "color",
   };
 
+  let SQUARE;
+  let CORNER_RADIUS;
+  if (ScratchBlocks.registry) {
+    // new Blockly
+    const constants = new ScratchBlocks.zelos.ConstantProvider();
+    SQUARE = constants.SHAPES.SQUARE;
+    CORNER_RADIUS = constants.CORNER_RADIUS;
+  } else {
+    SQUARE = ScratchBlocks.OUTPUT_SHAPE_SQUARE;
+  }
+
   const originalJsonInit = ScratchBlocks.BlockSvg.prototype.jsonInit;
 
   ScratchBlocks.BlockSvg.prototype.jsonInit = function (json) {
     if (!addon.self.disabled && opcodeToSettings[this.type] && addon.settings.get(opcodeToSettings[this.type])) {
       originalJsonInit.call(this, {
         ...json,
-        outputShape: ScratchBlocks.OUTPUT_SHAPE_SQUARE,
+        outputShape: SQUARE,
       });
     } else {
       originalJsonInit.call(this, json);
     }
   };
+
+  if (ScratchBlocks.registry) {
+    // new Blockly
+
+    const originalFieldTextInputWidgetCreate = ScratchBlocks.FieldTextInput.prototype.widgetCreate_;
+    ScratchBlocks.FieldTextInput.prototype.widgetCreate_ = function () {
+      const htmlInput = originalFieldTextInputWidgetCreate.call(this);
+      if (!addon.self.disabled && this.isFullBlockField() && this.getSourceBlock().getOutputShape() === SQUARE) {
+        // Change border radius of HTML input
+        const div = ScratchBlocks.WidgetDiv.getDiv();
+        const scale = this.workspace_.getAbsoluteScale();
+        const borderRadius = `${CORNER_RADIUS * scale}px`;
+        div.style.borderRadius = borderRadius;
+        htmlInput.style.borderRadius = borderRadius;
+      }
+      return htmlInput;
+    };
+
+    const originalFinalizeHorizontalAlignment = ScratchBlocks.zelos.RenderInfo.prototype.finalizeHorizontalAlignment_;
+    ScratchBlocks.zelos.RenderInfo.prototype.finalizeHorizontalAlignment_ = function () {
+      // Increase minimum width of square inputs
+      const oldMinBlockWidth = this.constants_.MIN_BLOCK_WIDTH;
+      if (!addon.self.disabled && this.block_.getOutputShape() === SQUARE) {
+        this.constants_.MIN_BLOCK_WIDTH = 6 * this.constants_.GRID_UNIT;
+      }
+      originalFinalizeHorizontalAlignment.call(this);
+      this.constants_.MIN_BLOCK_WIDTH = oldMinBlockWidth;
+    };
+  }
 
   function update() {
     updateAllBlocks(addon.tab);
