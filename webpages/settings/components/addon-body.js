f5baeda9ad68ad7495d581189d1d95f63ecaed78
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-07-25 12:07:01 -0400

    Import and export individual addon's settings

--- webpages/settings/components/addon-body.js
+++ webpages/settings/components/addon-body.js
@@ -1,3 +1,4 @@
+import downloadBlob from "../../../libraries/common/cs/download-blob.js";
 const isIframe = window.parent !== window;
 
 export default async function ({ template }) {
@@ -53,6 +54,51 @@ export default async function ({ template }) {
           this.$root.updateSettings(this.addon);
           console.log(`Loaded preset ${preset.id} for ${this.addon._addonId}`);
         }
+      },
+      importPreset() {
+        const inputElem = Object.assign(document.createElement("input"), {
+          hidden: true,
+          type: "file",
+          accept: "application/json",
+        });
+        inputElem.addEventListener(
+          "change",
+          async (e) => {
+            const file = inputElem.files[0];
+            if (!file) {
+              inputElem.remove();
+              alert(chrome.i18n.getMessage("fileNotSelected"));
+              return;
+            }
+            const text = await file.text();
+            inputElem.remove();
+            let preset;
+            try {
+              preset = JSON.parse(text);
+              if (preset.addonId !== this.addon._addonId) {
+                throw "Incorrect or missing addon ID";
+              }
+            } catch (e) {
+              console.warn(`Error importing settings file for ${this.addon._addonId}:`, e);
+              alert(chrome.i18n.getMessage("importFailed"));
+              return;
+            }
+            this.loadPreset(preset);
+          },
+          { once: true }
+        );
+        document.body.appendChild(inputElem);
+        inputElem.click();
+      },
+      exportPreset() {
+        const preset = Object.assign({
+          addonId: this.addon._addonId,
+          id: "custom-preset",
+          values: this.addonSettings,
+        });
+        const blob = new Blob([JSON.stringify(preset)], { type: "application/json" });
+        const name = this.addon.name.replaceAll(" ", "-").toLowerCase();
+        downloadBlob(`${name}.json`, blob);
       },
       loadDefaults() {
         if (window.confirm(chrome.i18n.getMessage("confirmReset"))) {
