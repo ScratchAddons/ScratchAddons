bd7f6974536356bbad574a26eb8d4f76db0f2320
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-08-10 13:39:32 -0400

    React instead of DOM, ID instead of name

--- addons/move-to-top-layer/userscript.js
+++ addons/move-to-top-layer/userscript.js
@@ -3,8 +3,8 @@
 export default async function ({ addon, console, msg }) {
   const vm = addon.tab.traps.vm;
 
-  function spriteToFront(name) {
-    const target = vm.runtime.getSpriteTargetByName(name);
+  function spriteToFront(id) {
+    const target = vm.runtime.getTargetById(id);
     target.goToFront();
     target.setVisible(true);
   }
@@ -17,26 +17,26 @@ export default async function ({ addon, console, msg }) {
         !e.target.closest("div[class^='delete-button_delete-button_']") &&
         !e.target.closest(".sa-sprite-properties-info-btn")
       ) {
-        // TODO: folders compatibility
-        const spriteName = parentDiv.querySelector("div[class^='sprite-selector-item_sprite-name']").innerText;
-        spriteToFront(spriteName);
+        const reactInternalKey = addon.tab.traps.getInternalKey(parentDiv);
+        const spriteId = parentDiv[reactInternalKey].child.key;
+        if (!spriteId.startsWith("&__")) {
+          // Not a folder
+          spriteToFront(spriteId);
+        }
       }
     }
   });
   addon.tab.createEditorContextMenu(
     (ctx) => {
-      if (typeof ctx.name === "object") {
-        // folders compatibility
-        spriteToFront(ctx.name.realName);
-      } else {
-        spriteToFront(ctx.name);
-      }
+      console.log(ctx)
+      spriteToFront(ctx.itemId);
     },
     {
       types: ["sprite"],
       position: "assetContextMenuAfterExport",
       order: 1,
       label: msg("move-to-front-layer"),
+      condition: (ctx) => typeof ctx.itemId === "string",
     }
   );
 }
