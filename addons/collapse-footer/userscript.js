d5955bfa47bd3dbcebd95e64762775767bc04483
Maximouse <mxmou.mouse@gmail.com>
2025-08-29 20:35:33 +0200

    collapse-footer: support updated scratch-www footer & other improvements (#8522)
    * Fixes the sticky footer not expanding on the home page - Scratch has removed the #donor element, which caused the userscript to crash. Also removes references to #donor from other addons.
    * Reduces the height of the expanded sticky footer on scratch-www pages. The contents of the footer have been updated, which made it less tall.
    * Fixes the footer not expanding after entering and leaving the editor.
    * Changes the icon on the collapsed footer to the one used by sprite-properties.
    * Hides the icon when the addon is dynamically disabled.
    * Adds a notice explaining how to work around a bug in mobile Firefox (#7073).

--- addons/collapse-footer/userscript.js
+++ addons/collapse-footer/userscript.js
@@ -1,15 +1,6 @@
 export default async function ({ addon, console }) {
   const enabledAddons = await addon.self.getEnabledAddons("community");
-  const footer = await addon.tab.waitForElement("#footer");
   const root = document.documentElement;
-
-  const icon = document.createElement("img");
-  icon.src = addon.self.dir + "/icon.svg";
-  icon.height = 16;
-  icon.width = 16;
-  icon.className = "sa-footer-arrow";
-  icon.draggable = false;
-  footer.insertBefore(icon, footer.firstChild);
 
   if (!(addon.settings.get("infiniteScroll") && enabledAddons.includes("infinite-scroll"))) {
     // If the setting is enabled, infinate-scroll
@@ -17,29 +8,38 @@ export default async function ({ addon, console }) {
     document.body.classList.add("sa-collapse-footer");
   }
 
-  if (addon.tab.clientVersion === "scratchr2" && !enabledAddons.includes("scratchr2")) {
+  if (addon.tab.clientVersion === "scratchr2") {
     root.style.setProperty("--footer-hover-height", "250px");
   }
 
-  if (location.pathname.split("/")[1] === "") {
-    // Moves the donor text into the footer on the front page
-    const donor = document.getElementById("donor");
-    footer.appendChild(donor);
-    root.style.setProperty("--footer-hover-height", "410px");
+  while (true) {
+    const footer = await addon.tab.waitForElement("#footer", {
+      markAsSeen: true,
+      reduxCondition: (state) => (state.scratchGui ? state.scratchGui.mode.isPlayerOnly : true),
+    });
+
+    const icon = document.createElement("img");
+    icon.src = addon.self.dir + "/icon.svg";
+    icon.height = 16;
+    icon.width = 16;
+    icon.className = "sa-footer-arrow";
+    icon.draggable = false;
+    footer.insertBefore(icon, footer.firstChild);
+    addon.tab.displayNoneWhileDisabled(icon);
+
+    let collapseTimeout;
+
+    footer.addEventListener("mouseover", () => {
+      footer.classList.add("transition", "expanded");
+      if (collapseTimeout) {
+        clearTimeout(collapseTimeout);
+      }
+    });
+
+    footer.addEventListener("mouseout", () => {
+      collapseTimeout = setTimeout(() => {
+        footer.classList.remove("expanded");
+      }, 200);
+    });
   }
-
-  let collapseTimeout;
-
-  footer.addEventListener("mouseover", () => {
-    footer.classList.add("transition", "expanded");
-    if (collapseTimeout) {
-      clearTimeout(collapseTimeout);
-    }
-  });
-
-  footer.addEventListener("mouseout", () => {
-    collapseTimeout = setTimeout(() => {
-      footer.classList.remove("expanded");
-    }, 200);
-  });
 }
