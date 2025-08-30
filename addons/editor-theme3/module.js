8c22f17a4d792a05acf4e4351ac9e53221fbbcaf
FlamedDogo <96555444+FlamedDogo99@users.noreply.github.com>
2025-08-30 12:00:37 -0600

    New Addon: Recolor Custom Blocks (#8476)
    * Created recolor-custom-blocks
    
    First commit
    
    * Fixed misformatted addons.json + label coloring
    
    * Removed debugger calls
    
    * Rewrote for new Blockly
    
    No longer using Blockly.BlockSvg.prototype.render for coloring, but Blockly.BlockSvg.prototype.updateColour, which is what the debugger addon is using. This required shimming procedures_declaration's init and onChangeFn to update colors instead of getting it for free through the Blockly.BlockSvg.prototype.render
    
    * Rewrote menu recolor shim
    
    reorder-custom-inputs's way of shimming stuff would cause a crash when this addon did it the same way. We now update procedure_delcaration's onChangeFn method the first time each procedure_delcaration is displayed, which avoids this issue.
    This also fixes the child stroke color issues mentioned previously, albeit with some hacky solutions
    
    * Removed debugger calls again
    
    * Fixed middle-click-popup addon compatibility
    
    Turns out that they already calculate the color based off of the blocks current color, but then override it with the .sa-block-color- class
    
    * Fixed theme updates in new blockly
    
    * Fixed (some) theme3 incompatibility
    
    Had to edit theme3 this time
    
    * Added `hide prefix` option to addon
    
    Also moved to only using blockInitSvg for catching procedure declarations, and everything else is handled through createCustomInputs_. This allowed us to drop listening to redux events
    
    * Added color menu to procedure editor
    
    * Added block color buttons
    
    Removed dropdown selector
    
    * Apply suggestions from code review
    
    Formatting changes from @Samq64
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    * Forgot userstyle + fixed theme3 problems
    
    * Fix pull conflicts
    
    * Moved inline svg to file
    
    * Format code
    
    * House-cleaning, module for theme3
    
    Lots of house cleaning
    Changed prefixes to match en_us category names instead of weird Blockly ones
    Added a module for theme3 to share it's colored text methods, and if it's enabled
    This should be fully compatible with theme3 now
    Cleaned up messiness from porting to new Blockly
    Added more comments, standardized naming conventions
    Color menu hidden on disable
    Suggestions implemented
    
    * Format code
    
    * Restored accidental change to editor-theme3/addons.json
    
    Oops
    
    * Format code
    
    * Updated addon description 
    
    Thanks for catching this @Samq64
    
    * Added comments for editor-theme3 addon changes
    
    * Format code
    
    * Fixed unlikely edge case
    
    * Revert "Fixed unlikely edge case"
    
    This reverts commit 54d0a7b685c3fe15f39ee135726f5fa6c9217314.
    
    * Fixed unlikely edge case v2
    
    * Update version added in addon.json
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    Co-authored-by: FlamedDogo99 <FlamedDogo99@users.noreply.github.com>

---
+++ addons/editor-theme3/module.js
@@ -0,0 +1,48 @@
+// Allowing theme3 and recolor-custom-blocks to communicate
+
+let addons = {};
+/**
+ * Registers an addon to have shared methods / values
+ * @param {Addon} addonObj - the addon object
+ * @param {string} addonName - the addon's name
+ */
+export const registerAddon = (addonObj, addonName) => {
+  addons[addonName] = { addonObj: addonObj, methods: {} };
+};
+/**
+ * Gets the value from `addon.self` from a registered addon
+ * @param {string} addonName - the addon's name
+ * @param {string} value - the requested value
+ * @returns {*} - the requested value or null if none exists
+ */
+export const getAddonValue = (addonName, value) => {
+  const addon = addons[addonName];
+  if (addon) return addon?.addonObj?.self?.[value];
+};
+/**
+ * Adds a method to be shared
+ * @param {string} addonName - the addon's name
+ * @param {Function} method - the method being shared
+ * @param methodName - the method's name
+ */
+export const shareMethod = (addonName, method, methodName) => {
+  const addon = addons[addonName];
+  if (addon) {
+    addon.methods[methodName] = method;
+  } else {
+    throw new Error("Addon " + addonName + "has not been registered");
+  }
+};
+/**
+ * Tries to call a registered addon's method
+ * @param {string} addonName - the addon's name
+ * @param {string} methodName - the name of the shared method
+ * @param {*[]} args - everything being passed into `call()`
+ * @returns {{return: *, exists: boolean}|{return: null, exists: boolean}} - what was returned from the method, and if the method exists under the specified name
+ */
+export const callSharedMethod = (addonName, methodName, ...args) => {
+  const addon = addons[addonName];
+  if (!addon) return { return: null, exists: false };
+  const method = addon.methods[methodName];
+  return { return: method?.call?.(...args), exists: method !== null };
+};
