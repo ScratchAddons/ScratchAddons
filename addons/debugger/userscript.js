e0f8634881ac7ea8cb05418fa028024e964f0e02
Joe Clinton <48254978+Joeclinton1@users.noreply.github.com>
2025-09-12 23:15:28 +0100

    Move right debugger header buttons to footer and make modal resizable (#8549)
    * Initial Commit
    
    * Format code
    
    * hide footer when empty and increase height of container to include footer
    
    * Format code
    
    * support compact editor
    
    * Update footer on pause toggle
    
    * Remove close button label text
    
    * Clean up styles
    
    Moves and removes some declarations
    
    * Update addons/debugger/style.css
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    * Update addons/debugger/style.css
    
    Co-authored-by: Maximouse <brestl1c@outlook.com>
    
    * add vertical divider between resume and other buttons
    
    * Format code
    
    * Bold all buttons and remove border of footer buttons to make it match scratch buttons.
    
    * Make debugger resizable whilst we're at it
    
    * Fix resume button divider appearing even no buttons on right side
    
    * fix compatibility between resume divider and editor-compact
    
    * Enable debugger tabs to wrap to multiple rows when space is limited
    
    * Replace redundant createHeaderButton with createIconButton
    
    * Add animation to resume button with heartbeat box shadow
    
    * Format code
    
    * Update addon.json
    
    * Format code
    
    * Use "dialog" not "modal" in addon.json
    
    Co-authored-by: DNin01 <106490990+DNin01@users.noreply.github.com>
    
    * Make resume button animation more subtle
    
    * Use dark mode compatible border css
    
    * Simplify padding expression for .sa-debugger-tabs
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    * Use modal api close button
    
    * Remove leftover debugger header button styles
    
    * Simplift padding expression and reduce right padding of close button
    
    * A tiny bit thicker padding
    
    * remove blur box shadow 100%
    
    Co-authored-by: Maximouse <brestl1c@outlook.com>
    
    * Remove blur box shadow 0%
    
    Co-authored-by: Maximouse <brestl1c@outlook.com>
    
    * Change to "window" instead of "dialog" for consistency
    
    ---------
    
    Co-authored-by: Joeclinton1 <Joeclinton1@users.noreply.github.com>
    Co-authored-by: DNin01 <106490990+DNin01@users.noreply.github.com>
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    Co-authored-by: Maximouse <brestl1c@outlook.com>

--- addons/debugger/userscript.js
+++ addons/debugger/userscript.js
@@ -110,6 +110,12 @@ export default async function ({ addon, console, msg }) {
   const tabContentContainer = Object.assign(document.createElement("div"), {
     className: "sa-debugger-tab-content",
   });
+  const interfaceFooter = Object.assign(document.createElement("div"), {
+    className: "sa-debugger-footer",
+  });
+  const footerButtonContainer = Object.assign(document.createElement("div"), {
+    className: "sa-debugger-footer-buttons",
+  });
 
   let isInterfaceVisible = false;
   const setInterfaceVisible = (_isVisible) => {
@@ -159,11 +165,12 @@ export default async function ({ addon, console, msg }) {
   interfaceHeader.addEventListener("mousedown", handleStartDrag);
 
   interfaceHeader.append(tabListElement, buttonContainerElement);
-  interfaceContainer.append(interfaceHeader, tabContentContainer);
+  interfaceFooter.appendChild(footerButtonContainer);
+  interfaceContainer.append(interfaceHeader, tabContentContainer, interfaceFooter);
   document.body.append(interfaceContainer);
   moveInterface(0, 0); // necessary to initialize position if running scratch-gui locally
 
-  const createHeaderButton = ({ text, icon, description }) => {
+  const createIconButton = ({ text, icon, description }) => {
     const button = Object.assign(document.createElement("div"), {
       className: addon.tab.scratchClass("card_shrink-expand-button"),
       draggable: false,
@@ -205,23 +212,41 @@ export default async function ({ addon, console, msg }) {
     };
   };
 
-  const unpauseButton = createHeaderButton({
+  const unpauseButton = createIconButton({
     text: msg("unpause"),
     icon: addon.self.dir + "/icons/play.svg",
   });
   unpauseButton.element.classList.add("sa-debugger-unpause");
   unpauseButton.element.addEventListener("click", () => setPaused(false));
+
+  const unpauseContainer = Object.assign(document.createElement("div"), {
+    className: "sa-debugger-unpause-container",
+  });
+  unpauseContainer.appendChild(unpauseButton.element);
   const updateUnpauseVisibility = (paused) => {
-    unpauseButton.element.style.display = paused ? "" : "none";
+    unpauseContainer.style.display = paused ? "" : "none";
+    setTimeout(updateFooterVisibility, 0); // Have to wait for other modules to update their buttons
   };
   updateUnpauseVisibility(isPaused());
   onPauseChanged(updateUnpauseVisibility);
 
-  const closeButton = createHeaderButton({
-    text: msg("close"),
-    icon: addon.self.dir + "/icons/close.svg",
+  // Close button structure copied from addon-api/content-script/modal.js
+  const closeContainer = Object.assign(document.createElement("div"), {
+    className: addon.tab.scratchClass("modal_header-item", "modal_header-item-close"),
   });
-  closeButton.element.addEventListener("click", () => setInterfaceVisible(false));
+  const closeButton = Object.assign(document.createElement("div"), {
+    className: addon.tab.scratchClass("close-button_close-button", "close-button_large"),
+    title: msg("close"),
+  });
+  closeContainer.appendChild(closeButton);
+  closeButton.appendChild(
+    Object.assign(document.createElement("img"), {
+      className: addon.tab.scratchClass("close-button_close-icon"),
+      src: import.meta.url + "/../../../images/cs/close-s3.svg",
+      draggable: false,
+    })
+  );
+  closeButton.addEventListener("click", () => setInterfaceVisible(false));
 
   const originalStep = vm.runtime._step;
   const afterStepCallbacks = [];
@@ -476,7 +501,7 @@ export default async function ({ addon, console, msg }) {
 
   const api = {
     debug: {
-      createHeaderButton,
+      createIconButton: createIconButton,
       createHeaderTab,
       setHasUnreadMessage,
       addAfterStepCallback,
@@ -499,6 +524,18 @@ export default async function ({ addon, console, msg }) {
   }
   messagesLoggedBeforeLogsTabLoaded.length = 0;
 
+  function updateFooterVisibility() {
+    // Show footer only if buttons are visible
+    interfaceFooter.style.display = "none";
+    const allButtons = footerButtonContainer.children;
+    for (const button of allButtons) {
+      if (button.style.display !== "none") {
+        interfaceFooter.style.display = "";
+        return;
+      }
+    }
+  }
+
   let activeTab;
   const setActiveTab = (tab) => {
     if (tab === activeTab) return;
@@ -514,11 +551,15 @@ export default async function ({ addon, console, msg }) {
     tabContentContainer.appendChild(tab.content);
 
     removeAllChildren(buttonContainerElement);
-    buttonContainerElement.appendChild(unpauseButton.element);
+    buttonContainerElement.appendChild(closeContainer);
+
+    removeAllChildren(footerButtonContainer);
+    footerButtonContainer.appendChild(unpauseContainer);
     for (const button of tab.buttons) {
-      buttonContainerElement.appendChild(button.element);
+      footerButtonContainer.appendChild(button.element);
     }
-    buttonContainerElement.appendChild(closeButton.element);
+
+    updateFooterVisibility();
 
     if (isInterfaceVisible) {
       activeTab.show();
