0ac821050b8f2b0f609dafb92e293e7bde1b110e
VIGARPAST_777 <vicgarpas3@alu.edu.gva.es>
2025-09-16 19:40:31 +0200

    Update userscript.js

--- addons/compact-profile/userscript.js
+++ addons/compact-profile/userscript.js
@@ -1,21 +1,39 @@
 export default async function ({ addon }) {
-  function groupSliders() {
-    const boxes = Array.from(document.querySelectorAll(".box.slider-carousel-container"));
-    const order = ["shared", "favorites", "studios-following", "studios-curating", "following", "followers"];
-    const orderedBoxes = [];
-    order.forEach(id => {
-      const box = boxes.find(el => el.querySelector(`#${id}`));
-      if (box) orderedBoxes.push(box);
+  const allSliders = Array.from(document.querySelectorAll('.box.slider-carousel-container'));
+  const parentContainer = allSliders[0]?.parentNode;
+  if (!allSliders.length || !parentContainer) return;
+
+  const ordered = [];
+  allSliders.forEach(slider => {
+    const carousel = slider.querySelector('.slider-carousel');
+    if (!carousel) return;
+    ordered.push(slider);
+  });
+
+  for (let i = 0; i < ordered.length - 1; i += 2) {
+    const left = ordered[i];
+    const right = ordered[i + 1];
+
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'flex';
+    wrapper.style.justifyContent = 'space-between';
+    wrapper.style.marginBottom = '20px';
+
+    left.style.width = '48%';
+    right.style.width = '48%';
+    left.style.display = 'block';
+    right.style.display = 'block';
+
+    parentContainer.insertBefore(wrapper, left);
+    wrapper.appendChild(left);
+    wrapper.appendChild(right);
+
+    [left, right].forEach(box => {
+      const carousel = box.querySelector('.slider-carousel');
+      if (carousel) {
+        carousel.style.overflowX = 'auto';
+        carousel.style.scrollBehavior = 'smooth';
+      }
     });
-    for (let i = 0; i < orderedBoxes.length - 1; i += 2) {
-      const left = orderedBoxes[i];
-      const right = orderedBoxes[i + 1];
-      const parent = document.createElement("div");
-      parent.className = "sa-slider-flex-wrapper";
-      left.parentNode.insertBefore(parent, left);
-      parent.appendChild(left);
-      parent.appendChild(right);
-    }
   }
-  groupSliders();
-}
+};
