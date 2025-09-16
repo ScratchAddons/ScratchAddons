6073dd847e707f9bca9bdb80b9da0c2b80b33e1c
VIGARPAST_777 <vicgarpas3@alu.edu.gva.es>
2025-09-16 19:35:17 +0200

    Update userscript.js

--- addons/compact-profile/userscript.js
+++ addons/compact-profile/userscript.js
@@ -1,33 +1,21 @@
 export default async function ({ addon }) {
   function groupSliders() {
     const boxes = Array.from(document.querySelectorAll(".box.slider-carousel-container"));
-
-    const order = [
-      "shared",
-      "favorites",
-      "studios-following",
-      "studios-curating",
-      "following",
-      "followers"
-    ];
-
+    const order = ["shared", "favorites", "studios-following", "studios-curating", "following", "followers"];
     const orderedBoxes = [];
     order.forEach(id => {
       const box = boxes.find(el => el.querySelector(`#${id}`));
       if (box) orderedBoxes.push(box);
     });
-
     for (let i = 0; i < orderedBoxes.length - 1; i += 2) {
       const left = orderedBoxes[i];
       const right = orderedBoxes[i + 1];
       const parent = document.createElement("div");
-      parent.className = "sa-compact-row";
+      parent.className = "sa-slider-flex-wrapper";
       left.parentNode.insertBefore(parent, left);
       parent.appendChild(left);
       parent.appendChild(right);
     }
   }
-
   groupSliders();
 }
-
