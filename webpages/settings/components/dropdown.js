787f12df117032cc8d3338a2c951cba67dd5786c
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-09-23 16:48:47 -0400

    Dropdown arrow navigation
    Thanks @Dnin01 for the code snippet!

--- webpages/settings/components/dropdown.js
+++ webpages/settings/components/dropdown.js
@@ -5,7 +5,21 @@ export default async function ({ template }) {
     data() {
       return {
         isOpen: false,
+        focusedIndex: 0,
+        shiftAmountsByKey: {
+          ArrowUp: -1,
+          ArrowDown: 1,
+          ArrowLeft: -1,
+          ArrowRight: 1,
+          Home: -1000,
+          End: 1000
+        }
       };
+    },
+    computed: {
+      items() {
+        return Array.from(this.$els.list.children);
+      },
     },
     methods: {
       toggle() {
@@ -19,7 +33,29 @@ export default async function ({ template }) {
         if (e.target.tagName === "LI") {
           this.$root.closeDropdowns();
         }
-      }
+      },
+      handleKeys(e) {
+        const element = this.$els.list;
+        if (e.ctrlKey || e.metaKey || e.altKey) return;
+        if (e.key === "Tab") {
+          this.$root.closeDropdowns();
+        } else if (document.activeElement.tagName === "LI" && e.key === "Enter") {
+          document.activeElement.click();
+        } else {
+          const shiftBy = this.shiftAmountsByKey[e.key];
+          if (shiftBy) e.preventDefault();
+          else return;
+
+          const oldFocusIndex = this.items.indexOf(document.activeElement);
+          // Move,
+          const adjustedFocusIndex = oldFocusIndex + shiftBy;
+          // clamp,
+          const newFocusIndex = Math.min(Math.max(adjustedFocusIndex, 0), element.childElementCount - 1);
+          // set focus!
+          const targetElement = element.children[newFocusIndex];
+          targetElement.focus();
+        }
+      },
     },
     events: {
       closeDropdowns(...params) {
