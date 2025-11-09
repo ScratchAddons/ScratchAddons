72046e61ef10f5f8ae57394fc185612bf3dacd13
pufferfish101007 <pufferfish101007@users.noreply.github.com>
2025-11-09 22:03:10 +0000

    Format code

--- addons/custom-reporters/userscript.js
+++ addons/custom-reporters/userscript.js
@@ -494,8 +494,8 @@ export default async function ({ addon, msg, console }) {
               "custom-procedures_option-card"
             )}" role="button" tabindex="0">
                 <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${
-        addon.self.dir
-      }/stack.svg">
+                  addon.self.dir
+                }/stack.svg">
                 <div class="${addon.tab.scratchClass("custom-procedures_option-title")}">
                     <span>${msg("stack")}</span>
                 </div>
@@ -504,8 +504,8 @@ export default async function ({ addon, msg, console }) {
               "custom-procedures_option-card"
             )}" role="button" tabindex="0">
                 <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${
-        addon.self.dir
-      }/reporter.svg">
+                  addon.self.dir
+                }/reporter.svg">
                 <div class="${addon.tab.scratchClass("custom-procedures_option-title")}">
                     <span>${msg("numortext")}</span>
                 </div>
@@ -514,8 +514,8 @@ export default async function ({ addon, msg, console }) {
               "custom-procedures_option-card"
             )}" role="button" tabindex="0">
                 <img class="${addon.tab.scratchClass("custom-procedures_option-icon")}" src="${
-        addon.self.dir
-      }/predicate.svg">
+                  addon.self.dir
+                }/predicate.svg">
                 <div class="${addon.tab.scratchClass("custom-procedures_option-title")}">
                     <span>${msg("boolean")}</span>
                 </div>
