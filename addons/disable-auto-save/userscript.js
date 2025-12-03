6af3e13aa44cd9508efae6d729c1748cb931a4e2
DNin01 <106490990+DNin01@users.noreply.github.com>
2025-12-02 17:46:30 -0800

    `disable-auto-save`: "Don't save" option on leave

--- addons/disable-auto-save/userscript.js
+++ addons/disable-auto-save/userscript.js
@@ -1,4 +1,12 @@
 export default async ({ addon, console, msg }) => {
+  function appendChildWithAttributes(parent, type, attrs = {}) {
+    const element = document.createElement(type);
+    Object.assign(element, attrs);
+    parent.appendChild(element);
+    return element;
+  }
+  const PROJECT_PAGE_BUTTON = "[class*='community-button_community-button_']";
+
   addon.tab.redux.initialize();
   addon.tab.redux.addEventListener("statechanged", ({ detail }) => {
     if (addon.self.disabled || detail.action.type !== "timeout/SET_AUTOSAVE_TIMEOUT_ID") return;
@@ -6,8 +14,54 @@ export default async ({ addon, console, msg }) => {
     console.log("Pending autosave prevented.");
   });
 
+  function showSaveAlert() {
+    const modal = addon.tab.createModal(msg("alert-title"), {
+      isOpen: true,
+      useEditorClasses: true,
+    });
+    modal.container.classList.add("unsavedChangesDialog");
+
+    appendChildWithAttributes(modal.content, "p", { textContent: msg("unsaved-changes") });
+    const buttonRow = appendChildWithAttributes(modal.content, "div", {
+      className: addon.tab.scratchClass("prompt_button-row", { others: "unsavedChangesDialog-actions" }),
+    });
+    const buttons = {};
+    for (const option of ["cancel", "discard", "save"]) {
+      buttons[option] = appendChildWithAttributes(buttonRow, "button", {
+        value: option,
+        textContent: msg(option),
+      });
+    }
+    buttons.save.classList.add(addon.tab.scratchClass("prompt_ok-button"));
+
+    // On any button click:
+    buttonRow.addEventListener(
+      "click",
+      async (e) => {
+        if (!e.target.matches("button")) return;
+        // All buttons close the modal
+        modal.remove();
+
+        if (e.target.value === "discard") {
+          // Mark changes as saved to avoid a save-on-navigation
+          addon.tab.redux.dispatch({
+            type: "scratch-gui/project-changed/SET_PROJECT_CHANGED",
+            changed: false,
+          });
+          await new Promise((resolve) => setTimeout(resolve, 0));
+        }
+        if (e.target.value === "discard" || e.target.value === "save") {
+          document.querySelector(PROJECT_PAGE_BUTTON).click();
+        }
+      },
+      { bubble: true }
+    );
+
+    modal.closeButton.addEventListener("click", modal.remove);
+  }
+
   while (true) {
-    const btn = await addon.tab.waitForElement('[class*="community-button_community-button_"]', {
+    const btn = await addon.tab.waitForElement(PROJECT_PAGE_BUTTON, {
       markAsSeen: true,
       reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
       reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
@@ -22,13 +76,14 @@ export default async ({ addon, console, msg }) => {
         // Don't show if it's on someone else's project page,
         // or if there are no changes.
         if (
+          e.isTrusted &&
           !addon.self.disabled &&
           addon.tab.redux.state.scratchGui.projectChanged &&
-          document.querySelector('[class*="project-title-input_title-field_"]') &&
-          !confirm(msg("save-and-leave"))
+          document.querySelector("[class*='project-title-input_title-field_']")
         ) {
           e.preventDefault();
           e.stopPropagation();
+          showSaveAlert();
         }
       },
       { capture: true }
