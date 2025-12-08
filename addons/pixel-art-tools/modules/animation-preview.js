7dcef75f521c552a1746c7641cea7f7ae08178e4
joeclinton1 <joeclinton1@btinternet.com>
2025-12-08 14:30:03 +0000

    use external svg file for range toggle

--- addons/pixel-art-tools/modules/animation-preview.js
+++ addons/pixel-art-tools/modules/animation-preview.js
@@ -238,8 +238,12 @@ export function createAnimationPreview(addon, state, msg) {
       ariaExpanded: "false",
       title: msg("animationRangeToggle") || "Animation range",
     });
-    rangeToggle.innerHTML =
-      '<svg viewBox="0 0 16 16" role="img" aria-hidden="true" focusable="false" class="sa-pixel-art-animation-range-icon"><path d="M4 6.5 8 10l4-3.5" fill="none" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+    const rangeIcon = Object.assign(document.createElement("img"), {
+      src: `${addon.self.dir}/icons/range-toggle.svg`,
+      alt: "",
+      className: "sa-pixel-art-animation-range-icon",
+    });
+    rangeToggle.append(rangeIcon);
 
     const rangeRow = Object.assign(document.createElement("div"), { className: "sa-pixel-art-animation-range" });
     const makeRangeInput = (onChange) => {
