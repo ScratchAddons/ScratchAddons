be160ed57976668e4f49842f41dcf91b653b0e83
Maximouse <mxmou.mouse@gmail.com>
2025-12-12 10:18:29 +0100

    theme -> settings

--- addons/hide-flyout/userscript.js
+++ addons/hide-flyout/userscript.js
@@ -350,7 +350,8 @@ export default async function ({ addon, console, msg }) {
       reduxEvents: [
         "scratch-gui/mode/SET_PLAYER",
         "scratch-gui/locales/SELECT_LOCALE",
-        "scratch-gui/theme/SET_THEME",
+        "scratch-gui/settings/SET_COLOR_MODE",
+        "scratch-gui/settings/SET_THEME",
         "fontsLoaded/SET_FONTS_LOADED",
       ],
       reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
