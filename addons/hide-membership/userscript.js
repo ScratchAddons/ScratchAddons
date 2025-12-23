816b1ca14ff1e7f93677e399a907f87c16f54605
random-beta-tube <purintheo@hotmail.com>
2025-12-23 19:23:23 +0000

    Idk
    There are a million edge cases!

--- addons/hide-membership/userscript.js
+++ addons/hide-membership/userscript.js
@@ -1,15 +1,33 @@
+/*
 export default async function ({ addon, console }) {
-    console.log('123')
-    if (addon.settings.get("flairs") === true){
-        console.log('456')
-        const remove_class = class_name => {
-            const avatars = document.querySelectorAll("."+class_name);
-            console.log(avatars)
-            avatars.forEach(element => {
-                element.classList.remove(class_name);
-            });
-        }
-        remove_class("avatar-badge-wrapper");
-        remove_class("avatar-badge");
+    const ENABLED = true;
+    if (!ENABLED) return;
+    function sub_class(old_name, new_name){
+        const avatars = document.querySelectorAll("."+old_name);
+        console.log(avatars)
+        avatars.forEach(element => {
+            element.classList.add(new_name);
+            element.classList.remove(old_name);
+
+        });
     }
+    function hide(){
+        sub_class("avatar-badge", "avatar-badge-disabled");
+        sub_class("avatar-badge-wrapper", "avatar-badge-wrapper-disabled");
+    }
+    function show(){
+        sub_class("avatar-badge-disabled", "avatar-badge");
+        sub_class("avatar-badge-wrapper-disabled", "avatar-badge-wrapper");
+    }
+    function update(){
+        if (!ENABLED) return;
+        if (addon.settings.get("avatars") === true){
+            hide()
+        } else {
+            show()
+        }
+    }
+    update()
+    addon.settings.addEventListener("change", update)
 }
+*/
