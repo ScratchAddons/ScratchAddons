2be32757bd0489f97a23a3af540bf356191a1e55
redspacecat <162653322+redspacecat@users.noreply.github.com>
2026-01-15 21:44:40 -0800

    add remix tree addon

---
+++ addons/remix-tree-button/main.js
@@ -0,0 +1,30 @@
+export default async function ({ addon, console, msg }) {
+  //define remix tree button elements
+  while (true) {
+    await addon.tab.waitForElement(".flex-row.subactions > .flex-row.action-buttons", {
+      markAsSeen: true,
+      reduxEvents: [
+        "SET_PROJECT_INFO",
+        "scratch-gui/mode/SET_PLAYER",
+        "fontsLoaded/SET_FONTS_LOADED",
+        "scratch-gui/locales/SELECT_LOCALE",
+      ],
+      resizeEvent: true,
+      reduxCondition: (state) => state.scratchGui.mode.isPlayerOnly,
+    });
+    const remixtree = document.createElement("button");
+
+    const remixtreeSpan = document.createElement("span");
+    remixtreeSpan.innerText = msg("remix-tree");
+    addon.tab.displayNoneWhileDisabled(remixtree);
+    remixtree.className = "button action-button remixtree-button";
+    remixtree.id = "scratchAddonsRemixTreeBtn";
+    remixtree.appendChild(remixtreeSpan);
+    remixtree.addEventListener("click", () => {
+      window.location.href = `https://retree.quuq.dev/tree#${
+        window.location.href.split("projects")[1].split("/")[1]
+      }`;
+    });
+    addon.tab.appendToSharedSpace({ space: "afterCopyLinkButton", element: remixtree, order: 0 });
+  }
+}
