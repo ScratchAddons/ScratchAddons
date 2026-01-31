14086d227396de70fd3e3c01fcc270ce7f54683e
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-01-31 16:05:32 -0500

    player-thumb: less strict selectors (#8814)

--- addons/player-thumb/userscript.js
+++ addons/player-thumb/userscript.js
@@ -7,9 +7,9 @@ export default async function ({ addon, console }) {
     `url(https://uploads.scratch.mit.edu/get_image/project/${projectId}_480x360.png)`
   );
 
-  const stageWrapper = await addon.tab.waitForElement('div[class*="stage-wrapper_stage-wrapper_"]');
+  const stageWrapper = await addon.tab.waitForElement('[class*="stage-wrapper_stage-wrapper_"]');
   addon.tab.redux.initialize();
-  const controls = stageWrapper.querySelector('div[class^="controls_controls-container_"]');
+  const controls = stageWrapper.querySelector('[class*="controls_controls-container_"]');
   // Ensure the project has not already loaded when disabling the controls
   if (addon.tab.redux.state?.scratchGui?.projectState?.loadingState === "SHOWING_WITH_ID") return;
   controls.classList.add("sa-controls-disabled");
