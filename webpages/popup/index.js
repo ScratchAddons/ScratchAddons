0a09f8ca103aca2000f8a3ade159a41ae5dc0b73
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-03-17 14:32:38 -0400

    Remove calculatePopupSize()
    This seems to have been for Firefox's extension overflow menu which no
    longer exists.
    
    Still needs testing on mobile.

--- webpages/popup/index.js
+++ webpages/popup/index.js
@@ -2,20 +2,6 @@ import globalTheme from "../../libraries/common/global-theme.js";
 
 globalTheme();
 
-function calculatePopupSize() {
-  if (!window.innerWidth || !window.innerHeight) {
-    setTimeout(calculatePopupSize, 0);
-    return;
-  }
-  let width = window.innerWidth;
-  document.documentElement.style.setProperty("--width", `${width}px`);
-  let height = window.innerHeight - 3;
-  document.documentElement.style.setProperty("--height", `${height}px`);
-  document.body.classList.remove("loading");
-}
-
-window.addEventListener("load", () => setTimeout(calculatePopupSize, 0));
-
 const vue = new Vue({
   el: "body",
   data() {
