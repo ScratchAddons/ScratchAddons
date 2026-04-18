dc92fb9fa8b539fcbb7055cc043aad029544edb1
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-04-18 15:49:55 -0400

    Fix selector again
    Co-authored-by: Maximouse <brestl1c@outlook.com>

--- addons/no-sprite-confirm/userscript.js
+++ addons/no-sprite-confirm/userscript.js
@@ -2,7 +2,7 @@ export default async ({ addon, console }) => {
   document.body.addEventListener("pointerup", () => {
     setTimeout(() => {
       const confirmButton = document.querySelector(
-        "[class*='confirmation-prompt_modal-container_'] [class*='confirmation-prompt_confirm-button_']"
+        "[class*='delete-confirmation-prompt_body_'] [class*='confirmation-prompt_confirm-button_']"
       );
       if (!addon.self.disabled && confirmButton) confirmButton.click();
     }, 0);
