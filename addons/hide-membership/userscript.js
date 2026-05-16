3398e875e337fd0cd64f1daf9ced0fee33f031c6
John <roblox888i12345@gmail.com>
2026-05-16 18:24:01 -0400

    Remove donation banner from userscript
    Removed the donation banner from the user script.

--- addons/hide-membership/userscript.js
+++ addons/hide-membership/userscript.js
@@ -14,24 +14,6 @@ membershipOnProfilePictureAlt.forEach(function(el) {
 membershipOnProfilePicture.forEach(function(el) {
   el.classList.remove('avatar-badge');
   });
-
-const targetBanner = document.querySelector('.become-member-container');
-  if (targetBanner) {
-  targetBanner.outerHTML = `
-  <div class="title-banner donate-banner">
-   <img aria-hidden="true" class="donate-icon" src="/images/ideas/try-it-icon.svg">
-  <div class="donate-central-items">
-   <p class="donate-text"><span>Scratch is a nonprofit that relies on donations to keep our platform free for all kids. Your gift of $5 will make a difference.</span></p>
-   <a href="https://www.scratchfoundation.org/donate" class="button donate-button">
-    <span>Donate</span>
-   </a>
-</div>
- </div>
- <button class="button donate-close-button forms-close-button" name="closeButton" type="button">
- <img alt="close-icon" class="modal-content-close-img" draggable="false" src="/svgs/modal/close-x.svg">
- </button>
-</div>
-`;
   }
 }
 
