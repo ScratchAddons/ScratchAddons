6f98667e3ef0edd9386c98a1b61bfc3e18f22462
John <roblox888i12345@gmail.com>
2026-05-16 21:40:22 -0400

    Remove membership badges from forum avatars
    Added functionality to remove membership badges from forum avatars.

--- addons/hide-membership/userscript.js
+++ addons/hide-membership/userscript.js
@@ -1,11 +1,16 @@
 function removeScratchBadges() {
 const membershipInComments = document.querySelectorAll('.avatar-wrapper.avatar-badge-wrapper');
+const membershipInForums = document.querySelectorAll('.avatar-badge-wrapper');
 const membershipOnProfilePicture = document.querySelectorAll('.user-icon.avatar-badge');
 const membershipOnProfilePictureAlt = document.querySelectorAll('.avatar.avatar-badge');
 
 membershipInComments.forEach(function(el) {
   el.classList.remove('avatar-badge-wrapper');
   });
+
+membershipInForums.forEach(function(el) {
+  el.classList.remove('avatar-badge-wrapper');
+});
 
 membershipOnProfilePictureAlt.forEach(function(el) {
   el.classList.remove('avatar-badge-wrapper');
