3cabeb244fd320195b33391baaaa2fafdb1c0935
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-06-05 21:31:30 -0400

    custom-menu-bar fixes

--- addons/custom-menu-bar/username.js
+++ addons/custom-menu-bar/username.js
@@ -1,10 +1,8 @@
 export default async function ({ addon }) {
   while (true) {
     // Profile account menu item
-    const menuItem = await addon.tab.waitForElement("[class*=account-nav_user-info_] + div > ul > :first-child", {
+    const menuItem = await addon.tab.waitForElement("[class*=account-menu_user-info_] + div > ul > :first-child", {
       markAsSeen: true,
-      reduxEvents: ["scratch-gui/menus/OPEN_MENU"],
-      reduxCondition: (state) => state.scratchGui.menus.accountMenu,
     });
     if (addon.settings.get("compact-username")) {
       const usernameSpan = document.createElement("span");
