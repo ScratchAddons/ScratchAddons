d4fc4502cd20292f1185064bd8daaca53493a28b
Maximouse <mxmou.mouse@gmail.com>
2026-06-06 13:31:38 +0200

    editor-unshare-button: update selector

--- addons/editor-unshare-button/userscript.js
+++ addons/editor-unshare-button/userscript.js
@@ -1,7 +1,7 @@
 export default async function ({ addon, msg, console }) {
   const { redux } = addon.tab;
   while (true) {
-    const button = await addon.tab.waitForElement("span[class*='share-button_share-button'][class*='is-shared']", {
+    const button = await addon.tab.waitForElement("[class*='share-button_share-button_'][class*='is-shared']", {
       reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
       markAsSeen: true,
     });
