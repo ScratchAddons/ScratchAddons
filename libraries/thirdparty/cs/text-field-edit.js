dcd419fa7b6bbfd9008ec5758bbcb5ae2c72d64d
apple502j <33279053+apple502j@users.noreply.github.com>
2026-07-28 05:23:44 +0900

    Update text-field-edit

--- libraries/thirdparty/cs/text-field-edit.js
+++ libraries/thirdparty/cs/text-field-edit.js
@@ -1,75 +1,155 @@
-// https://github.com/fregante/text-field-edit/issues/16
-function safeTextInsert(text) {
-    if (text === '') {
-        return document.execCommand('delete');
-    }
-    return document.execCommand('insertText', false, text);
+function isNativeField(field) {
+    return field instanceof HTMLInputElement || field instanceof HTMLTextAreaElement;
 }
-function insertTextFirefox(field, text) {
-    // Found on https://www.everythingfrontend.com/posts/insert-text-into-textarea-at-cursor-position.html 🎈
-    field.setRangeText(text, field.selectionStart || 0, field.selectionEnd || 0, 'end');
-    field.dispatchEvent(new InputEvent('input', {
-        data: text,
-        inputType: 'insertText',
-    }));
+/** Call a function after focusing a field and then restore the previous focus afterwards if necessary */
+function withFocus(field, callback) {
+    const document = field.ownerDocument;
+    const initialFocus = document.activeElement;
+    if (initialFocus === field) {
+        return callback();
+    }
+    try {
+        field.focus();
+        return callback();
+    }
+    finally {
+        field.blur(); // Supports `intialFocus === body`
+        if (initialFocus instanceof HTMLElement) {
+            initialFocus.focus();
+        }
+    }
+}
+// This will insert into the focused field. It shouild always be called inside withFocus.
+// Use this one locally if there are multiple `insertTextIntoField` or `document.execCommand` calls
+function insertTextWhereverTheFocusIs(document, text) {
+    if (text === '') {
+        // https://github.com/fregante/text-field-edit/issues/16
+        document.execCommand('delete');
+    }
+    else {
+        document.execCommand('insertText', false, text);
+    }
 }
 /** Inserts `text` at the cursor’s position, replacing any selection, with **undo** support and by firing the `input` event. */
-export function insert(field, text) {
-    var document = field.ownerDocument;
-    var initialFocus = document.activeElement;
-    if (initialFocus !== field) {
-        field.focus();
-    }
-    if (!safeTextInsert(text)) {
-        insertTextFirefox(field, text);
-    }
-    if (initialFocus === document.body) {
-        field.blur();
-    }
-    else if (initialFocus instanceof HTMLElement && initialFocus !== field) {
-        initialFocus.focus();
-    }
+export function insertTextIntoField(field, text) {
+    withFocus(field, () => {
+        insertTextWhereverTheFocusIs(field.ownerDocument, text);
+    });
 }
 /** Replaces the entire content, equivalent to `field.value = text` but with **undo** support and by firing the `input` event. */
-export function set(field, text) {
-    field.select();
-    insert(field, text);
+export function setFieldText(field, text) {
+    if (isNativeField(field)) {
+        field.select();
+        insertTextIntoField(field, text);
+    }
+    else {
+        const document = field.ownerDocument;
+        withFocus(field, () => {
+            document.execCommand('selectAll', false, text);
+            insertTextWhereverTheFocusIs(document, text);
+        });
+    }
 }
 /** Get the selected text in a field or an empty string if nothing is selected. */
-export function getSelection(field) {
-    return field.value.slice(field.selectionStart, field.selectionEnd);
+export function getFieldSelection(field) {
+    if (isNativeField(field)) {
+        return field.value.slice(field.selectionStart, field.selectionEnd);
+    }
+    const selection = field.ownerDocument.getSelection();
+    if (selection && field.contains(selection.anchorNode)) {
+        // The selection is inside the field
+        return selection.toString();
+    }
+    return '';
 }
-/** Adds the `wrappingText` before and after field’s selection (or cursor). If `endWrappingText` is provided, it will be used instead of `wrappingText` at on the right. */
-export function wrapSelection(field, wrap, wrapEnd) {
-    var selectionStart = field.selectionStart, selectionEnd = field.selectionEnd;
-    var selection = getSelection(field);
-    insert(field, wrap + selection + (wrapEnd !== null && wrapEnd !== void 0 ? wrapEnd : wrap));
+function wrapFieldSelectionNative(field, wrap, wrapEnd) {
+    const { selectionStart, selectionEnd } = field;
+    const selection = getFieldSelection(field);
+    insertTextIntoField(field, wrap + selection + wrapEnd);
     // Restore the selection around the previously-selected text
     field.selectionStart = selectionStart + wrap.length;
     field.selectionEnd = selectionEnd + wrap.length;
 }
+function collapseCursor(selection, range, toStart) {
+    const alteredRange = range.cloneRange();
+    alteredRange.collapse(toStart);
+    selection.removeAllRanges();
+    selection.addRange(alteredRange);
+}
+function wrapFieldSelectionContentEditable(field, before, after) {
+    const document = field.ownerDocument;
+    const selection = document.getSelection();
+    const selectionRange = selection.getRangeAt(0);
+    if (after) {
+        collapseCursor(selection, selectionRange, false);
+        insertTextIntoField(field, after);
+    }
+    if (before) {
+        collapseCursor(selection, selectionRange, true);
+        insertTextIntoField(field, before);
+        // The text added by at the beginning is included in the new selection, while wrapEnd isn't.
+        // This nudges the selection after the newly-inserted text.
+        selectionRange.setStart(selectionRange.startContainer, selectionRange.startOffset + before.length);
+    }
+    if (after ?? before) {
+        // Restore selection
+        selection.removeAllRanges();
+        selection.addRange(selectionRange);
+    }
+}
+/** Adds the `wrappingText` before and after field’s selection (or cursor). If `endWrappingText` is provided, it will be used instead of `wrappingText` at on the right. */
+export function wrapFieldSelection(field, wrap, 
+// TODO: Ensure that it works regardless of direction
+wrapEnd = wrap) {
+    if (isNativeField(field)) {
+        wrapFieldSelectionNative(field, wrap, wrapEnd);
+    }
+    else {
+        wrapFieldSelectionContentEditable(field, wrap, wrapEnd);
+    }
+}
 /** Finds and replaces strings and regex in the field’s value, like `field.value = field.value.replace()` but better */
-export function replace(field, searchValue, replacer, cursor) {
-    if (cursor === void 0) { cursor = 'select'; }
-    /** Remembers how much each match offset should be adjusted */
-    var drift = 0;
-    field.value.replace(searchValue, function () {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        // Select current match to replace it later
-        var matchStart = drift + args[args.length - 2];
-        var matchLength = args[0].length;
-        field.selectionStart = matchStart;
-        field.selectionEnd = matchStart + matchLength;
-        var replacement = typeof replacer === 'string' ? replacer : replacer.apply(void 0, args);
-        insert(field, replacement);
-        if (cursor === 'select') {
-            // Select replacement. Without this, the cursor would be after the replacement
+export function replaceFieldText(field, searchValue, replacer, cursor = 'select') {
+    if (!isNativeField(field)) {
+        throw new TypeError('replaceFieldText only supports input and textarea fields');
+    }
+    /** Keeps track of how much each match offset should be adjusted */
+    let drift = 0;
+    withFocus(field, () => {
+        field.value.replace(searchValue, (...arguments_) => {
+            // Select current match to replace it later
+            const matchStart = drift + arguments_.at(-2);
+            const matchLength = arguments_[0].length;
             field.selectionStart = matchStart;
-        }
-        drift += replacement.length - matchLength;
-        return replacement;
+            field.selectionEnd = matchStart + matchLength;
+            const replacement = typeof replacer === 'string' ? replacer : replacer(...arguments_);
+            insertTextWhereverTheFocusIs(field.ownerDocument, replacement);
+            if (cursor === 'select') {
+                // Select replacement. Without this, the cursor would be after the replacement
+                field.selectionStart = matchStart;
+            }
+            drift += replacement.length - matchLength;
+            return replacement;
+        });
     });
 }
+/** @deprecated Import `insertTextIntoField` instead */
+export const insert = insertTextIntoField;
+/** @deprecated Import `setFieldText` instead */
+export const set = setFieldText;
+/** @deprecated Import `replaceFieldText` instead */
+export const replace = replaceFieldText;
+/** @deprecated Import `wrapFieldSelection` instead */
+export const wrapSelection = wrapFieldSelection;
+/** @deprecated Import `getFieldSelection` instead */
+export const getSelection = getFieldSelection;
+// Note: Don't reuse deprecated constant from above
+const textFieldEdit = {
+    insert: insertTextIntoField,
+    set: setFieldText,
+    replace: replaceFieldText,
+    wrapSelection: wrapFieldSelection,
+    getSelection: getFieldSelection,
+};
+export default textFieldEdit;
+export { withFocus as _TEST_ONLY_withFocus };
