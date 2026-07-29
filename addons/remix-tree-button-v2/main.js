3c8921be5095086c23429471144afcc8f7fa25ca
redspacecat <162653322+redspacecat@users.noreply.github.com>
2026-07-29 07:35:47 -0700

    Readd remix tree (#8779)
    * add remix tree addon
    
    * remove unused files
    
    * Update addon.json on suggestion
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    * add website to description
    
    * format code with prettier
    
    * change addon name
    
    * Update addon versionAdded
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    * add credit
    
    * make remixtree open in new tab
    
    I tested and figured this was better, but I'm not sure
    
    * Update addons/remix-tree-button-v2/main.js
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    * separate project id
    
    * update class name
    
    * trim url
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    * update other remixtree-button uses
    
    * Update forgotten css
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Samq64 <81489795+Samq64@users.noreply.github.com>
    Co-authored-by: DNin01 <106490990+DNin01@users.noreply.github.com>

---
+++ addons/remix-tree-button-v2/main.js
@@ -0,0 +1,29 @@
+export default async function ({ addon, console, msg }) {
+  //define remix tree button elements
+  while (true) {
+    await addon.tab.waitForElement(".flex-row.subactions > .flex-row.action-buttons", {
+      markAsSeen: true,
+      reduxEvents: [
+        "SET_PROJECT_INFO",
+        "scratch-gui/mode/SET_PLAYER",
+        "fontsLoaded/SET_FONTS_LOADED",
+        "scratch-gui/locales/SELECT_LOCALE",
+      ],
+      resizeEvent: true,
+      reduxCondition: (state) => state.scratchGui.mode.isPlayerOnly,
+    });
+    const remixtree = document.createElement("button");
+
+    const remixtreeSpan = document.createElement("span");
+    remixtreeSpan.innerText = msg("remix-tree");
+    addon.tab.displayNoneWhileDisabled(remixtree);
+    remixtree.className = "button action-button sa-remixtree-button";
+    remixtree.id = "scratchAddonsRemixTreeBtn";
+    remixtree.appendChild(remixtreeSpan);
+    remixtree.addEventListener("click", () => {
+      const projectID = window.location.href.split("projects")[1].split("/")[1];
+      window.open(`https://retree.quuq.dev/tree#${projectID}`, "_blank", "noopener,noreferrer");
+    });
+    addon.tab.appendToSharedSpace({ space: "afterCopyLinkButton", element: remixtree, order: 0 });
+  }
+}
