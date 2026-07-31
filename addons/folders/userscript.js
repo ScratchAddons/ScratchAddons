0005ca15404492c1a084a3782ed2439de7aa05a5
Maximouse <mxmou.mouse@gmail.com>
2026-07-31 20:53:22 +0200

    folders: fix backpack crash

--- addons/folders/userscript.js
+++ addons/folders/userscript.js
@@ -320,7 +320,7 @@ export default async function ({ addon, console, msg }) {
       const Backpack = backpackInstance.constructor;
       return (
         typeof Backpack.prototype.handleDrop === "function" &&
-        typeof Backpack.prototype.componentDidUpdate === "undefined"
+        typeof Backpack.prototype.componentDidUpdate === "function"
       );
     } catch {
       return false;
@@ -1396,7 +1396,9 @@ export default async function ({ addon, console, msg }) {
       }
     };
 
+    const originalComponentDidUpdate = Backpack.prototype.componentDidUpdate;
     Backpack.prototype.componentDidUpdate = function (prevProps, prevState) {
+      originalComponentDidUpdate.call(this, prevProps, prevState);
       if (!this.state.loading && prevState.loading && !this.state.error) {
         this.sa_loadNextItem();
       }
