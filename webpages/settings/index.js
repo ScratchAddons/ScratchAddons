5b79611faac2c8a9a51c16f342dad82bcc7b0556
DNin01 <106490990+DNin01@users.noreply.github.com>
2026-08-06 16:45:19 -0700

    Update index.js
    Remove guard, rename some variables

--- webpages/settings/index.js
+++ webpages/settings/index.js
@@ -551,23 +551,18 @@ let fuse;
       manifest._addonId = addonId;
       manifest._groups = [];
 
-      // In production, all addons should have versionAdded.
-      // However, it shouldn't have to be set in stone during development.
-      if (manifest.versionAdded) {
-        const [extMajor, extMinor, _] = vue.version.split(".");
-        const [addonMajor, addonMinor, __] = manifest.versionAdded.split(".");
-        if (extMajor === addonMajor && extMinor === addonMinor) {
-          manifest.tags.push("new");
-          manifest._groups.push(
-            manifest.tags.includes("recommended") || manifest.tags.includes("featured") ? "featuredNew" : "new"
-          );
-        } else if (manifest.latestUpdate) {
-          const [extMajor, extMinor, _] = vue.version.split(".");
-          const [addonMajor, addonMinor, __] = manifest.latestUpdate.version.split(".");
-          if (extMajor === addonMajor && extMinor === addonMinor) {
-            manifest.tags.push(manifest.latestUpdate.newSettings?.length ? "updatedWithSettings" : "updated");
-            manifest._groups.push(manifest.latestUpdate.isMajor ? "featuredNew" : "new");
-          }
+      const [extMajor, extMinor] = vue.version.split(".");
+      const [addonAddedMajor, addonAddedMinor] = manifest.versionAdded.split(".");
+      if (extMajor === addonAddedMajor && extMinor === addonAddedMinor) {
+        manifest.tags.push("new");
+        manifest._groups.push(
+          manifest.tags.includes("recommended") || manifest.tags.includes("featured") ? "featuredNew" : "new"
+        );
+      } else if (manifest.latestUpdate) {
+        const [addonUpdateMajor, addonUpdateMinor] = manifest.latestUpdate.version.split(".");
+        if (extMajor === addonUpdateMajor && extMinor === addonUpdateMinor) {
+          manifest.tags.push(manifest.latestUpdate.newSettings?.length ? "updatedWithSettings" : "updated");
+          manifest._groups.push(manifest.latestUpdate.isMajor ? "featuredNew" : "new");
         }
       }
 
